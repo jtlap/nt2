ef6c6a1d01a04cf6324eea6b1ae1fe4e54c971a9
joel <joel@skell-r8.(none)>
2010-06-04 09:04:07 +0200

    Top level sdk/config header

---
+++ include/nt2/sdk/config.hpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_NT2_SDK_CONFIG_HPP_INCLUDED
+#define NT2_NT2_SDK_CONFIG_HPP_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+//Includes NT2 configuration files
+////////////////////////////////////////////////////////////////////////////////
+#include <nt2/sdk/config/version.hpp>
+#include <nt2/sdk/config/arch.hpp>
+#include <nt2/sdk/config/compiler.hpp>
+#include <nt2/sdk/config/os.hpp>
+
+//#include <nt2/extensions/sdk/config.hpp>
+#endif
