0d57f787794bcf4501fb75716bfc408b28cb6519
Joel Falcou <joel.falcou@lri.fr>
2010-06-15 23:06:18 +0200

    Added system level helpers

---
+++ include/nt2/sdk/sys/string.hpp
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_NT2_SDK_SYS_STRING_HPP_INCLUDED
+#define NT2_NT2_SDK_SYS_STRING_HPP_INCLUDED
+
+#include <stdio.h>
+#include <cstddef>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <algorithm>
+#include <nt2/sdk/config/compiler.hpp>
+
+namespace nt2 { namespace sys
+{
+  //////////////////////////////////////////////////////////////////////////////
+  // Lightweight, read-only, string class with formating constructor
+  //////////////////////////////////////////////////////////////////////////////
+  struct string
+  {
+    string() : data(0),sz(0) {}
+
+    string(std::size_t n) : data(0),sz(n)
+    {
+      if(sz)  data = (char*)malloc(sizeof(char)*(sz+1));
+    }
+
+    ~string() { if(data) free(data); }
+
+    string(char const* format, ...) : data(0)
+    {
+      va_list ap;
+      va_start( ap, format );
+
+      #if defined(NT2_COMPILER_MSVC)
+      sz  = _vscprintf( format, ap );
+      #else
+      sz = vsnprintf( 0, 0, format, ap );
+      #endif
+
+      if(sz) data = (char*)malloc(sizeof(char)*(sz+1));
+
+      #if defined(NT2_COMPILER_MSVC)
+      _vsnprintf_s( data, sz, _TRUNCATE, format, ap );
+      #else
+      vsnprintf( data, sz+1, format, ap );
+      #endif
+
+      va_end( ap );
+    }
+
+    string& operator=( string src ) { swap(src); return *this; }
+
+    void swap( string& src )
+    {
+      std::swap(data,src.data);
+      std::swap(sz,src.sz);
+    }
+
+    char const* c_str() const { return data; }
+    char*       c_str()       { return data; }
+
+    private:
+    char*       data;
+    std::size_t sz;
+  };
+} }
+
+#endif
---
+++ include/nt2/sdk/sys/timestamp.hpp
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_NT2_SDK_SYS_TIMESTAMP_HPP_INCLUDED
+#define NT2_NT2_SDK_SYS_TIMESTAMP_HPP_INCLUDED
+
+#include <time.h>
+#include <nt2/sdk/sys/string.hpp>
+
+namespace nt2 { namespace sys
+{
+  //////////////////////////////////////////////////////////////////////////////
+  // Return a string with time stamp in readable human format
+  //////////////////////////////////////////////////////////////////////////////
+  sys::string timestamp()
+  {
+    time_t rawtime;
+    char buffer[64];
+
+    time(&rawtime);
+    strftime(buffer,64,"%c",localtime(&rawtime));
+
+    return sys::string(buffer);
+  }
+} }
+
+#endif
