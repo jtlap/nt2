00bc02dc6478d2eb76a3a9a98ad5a9c8171748d6
Joel Falcou <joel.falcou@lri.fr>
2010-06-20 21:56:49 +0200

    Added linear level include for sdk/errors and sdk/sys

---
+++ include/nt2/include/errors.hpp
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_INCLUDE_ERRORS_HPP_INCLUDED
+#define NT2_INCLUDE_ERRORS_HPP_INCLUDED
+
+#include <nt2/sdk/errors/error.hpp>
+#include <nt2/sdk/errors/failure.hpp>
+#include <nt2/sdk/errors/warning.hpp>
+#include <nt2/sdk/errors/concepts.hpp>
+#include <nt2/sdk/errors/static_assert.hpp>
+
+#endif
