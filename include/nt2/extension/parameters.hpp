86d7938c61cd65cce51b7cba0c227fc06119d84f
Joel Falcou <joel.falcou@lri.fr>
2010-07-03 09:17:19 +0200

    Added external parameters file in nt2/extension/

---
+++ include/nt2/extension/parameters.hpp
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_EXTENSION_PARAMETERS_HPP_INCLUDED
+#define NT2_EXTENSION_PARAMETERS_HPP_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+// Global parameters for nt2 code generator
+//* TODO Documentation: http://nt2.lri.fr/etensions/parameters.hpp
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Maximum number of dimensions for all container
+////////////////////////////////////////////////////////////////////////////////
+#define NT2_MAX_DIMENSIONS  4
+
+////////////////////////////////////////////////////////////////////////////////
+// Default base index for container
+////////////////////////////////////////////////////////////////////////////////
+#define NT2_DEFAULT_INDEX   1
+
+#endif
