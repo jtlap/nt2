b44260a925722079984c060d598da5a65383f1df
Joel Falcou <joel.falcou@lri.fr>
2010-07-15 15:15:14 +0200

    Added static constant definition for unknown and category so no link error appears in unit tests.

--- include/nt2/sdk/meta/unknown.hpp
+++ include/nt2/sdk/meta/unknown.hpp
@@ -20,8 +20,13 @@ namespace nt2 { namespace tag
   struct unknown
   {
     typedef unknown tag;
-    BOOST_STATIC_CONSTANT(boost::uint32_t, rank = boost::uint32_t(~0));
+    static const boost::uint32_t rank = boost::uint32_t(~0);
   };
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Required to prevent spurrious link errors
+  //////////////////////////////////////////////////////////////////////////////
+  const boost::uint32_t unknown::rank;
 } }
 
 #endif
