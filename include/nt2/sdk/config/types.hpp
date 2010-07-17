2d44c8b2b43bae20647eb2bd8bb00a0163d5e22b
Joel Falcou <joel.falcou@lri.fr>
2010-07-17 13:59:56 +0200

    Added sdk/config/types.hpp to inject boost cstdint types into nt2. This simplify and deambiguates which (u)int<n>_t is used.

---
+++ include/nt2/sdk/config/types.hpp
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_CONFIG_TYPES_HPP_INCLUDED
+#define NT2_SDK_CONFIG_TYPES_HPP_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+// Types injection into nt2
+// Injects or define some absic types in NT2 namespace for consistency sake.
+//* TODO : Documentation: http://nt2.lri.fr/doc/build/html/sdk/config/types.html
+////////////////////////////////////////////////////////////////////////////////
+#include <boost/config.hpp>
+#include <boost/cstdint.hpp>
+#include <nt2/sdk/config/details/boost.hpp>
+#include <nt2/sdk/config/details/reporter.hpp>
+
+namespace nt2
+{
+  using ::boost::uint8_t;
+  using ::boost::uint16_t;
+  using ::boost::uint32_t;
+  using ::boost::uint64_t;
+  using ::boost::int8_t;
+  using ::boost::int16_t;
+  using ::boost::int32_t;
+  using ::boost::int64_t;
+}
+
+#endif
