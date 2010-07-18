31153d8533df31b8bdb35361b63f450bca6794de
Joel Falcou <joel.falcou@lri.fr>
2010-07-18 09:45:03 +0200

    Added main include file for functor in nt2/include/

---
+++ include/nt2/include/functor.hpp
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_INCLUDE_FUNCTOR_HPP_INCLUDED
+#define NT2_INCLUDE_FUNCTOR_HPP_INCLUDED
+
+#include <nt2/sdk/functor/functor.hpp>
+#include <nt2/sdk/functor/category.hpp>
+#include <nt2/sdk/functor/hierarchy.hpp>
+#include <nt2/sdk/functor/operators.hpp>
+
+#endif
