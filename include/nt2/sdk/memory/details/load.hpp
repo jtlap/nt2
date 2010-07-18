f8980107560132e4e2e65310f46e37d7eedc5039
Joel Falcou <joel.falcou@lri.fr>
2010-07-18 20:28:10 +0200

    Added sdk/memory component. Added load and store functor and function. Added load/store for scalar values specializations.

---
+++ include/nt2/sdk/memory/details/load.hpp
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_MEMORY_DETAILS_LOAD_HPP_INCLUDED
+#define NT2_SDK_MEMORY_DETAILS_LOAD_HPP_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+// load for scalar types
+////////////////////////////////////////////////////////////////////////////////
+#include <nt2/sdk/functor/category.hpp>
+#include <boost/type_traits/remove_pointer.hpp>
+#include <nt2/sdk/functor/preprocessor/call.hpp>
+
+namespace nt2 { namespace functors
+{
+  template<class T, int32_t Offset,class Category, class Info>
+  struct call<load_<T,Offset>,tag::scalar_(Category), Info>
+  {
+    typedef T result_type;
+    NT2_FUNCTOR_CALL(2) { return a0[a1 + Offset]; }
+  };
+} }
+
+#endif
---
+++ include/nt2/sdk/memory/details/store.hpp
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_MEMORY_DETAILS_STORE_HPP_INCLUDED
+#define NT2_SDK_MEMORY_DETAILS_STORE_HPP_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+// load for scalar types
+////////////////////////////////////////////////////////////////////////////////
+#include <nt2/sdk/functor/category.hpp>
+#include <boost/type_traits/remove_pointer.hpp>
+#include <nt2/sdk/functor/preprocessor/call.hpp>
+
+namespace nt2 { namespace functors
+{
+  template<class Category, class Info>
+  struct call<store_,tag::scalar_(Category), Info>
+  {
+    template<class Sig> struct result;
+    template<class This,class A0,class A1, class A2>
+    struct  result<This(A0,A1,A2)> { typedef A0 type; };
+
+    NT2_FUNCTOR_CALL(3) { return a1[a2] = a0; }
+  };
+} }
+
+#endif
