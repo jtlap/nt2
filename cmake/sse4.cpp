b4ba772adfcdc702ea12563e4d1f39f8c8d45a48
Joel Falcou <joel.falcou@lri.fr>
2010-07-27 10:14:35 +0200

    Fixed incorrect SSE4 detection code

--- cmake/sse4.cpp
+++ cmake/sse4.cpp
@@ -1,8 +1,7 @@
 #include <smmintrin.h>
-#include <ammintrin.h>
 
 int main() 
 { 
-  __m128d x = _mm_blend_pd(_mm_set1_pd(0),_mm_set1_pd(0));
+  __m128d x = _mm_blend_pd(_mm_set1_pd(0),_mm_set1_pd(0),0);
   return 1; 
 }
