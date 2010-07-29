3c54ba3240b3310c9c3eeea2e0820b2b7a7c7c58
Joel Falcou <joel.falcou@lri.fr>
2010-07-29 17:58:11 +0200

    Added memory and error related config reports

--- include/nt2/sdk/memory/config.hpp
+++ include/nt2/sdk/memory/config.hpp
@@ -6,24 +6,26 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#ifndef NT2_SDK_CONFIG_FLAGS_HPP_INCLUDED
-#define NT2_SDK_CONFIG_FLAGS_HPP_INCLUDED
+#ifndef NT2_SDK_MEMORY_CONFIG_HPP_INCLUDED
+#define NT2_SDK_MEMORY_CONFIG_HPP_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+// Memory config header
+// TODO: Documentation: http://nt2.lri.fr/sdk/memory/config.html
+////////////////////////////////////////////////////////////////////////////////
+#include <nt2/sdk/config.hpp>
 
 namespace nt2 { namespace config
 {
   //////////////////////////////////////////////////////////////////////////////
-  // Status header reporter - Head for the reporter list
+  // Memory configuration status reporter
   //////////////////////////////////////////////////////////////////////////////
-  static void flags()
+  static void memories()
   {
-    #if defined(NT2_VERBOSE)
-    puts(" Verbose mode            : activated" );
-    #else
-    puts(" Verbose mode            : desactivated" );
-    #endif
+    printf(" Memory alignment        : %d\n\n", NT2_CONFIG_ALIGNMENT );
   }
 
-  NT2_REGISTER_STATUS(flags);
+  NT2_REGISTER_STATUS(memories);
 } }
 
 #endif
