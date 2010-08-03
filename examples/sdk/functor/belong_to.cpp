744cdf02d88d3dc552a3b14b92ae98ae7432ccc7
Joel Falcou <joel.falcou@lri.fr>
2010-08-03 18:57:09 +0200

    Fixed error in belong_to sample

--- examples/sdk/functor/belong_to.cpp
+++ examples/sdk/functor/belong_to.cpp
@@ -11,7 +11,7 @@ int main()
   using nt2::meta::set;
   using nt2::meta::belong_to;
 
-  printf("Dos foo belong in {foo,bar,chu} : %d\n",belong_to<foo, set<foo,bar,chu> >::value) );
-  printf("Dos foo belong in {foo} : %d\n",belong_to<foo, set<foo> >::value) );
-  printf("Dos foo belong in foo : %d\n",belong_to<foo, foo> >::value) );
+  printf("Does foo belong in {foo,bar,chu} : %d\n", belong_to<foo, set<foo,bar,chu> >::value );
+  printf("Does foo belong in {foo} : %d\n"        , belong_to<foo, set<foo> >::value );
+  printf("Does foo belong in foo : %d\n"          , belong_to<foo, foo>::value );
 }
