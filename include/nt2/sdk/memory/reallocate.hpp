ee4767bb407892049de87542d70088c18e47c356
Joel Falcou <joel.falcou@lri.fr>
2010-08-04 22:09:56 +0200

    Added memory allocation,deallcoation and reallcoation raw function Added Allocator Concept

---
+++ include/nt2/sdk/memory/reallocate.hpp
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_MEMORY_REALLOCATE_HPP_INCLUDED
+#define NT2_SDK_MEMORY_REALLOCATE_HPP_INCLUDED
+
+#include <cstddef>
+#include <nt2/sdk/errors/error.hpp>
+#include <nt2/sdk/memory/config.hpp>
+#include <nt2/sdk/memory/align_on.hpp>
+
+namespace nt2 { namespace memory
+{
+  //////////////////////////////////////////////////////////////////////////////
+  // Reallocate a raw buffer of bytes to a new size
+  //////////////////////////////////////////////////////////////////////////////
+  byte* reallocate( byte* ptr, std::size_t nbytes, std::size_t obytes);
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Reallocate a raw buffer of bytes to a new size using an allocator
+  //////////////////////////////////////////////////////////////////////////////
+  template<class Allocator> static inline
+  byte* reallocate( Allocator& a,  byte* ptr, std::size_t nbytes, std::size_t obytes)
+  {
+    byte* result;
+    if(obytes < nbytes)
+    {
+      byte* tmp = reinterpret_cast<byte*>(allocate(a,nbytes));
+      ::memmove(tmp,ptr,obytes);
+      deallocate(a,ptr,obytes);
+      result = tmp;
+    }
+    else
+    {
+      result = ptr;
+    }
+
+    return result;
+  }
+} }
+
+#endif
---
+++ src/reallocate.cpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#include <nt2/sdk/memory/allocate.hpp>
+#include <nt2/sdk/memory/deallocate.hpp>
+#include <nt2/sdk/memory/reallocate.hpp>
+
+namespace nt2 { namespace memory
+{
+  //////////////////////////////////////////////////////////////////////////////
+  // Reallocate a raw buffer of bytes to a new size
+  //////////////////////////////////////////////////////////////////////////////
+  byte* reallocate( byte* ptr, std::size_t nbytes, std::size_t obytes)
+  {
+    byte* result;
+
+    #if (defined _MSC_VER)
+    ////////////////////////////////////////////////////////////////////////////
+    // MSVC systems use _aligned_realloc
+    ////////////////////////////////////////////////////////////////////////////
+    result = _aligned_realloc(ptr, nbytes, NT2_CONFIG_ALIGNMENT);
+    #else
+    ////////////////////////////////////////////////////////////////////////////
+    // Other systems allocate/copy/deallocate
+    ////////////////////////////////////////////////////////////////////////////
+    if(obytes < nbytes)
+    {
+      byte* tmp = reinterpret_cast<byte*>(allocate(nbytes));
+      ::memmove(tmp,ptr,obytes);
+      deallocate(ptr);
+      result = tmp;
+    }
+    else
+    {
+      result = ptr;
+    }
+    return result;
+    #endif
+  }
+} }
