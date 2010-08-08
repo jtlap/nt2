a63d04e8a8752d2868404118ebe0f1a718c45ccc
Joel Falcou <joel.falcou@lri.fr>
2010-08-08 16:48:24 +0200

    Added stub forwarding operators file for SSEx > 2 Fixed small typos and missing ref in simd/include

---
+++ include/nt2/sdk/simd/details/impl/sse/sse4_1/is_equal_to.hpp
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_SIMD_NATIVE_DETAILS_IMPL_SSE_SSE4_1_EQUAL_TO_HPP_INCLUDED
+#define NT2_SDK_SIMD_NATIVE_DETAILS_IMPL_SSE_SSE4_1_EQUAL_TO_HPP_INCLUDED
+
+namespace nt2 { namespace functors
+{
+  template<,class Dummy>
+  struct call<is_equal_,tag::simd_(tag::arithmetic_,tag::sse_),Dummy>
+  {
+    template<class Sig> struct result;
+    template<class This,class A> struct result<This(A,A)> { typedef A type; };
+
+    NT2_FUNCTOR_CALL_DISPATCH( 2
+                             , typename nt2::meta::scalar_of<A0>::type
+                             , (6,(double,float,int8_,int16_,int32_,int64_))
+                             )
+
+    NT2_FUNCTOR_CALL_EVAL_IF(2,double)  { A0 that = { _mm_cmpeq_pd(a0,a1)    }; return that; }
+    NT2_FUNCTOR_CALL_EVAL_IF(2,float )  { A0 that = { _mm_cmpeq_ps(a0,a1)    }; return that; }
+    NT2_FUNCTOR_CALL_EVAL_IF(2,int8_ )  { A0 that = { _mm_cmpeq_epi8(a0,a1)  }; return that; }
+    NT2_FUNCTOR_CALL_EVAL_IF(2,int16_)  { A0 that = { _mm_cmpeq_epi16(a0,a1) }; return that; }
+    NT2_FUNCTOR_CALL_EVAL_IF(2,int32_)  { A0 that = { _mm_cmpeq_epi32(a0,a1) }; return that; }
+    NT2_FUNCTOR_CALL_EVAL_IF(2,int64_)  { A0 that = { _mm_cmpeq_epi64(a0,a1) }; return that; }
+  };
+} }
+
+#endif
