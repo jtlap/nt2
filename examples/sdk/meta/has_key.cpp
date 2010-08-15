7bfdb92fc35ae8e518d3c2495cd91d5d7e1533c3
Joel Falcou <joel.falcou@lri.fr>
2010-08-15 12:18:18 +0200

    Corrected has_key examples thansk to Goten.

--- examples/sdk/meta/has_key.cpp
+++ examples/sdk/meta/has_key.cpp
@@ -8,7 +8,7 @@ int main()
   typedef nt2::meta::set<char,short,int>  ints;
 
   printf("Is int    in reals: %d\n",nt2::meta::has_key<reals,int>::value);
-  printf("Is int    in ints : %d\n",nt2::meta::has_key<ints,void>::value);
+  printf("Is int    in ints : %d\n",nt2::meta::has_key<ints ,int>::value);
   printf("Is double in reals: %d\n",nt2::meta::has_key<reals,double>::value);
-  printf("Is double in ints : %d\n",nt2::meta::has_key<ints,double>::value);
+  printf("Is double in ints : %d\n",nt2::meta::has_key<ints ,double>::value);
 }
