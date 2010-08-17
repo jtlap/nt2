206a5db3d7bc18f8dc41cca12c45c57cae95cd1c
Joel Falcou <joel.falcou@lri.fr>
2010-08-17 10:49:02 +0200

    Fixed improper usage of int32_t in any.cpp example

--- examples/sdk/meta/any.cpp
+++ examples/sdk/meta/any.cpp
@@ -8,11 +8,11 @@ using namespace boost::mpl;
 
 int main()
 {
-  typedef vector<int32_t,bool,float> seq_t;
+  typedef vector<int,bool,float> seq_t;
 
   typedef nt2::meta::any<is_same<_,bool>,seq_t> any_seq;
-  typedef nt2::meta::any<is_same<_,bool>,int32_t,bool,float> any_types;
+  typedef nt2::meta::any<is_same<_,bool>,int,bool,float> any_types;
 
-  printf("is any type bool in vector<int32_t,bool,float> : %d\n", any_seq::value);
-  printf("is any type bool in (int32_t,bool,float) : %d\n", any_types::value);
+  printf("is any type bool in vector<int,bool,float> : %d\n", any_seq::value);
+  printf("is any type bool in (int,bool,float) : %d\n", any_types::value);
 }
