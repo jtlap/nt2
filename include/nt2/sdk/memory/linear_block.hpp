7e8b001d604b1a97c9a98305cc568ac777799213
Joel Falcou <joel.falcou@lri.fr>
2010-08-26 17:19:51 +0200

    Basic linear_block code

--- include/nt2/sdk/memory/linear_block.hpp
+++ include/nt2/sdk/memory/linear_block.hpp
@@ -14,7 +14,9 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include <boost/typeof/typeof.hpp>
 #include <nt2/sdk/memory/slice.hpp>
+#include <nt2/sdk/memory/stride.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
+#include <boost/fusion/include/at.hpp>
 
 namespace nt2 { namespace memory
 {
@@ -42,16 +44,101 @@ namespace nt2 { namespace memory
     // Type related to internal buffer
     // A linear_block holds a single [0,slice<1>(size)[ buffer
     ////////////////////////////////////////////////////////////////////////////
-    typedef BOOST_TYPEOF_TPL(( slice<Padding,1>(Sizes()) )) size_value_type;
-    typedef boost::mpl::int_<0>                             base_value_type;
+    typedef BOOST_TYPEOF_TPL(( slice<1>(Sizes(),Padding()) )) size_value_type;
+    typedef boost::mpl::int_<0>                               base_value_type;
+    typedef buffer< value_type      , base_value_type
+                  , size_value_type , allocator_type  >       data_type;
 
+    ////////////////////////////////////////////////////////////////////////////
+    // Constructors
+    ////////////////////////////////////////////////////////////////////////////
+    linear_block() : data_(), bases_(), sizes_(), padding_() {}
 
-    typedef buffer<Type, boost::mpl::int_<0>, sizes_type, Allocator> data_type;
+    linear_block( Bases const& b, Sizes const& s, Padding const& p )
+                : data_(base_value_type(),slice<1>(s,p))
+                , bases_(b), sizes_(s), padding_(p)
+    {}
 
-    private:
-    Bases                                                     bases_;
-    Sizes                                                     sizes_;
-    buffer<Type, boost::mpl::int_<0>, sizes_type, Allocator>  data_;
+    linear_block( linear_block const& src )
+                : data_(src.data_)
+                , bases_(src.bases_), sizes_(src.sizes_)
+                , padding_(src.padding_)
+    {}
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Assignment
+    ////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Swap
+    ////////////////////////////////////////////////////////////////////////////
+    void swap( linear_block& src )
+    {
+      boost::swap(data_   , src.data_   );
+      boost::swap(bases_  , src.bases_  );
+      boost::swap(sizes_  , src.sizes_  );
+      boost::swap(padding_, src.padding_);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Element access
+    ////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Size manipulation
+    ////////////////////////////////////////////////////////////////////////////
+    void rebase( Bases const& b ) { data_.rebase(b); }
+    void resize( Sizes const& s ) { data_.resize(s); }
+    void restructure( Bases const& b, Sizes const& s )
+    {
+      data_.restructure(b,s);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Size informations
+    ////////////////////////////////////////////////////////////////////////////
+    template<std::size_t N>
+    typename boost::enable_if_c< (N>=1) && (N<=Dims), size_type>::type
+    size() const
+    {
+      return boost::fusion::at_c<N-1>(sizes_);
+    }
+
+    template<std::size_t N>
+    typename boost::disable_if_c< (N>=1) && (N<=Dims), size_type>::type
+    size() const
+    {
+      return 1;
+    }
+
+    template<std::size_t N>
+    typename boost::enable_if_c< (N>=1) && (N<=Dims), difference_type>::type
+    lower() const
+    {
+      return boost::fusion::at_c<N-1>(bases_);
+    }
+
+    template<std::size_t N>
+    typename boost::disable_if_c< (N>=1) && (N<=Dims), difference_type>::type
+    lower() const
+    {
+      return 1;
+    }
+
+    template<std::size_t N> difference_type upper() const
+    {
+      return size<N>() + lower<N>() - 1;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Data members : sizes,bases, buffer and padder
+    ////////////////////////////////////////////////////////////////////////////
+    //private:
+    data_type   data_;
+    Bases       bases_;
+    Sizes       sizes_;
+    Padding     padding_;
+
   };
 } }
 
--- main.cpp
+++ main.cpp
@@ -3,38 +3,37 @@
 #include <boost/mpl/vector_c.hpp>
 #include <boost/fusion/adapted/array.hpp>
 #include <boost/fusion/adapted/mpl.hpp>
-#include <nt2/sdk/details/type_id.hpp>
 #include <nt2/sdk/memory/linear_block.hpp>
 #include <nt2/sdk/memory/allocator.hpp>
 #include <nt2/sdk/memory/no_padding.hpp>
 #include <nt2/sdk/memory/global_padding.hpp>
+#include <nt2/sdk/options/options.hpp>
 
 using namespace std;
+
+NT2_REGISTER_PARAMETERS(size);
+NT2_REGISTER_PARAMETERS(base);
+NT2_REGISTER_PARAMETERS(padding);
 
 int main()
 {
   {
     typedef nt2::memory::linear_block<1, float
                                        , boost::array<int,1>
-                                       , boost::mpl::vector4_c<int,2,3,4,5>
+                                       , boost::array<int,1>
                                        , nt2::memory::global_padding
                                        , nt2::memory::allocator<float>
                                        > t;
-
-    t u;
-    cout << nt2::type_id<t>() << endl;
-    cout << nt2::type_id<t::sizes_type>() << endl;
-  }
-  {
-    typedef nt2::memory::linear_block<1, float
-                                       , boost::array<int,1>
-                                       , boost::mpl::vector4_c<int,2,3,4,5>
-                                       , nt2::memory::no_padding
-                                       , nt2::memory::allocator<float>
-                                       > t;
-
-    t u;
-    cout << nt2::type_id<t>() << endl;
-    cout << nt2::type_id<t::sizes_type>() << endl;
+    boost::array<int,1> s = {{5}};
+    boost::array<int,1> b = {{-2}};
+    t u(b,s);
+    cout << u.data_.size() << endl;
+    cout << u.data_.lower() << endl;
+    cout << u.data_.upper() << endl;
+    cout << u.data_.capacity() << endl;
+    cout << "\n";
+    cout << u.size<1>() << endl;
+    cout << u.lower<1>() << endl;
+    cout << u.upper<1>() << endl;
   }
 }
