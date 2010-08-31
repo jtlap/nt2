2b041a8b0c1a637ce51fd36a39d74994ba7c0512
Joel Falcou <joel.falcou@lri.fr>
2010-08-31 18:58:24 +0200

    meta::dominant is now a real PFO.

--- include/nt2/sdk/functor/meta/dominant.hpp
+++ include/nt2/sdk/functor/meta/dominant.hpp
@@ -18,8 +18,10 @@
 
 #if !defined(BOOST_HAS_VARIADIC_TMPL)
 #include <nt2/extension/parameters.hpp>
-#include <boost/preprocessor/repetition/repeat_from_to.hpp>
+#include <boost/preprocessor/facilities/intercept.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/preprocessor/repetition/repeat_from_to.hpp>
+#include <boost/preprocessor/repetition/enum_binary_params.hpp>
 #endif
 
 namespace nt2 { namespace meta
@@ -32,12 +34,30 @@ namespace nt2 { namespace meta
     template<class This, class... Args>
     struct  result<This(Args...)> : details::dominant< Args... >
     {};
+
+    template<class... Args>
+    typename result<dominant(Args...)>::type
+    operator()(Args&& ... ) const
+    {
+      typename result<dominant(Args...)>::type that;
+      return that;
+    };
+
     #else
-    #define M0(z,n,t)                                       \
-    template<class This, BOOST_PP_ENUM_PARAMS(n,class A) >  \
-    struct  result<This(BOOST_PP_ENUM_PARAMS(n,A))>         \
-          : details::dominant<BOOST_PP_ENUM_PARAMS(n,A)>    \
-    {};                                                     \
+    #define M0(z,n,t)                                                     \
+    template<class This, BOOST_PP_ENUM_PARAMS(n,class A) >                \
+    struct  result<This(BOOST_PP_ENUM_PARAMS(n,A))>                       \
+          : details::dominant<BOOST_PP_ENUM_PARAMS(n,A)>                  \
+    {};                                                                   \
+                                                                          \
+    template<BOOST_PP_ENUM_PARAMS(n,class A) >                            \
+    typename result<dominant(BOOST_PP_ENUM_PARAMS(n,A))>::type            \
+    operator()                                                            \
+    (BOOST_PP_ENUM_BINARY_PARAMS(n,A, const & BOOST_PP_INTERCEPT)) const  \
+    {                                                                     \
+      typename result<dominant(BOOST_PP_ENUM_PARAMS(n,A))>::type that;    \
+      return that;                                                        \
+    }                                                                     \
     /**/
     BOOST_PP_REPEAT_FROM_TO(1,BOOST_PP_INC(NT2_MAX_ARITY),M0,~)
     #undef M0
