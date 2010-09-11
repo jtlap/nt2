a4f30880240857777f20cc0991cc6b0b502ca230
Joel Falcou <joel.falcou@lri.fr>
2010-09-11 15:03:28 +0200

    Added python sample for unit test

--- include/nt2/toolbox/gmp/unit/scalar/add.cpp
+++ include/nt2/toolbox/gmp/unit/scalar/add.cpp
@@ -1,9 +1,17 @@
-//////////////////////////////////////////////////////////////////////////////
-///      Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///      Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
-// Je ne sais qu'y mettre
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 0x01.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#define NT2_UNIT_MODULE "nt2 gmp toolbox - Scalar Mode"
+
+#include <nt2/toolbox/gmp.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+// Test behavior of gmp components using NT2_TEST_CASE
+////////////////////////////////////////////////////////////////////////////////
