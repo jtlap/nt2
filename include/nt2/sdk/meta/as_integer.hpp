2324b2534ca90db33865b40fa821a518fe9d7886
Joel Falcou <joel.falcou@lri.fr>
2010-09-19 20:12:58 +0200

    Made as_integer throws an error if an Uncategorized type is used.

--- include/nt2/sdk/meta/as_integer.hpp
+++ include/nt2/sdk/meta/as_integer.hpp
@@ -15,8 +15,12 @@
 
 namespace nt2 { namespace details
 {
-  template<class T, class Sign, class Category>
-  struct as_integer : meta::make_integer<sizeof(T),Sign> {};
+  template<class T, class Sign, class Category> struct as_integer;
+
+  template<class T, class Sign>
+  struct 	as_integer<T,Sign,tag::scalar_(tag::arithmetic_)>
+				: meta::make_integer<sizeof(T),Sign> {};
+	
 } }
 
 namespace nt2 { namespace meta
