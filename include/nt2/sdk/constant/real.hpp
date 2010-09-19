da3f9bd602f005cc5a45b6807eef06c316c8ba2f
Joel Falcou <joel.falcou@lri.fr>
2010-09-19 23:14:09 +0200

    Correctif ldexpmask

--- include/nt2/sdk/constant/real.hpp
+++ include/nt2/sdk/constant/real.hpp
@@ -36,11 +36,8 @@ namespace nt2
   NT2_CONSTANT_IMPLEMENTATION(constants::half_      , Half    )
   NT2_CONSTANT_IMPLEMENTATION(constants::third_     , Third   )
   NT2_CONSTANT_IMPLEMENTATION(constants::quarter_   , Quarter )
-<<<<<<< Updated upstream
   NT2_CONSTANT_IMPLEMENTATION(constants::ldexp_mask_, Ldexpmask)
-=======
 	NT2_CONSTANT_IMPLEMENTATION(constants::two_to_m10_, Twotom10)
->>>>>>> Stashed changes
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -96,27 +93,14 @@ namespace nt2 { namespace functors                                        \
 ////////////////////////////////////////////////////////////////////////////////
 // Real only constants
 ////////////////////////////////////////////////////////////////////////////////
-NT2_CONSTANT_MAKE_REAL(constants::nan_    , 0xFFFFFFFFFFFFFFFFLL, 0xFFFFFFFF  )
-NT2_CONSTANT_MAKE_REAL(constants::m_half_ , 0xBFE0000000000000LL, 0xBF000000  )
-NT2_CONSTANT_MAKE_REAL(constants::m_zero_ , 0x8000000000000000LL, 0x80000000  )
-NT2_CONSTANT_MAKE_REAL(constants::half_   , 0x3FE0000000000000LL, 0x3F000000  )
-NT2_CONSTANT_MAKE_REAL(constants::third_  , 0x3FD5555555555555LL, 0x3EAAAAAB  )
-NT2_CONSTANT_MAKE_REAL(constants::quarter_, 0x3FD0000000000000LL, 0x3E800000  )
-<<<<<<< Updated upstream
-NT2_CONSTANT_MAKE_REAL(constants::ldexp_mask_,9218868437227405312ll, 2139095040) 
-=======
-NT2_CONSTANT_MAKE_REAL(constants::two_to_m10_,0x804D558B7EB8AD0ll,0x3a800000 )
->>>>>>> Stashed changes
-
-/*
-  namespace nt2 { namespace constants
-  {
-  } }
-
-  namespace nt2
-  {
-  }
-*/
+NT2_CONSTANT_MAKE_REAL(constants::nan_    		, 0xFFFFFFFFFFFFFFFFLL, 0xFFFFFFFF  )
+NT2_CONSTANT_MAKE_REAL(constants::m_half_ 		, 0xBFE0000000000000LL, 0xBF000000  )
+NT2_CONSTANT_MAKE_REAL(constants::m_zero_ 		, 0x8000000000000000LL, 0x80000000  )
+NT2_CONSTANT_MAKE_REAL(constants::half_   		, 0x3FE0000000000000LL, 0x3F000000  )
+NT2_CONSTANT_MAKE_REAL(constants::third_  		, 0x3FD5555555555555LL, 0x3EAAAAAB  )
+NT2_CONSTANT_MAKE_REAL(constants::quarter_		, 0x3FD0000000000000LL, 0x3E800000  )
+NT2_CONSTANT_MAKE_REAL(constants::ldexp_mask_	, 0x7FF0000000000000LL, 0x7F800000	) 
+NT2_CONSTANT_MAKE_REAL(constants::two_to_m10_ , 0x3F50000000000000LL, 0x3a800000	)
 
 ////////////////////////////////////////////////////////////////////////////////
 // Real only constants - PI related
@@ -144,64 +128,6 @@ NT2_CONSTANT_MAKE_REAL(constants::mone_o_6_ , Category , boost::mpl::_
 NT2_CONSTANT_MAKE_REAL(constants::one_o_120_ , Category , boost::mpl::_
                         , 0x3f81111111111111ll,0x3c088889
                         );
-*/
-/*
-////////////////////////////////////////////////////////////////////////////////
-// Real only constants - Trigonometrics
-////////////////////////////////////////////////////////////////////////////////
-NT2_CONSTANT_MAKE_REAL(constants::inv_180_ , Category , boost::mpl::_
-                        , 0x3f76c16c16c16c17ll,0x3BB60B61
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::deg_in_rad_ , Category , boost::mpl::_
-                        , 0x3f91df46a2529d39LL,0x3c8efa35
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::rad_in_deg_ , Category , boost::mpl::_
-                        , 0x404ca5dc1a63c1f8LL,0x42652ee1
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::rad_in_deg_r_ , Category , boost::mpl::_
-                        , 0xbce1e7ab456405f8ll,0xb53387c1
-                        )
-
-////////////////////////////////////////////////////////////////////////////////
-// Real only constants - Remarkable values
-////////////////////////////////////////////////////////////////////////////////
-NT2_CONSTANT_MAKE_REAL(constants::gold_  , Category , boost::mpl::_
-                        , 0x3ff9e3779b97f4a8LL,0x3fcf1bbd
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::c_gold_, Category , boost::mpl::_
-                        , 0x3fd8722191a02d61LL,0x3ec3910d
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::sqrt_2_ , Category , boost::mpl::_
-                        , 0x3ff6a09e667f3bcdLL,0x3fb504f3
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::sqrt_2_o_2_  , Category , boost::mpl::_
-                        , 0x3fe6a09e667f3bcdLL,0x3f3504f3
-                        )
-
-////////////////////////////////////////////////////////////////////////////////
-// Real only constants - Log and Exp
-////////////////////////////////////////////////////////////////////////////////
-NT2_CONSTANT_MAKE_REAL(constants::exp_1_ , Category , boost::mpl::_
-                        , 0x4005bf0a8b145769LL,0x402df854
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::log_2_ , Category , boost::mpl::_
-                        , 0x3fe62e42fefa39efLL,0x3f317218
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::inv_log_2_ , Category , boost::mpl::_
-                        , 0x3ff71547652b82feLL,0x3fb8aa3b
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::log_10_ , Category , boost::mpl::_
-                        , 0x40026bb1bbb55516LL,0x40135d8e
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::inv_log_10_ , Category , boost::mpl::_
-                        , 0x3fdbcb7b1526e50dLL,0x3ede5bd9
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::log2_o_log10_ , Category , boost::mpl::_
-                        , 0x3fd34413509f79ffLL,0x3e9a209b
-                        )
-NT2_CONSTANT_MAKE_REAL(constants::split_factor_ , Category, boost::mpl::_
-                        , 0x41a0000000000000ll,0x46000000
-                        )
 */
 
 #endif
