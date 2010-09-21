522f5f7ecf5e53a0f569e1add9d4d06e40bc93c2
Jt lapreste <jtlapreste@gmail.com>
2010-09-21 14:28:52 +0200

    real cts added real.hpp

--- include/nt2/sdk/constant/real.hpp
+++ include/nt2/sdk/constant/real.hpp
@@ -28,20 +28,22 @@ namespace nt2 { namespace constants
   struct two_to_m10_  {};
   struct two_to_nmb_  {};
   struct pi_          {};
+  struct split_factor_{};
 }  }
 
 namespace nt2
 {
-  NT2_CONSTANT_IMPLEMENTATION(constants::nan_       , Nan     )
-  NT2_CONSTANT_IMPLEMENTATION(constants::m_half_    , MHalf   )
-  NT2_CONSTANT_IMPLEMENTATION(constants::m_zero_    , Mzero   )
-  NT2_CONSTANT_IMPLEMENTATION(constants::half_      , Half    )
-  NT2_CONSTANT_IMPLEMENTATION(constants::third_     , Third   )
-  NT2_CONSTANT_IMPLEMENTATION(constants::quarter_   , Quarter )
-  NT2_CONSTANT_IMPLEMENTATION(constants::ldexp_mask_, Ldexpmask)
-  NT2_CONSTANT_IMPLEMENTATION(constants::two_to_m10_, Twotom10)
-  NT2_CONSTANT_IMPLEMENTATION(constants::two_to_nmb_, Two2nmb )
-  NT2_CONSTANT_IMPLEMENTATION(constants::pi_,         Pi      ) 
+  NT2_CONSTANT_IMPLEMENTATION(constants::nan_         , Nan     )
+  NT2_CONSTANT_IMPLEMENTATION(constants::m_half_      , MHalf   )
+  NT2_CONSTANT_IMPLEMENTATION(constants::m_zero_      , Mzero   )
+  NT2_CONSTANT_IMPLEMENTATION(constants::half_        , Half    )
+  NT2_CONSTANT_IMPLEMENTATION(constants::third_       , Third   )
+  NT2_CONSTANT_IMPLEMENTATION(constants::quarter_     , Quarter )
+  NT2_CONSTANT_IMPLEMENTATION(constants::ldexp_mask_  , Ldexpmask)
+  NT2_CONSTANT_IMPLEMENTATION(constants::two_to_m10_  , Twotom10)
+  NT2_CONSTANT_IMPLEMENTATION(constants::two_to_nmb_  , Two2nmb )
+  NT2_CONSTANT_IMPLEMENTATION(constants::pi_          , Pi      ) 
+  NT2_CONSTANT_IMPLEMENTATION(constants::split_factor_, Splitfactor) 
 
 }
 
@@ -107,7 +109,9 @@ NT2_CONSTANT_MAKE_REAL(constants::quarter_,   0x3FD0000000000000LL, 0x3E800000  
 NT2_CONSTANT_MAKE_REAL(constants::ldexp_mask_,0x7FF0000000000000ll, 0x7F800000  ) //      9218868437227405312ll, 2139095040) 
 NT2_CONSTANT_MAKE_REAL(constants::two_to_m10_,0x0804D558B7EB8AD0ll, 0x3a800000  )
 NT2_CONSTANT_MAKE_REAL(constants::two_to_nmb_,0x4330000000000000ll, 0x4b000000  )
-NT2_CONSTANT_MAKE_REAL(constants::pi_,        0x400921fb54442d18ll, 0x40490fdb  ) 
+NT2_CONSTANT_MAKE_REAL(constants::pi_,        0x400921fb54442d18ll, 0x40490fdb  )
+NT2_CONSTANT_MAKE_REAL(constants::split_factor_, 0x41a0000000000000ll,0x46000000) 
+
 
 /*
   namespace nt2 { namespace constants
