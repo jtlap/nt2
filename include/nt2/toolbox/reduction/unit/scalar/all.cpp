a9e213187b0d89c08a44e39133a3818878485805
Joel Falcou <joel.falcou@lri.fr>
2010-09-22 15:51:35 +0200

    Added reduction::all unit test in scalar mode

--- include/nt2/toolbox/reduction/unit/scalar/all.cpp
+++ include/nt2/toolbox/reduction/unit/scalar/all.cpp
@@ -6,13 +6,32 @@
 ///                 See accompanying file LICENSE.txt or copy at
 ///                     http://www.boost.org/LICENSE_1_0.txt
 //////////////////////////////////////////////////////////////////////////////
-#define NT2_UNIT_MODULE "nt2 reduction toolbox - unit/scalar Mode"
+#define NT2_UNIT_MODULE "nt2 reduction toolbox - all scalar Mode"
 
+#include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/toolbox/reduction/include/all.hpp>
+
+#include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 
 //////////////////////////////////////////////////////////////////////////////
-// Test behavior of reduction components using NT2_TEST_CASE
+// Test behavior of swar components using NT2_TEST_CASE
 //////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE_TPL ( all,  (double)(nt2::uint64_t)(nt2::int64_t) 
+                          (float)(nt2::uint32_t)(nt2::int32_t)  
+                          (nt2::uint16_t)(nt2::int16_t)         
+                          (nt2::uint8_t)(nt2::int8_t)
+                          (bool)
+                  )
+{
+  using nt2::all;
+  using nt2::functors::all_;
 
+  NT2_TEST( (boost::is_same < typename nt2::meta::call<all_(T,T)>::type
+                            , bool
+                            >::value)
+          );
+  NT2_TEST(  all(T(1337))  );
+  NT2_TEST( !all(T(0))     );
+}
