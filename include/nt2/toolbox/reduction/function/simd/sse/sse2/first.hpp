ae7cabdeaa4b99a647d9b1faf9b88bfe15c5674d
Joel Falcou <joel.falcou@lri.fr>
2010-09-23 17:15:29 +0200

    Added SIMD implementation for first() and associated unit test

--- include/nt2/toolbox/reduction/function/simd/sse/sse2/first.hpp
+++ include/nt2/toolbox/reduction/function/simd/sse/sse2/first.hpp
@@ -8,34 +8,35 @@
 //////////////////////////////////////////////////////////////////////////////
 #ifndef NT2_TOOLBOX_REDUCTION_FUNCTION_SIMD_SSE_SSE2_FIRST_HPP_INCLUDED
 #define NT2_TOOLBOX_REDUCTION_FUNCTION_SIMD_SSE_SSE2_FIRST_HPP_INCLUDED
-#include <nt2/sdk/meta/from_bits.hpp>
-#include <nt2/sdk/meta/as_real.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/as_bits.hpp>
-#include <nt2/sdk/meta/strip.hpp>
 
+#include <nt2/sdk/meta/strip.hpp>
+#include <nt2/sdk/meta/as_bits.hpp>
+#include <nt2/sdk/meta/as_real.hpp>
+#include <nt2/sdk/meta/from_bits.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
 
 namespace nt2 { namespace functors
 {
-  //  no special validate for first
-
   template<class Extension,class Info>
   struct call<first_,tag::simd_(tag::arithmetic_,Extension),Info>
   {
     template<class Sig> struct result;
     template<class This,class A0>
-    struct result<This(A0)> : meta::scalar_of<A0>{};
+    struct  result<This(A0)>
+          : meta::scalar_of<typename meta::strip<A0>::type> {};
 
-    NT2_FUNCTOR_CALL_DISPATCH(
-      1,
-      typename nt2::meta::scalar_of<A0>::type,
-      (6, (types8_,types16_,float,types32_,double, types64_))
-     )
+    NT2_FUNCTOR_CALL_DISPATCH ( 1
+                              , typename nt2::meta::scalar_of<A0>::type
+                              , ( 6
+                                , (types8_,types16_,float,types32_,double, types64_)
+                                )
+                              )
 
     NT2_FUNCTOR_CALL_EVAL_IF(1,    types8_)
     {
       typedef typename NT2_CALL_RETURN_TYPE(1)::type type;
       int that = _mm_extract_epi16(a0, 0);
+      printf("%d\n",that);
       return that & 0xFF;
     }
     NT2_FUNCTOR_CALL_EVAL_IF(1,    types16_)
--- include/nt2/toolbox/reduction/unit/simd/first.cpp
+++ include/nt2/toolbox/reduction/unit/simd/first.cpp
@@ -8,11 +8,39 @@
 //////////////////////////////////////////////////////////////////////////////
 #define NT2_UNIT_MODULE "nt2 reduction toolbox - first simd Mode"
 
+#include <nt2/sdk/simd/native.hpp>
+#include <nt2/sdk/meta/cardinal_of.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/memory/aligned_type.hpp>
 #include <nt2/toolbox/reduction/include/first.hpp>
+
+#include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 
 //////////////////////////////////////////////////////////////////////////////
-// Test behavior of reduction components using NT2_TEST_CASE
+// Test behavior of nt2::first in scalar mode
 //////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE_TPL ( first, NT2_SIMD_TYPES )
+{
+  using nt2::first;
+  using nt2::load;
+  using nt2::functors::first_;
+  using nt2::simd::native;
+  using nt2::meta::cardinal_of;
+ 
+  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>             n_t;
+
+  NT2_TEST( (boost::is_same < typename nt2::meta::call<first_(native<T,ext_t>)>::type
+                            , T 
+                            >::value)
+          );
+  NT2_ALIGNED_TYPE(T) data[cardinal_of<n_t>::value];
+  for(std::size_t i=0;i<cardinal_of<n_t>::value;++i)
+    data[i] = 7+i;
+
+  n_t v = load<n_t>(&data[0],0);
+  NT2_TEST_EQUAL( nt2::first(v), 7 );
+}
 
