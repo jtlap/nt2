3e1bf2c5bd137d43b2843f11fd1a93cc6cc0acb7
Jt lapreste <jtlapreste@gmail.com>
2010-09-24 15:21:25 +0200

    euler toolbox

---
+++ include/nt2/toolbox/euler/euler.hpp
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#ifndef NT2_TOOLBOX_EULER_EULER_HPP_INCLUDED
+#define NT2_TOOLBOX_EULER_EULER_HPP_INCLUDED
+
+//<include> please don't modify between these tags
+#include <nt2/toolbox/euler/include/fast_gamma.hpp>
+#include <nt2/toolbox/euler/include/spence.hpp>
+#include <nt2/toolbox/euler/include/stirling.hpp>
+#include <nt2/toolbox/euler/include/dawson.hpp>
+#include <nt2/toolbox/euler/include/digamma.hpp>
+#include <nt2/toolbox/euler/include/signgam.hpp>
+#include <nt2/toolbox/euler/include/gammaln.hpp>
+#include <nt2/toolbox/euler/include/gamma.hpp>
+#include <nt2/toolbox/euler/include/beta.hpp>
+//<\include>
+
+#endif
---
+++ include/nt2/toolbox/euler/function/scalar/constants.hpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *         Copyright 2003-2010 LASMEA UMR 6602 CNRS/U.B.P
+ *         Copyright 2009-2010 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_TOOLBOX_EULER_FUNCTION_SCALAR_CONSTANTS_HPP_INCLUDED
+#define NT2_TOOLBOX_EULER_FUNCTION_SCALAR_CONSTANTS_HPP_INCLUDED
+#include <nt2/sdk/constant/constant.hpp>
+#include <nt2/sdk/constant/details/real_constant.hpp>
+#include <nt2/sdk/constant/real.hpp>
+
+namespace nt2 { namespace constants
+{
+  struct stirling_splitlim_  {};
+  struct stirling_largelim_  {};
+  struct digamma_largelim_   {};
+  struct fast_gamma_largelim_{};
+}  }
+
+namespace nt2
+{
+  NT2_CONSTANT_IMPLEMENTATION(constants::stirling_splitlim_,Stirlingsplitlim )
+  NT2_CONSTANT_IMPLEMENTATION(constants::stirling_largelim_,Stirlinglargelim )
+  NT2_CONSTANT_IMPLEMENTATION(constants::digamma_largelim_,Digammalargelim )
+  NT2_CONSTANT_IMPLEMENTATION(constants::fast_gamma_largelim_,Fastgammalargelim )
+}
+
+
+NT2_CONSTANT_MAKE_REAL(constants::stirling_splitlim_   ,0x4061e083ba3443d4ll,0x41d628f6)// double 143.01608 float  26.77 //TODO
+NT2_CONSTANT_MAKE_REAL(constants::stirling_largelim_   ,0x4061e083ba3443d4ll,0x41d628f6)// double 172.0     float  35.0399895
+NT2_CONSTANT_MAKE_REAL(constants::digamma_largelim_    ,0x4034000000000000ll,0x41200000)
+NT2_CONSTANT_MAKE_REAL(constants::fast_gamma_largelim_ ,0x4040800000000000ll,0x41200000)// double 33.0      float  10.0
+
+#endif
+// NT2_CONSTANT(fast_gamma_small_   , fast_gamma_small    )
+// // double 1.0e-9
+// // float  1.0e-4
+// NT2_SPECIALIZE_CONSTANT( fast_gamma_small_, Category, boost::mpl::_
+//                        , ((double   ,0x3e112e0be826d695ll))
+//                          ((float    ,0x38d1b717          ))
+//                        )
+
