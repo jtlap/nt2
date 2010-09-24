85242fbc06ac33a54e09f419dd898ccfb2dfce99
Joel Falcou <joel.falcou@lri.fr>
2010-09-24 15:39:00 +0200

    Added master driver rule for unit test

--- unit/sdk/simd/types.hpp
+++ unit/sdk/simd/types.hpp
@@ -11,20 +11,4 @@
 
 #include <nt2/sdk/simd/extensions.hpp>
 
-////////////////////////////////////////////////////////////////////////////////
-// Generates sequence of supported types for SIMD
-////////////////////////////////////////////////////////////////////////////////
-#if defined(NT2_SIMD_SSE_FAMILY)
-#define NT2_SIMD_TYPES  (double)(nt2::uint64_t)(nt2::int64_t) \
-                        (float)(nt2::uint32_t)(nt2::int32_t)  \
-                        (nt2::uint16_t)(nt2::int16_t)         \
-                        (nt2::uint8_t)(nt2::int8_t)           \
-/**/
-#elif defined(NT2_SIMD_VMX_FAMILY)
-#define NT2_SIMD_TYPES  (float)(nt2::uint32_t)(nt2::int32_t)  \
-                        (nt2::uint16_t)(nt2::int16_t)         \
-                        (nt2::uint8_t)(nt2::int8_t)           \
-/**/
-#endif
-
 #endif
