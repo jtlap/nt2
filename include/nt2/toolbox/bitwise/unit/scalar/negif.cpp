42b4d5244b5d0e86e59c18a1afb92a9d20a6c968
Jt lapreste <jtlapreste@gmail.com>
2010-09-27 20:52:42 +0200

    test negif erroné

--- include/nt2/toolbox/bitwise/unit/scalar/negif.cpp
+++ include/nt2/toolbox/bitwise/unit/scalar/negif.cpp
@@ -11,12 +11,14 @@
 #include <nt2/toolbox/bitwise/include/negif.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <boost/type_traits/is_same.hpp>
 
 //////////////////////////////////////////////////////////////////////////////
 // Test behavior of bitwise components using NT2_TEST_CASE
 //////////////////////////////////////////////////////////////////////////////
 NT2_TEST_CASE_TPL ( negif,  (double)(nt2::uint64_t)(nt2::int64_t) 
-		    (fnegifat)(nt2::uint32_t)(nt2::int32_t)  
+		    (float)(nt2::uint32_t)(nt2::int32_t)  
 		    (nt2::uint16_t)(nt2::int16_t)         
 		    (nt2::uint8_t)(nt2::int8_t)
                   )
