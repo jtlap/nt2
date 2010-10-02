739d148ad4683ce580d29349e55b9d8a43398da5
Joel Falcou <joel.falcou@lri.fr>
2010-10-02 12:34:25 +0200

    Fixed base functors operators to use new call system

--- include/nt2/sdk/functor/details/if_else.hpp
+++ include/nt2/sdk/functor/details/if_else.hpp
@@ -19,25 +19,29 @@
 
 namespace nt2 { namespace functors
 {
+  template<class Category>
+  struct  dispatch<if_else_,tag::scalar_(Category)>
+        : boost::mpl::_1 {};
+
   template<class Category, class Info>
-  struct validate<if_else_,Category, Info>
+  struct validate<if_else_,tag::scalar_(Category), Info>
   {
     template<class Sig> struct result;
     template<class This,class A0,class A1,class A2>
     struct  result<This(A0,A1,A2)>
-					: boost::is_convertible<A0,bool> {};
+          : boost::is_convertible<A0,bool> {};
   };
 
-  template<class Category, class Info>
-  struct call<if_else_,Category, Info>
+  template<class Category, class Hierarchy, class Info>
+  struct call<if_else_,tag::scalar_(Category), Hierarchy, Info> : callable
   {
     template<class Sig> struct result;
     template<class This,class A0,class A1,class A2>
     struct result<This(A0,A1,A2)>
     {
-			typedef typename meta::strip<A0>::type t0;
-			typedef typename meta::strip<A1>::type t1;
-			typedef typename meta::strip<A2>::type t2;
+      typedef typename meta::strip<A0>::type t0;
+      typedef typename meta::strip<A1>::type t1;
+      typedef typename meta::strip<A2>::type t2;
       static t0& a0;  static t1& a1;  static t2& a2;
       BOOST_TYPEOF_NESTED_TYPEDEF_TPL(nested,a0 ? a1 : a2)
       typedef typename nested::type type;
