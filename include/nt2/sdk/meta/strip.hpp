03f010ee0b92ecfe2ad6ced3d762bbc97df83da1
Joel Falcou <joel.falcou@lri.fr>
2010-10-03 11:11:26 +0200

    Added missing cstddef include in strip

--- include/nt2/sdk/meta/strip.hpp
+++ include/nt2/sdk/meta/strip.hpp
@@ -9,19 +9,38 @@
 #ifndef NT2_SDK_META_STRIP_HPP_INCLUDED
 #define NT2_SDK_META_STRIP_HPP_INCLUDED
 
+#include <cstddef>
+
 ////////////////////////////////////////////////////////////////////////////////
 // Remove all type qualifier from a type
 // TODO: Documentation: http://nt2.lri.fr/sdk/meta/strip.html
 ////////////////////////////////////////////////////////////////////////////////
-#include  <boost/type_traits/remove_const.hpp>
-#include  <boost/type_traits/remove_reference.hpp>
 
 namespace nt2 { namespace meta
 {
-  template<class T>
-  struct  strip
-        : boost::remove_const< typename boost::remove_reference<T>::type >
-  {};
+  //////////////////////////////////////////////////////////////////////////////
+  // strip removes const, ref and volatile -- better than Mr Proper
+  //////////////////////////////////////////////////////////////////////////////
+  template<class T> struct strip                      { typedef T type; };
+  template<class T> struct strip<T const            > { typedef T type; };
+  template<class T> struct strip<T &                > { typedef T type; };
+  template<class T> struct strip<T const &          > { typedef T type; };
+  template<class T> struct strip<volatile T         > { typedef T type; };
+  template<class T> struct strip<volatile T const   > { typedef T type; };
+  template<class T> struct strip<volatile T &       > { typedef T type; };
+  template<class T> struct strip<volatile T const & > { typedef T type; };
+
+  //////////////////////////////////////////////////////////////////////////////
+  // strip on C-style array
+  //////////////////////////////////////////////////////////////////////////////
+  template<class T, std::size_t N>
+  struct strip<T const[N]     > { typedef T type[N];  };
+
+  template<class T, std::size_t N>
+  struct strip<T (&)[N]       > { typedef T type[N];  };
+
+  template<class T, std::size_t N>
+  struct strip<T const (&)[N] > { typedef T type[N];  };
 } }
 
 #endif
