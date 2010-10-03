60e02abc26daf973111762883ec45b413dd14067
Joel Falcou <joel.falcou@lri.fr>
2010-10-03 17:33:37 +0200

    Fixed boolean constant for new functor system

--- include/nt2/sdk/constant/boolean.hpp
+++ include/nt2/sdk/constant/boolean.hpp
@@ -30,33 +30,33 @@ namespace nt2
 
 namespace nt2 { namespace functors
 {
-  template<class Category,class Info>
-  struct  call<constants::true_,tag::constant_(Category),Info>
+  template<class Category,class Hierarchy,class Info>
+  struct  call<constants::true_,tag::constant_(Category),Hierarchy,Info>
+        : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
     struct  result<This(A0)> : meta::strip<A0>::type {};
 
-    template<class Target> inline typename Target::type const
-    operator()( Target const& ) const
+    NT2_FUNCTOR_CALL(1)
     {
-      return true;
+      typedef typename meta::scalar_of<typename Target::type>::type type;
+      return splat<typename Target::type>(true);
     }
   };
 
-  template<class Category,class Info>
-  struct  call<constants::false_,tag::constant_(Category),Info>
+  template<class Category,class Hierarchy,class Info>
+  struct  call<constants::false_,tag::constant_(Category),Hierarchy,Info>
+        : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
-    struct  result<This(A0)>
-          : meta::strip<A0>::type {};
+    struct  result<This(A0)> : meta::strip<A0>::type {};
 
-    template<class Target> inline typename Target::type const
-    operator()( Target const& ) const
+    NT2_FUNCTOR_CALL(1)
     {
       typedef typename meta::scalar_of<typename Target::type>::type type;
-      return splat<typename Target::type>(0);
+      return splat<typename Target::type>(false);
     }
   };
 } }
