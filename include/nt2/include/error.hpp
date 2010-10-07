4852f6b837d7b4d0f4047081b2814b32aef701c9
Jt lapreste <jtlapreste@gmail.com>
2010-10-07 22:21:55 +0200

    a wrong header

--- include/nt2/include/error.hpp
+++ include/nt2/include/error.hpp
@@ -6,8 +6,8 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#ifndef NT2_INCLUDE_ERRORS_HPP_INCLUDED
-#define NT2_INCLUDE_ERRORS_HPP_INCLUDED
+#ifndef NT2_INCLUDE_ERROR_HPP_INCLUDED
+#define NT2_INCLUDE_ERROR_HPP_INCLUDED
 
 #include <nt2/sdk/config/types.hpp>
 
