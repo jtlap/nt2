fac95d081d80873143a72eddca639bd534f71b18
Thomas Heller <thom.heller@gmail.com>
2010-10-21 16:27:30 +0200

    fixed rule based visitation

--- include/nt2/sdk/simd/pack/data.hpp
+++ include/nt2/sdk/simd/pack/data.hpp
@@ -28,10 +28,9 @@ namespace nt2 { namespace simd
     typedef typename meta::category_of<parent>::type              parent_tag;
     typedef functors::ast_<parent_tag>                            nt2_category_tag;
 
-    typedef dsl::compile< dsl::compute<parent> >  evaluator_type;
+    //typedef dsl::compile< dsl::compute<parent> >  evaluator_type;
+    typedef dsl::compile<parent>  evaluator_type;
 
-    typedef typename meta::category_of<parent>::type  parent_tag;
-    typedef functors::ast_<parent_tag>                nt2_category_tag;
     typedef typename parent::value_type               value_type;
     typedef typename parent::reference                reference;
     typedef typename parent::const_reference          const_reference;
@@ -66,10 +65,10 @@ namespace nt2 { namespace simd
     ////////////////////////////////////////////////////////////////////////////
     // Range interface
     ////////////////////////////////////////////////////////////////////////////
-    const_iterator begin()  const { return mData.begin(); }
-    const_iterator end()    const { return mData.end();   }
-    iterator begin()  { return mData.begin(); }
-    iterator end()    { return mData.end();   }
+    const_iterator  begin()  const  { return mData.begin(); }
+    const_iterator  end()    const  { return mData.end();   }
+    iterator        begin()         { return mData.begin(); }
+    iterator        end()           { return mData.end();   }
 
     ////////////////////////////////////////////////////////////////////////////
     // Fill current data by evaluating soem expression
@@ -118,8 +117,7 @@ namespace nt2 { namespace simd
     template<class X>
     void evaluate ( X const& xpr, boost::mpl::false_ const& )
     {
-      evaluator_type eval;
-
+        evaluator_type eval;
       for(std::size_t i=0;i<Cardinal::value;++i)
         mData[i] = eval(xpr,i,i);
     }
