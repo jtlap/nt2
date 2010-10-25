f432a40112337f9a074351240c9a4a81794cf4ca
Jt lapreste <jtlapreste@gmail.com>
2010-10-25 20:30:33 +0200

    exponential toolbox benches

--- include/nt2/toolbox/exponential/bench/simd/log.cpp
+++ include/nt2/toolbox/exponential/bench/simd/log.cpp
@@ -8,15 +8,24 @@
 //////////////////////////////////////////////////////////////////////////////
 #include <nt2/toolbox/exponential/include/log.hpp>
 #include <nt2/sdk/unit/benchmark.hpp>
+#include <nt2/sdk/simd/native.hpp>
+#include <cmath>
+
+typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+typedef nt2::simd::native<float,ext_t> vfloat;
+typedef nt2::simd::native<double,ext_t> vdouble;
 
 //////////////////////////////////////////////////////////////////////////////
-// Runtime benchmark for functor<log_> from exponential
+// Simd Runtime benchmark for functor<log_> from exponential
 //////////////////////////////////////////////////////////////////////////////
 using nt2::functors::log_;
 
 //////////////////////////////////////////////////////////////////////////////
-// bench/simd
-// E.G:
-// NT2_TIMING( log_ , ((nt2::simd::native<float,nt2::tag::sse_>, -10, 10)) ) 
-//           )
+// range macro
 //////////////////////////////////////////////////////////////////////////////
+#define RS(T,V1,V2) (T, V1 , V2)
+
+NT2_TIMING(nt2::functors::log_,(RS(vfloat,0.0f,10000.0f)))
+NT2_TIMING(nt2::functors::log_,(RS(vdouble,0.0,10000.0)))
+
+#undef RS
--- include/nt2/toolbox/exponential/bench/simd/pow.cpp
+++ include/nt2/toolbox/exponential/bench/simd/pow.cpp
@@ -8,16 +8,24 @@
 //////////////////////////////////////////////////////////////////////////////
 #include <nt2/toolbox/exponential/include/pow.hpp>
 #include <nt2/sdk/unit/benchmark.hpp>
+#include <nt2/sdk/simd/native.hpp>
+#include <cmath>
+
+typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+typedef nt2::simd::native<float,ext_t> vfloat;
+typedef nt2::simd::native<double,ext_t> vdouble;
 
 //////////////////////////////////////////////////////////////////////////////
-// Runtime benchmark for functor<pow_> from exponential
+// Simd Runtime benchmark for functor<pow_> from exponential
 //////////////////////////////////////////////////////////////////////////////
 using nt2::functors::pow_;
 
 //////////////////////////////////////////////////////////////////////////////
-// bench/simd
-// E.G:
-// NT2_TIMING( pow_ , ((nt2::simd::native<float,nt2::tag::sse_>, -10, 10))
-//                    ((nt2::simd::native<float,nt2::tag::sse_>, -10, 10)) ) 
-//           )
+// range macro
 //////////////////////////////////////////////////////////////////////////////
+#define RS(T,V1,V2) (T, V1 , V2)
+
+NT2_TIMING(nt2::functors::pow_,(RS(vfloat,-10.0f,10.0f))(RS(vfloat,-10.0f,10.0f)))
+NT2_TIMING(nt2::functors::pow_,(RS(vdouble,-10.0,10.0))(RS(vdouble,-10.0,10.0)))
+
+#undef RS
