d8e178b96edacbea35904ac3152c5ca23562abea
Jt lapreste <jtlapreste@gmail.com>
2010-10-27 00:21:42 +0200

    dirty <<irrelevant>> temporary trick for tchebeval

--- include/nt2/toolbox/polynomials/function/scalar/tchebeval.hpp
+++ include/nt2/toolbox/polynomials/function/scalar/tchebeval.hpp
@@ -9,9 +9,25 @@
 #ifndef NT2_TOOLBOX_POLYNOMIALS_FUNCTION_SCALAR_TCHEBEVAL_HPP_INCLUDED
 #define NT2_TOOLBOX_POLYNOMIALS_FUNCTION_SCALAR_TCHEBEVAL_HPP_INCLUDED
 #include <nt2/sdk/constant/digits.hpp>
+#include <nt2/include/functions/average.hpp>
+#include <boost/array.hpp>
+#include <nt2/sdk/meta/irrelevant.hpp>
+namespace nt2 { namespace details
+{
+  template<class T,std::size_t N> 
+  struct category_of_impl<boost::array<T,N>,false>
+  {
+    typedef tag::irrelevant type;
+  };
+} }
 
 namespace nt2 { namespace functors
 {
+  template<class Info>
+  struct validate<tchebeval_,tag::scalar_(tag::arithmetic_),Info>
+  {
+    typedef boost::mpl::true_ result_type;
+  };
 
   /////////////////////////////////////////////////////////////////////////////
   // Compute tchebeval(const A0& a0, const A1& a1)
@@ -22,11 +38,11 @@ namespace nt2 { namespace functors
     template<class Sig> struct result;
     template<class This,class A0,class A1>
     struct result<This(A0,A1)> : 
-      boost::result_of<meta::floating(A0,A1)>{};
+      boost::result_of<meta::floating(A0)>{};
 
     NT2_FUNCTOR_CALL_DISPATCH(
       2,
-      A1,
+      A0,
       (2, (real_, arithmetic_))
     )
 
@@ -47,7 +63,7 @@ namespace nt2 { namespace functors
     NT2_FUNCTOR_CALL_EVAL_IF(2, arithmetic_)
     {
       typedef typename NT2_CALL_RETURN_TYPE(2)::type type; 
-      return tchebeval(a0, type(a1)); 
+      return tchebeval(type(a0), a1); 
     }
 
   };
