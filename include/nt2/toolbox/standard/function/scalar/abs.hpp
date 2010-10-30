1001d369f8ed4d5d0aa6affc2b4b08b3d71278fd
Jt lapreste <jtlapreste@gmail.com>
2010-10-30 13:49:23 +0200

    toolbox standard

---
+++ include/nt2/toolbox/standard/function/scalar/abs.hpp
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#ifndef NT2_TOOLBOX_STANDARD_FUNCTION_SCALAR_ABS_HPP_INCLUDED
+#define NT2_TOOLBOX_STANDARD_FUNCTION_SCALAR_ABS_HPP_INCLUDED
+
+namespace nt2 { namespace functors
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Compute abs(const A0& a0)
+  /////////////////////////////////////////////////////////////////////////////
+  template<class Info>
+  struct call<standard::abs_,tag::scalar_(tag::arithmetic_),Info>
+  {
+    template<class Sig> struct result;
+    template<class This,class A0>
+    struct result<This(A0)> : boost::result_of<meta::floating(A0)>{};
+
+    NT2_FUNCTOR_CALL_DISPATCH( 1, A0, (2, (real_,arithmetic_)) )
+
+    NT2_FUNCTOR_CALL_EVAL_IF(1, real_)  { return std::abs(a0); }
+    NT2_FUNCTOR_CALL_EVAL_IF(1, arithmetic_)
+    {
+      typedef typename NT2_CALL_RETURN_TYPE(1)::type type;
+      return nt2::standard::abs(type(a0));
+    }
+  };
+} }
+
+
+      
+#endif
---
+++ include/nt2/toolbox/standard/standard.hpp
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#ifndef NT2_TOOLBOX_STANDARD_STANDARD_HPP_INCLUDED
+#define NT2_TOOLBOX_STANDARD_STANDARD_HPP_INCLUDED
+
+//<include> please don't modify between these tags
+#include <nt2/toolbox/standard/include/sin.hpp>
+#include <nt2/toolbox/standard/include/acos.hpp>
+#include <nt2/toolbox/standard/include/log10.hpp>
+#include <nt2/toolbox/standard/include/cosh.hpp>
+#include <nt2/toolbox/standard/include/tan.hpp>
+#include <nt2/toolbox/standard/include/exp.hpp>
+#include <nt2/toolbox/standard/include/sinh.hpp>
+#include <nt2/toolbox/standard/include/abs.hpp>
+#include <nt2/toolbox/standard/include/atan2.hpp>
+#include <nt2/toolbox/standard/include/frexp.hpp>
+#include <nt2/toolbox/standard/include/ceil.hpp>
+#include <nt2/toolbox/standard/include/sqrt.hpp>
+#include <nt2/toolbox/standard/include/fmod.hpp>
+#include <nt2/toolbox/standard/include/tanh.hpp>
+#include <nt2/toolbox/standard/include/pow.hpp>
+#include <nt2/toolbox/standard/include/atan.hpp>
+#include <nt2/toolbox/standard/include/floor.hpp>
+#include <nt2/toolbox/standard/include/fabs.hpp>
+#include <nt2/toolbox/standard/include/log.hpp>
+#include <nt2/toolbox/standard/include/cos.hpp>
+#include <nt2/toolbox/standard/include/asin.hpp>
+//<\include>
+
+#endif
