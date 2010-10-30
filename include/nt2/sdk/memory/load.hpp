3bb2ab6bdb1520c2355ac94f3596e9e64dc36067
Joel Falcou <joel.falcou@lri.fr>
2010-10-30 15:31:11 +0200

    Use new traits to check that load works with iterators

--- include/nt2/sdk/memory/load.hpp
+++ include/nt2/sdk/memory/load.hpp
@@ -14,6 +14,7 @@
 // Documentation: http://nt2.lri.fr/sdk/memory/function/load.html
 ////////////////////////////////////////////////////////////////////////////////
 #include <nt2/sdk/functor/functor.hpp>
+#include <nt2/sdk/meta/is_iterator.hpp>
 #include <nt2/sdk/functor/preprocessor/function.hpp>
 
 namespace nt2 { namespace functors
@@ -32,7 +33,7 @@ namespace nt2 { namespace functors
     template<class Sig> struct result;
     template<class This,class A0,class A1>
     struct  result<This(A0,A1)>     // make it check for iterator
-      : boost::mpl::and_< boost::is_pointer<typename meta::strip<A0>::type>
+      : boost::mpl::and_< meta::is_iterator<typename meta::strip<A0>::type>
                         , boost::is_integral<typename meta::strip<A1>::type>
                         >
     {};
