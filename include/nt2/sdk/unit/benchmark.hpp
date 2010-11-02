4fd4ec1ac1fbf78302e13915557c0973e563ece2
Jt lapreste <jtlapreste@gmail.com>
2010-11-03 00:47:05 +0800

    bench loop size corrective

--- include/nt2/sdk/unit/benchmark.hpp
+++ include/nt2/sdk/unit/benchmark.hpp
@@ -57,6 +57,7 @@ NT2_TEST_CASE( benchmark )
 #include <nt2/sdk/unit/details/unary_benchmark.hpp>
 #include <nt2/sdk/unit/details/binary_benchmark.hpp>
 #include <nt2/sdk/unit/details/ternary_benchmark.hpp>
+#include <nt2/sdk/unit/details/_4_ary_benchmark.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
 // Benchmark macros
