cf37ae3cda4f45ef6706aeb6cb6d230188a93b3b
Jt lapreste <jtlapreste@gmail.com>
2010-11-04 17:54:04 +0800

    gsl_specfun toolbox

---
+++ include/nt2/toolbox/gsl_specfun/function/scalar/gsl_sf_conicalP_cyl_reg.hpp
@@ -0,0 +1,52 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#ifndef NT2_TOOLBOX_GSL_SPECFUN_FUNCTION_SCALAR_GSL_SF_CONICALP_CYL_REG_HPP_INCLUDED
+#define NT2_TOOLBOX_GSL_SPECFUN_FUNCTION_SCALAR_GSL_SF_CONICALP_CYL_REG_HPP_INCLUDED
+
+namespace nt2 { namespace functors
+{
+  extern "C"{
+    double gsl_sf_conicalP_cyl_reg(double, double, double); 
+  }
+  template<class Info>
+  struct validate<gsl_specfun::gsl_sf_conicalP_cyl_reg_,tag::scalar_(tag::arithmetic_),Info>
+    {
+      template<class Sig> struct result;
+      template<class This,class A0, class A1, class A2>
+      struct result<This(A0, A1, A2)> :
+        boost::mpl::and_<
+           boost::is_integral<A0>,
+           meta::has_smaller_size<A1,long double>,
+           meta::has_smaller_size<A2,long double>
+        >{};
+    };
+  /////////////////////////////////////////////////////////////////////////////
+  // Compute gsl_sf_conicalP_cyl_reg(const A0& a0, const A1& a1, const A2& a2)
+  /////////////////////////////////////////////////////////////////////////////
+  template<class Info>
+  struct call<gsl_specfun::gsl_sf_conicalP_cyl_reg_,tag::scalar_(tag::arithmetic_),Info>
+  {
+    template<class Sig> struct result;
+    template<class This,class A0, class A1, class A2>
+    struct result<This(A0, A1, A2)> : boost::result_of<meta::floating(A1)>{};
+
+    NT2_FUNCTOR_CALL_DISPATCH( 3, A1, (3, (float,double,arithmetic_)) )
+    NT2_FUNCTOR_CALL_EVAL_IF(3,  float){ return gsl_sf_conicalP_cyl_reg(a0, a1, a2);}
+    NT2_FUNCTOR_CALL_EVAL_IF(3,  double){ return gsl_sf_conicalP_cyl_reg(a0, a1, a2);}
+    NT2_FUNCTOR_CALL_EVAL_IF(3, arithmetic_)
+    {
+      typedef typename NT2_CALL_RETURN_TYPE(3)::type type;
+      return nt2::gsl_specfun::gsl_sf_conicalP_cyl_reg(type(a0), type(a1), type(a2));
+    }
+  };
+} }
+
+
+      
+#endif
