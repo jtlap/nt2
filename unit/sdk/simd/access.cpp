dd556ea66b6bf69aa1a12dfbe4da6800b84c6160
Joel Falcou <joel.falcou@lri.fr>
2010-11-07 03:39:09 +0800

    Fixed load<T,N> for working in SIMD with new functor system

--- unit/sdk/simd/access.cpp
+++ unit/sdk/simd/access.cpp
@@ -8,8 +8,9 @@
  ******************************************************************************/
 #define NT2_UNIT_MODULE "nt2::memory::load and store SIMD"
 
-#include <nt2/sdk/memory/load.hpp>
 #include <nt2/sdk/simd/native.hpp>
+#include <nt2/sdk/simd/io.hpp>
+#include <nt2/sdk/memory/load.hpp>
 #include <nt2/sdk/memory/store.hpp>
 #include <nt2/sdk/memory/is_aligned.hpp>
 #include <nt2/sdk/memory/aligned_type.hpp>
@@ -44,11 +45,10 @@ NT2_TEST_CASE_TPL(load, NT2_SIMD_TYPES )
   }
 }
 
-/*
 ////////////////////////////////////////////////////////////////////////////////
-// Test shifed load behavior
+// Test forward periodic case -- load 1 and 2 cardinal in front
 ////////////////////////////////////////////////////////////////////////////////
-NT2_TEST_CASE_TPL(shifted_load, NT2_SIMD_TYPES )
+NT2_TEST_CASE_TPL(shifted_load_fwd_periodic, NT2_SIMD_TYPES )
 {
   using nt2::load;
   using nt2::simd::native;
@@ -56,32 +56,73 @@ NT2_TEST_CASE_TPL(shifted_load, NT2_SIMD_TYPES )
 
   typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
   typedef native<T,ext_t>             n_t;
+  n_t v;
 
   NT2_ALIGNED_TYPE(T) data[3*cardinal_of<n_t>::value];
   for(std::size_t i=0;i<3*cardinal_of<n_t>::value;++i)
     data[i] = 1+i;
 
-  n_t v = load<n_t,-2>(&data[0],1);
+  v = load<n_t,1*cardinal_of<n_t>::value>(&data[0],0);
   for(std::size_t j=0;j<cardinal_of<n_t>::value;++j)
-    NT2_TEST_EQUAL( v[j], 1+cardinal_of<n_t>::value+j-2 );
+    NT2_TEST_EQUAL( v[j], data[j+1*cardinal_of<n_t>::value] );
 
-  v = load<n_t,-1>(&data[0],1);
+  v = load<n_t,2*cardinal_of<n_t>::value>(&data[0],0);
   for(std::size_t j=0;j<cardinal_of<n_t>::value;++j)
-    NT2_TEST_EQUAL( v[j], 1+cardinal_of<n_t>::value+j-1 );
+    NT2_TEST_EQUAL( v[j], data[j+2*cardinal_of<n_t>::value] );
+}
 
-  v = load<n_t,0>(&data[0],1);
-  for(std::size_t j=0;j<cardinal_of<n_t>::value;++j)
-    NT2_TEST_EQUAL( v[j], 1+cardinal_of<n_t>::value+j );
+////////////////////////////////////////////////////////////////////////////////
+// Test non-periodic case -- load up to cardinal-1 front
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE_TPL(shifted_load_non_periodic, NT2_SIMD_TYPES )
+{
+  using nt2::load;
+  using nt2::simd::native;
+  using nt2::meta::cardinal_of;
+
+  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>             n_t;
+  n_t v;
+
+  NT2_ALIGNED_TYPE(T) data[3*cardinal_of<n_t>::value];
+  for(std::size_t i=0;i<3*cardinal_of<n_t>::value;++i)
+    data[i] = 1+i;
 
   v = load<n_t,1>(&data[0],1);
   for(std::size_t j=0;j<cardinal_of<n_t>::value;++j)
-    NT2_TEST_EQUAL( v[j], 1+cardinal_of<n_t>::value+j+1 );
+    NT2_TEST_EQUAL( v[j], data[j+cardinal_of<n_t>::value+1] );
 
-  v = load<n_t,2>(&data[0],1);
+  v = load<n_t,-1>(&data[0],1);
   for(std::size_t j=0;j<cardinal_of<n_t>::value;++j)
-    NT2_TEST_EQUAL( v[j], 1+cardinal_of<n_t>::value+j+2 );
+    NT2_TEST_EQUAL( v[j], data[j+cardinal_of<n_t>::value-1] );
 }
-*/
+
+////////////////////////////////////////////////////////////////////////////////
+// Test backward periodic case -- load 1 and 2 cardinal in front
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE_TPL(shifted_load_bkwd_periodic, NT2_SIMD_TYPES )
+{
+  using nt2::load;
+  using nt2::simd::native;
+  using nt2::meta::cardinal_of;
+
+  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>             n_t;
+  n_t v;
+
+  NT2_ALIGNED_TYPE(T) data[3*cardinal_of<n_t>::value];
+  for(std::size_t i=0;i<3*cardinal_of<n_t>::value;++i)
+    data[i] = 1+i;
+
+  v = load<n_t,-1*cardinal_of<n_t>::value>(&data[0],2);
+  for(std::size_t j=0;j<cardinal_of<n_t>::value;++j)
+    NT2_TEST_EQUAL( v[j], data[j+cardinal_of<n_t>::value] );
+
+  v = load<n_t,-2*cardinal_of<n_t>::value>(&data[0],2);
+  for(std::size_t j=0;j<cardinal_of<n_t>::value;++j)
+    NT2_TEST_EQUAL( v[j], data[j] );
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Test store behavior
 ////////////////////////////////////////////////////////////////////////////////
