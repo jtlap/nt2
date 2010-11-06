fa3aaa31aa56868b5c4a2cc089c28e40a2d48854
Joel Falcou <joel.falcou@lri.fr>
2010-11-06 23:36:23 +0100

    Merged jtlap fix for tchebeval and remquo

--- include/nt2/toolbox/arithmetic/unit/scalar/remquo.cpp
+++ include/nt2/toolbox/arithmetic/unit/scalar/remquo.cpp
@@ -43,8 +43,8 @@ NT2_TEST_CASE_TPL ( remquo,  (double)
  
   for(int i = 0;  i < 3;  i++){
     type_t r = remquo(n[i], d[i]);
-    NT2_TEST_LESSER_EQUAL(  nt2::ulpdist( boost::fusion::get<0>(r), nt2::remainder(n[i], d[i]) ), 0.5  );
-    NT2_TEST_EQUAL(  boost::fusion::get<1>(r), nt2::idivfix(n[i], d[i])); 
+    NT2_TEST_EQUAL(  boost::fusion::get<0>(r), nt2::remainder(n[i], d[i]));
+    NT2_TEST_EQUAL(  boost::fusion::get<1>(r), nt2::idivfix(n[i], d[i]));
   }
 }
           
