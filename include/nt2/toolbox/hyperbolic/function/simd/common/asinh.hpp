8df7e5bd3a406d47724c551f4ea5424e84e536bf
Jt lapreste <jtlapreste@gmail.com>
2010-11-16 15:35:23 +0100

    hyperbolic update

--- include/nt2/toolbox/hyperbolic/function/simd/common/asinh.hpp
+++ include/nt2/toolbox/hyperbolic/function/simd/common/asinh.hpp
@@ -44,8 +44,8 @@ namespace nt2 { namespace functors
   /////////////////////////////////////////////////////////////////////////////
   // Implementation when type A0 is float
   /////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct  call<asinh_,tag::simd_(tag::arithmetic_),float,Info> : callable
+  template<class Extension, class Info>
+  struct call<asinh_,tag::simd_(tag::arithmetic_,Extension),float,Info> : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
@@ -74,8 +74,8 @@ namespace nt2 { namespace functors
   /////////////////////////////////////////////////////////////////////////////
   // Implementation when type A0 is double
   /////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct  call<asinh_,tag::simd_(tag::arithmetic_),double,Info> : callable
+  template<class Extension, class Info>
+  struct call<asinh_,tag::simd_(tag::arithmetic_,Extension),double,Info> : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
@@ -102,8 +102,8 @@ namespace nt2 { namespace functors
   /////////////////////////////////////////////////////////////////////////////
   // Implementation when type A0 is arithmetic_
   /////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct  call<asinh_,tag::simd_(tag::arithmetic_),arithmetic_,Info> : callable
+  template<class Extension, class Info>
+  struct call<asinh_,tag::simd_(tag::arithmetic_,Extension),arithmetic_,Info> : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
--- include/nt2/toolbox/hyperbolic/function/simd/common/sinh.hpp
+++ include/nt2/toolbox/hyperbolic/function/simd/common/sinh.hpp
@@ -35,8 +35,8 @@ namespace nt2 { namespace functors
   /////////////////////////////////////////////////////////////////////////////
   // Implementation when type A0 is real_
   /////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct  call<sinh_,tag::simd_(tag::arithmetic_),real_,Info> : callable
+  template<class Extension, class Info>
+  struct call<sinh_,tag::simd_(tag::arithmetic_,Extension),real_,Info> : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
@@ -80,8 +80,8 @@ namespace nt2 { namespace functors
   /////////////////////////////////////////////////////////////////////////////
   // Implementation when type A0 is arithmetic_
   /////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct  call<sinh_,tag::simd_(tag::arithmetic_),arithmetic_,Info> : callable
+  template<class Extension, class Info>
+  struct call<sinh_,tag::simd_(tag::arithmetic_,Extension),arithmetic_,Info> : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
@@ -91,7 +91,7 @@ namespace nt2 { namespace functors
     {
       typedef typename NT2_CALL_RETURN_TYPE(1)::type type; 
       return sinh(tofloat(a0));
-    }  };
+    } 
   };
 
 } }
