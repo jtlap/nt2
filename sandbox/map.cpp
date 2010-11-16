785e7d2f8d49634f7ef422d457917bdb4e2471c9
Joel Falcou <joel.falcou@lri.fr>
2010-11-16 20:04:14 +0100

    Modified digits constants to take care of proper hierarchy dispatching

--- sandbox/map.cpp
+++ sandbox/map.cpp
@@ -9,6 +9,6 @@ int main()
 {
   native<float, tag::sse_> x = {{2,4,4,6}};
 
-  cout << (~x) << endl;
-  cout << x/x << endl;
+  cout << True< native<float, tag::sse_> >() << endl;
+  cout << True< native<double, tag::sse_> >() << endl;
 }
