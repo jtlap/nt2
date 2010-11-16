7871ab6288b6b6ba9d0023f03599f3d9df0abd31
Jt lapreste <jtlapreste@gmail.com>
2010-11-16 15:34:19 +0100

    exponential update

--- include/nt2/toolbox/exponential/function/simd/common/expm1.hpp
+++ include/nt2/toolbox/exponential/function/simd/common/expm1.hpp
@@ -45,8 +45,8 @@ namespace nt2 { namespace functors
   /////////////////////////////////////////////////////////////////////////////
   // Implementation when type A0 is float
   /////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct  call<expm1_,tag::simd_(tag::arithmetic_),float,Info> : callable
+  template<class Extension, class Info>
+  struct call<expm1_,tag::simd_(tag::arithmetic_,Extension),float,Info> : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
@@ -67,8 +67,8 @@ namespace nt2 { namespace functors
   /////////////////////////////////////////////////////////////////////////////
   // Implementation when type A0 is double
   /////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct  call<expm1_,tag::simd_(tag::arithmetic_),double,Info> : callable
+  template<class Extension, class Info>
+  struct call<expm1_,tag::simd_(tag::arithmetic_,Extension),double,Info> : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
@@ -112,8 +112,8 @@ namespace nt2 { namespace functors
   /////////////////////////////////////////////////////////////////////////////
   // Implementation when type A0 is arithmetic_
   /////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct  call<expm1_,tag::simd_(tag::arithmetic_),arithmetic_,Info> : callable
+  template<class Extension, class Info>
+  struct call<expm1_,tag::simd_(tag::arithmetic_,Extension),arithmetic_,Info> : callable
   {
     template<class Sig> struct result;
     template<class This,class A0>
