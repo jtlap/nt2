2cf9ca778f85c15c61cc88e1ceeb11c9386011da
Joel Falcou <joel.falcou@lri.fr>
2010-11-20 19:20:05 +0800

    Fixed SIMD store to use proper external call dispacth

--- include/nt2/sdk/simd/details/impl/sse/sse2/store.hpp
+++ include/nt2/sdk/simd/details/impl/sse/sse2/store.hpp
@@ -20,8 +20,8 @@ namespace nt2 { namespace functors
   // Store a vector
   //////////////////////////////////////////////////////////////////////////////
   template<class Info>
-  struct  call< store_      , tag::simd_(tag::arithmetic_,tag::sse_)
-              , fundamental_, Info
+  struct  call< store_, tag::simd_(tag::arithmetic_,tag::sse_)
+              ,double , Info
               >
         : callable
   {
@@ -29,24 +29,41 @@ namespace nt2 { namespace functors
     template<class This,class A0,class A1, class A2>
     struct  result<This(A0,A1,A2)> : meta::strip<A0> {};
 
-    NT2_FUNCTOR_CALL_DISPATCH ( 3
-                              , typename boost::remove_pointer<A1>::type
-                              , (3, (double,float, integer_))
-                              )
-
-    NT2_FUNCTOR_CALL_EVAL_IF(3,double)
+    NT2_FUNCTOR_CALL(3)
     {
       _mm_store_pd(a1+2*a2,a0);
       return a0;
     }
+  };
 
-    NT2_FUNCTOR_CALL_EVAL_IF(3,float)
+  template<class Info>
+  struct  call< store_, tag::simd_(tag::arithmetic_,tag::sse_)
+              , float , Info
+              >
+        : callable
+  {
+    template<class Sig> struct result;
+    template<class This,class A0,class A1, class A2>
+    struct  result<This(A0,A1,A2)> : meta::strip<A0> {};
+
+    NT2_FUNCTOR_CALL(3)
     {
       _mm_store_ps(a1+4*a2,a0);
       return a0;
     }
+  };
 
-    NT2_FUNCTOR_CALL_EVAL_IF(3,integer_)
+  template<class Info>
+  struct  call< store_  , tag::simd_(tag::arithmetic_,tag::sse_)
+              , integer_, Info
+              >
+        : callable
+  {
+    template<class Sig> struct result;
+    template<class This,class A0,class A1, class A2>
+    struct  result<This(A0,A1,A2)> : meta::strip<A0> {};
+
+    NT2_FUNCTOR_CALL(3)
     {
       _mm_store_si128(reinterpret_cast<__m128i*>(a1)+a2, a0);
       return a0;
