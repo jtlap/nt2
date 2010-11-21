458a0ce94ab4328434db2c6af36b889838bae69f
Joel Falcou <joel.falcou@lri.fr>
2010-11-21 14:25:05 +0100

    Fixed spalt for SIMD types. We missed the dispatch over scalr_of<T> and the split using new functor dispacth system.

--- include/nt2/sdk/simd/details/impl/splat.hpp
+++ include/nt2/sdk/simd/details/impl/splat.hpp
@@ -10,6 +10,18 @@
 #define NT2_SDK_SIMD_DETAILS_IMPL_SPLAT_HPP_INCLUDED
 
 #include <nt2/sdk/simd/preprocessor/include.hpp>
+
+namespace nt2 { namespace functors
+{
+  //////////////////////////////////////////////////////////////////////////////
+  // We dispatch on the scalar T type for SIMD splat
+  //////////////////////////////////////////////////////////////////////////////
+  template<class T,class Category, class Extension, class Info>
+  struct  dispatch<splat_<T>,tag::simd_(Category,Extension),Info> 
+        : boost::mpl::always<typename meta::scalar_of<T>::type>
+  {};
+} }
+
 #include NT2_SIMD_INCLUDE(splat.hpp)
 
 #endif
