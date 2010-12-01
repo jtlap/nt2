1277e63e4ffd674e10bb5a6f0c4acd6915bdf3a1
Joel Falcou <joel.falcou@lri.fr>
2010-12-01 07:12:53 +0100

    Updated sandbox test

--- sandbox/functor3.cpp
+++ sandbox/functor3.cpp
@@ -7,30 +7,80 @@ using namespace std;
 
 namespace nt2 { namespace tag
 {
-  struct scalar_  {};
-  struct simd_    {};
+  struct cpu_  {};
 } }
 
 // new files
-#include <nt2/sdk/functor/forward.hpp>
-#include <nt2/sdk/functor/details/call.hpp>
-#include <nt2/sdk/functor/meta/hierarchy.hpp>
-#include <nt2/sdk/functor/details/dispatch.hpp>
-#include <nt2/sdk/functor/meta/hierarchy_of.hpp>
+#include <nt2/sdk/functor/functor.hpp>
 
 // test
 #include <nt2/sdk/functor/details/tags.hpp>
 #include <nt2/sdk/details/type_id.hpp>
 
 typedef nt2::tag::complement_ cmp;
+#include <nt2/sdk/functor/preprocessor/function.hpp>
 
-NT2_REGISTER_DISPATCH(cmp, tag::scalar_, (A0)(A1), (uint32_<A0>)(real_<A1>) );
-NT2_REGISTER_DISPATCH(cmp, tag::scalar_, (A0), (real_<A0>) );
-NT2_REGISTER_DISPATCH(cmp, tag::scalar_, (A0), (unsigned_<A0>) )
-NT2_REGISTER_DISPATCH(cmp, tag::scalar_, (A0), (signed_<A0>) );
-NT2_REGISTER_DISPATCH(cmp, tag::scalar_, (A0), (real_<A0>)(real_<A0>)  );
+NT2_FUNCTION_IMPLEMENTATION(cmp,ffff,2)
+NT2_FUNCTION_IMPLEMENTATION(cmp,ffff,1)
+
+NT2_REGISTER_DISPATCH(cmp, tag::cpu_, (A0)(A1), (uint32_<A0>)(real_<A1>) );
+NT2_REGISTER_DISPATCH(cmp, tag::cpu_, (A0), (real_<A0>) );
+NT2_REGISTER_DISPATCH(cmp, tag::cpu_, (A0), (unsigned_<A0>) )
+NT2_REGISTER_DISPATCH(cmp, tag::cpu_, (A0), (signed_<A0>) );
+NT2_REGISTER_DISPATCH(cmp, tag::cpu_, (A0), (real_<A0>)(real_<A0>)  );
 
 NT2_REGISTER_HIERARCHY(vector_)
+
+namespace nt2 { namespace ext
+{
+  template<class Dummy>
+  struct call<tag::complement_(tag::real_),tag::cpu_,Dummy> : callable
+  {
+    typedef float result_type;
+
+    template<class A0> inline
+    result_type operator()(A0 const& a0) const
+    {
+      return 3.1415*a0;
+    }
+  };
+
+  template<class Dummy>
+  struct call<tag::complement_(tag::real_,tag::real_),tag::cpu_,Dummy> : callable
+  {
+    typedef float result_type;
+
+    template<class A0, class A1> inline
+    result_type operator()(A0 const& a0,A1 const& a1) const
+    {
+      return a1*a0;
+    }
+  };
+
+  template<class Dummy>
+  struct call<tag::complement_(tag::signed_),tag::cpu_,Dummy> : callable
+  {
+    typedef float result_type;
+
+    template<class A0> inline
+    result_type operator()(A0 const& a0) const
+    {
+      return -a0;
+    }
+  };
+
+  template<class Dummy>
+  struct call<tag::complement_(tag::vector_(tag::real_)),tag::cpu_,Dummy> : callable
+  {
+    typedef float result_type;
+
+    template<class A0> inline
+    result_type operator()(A0 const& a0) const
+    {
+      return a0.size();
+    }
+  };
+} }
 
 namespace nt2 { namespace meta
 {
@@ -41,25 +91,15 @@ namespace nt2 { namespace meta
   };
 } }
 
-NT2_REGISTER_DISPATCH(cmp, tag::scalar_, (A0), (vector_< nt2::meta::integer_<A0> >) );
-NT2_REGISTER_DISPATCH(cmp, tag::scalar_, (A0), (vector_< nt2::meta::real_<A0> >) );
+NT2_REGISTER_DISPATCH(cmp, tag::cpu_, (A0), (vector_< nt2::meta::integer_<A0> >) );
+NT2_REGISTER_DISPATCH(cmp, tag::cpu_, (A0), (vector_< nt2::meta::real_<A0> >) );
 
 int main()
 {
-  cout
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(char), nt2::tag::scalar_>::type>() << "\n"
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(short), nt2::tag::scalar_>::type>() << "\n"
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(std::vector<short>), nt2::tag::scalar_>::type>() << "\n"
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(std::vector<float>), nt2::tag::scalar_>::type>() << "\n"
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(int), nt2::tag::scalar_>::type>() << "\n\n";
-  cout
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(unsigned char), nt2::tag::scalar_>::type>() << "\n"
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(unsigned short), nt2::tag::scalar_>::type>() << "\n"
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(unsigned int), nt2::tag::scalar_>::type>() << "\n\n";
-  cout
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(float), nt2::tag::scalar_>::type>() << "\n"
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(double), nt2::tag::scalar_>::type>() << "\n"
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(float,float), nt2::tag::scalar_>::type>() << "\n"
-  << nt2::type_id<nt2::meta::dispatch_call<cmp(double,double), nt2::tag::scalar_>::type>() << "\n"
-;//  << nt2::type_id<nt2::meta::dispatch_call<cmp(double,float), nt2::tag::scalar_>::type>() << "\n";
+  std::vector<float> k(1337);
+
+  cout << ffff('A') << "\n";
+  cout << ffff(1000.) << "\n";
+  cout << ffff(k) << "\n";
+  cout << ffff(1000.f,0.001f) << "\n";
 }
