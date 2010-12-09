f66db5c375e0519cfb7675e4450f3bc3375adc1f
Joel Falcou <joel.falcou@lri.fr>
2010-12-09 16:51:49 +0100

    load and store now works with new functor system

---
+++ include/nt2/sdk/memory/details/category.hpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_MEMORY_DETAILS_CATEGORY_HPP_INCLUDED
+#define NT2_SDK_MEMORY_DETAILS_CATEGORY_HPP_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+// Various memory hierarchy stuff
+////////////////////////////////////////////////////////////////////////////////
+#include <nt2/sdk/meta/is_iterator.hpp>
+#include <nt2/sdk/meta/hierarchy_of.hpp>
+#include <boost/pointee.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+// Specialize hierarchy for all iterator types
+////////////////////////////////////////////////////////////////////////////////
+NT2_REGISTER_HIERARCHY(iterator_)
+
+////////////////////////////////////////////////////////////////////////////////
+// Specialize hierarchy for all iterator types
+////////////////////////////////////////////////////////////////////////////////
+namespace nt2 { namespace meta
+{
+  template<class T>
+  struct  hierarchy_of< T
+                      , typename boost::enable_if_c<is_iterator<T>::value>::type
+                      >
+  {
+    typedef typename boost::pointee<T>::type                        pointee_type;
+    typedef iterator_< typename hierarchy_of<pointee_type>::type > type;
+  };
+} }
+
+#endif
--- include/nt2/sdk/memory/load.hpp
+++ include/nt2/sdk/memory/load.hpp
@@ -13,9 +13,9 @@
 // Load from memory functor and function
 // Documentation: http://nt2.lri.fr/sdk/memory/function/load.html
 ////////////////////////////////////////////////////////////////////////////////
-#include <boost/mpl/always.hpp>
+#include <nt2/sdk/meta/as.hpp>
 #include <nt2/sdk/functor/functor.hpp>
-#include <nt2/sdk/meta/is_iterator.hpp>
+#include <boost/utility/enable_if.hpp>
 #include <nt2/sdk/functor/preprocessor/function.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -29,29 +29,32 @@ namespace nt2
   // Load a data of type T from the memory zone given by (a0,a1)
   //////////////////////////////////////////////////////////////////////////////
   template<class T,class A0,class A1> inline
-  typename boost::lazy_enable_if_type
-          < meta::is_iterator<A0>
-          , meta::enable_call<tag::load_(A0,A1,meta::as_<T>,boost::mpl::int_<0>)>
-          >::type
+  typename meta::enable_call<tag::load_ ( A0 const&, A1 const&
+                                        , meta::as_<T>
+                                        , boost::mpl::int_<0>
+                                        )
+                            >::type
   load(A0 const& a0,A1 const& a1 )
   {
-    functor<load_> callee;
+    functor<tag::load_> callee;
     return callee(a0,a1,meta::as_<T>(),boost::mpl::int_<0>());
   }
-/*
+
   //////////////////////////////////////////////////////////////////////////////
   // Load a data of type T from the memory zone given by (a0,a1) and a sub-type
   // level offset
   //////////////////////////////////////////////////////////////////////////////
-  template<class T, int Offset, class A0,class A1> inline
-  typename
-  nt2::meta::enable_call<functors::load_<T,Offset>(A0 const&,A1 const&)>::type
+  template<class T,int Offset, class A0,class A1> inline
+  typename meta::enable_call<tag::load_ ( A0 const&, A1 const&
+                                        , meta::as_<T>
+                                        , boost::mpl::int_<Offset>
+                                        )
+                            >::type
   load(A0 const& a0,A1 const& a1 )
   {
-    typedef nt2::functors::load_<T,Offset> tag_;
-    NT2_FUNCTION_BODY(tag_,2)
+    functor<tag::load_> callee;
+    return callee(a0,a1,meta::as_<T>(),boost::mpl::int_<Offset>());
   }
-*/
 }
 
 #include <nt2/sdk/memory/details/load.hpp>
