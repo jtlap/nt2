d79929cde0084394994b046304b4106330f1c9af
Joel Falcou <joel.falcou@lri.fr>
2010-12-14 15:06:02 +0100

    Added the no_padding strategy in new functor implementation

--- include/nt2/sdk/memory/details/no_padding.hpp
+++ include/nt2/sdk/memory/details/no_padding.hpp
@@ -10,10 +10,11 @@
 #define NT2_SDK_MEMORY_DETAILS_NO_PADDING_HPP_INCLUDED
 
 ////////////////////////////////////////////////////////////////////////////////
-// Padding strategies for memory allocation
+// Implementation of the no_padding strategy for memory allocation
 ////////////////////////////////////////////////////////////////////////////////
 #include <boost/mpl/pair.hpp>
 #include <boost/typeof/typeof.hpp>
+#include <nt2/sdk/meta/fusion.hpp>
 #include <nt2/sdk/memory/slice.hpp>
 #include <nt2/sdk/memory/stride.hpp>
 #include <boost/fusion/include/at.hpp>
@@ -22,71 +23,77 @@
 #include <nt2/sdk/functor/preprocessor/call.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
-// slice Functor implementation
+// slice implementation for no_padding strategy
 ////////////////////////////////////////////////////////////////////////////////
-namespace nt2 { namespace functors
+namespace nt2 { namespace ext
 {
-  //////////////////////////////////////////////////////////////////////////////
-  // For no_padding stride, we dispatch on the fact the Level is the last one
-  //////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct dispatch<slice_,tag::fusion_(memory::no_padding),Info>
-  {
-    template<class A0,class A1,class A2> struct apply
-    {
-      typedef boost::mpl::bool_ <   A2::value
-                                ==  boost::fusion::result_of::size<A0>::value
-                                >                     type;
-    };
-  };
-
-  template<class Info>
-  struct  call<slice_,tag::fusion_(memory::no_padding),boost::mpl::true_,Info>
+  template<class Dummy>
+  struct  call< tag::slice_ ( tag::fusion_sequence_
+                            , tag::padding_(memory::no_padding)
+                            , tag::mpl_integral_(tag::integer_)
+                            )
+              , tag::cpu_, Dummy  >
         : callable
   {
+    ////////////////////////////////////////////////////////////////////////////
+    // Small internal to see if the index is the same as the sequence size
+    ////////////////////////////////////////////////////////////////////////////
+    template<class S,class N>
+    struct  same_size
+          : boost::mpl::bool_<(   N::value
+                              ==  boost::fusion::result_of::size<S>::value
+                              )>
+    {};
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Computes the actual result type depending on A0 size and A2 value
+    ////////////////////////////////////////////////////////////////////////////
     template<class Sig> struct result;
 
     template<class This,class A0, class A1, class A2>
     struct  result<This(A0,A1,A2)>
-          : boost::fusion::result_of::at_c< typename meta::strip<A0>::type const
-                                          , meta::strip<A2>::type::value-1>
-    {};
+    {
+      typedef typename meta::strip<A0>::type arg0;
+      typedef typename meta::strip<A1>::type arg1;
+      typedef typename meta::strip<A2>::type arg2;
 
-    NT2_FUNCTOR_CALL(3)
+      typedef boost::fusion::result_of::at_c<arg0 const,arg2::value-1>  true_case;
+
+      static arg0 const& s; static arg1 const&  p;
+
+      BOOST_TYPEOF_NESTED_TYPEDEF_TPL
+      ( false_case
+      , nt2::mul( slice<arg2::value+1>(s,p)
+                , boost::fusion::at_c<arg2::value-1>(s)
+                )
+      );
+
+      typedef typename boost::mpl::eval_if< same_size<arg0,arg2>
+                                          , true_case
+                                          , false_case
+                                          >::type type;
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Implementation when size<A0> == A2
+    ////////////////////////////////////////////////////////////////////////////
+    template<class A0, class A1, class A2> inline
+    typename boost::lazy_enable_if< same_size<A0,A2>, NT2_RETURN_TYPE(3)>::type
+    operator()( A0 const& a0, A1 const&, A2 const& ) const
     {
       return boost::fusion::at_c<A2::value-1>(a0);
     }
-  };
 
-  template<class Info>
-  struct  call<slice_,tag::fusion_(memory::no_padding),boost::mpl::false_,Info>
-        : callable
-  {
-    template<class Sig> struct result;
-
-    template<class This,class A0, class A1, class A2>
-    struct  result<This(A0,A1,A2)>
+    ////////////////////////////////////////////////////////////////////////////
+    // Implementation when size<A0> != A2
+    ////////////////////////////////////////////////////////////////////////////
+    template<class A0, class A1, class A2> inline
+    typename boost::lazy_disable_if< same_size<A0,A2>, NT2_RETURN_TYPE(3)>::type
+    operator()( A0 const& a0, A1 const& a1, A2 const& ) const
     {
-      static typename meta::strip<A0>::type const&  s;
-      static typename meta::strip<A1>::type const&  p;
-      static details::times callee;
-
-      BOOST_TYPEOF_NESTED_TYPEDEF_TPL
-      ( nested
-      , callee( slice<meta::strip<A2>::type::value+1>(s,p)
-              , boost::fusion::at_c<meta::strip<A2>::type::value-1>(s)
-              )
-      );
-
-      typedef typename nested::type type;
-    };
-
-    NT2_FUNCTOR_CALL(3)
-    {
-      details::times callee;
-      return callee ( slice<A2::value+1>(a0,a1)
-                    , boost::fusion::at_c<A2::value-1>(a0)
-                    );
+      return nt2::mul ( slice<A2::value+1>(a0,a1)
+                      , boost::fusion::at_c<A2::value-1>(a0)
+                      );
     }
   };
 } }
@@ -94,22 +101,14 @@ namespace nt2 { namespace functors
 ////////////////////////////////////////////////////////////////////////////////
 // stride Functor implementation
 ////////////////////////////////////////////////////////////////////////////////
-namespace nt2 { namespace functors
+namespace nt2 { namespace ext
 {
-  //////////////////////////////////////////////////////////////////////////////
-  // For no_padding stride, we just don't care about the dispatching
-  //////////////////////////////////////////////////////////////////////////////
-  template<class Info>
-  struct dispatch<stride_,tag::fusion_(memory::no_padding),Info>
-  {
-    template<class A0,class A1,class A2> struct apply
-    {
-      typedef fundamental_ type;
-    };
-  };
-
-  template<class Hierarchy, class Info>
-  struct call<stride_,tag::fusion_(memory::no_padding),Hierarchy,Info>
+  template<class Dummy>
+  struct  call< tag::stride_( tag::fusion_sequence_
+                            , tag::padding_(memory::no_padding)
+                            , tag::mpl_integral_(tag::integer_)
+                            )
+              , tag::cpu_, Dummy  >
         : callable
   {
     template<class Sig> struct result;
