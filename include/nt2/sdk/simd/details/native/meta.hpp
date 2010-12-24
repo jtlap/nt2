81c2b1ea2002d9af1920333938782216e14e9929
Joel Falcou <joel.falcou@lri.fr>
2010-12-24 19:56:56 +0100

    Added SIMD meta::downgrade and meta::upgrade

--- include/nt2/sdk/simd/details/native/meta.hpp
+++ include/nt2/sdk/simd/details/native/meta.hpp
@@ -18,12 +18,6 @@
 #include <nt2/sdk/simd/details/native/meta/is_signed.hpp>
 #include <nt2/sdk/simd/details/native/meta/as_integer.hpp>
 #include <nt2/sdk/simd/details/native/meta/cardinal_of.hpp>
-
-/*
-#include <nt2/sdk/simd/details/native/as_bits.hpp>
-#include <nt2/sdk/simd/details/native/logical.hpp>
-#include <nt2/sdk/simd/details/native/model_of.hpp>
-#include <nt2/sdk/simd/details/native/from_bits.hpp>
-*/
+#include <nt2/sdk/simd/details/native/meta/downgrade.hpp>
 
 #endif
