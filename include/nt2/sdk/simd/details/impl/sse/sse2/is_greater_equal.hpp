b3e4f43e8c1f20fbbd233d103131c13b6a3b719c
Joel Falcou <joel.falcou@lri.fr>
2011-01-02 21:29:32 +0100

    Added greater_equal in SIMD + test

--- include/nt2/sdk/simd/details/impl/sse/sse2/is_greater_equal.hpp
+++ include/nt2/sdk/simd/details/impl/sse/sse2/is_greater_equal.hpp
@@ -45,6 +45,36 @@ namespace nt2 { namespace ext
 // Overloads implementation for float
 ////////////////////////////////////////////////////////////////////////////////
 NT2_REGISTER_DISPATCH ( tag::is_greater_equal_, tag::cpu_, (A0)
+                      , ((simd_<float_<A0>,tag::sse_>))
+                        ((simd_<float_<A0>,tag::sse_>))
+                      );
+
+namespace nt2 { namespace ext
+{
+  template<class Dummy>
+  struct  call< tag::is_greater_equal_( tag::simd_(tag::float_,tag::sse_)
+                                      , tag::simd_(tag::float_,tag::sse_)
+                                      )
+              , tag::cpu_, Dummy
+              >
+        : callable
+  {
+    template<class Sig>           struct result;
+    template<class This,class A0>
+    struct result<This(A0,A0)> : meta::strip<A0> {};
+
+    NT2_FUNCTOR_CALL(2)
+    {
+      A0 that = { _mm_cmpge_ps(a0,a1) };
+      return that;
+    }
+  };
+} }
+
+////////////////////////////////////////////////////////////////////////////////
+// Overloads implementation for integers
+////////////////////////////////////////////////////////////////////////////////
+NT2_REGISTER_DISPATCH ( tag::is_greater_equal_, tag::cpu_, (A0)
                       , ((simd_<integer_<A0>,tag::sse_>))
                         ((simd_<integer_<A0>,tag::sse_>))
                       );
