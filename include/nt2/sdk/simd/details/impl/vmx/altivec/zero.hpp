1feb1e0257911108569db9153cc2008228c78389
Joel Falcou <joel.falcou@lri.fr>
2011-01-03 20:48:41 +0100

    Added a proper Zero implementation for altivec using splat_u8

--- include/nt2/sdk/simd/details/impl/vmx/altivec/zero.hpp
+++ include/nt2/sdk/simd/details/impl/vmx/altivec/zero.hpp
@@ -6,20 +6,23 @@
 ///                 See accompanying file LICENSE.txt or copy at
 ///                     http://www.boost.org/LICENSE_1_0.txt
 //////////////////////////////////////////////////////////////////////////////
-#ifndef NT2_SDK_SIMD_DETAILS_IMPL_VMX_ALTIVEC_TRUE_HPP_INCLUDED
-#define NT2_SDK_SIMD_DETAILS_IMPL_VMX_ALTIVEC_TRUE_HPP_INCLUDED
+#ifndef NT2_SDK_SIMD_DETAILS_IMPL_VMX_ALTIVEC_ZERO_HPP_INCLUDED
+#define NT2_SDK_SIMD_DETAILS_IMPL_VMX_ALTIVEC_ZERO_HPP_INCLUDED
 
 ////////////////////////////////////////////////////////////////////////////////
-// in SIMD, True is not !0 but ~0 whatever the type
+//
 ////////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH ( tag::true_, tag::cpu_, (A0)
+NT2_REGISTER_DISPATCH ( tag::digit_<0>, tag::cpu_, (A0)
                       , ((target_< simd_< arithmetic_<A0>,tag::altivec_> >))
                       )
 
 namespace nt2 { namespace ext
 {
   template<class Dummy>
-  struct  call< tag::true_( tag::target_(tag::simd_(tag::arithmetic_,tag::altivec_)) )
+  struct  call< tag::digit_<0>( tag::target_(tag::simd_ ( tag::arithmetic_
+                                                        , tag::altivec_
+                                                        )
+                              ) )
               , tag::cpu_
               , Dummy
               >
@@ -32,7 +35,7 @@ namespace nt2 { namespace ext
     NT2_FUNCTOR_CALL(1)
     {
       typedef typename NT2_RETURN_TYPE(1)::type type;
-      type that = { simd::native_cast<type>(vec_splat_u8(-1)) };
+      type that = { simd::native_cast<type>(vec_splat_u8(0)) };
       return that;
     }
   };
