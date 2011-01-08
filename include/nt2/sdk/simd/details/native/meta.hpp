3e2755ad83d25c98e0f08e9cee988ebc26b517a7
Joel Falcou <joel.falcou@lri.fr>
2011-01-08 18:21:41 +0100

    Fixed hierarchy_of for native SIMD type

--- include/nt2/sdk/simd/details/native/meta.hpp
+++ include/nt2/sdk/simd/details/native/meta.hpp
@@ -18,6 +18,7 @@
 #include <nt2/sdk/simd/details/native/meta/is_signed.hpp>
 #include <nt2/sdk/simd/details/native/meta/is_scalar.hpp>
 #include <nt2/sdk/simd/details/native/meta/as_integer.hpp>
+#include <nt2/sdk/simd/details/native/meta/hierarchy_of.hpp>
 #include <nt2/sdk/simd/details/native/meta/cardinal_of.hpp>
 #include <nt2/sdk/simd/details/native/meta/downgrade.hpp>
 #include <nt2/sdk/simd/details/native/meta/upgrade.hpp>
