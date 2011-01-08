149f1e01df163094cd76c3e29597df6052fff979
Joel Falcou <joel.falcou@lri.fr>
2011-01-08 21:04:30 +0100

    Function pointers are not hierarchized as iterator anymore. Make map works with function pointer.

--- include/nt2/sdk/meta/is_iterator.hpp
+++ include/nt2/sdk/meta/is_iterator.hpp
@@ -10,21 +10,36 @@
 #define NT2_SDK_META_IS_ITERATOR_HPP_INCLUDED
 
 #include <boost/mpl/bool.hpp>
+#include <boost/type_traits/is_function.hpp>
+#include <boost/type_traits/remove_pointer.hpp>
 #include <nt2/sdk/meta/enable_if_type.hpp>
 
 namespace nt2 { namespace meta
 {
   template<class T, class Enable=void>
-  struct  is_iterator
-        : boost::mpl::false_  {};
+  struct  is_iterator : boost::mpl::false_  {};
 
-  template<class T, class Enable>
-  struct  is_iterator<T*,Enable>
-        : boost::mpl::true_   {};
+  template<class T>
+  struct  is_iterator < T*
+                      , typename boost::
+                        enable_if_c< !boost::
+                                     is_function<typename boost::
+                                                 remove_pointer<T>::type
+                                                >::value
+                                   >::type
+                    >
+    : boost::mpl::true_   {};
 
-  template<class T, class Enable>
-  struct  is_iterator<const T*,Enable>
-        : boost::mpl::true_   {};
+  template<class T>
+  struct  is_iterator < T
+                      , typename boost::
+                        enable_if_c< boost::
+                                     is_function<typename boost::
+                                                 remove_pointer<T>::type
+                                                >::value
+                                      >::type
+                      >
+        : boost::mpl::false_ {};
 
   template<class T>
   struct  is_iterator < T
