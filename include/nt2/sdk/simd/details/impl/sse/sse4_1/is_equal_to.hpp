95551c517f22d7d8f9e3a38044d5ce87d7d64b47
Joel Falcou <joel.falcou@lri.fr>
2011-01-08 11:59:12 +0100

    Fixed missing is_equal implementation in SSE 4.1

--- include/nt2/sdk/simd/details/impl/sse/sse4_1/is_equal_to.hpp
+++ include/nt2/sdk/simd/details/impl/sse/sse4_1/is_equal_to.hpp
@@ -11,24 +11,89 @@
 
 namespace nt2 { namespace functors
 {
-  template<class Dummy>
-  struct call<is_equal_,tag::simd_(tag::arithmetic_,tag::sse_),Dummy>
-  {
-    template<class Sig> struct result;
-    template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
-
-    NT2_FUNCTOR_CALL_DISPATCH( 2
-                             , typename nt2::meta::scalar_of<A0>::type
-                             , (6,(double,float,int8_,int16_,int32_,int64_))
-                             )
-
-    NT2_FUNCTOR_CALL_EVAL_IF(2,double)  { A0 that = { _mm_cmpeq_pd(a0,a1)    }; return that; }
-    NT2_FUNCTOR_CALL_EVAL_IF(2,float )  { A0 that = { _mm_cmpeq_ps(a0,a1)    }; return that; }
-    NT2_FUNCTOR_CALL_EVAL_IF(2,int8_ )  { A0 that = { _mm_cmpeq_epi8(a0,a1)  }; return that; }
-    NT2_FUNCTOR_CALL_EVAL_IF(2,int16_)  { A0 that = { _mm_cmpeq_epi16(a0,a1) }; return that; }
-    NT2_FUNCTOR_CALL_EVAL_IF(2,int32_)  { A0 that = { _mm_cmpeq_epi32(a0,a1) }; return that; }
-    NT2_FUNCTOR_CALL_EVAL_IF(2,int64_)  { A0 that = { _mm_cmpeq_epi64(a0,a1) }; return that; }
-  };
+			template<class Info>
+			struct  call<is_equal_,tag::simd_(tag::arithmetic_,tag::sse_),double,Info>
+			: callable
+			{
+				template<class Sig> struct result;
+				template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+				
+				NT2_FUNCTOR_CALL(2)
+				{
+					A0 that = { _mm_cmpeq_pd(a0,a1) };
+					return that;
+				}
+			};
+			
+			template<class Info>
+			struct  call<is_equal_,tag::simd_(tag::arithmetic_,tag::sse_),float,Info>
+			: callable
+			{
+				template<class Sig> struct result;
+				template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+				
+				NT2_FUNCTOR_CALL(2)
+				{
+					A0 that = { _mm_cmpeq_ps(a0,a1) };
+					return that;
+				}
+			};
+			
+			template<class Info>
+			struct  call<is_equal_,tag::simd_(tag::arithmetic_,tag::sse_),int8_,Info>
+			: callable
+			{
+				template<class Sig> struct result;
+				template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+				
+				NT2_FUNCTOR_CALL(2)
+				{
+					A0 that = { _mm_cmpeq_epi8(a0,a1)  };
+					return that;
+				}
+			};
+			
+			template<class Info>
+			struct  call<is_equal_,tag::simd_(tag::arithmetic_,tag::sse_),int16_,Info>
+			: callable
+			{
+				template<class Sig> struct result;
+				template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+				
+				NT2_FUNCTOR_CALL(2)
+				{
+					A0 that = { _mm_cmpeq_epi16(a0,a1) };
+					return that;
+				}
+			};
+			
+			template<class Info>
+			struct  call<is_equal_,tag::simd_(tag::arithmetic_,tag::sse_),int32_,Info>
+			: callable
+			{
+				template<class Sig> struct result;
+				template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+				
+				NT2_FUNCTOR_CALL(2)
+				{
+					A0 that = { _mm_cmpeq_epi32(a0,a1) };
+					return that;
+				}
+			};
+			
+			template<class Info>
+			struct  call<is_equal_,tag::simd_(tag::arithmetic_,tag::sse_),int64_,Info>
+			: callable
+			{
+				template<class Sig> struct result;
+				template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+				
+				NT2_FUNCTOR_CALL(2)
+				{
+					A0   that     = { _mm_cmpeq_epi64(a0,a1) };
+					return that;
+				}
+			};
 } }
 
 #endif
