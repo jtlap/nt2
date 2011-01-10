da93a59f933cb32ecede24a213897ac47467f8dc
Joel Falcou <joel.falcou@lri.fr>
2011-01-10 22:56:21 +0100

    Added SIMD multiplies code for SSEx

--- include/nt2/sdk/simd/details/impl/sse/sse2/multiplies.hpp
+++ include/nt2/sdk/simd/details/impl/sse/sse2/multiplies.hpp
@@ -13,42 +13,100 @@
 #include <nt2/sdk/meta/scalar_of.hpp>
 #include <nt2/sdk/simd/native_cast.hpp>
 
-namespace nt2 { namespace functors
+////////////////////////////////////////////////////////////////////////////////
+// operator binary plus
+////////////////////////////////////////////////////////////////////////////////
+#include <nt2/sdk/meta/strip.hpp>
+#include <nt2/sdk/functor/preprocessor/call.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+// Overload registration
+////////////////////////////////////////////////////////////////////////////////
+NT2_REGISTER_DISPATCH ( tag::multiplies_, tag::cpu_, (A0)
+                      , ((simd_<double_<A0>,tag::sse_>))
+                        ((simd_<double_<A0>,tag::sse_>))
+                      );
+
+NT2_REGISTER_DISPATCH ( tag::multiplies_, tag::cpu_, (A0)
+                      , ((simd_<float_<A0>,tag::sse_>))
+                        ((simd_<float_<A0>,tag::sse_>))
+                      );
+
+NT2_REGISTER_DISPATCH ( tag::multiplies_, tag::cpu_, (A0)
+                      , ((simd_<ints8_<A0>,tag::sse_>))
+                        ((simd_<ints8_<A0>,tag::sse_>))
+                      );
+
+NT2_REGISTER_DISPATCH ( tag::multiplies_, tag::cpu_, (A0)
+                      , ((simd_<ints16_<A0>,tag::sse_>))
+                        ((simd_<ints16_<A0>,tag::sse_>))
+                      );
+
+NT2_REGISTER_DISPATCH ( tag::multiplies_, tag::cpu_, (A0)
+                      , ((simd_<ints32_<A0>,tag::sse_>))
+                        ((simd_<ints32_<A0>,tag::sse_>))
+                      );
+
+NT2_REGISTER_DISPATCH ( tag::multiplies_, tag::cpu_, (A0)
+                      , ((simd_<ints64_<A0>,tag::sse_>))
+                        ((simd_<ints64_<A0>,tag::sse_>))
+                      );
+
+////////////////////////////////////////////////////////////////////////////////
+// Overloads implementation
+////////////////////////////////////////////////////////////////////////////////
+namespace nt2 { namespace ext
 {
-  template<class Info>
-  struct  call<multiplies_,tag::simd_(tag::arithmetic_,tag::sse_),double,Info>
+  template<class Dummy>
+  struct  call< tag::multiplies_( tag::simd_(tag::double_,tag::sse_)
+                                , tag::simd_(tag::double_,tag::sse_)
+                                )
+              , tag::cpu_, Dummy
+              >
         : callable
   {
-    template<class Sig> struct result;
-    template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
-
-    NT2_FUNCTOR_CALL(2) { A0 that = { _mm_mul_pd(a0,a1) }; return that; }
-  };
-
-  template<class Info>
-  struct  call<multiplies_,tag::simd_(tag::arithmetic_,tag::sse_),float,Info>
-        : callable
-  {
-    template<class Sig> struct result;
-    template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
-
-    NT2_FUNCTOR_CALL(2) { A0 that = { _mm_mul_ps(a0,a1) }; return that; }
-  };
-
-  template<class Info>
-  struct  call<multiplies_,tag::simd_(tag::arithmetic_,tag::sse_),int8_,Info>
-        : callable
-  {
-    template<class Sig> struct result;
-    template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+    template<class Sig>           struct result;
+    template<class This,class A>  struct result<This(A,A)> : meta::strip<A> {};
 
     NT2_FUNCTOR_CALL(2)
     {
-      typedef typename meta::make_integer < 2,signed
-                                          , simd::native< boost::mpl::_
-                                                        , tag::sse_
-                                                        >
-                                          >::type             type;
+      A0 that = { _mm_mul_pd(a0,a1) };
+      return that;
+    }
+  };
+
+  template<class Dummy>
+  struct  call< tag::multiplies_( tag::simd_(tag::float_,tag::sse_)
+                                , tag::simd_(tag::float_,tag::sse_)
+                                )
+              , tag::cpu_, Dummy
+              >
+        : callable
+  {
+    template<class Sig>           struct result;
+    template<class This,class A>  struct result<This(A,A)> : meta::strip<A> {};
+
+    NT2_FUNCTOR_CALL(2)
+    {
+      A0 that = { _mm_mul_ps(a0,a1) };
+      return that;
+    }
+  };
+
+  template<class Dummy>
+  struct  call< tag::multiplies_( tag::simd_(tag::ints8_,tag::sse_)
+                                , tag::simd_(tag::ints8_,tag::sse_)
+                                )
+              , tag::cpu_, Dummy
+              >
+        : callable
+  {
+    template<class Sig>           struct result;
+    template<class This,class A>  struct result<This(A,A)> : meta::strip<A> {};
+
+    NT2_FUNCTOR_CALL(2)
+    {
+      typedef typename meta::upgrade<A0,signed>::type  type;
 
       type mask1 = integral_constant<type,0x00FF>();
       type a0_16 = {a0};
@@ -58,27 +116,39 @@ namespace nt2 { namespace functors
       type abh   = {_mm_and_si128(mask1, _mm_mullo_epi16(a0,a1))};
       type ab    = {_mm_mullo_epi16(al,bl)};
       type abl   = {_mm_slli_epi16(_mm_and_si128(mask1, ab), 8)};
-      A0 r       = { abh | abl };
-      return r;
+      A0 that    = { abh | abl };
+      return that;
     }
   };
 
-  template<class Info>
-  struct  call<multiplies_,tag::simd_(tag::arithmetic_,tag::sse_),int16_,Info>
+  template<class Dummy>
+  struct  call< tag::multiplies_( tag::simd_(tag::ints16_,tag::sse_)
+                                , tag::simd_(tag::ints16_,tag::sse_)
+                                )
+              , tag::cpu_, Dummy
+              >
         : callable
   {
-    template<class Sig> struct result;
-    template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+    template<class Sig>           struct result;
+    template<class This,class A>  struct result<This(A,A)> : meta::strip<A> {};
 
-      NT2_FUNCTOR_CALL(2) { A0 that = { _mm_mullo_epi16(a0, a1)}; return that; }
+    NT2_FUNCTOR_CALL(2)
+    {
+      A0 that = { _mm_mullo_epi16(a0, a1) };
+      return that;
+    }
   };
 
-  template<class Info>
-  struct  call<multiplies_,tag::simd_(tag::arithmetic_,tag::sse_),int32_,Info>
+  template<class Dummy>
+  struct  call< tag::multiplies_( tag::simd_(tag::ints32_,tag::sse_)
+                                , tag::simd_(tag::ints32_,tag::sse_)
+                                )
+              , tag::cpu_, Dummy
+              >
         : callable
   {
-    template<class Sig> struct result;
-    template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+    template<class Sig>           struct result;
+    template<class This,class A>  struct result<This(A,A)> : meta::strip<A> {};
 
     NT2_FUNCTOR_CALL(2)
     {
@@ -101,18 +171,20 @@ namespace nt2 { namespace functors
     }
   };
 
-  template<class Info>
-  struct  call< multiplies_ , tag::simd_(tag::arithmetic_,tag::sse_)
-              , arithmetic_ , Info
+  template<class Dummy>
+  struct  call< tag::multiplies_( tag::simd_(tag::ints64_,tag::sse_)
+                                , tag::simd_(tag::ints64_,tag::sse_)
+                                )
+              , tag::cpu_, Dummy
               >
         : callable
   {
-    template<class Sig> struct result;
-    template<class This,class A> struct result<This(A,A)> : meta::strip<A> {};
+    template<class Sig>           struct result;
+    template<class This,class A>  struct result<This(A,A)> : meta::strip<A> {};
 
     NT2_FUNCTOR_CALL(2)
     {
-      A0 that = map(functor<multiplies_>(), a0, a1);
+      A0 that = { map(functor<multiplies_>(), a0, a1) };
       return that;
     }
   };
