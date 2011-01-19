f45124396a26ecb9f982a8d882959ac9d1bad28e
Joel Falcou <joel.falcou@lri.fr>
2011-01-19 17:33:36 +0100

    Altivec multiplies for real types

--- include/nt2/sdk/simd/details/impl/vmx/altivec/multiplies.hpp
+++ include/nt2/sdk/simd/details/impl/vmx/altivec/multiplies.hpp
@@ -12,26 +12,41 @@
 #include <nt2/sdk/meta/strip.hpp>
 #include <nt2/sdk/meta/scalar_of.hpp>
 
-namespace nt2 { namespace functors
+
+////////////////////////////////////////////////////////////////////////////////
+// Overload registration
+////////////////////////////////////////////////////////////////////////////////
+NT2_REGISTER_DISPATCH ( tag::multiplies_, tag::cpu_, (A0)
+                      , ((simd_<float_<A0>,tag::altivec_>))
+                        ((simd_<float_<A0>,tag::altivec_>))
+                      );
+
+////////////////////////////////////////////////////////////////////////////////
+// Overloads implementation
+////////////////////////////////////////////////////////////////////////////////
+namespace nt2 { namespace ext
 {
-  template<class Info>
-  struct call<multiplies_,tag::simd_(tag::arithmetic_,tag::altivec_),Info>
+  template<class Dummy>
+  struct  call< tag::multiplies_( tag::simd_(tag::float_,tag::altivec_)
+                                , tag::simd_(tag::float_,tag::altivec_)
+                                )
+              , tag::cpu_, Dummy
+              >
+        : callable
   {
-    template<class Sig> struct result;
-    template<class This,class A>
-    struct result<This(A,A)> : meta::strip<A> {};
+    template<class Sig>           struct result;
+    template<class This,class A0>
+    struct result<This(A0,A0)> : meta::strip<A0> {};
 
-    NT2_FUNCTOR_CALL_DISPATCH ( 2
-                              , typename nt2::meta::scalar_of<A0>::type
-                              , (5, ( float, int32_t, uint32_t, types16_, types8_ ) )
-                              )
-
-    NT2_FUNCTOR_CALL_EVAL_IF(2,float )
+    NT2_FUNCTOR_CALL(2)
     {
-      A0 that = { vec_madd(a0,a1,vec_xor(a0,a0)) };
+      A0 that = { vec_madd(a0(),a1(),Zero<A0>()) };
       return that;
     }
+  };
+} }
 
+/*
     NT2_FUNCTOR_CALL_EVAL_IF(2,types16_ )
     {
       A0 that = { vec_mladd(a0,a1,vec_xor(a0,a0)); };
@@ -48,33 +63,27 @@ namespace nt2 { namespace functors
 
     NT2_FUNCTOR_CALL_EVAL_IF(2,int32_t )
     {
-/*
       static inline type_t Multiply( const type_t& a, const type_t& b, const ttt::boxed<2>&, const true_t&  )
       {
         typedef __vu32_4 cast_t;
         type_t r  = type_t( av_multiply<__vu32_4>::Multiply(cast_t(a),cast_t(b), ttt::boxed<2>(), false_t()) );
         return vec_sel(r,vec_sub(vec_splat_u32(0), r),vec_cmpgt(vec_splat_u32(0), vec_xor(a, b)));
-      }  
-      */
+      }
     }
 
     NT2_FUNCTOR_CALL_EVAL_IF(2,uint32_t )
     {
 
-/*
       static inline type_t Multiply( const type_t& a, const type_t& b, const ttt::boxed<2>&, const false_t&  )
       {
         typedef __vu16_8 short_t;
         type_t sixteen = type_t(vec_vspltisw (-16));
-        type_t low = vec_mulo (short_t(a),short_t(b));										
+        type_t low = vec_mulo (short_t(a),short_t(b));
         type_t high = vec_msum (short_t(a),short_t(vec_rl(b, sixteen)),vec_splat_u32(0));
         return vec_add(vec_sl(high, sixteen),low);
-      }  
+      }
 
 */
-    }
-  };
-} }
 
 #endif
 
