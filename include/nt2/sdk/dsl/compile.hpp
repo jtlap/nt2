954e2908740284d2db20da4dc9cdd7817c2f35c7
Joel Falcou <joel.falcou@lri.fr>
2011-02-05 05:44:56 +0800

    Added meta::compile AST handler

---
+++ include/nt2/sdk/dsl/compile.hpp
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_DSL_COMPILE_HPP_INCLUDED
+#define NT2_SDK_DSL_COMPILE_HPP_INCLUDED
+
+#include <boost/proto/transform.hpp>
+#include <nt2/sdk/dsl/proto/unpack.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+// compile recursively traverse a nt2 AST to perform a given trasnformation
+// defined by externally extendable PFO
+////////////////////////////////////////////////////////////////////////////////
+namespace nt2 { namespace meta
+{
+  template<template<class,class> class Phase,class Target>
+  struct compile
+      : boost::proto::switch_< compile<Phase,Target> >
+  {
+    template <typename Tag, typename Dummy = void>
+    struct case_ : boost::proto::otherwise < Phase<Target,Tag> >
+    {};
+  };
+} }
+
+namespace boost { namespace proto
+{
+  template<template<class,class> class Phase,class Target>
+  struct  is_callable<nt2::meta::compile<Phase,Target> >
+        : boost::mpl::true_  {};
+} }
+
+#endif
