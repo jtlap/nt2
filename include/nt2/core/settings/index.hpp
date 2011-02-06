bcab5b812717ba6c337305df3f38bbe3a2565cd0
Joel Falcou <joel.falcou@lri.fr>
2011-02-06 23:34:59 +0800

    Small index_ options fix of name and comments.

--- include/nt2/core/settings/index.hpp
+++ include/nt2/core/settings/index.hpp
@@ -6,8 +6,8 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#ifndef NT2_CORE_SETTINGS_INDEX_INDEX_HPP_INCLUDED
-#define NT2_CORE_SETTINGS_INDEX_INDEX_HPP_INCLUDED
+#ifndef NT2_CORE_SETTINGS_INDEX_HPP_INCLUDED
+#define NT2_CORE_SETTINGS_INDEX_HPP_INCLUDED
 
 #include <cstddef>
 #include <boost/mpl/vector_c.hpp>
