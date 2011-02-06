0e3fb6dbe89e3d72d4cb456f0417d2f31ca2fc04
Joel Falcou <joel.falcou@lri.fr>
2011-02-06 18:13:52 +0100

    Added stub for value_at function and its scalar implementation

---
+++ include/nt2/core/functions/impl/value_at.hpp
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#ifndef NT2_CORE_FUNCTIONS_IMPL_SIZE_VALUE_AT_HPP_INCLUDED
+#define NT2_CORE_FUNCTIONS_IMPL_SIZE_VALUE_AT_HPP_INCLUDED
+
+#include <nt2/core/functions/impl/value_at/scalar.hpp>
+
+#endif
