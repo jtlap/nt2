13c84ea6e9a2d408b1252d02828ce0b7f1fadb2b
Joel Falcou <joel.falcou@lri.fr>
2011-02-09 21:35:27 +0100

    Fully qualified is_nan in ifloor to fix error under clang

--- include/nt2/toolbox/arithmetic/function/scalar/ifloor.hpp
+++ include/nt2/toolbox/arithmetic/function/scalar/ifloor.hpp
@@ -58,12 +58,12 @@ namespace nt2 { namespace ext
     NT2_FUNCTOR_CALL(1)
     {
       typedef typename NT2_RETURN_TYPE(1)::type rtype; 
-      if (is_inf(a0))
-	if (is_ltz(a0))
+      if (nt2::is_inf(a0))
+	if (nt2::is_ltz(a0))
 	  return Valmin<rtype>(); 
 	else
 	  return  Valmax<rtype>();
-      if (is_nan(a0)) return Zero<rtype>(); 
+      if (nt2::is_nan(a0)) return Zero<rtype>(); 
       return rtype(floor(a0));
     }
 
