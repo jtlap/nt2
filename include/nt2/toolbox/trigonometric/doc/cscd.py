2ec7333f98e44f4fb54e363e02cc1e6de4441492
Jt lapreste <jtlapreste@gmail.com>
2011-02-13 23:45:16 +0100

    py doc for trigo

--- include/nt2/toolbox/trigonometric/doc/cscd.py
+++ include/nt2/toolbox/trigonometric/doc/cscd.py
@@ -1,60 +1,69 @@
-[ ##script manually modified 
-{
-    'functor' : {       ## intrinsics values related to functor cscd
-        'arity'         : '1',    ## functor number of parameters
-        'ret_arity'     : '0',## must be '0' or omitted if not a tuple
-        'types'         : ['real_', 'unsigned_int_', 'signed_int_'],     ## list of types string
-        'rturn'         : { 'default' : 'typename boost::result_of<nt2::meta::floating(T)>::type' },     ## ditionary of return types
-        'call_types'    : [],## types used in call ['T']*arity if omitted 
-        'type_defs'     : [],  ## supplementary typedefs
-        }, ## end of functor
-    'unit'    : {       ## informations for creating unit tests
-        'global_header' : { ## header of machin.cpp
-            'first_stamp' : 'created  by $author$ the $date$', ## creation stamp
-            'stamp'       : 'modified by $author$ the $date$', ## modification stamp
-            'included'    : ['#include <nt2/toolbox/trigonometric/include/constants.hpp>',
-                             '#include <nt2/include/functions/csc.hpp>',
-                             '#include <nt2/include/functions/inrad.hpp>'],     ## list of supplementary include directives
-            'notes'       : [],        ## list of strings that will be included in a
-                                            ## global comment
+[ ## this file was manually modified by jt
+    {
+     'functor' : {
+         'arity' : '1',
+         'call_types' : [],
+         'ret_arity' : '0',
+         'rturn' : {
+             'default' : 'typename boost::result_of<nt2::meta::floating(T)>::type',
             },
-        'specific_values' : {
-                'real_'       : {
-                    'nt2::Inf<T>()' :  {'result' : 'nt2::Nan<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::Minf<T>()':  {'result' : 'nt2::Nan<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::Nan<T>()' :  {'result' : 'nt2::Nan<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::_180<T>()':  {'result' : 'nt2::Nan<r_t>()' ,'ulp_thresh':'0.5'},
-                    '-nt2::_180<T>()': {'result' : 'nt2::Nan<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::_90<T>()':   {'result' : 'nt2::One<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::_45<T>()':   {'result' : 'nt2::Sqrt_2<r_t>()' ,'ulp_thresh':'0.5'},
-                    '-nt2::_90<T>()':  {'result' : 'nt2::Mone<r_t>()' ,'ulp_thresh':'0.5'},
-                    '-nt2::_45<T>()':  {'result' : '-nt2::Sqrt_2<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::Zero<T>()':  {'result' : 'nt2::Inf<r_t>()','ulp_thresh':'0.5'},
-                    '-nt2::Zero<T>()':  {'result' : 'nt2::Minf<r_t>()','ulp_thresh':'0.5'},
+         'simd_types' : ['real_convert_'],
+         'special' : ['trigonometric'],
+         'type_defs' : [],
+         'types' : ['real_', 'unsigned_int_', 'signed_int_'],
+        },
+     'info' : 'manually modified',
+     'unit' : {
+         'global_header' : {
+             'first_stamp' : 'created  by jt the 11/02/2011',
+             'included' : 
+                ['#include <nt2/toolbox/trigonometric/include/constants.hpp>',
+                'extern "C" {extern long double cephes_sinl(long double);}'],
+             'notes' : [],
+             'stamp' : 'modified by jt the 11/02/2011',
+            },
+         'ranges' : {
+             'default' : [['T(-79)', 'T(79)']],
+            },
+         'specific_values' : {
+             'default' : {
+                 'nt2::Zero<T>()' : {'result' : 'nt2::Nan<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::_45<T>()' : {'result' : 'nt2::Sqrt_2<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::_90<T>()' : {'result' : 'nt2::One<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::_180<T>()' : {'result' : 'nt2::Nan<r_t>()','ulp_thresh' : '0.5',},
+                 },
+             'real_' : {
+                 '-nt2::Zero<T>()' : {'result' : 'nt2::Minf<r_t>()','ulp_thresh' : '0.5',},
+                 '-nt2::_180<T>()' : {'result' : 'nt2::Nan<r_t>()','ulp_thresh' : '0.5',},
+                 '-nt2::_45<T>()' : {'result' : '-nt2::Sqrt_2<r_t>()','ulp_thresh' : '0.5',},
+                 '-nt2::_90<T>()' : {'result' : 'nt2::Mone<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Inf<T>()' : {'result' : 'nt2::Nan<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Minf<T>()' : {'result' : 'nt2::Nan<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Nan<T>()' : {'result' : 'nt2::Nan<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Zero<T>()' : {'result' : 'nt2::Inf<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::_180<T>()' : {'result' : 'nt2::Nan<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::_45<T>()' : {'result' : 'nt2::Sqrt_2<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::_90<T>()' : {'result' : 'nt2::One<r_t>()','ulp_thresh' : '0.5',},
                 },
-                'signed_int_   ': {
-                    'nt2::Zero<T>()':  {'result' : 'nt2::Nan<r_t>()','ulp_thresh':'0.5'},
-                    'nt2::_180<T>()':  {'result' : 'nt2::Nan<r_t>()' ,'ulp_thresh':'0.5'},
-                    '-nt2::_180<T>()': {'result' : 'nt2::Nan<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::_90<T>()':   {'result' : 'nt2::One<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::_45<T>()':   {'result' : 'nt2::Sqrt_2<r_t>()' ,'ulp_thresh':'0.5'},
-                    '-nt2::_90<T>()':  {'result' : 'nt2::Mone<r_t>()' ,'ulp_thresh':'0.5'},
-                    '-nt2::_45<T>()':  {'result' : '-nt2::Sqrt_2<r_t>()' ,'ulp_thresh':'0.5'},
+             'signed_int_' : {
+                 '-nt2::_45<T>()' : {'result' : '-nt2::Sqrt_2<r_t>()','ulp_thresh' : '0.5',},
+                 '-nt2::_90<T>()' : {'result' : 'nt2::Mone<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Zero<T>()' : {'result' : 'nt2::Nan<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::_45<T>()' : {'result' : 'nt2::Sqrt_2<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::_90<T>()' : {'result' : 'nt2::One<r_t>()','ulp_thresh' : '0.5',},
                 },
-                'default': {
-                    'nt2::Zero<T>()': {'result' : 'nt2::Nan<r_t>()','ulp_thresh':'0.5'},
-                    'nt2::_90<T>()':  {'result' : 'nt2::One<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::_45<T>()':  {'result' : 'nt2::Sqrt_2<r_t>()' ,'ulp_thresh':'0.5'},
+            },
+         'verif_test' : {
+             'property_call' : {
+                 'real_' : ['nt2::cscd(a0)'],
                 },
-           },  ## dictionary of parameters and values to be tested
-        'ranges'          :  {
-             'default'   : [["T(-179)","T(179)"]],
-              },  ## dictionary of ranges for random tests
-        'verif_test'      : { ## verification dictionary
-            'property_call'   : {'real_' : ['nt2::cscd(a0)'],},  ## dictionary of calls per types
-            'property_value'  : {'real_' : ['nt2::csc(nt2::inrad(a0))'],}, ## dictionary of alternate calls
-            'ulp_thresh'      : {'real_' : ['1.0'],},     ## validity thresholds(s)
-        },  ## end of verif_test
-    },  ## end of unit 
-},
+             'property_value' : {
+                 'real_' : ['1.0l/(::cephes_sinl(nt2::long_deginrad*(a0)))'],
+                },
+             'ulp_thresh' : {
+                 'real_' : ['1.0'],
+                },
+            },
+        },
+    },
 ]
--- include/nt2/toolbox/trigonometric/doc/indeg.py
+++ include/nt2/toolbox/trigonometric/doc/indeg.py
@@ -1,49 +1,59 @@
-[ ##script modified by create_py_doc.py
-{
-    'functor' : {       ## intrinsics values related to functor indeg
-        'arity'         : '1',    ## functor number of parameters
-        'ret_arity'     : '0',## must be '0' or omitted if not a tuple
-        'types'         : ['real_', 'unsigned_int_', 'signed_int_'],     ## list of types string
-        'rturn'         : { 'default' : 'typename boost::result_of<nt2::meta::floating(T)>::type' },     ## ditionary of return types
-        'call_types'    : [],## types used in call ['T']*arity if omitted 
-        'type_defs'     : [],  ## supplementary typedefs
-        }, ## end of functor
-    'unit'    : {       ## informations for creating unit tests
-        'global_header' : { ## header of machin.cpp
-            'first_stamp' : 'created  by $author$ the $date$', ## creation stamp
-            'stamp'       : 'modified by $author$ the $date$', ## modification stamp
-            'included'    : ['#include <nt2/toolbox/trigonometric/include/constants.hpp>'],     ## list of supplementary include directives
-            'notes'       : [],        ## list of strings that will be included in a
-                                            ## global comment
+[ ## this file was manually modified by jt
+    {
+     'functor' : {
+         'arity' : '1',
+         'call_types' : [],
+         'ret_arity' : '0',
+         'rturn' : {
+             'default' : 'typename boost::result_of<nt2::meta::floating(T)>::type',
             },
-        'specific_values' : {
-                'real_'       : {
-                    'nt2::Inf<T>()' :  {'result' : 'nt2::Inf<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::Minf<T>()':  {'result' : 'nt2::Minf<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::Nan<T>()' :  {'result' : 'nt2::Nan<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::Pi<T>()'  :  {'result' : 'nt2::_180<r_t>()','ulp_thresh':'0.5'},
-                    'nt2::Pi<T>()/2':  {'result' : 'nt2::_90<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::Pi<T>()/4':  {'result' : 'nt2::_45<r_t>()' ,'ulp_thresh':'0.5'},
-                    '-nt2::Pi<T>()'  : {'result' : '-nt2::_180<r_t>()','ulp_thresh':'0.5'},
-                    '-nt2::Pi<T>()/2': {'result' : '-nt2::_90<r_t>()' ,'ulp_thresh':'0.5'},
-                    '-nt2::Pi<T>()/4': {'result' : '-nt2::_45<r_t>()' ,'ulp_thresh':'0.5'},
-                    'nt2::Zero<T>()':  {'result' : 'nt2::Zero<r_t>()','ulp_thresh':'0.5'},
+         'simd_types' : ['real_convert_'],
+         'special' : ['trigonometric'],
+         'type_defs' : [],
+         'types' : ['real_', 'unsigned_int_', 'signed_int_'],
+        },
+     'info' : 'manually modified',
+     'unit' : {
+         'global_header' : {
+             'first_stamp' : 'created  by jt the 11/02/2011',
+             'included' : ['#include <nt2/toolbox/trigonometric/include/constants.hpp>'],
+             'notes' : [],
+             'stamp' : 'modified by jt the 11/02/2011',
+            },
+         'ranges' : {
+             'real_' : [['-20*nt2::Pi<T>()', '20*nt2::Pi<T>()']],
+            },
+         'specific_values' : {
+             'default' : {
+                 'nt2::Zero<T>()' : {'result' : 'nt2::Zero<r_t>()','ulp_thresh' : '0.5',},
                 },
-                'signed_int_   ': {
-                    'nt2::Zero<T>()': {'result' : 'nt2::Zero<r_t>()','ulp_thresh':'0.5'},
+             'real_' : {
+                 '-nt2::Pi<T>()' : {'result' : '-nt2::_180<r_t>()','ulp_thresh' : '0.5',},
+                 '-nt2::Pi<T>()/2' : {'result' : '-nt2::_90<r_t>()','ulp_thresh' : '0.5',},
+                 '-nt2::Pi<T>()/4' : {'result' : '-nt2::_45<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Inf<T>()' : {'result' : 'nt2::Inf<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Minf<T>()' : {'result' : 'nt2::Minf<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Nan<T>()' : {'result' : 'nt2::Nan<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Pi<T>()' : {'result' : 'nt2::_180<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Pi<T>()/2' : {'result' : 'nt2::_90<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Pi<T>()/4' : {'result' : 'nt2::_45<r_t>()','ulp_thresh' : '0.5',},
+                 'nt2::Zero<T>()' : {'result' : 'nt2::Zero<r_t>()','ulp_thresh' : '0.5',},
                 },
-                'default': {
-                    'nt2::Zero<T>()': {'result' : 'nt2::Zero<r_t>()','ulp_thresh':'0.5'},
+             'signed_int_' : {
+                 'nt2::Zero<T>()' : {'result' : 'nt2::Zero<r_t>()','ulp_thresh' : '0.5',},
                 },
-           },  ## dictionary of parameters and values to be tested
-        'ranges'          :  {
-             'real_'         : [["-20*nt2::Pi<T>()","20*nt2::Pi<T>()"]],
-              },  ## dictionary of ranges for random tests
-        'verif_test'      : { ## verification dictionary
-            'property_call'   : {'real_' : ['nt2::indeg(a0)'],},  ## dictionary of calls per types
-            'property_value'  : {'real_' : ['a0*nt2::_180<T>()/nt2::Pi<T>()'],}, ## dictionary of alternate calls
-            'ulp_thresh'      : {'real_' : ['1.0'],},     ## validity thresholds(s)
-        },  ## end of verif_test
-    },  ## end of unit 
-},
+            },
+         'verif_test' : {
+             'property_call' : {
+                 'real_' : ['nt2::indeg(a0)'],
+                },
+             'property_value' : {
+                 'real_' : ['a0*nt2::_180<T>()/nt2::Pi<T>()'],
+                },
+             'ulp_thresh' : {
+                 'real_' : ['1.0'],
+                },
+            },
+        },
+    },
 ]
