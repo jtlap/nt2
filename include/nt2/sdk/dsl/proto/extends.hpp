936ae801cf3dff63671ba38d553be86dfe2d3346
Joel Falcou <joel.falcou@lri.fr>
2011-02-15 04:01:25 +0800

    Made BOOST_PROTO_BASIC_EXTENDS_TPL uses NT2_STRIP to use templaet domain properly

--- include/nt2/sdk/dsl/proto/extends.hpp
+++ include/nt2/sdk/dsl/proto/extends.hpp
@@ -9,13 +9,17 @@
 #ifndef NT2_SDK_DSL_PROTO_EXTENDS_HPP
 #define NT2_SDK_DSL_PROTO_EXTENDS_HPP
 
+#include <nt2/sdk/details/preprocessor.hpp>
 ////////////////////////////////////////////////////////////////////////////////
 // BOOST_PROTO_BASIC_EXTENDS working with template Domain
 ////////////////////////////////////////////////////////////////////////////////
-#define BOOST_PROTO_BASIC_EXTENDS_TPL(Expr, Derived, Domain)    \
-BOOST_PROTO_BASIC_EXTENDS_(Expr, Derived, Domain)               \
-typedef void                              proto_is_aggregate_;  \
-typedef typename Domain::proto_generator  proto_generator;      \
+#define BOOST_PROTO_BASIC_EXTENDS_TPL(Expr, Derived, Domain)                  \
+BOOST_PROTO_BASIC_EXTENDS_( NT2_PP_STRIP(Expr)                                \
+                          , NT2_PP_STRIP(Derived)                             \
+                          , NT2_PP_STRIP(Domain)                              \
+                          )                                                   \
+typedef void                                            proto_is_aggregate_;  \
+typedef typename NT2_PP_STRIP(Domain)::proto_generator  proto_generator;      \
 /**/
 
 #endif
