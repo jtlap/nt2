9567b2ad57e8f5ca7705ad3466d83abf374f8b5c
Joel Falcou <joel.falcou@lri.fr>
2011-02-16 06:16:20 +0800

    Added primitive/factory specialization for simd::native

---
+++ include/nt2/sdk/simd/details/native/meta/primitive_of.hpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_SIMD_DETAILS_NATIVE_META_PRIMITIVE_OF_HPP_INCLUDED
+#define NT2_SDK_SIMD_DETAILS_NATIVE_META_PRIMITIVE_OF_HPP_INCLUDED
+
+#include <boost/mpl/placeholders.hpp>
+#include <nt2/sdk/meta/primitive_of.hpp>
+
+namespace nt2 { namespace meta
+{
+  template<class Type,class Extension>
+  struct primitive_of< simd::native<Type,Extension> >
+  {
+    typedef Type type;
+  };
+} }
+
+#endif
