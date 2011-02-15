bdde822c467390a1f11164a5e7d6871b1dd63701
Joel Falcou <joel.falcou@lri.fr>
2011-02-16 06:20:03 +0800

    Removed metafunction specialization for simd::native on as_real and as_integer. .

--- include/nt2/sdk/simd/details/native/meta.hpp
+++ include/nt2/sdk/simd/details/native/meta.hpp
@@ -9,15 +9,16 @@
 #ifndef NT2_SDK_SIMD_DETAILS_NATIVE_META_HPP_INCLUDED
 #define NT2_SDK_SIMD_DETAILS_NATIVE_META_HPP_INCLUDED
 
+#include <nt2/sdk/simd/details/native/meta/factory_of.hpp>
+#include <nt2/sdk/simd/details/native/meta/primitive_of.hpp>
+
 #include <nt2/sdk/simd/details/native/meta/as_signed.hpp>
 #include <nt2/sdk/simd/details/native/meta/as_unsigned.hpp>
-#include <nt2/sdk/simd/details/native/meta/as_real.hpp>
 #include <nt2/sdk/simd/details/native/meta/sign_of.hpp>
 #include <nt2/sdk/simd/details/native/meta/scalar_of.hpp>
 #include <nt2/sdk/simd/details/native/meta/behave_as.hpp>
 #include <nt2/sdk/simd/details/native/meta/is_signed.hpp>
 #include <nt2/sdk/simd/details/native/meta/is_scalar.hpp>
-#include <nt2/sdk/simd/details/native/meta/as_integer.hpp>
 #include <nt2/sdk/simd/details/native/meta/cardinal_of.hpp>
 #include <nt2/sdk/simd/details/native/meta/downgrade.hpp>
 #include <nt2/sdk/simd/details/native/meta/upgrade.hpp>
