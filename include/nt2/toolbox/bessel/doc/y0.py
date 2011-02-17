d90479d54415fae52b21994130d2e39f8ba960db
Jt lapreste <jtlapreste@gmail.com>
2011-02-17 13:15:30 +0100

    bessel py and scalar units 2

---
+++ include/nt2/toolbox/bessel/doc/y0.py
@@ -0,0 +1,46 @@
+[ ## this file was manually modified by jt
+    {
+     'functor' : {
+         'arity' : '1',
+         'call_types' : [],
+         'ret_arity' : '0',
+         'rturn' : {
+             'default' : 'typename boost::result_of<nt2::meta::floating(T)>::type',
+            },
+         'type_defs' : [],
+         'types' : ['real_'],
+        },
+     'info' : 'manually modified',
+     'unit' : {
+         'global_header' : {
+             'first_stamp' : 'created  by jt the 16/02/2011',
+             'included' : ['extern "C" {long double cephes_y0l(long double);}'],
+             'notes' : [],
+             'stamp' : 'modified by jt the 16/02/2011',
+            },
+         'ranges' : {
+             'real_' : [['T(0)', 'T(10)']],
+            },
+         'specific_values' : {
+             'real_' : {
+                 'nt2::One<T>()' : {'result' : 'r_t( 8.825696421567700e-02)','ulp_thresh' : '1.5',},
+                 'nt2::Zero<T>()' : {'result' : 'nt2::Minf<r_t>()','ulp_thresh' : '0',},
+                },
+            },
+         'verif_test' : {
+             'nb_rand' : {
+                 'default' : 'NT2_NB_RANDOM_TEST',
+                },
+             'property_call' : {
+                 'default' : ['nt2::y0(a0)'],
+                },
+             'property_value' : {
+                 'default' : ['cephes_y0l(a0)'],
+                },
+             'ulp_thresh' : {
+                 'default' : ['100'],
+                },
+            },
+        },
+    },
+]
--- include/nt2/toolbox/bessel/unit/scalar/j1.cpp
+++ include/nt2/toolbox/bessel/unit/scalar/j1.cpp
@@ -6,13 +6,58 @@
 ///                 See accompanying file LICENSE.txt or copy at
 ///                     http://www.boost.org/LICENSE_1_0.txt
 //////////////////////////////////////////////////////////////////////////////
-#define NT2_UNIT_MODULE "nt2 bessel toolbox - unit/scalar Mode"
+#define NT2_UNIT_MODULE "nt2 bessel toolbox - j1/scalar Mode"
 
-#include <nt2/toolbox/bessel/include/j1.hpp>
+//////////////////////////////////////////////////////////////////////////////
+// Test behavior of bessel components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+/// created  by jt the 16/02/2011
+/// modified by jt the 17/02/2011
+#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
+#include <nt2/sdk/constant/real.hpp>
+#include <nt2/sdk/constant/infinites.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/toolbox/bessel/include/j1.hpp>
+// specific includes for arity 1 tests
+extern "C" {long double cephes_j1l(long double);}
 
-//////////////////////////////////////////////////////////////////////////////
-// Test behavior of bessel components using NT2_TEST_CASE
-//////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE_TPL ( j1_real__1,  NT2_REAL_TYPES)
+{
+  using nt2::j1;
+  using nt2::tag::j1_;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef typename nt2::meta::call<j1_(T)>::type r_t;
+  typedef typename nt2::meta::upgrade<T>::type u_t;
+  typedef typename boost::result_of<nt2::meta::floating(T)>::type wished_r_t;
 
+
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+
+
+  // specific values tests
+  NT2_TEST_ULP_EQUAL(  j1(nt2::Inf<T>()), nt2::Zero<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(  j1(nt2::One<T>()), r_t(4.400505857449336e-01), 1.5);
+  NT2_TEST_ULP_EQUAL(  j1(nt2::Zero<T>()), nt2::Zero<r_t>(), 0.5);
+  // random verifications
+  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  {
+    NT2_CREATE_SCALAR_BUFFER(a0,T, NR, T(0), T(10));
+    double ulp0 = 0.0, ulpd = 0.0;
+    for (int j =0; j < NR; ++j )
+      {
+        std::cout << "for param "
+                  << "  a0 = "<< u_t(a0 = tab_a0[j])
+                  << std::endl;
+        NT2_TEST_ULP_EQUAL( nt2::j1(a0),cephes_j1l(a0),200);
+        ulp0=nt2::max(ulpd,ulp0);
+     }
+     std::cout << "max ulp found is: " << ulp0 << std::endl;
+   }
+} // end of test for real_
