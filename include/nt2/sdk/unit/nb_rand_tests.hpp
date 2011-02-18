fcf3ed3d99551e31abb34a7185a7cf39de6e3642
Jt lapreste <jtlapreste@gmail.com>
2011-02-18 13:14:14 +0100

    test modifs

---
+++ include/nt2/sdk/unit/nb_rand_tests.hpp
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ *         Copyright 2003-2011 LASMEA UMR 6602 CNRS/U.B.P
+ *         Copyright 2009-2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_UNIT_NB_RAND_TESTS_HPP_INCLUDED
+#define NT2_SDK_UNIT_NB_RAND_TESTS_HPP_INCLUDED
+
+// this is the number of tests in each loop in the unit tests
+// If you change that you had better choose a multiple of the
+// maximum number of items in an simd vector (currently 16) 
+
+#define NT2_NB_RANDOM_TEST 128
+
+#endif
+
+// /////////////////////////////////////////////////////////////////////////////
+// End of nb_rand_tests.hpp
+// /////////////////////////////////////////////////////////////////////////////
