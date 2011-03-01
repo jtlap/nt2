0743cadce4f4ea3e4750c6c38530eab2bd42506c
Jt lapreste <jtlapreste@gmail.com>
2011-03-01 13:16:08 +0100

    more constants for trigo

--- include/nt2/toolbox/trigonometric/function/scalar/impl/constants.hpp
+++ include/nt2/toolbox/trigonometric/function/scalar/impl/constants.hpp
@@ -22,6 +22,9 @@ namespace nt2 { namespace constants
   struct pi_x_2_   {};
   struct sqrt_2pi_ {};
   struct _4_o_pi_  {};
+  struct _2_o_pi_  {};
+  struct sqrt_2_o_pi {};
+  struct three_pi_o_4 {};
   struct inv_pi_   {};
   struct pi_o_180_ {};
   struct one_o_180_{};
@@ -45,6 +48,9 @@ namespace nt2
   NT2_CONSTANT_IMPLEMENTATION(constants::rad_in_deg_   , Radindeg )
   NT2_CONSTANT_IMPLEMENTATION(constants::rad_in_deg_r_ , Radindegr)
   NT2_CONSTANT_IMPLEMENTATION(constants::sqrt_2pi_     , Sqrt_2pi)
+  NT2_CONSTANT_IMPLEMENTATION(constants::_2_o_pi_      , Twoopi) 
+  NT2_CONSTANT_IMPLEMENTATION(constants::sqrt_2_o_pi   , Sqrt_2opi)
+  NT2_CONSTANT_IMPLEMENTATION(constants::three_pi_o_4  , Threepio_4)
 }
 
 NT2_CONSTANT_MAKE_REAL(constants::pi_o_2_      , 0x3ff921fb54442d18LL, 0x3fc90fdb)
@@ -59,7 +65,11 @@ NT2_CONSTANT_MAKE_REAL(constants::deg_in_rad_  , 0x3f91df46a2529d39LL, 0x3c8efa3
 NT2_CONSTANT_MAKE_REAL(constants::rad_in_deg_  , 0x404ca5dc1a63c1f8LL, 0x42652ee1)
 NT2_CONSTANT_MAKE_REAL(constants::rad_in_deg_r_, 0xbce1e7ab456405f8ll, 0xb53387c1)
 NT2_CONSTANT_MAKE_REAL(constants::sqrt_2pi_    , 0x40040d931ff62704ll, 0x40206c99)
+NT2_CONSTANT_MAKE_REAL(constants::_2_o_pi_     , 0x3fe45f306dc9c883ll, 0x3f22f983) 
+NT2_CONSTANT_MAKE_REAL(constants::sqrt_2_o_pi  , 0x3fe9884533d43651ll, 0x3f4c422a)
+NT2_CONSTANT_MAKE_REAL(constants::three_pi_o_4 , 0x4002d97c7f3321d2ll, 0x4016cbe4)
 
+  
   namespace nt2 {
     static const long double long_pi = 3.141592653589793238462643383279502884197l;
     static const long double long_deginrad = 0.017453292519943295769236907684886l; 
