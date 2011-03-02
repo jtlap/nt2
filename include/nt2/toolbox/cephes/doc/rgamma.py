9353571ec1484a6ffe9fb1fba63671d0b978b635
Jt lapreste <jtlapreste@gmail.com>
2011-03-02 09:47:24 +0100

    py doc for cephes

---
+++ include/nt2/toolbox/cephes/doc/rgamma.py
@@ -0,0 +1,43 @@
+[ ## this file was manually modified by jt
+    {
+     'functor' : {
+         'arity' : '1',
+         'call_types' : [],
+         'ret_arity' : '0',
+         'rturn' : {
+             'default' : 'T',
+            },
+         'simd_types' : [],
+         'special' : ['cephes'],
+         'type_defs' : [],
+         'types' : ['real_'],
+        },
+     'info' : 'manually modified',
+     'unit' : {
+         'global_header' : {
+             'first_stamp' : 'created  by jt the 01/03/2011',
+             'included' : [],
+             'notes' : [],
+             'stamp' : 'modified by jt the 01/03/2011',
+            },
+         'ranges' : {
+             'default' : [['T(-100)', 'T(100)']],
+            },
+         'specific_values' : {
+            },
+         'verif_test' : {
+             'property_call' : {
+                 'default' : ['nt2::cephes::rgamma(a0)'],
+                },
+             'property_value' : {
+                 'default' : ['nt2::cephes::rgamma(a0)'],
+                },
+             'simd' : {
+                },
+             'ulp_thresh' : {
+                 'default' : ['0.5'],
+                },
+            },
+        },
+    },
+]
