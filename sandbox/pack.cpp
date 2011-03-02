2c3e3f648ed64c412c3a17ec9f20e55e63c71b5d
Joel Falcou <joel.falcou@lri.fr>
2011-03-02 14:29:21 +0800

    Updated pack prototype

--- sandbox/pack.cpp
+++ sandbox/pack.cpp
@@ -2,72 +2,47 @@
 //#define BOOST_PROTO_STRICT_RESULT_OF
 
 #include <nt2/sdk/details/type_id.hpp>
-#include <nt2/sdk/meta/any.hpp>
 #include <nt2/sdk/simd/pack.hpp>
-#include <boost/proto/debug.hpp>
-
-#include <nt2/include/functions/cos.hpp>
-#include <nt2/include/functions/abs.hpp>
-#include <nt2/include/functions/exponentbits.hpp>
+#include <nt2/sdk/dsl/compute.hpp>
 
 using namespace std;
 
+NT2_REGISTER_DISPATCH(tag::terminal_,tag::cpu_
+                     ,(A0)(A1)(A2)(X)
+                     ,((simd_<arithmetic_<A0>,X>))(int32_<A1>)(int32_<A2>)
+                     )
+
 namespace nt2 { namespace ext
 {
-  template<class Dummy >
-  struct  call< tag::exponentbits_(tag::ast_), tag::cpu_, Dummy>
-        : callable
+  template<class Dummy,class X>
+  struct call<tag::terminal_(tag::simd_<tag::arithmetic_,X>,tag::int32_,tag::int32_),tag::cpu_,Dummy>
+  : callable
   {
     template<class Sig> struct result;
 
-    template<class This,class A0>
-    struct result<This(A0)>
+    template<class This, class Value, class State, class Data>
+    struct result<This(Value,State,Data)>
     {
-      typedef typename boost::proto::domain_of<A0>::type domain_type;
-      typedef typename boost::proto::result_of::
-      make_expr < tag::exponentbits_
-                , simd::domain< typename meta::
-                                as_integer<typename domain_type::simd_type, signed>::type
-                              , typename domain_type::cardinal_type
-                              >
-                , typename meta::strip<A0>::type const&
-                >::type type;
+      typedef typename meta::strip<Value>::type::parent type;
     };
 
-    NT2_FUNCTOR_CALL(1)
+    template<class Value, class State, class Data> inline
+    typename result<call(Value,State,Data)>::type
+    operator()( Value& v, State& , Data&  ) const
     {
-      typedef typename boost::proto::domain_of<A0>::type base_type;
-      typedef simd::domain< typename meta::
-                            as_integer<typename base_type::simd_type, signed>::type
-                          , typename base_type::cardinal_type
-                          > domain_type;
-
-      return  boost::proto::
-              make_expr<tag::exponentbits_,domain_type>(boost::cref(a0));
+      return v.value();
     }
   };
 } }
 
-template<class Xpr> void
-foo(Xpr const& x)
-{
-  std::cout << nt2::type_id(x) << "\n";
-}
 
 int main()
 {
-  nt2::simd::pack<nt2::int32_t> x(5.321f);
   nt2::simd::pack<float> y(5.321);
 
-  foo(x);
-  foo(nt2::exponentbits(1.f));
-  foo(nt2::exponentbits(y));
-  foo(nt2::cos(y) + nt2::cos(y)); // ok
-  foo(nt2::plus(x , x) ); //  ok
-  foo(nt2::plus(nt2::exponentbits(y) , 3) ); //  ok
-  foo(nt2::plus(nt2::exponentbits(y) , nt2::exponentbits(y)) ); //  ok
-  foo(nt2::plus(nt2::exponentbits(1.f) , x) ); //  ok
-  foo(nt2::plus(nt2::exponentbits(1.f) , 3) ); //  ok
-  foo(nt2::where(y,y,y) ); //  ok
-  foo(nt2::where(1,2,y) ); //  ok
+  nt2::meta::compile< nt2::meta::compute, nt2::tag::cpu_ > compiler;
+
+  std::cout << compiler(y) << "\n";
+
+  std::cout << compiler(y+y) << "\n";
 }
