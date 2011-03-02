38dc08030b0ca858760465d7535310ae64881c15
Joel Falcou <joel.falcou@lri.fr>
2011-03-03 05:26:47 +0800

    Updated pack test

--- sandbox/pack.cpp
+++ sandbox/pack.cpp
@@ -1,48 +1,20 @@
 #include <iostream>
 //#define BOOST_PROTO_STRICT_RESULT_OF
 
-#include <nt2/sdk/details/type_id.hpp>
 #include <nt2/sdk/simd/pack.hpp>
-#include <nt2/sdk/dsl/compute.hpp>
+#include <nt2/include/functions/cos.hpp>
+#include <nt2/include/functions/sin.hpp>
 
 using namespace std;
 
-NT2_REGISTER_DISPATCH(tag::terminal_,tag::cpu_
-                     ,(A0)(A1)(A2)(X)
-                     ,((simd_<arithmetic_<A0>,X>))(int32_<A1>)(int32_<A2>)
-                     )
-
-namespace nt2 { namespace ext
-{
-  template<class Dummy,class X>
-  struct call<tag::terminal_(tag::simd_<tag::arithmetic_,X>,tag::int32_,tag::int32_),tag::cpu_,Dummy>
-  : callable
-  {
-    template<class Sig> struct result;
-
-    template<class This, class Value, class State, class Data>
-    struct result<This(Value,State,Data)>
-    {
-      typedef typename meta::strip<Value>::type::parent type;
-    };
-
-    template<class Value, class State, class Data> inline
-    typename result<call(Value,State,Data)>::type
-    operator()( Value& v, State& , Data&  ) const
-    {
-      return v.value();
-    }
-  };
-} }
-
-
 int main()
 {
-  nt2::simd::pack<float> y(5.321);
-
-  nt2::meta::compile< nt2::meta::compute, nt2::tag::cpu_ > compiler;
-
-  std::cout << compiler(y) << "\n";
-
-  std::cout << compiler(y+y) << "\n";
+  nt2::simd::pack<float> x,y(5.321);
+  std::cout << y << "\n";
+  x = y+y;
+  std::cout << x << "\n";
+  x = y*1000;
+  std::cout << x << "\n";
+  x = nt2::cos(y)*nt2::cos(y) + nt2::sin(y)*nt2::sin(y);
+  std::cout << x << "\n";
 }
