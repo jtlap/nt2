f066c5cc9bfab1c130004b3070e0de84613d016b
Joel Falcou <joel.falcou@lri.fr>
2011-03-03 21:15:59 +0800

    Provided implementation for Altivec ==

---
+++ include/nt2/sdk/simd/details/impl/vmx/altivec/compare_equal.hpp
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_SIMD_DETAILS_IMPL_VMX_ALTIVEC_COMPARE_EQUAL_HPP_INCLUDED
+#define NT2_SDK_SIMD_DETAILS_IMPL_VMX_ALTIVEC_COMPARE_EQUAL_HPP_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+// Overload registration
+////////////////////////////////////////////////////////////////////////////////
+NT2_REGISTER_DISPATCH ( tag::compare_equal_, tag::cpu_, (A0)
+                      , ((simd_<arithmetic_<A0>,tag::altivec_>))
+                        ((simd_<arithmetic_<A0>,tag::altivec_>))
+                      );
+
+////////////////////////////////////////////////////////////////////////////////
+// Overloads implementation
+////////////////////////////////////////////////////////////////////////////////
+namespace nt2 { namespace ext
+{
+  template<class Dummy>
+  struct  call< tag::compare_equal_ ( tag::simd_<tag::arithmetic_,tag::altivec_>
+                                    , tag::simd_<tag::arithmetic_,tag::altivec_>
+                                    )
+              , tag::cpu_, Dummy
+              >
+        : callable
+  {
+    typedef bool result_type;
+    NT2_FUNCTOR_CALL(2) { return vec_all_eq(a0(),a1()); }
+  };
+} }
+
+#endif
