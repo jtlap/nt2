b2c7c2cf419b8bcf6261cdbfc387330ea3a1dd1a
Jt lapreste <jtlapreste@gmail.com>
2011-03-05 21:45:20 +0100

    py doc for libc

---
+++ include/nt2/toolbox/libc/doc/asin.py
@@ -0,0 +1,42 @@
+[ ## this file was manually modified by jt
+    {
+     'functor' : {
+         'arity' : '1',
+         'call_types' : [],
+         'ret_arity' : '0',
+         'rturn' : {
+             'default' : 'T',
+            },
+         'simd_types' : [],
+         'special' : ['libc'],
+         'type_defs' : [],
+         'types' : ['real_'],
+        },
+     'info' : 'manually modified',
+     'unit' : {
+         'global_header' : {
+             'first_stamp' : 'created  by jt the 05/03/2011',
+             'included' : ['#include <nt2/include/functions/asin.hpp>'],
+             'stamp' : 'modified by jt the 05/03/2011',
+            },
+         'ranges' : {
+             'default' : [['T(-100)', 'T(100)']],
+            },
+         'specific_values' : {
+            },
+         'verif_test' : {
+             'property_call' : {
+                 'default' : ['nt2::libc::asin(a0)'],
+                },
+             'property_value' : {
+                 'default' : ['nt2::asin(a0)'],
+                },
+             'simd' : {
+                },
+             'ulp_thresh' : {
+                 'default' : ['1'],
+                },
+            },
+        },
+    },
+]
