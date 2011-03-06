970c1731e0f90209c0c61aa0c769e3f88f4eb7fb
Jt lapreste <jtlapreste@gmail.com>
2011-03-06 18:19:59 +0100

    scalar units for polynomials

--- include/nt2/toolbox/boost_math/function/scalar/legendre_p.hpp
+++ include/nt2/toolbox/boost_math/function/scalar/legendre_p.hpp
@@ -15,13 +15,13 @@
 /////////////////////////////////////////////////////////////////////////////
 NT2_REGISTER_DISPATCH(boost_math::tag::legendre_p_, tag::cpu_,
                             (A0)(A1),
-                            (arithmetic_<A0>)(arithmetic_<A1>)
+                            (integer_<A0>)(arithmetic_<A1>)
                            )
 
 namespace nt2 { namespace ext
 {
   template<class Dummy>
-  struct call<boost_math::tag::legendre_p_(tag::arithmetic_,tag::arithmetic_),
+  struct call<boost_math::tag::legendre_p_(tag::integer_,tag::arithmetic_),
               tag::cpu_, Dummy> : callable
   {
     template<class Sig> struct result;
@@ -42,13 +42,13 @@ namespace nt2 { namespace ext
 /////////////////////////////////////////////////////////////////////////////
 NT2_REGISTER_DISPATCH(boost_math::tag::legendre_p_, tag::cpu_,
                             (A0)(A1),
-                            (real_<A0>)(real_<A1>)
+                            (integer_<A0>)(real_<A1>)
                            )
 
 namespace nt2 { namespace ext
 {
   template<class Dummy>
-  struct call<boost_math::tag::legendre_p_(tag::real_,tag::real_),
+  struct call<boost_math::tag::legendre_p_(tag::integer_,tag::real_),
               tag::cpu_, Dummy> : callable
   {
     template<class Sig> struct result;
