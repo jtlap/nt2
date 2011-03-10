385b16a097f9dcc2fe7b0d2bbb2b567114cc4cbd
Jt lapreste <jtlapreste@gmail.com>
2011-03-10 16:59:34 +0100

    gsl unit

--- include/nt2/toolbox/gsl_specfun/unit/scalar/gsl_sf_psi_1piy.cpp
+++ include/nt2/toolbox/gsl_specfun/unit/scalar/gsl_sf_psi_1piy.cpp
@@ -6,29 +6,53 @@
 ///                 See accompanying file LICENSE.txt or copy at
 ///                     http://www.boost.org/LICENSE_1_0.txt
 //////////////////////////////////////////////////////////////////////////////
-#define NT2_UNIT_MODULE "nt2 gsl_specfun toolbox - gsl_sf_psi_1piy - unit/scalar Mode"
+#define NT2_UNIT_MODULE "nt2 gsl_specfun toolbox - gsl_sf_psi_1piy/scalar Mode"
 
-#include <nt2/toolbox/gsl_specfun/include/gsl_sf_psi_1piy.hpp>
+//////////////////////////////////////////////////////////////////////////////
+// Test behavior of gsl_specfun components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+/// created  by jt the 10/03/2011
+/// modified by jt the 10/03/2011
+#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
+#include <nt2/sdk/constant/real.hpp>
+#include <nt2/sdk/constant/infinites.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/toolbox/gsl_specfun/include/gsl_sf_psi_1piy.hpp>
 
-//////////////////////////////////////////////////////////////////////////////
-// Test behavior of gsl_specfun component gsl_sf_psi_1piy using NT2_TEST_CASE
-//////////////////////////////////////////////////////////////////////////////
-//NT2_TEST_CASE_TPL ( gsl_specfun,  (double)(nt2::uint64_t)(nt2::int64_t) 
-//                          (float)(nt2::uint32_t)(nt2::int32_t)  
-//                          (nt2::uint16_t)(nt2::int16_t)         
-//                          (nt2::uint8_t)(nt2::int8_t)
-//                          (bool)
-//                  )
-//{
-//  using nt2::gsl_sf_psi_1piy;
-//  using nt2::functors::gsl_sf_psi_1piy_;
-//
-//  NT2_TEST( (boost::is_same<typename nt2::meta::call<gsl_specfun_(T)>::type,
-//                            typename std::tr1::result_of<nt2::meta::floating<($self.const_T_type_list$)>::type
-//                            >::value)
-//          );
-//}
+NT2_TEST_CASE_TPL ( gsl_sf_psi_1piy_real__1,  NT2_REAL_TYPES)
+{
+  
+  using nt2::gsl_specfun::gsl_sf_psi_1piy;
+  using nt2::gsl_specfun::tag::gsl_sf_psi_1piy_;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef typename nt2::meta::call<gsl_sf_psi_1piy_(T)>::type r_t;
+  typedef typename nt2::meta::upgrade<T>::type u_t;
+  typedef T wished_r_t;
+
+
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+
+  // random verifications
+  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  {
+    NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
+    double ulp0 = 0.0, ulpd = 0.0;
+    T a0;
+    for (int j =0; j < NR; ++j )
+      {
+        std::cout << "for param "
+                  << "  a0 = "<< u_t(a0 = tab_a0[j])
+                  << std::endl;
+        NT2_TEST_ULP_EQUAL( nt2::gsl_specfun::gsl_sf_psi_1piy(a0),nt2::gsl_specfun::gsl_sf_psi_1piy(a0),1);
+        ulp0=nt2::max(ulpd,ulp0);
+     }
+     std::cout << "max ulp found is: " << ulp0 << std::endl;
+   }
+} // end of test for real_
