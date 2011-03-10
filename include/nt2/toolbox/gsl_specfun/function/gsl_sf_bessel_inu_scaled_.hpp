3ae90dc731c96ac49826d07c1a5dfd78c4b2fd36
Jt lapreste <jtlapreste@gmail.com>
2011-03-10 17:01:06 +0100

    gsl functions

--- include/nt2/toolbox/gsl_specfun/function/gsl_sf_bessel_inu_scaled_.hpp
+++ include/nt2/toolbox/gsl_specfun/function/gsl_sf_bessel_inu_scaled_.hpp
@@ -14,9 +14,9 @@
 
 namespace nt2 { namespace gsl_specfun { namespace tag
   {         
-    struct gsl_sf_bessel_inu_scaled__ {};
+    struct gsl_sf_bessel_inu_scaled_ {};
   }
-  NT2_FUNCTION_IMPLEMENTATION(gsl_specfun::tag::gsl_sf_bessel_inu_scaled__, gsl_sf_bessel_inu_scaled_, 2)
+  NT2_FUNCTION_IMPLEMENTATION(gsl_specfun::tag::gsl_sf_bessel_inu_scaled_, gsl_sf_bessel_inu_scaled, 2)
   } }
  
 #include <nt2/toolbox/gsl_specfun/function/scalar/gsl_sf_bessel_inu_scaled_.hpp>
