75897269bca28603cae79357f205b3aae71bcf90
Jt lapreste <jtlapreste@gmail.com>
2011-03-10 17:00:03 +0100

    gsl py doc

---
+++ include/nt2/toolbox/gsl_specfun/doc/gsl_sf_psi.py
@@ -0,0 +1,44 @@
+[ ## this file was manually modified by jt
+    {
+     'functor' : {
+         'arity' : '1',
+         'call_types' : [],
+         'ret_arity' : '0',
+         'rturn' : {
+             'default' : 'T',
+            },
+         'simd_types' : [],
+         'special' : ['gsl_specfun'],
+         'type_defs' : [],
+         'types' : ['real_'],
+        },
+     'info' : 'manually modified',
+     'unit' : {
+         'global_header' : {
+             'first_stamp' : 'created  by jt the 10/03/2011',
+             'included' : [],
+             'no_ulp' : 'False',
+             'notes' : [],
+             'stamp' : 'modified by jt the 10/03/2011',
+            },
+         'ranges' : {
+             'default' : [['T(-10)', 'T(10)']],
+            },
+         'specific_values' : {
+            },
+         'verif_test' : {
+             'property_call' : {
+                 'default' : ['nt2::gsl_specfun::gsl_sf_psi(a0)'],
+                },
+             'property_value' : {
+                 'default' : ['nt2::gsl_specfun::gsl_sf_psi(a0)'],
+                },
+             'simd' : {
+                },
+             'ulp_thresh' : {
+                 'default' : ['1'],
+                },
+            },
+        },
+    },
+]
