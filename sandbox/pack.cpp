f8bcdad8e0919b1fb07134ba30679f4b8946ff75
Joel Falcou <joel.falcou@lri.fr>
2011-03-15 02:00:30 +0800

    Added small test of std:;accumulate over simd pack

--- sandbox/pack.cpp
+++ sandbox/pack.cpp
@@ -1,23 +1,52 @@
 #include <iostream>
-//#define BOOST_PROTO_STRICT_RESULT_OF
+#include <numeric>
+#define BOOST_PROTO_STRICT_RESULT_OF
 
 #include <nt2/sdk/simd/pack.hpp>
+#include <nt2/sdk/simd/begin.hpp>
+#include <nt2/sdk/simd/end.hpp>
+#include <nt2/include/timing.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/sdk/memory/aligned_type.hpp>
-#include <nt2/include/functions/cos.hpp>
-#include <nt2/include/functions/sin.hpp>
+#include <nt2/sdk/constant/dsl/digits.hpp>
+#include <nt2/sdk/constant/dsl/boolean.hpp>
 
 using namespace std;
 
 int main()
 {
-  NT2_ALIGNED_TYPE(float) v[4] = { 4.5,3.4,3.2,2.1};
-  nt2::simd::pack<float> x,y(5.321),w(&v[0],0);
-  std::cout << y << "\n";
-  std::cout << w << "\n";
-  x = y+y;
-  std::cout << x << "\n";
-  x = y*1000;
-  std::cout << x << "\n";
-  x = nt2::cos(y)*nt2::cos(y) + nt2::sin(y)*nt2::sin(y);
-  std::cout << x << "\n";
+  static volatile float r,vr;
+
+  std::cout << nt2::splat< nt2::simd::pack<float> >( 1.234 ) << std::endl;
+  std::cout << nt2::splat< nt2::simd::pack<float,8> >( 1.234 ) << std::endl;
+
+	nt2::memory::buffer<float, nt2::memory::allocator<float> > b(0,4096);
+	for(int i=0;i<4096;++i) b[i] = 1;
+	
+	nt2::tic();
+	for(int i=0;i<100000;++i) r = std::accumulate(b.begin(),b.end(),0.f);
+	double d = nt2::toc();
+	std::cout << r << std::endl;
+	
+	nt2::tic();
+	for(int i=0;i<100000;++i)
+	{
+	  nt2::simd::pack<float> zz(nt2::zero_),itr;
+	  itr = std::accumulate( nt2::simd::begin(b),nt2::simd::end(b),zz);
+    vr = std::accumulate( itr.begin(),itr.end(),0.f);
+	 }
+	double vd = nt2::toc();
+  std::cout << vr << std::endl;
+  std::cout << d/vd << std::endl;
+
+  nt2::simd::pack<float> xxx =
+  nt2::splat< nt2::simd::pack<float> >( 1.234 );
+
+  nt2::store( xxx, &b[0], 0);
+  std::cout << b[0] << b[1] << b[2] << b[3] << "\n";
+
+  nt2::store( xxx, &b[0], 10);
+  std::cout << b[40] << b[41] << b[42] << b[43] << "\n";
+
+
 }
