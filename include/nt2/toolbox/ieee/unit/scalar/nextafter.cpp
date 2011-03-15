2683fa7fba810c04d649ff4bbb3a284c2b84a02a
Mathias Gaunard <mathias@gaunard.com>
2011-03-15 22:33:53 +0800

    make nextafter test use boost::math::nextafter to avoid dependency on nextafter C99 function

--- include/nt2/toolbox/ieee/unit/scalar/nextafter.cpp
+++ include/nt2/toolbox/ieee/unit/scalar/nextafter.cpp
@@ -22,8 +22,9 @@
 #include <nt2/sdk/constant/infinites.hpp>
 #include <nt2/include/functions/ulpdist.hpp>
 #include <nt2/toolbox/ieee/include/nextafter.hpp>
+#include <boost/math/special_functions/next.hpp>
 
-NT2_TEST_CASE_TPL ( nextafter_float_2,  (float))
+NT2_TEST_CASE_TPL ( nextafter_real__2,  NT2_REAL_TYPES)
 {
   using nt2::nextafter;
   using nt2::tag::nextafter_;
@@ -51,47 +52,13 @@ NT2_TEST_CASE_TPL ( nextafter_float_2,  (float))
                   << "  a0 = "<< u_t(a0 = tab_a0[j])
                   << ", a1 = "<< u_t(a1 = tab_a1[j])
                   << std::endl;
-        NT2_TEST_EQUAL( nt2::nextafter(a0,a1),::nextafterf(a0,a1));
+        NT2_TEST_EQUAL( nt2::nextafter(a0,a1),boost::math::nextafter(a0,a1));
      }
      
    }
-} // end of test for float
+} // end of test for real_
 
-NT2_TEST_CASE_TPL ( nextafter_double_2,  (double))
-{
-  using nt2::nextafter;
-  using nt2::tag::nextafter_;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef typename nt2::meta::call<nextafter_(T,T)>::type r_t;
-  typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef T wished_r_t;
-
-
-  // return type conformity test 
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl; 
-  double ulpd;
-
-  // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(-10), T(10));
-    double ulp0 = 0.0, ulpd = 0.0;
-    T a0,a1;
-    for (int j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_EQUAL( nt2::nextafter(a0,a1),::nextafter(a0,a1));
-     }
-     
-   }
-} // end of test for double
-
-NT2_TEST_CASE_TPL ( nextafter_unsigned_int__2,  NT2_UNSIGNED_TYPES)
+NT2_TEST_CASE_TPL ( nextafter_integral__2,  NT2_INTEGRAL_TYPES)
 {
   using nt2::nextafter;
   using nt2::tag::nextafter_;
@@ -123,38 +90,4 @@ NT2_TEST_CASE_TPL ( nextafter_unsigned_int__2,  NT2_UNSIGNED_TYPES)
      }
      
    }
-} // end of test for unsigned_int_
-
-NT2_TEST_CASE_TPL ( nextafter_signed_int__2,  NT2_INTEGRAL_SIGNED_TYPES)
-{
-  using nt2::nextafter;
-  using nt2::tag::nextafter_;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef typename nt2::meta::call<nextafter_(T,T)>::type r_t;
-  typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef T wished_r_t;
-
-
-  // return type conformity test 
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl; 
-  double ulpd;
-
-  // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, -100, 100);
-    NT2_CREATE_BUF(tab_a1,T, NR, -100, 100);
-    double ulp0 = 0.0, ulpd = 0.0;
-    T a0,a1;
-    for (int j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_EQUAL( nt2::nextafter(a0,a1),(a1 == a0) ? a0 : (a1 > a0) ? a0+nt2::One<T>() : a0-nt2::One<T>());
-     }
-     
-   }
-} // end of test for signed_int_
+} // end of test for integral_
