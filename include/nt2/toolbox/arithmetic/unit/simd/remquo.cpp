91b61c0aa7e183520f823105c3c62536d0bd754e
Jt lapreste <jtlapreste@gmail.com>
2011-03-17 13:13:40 +0100

    simd arithmetic units

--- include/nt2/toolbox/arithmetic/unit/simd/remquo.cpp
+++ include/nt2/toolbox/arithmetic/unit/simd/remquo.cpp
@@ -12,7 +12,7 @@
 // Test behavior of arithmetic components in simd mode
 //////////////////////////////////////////////////////////////////////////////
 /// created by jt the 01/12/2010
-/// modified by jt the 15/02/2011
+/// modified by jt the 16/03/2011
 #include <nt2/sdk/memory/is_aligned.hpp>
 #include <nt2/sdk/memory/aligned_type.hpp>
 #include <nt2/sdk/memory/load.hpp>
@@ -23,7 +23,9 @@
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/constant/real.hpp>
 #include <nt2/sdk/constant/infinites.hpp>
+#include <nt2/include/functions/max.hpp>
 #include <nt2/toolbox/arithmetic/include/remquo.hpp>
+#include <boost/fusion/tuple.hpp>
 
 NT2_TEST_CASE_TPL ( remquo_real__2,  NT2_REAL_TYPES)
 {
@@ -40,31 +42,31 @@ NT2_TEST_CASE_TPL ( remquo_real__2,  NT2_REAL_TYPES)
   typedef native<iT,ext_t>                       ivT;
   typedef typename nt2::meta::call<remquo_(vT,vT)>::type r_t;
   typedef typename nt2::meta::call<remquo_(T,T)>::type sr_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
 
   // random verifications
   static const uint32_t NR = NT2_NB_RANDOM_TEST;
   {
-    NT2_CREATE_SIMD_BUFFER(a0,T, NR, T(0), T(10));
-    NT2_CREATE_SIMD_BUFFER(a1,T, NR, T(0), T(10));
+    NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(10));
+    NT2_CREATE_BUF(tab_a1,T, NR, T(0), T(10));
     double ulp0 = 0.0, ulpd = 0.0;
-    for(int j = 0; j < NR/cardinal_of<n_t>::value; j++)
+    for(uint32_t j = 0; j < NR/cardinal_of<n_t>::value; j++)
       {
-        vT a0 = load<n_t>(&tab_a0[0],j);
-        vT a1 = load<n_t>(&tab_a1[0],j);
+        vT a0 = load<vT>(&tab_a0[0],j);
+        vT a1 = load<vT>(&tab_a1[0],j);
         r_t r = nt2::remquo(a0,a1);
         for(int i = 0; i< cardinal_of<n_t>::value; i++)
         {
           int k = i+j*cardinal_of<n_t>::value;
           sr_t sr =  nt2::remquo(tab_a0[k],tab_a1[k]);
           NT2_TEST_TUPLE_ULP_EQUAL( boost::fusion::get<0>(r)[i],
-                                    boost::fusion::get<0>(sr), 0.5);
+                                    boost::fusion::get<0>(sr), 1.5);
           ulp0 = nt2::max(ulpd,ulp0);
-	  NT2_TEST_TUPLE_ULP_EQUAL( boost::fusion::get<1>(r)[i],
-                                     boost::fusion::get<1>(sr), 0.5);
-	  ulp0 = nt2::max(ulpd,ulp0);
+          NT2_TEST_TUPLE_ULP_EQUAL( boost::fusion::get<1>(r)[i],
+                                    boost::fusion::get<1>(sr), 1.5);
+          ulp0 = nt2::max(ulpd,ulp0);
         }
       }
-    std::cout << "max ulp found is: " << ulp0 << std::endl; 
+    std::cout << "max ulp found is: " << ulp0 << std::endl;
   }
 } // end of test for real_
- 
