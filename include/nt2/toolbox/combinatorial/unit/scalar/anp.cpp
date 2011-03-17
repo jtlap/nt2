91165dbd74be96bea505a211394ea1caf89ef076
Jt lapreste <jtlapreste@gmail.com>
2011-03-17 13:22:39 +0100

    combinatorial unit scalar

--- include/nt2/toolbox/combinatorial/unit/scalar/anp.cpp
+++ include/nt2/toolbox/combinatorial/unit/scalar/anp.cpp
@@ -12,7 +12,7 @@
 // Test behavior of combinatorial components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created by jt the 30/11/2010
-/// modified by jt the 15/02/2011
+/// modified by jt the 16/03/2011
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -25,65 +25,71 @@
 
 NT2_TEST_CASE_TPL ( anp_real__2,  NT2_REAL_TYPES)
 {
+  
   using nt2::anp;
   using nt2::tag::anp_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<anp_(T,T)>::type r_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef typename std::tr1::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
+  typedef typename boost::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
 
 
   // return type conformity test 
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
   double ulpd;
+  ulpd=0.0;
 
 
   // specific values tests
-  NT2_TEST_ULP_EQUAL(  anp(nt2::Inf<T>(), nt2::Inf<T>()), nt2::Nan<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  anp(nt2::Nan<T>(), nt2::Nan<T>()), nt2::Nan<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  anp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  anp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(anp(nt2::Inf<T>(), nt2::Inf<T>()), nt2::Nan<T>(), 0);
+  NT2_TEST_ULP_EQUAL(anp(nt2::Nan<T>(), nt2::Nan<T>()), nt2::Nan<T>(), 0);
+  NT2_TEST_ULP_EQUAL(anp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(anp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
 } // end of test for real_
 
 NT2_TEST_CASE_TPL ( anp_unsigned_int__2,  NT2_UNSIGNED_TYPES)
 {
+  
   using nt2::anp;
   using nt2::tag::anp_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<anp_(T,T)>::type r_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef typename std::tr1::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
+  typedef typename boost::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
 
 
   // return type conformity test 
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
   double ulpd;
+  ulpd=0.0;
 
 
   // specific values tests
-  NT2_TEST_ULP_EQUAL(  anp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  anp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(anp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(anp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
 } // end of test for unsigned_int_
 
 NT2_TEST_CASE_TPL ( anp_signed_int__2,  NT2_INTEGRAL_SIGNED_TYPES)
 {
+  
   using nt2::anp;
   using nt2::tag::anp_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<anp_(T,T)>::type r_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef typename std::tr1::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
+  typedef typename boost::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
 
 
   // return type conformity test 
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
   double ulpd;
+  ulpd=0.0;
 
 
   // specific values tests
-  NT2_TEST_ULP_EQUAL(  anp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  anp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(anp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(anp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
 } // end of test for signed_int_
--- include/nt2/toolbox/combinatorial/unit/scalar/cnp.cpp
+++ include/nt2/toolbox/combinatorial/unit/scalar/cnp.cpp
@@ -12,7 +12,7 @@
 // Test behavior of combinatorial components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created by jt the 30/11/2010
-/// modified by jt the 15/02/2011
+/// modified by jt the 16/03/2011
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -25,70 +25,76 @@
 
 NT2_TEST_CASE_TPL ( cnp_real__2,  NT2_REAL_TYPES)
 {
+  
   using nt2::cnp;
   using nt2::tag::cnp_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<cnp_(T,T)>::type r_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef typename std::tr1::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
+  typedef typename boost::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
 
 
   // return type conformity test 
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
   double ulpd;
+  ulpd=0.0;
 
 
   // specific values tests
-  NT2_TEST_ULP_EQUAL(  cnp(T(10),T(1)), T(10), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(T(10),T(2)), T(45), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(T(10),T(8)), T(45), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(T(2),T(1)), T(2), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(T(2),T(2)), T(1), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(nt2::Inf<T>(), nt2::Inf<T>()), nt2::Nan<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(nt2::Nan<T>(), nt2::Nan<T>()), nt2::Nan<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(cnp(T(10),T(1)), T(10), 0);
+  NT2_TEST_ULP_EQUAL(cnp(T(10),T(2)), T(45), 0);
+  NT2_TEST_ULP_EQUAL(cnp(T(10),T(8)), T(45), 0);
+  NT2_TEST_ULP_EQUAL(cnp(T(2),T(1)), T(2), 0);
+  NT2_TEST_ULP_EQUAL(cnp(T(2),T(2)), T(1), 0);
+  NT2_TEST_ULP_EQUAL(cnp(nt2::Inf<T>(), nt2::Inf<T>()), nt2::Nan<T>(), 0);
+  NT2_TEST_ULP_EQUAL(cnp(nt2::Nan<T>(), nt2::Nan<T>()), nt2::Nan<T>(), 0);
+  NT2_TEST_ULP_EQUAL(cnp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(cnp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
 } // end of test for real_
 
 NT2_TEST_CASE_TPL ( cnp_unsigned_int__2,  NT2_UNSIGNED_TYPES)
 {
+  
   using nt2::cnp;
   using nt2::tag::cnp_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<cnp_(T,T)>::type r_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef typename std::tr1::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
+  typedef typename boost::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
 
 
   // return type conformity test 
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
   double ulpd;
+  ulpd=0.0;
 
 
   // specific values tests
-  NT2_TEST_ULP_EQUAL(  cnp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(cnp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(cnp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
 } // end of test for unsigned_int_
 
 NT2_TEST_CASE_TPL ( cnp_signed_int__2,  NT2_INTEGRAL_SIGNED_TYPES)
 {
+  
   using nt2::cnp;
   using nt2::tag::cnp_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<cnp_(T,T)>::type r_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef typename std::tr1::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
+  typedef typename boost::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
 
 
   // return type conformity test 
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
   double ulpd;
+  ulpd=0.0;
 
 
   // specific values tests
-  NT2_TEST_ULP_EQUAL(  cnp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
-  NT2_TEST_ULP_EQUAL(  cnp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(cnp(nt2::One<T>(), nt2::One<T>()), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(cnp(nt2::Zero<T>(), nt2::Zero<T>()), nt2::One<T>(), 0);
 } // end of test for signed_int_
