8c498359616762cd083799a5c8d96c4fffd15f4b
Jt lapreste <jtlapreste@gmail.com>
2011-03-18 17:35:07 +0100

    predicates simd units

--- include/nt2/toolbox/predicates/unit/simd/is_positive.cpp
+++ include/nt2/toolbox/predicates/unit/simd/is_positive.cpp
@@ -12,21 +12,21 @@
 // Test behavior of predicates components in simd mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 21/02/2011
-/// modified by jt the 21/02/2011
+/// modified by jt the 18/03/2011
 #include <nt2/sdk/memory/is_aligned.hpp>
 #include <nt2/sdk/memory/aligned_type.hpp>
 #include <nt2/sdk/memory/load.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/unit/no_ulp_tests.hpp>
+#include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/constant/real.hpp>
 #include <nt2/sdk/constant/infinites.hpp>
 #include <nt2/include/functions/max.hpp>
 #include <nt2/toolbox/predicates/include/is_positive.hpp>
 
-NT2_TEST_CASE_TPL ( is_positive_real__1,  NT2_REAL_TYPES)
+NT2_TEST_CASE_TPL ( is_positive_real__1_0,  NT2_REAL_TYPES)
 {
   using nt2::is_positive;
   using nt2::tag::is_positive_;
@@ -47,8 +47,8 @@ NT2_TEST_CASE_TPL ( is_positive_real__1,  NT2_REAL_TYPES)
   static const uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
-    double ulp0 = 0.0, ulpd = 0.0;
-    for(int j = 0; j < NR/cardinal_of<n_t>::value; j++)
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    for(uint32_t j = 0; j < NR/cardinal_of<n_t>::value; j++)
       {
         vT a0 = load<vT>(&tab_a0[0],j);
         r_t v = is_positive(a0);
