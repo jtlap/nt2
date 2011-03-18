1f24b5557511b5bc4530b798b2a31b98579565cc
Mathias Gaunard <mathias@gaunard.com>
2011-03-19 01:58:23 +0800

    replace call to __builtin_popcount by nt2::popcnt

--- include/nt2/toolbox/reduction/function/simd/sse/sse2/nbtrue.hpp
+++ include/nt2/toolbox/reduction/function/simd/sse/sse2/nbtrue.hpp
@@ -12,6 +12,7 @@
 #include <nt2/sdk/meta/strip.hpp>
 
 #include <nt2/include/functions/is_nez.hpp>
+#include <nt2/include/functions/popcnt.hpp>
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -33,7 +34,7 @@ namespace nt2 { namespace ext
     {
       typedef typename simd::native<typename meta::int8_t_<A0>::type,tag::sse_> i8type;
       i8type tmp = {is_nez(a0)};
-      return __builtin_popcount(_mm_movemask_epi8(tmp))*meta::cardinal_of<A0>::value >> 4;
+      return nt2::popcnt(_mm_movemask_epi8(tmp))*meta::cardinal_of<A0>::value >> 4;
     }
   };
 } }
@@ -81,7 +82,7 @@ namespace nt2 { namespace ext
       typedef typename meta::as_real<A0>::type type;
       int32_t  r = _mm_movemask_ps(is_nez(a0));
       return   (r&1)+((r>>1)&1)+((r>>2)&1)+(r>>3);
-      //      return __builtin_popcount(_mm_movemask_ps(isnez(cast<type>(a0))));
+      //      return nt2::popcnt(_mm_movemask_ps(is_nez(cast<type>(a0))));
     }
   };
 } }
