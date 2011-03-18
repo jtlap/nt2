ff26746bf1988a95169bee71d87bb71f7f026226
Joel Falcou <joel.falcou@lri.fr>
2011-03-19 00:34:44 +0800

    Added a meta::function to detect assign-based node in proto expression

---
+++ include/nt2/sdk/dsl/is_assignment_expression.hpp
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_CORE_CONTAINER_META_IS_ASSIGNMENT_EXPRESSION_HPP_INCLUDED
+#define NT2_CORE_CONTAINER_META_IS_ASSIGNMENT_EXPRESSION_HPP_INCLUDED
+
+////////////////////////////////////////////////////////////////////////////////
+// Check if a given proto expression has a ?= tag
+////////////////////////////////////////////////////////////////////////////////
+#include <boost/mpl/bool.hpp>
+#include <boost/proto/tags.hpp>
+#include <boost/proto/matches.hpp>
+
+namespace nt2 { namespace details
+{
+  struct assign_cases
+  {
+    template<class Tag> struct case_ : boost::proto::not_< boost::proto::_ > {};
+  };
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::shift_right_assign >
+        : boost::proto::_ {};
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::shift_left_assign >
+        : boost::proto::_ {};
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::multiplies_assign >
+        : boost::proto::_ {};
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::divides_assign >
+        : boost::proto::_ {};
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::modulus_assign >
+        : boost::proto::_ {};
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::plus_assign >
+        : boost::proto::_ {};
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::minus_assign >
+        : boost::proto::_ {};
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::bitwise_and_assign >
+        : boost::proto::_ {};
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::bitwise_or_assign >
+        : boost::proto::_ {};
+
+  template<>
+  struct  assign_cases::case_< boost::proto::tag::bitwise_xor_assign >
+        : boost::proto::_ {};
+} }
+
+namespace nt2 { namespace meta
+{
+  template<class Expr>
+  struct  is_assignment_expression
+        : boost::proto::matches < Expr
+                                , boost::proto::switch_< details::assign_cases>
+                                >
+  {};
+} }
+
+#endif
