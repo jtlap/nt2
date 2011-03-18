6f7c4780c363a5dd91ebb2f35300c675fb49ec93
Jt lapreste <jtlapreste@gmail.com>
2011-03-18 17:35:31 +0100

    predicates scalar units

--- include/nt2/toolbox/predicates/unit/scalar/is_not_greater.cpp
+++ include/nt2/toolbox/predicates/unit/scalar/is_not_greater.cpp
@@ -12,10 +12,10 @@
 // Test behavior of predicates components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 21/02/2011
-/// modified by jt the 21/02/2011
+/// modified by jt the 18/03/2011
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/unit/no_ulp_tests.hpp>
+#include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/sdk/constant/real.hpp>
@@ -25,8 +25,9 @@
 // specific includes for arity 2 tests
 #include <nt2/sdk/meta/logical.hpp>
 
-NT2_TEST_CASE_TPL ( is_not_greater_real__2,  NT2_REAL_TYPES)
+NT2_TEST_CASE_TPL ( is_not_greater_real__2_0,  NT2_REAL_TYPES)
 {
+  
   using nt2::is_not_greater;
   using nt2::tag::is_not_greater_;
   typedef typename nt2::meta::as_integer<T>::type iT;
@@ -39,6 +40,7 @@ NT2_TEST_CASE_TPL ( is_not_greater_real__2,  NT2_REAL_TYPES)
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
   double ulpd;
+  ulpd=0.0;
 
 
   // specific values tests
@@ -57,9 +59,10 @@ NT2_TEST_CASE_TPL ( is_not_greater_real__2,  NT2_REAL_TYPES)
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
     NT2_CREATE_BUF(tab_a1,T, NR, T(-10000), T(10000));
-    double ulp0 = 0.0, ulpd = 0.0;
-    T a0,a1;
-    for (int j =0; j < NR; ++j )
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    T a0;
+    T a1;
+    for (uint32_t j =0; j < NR; ++j )
       {
         std::cout << "for params "
                   << "  a0 = "<< u_t(a0 = tab_a0[j])
@@ -71,8 +74,9 @@ NT2_TEST_CASE_TPL ( is_not_greater_real__2,  NT2_REAL_TYPES)
    }
 } // end of test for real_
 
-NT2_TEST_CASE_TPL ( is_not_greater_signed_int__2,  NT2_INTEGRAL_SIGNED_TYPES)
+NT2_TEST_CASE_TPL ( is_not_greater_signed_int__2_0,  NT2_INTEGRAL_SIGNED_TYPES)
 {
+  
   using nt2::is_not_greater;
   using nt2::tag::is_not_greater_;
   typedef typename nt2::meta::as_integer<T>::type iT;
@@ -85,6 +89,7 @@ NT2_TEST_CASE_TPL ( is_not_greater_signed_int__2,  NT2_INTEGRAL_SIGNED_TYPES)
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
   double ulpd;
+  ulpd=0.0;
 
 
   // specific values tests
@@ -97,9 +102,10 @@ NT2_TEST_CASE_TPL ( is_not_greater_signed_int__2,  NT2_INTEGRAL_SIGNED_TYPES)
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
     NT2_CREATE_BUF(tab_a1,T, NR, T(-10000), T(10000));
-    double ulp0 = 0.0, ulpd = 0.0;
-    T a0,a1;
-    for (int j =0; j < NR; ++j )
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    T a0;
+    T a1;
+    for (uint32_t j =0; j < NR; ++j )
       {
         std::cout << "for params "
                   << "  a0 = "<< u_t(a0 = tab_a0[j])
@@ -111,8 +117,9 @@ NT2_TEST_CASE_TPL ( is_not_greater_signed_int__2,  NT2_INTEGRAL_SIGNED_TYPES)
    }
 } // end of test for signed_int_
 
-NT2_TEST_CASE_TPL ( is_not_greater_unsigned_int__2,  NT2_UNSIGNED_TYPES)
+NT2_TEST_CASE_TPL ( is_not_greater_unsigned_int__2_0,  NT2_UNSIGNED_TYPES)
 {
+  
   using nt2::is_not_greater;
   using nt2::tag::is_not_greater_;
   typedef typename nt2::meta::as_integer<T>::type iT;
@@ -125,6 +132,7 @@ NT2_TEST_CASE_TPL ( is_not_greater_unsigned_int__2,  NT2_UNSIGNED_TYPES)
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
   double ulpd;
+  ulpd=0.0;
 
 
   // specific values tests
@@ -136,9 +144,10 @@ NT2_TEST_CASE_TPL ( is_not_greater_unsigned_int__2,  NT2_UNSIGNED_TYPES)
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
     NT2_CREATE_BUF(tab_a1,T, NR, T(-10000), T(10000));
-    double ulp0 = 0.0, ulpd = 0.0;
-    T a0,a1;
-    for (int j =0; j < NR; ++j )
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    T a0;
+    T a1;
+    for (uint32_t j =0; j < NR; ++j )
       {
         std::cout << "for params "
                   << "  a0 = "<< u_t(a0 = tab_a0[j])
