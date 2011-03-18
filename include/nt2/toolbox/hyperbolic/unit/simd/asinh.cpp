6f940e7fd60a4fd0d35a8b6acbac56e5de565e90
Jt lapreste <jtlapreste@gmail.com>
2011-03-18 17:21:36 +0100

    hyperbolic simd units

--- include/nt2/toolbox/hyperbolic/unit/simd/asinh.cpp
+++ include/nt2/toolbox/hyperbolic/unit/simd/asinh.cpp
@@ -12,7 +12,7 @@
 // Test behavior of hyperbolic components in simd mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 20/02/2011
-/// modified by jt the 20/02/2011
+/// modified by jt the 18/03/2011
 #include <nt2/sdk/memory/is_aligned.hpp>
 #include <nt2/sdk/memory/aligned_type.hpp>
 #include <nt2/sdk/memory/load.hpp>
@@ -23,9 +23,10 @@
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/constant/real.hpp>
 #include <nt2/sdk/constant/infinites.hpp>
+#include <nt2/include/functions/max.hpp>
 #include <nt2/toolbox/hyperbolic/include/asinh.hpp>
 
-NT2_TEST_CASE_TPL ( asinh_real__1,  NT2_REAL_TYPES)
+NT2_TEST_CASE_TPL ( asinh_real__1_0,  NT2_REAL_TYPES)
 {
   using nt2::asinh;
   using nt2::tag::asinh_;
@@ -46,10 +47,10 @@ NT2_TEST_CASE_TPL ( asinh_real__1,  NT2_REAL_TYPES)
   static const uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
-    double ulp0 = 0.0, ulpd = 0.0;
-    for(int j = 0; j < NR/cardinal_of<n_t>::value; j++)
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    for(uint32_t j = 0; j < NR/cardinal_of<n_t>::value; j++)
       {
-        vT a0 = load<n_t>(&tab_a0[0],j);
+        vT a0 = load<vT>(&tab_a0[0],j);
         r_t v = asinh(a0);
         for(int i = 0; i< cardinal_of<n_t>::value; i++)
         {
