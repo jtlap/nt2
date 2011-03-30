97638581c1e5e37de8807ee24b0c7be1d58b43bf
Jt lapreste <jtlapreste@gmail.com>
2011-03-30 18:07:13 +0200

    modifs in sdk

--- modules/sdk/include/nt2/sdk/unit/details/ulpdist.hpp
+++ modules/sdk/include/nt2/sdk/unit/details/ulpdist.hpp
@@ -1,6 +1,6 @@
 /*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *         Copyright 2003-2011 LASMEA UMR 6602 CNRS/U.B.P
+ *         Copyright 2009-2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
  *
  *          Distributed under the Boost Software License, Version 1.0.
  *                 See accompanying file LICENSE.txt or copy at
@@ -8,16 +8,68 @@
  ******************************************************************************/
 #ifndef NT2_SDK_UNIT_DETAILS_ULPDIST_HPP_INCLUDED
 #define NT2_SDK_UNIT_DETAILS_ULPDIST_HPP_INCLUDED
+#include <nt2/sdk/constant/eps_related.hpp>
 
 namespace nt2
 {
-    namespace details
+  namespace details {
+    
+    struct other_ {};
+    struct double_{};
+    struct float_ {}; 
+    template < class T > T nt2_ulpdist(T a0, T a1,  double_ )
     {
-        double ulpdist(double a, double b)
-        {
-            return b-a;
-        }
+      typedef int32_t itype;
+      typedef T   type; 
+      if (a0 == a1)              return Zero<type>();
+      if ((a0!=a0) && (a1!= a1)) return Zero<type>();
+      itype e1 = 0, e2 = 0;
+      double m1, m2;
+      ::frexp(a0, &e1);
+      ::frexp(a1, &e2);
+      itype expo = -(e1 > e2)?e1:e2;
+      type e = (e1 == e2) ?
+	std::abs(m1-m2) :
+	std::abs(::ldexp(a0, expo) - ::ldexp(a1, expo));
+      return e/nt2::Eps<type>();
     }
-}
+    
+    template < class T > T nt2_ulpdist(T a0, T a1,  float_)
+    {
+      typedef int32_t itype;
+      typedef T   type; 
+      if (a0 == a1)              return Zero<type>();
+      if ((a0!=a0) && (a1!= a1)) return Zero<type>();
+      itype e1 = 0, e2 = 0;
+      double m1, m2;
+      ::frexpf(a0, &e1);
+      ::frexpf(a1, &e2);
+      itype expo = -(e1 > e2)?e1:e2;
+      type e = (e1 == e2) ?
+	std::abs(m1-m2) :
+	std::abs(::ldexpf(a0, expo) - ::ldexpf(a1, expo));
+      return e/Eps<type>();
+    }
+    
+    template < class T > T nt2_ulpdist(T a0, T a1,  other_)
+    {
+      return a0 > a1 ? a0-a1 : a1-a0; 
+    }
+    
+    template <class T> struct classif  { typedef   other_ type; };
+    template <> struct classif<double> { typedef  double_ type; };
+    template <> struct classif<float>  { typedef   float_ type; };
+  }    
 
+  template < class T > T  nt2_ulpdist(T a0, T a1)
+  {
+    typedef typename meta::strip<T>::type type; 
+    return nt2_ulpdist(type(a0), type(a1), typename details::classif<T>::type()); 
+  }
+
+}
 #endif
+
+// /////////////////////////////////////////////////////////////////////////////
+// End of ulpdist.hpp
+// /////////////////////////////////////////////////////////////////////////////
--- modules/sdk/include/nt2/sdk/unit/tests.hpp
+++ modules/sdk/include/nt2/sdk/unit/tests.hpp
@@ -14,9 +14,10 @@
 // Documentation: http://nt2.lri.fr/sdk/unit/tests.html
 ////////////////////////////////////////////////////////////////////////////////
 #include <nt2/sdk/details/preprocessor.hpp>
+#include <nt2/sdk/unit/details/random.hpp>
 #include <nt2/sdk/unit/details/stats.hpp>
 #include <nt2/sdk/unit/details/tests.hpp>
-#include <nt2/sdk/unit/details/random.hpp>
+
 #include <nt2/sdk/unit/tests/basic.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
 #include <nt2/sdk/unit/nb_rand_tests.hpp>
@@ -26,7 +27,7 @@
     typedef typename nt2::meta::scalar_of<r_t>::type sr_t;    \
     sr_t r1 = A;              \
     sr_t r2 = B;              \
-    ulpd = nt2::details::ulpdist(r1, r2);          \
+    ulpd = nt2::nt2_ulpdist(r1, r2);          \
     bool b;                 \
     b = ::nt2::details::test_ulp_eq(#A, #B, #N, __LINE__,    \
               BOOST_CURRENT_FUNCTION,    \
@@ -46,7 +47,7 @@
     b = ::nt2::details::test_ulp_eq(#A, #B, #N, __LINE__,    \
               BOOST_CURRENT_FUNCTION,    \
               A, B, N);       \
-    ulpd = nt2::details::ulpdist(A, B);          \
+    ulpd = nt2::nt2_ulpdist(A, B);          \
       if (!b)                \
   {                \
     std::cout << "   because " << #A << " = " << A    \
@@ -64,46 +65,52 @@
     if (!b)                \
       {                  \
   std::cout << "   because " << #A << " = " << r1 << " and " << #B << " = " << r2 <<  std::endl; \
-  std::cout << "   and ulp distance is " << nt2::details::ulpdist(A, B) << std::endl; \
+  std::cout << "   and ulp distance is " << nt2::nt2_ulpdist(A, B) << std::endl; \
       }                  \
 /**/
 #define NT2_SHOW_ARG1(ARG)        \
   std::cout << "   for a0 = " << ARG << std::endl;  \
 /**/
 
-#define NT2_CREATE_BUF(NAME, TYPE, SIZE, MIN, MAX)	\
-  nt2::memory::buffer<TYPE,				\
-		      nt2::memory::allocator<TYPE> >    \
-		      NAME(0, SIZE);			\
-  for(int k = 0; k < (int)SIZE; ++k){			\
-    NAME[k] = nt2::details::random(MIN, MAX);			\
-  }							\
+#define NT2_CREATE_BUF(NAME, TYPE, SIZE, MIN, MAX)		\
+  nt2::memory::buffer<TYPE,					\
+		      nt2::memory::allocator<TYPE> >		\
+  NAME(0, SIZE);						\
+  {/*TYPE fac = double((MAX-MIN))/(SIZE+2);*/			\
+    for(int k = 0; k < (int)SIZE; ++k){				\
+    /*NAME[k] = MIN+(k+1)*fac;*/				\
+    NAME[k] = nt2::random(MIN, MAX);				\
+    }}								\
   /**/
 
 #define NT2_CREATE_BUFFER(NAME, TYPE, SIZE, MIN, MAX)	\
-  nt2::memory::buffer<TYPE,        \
-          nt2::memory::allocator<TYPE> >    \
-  tab_##NAME(0, SIZE);            \
-  for(int k = 0; k < (int)SIZE; ++k){		\
-    tab_##NAME[k] = nt2::details::random(MIN, MAX);      \
-  }                \
+  nt2::memory::buffer<TYPE,				\
+		      nt2::memory::allocator<TYPE> >    \
+		      tab_##NAME(0, SIZE);		\
+  {/*TYPE fac = double((MAX-MIN))/(SIZE+2);*/   	\
+    for(int k = 0; k < (int)SIZE; ++k){			\
+      /*tab_##NAME[k] = MIN+(k+1)*fac;*/	      	\
+      NAME[k] = nt2::random(MIN, MAX);	       		\
+   }}							\
 /**/
+
 #define NT2_CREATE_SCALAR_BUFFER(NAME, TYPE, SIZE, MIN, MAX)  \
   nt2::memory::buffer<TYPE,          \
           nt2::memory::allocator<TYPE> >    \
   tab_##NAME(0, SIZE);            \
   for(int k = 0; k < (int)SIZE; ++k){        \
-    tab_##NAME[k] = nt2::details::random(MIN, MAX);      \
+    tab_##NAME[k] = nt2::random(MIN, MAX);      \
   }                \
 /**/
-#define NT2_CREATE_SIMD_BUFFER(NAME, TYPE, SIZE, MIN, MAX)  \
-  nt2::memory::buffer<TYPE,          \
-          nt2::memory::allocator<TYPE> >    \
-  tab_##NAME(0, SIZE);            \
-  for(int k = 0; k < (int)SIZE; ++k){        \
-    tab_##NAME[k] = nt2::details::random(MIN, MAX);      \
-  }                \
-/**/
+
+// #define NT2_CREATE_SIMD_BUFFER(NAME, TYPE, SIZE, MIN, MAX)  \
+//   nt2::memory::buffer<TYPE,          \
+//           nt2::memory::allocator<TYPE> >    \
+//   tab_##NAME(0, SIZE);            \
+//   for(int k = 0; k < (int)SIZE; ++k){        \
+//     tab_##NAME[k] = nt2::random(MIN, MAX);      \
+//   }                \
+// /**/
 
 ////////////////////////////////////////////////////////////////////////////////
 // Verbose predicate based tests
