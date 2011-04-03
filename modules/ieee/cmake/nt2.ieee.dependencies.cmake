bac847cf5b3959d3b04f610fde91f148ccb185e2
Mathias Gaunard <mathias@gaunard.com>
2011-04-03 23:34:02 +0200

    added generation of simd forwarding headers to several toolboxes in src + moving math.hpp configuration to src

--- modules/ieee/cmake/nt2.ieee.dependencies.cmake
+++ modules/ieee/cmake/nt2.ieee.dependencies.cmake
@@ -7,8 +7,6 @@
 #                     http://www.boost.org/LICENSE_1_0.txt
 ################################################################################
 
-INCLUDE(nt2.ieee.math)
-
 FIND_PACKAGE(NT2 COMPONENTS predicates reduction)
 SET(NT2_IEEE_DEPENDENCIES_FOUND ${NT2_FOUND})
 SET(NT2_IEEE_DEPENDENCIES_INCLUDE_DIR ${NT2_INCLUDE_DIR})
