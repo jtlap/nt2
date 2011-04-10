7ed955e9cc078abacc7eba0d382172f62774e863
Mathias Gaunard <mathias@gaunard.com>
2011-04-11 00:29:12 +0200

    add explicit files for the is_not_greater alias to is_ngt and other similar aliases

--- modules/predicates/include/nt2/toolbox/predicates/function/is_ngt.hpp
+++ modules/predicates/include/nt2/toolbox/predicates/function/is_ngt.hpp
@@ -17,7 +17,6 @@ namespace nt2 { namespace tag
     struct is_ngt_ {};
   }
   NT2_FUNCTION_IMPLEMENTATION(tag::is_ngt_, is_ngt, 2)
-  NT2_FUNCTION_IMPLEMENTATION(tag::is_ngt_, is_not_greater, 2)
 }
  
 #include <nt2/toolbox/predicates/function/scalar/is_ngt.hpp>
