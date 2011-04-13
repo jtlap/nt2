d554f900ebe4a34e17f0c703995d666a648126b3
Jt lapreste <jtlapreste@gmail.com>
2011-04-13 16:37:10 +0200

    units value only for predicates

--- modules/predicates/unit/scalar/is_not_greater_equal.cpp
+++ modules/predicates/unit/scalar/is_not_greater_equal.cpp
@@ -12,7 +12,7 @@
 // Test behavior of predicates components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 21/02/2011
-/// modified by jt the 18/03/2011
+/// modified by jt the 13/04/2011
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -44,34 +44,16 @@ NT2_TEST_CASE_TPL ( is_not_greater_equal_real__2_0,  NT2_REAL_TYPES)
 
 
   // specific values tests
-  NT2_TEST_EQUAL(is_not_greater_equal(-nt2::Zero<T>(), -nt2::Zero<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Half<T>(), nt2::Half<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Inf<T>(), nt2::Inf<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Minf<T>(), nt2::Minf<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Mone<T>(), nt2::Mone<T>()), nt2::True<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(-nt2::Zero<T>(), -nt2::Zero<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Half<T>(), nt2::Half<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Inf<T>(), nt2::Inf<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Minf<T>(), nt2::Minf<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Mone<T>(), nt2::Mone<T>()), nt2::False<r_t>());
   NT2_TEST_EQUAL(is_not_greater_equal(nt2::Nan<T>(), nt2::Nan<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::One<T>(), nt2::One<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Quarter<T>(), nt2::Quarter<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Two<T>(), nt2::Two<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Zero<T>(), nt2::Zero<T>()), nt2::True<r_t>());
-  // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(-10000), T(10000));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    T a1;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_EQUAL( nt2::is_not_greater_equal(a0,a1),nt2::is_not_greater_equal(a0,a1));
-     }
-     
-   }
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::One<T>(), nt2::One<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Quarter<T>(), nt2::Quarter<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Two<T>(), nt2::Two<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Zero<T>(), nt2::Zero<T>()), nt2::False<r_t>());
 } // end of test for real_
 
 NT2_TEST_CASE_TPL ( is_not_greater_equal_signed_int__2_0,  NT2_INTEGRAL_SIGNED_TYPES)
@@ -93,28 +75,10 @@ NT2_TEST_CASE_TPL ( is_not_greater_equal_signed_int__2_0,  NT2_INTEGRAL_SIGNED_T
 
 
   // specific values tests
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Mone<T>(), nt2::Mone<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::One<T>(), nt2::One<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Two<T>(), nt2::Two<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Zero<T>(), nt2::Zero<T>()), nt2::True<r_t>());
-  // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(-10000), T(10000));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    T a1;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_EQUAL( nt2::is_not_greater_equal(a0,a1),nt2::is_not_greater_equal(a0,a1));
-     }
-     
-   }
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Mone<T>(), nt2::Mone<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::One<T>(), nt2::One<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Two<T>(), nt2::Two<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Zero<T>(), nt2::Zero<T>()), nt2::False<r_t>());
 } // end of test for signed_int_
 
 NT2_TEST_CASE_TPL ( is_not_greater_equal_unsigned_int__2_0,  NT2_UNSIGNED_TYPES)
@@ -136,25 +100,7 @@ NT2_TEST_CASE_TPL ( is_not_greater_equal_unsigned_int__2_0,  NT2_UNSIGNED_TYPES)
 
 
   // specific values tests
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::One<T>(), nt2::One<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Two<T>(), nt2::Two<T>()), nt2::True<r_t>());
-  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Zero<T>(), nt2::Zero<T>()), nt2::True<r_t>());
-  // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(-10000), T(10000));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    T a1;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_EQUAL( nt2::is_not_greater_equal(a0,a1),nt2::is_not_greater_equal(a0,a1));
-     }
-     
-   }
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::One<T>(), nt2::One<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Two<T>(), nt2::Two<T>()), nt2::False<r_t>());
+  NT2_TEST_EQUAL(is_not_greater_equal(nt2::Zero<T>(), nt2::Zero<T>()), nt2::False<r_t>());
 } // end of test for unsigned_int_
--- modules/predicates/unit/scalar/is_unord.cpp
+++ modules/predicates/unit/scalar/is_unord.cpp
@@ -12,7 +12,7 @@
 // Test behavior of predicates components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 21/02/2011
-/// modified by jt the 18/03/2011
+/// modified by jt the 13/04/2011
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -54,24 +54,6 @@ NT2_TEST_CASE_TPL ( is_unord_real__2_0,  NT2_REAL_TYPES)
   NT2_TEST_EQUAL(is_unord(nt2::Quarter<T>(), nt2::Quarter<T>()), nt2::False<r_t>());
   NT2_TEST_EQUAL(is_unord(nt2::Two<T>(), nt2::Two<T>()), nt2::False<r_t>());
   NT2_TEST_EQUAL(is_unord(nt2::Zero<T>(), nt2::Zero<T>()), nt2::False<r_t>());
-  // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(-10000), T(10000));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    T a1;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_EQUAL( nt2::is_unord(a0,a1),nt2::False<r_t>());
-     }
-     
-   }
 } // end of test for real_
 
 NT2_TEST_CASE_TPL ( is_unord_signed_int__2_0,  NT2_INTEGRAL_SIGNED_TYPES)
@@ -97,24 +79,6 @@ NT2_TEST_CASE_TPL ( is_unord_signed_int__2_0,  NT2_INTEGRAL_SIGNED_TYPES)
   NT2_TEST_EQUAL(is_unord(nt2::One<T>(), nt2::One<T>()), nt2::False<r_t>());
   NT2_TEST_EQUAL(is_unord(nt2::Two<T>(), nt2::Two<T>()), nt2::False<r_t>());
   NT2_TEST_EQUAL(is_unord(nt2::Zero<T>(), nt2::Zero<T>()), nt2::False<r_t>());
-  // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(-10000), T(10000));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    T a1;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_EQUAL( nt2::is_unord(a0,a1),nt2::False<r_t>());
-     }
-     
-   }
 } // end of test for signed_int_
 
 NT2_TEST_CASE_TPL ( is_unord_unsigned_int__2_0,  NT2_UNSIGNED_TYPES)
@@ -139,22 +103,4 @@ NT2_TEST_CASE_TPL ( is_unord_unsigned_int__2_0,  NT2_UNSIGNED_TYPES)
   NT2_TEST_EQUAL(is_unord(nt2::One<T>(), nt2::One<T>()), nt2::False<r_t>());
   NT2_TEST_EQUAL(is_unord(nt2::Two<T>(), nt2::Two<T>()), nt2::False<r_t>());
   NT2_TEST_EQUAL(is_unord(nt2::Zero<T>(), nt2::Zero<T>()), nt2::False<r_t>());
-  // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(-10000), T(10000));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    T a1;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_EQUAL( nt2::is_unord(a0,a1),nt2::False<r_t>());
-     }
-     
-   }
 } // end of test for unsigned_int_
