06f5490868c38da7a2172247adf6783f8df409d1
Mathias Gaunard <mathias@gaunard.com>
2011-04-26 20:18:16 +0200

    when making expression trees, take nodes by value except for terminals that are taken by reference or const-reference

--- modules/sdk/include/nt2/sdk/dsl/call.hpp
+++ modules/sdk/include/nt2/sdk/dsl/call.hpp
@@ -45,26 +45,95 @@ BOOST_PP_REPEAT_FROM_TO(1,BOOST_PP_INC(NT2_MAX_ARITY),M0,~)
 ////////////////////////////////////////////////////////////////////////////////
 // Defines the catch-all call for proto expression
 ////////////////////////////////////////////////////////////////////////////////
-#define M1(z,n,t) boost::cref(BOOST_PP_CAT(a,n))
+namespace nt2 { namespace details
+{
+    template<typename Expr, typename Enable = void>
+    struct is_terminal : boost::mpl::false_
+    {
+    };
 
-#define M0(z,n,t)                                     \
-template<class This,BOOST_PP_ENUM_PARAMS(n,class A)>  \
-struct result<This(BOOST_PP_ENUM_PARAMS(n,A))>        \
-{                                                     \
-  typedef typename boost::proto::result_of::          \
-  make_expr < Func                                    \
-            , BOOST_PP_ENUM_BINARY_PARAMS             \
-              ( n                                     \
-              , typename meta::strip<A                \
-              , >::type const& BOOST_PP_INTERCEPT     \
-              )                                       \
-            >::type type;                             \
-};                                                    \
-NT2_FUNCTOR_CALL(n)                                   \
-{                                                     \
-  return boost::proto::                               \
-  make_expr<Func>( BOOST_PP_ENUM(n,M1,~) );           \
-}                                                     \
+    template<typename Expr>
+    struct is_terminal< Expr
+                      , typename boost::
+                        enable_if< boost::is_same< typename boost::proto::tag_of<Expr>::type
+                                                 , boost::proto::tag::terminal
+                                                 >
+                                 >::type
+                      >
+      : boost::mpl::true_
+    {
+    };
+
+    
+    namespace result_of
+    {
+        template<typename Expr, typename Enable = void>
+        struct as_child
+          : nt2::meta::strip<Expr>
+        {
+        };
+
+        template<typename Expr>
+        struct as_child<Expr, typename boost::enable_if< is_terminal<Expr> >::type>
+        {
+            typedef Expr& type;
+        };
+    }
+
+    template<typename Expr>
+    typename boost::disable_if<
+        is_terminal<Expr>,
+        Expr
+    >::type
+    as_child(Expr const& expr)
+    {
+        return expr;
+    }
+
+    template<typename Expr>
+    typename boost::enable_if<
+        is_terminal<Expr>,
+        boost::reference_wrapper<Expr>
+    >::type
+    as_child(Expr& expr)
+    {
+        return boost::ref(expr);
+    }
+
+    template<typename Expr>
+    typename boost::enable_if<
+        is_terminal<Expr>,
+        boost::reference_wrapper<Expr const>
+    >::type
+    as_child(Expr const& expr)
+    {
+        return boost::cref(expr);
+    }
+
+} }
+
+#define M1(z,n,t) nt2::details::as_child(BOOST_PP_CAT(a,n))
+
+#define M0(z,n,t)                                               \
+template<class This,BOOST_PP_ENUM_PARAMS(n,class A)>            \
+struct result<This(BOOST_PP_ENUM_PARAMS(n,A))>                  \
+{                                                               \
+  typedef typename boost::proto::result_of::                    \
+  make_expr < Func                                              \
+            , BOOST_PP_ENUM_BINARY_PARAMS                       \
+              ( n                                               \
+              , typename nt2::details::result_of                \
+                ::as_child< typename meta::strip< A             \
+              ,                                 >::type const&  \
+                          >::type BOOST_PP_INTERCEPT            \
+              )                                                 \
+            >::type type;                                       \
+};                                                              \
+NT2_FUNCTOR_CALL(n)                                             \
+{                                                               \
+  return boost::proto::                                         \
+  make_expr<Func>( BOOST_PP_ENUM(n,M1,~) );                     \
+}                                                               \
 /**/
 
 namespace nt2 { namespace ext
