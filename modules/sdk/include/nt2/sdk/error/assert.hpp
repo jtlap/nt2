e8b4c238b69ff742b67c8fcbaa0e97a73ae1a1dc
Joel Falcou <joel.falcou@lri.fr>
2011-05-03 21:31:08 -0500

    Doxygenation of error/assert.hpp

--- modules/sdk/include/nt2/sdk/error/assert.hpp
+++ modules/sdk/include/nt2/sdk/error/assert.hpp
@@ -1,37 +1,53 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_ERROR_ASSERT_HPP_INCLUDED
 #define NT2_SDK_ERROR_ASSERT_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// Define macro for asserting preconditions
-////////////////////////////////////////////////////////////////////////////////
-#include <nt2/sdk/error/trap.hpp>
+/*!
+ * \file
+ * Defines macros and functions controlling the handling of runtiem assertions
+ */
 #include <nt2/sdk/error/debug.hpp>
-#include <nt2/sdk/error/error.hpp>
-#include <nt2/sdk/error/warning.hpp>
 
-////////////////////////////////////////////////////////////////////////////////
-// Prepare the cases where we need a special handler
-////////////////////////////////////////////////////////////////////////////////
+#if defined(DOXYGEN_ONLY)
+//==============================================================================
+/*!
+ * \ingroup error
+ * If NT2_ASSERTS_AS_EXCEPTIONS is defined, all runtime assertion will throw
+ * an instance of assert_exception instead of triggering a runtime assertion.
+ *
+ * \see NT2_DISABLE_ERROR
+ * \see NT2_DISABLE_ASSERTS
+ */
+//==============================================================================
+#define NT2_ASSERTS_AS_EXCEPTIONS
+#endif
 
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 // Make assertion into exceptions
-////////////////////////////////////////////////////////////////////////////////
-#if defined(NT2_ASSERTS_AS_EXCEPTIONS) && !defined(NT2_DISABLE_ERROR)
+//==============================================================================
+#if   defined(NT2_ASSERTS_AS_EXCEPTIONS) && !defined(NT2_DISABLE_ERROR) \
+  ||  defined(DOXYGEN_ONLY)
 
-NT2_WARNING(Assertion Failures throw exceptions)
+#include <nt2/sdk/error/error.hpp>
 
 namespace nt2 { namespace details { NT2_ERROR_INFO(assert_info, char const*); } }
 
 namespace nt2
 {
+  //============================================================================
+  /*!
+   * \ingroup error
+   * assert_exception is the exception thrown when a runtime assertion fails and
+   * NT2_ASSERTS_AS_EXCEPTIONS is defined.
+   */
+  //============================================================================
   struct assert_exception : virtual nt2::exception
   {
     virtual ~assert_exception() throw() {}
@@ -47,38 +63,56 @@ namespace nt2
 #define BOOST_ENABLE_ASSERT_HANDLER
 #endif
 
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 // Debug mode has SIGTRAP to the assertion
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 #if defined(NT2_DEBUG) && !defined(NT2_ASSERTS_AS_EXCEPTIONS)
-
-NT2_WARNING(Assertion Failures emit Debug Traps)
-
+#include <nt2/sdk/error/trap.hpp>
 #define BOOST_ENABLE_ASSERT_HANDLER
 #endif
 
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 // Include assert and forward the macro
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 #include <boost/assert.hpp>
+
+//==============================================================================
+/*!
+ * \ingroup error
+ * If NT2_DISABLE_ASSERTS is not defined, evaluates \c XPR and, if \c XPR
+ * evaluates to \c false, trigger a runtime assertion failure. If not, no
+ * operationsa re performed.
+ *
+ * \param XPR Expression to assert.
+ */
+//==============================================================================
 #define NT2_ASSERT(XPR) BOOST_ASSERT(XPR)
+
+//==============================================================================
+/*!
+ * \ingroup error
+ * Evaluates \c XPR and, if \c XPR evaluates to \c false, trigger a runtime
+ * assertion failure if and only if NT2_DISABLE_ASSERTS is not defined
+ *
+ * \param XPR Expression to verify.
+ */
+//==============================================================================
 #define NT2_VERIFY(XPR) BOOST_VERIFY(XPR)
 
-////////////////////////////////////////////////////////////////////////////////
-// Define the handler if needed
-////////////////////////////////////////////////////////////////////////////////
 #if defined(BOOST_ENABLE_ASSERT_HANDLER)
+#if defined(NT2_DEBUG)
 #include <cstdio>
-#include <nt2/sdk/config/attributes.hpp>
+#endif
+
 namespace boost
 {
-  void NT2_FORCE_INLINE
+  void inline
   assertion_failed(char const* expr,char const* fn,char const* f,int l)
   {
     #if defined(NT2_ASSERTS_AS_EXCEPTIONS) && !defined(NT2_DISABLE_ERROR)
     ::boost::exception_detail
-    ::throw_exception_(   nt2::assert_exception()
-                      <<  nt2::details::assert_info(expr)
+    ::throw_exception_(   ::nt2::assert_exception()
+                      <<  ::nt2::details::assert_info(expr)
                         , fn,f,l
                       );
     #elif defined(NT2_DEBUG)
