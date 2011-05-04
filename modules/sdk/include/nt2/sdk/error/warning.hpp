45dfaaecda081e81a1565a53ae652c759793543c
Joel Falcou <joel.falcou@lri.fr>
2011-05-03 23:26:54 -0500

    Doxygenation of error/warning.hpp

--- modules/sdk/include/nt2/sdk/error/warning.hpp
+++ modules/sdk/include/nt2/sdk/error/warning.hpp
@@ -1,45 +1,56 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_ERROR_WARNING_HPP_INCLUDED
 #define NT2_SDK_ERROR_WARNING_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// Compile-time warning
-// Documentation: http://nt2.lri.fr/sdk/error/warning.html
-////////////////////////////////////////////////////////////////////////////////
+/*!
+ * \file
+ * Defines macros to output custom warnign messages at compile time
+ */
+
 #include <boost/config.hpp>
 #include <boost/preprocessor/stringize.hpp>
 
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 // Activate only in VERBOSE MODE
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 #if defined(NT2_VERBOSE)
 #if defined(BOOST_MSVC)
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 // #pragma message on MSVC is very light so we add some info
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 #define NT2_WARNING_LOCATION __FILE__ "(" BOOST_PP_STRINGIZE(__LINE__) ") :"
 #define NT2_WARN(exp) ( NT2_WARNING_LOCATION "[NT2 WARNING] -- "  BOOST_PP_STRINGIZE(exp) )
 #define NT2_WARNING(X) __pragma( message NT2_WARN(X) )
 
 #elif defined(__GNUC__)
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 // #pragma message on g++ just need a call to _Pragma
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 #define NT2_WARN(exp) "[NT2 WARNING] -- " #exp
 #define NT2_WARNING(X) _Pragma( BOOST_PP_STRINGIZE(message NT2_WARN(X)) )
 #endif
 #else
-////////////////////////////////////////////////////////////////////////////////
-// In non-VERBOSE MODE, NT2_WARNING is no-op
-////////////////////////////////////////////////////////////////////////////////
-#define NT2_WARNING(X)
+//==============================================================================
+/*!
+ * \ingroup error
+ * Emits a custom warning message at compile by displaying \c X in a file/line
+ * localized message.
+ *
+ * \param MESSAGE Custom warning message.
+ *
+ * \par Example Usage:
+ *
+ * \include ../local.hpp
+ */
+//==============================================================================
+#define NT2_WARNING(MESSAGE)
 #endif
 
 #endif
