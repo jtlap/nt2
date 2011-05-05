06a0fc3538df10189ac5c5df868d585bccbab5f9
Joel Falcou <joel.falcou@lri.fr>
2011-05-05 01:00:44 -0500

    Doxygenation of functor hierarchy

--- modules/sdk/include/nt2/sdk/functor/meta/hierarchy.hpp
+++ modules/sdk/include/nt2/sdk/functor/meta/hierarchy.hpp
@@ -1,21 +1,45 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_FUNCTOR_META_HIERARCHY_HPP_INCLUDED
 #define NT2_SDK_FUNCTOR_META_HIERARCHY_HPP_INCLUDED
+
+/*!
+ * \file
+ * Defines the basic evaluation context for \ref functor
+ */
 
 #include <nt2/sdk/meta/hierarchy.hpp>
 
 namespace nt2 { namespace tag
 {
+#if defined(DOXYGEN_ONLY)
+  //============================================================================
+  /*!
+   * Defines the formal evaluation context for functors. This context is the
+   * most abstract one and correspond to high-level code trasnformation function
+   */
+  //============================================================================
+  struct formal_ : meta::unspecified_<formal_> {};
+
+  //============================================================================
+  /*!
+   * Defines the CPU based evaluation context for functors. This context is used
+   * when no specific architecture informations is available or required by a
+   * \ref functor. Functors specialization under the \c cpu_ context is usually
+   * used as common, architecture independant implementation.
+   */
+  //============================================================================
+  struct cpu_.: formal_ {};
+#else
   NT2_HIERARCHY_CLASS(formal_, meta::unspecified_<formal_>);
   NT2_HIERARCHY_CLASS(cpu_, formal_);
-}
-}
+#endif
+} }
 
 #endif
