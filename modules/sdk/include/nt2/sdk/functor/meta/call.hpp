347405e82acc602c38c26e7dcbb060829ddb5a73
Joel Falcou <joel.falcou@lri.fr>
2011-05-05 01:00:22 -0500

    Doxygenation of meta::call

--- modules/sdk/include/nt2/sdk/functor/meta/call.hpp
+++ modules/sdk/include/nt2/sdk/functor/meta/call.hpp
@@ -1,18 +1,18 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_FUNCTOR_META_CALL_HPP_INCLUDED
 #define NT2_SDK_FUNCTOR_META_CALL_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// Computes the return type of a given functor with a given list of arguments
-// TODO: Documentation: http://nt2.lri.fr/sdk/functor/meta/call.html
-////////////////////////////////////////////////////////////////////////////////
+/*!
+ * \file
+ */
+
 #include <boost/config.hpp>
 #include <boost/tr1/functional.hpp>
 #include <nt2/sdk/functor/functor.hpp>
@@ -25,7 +25,32 @@
 
 namespace nt2 { namespace meta
 {
-  template<class Signature, class Site = tag::cpu_> struct call;
+  //============================================================================
+  /*!
+   * \ref call is a \metafunction that, for a given \c Tag, \c EvalContext and
+   * argument lists \c ...Args, computes the return type of
+   * functor<Tag,EvalContext>::operator()(Args...).
+   *
+   * \param Signature function type build from Tag(Args...)
+   * \param EvalContext evaluation context to use for type deduction
+   *
+   * \par Models:
+   * \metafunction
+   *
+   * \par Semantics:
+   * For a given \c Tag, \c EvalContext and argument lists \c ...Args,
+   *
+   * \code
+   * typedef call<Tag(Args...),EvalContext>::type type;
+   * \endcode
+   *
+   * is equivalent to:
+   * \code
+   * typedef decltype(functor<Tag,EvalContext>(declval<Args...>())) type;
+   * \endcode
+   */
+  //============================================================================
+  template<class Signature, class EvalContext = tag::cpu_> struct call {};
 
 #if defined(BOOST_HAS_VARIADIC_TMPL)
   template<class Tag, class... Args, class Site>
