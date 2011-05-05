4882cfaf5586481eb5352f3f900b0fda9d40a12a
Jt lapreste <jtlapreste@gmail.com>
2011-05-05 16:17:38 +0200

    amelioration of rshl/r in simd

--- modules/bitwise/include/nt2/toolbox/bitwise/function/simd/common/rshl.hpp
+++ modules/bitwise/include/nt2/toolbox/bitwise/function/simd/common/rshl.hpp
@@ -10,6 +10,9 @@
 #define NT2_TOOLBOX_BITWISE_FUNCTION_SIMD_COMMON_RSHL_HPP_INCLUDED
 #include <nt2/sdk/meta/size.hpp>
 #include <nt2/sdk/meta/strip.hpp>
+#include <nt2/include/functions/shli.hpp>
+#include <nt2/include/functions/shri.hpp>  
+#include <nt2/include/functions/is_gtz.hpp>  
 
 
 
@@ -44,6 +47,34 @@ namespace nt2 { namespace ext
   };
 } }
 
+/////////////////////////////////////////////////////////////////////////////
+// Implementation when type  is arithmetic_ and A1 is scalar
+/////////////////////////////////////////////////////////////////////////////
+NT2_REGISTER_DISPATCH(tag::rshl_, tag::cpu_,
+		      (A0)(A1)(X),
+		      ((simd_<arithmetic_<A0>,X>))
+		      ((integer_<A1>))
+		      );
+
+namespace nt2 { namespace ext
+{
+  template<class X, class Dummy>
+  struct call<tag::rshl_(tag::simd_<tag::arithmetic_, X> ,
+                         tag::integer_ ),
+              tag::cpu_, Dummy> : callable
+  {
+    template<class Sig> struct result;
+    template<class This,class A0,class A1>
+    struct result<This(A0,A1)>
+      : meta::strip<A0>{};//
+
+    NT2_FUNCTOR_CALL(2)
+    {
+      if (is_gtz(a1)) return shli(a0, a1);  else return shri(a0, -a1); 
+    }
+
+  };
+} }
 #endif
 // modified by jt the 04/01/2011
 // modified manually by jt the 06/01/2011
