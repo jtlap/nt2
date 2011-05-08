072f1724ea570ad494f4030ec7e80c5c78e855c7
Joel Falcou <joel.falcou@lri.fr>
2011-05-08 16:11:25 -0500

    Updated any documentation

--- modules/sdk/examples/meta/any.cpp
+++ modules/sdk/examples/meta/any.cpp
@@ -1,18 +1,19 @@
-#include <cstdio>
+#include <iostream>
 #include <boost/mpl/vector.hpp>
 #include <nt2/sdk/meta/any.hpp>
 #include <boost/type_traits/is_same.hpp>
 
+using namespace std;
 using namespace boost;
 using namespace boost::mpl;
 
 int main()
 {
-  typedef vector<int,bool,float> seq_t;
+  cout  << "Any bool in vector<int,bool,float> :"
+        << nt2::meta::any<is_same<_,bool>, vector<int,bool,float> >::value
+        << "\n";
 
-  typedef nt2::meta::any<is_same<_,bool>,seq_t> any_seq;
-  typedef nt2::meta::any<is_same<_,bool>,int,bool,float> any_types;
-
-  printf("is any type bool in vector<int,bool,float> : %d\n", any_seq::value);
-  printf("is any type bool in (int,bool,float) : %d\n", any_types::value);
+  cout  << "Any bool in {int,char,float} :"
+        << nt2::meta::any<is_same<_,bool>, int,char,float>::value
+        << "\n";
 }
