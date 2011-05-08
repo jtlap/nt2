9ed58e000b97c10a5b41820f0dd01997f796ff31
Joel Falcou <joel.falcou@lri.fr>
2011-05-08 12:47:24 -0500

    Added \mainpage for nt2.hpp

--- modules/sdk/include/nt2/nt2.hpp
+++ modules/sdk/include/nt2/nt2.hpp
@@ -1,22 +1,23 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_NT2_HPP_INCLUDED
 #define NT2_NT2_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-/*!
-  Provides direct access to all the base components of nt2 that makes it
-  behaves like Matlab. Note that it also greatly increases compilation times.
-  If you need to compile faster, you can decide to include the smallest set of
-  components your code requires by using the files in the nt2/include/ directory.
- **/
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+/*! \mainpage NT2 Reference Manual
+ *
+ * The objective of this documentation is to give a developper oriented look at
+ * NT2 code by providing a detailed reference of all its components. If you're
+ * looking for a nore user friendly documentation with tutorial and how-to's, we
+ * recommend you to look at the main user manual.
+ */
+//==============================================================================
 #include <nt2/include/config.hpp>
 #include <nt2/include/error.hpp>
 #include <nt2/include/timing.hpp>
