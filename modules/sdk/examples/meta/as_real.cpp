8cfda4726401e56fcf98342b806299287df1553d
Joel Falcou <joel.falcou@lri.fr>
2011-05-08 16:12:10 -0500

    Added documentation for as_real

--- modules/sdk/examples/meta/as_real.cpp
+++ modules/sdk/examples/meta/as_real.cpp
@@ -4,18 +4,16 @@
 #include <nt2/sdk/meta/as_real.hpp>
 #include <boost/type_traits/is_same.hpp>
 
-using boost::is_same;
-using nt2::meta::as_real;
-
 using nt2::int32_t;
 using nt2::uint32_t;
 using nt2::uint64_t;
+using boost::is_same;
 using nt2::simd::pack;
 
 int main()
 {
-  BOOST_MPL_ASSERT(( is_same<float,       as_real<int32_t>::type>         ));
-  BOOST_MPL_ASSERT(( is_same<float,       as_real<uint32_t>::type>        ));
-  BOOST_MPL_ASSERT(( is_same<double,      as_real<uint64_t>::type>        ));
-  BOOST_MPL_ASSERT(( is_same<pack<float>, as_real< pack<int32_t> >::type> ));
+  BOOST_MPL_ASSERT(( is_same<float,       nt2::meta::as_real<int32_t>::type>         ));
+  BOOST_MPL_ASSERT(( is_same<float,       nt2::meta::as_real<uint32_t>::type>        ));
+  BOOST_MPL_ASSERT(( is_same<double,      nt2::meta::as_real<uint64_t>::type>        ));
+  BOOST_MPL_ASSERT(( is_same<pack<float>, nt2::meta::as_real<pack<int32_t> >::type> ));
 }
