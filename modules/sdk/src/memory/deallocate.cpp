651981e0276ec8605f47f6f06af185413bfc212b
Joel Falcou <joel.falcou@lri.fr>
2011-05-14 01:07:15 +0200

    Removed spurious include in memory source files for alocate et al. to improve compile times

--- modules/sdk/src/memory/deallocate.cpp
+++ modules/sdk/src/memory/deallocate.cpp
@@ -6,7 +6,6 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#include <cstdlib>
 #include <nt2/sdk/memory/forward.hpp>
 
 namespace nt2 { namespace memory
