94740e0d7e913d3e790150389dbaf321b5170e94
Mathias Gaunard <mathias@gaunard.com>
2011-05-14 14:52:42 +0200

    refactor constant and operators out of sdk

---
+++ modules/operators/include/nt2/toolbox/operators/operators.hpp
@@ -0,0 +1,47 @@
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_TOOLBOX_OPERATORS_OPERATORS_HPP_INCLUDED
+#define NT2_TOOLBOX_OPERATORS_OPERATORS_HPP_INCLUDED
+
+#include <nt2/toolbox/operators/include/is_less_equal.hpp>
+#include <nt2/toolbox/operators/include/multiplies.hpp>
+#include <nt2/toolbox/operators/include/divides.hpp>
+#include <nt2/toolbox/operators/include/compare_equal.hpp>
+#include <nt2/toolbox/operators/include/modulo.hpp>
+#include <nt2/toolbox/operators/include/compare_greater.hpp>
+#include <nt2/toolbox/operators/include/load.hpp>
+#include <nt2/toolbox/operators/include/is_less.hpp>
+#include <nt2/toolbox/operators/include/logical_or.hpp>
+#include <nt2/toolbox/operators/include/compare_not_equal.hpp>
+#include <nt2/toolbox/operators/include/logical_and.hpp>
+#include <nt2/toolbox/operators/include/unary_minus.hpp>
+#include <nt2/toolbox/operators/include/is_greater.hpp>
+//#include <nt2/toolbox/operators/include/make.hpp>
+#include <nt2/toolbox/operators/include/minus.hpp>
+#include <nt2/toolbox/operators/include/store.hpp>
+#include <nt2/toolbox/operators/include/map.hpp>
+#include <nt2/toolbox/operators/include/bitwise_xor.hpp>
+#include <nt2/toolbox/operators/include/is_equal.hpp>
+#include <nt2/toolbox/operators/include/unary_plus.hpp>
+#include <nt2/toolbox/operators/include/plus.hpp>
+#include <nt2/toolbox/operators/include/bitwise_and.hpp>
+#include <nt2/toolbox/operators/include/compare_less_equal.hpp>
+#include <nt2/toolbox/operators/include/comma.hpp>
+#include <nt2/toolbox/operators/include/bitwise_or.hpp>
+#include <nt2/toolbox/operators/include/shift_right.hpp>
+#include <nt2/toolbox/operators/include/compare_less.hpp>
+#include <nt2/toolbox/operators/include/compare_greater_equal.hpp>
+#include <nt2/toolbox/operators/include/complement.hpp>
+#include <nt2/toolbox/operators/include/is_not_equal.hpp>
+#include <nt2/toolbox/operators/include/logical_not.hpp>
+#include <nt2/toolbox/operators/include/is_greater_equal.hpp>
+#include <nt2/toolbox/operators/include/shift_left.hpp>
+#include <nt2/toolbox/operators/include/if_else.hpp>
+
+#endif
