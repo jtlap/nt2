54c42b561cabf641b83812403567387fc7d3c6cc
Joel Falcou <joel.falcou@lri.fr>
2011-05-17 15:27:45 -0600

    Documentation for as_

--- modules/sdk/include/nt2/sdk/meta/as.hpp
+++ modules/sdk/include/nt2/sdk/meta/as.hpp
@@ -1,36 +1,53 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_META_AS_HPP_INCLUDED
 #define NT2_SDK_META_AS_HPP_INCLUDED
 
+/*!
+ * \file
+ * Defines and implement the as_ generic type wrapper
+ */
+
 #include <nt2/sdk/meta/hierarchy_of.hpp>
 
-////////////////////////////////////////////////////////////////////////////////
-// Type wrapper that acts *as* the type it boxes
-//* TODO: Documentation: http://nt2.lri.fr/sdk/meta/helpers/as.html
-////////////////////////////////////////////////////////////////////////////////
+#if defined(DOXYGEN_ONLY)
 namespace nt2 { namespace meta
 {
-  template<class T> struct as_ { typedef T type; };
+  //============================================================================
+  /*!
+   * \ingroup functors
+   * Type wrapper hierarchy.
+   *
+   * \tparam T Wrapped type hierarchy
+   */
+  //============================================================================
+  template<class T> struct target_ {};
 } }
+#endif
 
-////////////////////////////////////////////////////////////////////////////////
-// Register the const_ hierarchy an tie it to meta::as_
-////////////////////////////////////////////////////////////////////////////////
 NT2_REGISTER_HIERARCHY(target_)
 
-namespace nt2 { namespace details
+namespace nt2 { namespace meta
 {
-  template<class T>
-  struct hierarchy_of< meta::as_<T> >
+  //============================================================================
+  /*!
+   * \ingroup metafunctions
+   * No overhead type wrapper.Some implementation details of \nt2 require types
+   * to be wrapped into an empty structure so the type can be passed as a no
+   * overhead instance. \ref as_ performs such a wrapping and tag itself as
+   * belonging to the \ref target_ hierarchy.
+   */
+  //============================================================================
+  template<class T> struct as_
   {
-    typedef meta::target_<typename meta::hierarchy_of<T>::type > type;
+    typedef T                                         type;
+    typedef target_<typename hierarchy_of<T>::type >  nt2_hierarchy_tag;
   };
 } }
 
