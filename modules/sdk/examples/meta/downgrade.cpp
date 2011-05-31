743caa43e9a41d9385ea4542649430b553f27670
Joel Falcou <joel.falcou@lri.fr>
2011-05-30 23:29:15 -0500

    Modified downgrade to use the details/ folder and update documentation.

--- modules/sdk/examples/meta/downgrade.cpp
+++ modules/sdk/examples/meta/downgrade.cpp
@@ -1,21 +1,15 @@
 #include <boost/mpl/assert.hpp>
+#include <nt2/sdk/simd/pack.hpp>
 #include <nt2/sdk/config/types.hpp>
 #include <nt2/sdk/meta/downgrade.hpp>
-#include <nt2/sdk/simd/pack.hpp>
 #include <boost/type_traits/is_same.hpp>
 
 using boost::is_same;
-using boost::mpl::_;
-using nt2::meta::downgrade;
-
-using nt2::uint16_t;
-using nt2::uint8_t;
-using nt2::simd::pack;
 
 int main()
 {
-  BOOST_MPL_ASSERT(( is_same<uint8_t,        downgrade<uint16_t>::type>          ));
-  BOOST_MPL_ASSERT(( is_same<int8_t,         downgrade<uint8_t, signed>::type>   ));
-  BOOST_MPL_ASSERT(( is_same<float,          downgrade<double>::type>            ));
-  BOOST_MPL_ASSERT(( is_same<pack<float, 4>, downgrade< pack<double, 2> >::type> ));
+  BOOST_MPL_ASSERT(( is_same<nt2::uint8_t             , nt2::meta::downgrade<uint16_t>::type>                     ));
+  BOOST_MPL_ASSERT(( is_same<nt2::int8_t              , nt2::meta::downgrade<uint8_t, signed>::type>              ));
+  BOOST_MPL_ASSERT(( is_same<float                    , nt2::meta::downgrade<double>::type>                        ));
+  BOOST_MPL_ASSERT(( is_same<nt2::simd::pack<float, 4>, nt2::meta::downgrade< nt2::simd::pack<double, 2> >::type> ));
 }
