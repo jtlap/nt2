0bef0a42357ae0ecf950a10cfd283fc8692a3388
Joel Falcou <joel.falcou@lri.fr>
2011-05-30 23:29:15 -0500

    Modified downgrade to use the details/ folder and update documentation.

--- modules/sdk/include/nt2/sdk/meta/downgrade.hpp
+++ modules/sdk/include/nt2/sdk/meta/downgrade.hpp
@@ -1,74 +1,94 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_META_DOWNGRADE_HPP_INCLUDED
 #define NT2_SDK_META_DOWNGRADE_HPP_INCLUDED
 
-//////////////////////////////////////////////////////////////////////////////
-// Returns the input type rebound with a type that is twice as small
-// as its primitive, with optional sign.
-// If the primitive type is floating-point, sign is ignored.
-// See: http://nt2.metascale.org/sdk/meta/traits/downgrade.html
-//////////////////////////////////////////////////////////////////////////////
+/*!
+ * \file
+ * \brief Defines and implements the \ref nt2::meta::downgrade \metafunction
+ */
+
+#include <boost/mpl/apply.hpp>
 #include <nt2/sdk/meta/strip.hpp>
 #include <nt2/sdk/meta/sign_of.hpp>
+#include <nt2/sdk/meta/factory_of.hpp>
 #include <nt2/sdk/meta/make_integer.hpp>
 #include <nt2/sdk/meta/primitive_of.hpp>
-#include <nt2/sdk/meta/factory_of.hpp>
 #include <nt2/sdk/meta/is_fundamental.hpp>
-#include <boost/mpl/apply.hpp>
-
-namespace nt2 { namespace details
-{
-  //////////////////////////////////////////////////////////////////////////////
-  // Scalar arithmetic types are downgraded using make_integer unless they're
-  // floating point types
-  //////////////////////////////////////////////////////////////////////////////
-  template<class T, std::size_t Size, class Sign, class Lambda>
-  struct downgrade : meta::make_integer<Size/2,Sign,Lambda>
-  {};
-
-  template<class Sign, class Lambda>
-  struct downgrade<double,sizeof(double),Sign, Lambda>
-  {
-    typedef typename boost::mpl::apply1<Lambda, float>::type type;
-  };
-
-  template<class Sign, class Lambda>
-  struct downgrade<float,sizeof(float),Sign, Lambda>
-  {
-    typedef typename boost::mpl::apply1<Lambda, float>::type type;
-  };
-
-  //////////////////////////////////////////////////////////////////////////////
-  // If type size is 1, return the type itself for any category
-  //////////////////////////////////////////////////////////////////////////////
-  template<class T, class Sign, class Lambda>
-  struct  downgrade<T,1,Sign, Lambda>
-        : meta::make_integer<1,Sign, Lambda> {};
-} }
+#include <nt2/sdk/meta/details/downgrade.hpp>
 
 namespace nt2 { namespace meta
 {
-  template< class T
-          , class S=typename meta::sign_of<typename meta::strip<T>::type>::type
-          >
+  //============================================================================
+  /*!
+   * Returns the input type rebound with a type that is twice as small as
+   * its primitive, with optional sign change.\
+   *
+   * \tparam Type Hierarchizable type to downgrade.
+   * \tparam Sign Optional sign to use while downgrading \c Type. If unspecified,
+   * \c Sign is equals to the sign of \c Type.
+   *
+   * \par Models:
+   *
+   * \metafunction
+   *
+   * \par Semantic:
+   *
+   * \code
+   * typedef nt2::meta::downgrade<T, Sign>::type r;
+   * \endcode
+   *
+   * is equivalent to:
+   *
+   * \code
+   * typedef make_real< max< sizeof(primitive_of<T>::type)/2
+   *                       , sizeof(float)
+   *                       >::value
+   *                  , factory_of<T>::type
+   *                  >::type                                  r;
+   * \endcode
+   *
+   * if \c primitive_of<T> is of hierarchy \ref nt2::tag::real_ and to:
+   *
+   * \code
+   * typedef make_integer< max<sizeof(primitive_of<T>::type)/2, 1>::value
+   *                     , Sign
+   *                     , factory_of<T>::type
+   *                     >::type                                r;
+   * \endcode
+   *
+   * otherwise.
+   *
+   * \usage
+   *
+   * \include downgrade.cpp
+   */
+  //============================================================================
+  template< class Type, class Sign>
   struct  downgrade
-        : details::downgrade< typename meta::primitive_of<typename meta::strip<T>::type>::type
-                            , sizeof(typename meta::primitive_of<typename meta::strip<T>::type>::type)
-                            , S
-                            , typename meta::factory_of<typename meta::strip<T>::type>::type
-                            >
+        : details::downgrade_impl
+          < typename meta::primitive_of<typename meta::strip<Type>::type>::type
+          , sizeof(typename meta::primitive_of
+                            < typename meta::strip<Type>::type >::type
+                  )
+          , Sign
+          , typename meta::factory_of<typename meta::strip<Type>::type>::type
+          >
   {
-    NT2_STATIC_ASSERT ( (is_fundamental<typename meta::primitive_of<typename meta::strip<T>::type>::type>::value)
-                      , NT2_NON_FUNDAMENTAL_PRIMITIVE_USED_IN_META_DOWNGRADE
-                      , "A type with a non-fundamental primitive is used in nt2::meta::downgrade."
-                      );
+    NT2_STATIC_ASSERT
+    ( ( is_fundamental
+        < typename meta::primitive_of<typename meta::strip<T>::type>::type
+        >::value
+      )
+    , NT2_NON_FUNDAMENTAL_PRIMITIVE_USED_IN_META_DOWNGRADE
+    , "A type with a non-fundamental primitive is used in nt2::meta::downgrade"
+    );
   };
 
 } }
