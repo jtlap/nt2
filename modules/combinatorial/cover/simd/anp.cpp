53c9c22095dcdd9ed43cdb7f35a30166e9e30fcc
Jt lapreste <jtlapreste@gmail.com>
2011-06-05 22:03:04 +0200

    coverage for combinatorial

---
+++ modules/combinatorial/cover/simd/anp.cpp
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#define NT2_UNIT_MODULE "nt2 combinatorial toolbox - anp/simd Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+//  $testcat$ test behavior of combinatorial components in simd mode
+//////////////////////////////////////////////////////////////////////////////
+/// created by jt the 30/11/2010
+/// modified by jt the 05/06/2011
+#include <nt2/sdk/memory/is_aligned.hpp>
+#include <nt2/sdk/memory/aligned_type.hpp>
+#include <nt2/include/functions/load.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/include/constants/real.hpp>
+#include <nt2/include/constants/infinites.hpp>
+#include <nt2/include/functions/max.hpp>
+#include <nt2/toolbox/combinatorial/include/anp.hpp>
+
+NT2_TEST_CASE_TPL ( anp_real__2_0,  NT2_REAL_TYPES)
+{
+  using nt2::anp;
+  using nt2::tag::anp_;
+  using nt2::load; 
+  using nt2::simd::native;
+  using nt2::meta::cardinal_of;
+  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef typename nt2::meta::upgrade<T>::type   u_t;
+  typedef native<T,ext_t>                        n_t;
+  typedef n_t                                     vT;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef native<iT,ext_t>                       ivT;
+  typedef typename nt2::meta::call<anp_(vT,vT)>::type r_t;
+  typedef typename nt2::meta::call<anp_(T,T)>::type sr_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
+  double ulpd;
+  ulpd=0.0;
+
+} // end of test for real_
