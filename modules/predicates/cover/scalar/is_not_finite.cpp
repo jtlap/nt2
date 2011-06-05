8aef35ce89952422a14eee4e0df613ddaf160550
Jt lapreste <jtlapreste@gmail.com>
2011-06-05 17:10:05 +0200

    coverage for predicates

---
+++ modules/predicates/cover/scalar/is_not_finite.cpp
@@ -0,0 +1,128 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#define NT2_UNIT_MODULE "nt2 predicates toolbox - is_not_finite/scalar Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// $testcat$ test behavior of predicates components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+/// created  by jt the 21/02/2011
+/// modified by jt the 05/06/2011
+#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
+#include <nt2/include/constants/real.hpp>
+#include <nt2/include/constants/infinites.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/toolbox/predicates/include/is_not_finite.hpp>
+// specific includes for arity 1 tests
+#include <nt2/sdk/meta/logical.hpp>
+
+NT2_TEST_CASE_TPL ( is_not_finite_real__1_0,  NT2_REAL_TYPES)
+{
+  
+  using nt2::is_not_finite;
+  using nt2::tag::is_not_finite_;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef typename nt2::meta::call<is_not_finite_(T)>::type r_t;
+  typedef typename nt2::meta::upgrade<T>::type u_t;
+  typedef typename nt2::meta::logical<T>::type wished_r_t;
+
+
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+  ulpd=0.0;
+
+  // random verifications
+  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  {
+    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    T a0;
+    for (uint32_t j =0; j < NR; ++j )
+      {
+        std::cout << "for param "
+                  << "  a0 = "<< u_t(a0 = tab_a0[j])
+                  << std::endl;
+        NT2_TEST_EQUAL( nt2::is_not_finite(a0),nt2::False<r_t>());
+     }
+     
+   }
+} // end of test for real_
+
+NT2_TEST_CASE_TPL ( is_not_finite_signed_int__1_0,  NT2_INTEGRAL_SIGNED_TYPES)
+{
+  
+  using nt2::is_not_finite;
+  using nt2::tag::is_not_finite_;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef typename nt2::meta::call<is_not_finite_(T)>::type r_t;
+  typedef typename nt2::meta::upgrade<T>::type u_t;
+  typedef typename nt2::meta::logical<T>::type wished_r_t;
+
+
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+  ulpd=0.0;
+
+  // random verifications
+  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  {
+    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    T a0;
+    for (uint32_t j =0; j < NR; ++j )
+      {
+        std::cout << "for param "
+                  << "  a0 = "<< u_t(a0 = tab_a0[j])
+                  << std::endl;
+        NT2_TEST_EQUAL( nt2::is_not_finite(a0),nt2::False<r_t>());
+     }
+     
+   }
+} // end of test for signed_int_
+
+NT2_TEST_CASE_TPL ( is_not_finite_unsigned_int__1_0,  NT2_UNSIGNED_TYPES)
+{
+  
+  using nt2::is_not_finite;
+  using nt2::tag::is_not_finite_;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef typename nt2::meta::call<is_not_finite_(T)>::type r_t;
+  typedef typename nt2::meta::upgrade<T>::type u_t;
+  typedef typename nt2::meta::logical<T>::type wished_r_t;
+
+
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+  ulpd=0.0;
+
+  // random verifications
+  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  {
+    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    T a0;
+    for (uint32_t j =0; j < NR; ++j )
+      {
+        std::cout << "for param "
+                  << "  a0 = "<< u_t(a0 = tab_a0[j])
+                  << std::endl;
+        NT2_TEST_EQUAL( nt2::is_not_finite(a0),nt2::False<r_t>());
+     }
+     
+   }
+} // end of test for unsigned_int_
---
+++ modules/predicates/cover/simd/is_not_infinite.cpp
@@ -0,0 +1,65 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#define NT2_UNIT_MODULE "nt2 predicates toolbox - is_not_infinite/simd Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+//  $testcat$ test behavior of predicates components in simd mode
+//////////////////////////////////////////////////////////////////////////////
+/// created  by jt the 21/02/2011
+/// modified by jt the 05/06/2011
+#include <nt2/sdk/memory/is_aligned.hpp>
+#include <nt2/sdk/memory/aligned_type.hpp>
+#include <nt2/include/functions/load.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/include/constants/real.hpp>
+#include <nt2/include/constants/infinites.hpp>
+#include <nt2/include/functions/max.hpp>
+#include <nt2/toolbox/predicates/include/is_not_infinite.hpp>
+
+NT2_TEST_CASE_TPL ( is_not_infinite_real__1_0,  NT2_REAL_TYPES)
+{
+  using nt2::is_not_infinite;
+  using nt2::tag::is_not_infinite_;
+  using nt2::load; 
+  using nt2::simd::native;
+  using nt2::meta::cardinal_of;
+  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef typename nt2::meta::upgrade<T>::type   u_t;
+  typedef native<T,ext_t>                        n_t;
+  typedef n_t                                     vT;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef native<iT,ext_t>                       ivT;
+  typedef typename nt2::meta::call<is_not_infinite_(vT)>::type r_t;
+  typedef typename nt2::meta::call<is_not_infinite_(T)>::type sr_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
+  double ulpd;
+  ulpd=0.0;
+
+  // random verifications
+  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  {
+    NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    for(uint32_t j = 0; j < NR/cardinal_of<n_t>::value; j++)
+      {
+        vT a0 = load<vT>(&tab_a0[0],j);
+        r_t v = is_not_infinite(a0);
+        for(int i = 0; i< cardinal_of<n_t>::value; i++)
+        {
+          int k = i+j*cardinal_of<n_t>::value;
+          NT2_TEST_EQUAL( v[i]!=0,ssr_t(nt2::is_not_infinite(tab_a0[k])));
+        }
+      }
+    
+  }
+} // end of test for real_
