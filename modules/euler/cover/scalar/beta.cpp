b707ba40b337179ee527d87717e8682888e69367
Jt lapreste <jtlapreste@gmail.com>
2011-06-06 19:50:11 +0200

    cover for euler

---
+++ modules/euler/cover/scalar/beta.cpp
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#define NT2_UNIT_MODULE "nt2 euler toolbox - beta/scalar Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// $testcat$ test behavior of euler components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+/// created  by jt the 22/02/2011
+/// modified by jt the 06/06/2011
+#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
+#include <nt2/include/constants/real.hpp>
+#include <nt2/include/constants/infinites.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/toolbox/euler/include/beta.hpp>
+
+NT2_TEST_CASE_TPL ( beta_real__2_0,  NT2_REAL_TYPES)
+{
+  
+  using nt2::beta;
+  using nt2::tag::beta_;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef typename nt2::meta::call<beta_(T,T)>::type r_t;
+  typedef typename nt2::meta::upgrade<T>::type u_t;
+  typedef typename boost::result_of<nt2::meta::floating(T)>::type wished_r_t;
+
+
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+  ulpd=0.0;
+
+  // random verifications
+  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  {
+    NT2_CREATE_BUF(tab_a0,T, NR, nt2::Eps<T>(), T(1));
+    NT2_CREATE_BUF(tab_a1,T, NR, nt2::Eps<T>(), T(1));
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    T a0;
+    T a1;
+    for (uint32_t j =0; j < NR; ++j )
+      {
+        std::cout << "for params "
+                  << "  a0 = "<< u_t(a0 = tab_a0[j])
+                  << ", a1 = "<< u_t(a1 = tab_a1[j])
+                  << std::endl;
+        NT2_TEST_ULP_EQUAL( nt2::beta(a0,a1),nt2::beta(a0,a1),0.5);
+        ulp0=nt2::max(ulpd,ulp0);
+     }
+     std::cout << "max ulp found is: " << ulp0 << std::endl;
+   }
+} // end of test for real_
