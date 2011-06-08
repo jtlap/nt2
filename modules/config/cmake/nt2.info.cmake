645cf6b2faf9ea8deae8158eb28865a6c3067b2a
Mathias Gaunard <mathias@gaunard.com>
2011-06-08 17:04:54 +0200

    fix bug in nt2.info that didn't define NT2_COMPILER_MSVC correctly

--- modules/config/cmake/nt2.info.cmake
+++ modules/config/cmake/nt2.info.cmake
@@ -24,42 +24,42 @@ ENDIF(WIN32)
 ################################################################################
 # ARM processor
 ################################################################################
-IF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm*")
+IF(CMAKE_SYSTEM_PROCESSOR MATCHES "arm*")
   SET(NT2_ARCH "ARM")
   SET(NT2_ARCH_ARM 1)
 ################################################################################
 # x86 family processor, 64-bits
 ################################################################################
-ELSEIF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "amd64*")
+ELSEIF(CMAKE_SYSTEM_PROCESSOR MATCHES "amd64*")
   SET(NT2_ARCH "amd64")
   SET(NT2_ARCH_X86 1)
   SET(NT2_ARCH_X86_64 1)
   SET(NT2_ARCH_AMD 1)
   SET(NT2_ARCH_AMD64 1)
-ELSEIF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "x86_64*")
+ELSEIF(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64*")
   SET(NT2_ARCH "x86_64")
   SET(NT2_ARCH_X86 1)
   SET(NT2_ARCH_X86_64 1)
 ################################################################################
 # x86 family processor
 ################################################################################
-ELSEIF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "amd*")
+ELSEIF(CMAKE_SYSTEM_PROCESSOR MATCHES "amd*")
   SET(NT2_ARCH "amd")
   SET(NT2_ARCH_X86 1)
   SET(NT2_ARCH_AMD 1)
-ELSEIF(  ${CMAKE_SYSTEM_PROCESSOR} MATCHES "i[3-9]86*"
-      OR ${CMAKE_SYSTEM_PROCESSOR} MATCHES "x86*"
+ELSEIF(  CMAKE_SYSTEM_PROCESSOR MATCHES "i[3-9]86*"
+      OR CMAKE_SYSTEM_PROCESSOR MATCHES "x86*"
       )
   SET(NT2_ARCH "x86")
   SET(NT2_ARCH_X86 1)
 ################################################################################
 # Power PC processor
 ################################################################################
-ELSEIF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "ppc64*")
+ELSEIF(CMAKE_SYSTEM_PROCESSOR MATCHES "ppc64*")
   SET(NT2_ARCH "PowerPC 64")
   SET(NT2_ARCH_POWERPC 1)
   SET(NT2_ARCH_POWERPC_64 1)
-ELSEIF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "powerpc*")
+ELSEIF(CMAKE_SYSTEM_PROCESSOR MATCHES "powerpc*")
   SET(NT2_ARCH "PowerPC")
   SET(NT2_ARCH_POWERPC 1)
 ENDIF()
@@ -69,5 +69,5 @@ IF(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
   SET(NT2_COMPILER_GNU_C 1)
 ELSEIF(MSVC)
   SET(NT2_COMPILER "Microsoft Visual C++")
-  SET(NT2_COMPILER_MSVC)
+  SET(NT2_COMPILER_MSVC 1)
 ENDIF()
