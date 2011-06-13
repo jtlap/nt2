1fc72598dc91768d3463f426b0269b0fe413ac85
Joel Falcou <joel.falcou@lri.fr>
2011-06-13 17:31:26 -0500

    Added documentation to basic Unit Tests facility

--- modules/sdk/include/nt2/sdk/unit/tests/basic.hpp
+++ modules/sdk/include/nt2/sdk/unit/tests/basic.hpp
@@ -1,22 +1,27 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_UNIT_TESTS_BASIC_HPP_INCLUDED
 #define NT2_SDK_UNIT_TESTS_BASIC_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// Basic tests
-////////////////////////////////////////////////////////////////////////////////
+/*!
+ * \file
+ * \brief Unit Test macros for basic checks and forced failure
+ */
+
 #include <nt2/sdk/unit/details/stats.hpp>
 
-////////////////////////////////////////////////////////////////////////////////
-// Evaluates an expression and checks if it evaluates to true or not
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+/*!
+ * Checks if an expression to true and then raise the failure count if not.
+ * \param X Expression to check
+ */
+//==============================================================================
 #define NT2_TEST(X)                                                 \
 ( ::nt2::details::test_count()++                                    \
 , (X) ? ::nt2::details::pass(#X)                                    \
@@ -24,9 +29,12 @@
 )                                                                   \
 /**/
 
-////////////////////////////////////////////////////////////////////////////////
-// Emit a strong error message
-////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+/*!
+ * Emit a strong error message and raise the failure count
+ * \param X Error message to display
+ */
+//==============================================================================
 #define NT2_TEST_ERROR(X)                                   \
 ::nt2::details::error(X, __LINE__, BOOST_CURRENT_FUNCTION)  \
 /**/
--- modules/sdk/include/nt2/sdk/unit/tests/relation.hpp
+++ modules/sdk/include/nt2/sdk/unit/tests/relation.hpp
@@ -1,44 +1,78 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_UNIT_TESTS_RELATION_HPP_INCLUDED
 #define NT2_SDK_UNIT_TESTS_RELATION_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// Comparison tests
-////////////////////////////////////////////////////////////////////////////////
+/*!
+ * \file
+ * \brief Macros for checking relation between values in Unit Tests
+ */
+
 #include <nt2/sdk/unit/details/suite.hpp>
 #include <nt2/sdk/unit/details/test_func.hpp>
 
-////////////////////////////////////////////////////////////////////////////////
-// Predicate based tests
-////////////////////////////////////////////////////////////////////////////////
-#define NT2_TEST_EQUAL(A,B)           \
-  /*r1 =  A;  r2 =  B;*/            \
-  ( ::nt2::details::test_eq(#A, #B , __LINE__, BOOST_CURRENT_FUNCTION, A, B) ) \
+//==============================================================================
+/*!
+ * Test if two expression evaluates to the same value and raise the failure
+ * count if not.
+ */
+//==============================================================================
+#define NT2_TEST_EQUAL(A,B)                                                   \
+( ::nt2::details::test_eq(#A, #B , __LINE__, BOOST_CURRENT_FUNCTION, A, B) )  \
 /**/
 
+//==============================================================================
+/*!
+ * Test if two expression evaluates to different values and raise the failure
+ * count if not.
+ */
+//==============================================================================
 #define NT2_TEST_NOT_EQUAL(A,B)                                               \
 ( ::nt2::details::test_neq(#A, #B , __LINE__, BOOST_CURRENT_FUNCTION, A, B) ) \
 /**/
 
+//==============================================================================
+/*!
+ * Test if an expression evaluates to a smaller value than the second one and
+ * raise the failure count if not.
+ */
+//==============================================================================
 #define NT2_TEST_LESSER(A,B)                                                  \
 ( ::nt2::details::test_lt(#A, #B , __LINE__, BOOST_CURRENT_FUNCTION, A, B) )  \
 /**/
 
+//==============================================================================
+/*!
+ * Test if an expression evaluates to a greater value than the second one and
+ * raise the failure count if not.
+ */
+//==============================================================================
 #define NT2_TEST_GREATER(A,B)                                                 \
 ( ::nt2::details::test_gt(#A, #B , __LINE__, BOOST_CURRENT_FUNCTION, A, B) )  \
 /**/
 
+//==============================================================================
+/*!
+ * Test if an expression evaluates to a smaller or equal value than the second
+ * one and raise the failure count if not.
+ */
+//==============================================================================
 #define NT2_TEST_LESSER_EQUAL(A,B)                                           \
 ( ::nt2::details::test_le(#A, #B , __LINE__, BOOST_CURRENT_FUNCTION, A, B) ) \
 /**/
 
+//==============================================================================
+/*!
+ * Test if an expression evaluates to a greater or equal value than the second
+ * one and raise the failure count if not.
+ */
+//==============================================================================
 #define NT2_TEST_GREATER_EQUAL(A,B)                                           \
 ( ::nt2::details::test_ge(#A, #B , __LINE__, BOOST_CURRENT_FUNCTION, A, B) )  \
 /**/
