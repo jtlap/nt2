8e07c6d91c392bdf09758a6f93434b231cc62e18
Joel Falcou <joel.falcou@lri.fr>
2011-06-15 13:04:26 -0500

    Added generic and scalar hierarchy and fixed their relationship

---
+++ modules/sdk/include/nt2/sdk/meta/details/generic.hpp
@@ -0,0 +1,32 @@
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_SDK_META_DETAILS_GENERIC_HPP_INCLUDED
+#define NT2_SDK_META_DETAILS_GENERIC_HPP_INCLUDED
+
+namespace nt2 { namespace meta
+{
+  template<class T> struct generic_ : generic_< typename T::parent >
+  {
+    typedef generic_< typename T::parent >  parent;
+    typedef typename T::origin              origin;
+  };
+
+  template<class T> struct generic_< unspecified_<T> > : unspecified_<T>
+  {
+    typedef unspecified_<T> parent;
+    typedef T               origin;
+  };
+  template<class T> struct generic_< unknown_<T> > : unknown_<T>
+  {
+    typedef unknown_<T> parent;
+    typedef T           origin;
+  };
+} }
+
+#endif
---
+++ modules/sdk/include/nt2/sdk/meta/details/scalar.hpp
@@ -0,0 +1,29 @@
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_SDK_META_DETAILS_SCALAR_HPP_INCLUDED
+#define NT2_SDK_META_DETAILS_SCALAR_HPP_INCLUDED
+
+namespace nt2 { namespace meta
+{
+  template<class T> struct scalar_ : scalar_< typename T::parent >
+  {
+    typedef scalar_< typename T::parent > parent;
+    typedef typename T::origin            origin;
+  };
+
+  template<class T>
+  struct  scalar_< unspecified_<T> >
+        : generic_< typename unspecified_<T>::origin >
+  {
+    typedef generic_< typename unspecified_<T>::origin >  parent;
+    typedef typename unspecified_<T>::origin              origin;
+  };
+} }
+
+#endif
