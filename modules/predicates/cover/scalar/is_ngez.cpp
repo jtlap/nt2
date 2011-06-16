222cdfb3c7f3c446d59eb8daa2eed77d4fddb973
Jt lapreste <jtlapreste@gmail.com>
2011-06-16 23:37:37 +0200

    predicates cpp with nt2::int...

--- modules/predicates/cover/scalar/is_ngez.cpp
+++ modules/predicates/cover/scalar/is_ngez.cpp
@@ -43,7 +43,7 @@ NT2_TEST_CASE_TPL ( is_ngez_real__1_0,  NT2_REAL_TYPES)
   ulpd=0.0;
 
   // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
     double ulp0, ulpd ; ulpd=ulp0=0.0;
@@ -77,7 +77,7 @@ NT2_TEST_CASE_TPL ( is_ngez_signed_int__1_0,  NT2_INTEGRAL_SIGNED_TYPES)
   ulpd=0.0;
 
   // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
     double ulp0, ulpd ; ulpd=ulp0=0.0;
@@ -111,7 +111,7 @@ NT2_TEST_CASE_TPL ( is_ngez_unsigned_int__1_0,  NT2_UNSIGNED_TYPES)
   ulpd=0.0;
 
   // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10000), T(10000));
     double ulp0, ulpd ; ulpd=ulp0=0.0;
