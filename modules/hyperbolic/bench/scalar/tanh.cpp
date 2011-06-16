37fe899f23e1202ac1a0a26fb900dc57dc98ca69
Jt lapreste <jtlapreste@gmail.com>
2011-06-16 23:40:36 +0200

    hyperb cpp with nt2::int...

--- modules/hyperbolic/bench/scalar/tanh.cpp
+++ modules/hyperbolic/bench/scalar/tanh.cpp
@@ -38,42 +38,42 @@ namespace n2 {
   NT2_TIMING(tanh_,(RS(T,T(-10),T(10))))
 }
 namespace n3 {
-  typedef uint8_t T;
+  typedef nt2::uint8_t T;
   typedef nt2::meta::as_integer<T>::type iT;
   NT2_TIMING(tanh_,(RS(T,T(-10),T(10))))
 }
 namespace n4 {
-  typedef uint16_t T;
+  typedef nt2::uint16_t T;
   typedef nt2::meta::as_integer<T>::type iT;
   NT2_TIMING(tanh_,(RS(T,T(-10),T(10))))
 }
 namespace n5 {
-  typedef uint32_t T;
+  typedef nt2::uint32_t T;
   typedef nt2::meta::as_integer<T>::type iT;
   NT2_TIMING(tanh_,(RS(T,T(-10),T(10))))
 }
 namespace n6 {
-  typedef uint64_t T;
+  typedef nt2::uint64_t T;
   typedef nt2::meta::as_integer<T>::type iT;
   NT2_TIMING(tanh_,(RS(T,T(-10),T(10))))
 }
 namespace n7 {
-  typedef int8_t T;
+  typedef nt2::int8_t T;
   typedef nt2::meta::as_integer<T>::type iT;
   NT2_TIMING(tanh_,(RS(T,T(-10),T(10))))
 }
 namespace n8 {
-  typedef int16_t T;
+  typedef nt2::int16_t T;
   typedef nt2::meta::as_integer<T>::type iT;
   NT2_TIMING(tanh_,(RS(T,T(-10),T(10))))
 }
 namespace n9 {
-  typedef int32_t T;
+  typedef nt2::int32_t T;
   typedef nt2::meta::as_integer<T>::type iT;
   NT2_TIMING(tanh_,(RS(T,T(-10),T(10))))
 }
 namespace n10 {
-  typedef int64_t T;
+  typedef nt2::int64_t T;
   typedef nt2::meta::as_integer<T>::type iT;
   NT2_TIMING(tanh_,(RS(T,T(-10),T(10))))
 }
