18175450d97a96bd53e9feacc7324572f53f6ac6
Jt lapreste <jtlapreste@gmail.com>
2011-06-16 23:44:58 +0200

    cephes cpp with nt2::int...

--- modules/cephes/unit/scalar/struve.cpp
+++ modules/cephes/unit/scalar/struve.cpp
@@ -41,7 +41,7 @@ NT2_TEST_CASE_TPL ( struve_real__2_0,  NT2_REAL_TYPES)
   ulpd=0.0;
 
   // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-100), T(100));
     NT2_CREATE_BUF(tab_a1,T, NR, T(-100), T(100));
