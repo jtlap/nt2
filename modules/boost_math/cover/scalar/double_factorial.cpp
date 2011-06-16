6e7d260e3a91f89318acb3788024e259ea50d8b7
Jt lapreste <jtlapreste@gmail.com>
2011-06-16 23:46:21 +0200

    boost_math cpp with nt2::int...

--- modules/boost_math/cover/scalar/double_factorial.cpp
+++ modules/boost_math/cover/scalar/double_factorial.cpp
@@ -41,7 +41,7 @@ NT2_TEST_CASE_TPL ( double_factorial_integer__1_0,  NT2_INTEGRAL_TYPES)
   ulpd=0.0;
 
   // random verifications
-  static const uint32_t NR = NT2_NB_RANDOM_TEST;
+  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(100));
     double ulp0, ulpd ; ulpd=ulp0=0.0;
