01b27b711302f97804d0c7017a618a562a14e5af
Jt lapreste <jtlapreste@gmail.com>
2011-06-20 15:31:39 +0200

    gsl_specfun  units revisited

--- modules/gsl_specfun/unit/scalar/gsl_sf_psi.cpp
+++ modules/gsl_specfun/unit/scalar/gsl_sf_psi.cpp
@@ -9,10 +9,13 @@
 #define NT2_UNIT_MODULE "nt2 gsl_specfun toolbox - gsl_sf_psi/scalar Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// Test behavior of gsl_specfun components in scalar mode
+// unit test behavior of gsl_specfun components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 10/03/2011
-/// modified by jt the 18/03/2011
+/// 
+#include <nt2/toolbox/gsl_specfun/include/gsl_sf_psi.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -20,7 +23,7 @@
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
 #include <nt2/include/constants/infinites.hpp>
-#include <nt2/toolbox/gsl_specfun/include/gsl_sf_psi.hpp>
+
 
 NT2_TEST_CASE_TPL ( gsl_sf_psi_real__1_0,  NT2_REAL_TYPES)
 {
@@ -39,20 +42,4 @@ NT2_TEST_CASE_TPL ( gsl_sf_psi_real__1_0,  NT2_REAL_TYPES)
   double ulpd;
   ulpd=0.0;
 
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for param "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << std::endl;
-        NT2_TEST_ULP_EQUAL( nt2::gsl_specfun::gsl_sf_psi(a0),nt2::gsl_specfun::gsl_sf_psi(a0),1);
-        ulp0=std::max(ulpd,ulp0);
-     }
-     std::cout << "max ulp found is: " << ulp0 << std::endl;
-   }
 } // end of test for real_
