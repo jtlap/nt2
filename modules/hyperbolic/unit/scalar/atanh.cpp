2d2cd2543a728e032d2c957396fff6636c65dd48
Jt lapreste <jtlapreste@gmail.com>
2011-06-20 15:47:49 +0200

    hyperbolic units revisited

--- modules/hyperbolic/unit/scalar/atanh.cpp
+++ modules/hyperbolic/unit/scalar/atanh.cpp
@@ -9,10 +9,16 @@
 #define NT2_UNIT_MODULE "nt2 hyperbolic toolbox - atanh/scalar Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// Test behavior of hyperbolic components in scalar mode
+// unit test behavior of hyperbolic components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 20/02/2011
-/// modified by jt the 17/04/2011
+/// 
+#include <nt2/toolbox/hyperbolic/include/atanh.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/toolbox/exponential/include/constants.hpp>
+extern "C" { long double cephes_atanhl(long double); }
+#include <nt2/toolbox/boost_math/include/atanh.hpp>
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -20,8 +26,7 @@
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
 #include <nt2/include/constants/infinites.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/toolbox/hyperbolic/include/atanh.hpp>
+
 
 NT2_TEST_CASE_TPL ( atanh_real__1_0,  NT2_REAL_TYPES)
 {
