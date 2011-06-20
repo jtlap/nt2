2a88537f231d2a2ea0af5c9e408ef22008320063
Joel Falcou <joel.falcou@lri.fr>
2011-06-20 10:07:46 -0500

    Update to some sdk::meta documentation + some new examples

--- modules/sdk/include/nt2/sdk/meta/has_key.hpp
+++ modules/sdk/include/nt2/sdk/meta/has_key.hpp
@@ -1,26 +1,54 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_META_HAS_KEY_HPP_INCLUDED
 #define NT2_SDK_META_HAS_KEY_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// Check if a type T is a member of set S
-// See: http://nt2.metascale.org/sdk/meta/set/has_key.hpp
-////////////////////////////////////////////////////////////////////////////////
+/*!
+ * \file
+ * \brief Defines and implements the nt2::meta::has_key meta-function
+ */
+
 #include <boost/typeof/typeof.hpp>
 
 namespace nt2 { namespace meta
 {
-  template<class S,class K> struct has_key
+  //============================================================================
+  /*!
+   * For a given \c Key type and a given nt2::meta::set \c Set, returns a
+   * boolean \metaint equals to \true_ if said \c Key belongs to \c Set.
+   *
+   * \tparam Set nt2::meta::set to look into
+   * \tparam Key Type to look for in \c Set
+   *
+   * \semantic
+   *
+   * For any set \c S and any key type \c K,
+   *
+   * \code
+   * typedef nt2::meta::has_key<S,K>::type v;
+   * \endcode
+   *
+   * evalautes to \true_ is \c K is a member of \c S, otherwise evaluates to
+   * \false_
+   *
+   * \usage
+   *
+   * \include has_key.cpp
+   *
+   * \see nt2::meta::set
+   * \see nt2::meta::join
+   */
+  //============================================================================
+  template<class Set,class Key> struct has_key
   {
-    static K* keyptr;
-    BOOST_TYPEOF_NESTED_TYPEDEF_TPL(nested, S::key(keyptr) )
+    static Key* keyptr;
+    BOOST_TYPEOF_NESTED_TYPEDEF_TPL(nested, Set::key(keyptr) )
     typedef typename nested::type type;
     BOOST_STATIC_CONSTANT( bool, value = type::value );
   };
--- modules/sdk/include/nt2/sdk/meta/set.hpp
+++ modules/sdk/include/nt2/sdk/meta/set.hpp
@@ -1,18 +1,19 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_META_SET_HPP_INCLUDED
 #define NT2_SDK_META_SET_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// lightweight MPL set like class with minimal support
-// See: http://nt2.metascale.org/sdk/meta/set/set.hpp
-////////////////////////////////////////////////////////////////////////////////
+/*!
+ * \file
+ * \brief Defines and implements the nt2::meta::set class
+ */
+
 #include <boost/mpl/bool.hpp>
 #include <boost/mpl/apply.hpp>
 #include <nt2/sdk/meta/na.hpp>
@@ -24,9 +25,22 @@
 
 namespace nt2 { namespace meta
 {
-  //////////////////////////////////////////////////////////////////////////////
+  #if defined(DOXYGEN_ONLY)
+  //============================================================================
+  /*!
+   * Defines a compile-time set of types similar in principles to
+   * boost::mpl::set but optimized to be immutable, which means operations
+   * like insertion ordeletion of key or iteration is unavailable.
+   *
+   * \see nt2::meta::has_key
+   */
+  //============================================================================
+  template<class Args...> struct set {};
+  #endif
+
+  //============================================================================
   // Explicit set listing its elements
-  //////////////////////////////////////////////////////////////////////////////
+  //============================================================================
   #define M0(z,n,t) static boost::mpl::true_ key(BOOST_PP_CAT(A,n)*);
 
   template<BOOST_PP_ENUM_BINARY_PARAMS( NT2_MAX_META_ARITY
@@ -53,25 +67,14 @@ namespace nt2 { namespace meta
   };                                                      \
   /**/
 
-  //////////////////////////////////////////////////////////////////////////////
+  //============================================================================
   // Generate all hierarchy specialization
-  //////////////////////////////////////////////////////////////////////////////
+  //============================================================================
   BOOST_PP_REPEAT_FROM_TO(1,NT2_MAX_META_ARITY,M1,~)
 
   #undef M0
   #undef M1
 
-  //////////////////////////////////////////////////////////////////////////////
-  // Set using lambda to defines its components
-  //////////////////////////////////////////////////////////////////////////////
-  template<class Lambda>
-  struct lambda_set
-  {
-    typedef void       is_set_type;
-    typedef lambda_set type;
-    template<class T>
-    static typename boost::mpl::apply1<Lambda,T>::type key(T*);
-  };
 } }
 
 #endif
