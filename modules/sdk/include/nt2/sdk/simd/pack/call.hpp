0952996ee0d498fb3f14fb3aa9cfa9006f3532d9
Joel Falcou <joel.falcou@lri.fr>
2011-06-20 16:22:51 -0500

    Pack terminals use new functor system to be evaluated

--- modules/sdk/include/nt2/sdk/simd/pack/call.hpp
+++ modules/sdk/include/nt2/sdk/simd/pack/call.hpp
@@ -9,37 +9,23 @@
 #ifndef NT2_SDK_SIMD_PACK_CALL_HPP_INCLUDED
 #define NT2_SDK_SIMD_PACK_CALL_HPP_INCLUDED
 
-#include <nt2/sdk/dsl/litteral.hpp>
+#include <nt2/sdk/dsl/literal.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
 // Register terminal handlers for SIMD expression - native case
 ////////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(tag::terminal_,tag::cpu_
-                     ,(A0)(A1)(A2)(X)(Y)
-                     ,((simd_<arithmetic_<A0>,X>))
-                      ((target_<simd_<arithmetic_<A1>,Y> >))
-                      (integer_<A2>)
-                     )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy,class X,class Y>
-  struct call<tag::terminal_( tag::simd_<tag::arithmetic_,X>
-                            , tag::target_< tag::simd_<tag::arithmetic_,Y> >
-                            , tag::integer_
-                            ),tag::cpu_,Dummy>
-  : callable
+  NT2_FUNCTOR_IMPLEMENTATION( tag::terminal_,tag::cpu_
+                            , (Value)(State)(Data)(X)(Y)
+                            , ((simd_<arithmetic_<Value>,X>))
+                              ((target_<simd_<arithmetic_<State>,Y> >))
+                              (integer_<Data>)
+                            )
   {
-    template<class Sig> struct result;
+    typedef typename meta::strip<Value>::type::parent result_type;
 
-    template<class This, class Value, class State, class Data>
-    struct result<This(Value,State,Data)>
-    {
-      typedef typename meta::strip<Value>::type::parent type;
-    };
-
-    template<class Value, class State, class Data> inline
-    typename result<call(Value,State,Data)>::type
+    inline result_type
     operator()( Value& v, State const& , Data const&  ) const
     {
       return v.value();
@@ -50,36 +36,22 @@ namespace nt2 { namespace ext
 ////////////////////////////////////////////////////////////////////////////////
 // Register terminal handlers for SIMD expression - emulated case
 ////////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH_TPL (  tag::terminal_,tag::cpu_
-                          , (class A0)(class A1)(class A2)(std::size_t N)
-                          , ((array_<arithmetic_<A0>,N>))
-                            ((target_<array_<arithmetic_<A1>,N> >))
-                            (integer_<A2>)
-                          )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy,std::size_t N>
-  struct call<tag::terminal_( tag::array_<tag::arithmetic_,N>
-                            , tag::target_< tag::array_<tag::arithmetic_,N> >
-                            , tag::integer_
-                            ),tag::cpu_,Dummy>
-  : callable
-  {
-    template<class Sig> struct result;
+  NT2_FUNCTOR_IMPLEMENTATION_TPL(  tag::terminal_,tag::cpu_
+                                , (class Value)(class State)
+                                  (class Data)(std::size_t N)
+                                , ((array_<arithmetic_<Value>,N>))
+                                  ((target_<array_<arithmetic_<State>,N> >))
+                                  (integer_<Data>)
+                                )
+{
+    typedef typename meta::strip<Value>::type::parent::value_type result_type;
 
-    template<class This, class Value, class State, class Data>
-    struct result<This(Value,State,Data)>
+    inline result_type
+    operator()( Value& v, State const&, Data const& p ) const
     {
-      typedef typename meta::strip<Value>::type::parent base;
-      typedef typename base::value_type                 type;
-    };
-
-    template<class Value, class State, class Data> inline
-    typename result<call(Value,State,Data)>::type
-    operator()( Value& v, State const& , Data const& pos) const
-    {
-      return v[pos];
+      return v[p];
     }
   };
 } }
