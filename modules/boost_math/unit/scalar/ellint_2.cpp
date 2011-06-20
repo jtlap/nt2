821ebebc2f377d399225c52d893b1978ea6ea4c9
Jt lapreste <jtlapreste@gmail.com>
2011-06-20 09:05:33 +0200

    boost_math units revisited

--- modules/boost_math/unit/scalar/ellint_2.cpp
+++ modules/boost_math/unit/scalar/ellint_2.cpp
@@ -9,10 +9,14 @@
 #define NT2_UNIT_MODULE "nt2 boost_math toolbox - ellint_2/scalar Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// Test behavior of boost_math components in scalar mode
+// unit test behavior of boost_math components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 07/03/2011
-/// modified by jt the 23/03/2011
+/// 
+#include <nt2/toolbox/boost_math/include/ellint_2.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/include/functions/ellint_2.hpp>
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -20,12 +24,10 @@
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
 #include <nt2/include/constants/infinites.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/toolbox/boost_math/include/ellint_2.hpp>
-// specific includes for arity 1 tests
-#include <nt2/include/functions/ellint_2.hpp>
-// specific includes for arity 2 tests
+
 #include <nt2/toolbox/trigonometric/include/constants.hpp>
+
+
 
 NT2_TEST_CASE_TPL ( ellint_2_real__1_0,  NT2_REAL_TYPES)
 {
@@ -44,22 +46,6 @@ NT2_TEST_CASE_TPL ( ellint_2_real__1_0,  NT2_REAL_TYPES)
   double ulpd;
   ulpd=0.0;
 
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(1));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for param "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << std::endl;
-        NT2_TEST_ULP_EQUAL( nt2::boost_math::ellint_2(a0),nt2::ellint_2(a0),1);
-        ulp0=nt2::max(ulpd,ulp0);
-     }
-     std::cout << "max ulp found is: " << ulp0 << std::endl;
-   }
 } // end of test for real_
 
 NT2_TEST_CASE_TPL ( ellint_2_real__2_1,  NT2_REAL_TYPES)
@@ -79,23 +65,4 @@ NT2_TEST_CASE_TPL ( ellint_2_real__2_1,  NT2_REAL_TYPES)
   double ulpd;
   ulpd=0.0;
 
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(1));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(0), nt2::Pio_2<T>());
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    T a1;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_ULP_EQUAL( nt2::boost_math::ellint_2(a0,a1),nt2::boost_math::ellint_2(a0,a1),1);
-        ulp0=nt2::max(ulpd,ulp0);
-     }
-     std::cout << "max ulp found is: " << ulp0 << std::endl;
-   }
 } // end of test for real_
