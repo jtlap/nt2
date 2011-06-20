97d058e2f7982b78ba186bdae0a596b401215036
Jt lapreste <jtlapreste@gmail.com>
2011-06-20 10:39:20 +0200

    crlibm units revisited

--- modules/crlibm/unit/scalar/pow_rn.cpp
+++ modules/crlibm/unit/scalar/pow_rn.cpp
@@ -9,10 +9,14 @@
 #define NT2_UNIT_MODULE "nt2 crlibm toolbox - pow_rn/scalar Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// Test behavior of crlibm components in scalar mode
+// unit test behavior of crlibm components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 02/03/2011
-/// modified by jt the 16/03/2011
+/// 
+#include <nt2/toolbox/crlibm/include/pow_rn.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/include/functions/pow.hpp>
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -20,12 +24,9 @@
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
 #include <nt2/include/constants/infinites.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/toolbox/crlibm/include/pow_rn.hpp>
-// specific includes for arity 2 tests
-#include <nt2/include/functions/pow.hpp>
 
-NT2_TEST_CASE_TPL ( pow_rn_real__2,  NT2_REAL_TYPES)
+
+NT2_TEST_CASE_TPL ( pow_rn_real__2_0,  NT2_REAL_TYPES)
 {
   using nt2::rn;
   using nt2::crlibm::pow_rn;
@@ -42,23 +43,4 @@ NT2_TEST_CASE_TPL ( pow_rn_real__2,  NT2_REAL_TYPES)
   double ulpd;
   ulpd=0.0;
 
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(10));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(0), T(10));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    T a0;
-    T a1;
-    for (uint32_t j =0; j < NR; ++j )
-      {
-        std::cout << "for params "
-                  << "  a0 = "<< u_t(a0 = tab_a0[j])
-                  << ", a1 = "<< u_t(a1 = tab_a1[j])
-                  << std::endl;
-        NT2_TEST_ULP_EQUAL( nt2::crlibm::pow_rn(a0,a1),nt2::pow(a0,a1),5);
-        ulp0=nt2::max(ulpd,ulp0);
-     }
-     std::cout << "max ulp found is: " << ulp0 << std::endl;
-   }
 } // end of test for real_
