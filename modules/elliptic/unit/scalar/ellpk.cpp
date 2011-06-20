82add3f39efba94b0284f99104293f5f9ee87c71
Jt lapreste <jtlapreste@gmail.com>
2011-06-20 10:59:29 +0200

    elliptic units revisited

--- modules/elliptic/unit/scalar/ellpk.cpp
+++ modules/elliptic/unit/scalar/ellpk.cpp
@@ -9,10 +9,15 @@
 #define NT2_UNIT_MODULE "nt2 elliptic toolbox - ellpk/scalar Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// Test behavior of elliptic components in scalar mode
+// unit test behavior of elliptic components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 21/02/2011
-/// modified by jt the 17/04/2011
+/// 
+#include <nt2/toolbox/elliptic/include/ellpk.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+extern "C" {long double cephes_ellpkl(long double);}
+#include <nt2/toolbox/trigonometric/include/constants.hpp>
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -20,11 +25,7 @@
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
 #include <nt2/include/constants/infinites.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/toolbox/elliptic/include/ellpk.hpp>
-// specific includes for arity 1 tests
-extern "C" {long double cephes_ellpkl(long double);}
-#include <nt2/toolbox/trigonometric/include/constants.hpp>
+
 
 NT2_TEST_CASE_TPL ( ellpk_real__1_0,  NT2_REAL_TYPES)
 {
@@ -47,6 +48,6 @@ NT2_TEST_CASE_TPL ( ellpk_real__1_0,  NT2_REAL_TYPES)
   // specific values tests
   NT2_TEST_ULP_EQUAL(ellpk(nt2::Nan<T>()), nt2::Nan<r_t>(), 0);
   NT2_TEST_ULP_EQUAL(ellpk(nt2::One<T>()), nt2::Pi<r_t>()/2, 0);
-  NT2_TEST_ULP_EQUAL(ellpk(nt2::Pi<T>()/2), nt2::Nan<r_t>(), 0);
+  NT2_TEST_ULP_EQUAL(ellpk(nt2::Pi<T>()/nt2::Two<T>()), nt2::Nan<r_t>(), 0);
   NT2_TEST_ULP_EQUAL(ellpk(nt2::Zero<T>()), nt2::Inf<r_t>(), 0);
 } // end of test for real_
