b6e8a098da92f9919ea86cb6789f44937941307c
Joel Falcou <joel.falcou@lri.fr>
2011-06-20 16:21:59 -0500

    fixed constant to work with new functor system

--- modules/constant/include/nt2/sdk/constant/category.hpp
+++ modules/constant/include/nt2/sdk/constant/category.hpp
@@ -1,26 +1,29 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_SDK_CONSTANT_CATEGORY_HPP_INCLUDED
 #define NT2_SDK_CONSTANT_CATEGORY_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// Category for constants
-//* TODO:  Documentation: http://nt2.lri.fr/sdk/constant/meta/category.html
-////////////////////////////////////////////////////////////////////////////////
+/*!
+ * \file
+ * \brief Defines the constant_ hierarchy and EDSL generators
+ */
+
 #include <nt2/sdk/meta/as.hpp>
 #include <nt2/sdk/meta/hierarchy_of.hpp>
 
-////////////////////////////////////////////////////////////////////////////////
-// constant_ wraps a constant type into a EDSL element and has its own hierarchy
-////////////////////////////////////////////////////////////////////////////////
 namespace nt2 { namespace meta
 {
+  //============================================================================
+  /*!
+   * Defines the category class for all constants identifiers
+   */
+  //============================================================================
   template<class T> struct constant_ : unspecified_<T>
   {
     typedef unspecified_<T> parent;
@@ -30,6 +33,12 @@ namespace nt2 { namespace meta
 
 namespace nt2
 {
+  //============================================================================
+  /*!
+   * This class wraps a constant identifier usable by Boost.Proto to build
+   * named consatnt value terminal.
+   */
+  //============================================================================
   template<class ID>
   struct constant_ { typedef meta::constant_<ID> nt2_hierarchy_tag; };
 }
