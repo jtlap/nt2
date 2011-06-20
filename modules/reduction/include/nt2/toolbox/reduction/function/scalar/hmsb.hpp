5b539a2d8c37fa51c658325c1c2f29b68fb1a59d
Mathias Gaunard <mathias@gaunard.com>
2011-06-20 19:01:51 +0200

    adapt module reduction to new functor system

--- modules/reduction/include/nt2/toolbox/reduction/function/scalar/hmsb.hpp
+++ modules/reduction/include/nt2/toolbox/reduction/function/scalar/hmsb.hpp
@@ -1,11 +1,11 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
+//                                                                              
+//          Distributed under the Boost Software License, Version 1.0.          
+//                 See accompanying file LICENSE.txt or copy at                 
+//                     http://www.boost.org/LICENSE_1_0.txt                     
+//==============================================================================
 #ifndef NT2_TOOLBOX_REDUCTION_FUNCTION_SCALAR_HMSB_HPP_INCLUDED
 #define NT2_TOOLBOX_REDUCTION_FUNCTION_SCALAR_HMSB_HPP_INCLUDED
 #include <nt2/include/constants/properties.hpp>
@@ -14,27 +14,22 @@
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type  is fundamental_
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(tag::hmsb_, tag::cpu_,
-                      (A0),
-                      (fundamental_<A0>)
-                     )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<tag::hmsb_(tag::fundamental_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( tag::hmsb_, tag::cpu_
+                            , (A0)
+                            , (scalar_< fundamental_<A0> >)
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0>
-      struct result<This(A0)> : meta::as_integer<A0>{};
 
-    NT2_FUNCTOR_CALL(1) {
+    typedef typename meta::as_integer<A0>::type result_type;
+
+    NT2_FUNCTOR_CALL(1)
+    {
       typedef typename NT2_RETURN_TYPE(1)::type r_t;
       return b_and(a0, Signmask<r_t>()) != 0; }
-
   };
 } }
 
+
 #endif
-// modified by jt the 26/12/2010
