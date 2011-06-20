979c5d3423b16b52f53defed688b1ef5f2ea2f15
Mathias Gaunard <mathias@gaunard.com>
2011-06-20 19:01:55 +0200

    adapt module elliptic to new functor system

--- modules/elliptic/include/nt2/toolbox/elliptic/function/scalar/ellpk.hpp
+++ modules/elliptic/include/nt2/toolbox/elliptic/function/scalar/ellpk.hpp
@@ -1,11 +1,11 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
+//                                                                              
+//          Distributed under the Boost Software License, Version 1.0.          
+//                 See accompanying file LICENSE.txt or copy at                 
+//                     http://www.boost.org/LICENSE_1_0.txt                     
+//==============================================================================
 #ifndef NT2_TOOLBOX_ELLIPTIC_FUNCTION_SCALAR_ELLPK_HPP_INCLUDED
 #define NT2_TOOLBOX_ELLIPTIC_FUNCTION_SCALAR_ELLPK_HPP_INCLUDED
 #include <nt2/include/constants/eps_related.hpp>
@@ -19,49 +19,40 @@
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is arithmetic_
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(tag::ellpk_, tag::cpu_,
-                       (A0),
-                       (arithmetic_<A0>)
-                      )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<tag::ellpk_(tag::arithmetic_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( tag::ellpk_, tag::cpu_
+                            , (A0)
+                            , (scalar_< arithmetic_<A0> >)
+                            )
   {
-      template<class Sig> struct result;
-      template<class This,class A0>
-      struct result<This(A0)> : meta::result_of<meta::arithmetic(A0)>{};
+
+    typedef typename meta::result_of<meta::arithmetic(A0)>::type result_type;
 
     NT2_FUNCTOR_CALL(1)
-      {
+    {
         typedef typename NT2_RETURN_TYPE(1)::type type;
         return nt2::ellpk(type(a0));
       }
   };
 } }
 
+
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is double
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(tag::ellpk_, tag::cpu_,
-                       (A0),
-                       (double_<A0>)
-                      )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<tag::ellpk_(tag::double_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( tag::ellpk_, tag::cpu_
+                            , (A0)
+                            , (scalar_< double_<A0> >)
+                            )
   {
-      template<class Sig> struct result;
-      template<class This,class A0>
-      struct result<This(A0)> : meta::result_of<meta::arithmetic(A0)>{};
+
+    typedef typename meta::result_of<meta::arithmetic(A0)>::type result_type;
 
     NT2_FUNCTOR_CALL(1)
-      {
+    {
         if (a0>One<A0>()||(is_ltz(a0)))
           return Nan<A0>();
         else if (a0<=Eps<A0>())
@@ -84,26 +75,22 @@ namespace nt2 { namespace ext
   };
 } }
 
+
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is float
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(tag::ellpk_, tag::cpu_,
-                       (A0),
-                       (float_<A0>)
-                      )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<tag::ellpk_(tag::float_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( tag::ellpk_, tag::cpu_
+                            , (A0)
+                            , (scalar_< float_<A0> >)
+                            )
   {
-      template<class Sig> struct result;
-      template<class This,class A0>
-      struct result<This(A0)> : meta::result_of<meta::arithmetic(A0)>{};
+
+    typedef typename meta::result_of<meta::arithmetic(A0)>::type result_type;
 
     NT2_FUNCTOR_CALL(1)
-      {
+    {
         if (a0>One<A0>()||(is_ltz(a0)))
           return Nan<A0>();
         else if (a0<=Eps<A0>())
@@ -128,5 +115,5 @@ namespace nt2 { namespace ext
   };
 } }
 
+
 #endif
-// modified by jt the 26/12/2010
--- modules/elliptic/include/nt2/toolbox/elliptic/function/simd/common/ellik.hpp
+++ modules/elliptic/include/nt2/toolbox/elliptic/function/simd/common/ellik.hpp
@@ -1,11 +1,11 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
+//                                                                              
+//          Distributed under the Boost Software License, Version 1.0.          
+//                 See accompanying file LICENSE.txt or copy at                 
+//                     http://www.boost.org/LICENSE_1_0.txt                     
+//==============================================================================
 #ifndef NT2_TOOLBOX_ELLIPTIC_FUNCTION_SIMD_COMMON_ELLIK_HPP_INCLUDED
 #define NT2_TOOLBOX_ELLIPTIC_FUNCTION_SIMD_COMMON_ELLIK_HPP_INCLUDED
 #include <nt2/sdk/meta/as_real.hpp>
@@ -34,22 +34,15 @@
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is arithmetic_
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(tag::ellik_, tag::cpu_,
-                        (A0)(X),
-                        ((simd_<arithmetic_<A0>,X>))
-                        ((simd_<arithmetic_<A0>,X>))
-                       );
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class X, class Dummy>
-  struct call<tag::ellik_(tag::simd_<tag::arithmetic_, X> ,
-                          tag::simd_<tag::arithmetic_, X> ),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( tag::ellik_, tag::cpu_
+                            , (A0)(X)
+                            , ((simd_<arithmetic_<A0>,X>))((simd_<arithmetic_<A0>,X>))
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0>
-    struct result<This(A0,A0)> :  meta::as_real<A0>{};
+
+    typedef typename meta::as_real<A0>::type result_type;
 
     NT2_FUNCTOR_CALL(2)
     {
@@ -60,25 +53,19 @@ namespace nt2 { namespace ext
   };
 } }
 
+
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is double
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(tag::ellik_, tag::cpu_,
-                        (A0)(X),
-                        ((simd_<double_<A0>,X>))
-                        ((simd_<double_<A0>,X>))
-                       );
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class X, class Dummy>
-  struct call<tag::ellik_(tag::simd_<tag::double_, X> ,
-                          tag::simd_<tag::double_, X> ),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( tag::ellik_, tag::cpu_
+                            , (A0)(X)
+                            , ((simd_<double_<A0>,X>))((simd_<double_<A0>,X>))
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0>
-    struct result<This(A0,A0)> :  meta::as_real<A0>{};
+
+    typedef typename meta::as_real<A0>::type result_type;
 
     NT2_FUNCTOR_CALL(2)
     {
@@ -87,25 +74,19 @@ namespace nt2 { namespace ext
   };
 } }
 
+
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is float
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(tag::ellik_, tag::cpu_,
-                        (A0)(X),
-                        ((simd_<float_<A0>,X>))
-                        ((simd_<float_<A0>,X>))
-                       );
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class X, class Dummy>
-  struct call<tag::ellik_(tag::simd_<tag::float_, X> ,
-                          tag::simd_<tag::float_, X> ),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( tag::ellik_, tag::cpu_
+                            , (A0)(X)
+                            , ((simd_<float_<A0>,X>))((simd_<float_<A0>,X>))
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0>
-    struct result<This(A0,A0)> :  meta::as_real<A0>{};
+
+    typedef typename meta::as_real<A0>::type result_type;
 
     NT2_FUNCTOR_CALL(2)
     {
@@ -140,5 +121,5 @@ namespace nt2 { namespace ext
   };
 } }
 
+
 #endif
-// modified by jt the 05/01/2011
