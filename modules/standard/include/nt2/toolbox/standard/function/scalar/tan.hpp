412e25b79aa905607a2464e2e8a1cef6fa454342
Mathias Gaunard <mathias@gaunard.com>
2011-06-20 19:01:49 +0200

    adapt module standard to new functor system

--- modules/standard/include/nt2/toolbox/standard/function/scalar/tan.hpp
+++ modules/standard/include/nt2/toolbox/standard/function/scalar/tan.hpp
@@ -1,11 +1,11 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
+//                                                                              
+//          Distributed under the Boost Software License, Version 1.0.          
+//                 See accompanying file LICENSE.txt or copy at                 
+//                     http://www.boost.org/LICENSE_1_0.txt                     
+//==============================================================================
 #ifndef NT2_TOOLBOX_STANDARD_FUNCTION_SCALAR_TAN_HPP_INCLUDED
 #define NT2_TOOLBOX_STANDARD_FUNCTION_SCALAR_TAN_HPP_INCLUDED
 
@@ -13,20 +13,15 @@
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is arithmetic_
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(standard::tag::tan_, tag::cpu_,
-                     (A0),
-                     (arithmetic_<A0>)
-                    )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<standard::tag::tan_(tag::arithmetic_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( standard::tag::tan_, tag::cpu_
+                            , (A0)
+                            , (scalar_< arithmetic_<A0> >)
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0>
-    struct result<This(A0)> : meta::result_of<meta::floating(A0)>{};
+
+    typedef typename meta::result_of<meta::floating(A0)>::type result_type;
 
     NT2_FUNCTOR_CALL(1)
     {
@@ -36,27 +31,24 @@ namespace nt2 { namespace ext
   };
 } }
 
+
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is real_
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(standard::tag::tan_, tag::cpu_,
-                     (A0),
-                     (real_<A0>)
-                    )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<standard::tag::tan_(tag::real_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( standard::tag::tan_, tag::cpu_
+                            , (A0)
+                            , (scalar_< real_<A0> >)
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0>
-    struct result<This(A0)> : meta::result_of<meta::floating(A0)>{};
 
-    NT2_FUNCTOR_CALL(1){ return std::tan(a0); }
+    typedef typename meta::result_of<meta::floating(A0)>::type result_type;
+
+    NT2_FUNCTOR_CALL(1)
+    { return std::tan(a0); }
   };
 } }
 
+
 #endif
-// modified by jt the 29/12/2010
