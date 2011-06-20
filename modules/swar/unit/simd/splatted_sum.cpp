b144b87e0e05576439da211cc171d863a0e31770
Jt lapreste <jtlapreste@gmail.com>
2011-06-20 22:26:57 +0200

    swar units revisited

--- modules/swar/unit/simd/splatted_sum.cpp
+++ modules/swar/unit/simd/splatted_sum.cpp
@@ -9,24 +9,25 @@
 #define NT2_UNIT_MODULE "nt2 swar toolbox - splatted_sum/simd Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// Test behavior of swar components in simd mode
+// unit test behavior of swar components in simd mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 24/02/2011
-/// modified by jt the 20/03/2011
-#include <nt2/sdk/memory/is_aligned.hpp>
-#include <nt2/sdk/memory/aligned_type.hpp>
-#include <nt2/include/functions/load.hpp>
-#include <nt2/sdk/memory/buffer.hpp>
+/// 
+#include <nt2/toolbox/swar/include/splatted_sum.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/include/functions/sum.hpp>
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
 #include <nt2/include/constants/infinites.hpp>
-#include <nt2/include/functions/max.hpp>
-#include <nt2/toolbox/swar/include/splatted_sum.hpp>
-// specific includes for arity 1 tests
-#include <nt2/include/functions/sum.hpp>
+#include <nt2/sdk/memory/is_aligned.hpp>
+#include <nt2/sdk/memory/aligned_type.hpp>
+#include <nt2/include/functions/load.hpp>
+
 
 NT2_TEST_CASE_TPL ( splatted_sum_real__1_0,  NT2_REAL_TYPES)
 {
@@ -44,22 +45,15 @@ NT2_TEST_CASE_TPL ( splatted_sum_real__1_0,  NT2_REAL_TYPES)
   typedef typename nt2::meta::call<splatted_sum_(vT)>::type r_t;
   typedef typename nt2::meta::call<splatted_sum_(T)>::type sr_t;
   typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
+  double ulpd;
+  ulpd=0.0;
 
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-100), T(100));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(uint32_t j = 0; j < NR/cardinal_of<n_t>::value; j++)
-      {
-        vT a0 = load<vT>(&tab_a0[0],j);
-        r_t v = splatted_sum(a0);
-        T p= nt2::sum(a0);
-        for(uint32_t i=0; i<cardinal_of<n_t>::value; i++)
-        {
-           NT2_TEST_EQUAL(v[i],p);
-        }
-      }
-    
-  }
+
+  // specific values tests
+  NT2_TEST_EQUAL(splatted_sum(nt2::Inf<vT>())[0], nt2::Inf<sr_t>());
+  NT2_TEST_EQUAL(splatted_sum(nt2::Minf<vT>())[0], nt2::Minf<sr_t>());
+  NT2_TEST_EQUAL(splatted_sum(nt2::Mone<vT>())[0], -int(cardinal_of<n_t>()));
+  NT2_TEST_EQUAL(splatted_sum(nt2::Nan<vT>())[0], nt2::Nan<sr_t>());
+  NT2_TEST_EQUAL(splatted_sum(nt2::One<vT>())[0], cardinal_of<n_t>());
+  NT2_TEST_EQUAL(splatted_sum(nt2::Zero<vT>())[0], nt2::Zero<sr_t>());
 } // end of test for real_
