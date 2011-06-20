bea2139a7c53b1ba33ad381cf146d38179c9501e
Mathias Gaunard <mathias@gaunard.com>
2011-06-20 19:01:52 +0200

    adapt module cephes to new functor system

--- modules/cephes/include/nt2/toolbox/cephes/function/scalar/jn.hpp
+++ modules/cephes/include/nt2/toolbox/cephes/function/scalar/jn.hpp
@@ -1,11 +1,11 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
+//                                                                              
+//          Distributed under the Boost Software License, Version 1.0.          
+//                 See accompanying file LICENSE.txt or copy at                 
+//                     http://www.boost.org/LICENSE_1_0.txt                     
+//==============================================================================
 #ifndef NT2_TOOLBOX_CEPHES_FUNCTION_SCALAR_JN_HPP_INCLUDED
 #define NT2_TOOLBOX_CEPHES_FUNCTION_SCALAR_JN_HPP_INCLUDED
 #include <nt2/sdk/meta/adapted_traits.hpp>
@@ -21,20 +21,15 @@
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A1 is arithmetic_
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(cephes::tag::jn_, tag::cpu_,
-                    (A0)(A1),
-                    (integer_<A0>)(arithmetic_<A1>)
-                   )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<cephes::tag::jn_(tag::integer_,tag::arithmetic_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( cephes::tag::jn_, tag::cpu_
+                            , (A0)(A1)
+                            , (scalar_< integer_<A0> >)(scalar_< arithmetic_<A1> >)
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0, class A1>
-    struct result<This(A0, A1)> : meta::result_of<meta::floating(A1)>{};
+
+    typedef typename meta::result_of<meta::floating(A1)>::type result_type;
 
     NT2_FUNCTOR_CALL(2)
     {
@@ -44,71 +39,62 @@ namespace nt2 { namespace ext
   };
 } }
 
+
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A1 is double
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(cephes::tag::jn_, tag::cpu_,
-                    (A0)(A1),
-                    (integer_<A0>)(double_<A1>)
-                   )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<cephes::tag::jn_(tag::integer_,tag::double_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( cephes::tag::jn_, tag::cpu_
+                            , (A0)(A1)
+                            , (scalar_< integer_<A0> >)(scalar_< double_<A1> >)
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0, class A1>
-    struct result<This(A0, A1)> : meta::result_of<meta::floating(A1)>{};
 
-    NT2_FUNCTOR_CALL(2){ return cephes_jn(a0, a1); }
+    typedef typename meta::result_of<meta::floating(A1)>::type result_type;
+
+    NT2_FUNCTOR_CALL(2)
+    { return cephes_jn(a0, a1); }
   };
 } }
+
 
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A1 is float
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(cephes::tag::jn_, tag::cpu_,
-                    (A0)(A1),
-                    (integer_<A0>)(float_<A1>)
-                   )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<cephes::tag::jn_(tag::integer_,tag::float_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( cephes::tag::jn_, tag::cpu_
+                            , (A0)(A1)
+                            , (scalar_< integer_<A0> >)(scalar_< float_<A1> >)
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0, class A1>
-    struct result<This(A0, A1)> : meta::result_of<meta::floating(A1)>{};
 
-    NT2_FUNCTOR_CALL(2){ return cephes_jnf(a0, a1); }
+    typedef typename meta::result_of<meta::floating(A1)>::type result_type;
+
+    NT2_FUNCTOR_CALL(2)
+    { return cephes_jnf(a0, a1); }
   };
 } }
+
 
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A1 is long double
 /////////////////////////////////////////////////////////////////////////////
-NT2_REGISTER_DISPATCH(cephes::tag::jn_, tag::cpu_,
-                    (A0)(A1),
-                    (integer_<A0>)(long_double_<A1>)
-                   )
-
-namespace nt2 { namespace ext
+namespace nt2 { namespace meta
 {
-  template<class Dummy>
-  struct call<cephes::tag::jn_(tag::integer_,tag::long_double_),
-              tag::cpu_, Dummy> : callable
+  NT2_FUNCTOR_IMPLEMENTATION( cephes::tag::jn_, tag::cpu_
+                            , (A0)(A1)
+                            , (scalar_< integer_<A0> >)(long_double_<A1>)
+                            )
   {
-    template<class Sig> struct result;
-    template<class This,class A0, class A1>
-    struct result<This(A0, A1)> : meta::result_of<meta::floating(A1)>{};
 
-    NT2_FUNCTOR_CALL(2){ return cephes_jnl(a0, a1); }
+    typedef typename meta::result_of<meta::floating(A1)>::type result_type;
+
+    NT2_FUNCTOR_CALL(2)
+    { return cephes_jnl(a0, a1); }
   };
 } }
 
+
 #endif
-// modified by jt the 29/12/2010
