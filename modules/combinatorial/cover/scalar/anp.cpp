c9910b8f4fa909ec3ae9c886e674ee01931cfa34
Jt lapreste <jtlapreste@gmail.com>
2011-06-23 08:50:10 +0200

    cover for combinatorial

--- modules/combinatorial/cover/scalar/anp.cpp
+++ modules/combinatorial/cover/scalar/anp.cpp
@@ -9,10 +9,13 @@
 #define NT2_UNIT_MODULE "nt2 combinatorial toolbox - anp/scalar Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// $testcat$ test behavior of combinatorial components in scalar mode
+// cover test behavior of combinatorial components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created by jt the 30/11/2010
-/// modified by jt the 05/06/2011
+/// 
+#include <nt2/toolbox/combinatorial/include/anp.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/include/functions/max.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -20,8 +23,7 @@
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
 #include <nt2/include/constants/infinites.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/toolbox/combinatorial/include/anp.hpp>
+
 
 NT2_TEST_CASE_TPL ( anp_real__2_0,  NT2_REAL_TYPES)
 {
