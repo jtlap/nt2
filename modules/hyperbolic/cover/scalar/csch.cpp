e0d8bcb1a89f5bba6d853c6286c341768f832159
Jt lapreste <jtlapreste@gmail.com>
2011-06-23 21:11:04 +0200

    cover for hyperbolic

--- modules/hyperbolic/cover/scalar/csch.cpp
+++ modules/hyperbolic/cover/scalar/csch.cpp
@@ -9,10 +9,17 @@
 #define NT2_UNIT_MODULE "nt2 hyperbolic toolbox - csch/scalar Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// $testcat$ test behavior of hyperbolic components in scalar mode
+// cover test behavior of hyperbolic components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 20/02/2011
-/// modified by jt the 05/06/2011
+/// 
+#include <nt2/toolbox/hyperbolic/include/csch.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/include/functions/max.hpp>
+#include <nt2/toolbox/exponential/include/constants.hpp>
+#include <nt2/include/functions/rec.hpp>
+extern "C" { long double cephes_sinhl(long double); }
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -20,13 +27,7 @@
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
 #include <nt2/include/constants/infinites.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/toolbox/hyperbolic/include/csch.hpp>
-// specific includes for arity 1 tests
-#include <nt2/toolbox/exponential/include/constants.hpp>
-#include <nt2/toolbox/libc/include/sinh.hpp>
-#include <nt2/include/functions/rec.hpp>
-extern "C" { long double cephes_sinhl(long double); }
+
 
 NT2_TEST_CASE_TPL ( csch_real__1_0,  NT2_REAL_TYPES)
 {
@@ -51,7 +52,7 @@ NT2_TEST_CASE_TPL ( csch_real__1_0,  NT2_REAL_TYPES)
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
     double ulp0, ulpd ; ulpd=ulp0=0.0;
     T a0;
-    for (uint32_t j =0; j < NR; ++j )
+    for(nt2::uint32_t j =0; j < NR; ++j )
       {
         std::cout << "for param "
                   << "  a0 = "<< u_t(a0 = tab_a0[j])
--- modules/hyperbolic/cover/scalar/sinhc.cpp
+++ modules/hyperbolic/cover/scalar/sinhc.cpp
@@ -9,10 +9,17 @@
 #define NT2_UNIT_MODULE "nt2 hyperbolic toolbox - sinhc/scalar Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// $testcat$ test behavior of hyperbolic components in scalar mode
+// cover test behavior of hyperbolic components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 20/02/2011
-/// modified by jt the 05/06/2011
+/// 
+#include <nt2/toolbox/hyperbolic/include/sinhc.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/include/functions/max.hpp>
+#include <nt2/toolbox/boost_math/include/sinhc_pi.hpp>
+
+#include <nt2/toolbox/exponential/include/constants.hpp>
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
@@ -20,11 +27,7 @@
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
 #include <nt2/include/constants/infinites.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/toolbox/hyperbolic/include/sinhc.hpp>
-// specific includes for arity 1 tests
-#include <nt2/toolbox/exponential/include/constants.hpp>
-#include <nt2/toolbox/boost_math/include/sinhc_pi.hpp>
+
 
 NT2_TEST_CASE_TPL ( sinhc_real__1_0,  NT2_REAL_TYPES)
 {
@@ -49,7 +52,7 @@ NT2_TEST_CASE_TPL ( sinhc_real__1_0,  NT2_REAL_TYPES)
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
     double ulp0, ulpd ; ulpd=ulp0=0.0;
     T a0;
-    for (uint32_t j =0; j < NR; ++j )
+    for(nt2::uint32_t j =0; j < NR; ++j )
       {
         std::cout << "for param "
                   << "  a0 = "<< u_t(a0 = tab_a0[j])
