0a6035081e588d91ca93f693f51a95b2ae3b25f6
Jt lapreste <jtlapreste@gmail.com>
2011-07-01 20:42:47 +0200

    cephes powi

--- modules/cephes/include/nt2/toolbox/cephes/function/scalar/powi.hpp
+++ modules/cephes/include/nt2/toolbox/cephes/function/scalar/powi.hpp
@@ -32,7 +32,7 @@ namespace nt2 { namespace meta
 
     NT2_FUNCTOR_CALL(2)
     {
-      return nt2::cephes::powi(type(a0), type(a1));
+      return nt2::cephes::powi(result_type(a0), a1);
     }
   };
 } }
@@ -45,11 +45,11 @@ namespace nt2 { namespace meta
 {
   NT2_FUNCTOR_IMPLEMENTATION( cephes::tag::powi_, tag::cpu_
                             , (A0)(A1)
-                            , (scalar_< double_<A0> >)(scalar_< double_<A1> >)
+                            , (scalar_< double_<A0> >)(scalar_< integer_<A1> >)
                             )
   {
 
-    typedef typename meta::result_of<meta::floating(A0)>::type result_type;
+    typedef A0 result_type;
 
     NT2_FUNCTOR_CALL(2)
     { return cephes_powi(a0, a1); }
@@ -64,11 +64,11 @@ namespace nt2 { namespace meta
 {
   NT2_FUNCTOR_IMPLEMENTATION( cephes::tag::powi_, tag::cpu_
                             , (A0)(A1)
-                            , (scalar_< float_<A0> >)(scalar_< float_<A1> >)
+                            , (scalar_< float_<A0> >)(scalar_< integer_<A1> >)
                             )
   {
 
-    typedef typename meta::result_of<meta::floating(A0)>::type result_type;
+    typedef A0 result_type;
 
     NT2_FUNCTOR_CALL(2)
     { return cephes_powif(a0, a1); }
@@ -83,11 +83,11 @@ namespace nt2 { namespace meta
 {
   NT2_FUNCTOR_IMPLEMENTATION( cephes::tag::powi_, tag::cpu_
                             , (A0)(A1)
-                            , (long_double_<A0>)(long_double_<A1>)
+                            , (long_double_<A0>)(integer_<A1>)
                             )
   {
 
-    typedef typename meta::result_of<meta::floating(A0)>::type result_type;
+    typedef A0 result_type;
 
     NT2_FUNCTOR_CALL(2)
     { return cephes_powil(a0, a1); }
