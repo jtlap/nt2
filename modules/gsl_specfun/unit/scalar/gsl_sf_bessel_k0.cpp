22c04cd459a28e555dc65151ff1464559f52c58d
Mathias Gaunard <mathias@gaunard.com>
2011-07-02 19:53:02 +0200

    regenerate unit and cover tests for module gsl_specfun

--- modules/gsl_specfun/unit/scalar/gsl_sf_bessel_k0.cpp
+++ modules/gsl_specfun/unit/scalar/gsl_sf_bessel_k0.cpp
@@ -15,7 +15,6 @@
 /// 
 #include <nt2/toolbox/gsl_specfun/include/gsl_sf_bessel_k0.hpp>
 #include <nt2/include/functions/ulpdist.hpp>
-
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
