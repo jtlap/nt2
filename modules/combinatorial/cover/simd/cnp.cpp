1087f48408d7877944ac4ed4b9c5c3f4f9eb3c6f
Mathias Gaunard <mathias@gaunard.com>
2011-07-02 19:53:01 +0200

    regenerate unit and cover tests for module combinatorial

--- modules/combinatorial/cover/simd/cnp.cpp
+++ modules/combinatorial/cover/simd/cnp.cpp
@@ -28,7 +28,7 @@
 #include <nt2/include/functions/load.hpp>
 
 
-NT2_TEST_CASE_TPL ( cnp_real__2_0,  NT2_REAL_TYPES)
+NT2_TEST_CASE_TPL ( cnp_real__2_0,  NT2_SIMD_REAL_TYPES)
 {
   using nt2::cnp;
   using nt2::tag::cnp_;
