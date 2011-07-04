fe08452706a651936825cba602e3aea9409c8ce6
Joel Falcou <joel.falcou@lri.fr>
2011-07-04 11:12:55 -0500

    Fixed fast_sincospi

--- modules/trigonometric/include/nt2/toolbox/trigonometric/function/simd/common/fast_sincospi.hpp
+++ modules/trigonometric/include/nt2/toolbox/trigonometric/function/simd/common/fast_sincospi.hpp
@@ -17,25 +17,27 @@
 /////////////////////////////////////////////////////////////////////////////
 namespace nt2 { namespace meta
 {
-  NT2_FUNCTOR_IMPLEMENTATION_IF(tag::fast_sincospi_, tag::cpu_,(A0)(A1),
-                             (boost::mpl::equal_to<boost::mpl::sizeof_<A0>, 
-  			                           boost::mpl::sizeof_<A1> >), 
-                             ( tag::sincospi_
+  NT2_FUNCTOR_IMPLEMENTATION_IF(tag::fast_sincospi_, tag::cpu_,(A0)(A1)(X),
+                             (boost::mpl::equal_to<cardinal_of<A0>, 
+  			                           cardinal_of<A1> >
+                             ), 
+                             ( tag::fast_sincospi_
                                           ( simd_<arithmetic_<A0>,X> 
 					  , simd_<real_<A1>,X>
 				          , simd_<real_<A1>,X>
                                           )
                              ), 
-			     (simd_ < arithmetic_<A0> > )
-			     (simd_ < real_<A1> > )
-			     (simd_ < real_<A1> > )
+			     ((simd_ < arithmetic_<A0>, X > ))
+			     ((simd_ < real_<A1>, X > ))
+			     ((simd_ < real_<A1>, X > ))
 			     )
   {
-    typedef void result_type;    
+    typedef int result_type;    
     inline result_type operator()(A0 const& a0,A1 & a1,A1 & a2) const
     {
       impl::trig_base <A1,pi_tag,fast_tag,
 	               tag::simd_type>::sincosa(tofloat(a0),a1,a2); 
+      return 0;
     }
   };
 
