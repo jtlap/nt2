2d34f68a3b796970cec8be2635364c4583cff723
Mathias Gaunard <mathias@gaunard.com>
2011-07-08 10:24:34 +0200

    fill in master constant include

--- modules/simd_constant/include/boost/simd/toolbox/constant/constant.hpp
+++ modules/simd_constant/include/boost/simd/toolbox/constant/constant.hpp
@@ -9,5 +9,19 @@
 #ifndef BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANT_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANT_HPP_INCLUDED
 
+#include <boost/simd/toolbox/constant/include/digits.hpp>
+#include <boost/simd/toolbox/constant/include/eps_related.hpp>
+#include <boost/simd/toolbox/constant/include/false.hpp>
+#include <boost/simd/toolbox/constant/include/ieee_spec.hpp>
+#include <boost/simd/toolbox/constant/include/infinites.hpp>
+#include <boost/simd/toolbox/constant/include/max_left_shift.hpp>
+#include <boost/simd/toolbox/constant/include/real.hpp>
+#include <boost/simd/toolbox/constant/include/signmask.hpp>
+#include <boost/simd/toolbox/constant/include/true.hpp>
+#include <boost/simd/toolbox/constant/include/val_max.hpp>
+#include <boost/simd/toolbox/constant/include/val_min.hpp>
+
+#include <boost/simd/toolbox/constant/include/properties.hpp>
+#include <boost/simd/toolbox/constant/include/true_false.hpp>
 
 #endif
