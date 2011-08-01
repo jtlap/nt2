5a6c7c6cecba03b46643e3ab4ad491e640fd2437
Jt lapreste <jtlapreste@gmail.com>
2011-08-01 16:25:10 +0200

    boost/predicates hpp  updates

--- modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/function/scalar/is_not_equal_with_equal_nans.hpp
+++ modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/function/scalar/is_not_equal_with_equal_nans.hpp
@@ -16,10 +16,11 @@
 /////////////////////////////////////////////////////////////////////////////
 namespace boost { namespace dispatch { namespace meta
 {
-  BOOST_DISPATCH_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_not_equal_with_equal_nans_, tag::cpu_
-                            , (A0)(A1)
-                            , (scalar_< arithmetic_<A0> >)(scalar_< arithmetic_<A1> >)
-                            )
+  BOOST_DISPATCH_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_not_equal_with_equal_nans_,
+					 tag::cpu_, (A0)(A1), 
+					 (scalar_< arithmetic_<A0> >)
+					 (scalar_< arithmetic_<A1> >)
+					 )
   {
 
     typedef bool result_type;
@@ -37,17 +38,17 @@ namespace boost { namespace dispatch { namespace meta
 /////////////////////////////////////////////////////////////////////////////
 namespace boost { namespace dispatch { namespace meta
 {
-  BOOST_DISPATCH_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_not_equal_with_equal_nans_, tag::cpu_
-                            , (A0)(A1)
-                            , (scalar_< real_<A0> >)(scalar_< real_<A1> >)
-                            )
+  BOOST_DISPATCH_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_not_equal_with_equal_nans_,
+					 tag::cpu_, (A0)(A1), 
+					 (scalar_< real_<A0> >)(scalar_< real_<A1> >)
+					 )
   {
 
     typedef bool result_type;
 
     BOOST_DISPATCH_FUNCTOR_CALL(2)
     {
-      return   (a0 != a1) || (boost::simd::is_nan(a0) && boost::simd::is_nan(a1));
+      return   (a0 != a1) && !(boost::simd::is_nan(a0) && boost::simd::is_nan(a1));
     }
   };
 } } }
