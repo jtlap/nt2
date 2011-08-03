c1ba9749e773441ecd263a18a85c04eac2a6b4e8
Joel Falcou <joel.falcou@lri.fr>
2011-08-03 15:00:14 +0200

    Forward boost.simd.meta into nt2.meta

--- modules/sdk/include/nt2/sdk/meta/mutable_zip.hpp
+++ modules/sdk/include/nt2/sdk/meta/mutable_zip.hpp
@@ -6,8 +6,8 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#ifndef NT2_META_MUTABLE_ZIP_HPP_INCLUDED
-#define NT2_META_MUTABLE_ZIP_HPP_INCLUDED
+#ifndef NT2_SDK_META_MUTABLE_ZIP_HPP_INCLUDED
+#define NT2_SDK_META_MUTABLE_ZIP_HPP_INCLUDED
 
 ////////////////////////////////////////////////////////////////////////////////
 // Various memory hierarchy stuff
