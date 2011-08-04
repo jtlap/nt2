f0fda623158efb67231b618720be4689b366db41
Joel Falcou <joel.falcou@lri.fr>
2011-08-04 14:57:55 +0200

    Incorrect forward of simd tag

--- modules/sdk/include/nt2/sdk/simd/tags.hpp
+++ modules/sdk/include/nt2/sdk/simd/tags.hpp
@@ -6,12 +6,12 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#ifndef BOOST_SIMD_SDK_SIMD_TAGS_HPP_INCLUDED
-#define BOOST_SIMD_SDK_SIMD_TAGS_HPP_INCLUDED
+#ifndef NT2_SDK_SIMD_TAGS_HPP_INCLUDED
+#define NT2_SDK_SIMD_TAGS_HPP_INCLUDED
 
 #include <boost/simd/sdk/simd/tags.hpp>
 
-namespace nt2 { namespace simd { namespace tag
+namespace nt2 { namespace tag
 {
   using boost::simd::tag::not_simd_type;
   using boost::simd::tag::simd_type;
