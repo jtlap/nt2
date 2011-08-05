4a9ccedc9a400e5f54090c38ed860562b09c5fc4
Jt lapreste <jtlapreste@gmail.com>
2011-08-05 18:31:06 +0200

    hpp modifs for elliptic relative to boost.simd

--- modules/elliptic/include/nt2/toolbox/elliptic/function/scalar/ellipke.hpp
+++ modules/elliptic/include/nt2/toolbox/elliptic/function/scalar/ellipke.hpp
@@ -15,10 +15,11 @@
 #include <nt2/include/functions/sqr.hpp>
 #include <nt2/include/functions/average.hpp>
 #include <nt2/include/functions/oneminus.hpp>
+#include <nt2/include/functions/is_greater.hpp>
+#include <nt2/include/functions/is_equal.hpp>
+#include <nt2/include/functions/is_ltz.hpp>
 #include <nt2/toolbox/trigonometric/include/constants.hpp>
 #include <nt2/include/constants/real.hpp>
-#include <nt2/include/constants/infinites.hpp>
-#include <nt2/include/constants/eps_related.hpp>
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is arithmetic_
 /////////////////////////////////////////////////////////////////////////////
@@ -30,7 +31,7 @@ namespace nt2 { namespace meta
 			     )
   {
     typedef typename meta::result_of<meta::floating(A0)>::type etype;
-    typedef boost::fusion::tuple<etype, etype>                   result_type;
+    typedef boost::fusion::tuple<etype, etype>           result_type;
     
     NT2_FUNCTOR_CALL(1)
       {
