047e07ef65bcbe08e990b9df71f7e04893cd257f
Jt lapreste <jtlapreste@gmail.com>
2011-08-07 12:27:08 +0200

    missing nt2 namespace in euler const defs

--- modules/euler/include/nt2/toolbox/euler/function/scalar/constants.hpp
+++ modules/euler/include/nt2/toolbox/euler/function/scalar/constants.hpp
@@ -32,11 +32,11 @@
 // NT2_CONSTANT_MAKE_REAL(constants::stirling_largelim_   ,0x4061e083ba3443d4ll,0x41d628f6)// double 172.0     float  35.0399895
 // NT2_CONSTANT_MAKE_REAL(constants::digamma_largelim_    ,0x4034000000000000ll,0x41200000)
 // NT2_CONSTANT_MAKE_REAL(constants::fast_gamma_largelim_ ,0x4040800000000000ll,0x41200000)// double 33.0      float  10.0           
-
-NT2_MAKE_STD_CONSTANT(Stirlingsplitlim ,0x4061e083ba3443d4ll,0x41d628f6, 0)// double 143.01608 float  26.77 //TODO	 
-NT2_MAKE_STD_CONSTANT(Stirlinglargelim ,0x4061e083ba3443d4ll,0x41d628f6, 0)// double 172.0     float  35.0399895	 
-NT2_MAKE_STD_CONSTANT(Digammalargelim  ,0x4034000000000000ll,0x41200000, 0)					 
-NT2_MAKE_STD_CONSTANT(Fastgammalargelim,0x4040800000000000ll,0x41200000, 0)// double 33.0      float  10.0          
-
+namespace nt2 {
+  NT2_MAKE_STD_CONSTANT(Stirlingsplitlim ,0x4061e083ba3443d4ll,0x41d628f6, 0)// double 143.01608 float  26.77 //TODO	 
+  NT2_MAKE_STD_CONSTANT(Stirlinglargelim ,0x4061e083ba3443d4ll,0x41d628f6, 0)// double 172.0     float  35.0399895	 
+  NT2_MAKE_STD_CONSTANT(Digammalargelim  ,0x4034000000000000ll,0x41200000, 0)					 
+  NT2_MAKE_STD_CONSTANT(Fastgammalargelim,0x4040800000000000ll,0x41200000, 0)// double 33.0      float  10.0          
+  }
   
 #endif
