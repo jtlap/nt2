c723cfbad2a700a64a467bf136efd22dcbc7ab90
Jt lapreste <jtlapreste@gmail.com>
2011-08-08 19:01:32 +0200

    cover for combinatorial

--- modules/combinatorial/cover/scalar/gcd.cpp
+++ modules/combinatorial/cover/scalar/gcd.cpp
@@ -16,14 +16,23 @@
 #include <nt2/toolbox/combinatorial/include/gcd.hpp>
 #include <nt2/include/functions/ulpdist.hpp>
 #include <nt2/include/functions/max.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/as_real.hpp>
+#include <nt2/sdk/meta/as_signed.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+#include <nt2/sdk/meta/downgrade.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
+#include <nt2/sdk/meta/floating.hpp>
+#include <nt2/sdk/meta/arithmetic.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/upgrade.hpp>
 
 
 NT2_TEST_CASE_TPL ( gcd_real__2_0,  NT2_REAL_TYPES)
@@ -33,6 +42,7 @@ NT2_TEST_CASE_TPL ( gcd_real__2_0,  NT2_REAL_TYPES)
   using nt2::tag::gcd_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<gcd_(T,T)>::type r_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
   typedef typename boost::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
 
@@ -52,6 +62,7 @@ NT2_TEST_CASE_TPL ( gcd_unsigned_int__2_0,  NT2_UNSIGNED_TYPES)
   using nt2::tag::gcd_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<gcd_(T,T)>::type r_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
   typedef typename boost::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
 
@@ -71,6 +82,7 @@ NT2_TEST_CASE_TPL ( gcd_signed_int__2_0,  NT2_INTEGRAL_SIGNED_TYPES)
   using nt2::tag::gcd_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<gcd_(T,T)>::type r_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
   typedef typename boost::result_of<nt2::meta::arithmetic(T)>::type wished_r_t;
 
--- modules/combinatorial/cover/simd/anp.cpp
+++ modules/combinatorial/cover/simd/anp.cpp
@@ -18,11 +18,20 @@
 #include <nt2/include/functions/max.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/as_real.hpp>
+#include <nt2/sdk/meta/as_signed.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+#include <nt2/sdk/meta/downgrade.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
+#include <nt2/sdk/meta/floating.hpp>
+#include <nt2/sdk/meta/arithmetic.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
-#include <nt2/include/constants/infinites.hpp>
+#include <nt2/sdk/meta/cardinal_of.hpp>
+#include <nt2/include/functions/splat.hpp>
 #include <nt2/sdk/memory/is_aligned.hpp>
 #include <nt2/sdk/memory/aligned_type.hpp>
 #include <nt2/include/functions/load.hpp>
@@ -48,3 +57,45 @@ NT2_TEST_CASE_TPL ( anp_real__2_0,  NT2_SIMD_REAL_TYPES)
   ulpd=0.0;
 
 } // end of test for real_
+
+NT2_TEST_CASE_TPL ( anp_unsigned_int__2_0,  NT2_SIMD_UNSIGNED_TYPES)
+{
+  using nt2::anp;
+  using nt2::tag::anp_;
+  using nt2::load; 
+  using boost::simd::native;
+  using nt2::meta::cardinal_of;
+  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef typename nt2::meta::upgrade<T>::type   u_t;
+  typedef native<T,ext_t>                        n_t;
+  typedef n_t                                     vT;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef native<iT,ext_t>                       ivT;
+  typedef typename nt2::meta::call<anp_(vT,vT)>::type r_t;
+  typedef typename nt2::meta::call<anp_(T,T)>::type sr_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
+  double ulpd;
+  ulpd=0.0;
+
+} // end of test for unsigned_int_
+
+NT2_TEST_CASE_TPL ( anp_signed_int__2_0,  NT2_SIMD_INTEGRAL_SIGNED_TYPES)
+{
+  using nt2::anp;
+  using nt2::tag::anp_;
+  using nt2::load; 
+  using boost::simd::native;
+  using nt2::meta::cardinal_of;
+  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef typename nt2::meta::upgrade<T>::type   u_t;
+  typedef native<T,ext_t>                        n_t;
+  typedef n_t                                     vT;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef native<iT,ext_t>                       ivT;
+  typedef typename nt2::meta::call<anp_(vT,vT)>::type r_t;
+  typedef typename nt2::meta::call<anp_(T,T)>::type sr_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
+  double ulpd;
+  ulpd=0.0;
+
+} // end of test for signed_int_
