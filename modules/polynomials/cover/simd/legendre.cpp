b4d6bc7c8535082b89c502f0dffabf15eb6d5239
Jt lapreste <jtlapreste@gmail.com>
2011-08-08 15:33:04 +0200

    poly cpp cover

--- modules/polynomials/cover/simd/legendre.cpp
+++ modules/polynomials/cover/simd/legendre.cpp
@@ -20,11 +20,20 @@
 
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/as_real.hpp>
+#include <nt2/sdk/meta/as_signed.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+#include <nt2/sdk/meta/downgrade.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
+#include <nt2/sdk/meta/floating.hpp>
+#include <nt2/sdk/meta/arithmetic.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
-#include <nt2/include/constants/infinites.hpp>
+#include <nt2/sdk/meta/cardinal_of.hpp>
+#include <nt2/include/functions/splat.hpp>
 #include <nt2/sdk/memory/is_aligned.hpp>
 #include <nt2/sdk/memory/aligned_type.hpp>
 #include <nt2/include/functions/load.hpp>
@@ -35,7 +44,7 @@ NT2_TEST_CASE_TPL ( legendre_real__2_0,  NT2_SIMD_REAL_TYPES)
   using nt2::legendre;
   using nt2::tag::legendre_;
   using nt2::load; 
-  using nt2::simd::native;
+  using boost::simd::native;
   using nt2::meta::cardinal_of;
   typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
   typedef typename nt2::meta::upgrade<T>::type   u_t;
@@ -51,25 +60,6 @@ NT2_TEST_CASE_TPL ( legendre_real__2_0,  NT2_SIMD_REAL_TYPES)
 
   // random verifications
   static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,iT, NR, iT(0), iT(10));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(-1), T(1));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR/cardinal_of<n_t>::value; j++)
-      {
-        iT a0 = tab_a0[j];
-        vT a1 = load<vT>(&tab_a1[0],j);
-        r_t v = legendre(a0,a1);
-        for(int i = 0; i< cardinal_of<n_t>::value; i++)
-        {
-          int k = i+j*cardinal_of<n_t>::value;
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::legendre (tab_a0[j],tab_a1[k])), 50);
-          ulp0 = nt2::max(ulpd,ulp0);
-        }
-      }
-    std::cout << "max ulp found is: " << ulp0 << std::endl;
-  }
-  // random verifications
   {
     NT2_CREATE_BUF(tab_a0,iT, NR, iT(0), iT(10));
     NT2_CREATE_BUF(tab_a1,T, NR, T(-1), T(1));
