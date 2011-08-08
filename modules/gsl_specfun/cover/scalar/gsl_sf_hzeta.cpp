1d643fd6b1f651f008c3c79b184fcdf975609b4d
Jt lapreste <jtlapreste@gmail.com>
2011-08-08 12:31:18 +0200

    cover cpp for gsl_specfun

--- modules/gsl_specfun/cover/scalar/gsl_sf_hzeta.cpp
+++ modules/gsl_specfun/cover/scalar/gsl_sf_hzeta.cpp
@@ -18,11 +18,18 @@
 #include <nt2/include/functions/max.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/as_real.hpp>
+#include <nt2/sdk/meta/as_signed.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+#include <nt2/sdk/meta/downgrade.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
+#include <nt2/sdk/meta/floating.hpp>
+#include <nt2/sdk/meta/arithmetic.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
-#include <nt2/include/constants/infinites.hpp>
 
 
 NT2_TEST_CASE_TPL ( gsl_sf_hzeta_real__2_0,  NT2_REAL_TYPES)
@@ -32,6 +39,7 @@ NT2_TEST_CASE_TPL ( gsl_sf_hzeta_real__2_0,  NT2_REAL_TYPES)
   using nt2::gsl_specfun::tag::gsl_sf_hzeta_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<gsl_sf_hzeta_(T,T)>::type r_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
   typedef T wished_r_t;
 
