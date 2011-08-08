7ed045f1d251913a584cbea5a68e5a4fdf619ad5
Jt lapreste <jtlapreste@gmail.com>
2011-08-08 14:14:03 +0200

    correctives for boost.simd.ieee.saturate_at

--- modules/boost/simd/ieee/unit/scalar/saturate_at.cpp
+++ modules/boost/simd/ieee/unit/scalar/saturate_at.cpp
@@ -12,9 +12,8 @@
 // unit test behavior of boost.simd.ieee components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
 /// created  by jt the 20/03/2011
-///
-//COMMENTED
-//#include <boost/simd/toolbox/ieee/include/saturate_at.hpp>
+/// 
+#include <boost/simd/toolbox/ieee/include/saturate_at.hpp>
 #include <boost/simd/include/functions/ulpdist.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <boost/dispatch/functor/meta/call.hpp>
@@ -27,20 +26,28 @@
 NT2_TEST_CASE_TPL ( saturate_at_real__1_0,  BOOST_SIMD_REAL_TYPES)
 {
   
-//   using boost::simd::saturate_at;
-//   using boost::simd::tag::saturate_at_;
-//   typedef typename boost::dispatch::meta::as_integer<T>::type iT;
-//   typedef typename boost::dispatch::meta::call<saturate_at_<boost::simd::tag::Pi>(T)>::type r_t;
-//   typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-//   typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
-//   typedef typename boost::dispatch::meta::upgrade<T>::type u_t;
-//   typedef T wished_r_t;
+  using boost::simd::saturate_at;
+  using boost::simd::tag::saturate_at_;
+  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
+  typedef typename boost::dispatch::meta::call<saturate_at_<boost::simd::tag::Pi>(T)>::type r_t;
+  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
+  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
+  typedef typename boost::dispatch::meta::upgrade<T>::type u_t;
+  typedef T wished_r_t;
 
 
-//   // return type conformity test 
-//   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-//   std::cout << std::endl; 
-//   double ulpd;
-//   ulpd=0.0;
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+  ulpd=0.0;
 
+
+  // specific values tests
+  NT2_TEST_ULP_EQUAL(saturate_at<boost::simd::tag::Pi>(boost::simd::Inf<T>()), boost::simd::Pi<r_t>(), 0);
+  NT2_TEST_ULP_EQUAL(saturate_at<boost::simd::tag::Pi>(boost::simd::Minf<T>()), -boost::simd::Pi<r_t>(), 0);
+  NT2_TEST_ULP_EQUAL(saturate_at<boost::simd::tag::Pi>(boost::simd::Mone<T>()), boost::simd::Mone<r_t>(), 0);
+  NT2_TEST_ULP_EQUAL(saturate_at<boost::simd::tag::Pi>(boost::simd::Nan<T>()), boost::simd::Nan<r_t>(), 0);
+  NT2_TEST_ULP_EQUAL(saturate_at<boost::simd::tag::Pi>(boost::simd::One<T>()), boost::simd::One<r_t>(), 0);
+  NT2_TEST_ULP_EQUAL(saturate_at<boost::simd::tag::Pi>(boost::simd::Zero<T>()), boost::simd::Zero<r_t>(), 0);
 } // end of test for real_
