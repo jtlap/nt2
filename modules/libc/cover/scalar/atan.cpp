5b6f5a693d3e88b8a7d52c9d8e911b0f91b92304
Jt lapreste <jtlapreste@gmail.com>
2011-08-08 14:44:16 +0200

    libc cpp cover

--- modules/libc/cover/scalar/atan.cpp
+++ modules/libc/cover/scalar/atan.cpp
@@ -20,11 +20,18 @@
 
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/as_real.hpp>
+#include <nt2/sdk/meta/as_signed.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+#include <nt2/sdk/meta/downgrade.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
+#include <nt2/sdk/meta/floating.hpp>
+#include <nt2/sdk/meta/arithmetic.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
-#include <nt2/include/constants/infinites.hpp>
 
 
 NT2_TEST_CASE_TPL ( atan_real__1_0,  NT2_REAL_TYPES)
@@ -34,6 +41,7 @@ NT2_TEST_CASE_TPL ( atan_real__1_0,  NT2_REAL_TYPES)
   using nt2::libc::tag::atan_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<atan_(T)>::type r_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
   typedef T wished_r_t;
 
