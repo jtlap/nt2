6cfc7d7b2a8f639273c996796f4577646e0bdfd4
Jt lapreste <jtlapreste@gmail.com>
2011-08-08 10:59:01 +0200

    cover cpp for elliptic

--- modules/elliptic/cover/scalar/ellie.cpp
+++ modules/elliptic/cover/scalar/ellie.cpp
@@ -21,11 +21,18 @@ extern "C" {long double cephes_elliel(long double,long double);}
 
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/as_real.hpp>
+#include <nt2/sdk/meta/as_signed.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+#include <nt2/sdk/meta/downgrade.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
+#include <nt2/sdk/meta/floating.hpp>
+#include <nt2/sdk/meta/arithmetic.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/include/constants/real.hpp>
-#include <nt2/include/constants/infinites.hpp>
 
 
 NT2_TEST_CASE_TPL ( ellie_real__2_0,  NT2_REAL_TYPES)
@@ -35,6 +42,7 @@ NT2_TEST_CASE_TPL ( ellie_real__2_0,  NT2_REAL_TYPES)
   using nt2::tag::ellie_;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<ellie_(T,T)>::type r_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
   typedef typename nt2::meta::upgrade<T>::type u_t;
   typedef typename boost::result_of<nt2::meta::floating(T)>::type wished_r_t;
 
@@ -48,8 +56,8 @@ NT2_TEST_CASE_TPL ( ellie_real__2_0,  NT2_REAL_TYPES)
   // random verifications
   static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
-    NT2_CREATE_BUF(tab_a1,T, NR, T(0), T(1));
+    NT2_CREATE_BUF(tab_a0,T, NR, nt2::Valmin<T>(), nt2::Valmax<T>());
+    NT2_CREATE_BUF(tab_a1,T, NR, nt2::Valmin<T>(), nt2::Valmax<T>());
     double ulp0, ulpd ; ulpd=ulp0=0.0;
     T a0;
     T a1;
