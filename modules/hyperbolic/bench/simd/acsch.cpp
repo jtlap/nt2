c8712d3736a1f74f9fe8b4b4c645a9f547961e2e
Jt lapreste <jtlapreste@gmail.com>
2011-08-09 22:04:45 +0200

    benches for hyperbolic

--- modules/hyperbolic/bench/simd/acsch.cpp
+++ modules/hyperbolic/bench/simd/acsch.cpp
@@ -14,6 +14,7 @@
 #include <nt2/toolbox/hyperbolic/include/acsch.hpp>
 #include <nt2/sdk/unit/benchmark.hpp>
 #include <nt2/sdk/unit/bench_includes.hpp>
+#include <boost/dispatch/meta/as_integer.hpp>
 #include <cmath>
 typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
 
@@ -29,15 +30,15 @@ using nt2::tag::acsch_;
 
 namespace n1 {
   typedef float T;
-  typedef nt2::meta::as_integer<T>::type iT;
-  typedef nt2::simd::native<T,ext_t> vT;
+  typedef boost::dispatch::meta::as_integer<T>::type iT;
+  typedef boost::simd::native<T,ext_t> vT;
   NT2_TIMING(acsch_,(RS(vT,T(1),T(100))))
   NT2_TIMING(acsch_,(RS(vT,T(-100),T(-1))))
 }
 namespace n2 {
   typedef double T;
-  typedef nt2::meta::as_integer<T>::type iT;
-  typedef nt2::simd::native<T,ext_t> vT;
+  typedef boost::dispatch::meta::as_integer<T>::type iT;
+  typedef boost::simd::native<T,ext_t> vT;
   NT2_TIMING(acsch_,(RS(vT,T(1),T(100))))
   NT2_TIMING(acsch_,(RS(vT,T(-100),T(-1))))
 }
