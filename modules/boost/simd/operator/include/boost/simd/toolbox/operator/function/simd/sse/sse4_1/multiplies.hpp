937215e1525b2b470ff595ed253e17ce7e6db258
Jt lapreste <jtlapreste@gmail.com>
2011-08-11 16:09:09 +0200

    sse4 multiplies 32 bits integers

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/function/simd/sse/sse4_1/multiplies.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/function/simd/sse/sse4_1/multiplies.hpp
@@ -8,87 +8,12 @@
  ******************************************************************************/
 #ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTION_SIMD_SSE_SSE4_1_MULTIPLIES_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTION_SIMD_SSE_SSE4_1_MULTIPLIES_HPP_INCLUDED
+#include <boost/simd/toolbox/operator/function/simd/sse/sse4_1/multiplies.hpp>
 
-#include <boost/dispatch/meta/scalar_of.hpp>
-#include <boost/dispatch/meta/upgrade.hpp>
-#include <boost/simd/sdk/simd/native_cast.hpp>
-
-#include <boost/simd/include/functions/bitwise_or.hpp>
-#include <boost/simd/include/constants/digits.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::multiplies_, tag::cpu_
-                            , (A0)
-                            , ((simd_<double_<A0>,boost::simd::tag::sse_>))
-                              ((simd_<double_<A0>,boost::simd::tag::sse_>))
-                            )
-  {
-    typedef A0 result_type;
-
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      A0 that = { _mm_mul_pd(a0,a1) };
-      return that;
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::multiplies_, tag::cpu_
-                            , (A0)
-                            , ((simd_<float_<A0>,boost::simd::tag::sse_>))
-                              ((simd_<float_<A0>,boost::simd::tag::sse_>))
-                            )
-  {
-    typedef A0 result_type;
-
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      A0 that = { _mm_mul_ps(a0,a1) };
-      return that;
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::multiplies_, tag::cpu_
-                            , (A0)
-                            , ((simd_<ints8_<A0>,boost::simd::tag::sse_>))
-                              ((simd_<ints8_<A0>,boost::simd::tag::sse_>))
-                            )
-  {
-    typedef A0 result_type;
-
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      typedef typename dispatch::meta::upgrade<A0,signed>::type  type;
-
-      type mask1 = integral_constant<type,0x00FF>();
-      type a0_16 = {a0};
-      type a1_16 = {a1};
-      type al    = {_mm_srli_epi16(a0_16, 8)};
-      type bl    = {_mm_srli_epi16(a1_16, 8)};
-      type abh   = {_mm_and_si128(mask1, _mm_mullo_epi16(a0,a1))};
-      type ab    = {_mm_mullo_epi16(al,bl)};
-      type abl   = {_mm_slli_epi16(_mm_and_si128(mask1, ab), 8)};
-      A0 that    = { abh | abl };
-      return that;
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::multiplies_, tag::cpu_
-                            , (A0)
-                            , ((simd_<ints16_<A0>,boost::simd::tag::sse_>))
-                              ((simd_<ints16_<A0>,boost::simd::tag::sse_>))
-                            )
-  {
-    typedef A0 result_type;
-
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      A0 that = { _mm_mullo_epi16(a0, a1) };
-      return that;
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::multiplies_, tag::cpu_
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::multiplies_, boost::simd::tag::sse4_1_
                             , (A0)
                             , ((simd_<ints32_<A0>,boost::simd::tag::sse_>))
                               ((simd_<ints32_<A0>,boost::simd::tag::sse_>))
