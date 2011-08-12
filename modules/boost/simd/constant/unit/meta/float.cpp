4cf83e94d43e0d8772ef4a48eed5f8b4a2577726
Joel Falcou <joel.falcou@lri.fr>
2011-08-12 16:54:46 +0200

    Added a real_c meta type to acts as a real version of mpl::integral_c

---
+++ modules/boost/simd/constant/unit/meta/float.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#define NT2_UNIT_MODULE "boost::simd::meta float"
+
+#include <nt2/sdk/unit/module.hpp>
+#include <boost/simd/sdk/meta/float.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+// Test value of real_c constants for every base real types
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE( float_ )
+{
+  using boost::simd::meta::float_;
+  
+  float_<0x3F800000UL>          one_fc;
+  float  one_f = 1.f;
+  
+  NT2_TEST_EQUAL( one_f, float(one_fc)  );
+}
