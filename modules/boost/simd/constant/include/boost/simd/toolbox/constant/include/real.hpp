6a4a46edfe6049e489318550112fb309ed3c466d
Joel Falcou <joel.falcou@lri.fr>
2011-08-14 10:08:21 +0200

    Added stub for real constants

---
+++ modules/boost/simd/constant/include/boost/simd/toolbox/constant/include/real.hpp
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef BOOST_SIMD_TOOLBOX_CONSTANT_INCLUDE_REAL_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_CONSTANT_INCLUDE_REAL_HPP_INCLUDED
+
+#include <boost/simd/toolbox/constant/include/pi.hpp>
+#include <boost/simd/toolbox/constant/include/inf.hpp>
+#include <boost/simd/toolbox/constant/include/nan.hpp>
+#include <boost/simd/toolbox/constant/include/half.hpp>
+#include <boost/simd/toolbox/constant/include/minf.hpp>
+#include <boost/simd/toolbox/constant/include/gold.hpp>
+#include <boost/simd/toolbox/constant/include/sqrt2.hpp>
+#include <boost/simd/toolbox/constant/include/cgold.hpp>
+#include <boost/simd/toolbox/constant/include/mhalf.hpp>
+#include <boost/simd/toolbox/constant/include/mzero.hpp>
+#include <boost/simd/toolbox/constant/include/third.hpp>
+#include <boost/simd/toolbox/constant/include/sqrt2o2.hpp>
+#include <boost/simd/toolbox/constant/include/quarter.hpp>
+#include <boost/simd/toolbox/constant/include/twotom10.hpp>
+#include <boost/simd/toolbox/constant/include/twotonmb.hpp>
+#include <boost/simd/toolbox/constant/include/splitfactor.hpp>
+
+#endif
