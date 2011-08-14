031a41cebe0721ca59273e17e6d4e8f85942d86a
Joel Falcou <joel.falcou@lri.fr>
2011-08-14 18:57:49 +0200

    Added base cases for digits constants

---
+++ modules/boost/simd/constant/include/boost/simd/toolbox/constant/constants/mnine.hpp
@@ -0,0 +1,33 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_MNINE_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_MNINE_HPP_INCLUDED
+
+#include <boost/simd/include/simd.hpp>
+#include <boost/simd/sdk/meta/int_c.hpp>
+#include <boost/simd/sdk/constant/common.hpp>
+#include <boost/simd/sdk/constant/constant.hpp>
+
+namespace boost { namespace simd
+{
+  namespace tag
+  {
+    struct Mnine 
+    { 
+      typedef int default_type;
+      template<class Target, class Dummy=void> 
+      struct apply : meta::int_c<Target,-9> {};  
+    };
+  }
+
+  BOOST_SIMD_CONSTANT_IMPLEMENTATION(boost::simd::tag::Mnine, Mnine)
+} }
+#endif
+
+
