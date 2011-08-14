4102813ce328e50da2d9e4f2885e84290027a586
Joel Falcou <joel.falcou@lri.fr>
2011-08-14 19:32:17 +0200

    Added real specialisation for digit constants

---
+++ modules/boost/simd/constant/include/boost/simd/toolbox/constant/constants/generic/mseven.hpp
@@ -0,0 +1,27 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_GENERIC_MSEVEN_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_GENERIC_MSEVEN_HPP_INCLUDED
+
+#include <boost/simd/sdk/meta/float.hpp>
+#include <boost/simd/sdk/meta/double.hpp>
+
+namespace boost { namespace simd { namespace tag
+{
+  template<class Dummy>
+  struct  Mseven::apply<float,Dummy> 
+        : meta::float_<0xc0e00000UL> {};
+
+  template<class Dummy>
+  struct  Mseven::apply<double,Dummy> 
+        : meta::double_<0xc01c000000000000ULL> {};
+} } }
+#endif
+
+
---
+++ modules/boost/simd/constant/include/boost/simd/toolbox/constant/constants/generic/two.hpp
@@ -0,0 +1,27 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_GENERIC_TWO_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_GENERIC_TWO_HPP_INCLUDED
+
+#include <boost/simd/sdk/meta/float.hpp>
+#include <boost/simd/sdk/meta/double.hpp>
+
+namespace boost { namespace simd { namespace tag
+{
+  template<class Dummy>
+  struct  Two::apply<float,Dummy> 
+        : meta::float_<0x40000000UL> {};
+
+  template<class Dummy>
+  struct  Two::apply<double,Dummy> 
+        : meta::double_<0x4000000000000000ULL> {};
+} } }
+#endif
+
+
