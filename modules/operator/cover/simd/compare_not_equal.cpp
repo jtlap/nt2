99f2f9b8ddb69f63d1339e9072ca7d15959767e2
Jt lapreste <jtlapreste@gmail.com>
2011-08-17 16:01:32 +0200

    poly and operator regenerated cover

--- modules/operator/cover/simd/compare_not_equal.cpp
+++ modules/operator/cover/simd/compare_not_equal.cpp
@@ -18,6 +18,8 @@
 #include <nt2/include/functions/max.hpp>
 #include <nt2/include/functions/any.hpp>
 
+#include <nt2/include/functions/is_not_equal.hpp>
+
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/meta/as_integer.hpp>
@@ -31,12 +33,13 @@
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
-#include <nt2/include/constants/real.hpp>
+#include <nt2/toolbox/constant/constant.hpp>
 #include <nt2/sdk/meta/cardinal_of.hpp>
 #include <nt2/include/functions/splat.hpp>
 #include <nt2/sdk/memory/is_aligned.hpp>
 #include <nt2/sdk/memory/aligned_type.hpp>
 #include <nt2/include/functions/load.hpp>
+#include <nt2/toolbox/constant/constant.hpp>
 
 
 NT2_TEST_CASE_TPL ( compare_not_equal_real__2_0,  NT2_SIMD_REAL_TYPES)
