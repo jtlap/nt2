24c916abba10313ec803b9a165f85843c66ecd2a
Jt lapreste <jtlapreste@gmail.com>
2011-08-17 12:55:04 +0200

    py doc in trigo with test cover using long double pi

--- modules/trigonometric/doc/cotpi.py
+++ modules/trigonometric/doc/cotpi.py
@@ -9,7 +9,7 @@
             },
          'simd_types' : ['real_'],
          'special' : ['trigonometric'],
-         'type_defs' : [],
+         'type_defs' : [  'static const long double long_pi = 3.141592653589793238462643383279502884197l;'],
          'types' : ['real_', 'unsigned_int_', 'signed_int_'],
         },
      'info' : 'manually modified',
