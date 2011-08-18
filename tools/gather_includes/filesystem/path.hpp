ed1d4328d80dd21bf285416a34a59e895b022acf
Mathias Gaunard <mathias@gaunard.com>
2011-08-18 15:00:03 +0200

    adapting postconfigure tool to use header-only filesystem stuff

--- tools/gather_includes/filesystem/path.hpp
+++ tools/gather_includes/filesystem/path.hpp
@@ -24,6 +24,15 @@ namespace filesystem
         std::string::size_type const dot_position( path.find_last_of( '.' ) );
         return ( dot_position != std::string::npos ) ? path.substr( dot_position ) : std::string();
     }
+    
+    inline bool is_absolute( std::string const & path )
+    {
+    #ifdef BOOST_WINDOWS_API
+        return path.size() > 1 && path[1] == ':';
+    #else // POSIX
+        return !path.empty() && path[0] == '/';
+    #endif
+    }
 }
 
 #endif
