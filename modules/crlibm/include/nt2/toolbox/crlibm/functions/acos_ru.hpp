4a5fed8c403ade94a83e172eda85d6e256f52aa5
Joel Falcou <joel.falcou@metascale.org>
2011-08-25 16:50:33 +0200

    crlibm toolbox is refurbished to new preprocessor version

--- modules/crlibm/include/nt2/toolbox/crlibm/functions/acos_ru.hpp
+++ modules/crlibm/include/nt2/toolbox/crlibm/functions/acos_ru.hpp
@@ -1,27 +1,22 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_TOOLBOX_CRLIBM_FUNCTIONS_ACOS_RU_HPP_INCLUDED
 #define NT2_TOOLBOX_CRLIBM_FUNCTIONS_ACOS_RU_HPP_INCLUDED
 #include <nt2/include/simd.hpp>
 #include <nt2/include/functor.hpp>
+#include <nt2/toolbox/crlibm/specific/tags.hpp>
 
 namespace nt2 { namespace crlibm { namespace tag
-  {         
+  {
     struct acos_ru_ {};
   }
   NT2_FUNCTION_IMPLEMENTATION(crlibm::tag::acos_ru_, acos_ru, 1)
-  } }
- 
-#include <nt2/toolbox/crlibm/functions/scalar/acos_ru.hpp>
-// #include <nt2/toolbox/crlibm/functions/simd/all/acos_ru.hpp> 
+} }
 
- 
 #endif
-
-// modified by jt the 29/12/2010
--- modules/crlibm/include/nt2/toolbox/crlibm/functions/atan.hpp
+++ modules/crlibm/include/nt2/toolbox/crlibm/functions/atan.hpp
@@ -1,27 +1,22 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_TOOLBOX_CRLIBM_FUNCTIONS_ATAN_HPP_INCLUDED
 #define NT2_TOOLBOX_CRLIBM_FUNCTIONS_ATAN_HPP_INCLUDED
 #include <nt2/include/simd.hpp>
 #include <nt2/include/functor.hpp>
+#include <nt2/toolbox/crlibm/specific/interface.hpp>
 
 namespace nt2 { namespace crlibm { namespace tag
-  {         
+  {
     template <class T> struct atan_ {};
   }
   NT2_CRLIBM_FUNCTION_IMPLEMENTATION_TPL(atan, 1)
-  } }
- 
-#include <nt2/toolbox/crlibm/functions/scalar/atan.hpp>
-// #include <nt2/toolbox/crlibm/functions/simd/all/atan.hpp> 
+} }
 
- 
 #endif
-
-// modified by jt the 29/12/2010
