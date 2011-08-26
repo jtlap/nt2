ea34075a3ffe10c10d7fc57bc42e1e2c3542082a
Joel Falcou <joel.falcou@lri.fr>
2011-08-26 12:47:06 +0200

    Reworked avx max implementation to use the new nary_inter_overload file

--- modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/simd/sse/avx/max.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/simd/sse/avx/max.hpp
@@ -8,50 +8,16 @@
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_SSE_AVX_MAX_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_SSE_AVX_MAX_HPP_INCLUDED
-#ifdef BOOST_SIMD_HAS_AVX_SUPPORT
-#include <boost/simd/include/functions/seladd.hpp>
 
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type A0 is uint8_t
-/////////////////////////////////////////////////////////////////////////////
+#ifdef BOOST_SIMD_HAS_AVX_SUPPORT
+#include <boost/simd/sdk/meta/scalar_of.hpp>
+#include <boost/simd/sdk/simd/extensions/specific/avx/binary_integer_overload.hpp>
+
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::max_, boost::simd::tag::avx_,
-                      (A0),
-                      ((simd_<uint8_<A0>,boost::simd::tag::avx_>))
-                      ((simd_<uint8_<A0>,boost::simd::tag::avx_>))
-                     )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      BOOST_SIMD_AVX_JOIN128INT(that, _mm_max_epu8); return that;
-    }
-  };
-
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type A0 is arithmetic_
-/////////////////////////////////////////////////////////////////////////////
-
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::max_, boost::simd::tag::avx_,
-                      (A0),
-                      ((simd_<arithmetic_<A0>,boost::simd::tag::avx_>))
-                      ((simd_<arithmetic_<A0>,boost::simd::tag::avx_>))
-                     )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-       return seladd( is_lt(a0,a1),a0,a1-a0);
-    }
-  };
-
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type A0 is double
-/////////////////////////////////////////////////////////////////////////////
-
-
+  /////////////////////////////////////////////////////////////////////////////
+  // Implementation when type A0 is double
+  /////////////////////////////////////////////////////////////////////////////
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::max_, boost::simd::tag::avx_,
                       (A0),
                       ((simd_<double_<A0>,boost::simd::tag::avx_>))
@@ -62,24 +28,6 @@ namespace boost { namespace simd { namespace ext
     BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
     {
       A0 that =  {_mm256_max_pd(a0,a1)}; return that;
-    }
-  };
-
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type A0 is int32_t
-/////////////////////////////////////////////////////////////////////////////
-
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::max_, boost::simd::tag::avx_,
-                      (A0),
-                      ((simd_<int32_<A0>,boost::simd::tag::avx_>))
-                      ((simd_<int32_<A0>,boost::simd::tag::avx_>))
-                     )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      BOOST_SIMD_AVX_JOIN128INT(that, _mm_max_epi32); return that;
     }
   };
 
@@ -101,77 +49,6 @@ namespace boost { namespace simd { namespace ext
     }
   };
 
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type A0 is int8_t
-/////////////////////////////////////////////////////////////////////////////
-
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::max_, boost::simd::tag::avx_,
-                      (A0),
-                      ((simd_<int8_<A0>,boost::simd::tag::avx_>))
-                      ((simd_<int8_<A0>,boost::simd::tag::avx_>))
-                     )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      BOOST_SIMD_AVX_JOIN128INT(that, _mm_max_epi8); return that;
-    }
-  };
-
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type A0 is int16_t
-/////////////////////////////////////////////////////////////////////////////
-
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::max_, boost::simd::tag::avx_,
-                      (A0),
-                      ((simd_<int16_<A0>,boost::simd::tag::avx_>))
-                      ((simd_<int16_<A0>,boost::simd::tag::avx_>))
-                     )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      BOOST_SIMD_AVX_JOIN128INT(that, _mm_max_epi16); return that;
-    }
-  };
-
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type A0 is uint32_t
-/////////////////////////////////////////////////////////////////////////////
-
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::max_, boost::simd::tag::avx_,
-                      (A0),
-                      ((simd_<uint32_<A0>,boost::simd::tag::avx_>))
-                      ((simd_<uint32_<A0>,boost::simd::tag::avx_>))
-                     )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      BOOST_SIMD_AVX_JOIN128INT(that, _mm_max_epu32); return that;
-    }
-  };
-
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type A0 is uint16_t
-/////////////////////////////////////////////////////////////////////////////
-
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::max_, boost::simd::tag::avx_,
-                      (A0),
-                      ((simd_<uint16_<A0>,boost::simd::tag::avx_>))
-                      ((simd_<uint16_<A0>,boost::simd::tag::avx_>))
-                     )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      BOOST_SIMD_AVX_JOIN128INT(that, _mm_max_epu16); return that;
-    }
-  };
 } } }
 #endif
 #endif
