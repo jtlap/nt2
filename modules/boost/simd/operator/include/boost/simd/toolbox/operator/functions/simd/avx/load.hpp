7e70cc95fc67e848624f0e1f39d629f520655712
jt lapresté <jtlapreste@gmail.com>
2011-08-27 19:19:47 +0200

    splat load and store for avx

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/avx/load.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/avx/load.hpp
@@ -6,12 +6,12 @@
 //                 See accompanying file LICENSE.txt or copy at                 
 //                     http://www.boost.org/LICENSE_1_0.txt                     
 //==============================================================================
-#ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_SSE2_LOAD_HPP_INCLUDED
-#define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_SSE2_LOAD_HPP_INCLUDED
-#ifdef BOOST_SIMD_HAS_SSE2_SUPPORT
+#ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_AVX_LOAD_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_AVX_LOAD_HPP_INCLUDED
+#ifdef BOOST_SIMD_HAS_AVX_SUPPORT
 
 ////////////////////////////////////////////////////////////////////////////////
-// load for SSE2 SIMD types
+// load for AVX SIMD types
 ////////////////////////////////////////////////////////////////////////////////
 #include <boost/dispatch/meta/mpl.hpp>
 #include <boost/simd/sdk/simd/category.hpp>
@@ -19,13 +19,14 @@
 #include <boost/simd/sdk/meta/cardinal_of.hpp>
 #include <boost/dispatch/details/ignore_unused.hpp>
 #include <boost/dispatch/functor/preprocessor/call.hpp>
+#include <iostream>
 
 //==============================================================================
 // load vector of double
 //==============================================================================
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::load_ , boost::simd::tag::sse2_
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::load_ , boost::simd::tag::avx_
                             , (A0)(A1)(A2)(X)
                             , (iterator_< scalar_< fundamental_<A0> > >)
                               (scalar_< fundamental_<A1> >)
@@ -37,8 +38,7 @@ namespace boost { namespace simd { namespace ext
     BOOST_SIMD_FUNCTOR_CALL(3)
     {
       ignore_unused(a2);
-      result_type
-      that = { _mm_load_pd(a0+a1*boost::simd::meta::cardinal_of<result_type>::value) };
+      result_type that = { _mm256_load_pd(a0+a1*boost::simd::meta::cardinal_of<result_type>::value) };
       return that;
     }
   };
@@ -49,7 +49,7 @@ namespace boost { namespace simd { namespace ext
 //==============================================================================
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::load_ , boost::simd::tag::sse2_
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::load_ , boost::simd::tag::avx_
                             , (A0)(A1)(A2)(X)
                             , (iterator_< scalar_< fundamental_<A0> > >)
                               (scalar_< fundamental_<A1> >)
@@ -61,8 +61,7 @@ namespace boost { namespace simd { namespace ext
     BOOST_SIMD_FUNCTOR_CALL(3)
     {
       ignore_unused(a2);
-      result_type
-      that = { _mm_load_ps(a0+a1*boost::simd::meta::cardinal_of<result_type>::value) };
+      result_type that = {_mm256_load_ps(a0+a1*boost::simd::meta::cardinal_of<result_type>::value)}; 
       return that;
     }
   };
@@ -73,7 +72,7 @@ namespace boost { namespace simd { namespace ext
 //==============================================================================
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::load_ , boost::simd::tag::sse2_
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::load_ , boost::simd::tag::avx_
                             , (A0)(A1)(A2)(X)
                             , (iterator_< scalar_< fundamental_<A0> > >)
                               (scalar_< fundamental_<A1> >)
@@ -85,15 +84,14 @@ namespace boost { namespace simd { namespace ext
     BOOST_SIMD_FUNCTOR_CALL(3)
     {
       ignore_unused(a2);
-      result_type
-      that = { _mm_load_si128(reinterpret_cast<__m128i const*>(a0)+a1) };
+      result_type that = { _mm256_load_si256(reinterpret_cast<__m256i const*>(a0)+a1) };
       return that;
     }
   };
 } } }
 
 
-#include <boost/simd/toolbox/operator/functions/simd/sse/sse2/load_offset.hpp>
+#include <boost/simd/toolbox/operator/functions/simd/sse/avx/load_offset.hpp>
 
 #endif
 #endif
