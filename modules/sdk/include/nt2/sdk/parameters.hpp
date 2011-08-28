40ef3d1d97f7623cb19a17075bed6f49671b76dc
Joel Falcou <joel.falcou@lri.fr>
2011-08-28 15:51:45 +0200

    Moved parameters out of extension/

--- modules/sdk/include/nt2/sdk/parameters.hpp
+++ modules/sdk/include/nt2/sdk/parameters.hpp
@@ -6,8 +6,8 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#ifndef NT2_EXTENSION_PARAMETERS_HPP_INCLUDED
-#define NT2_EXTENSION_PARAMETERS_HPP_INCLUDED
+#ifndef NT2_SDK_PARAMETERS_HPP_INCLUDED
+#define NT2_SDK_PARAMETERS_HPP_INCLUDED
 
 ////////////////////////////////////////////////////////////////////////////////
 // Global parameters for nt2 code generator
