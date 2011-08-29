baf3d357e7c9c8f4b672ca28485935d4d98ef0d1
jt lapresté <jtlapreste@gmail.com>
2011-08-29 21:24:51 +0200

    avx operators

---
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/sse/avx/multiplies.hpp
@@ -0,0 +1,49 @@
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
+//                                                                              
+//          Distributed under the Boost Software License, Version 1.0.          
+//                 See accompanying file LICENSE.txt or copy at                 
+//                     http://www.boost.org/LICENSE_1_0.txt                     
+//==============================================================================
+#ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_AVX_AVX_MULTIPLIES_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_AVX_AVX_MULTIPLIES_HPP_INCLUDED
+#ifdef BOOST_SIMD_HAS_AVX_SUPPORT
+
+namespace boost { namespace simd { namespace ext
+{
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::multiplies_, boost::simd::tag::avx_
+                            , (A0)
+                            , ((simd_<double_<A0>,boost::simd::tag::avx_>))
+                              ((simd_<double_<A0>,boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A0 result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      A0 that = { _mm256_mul_pd(a0,a1) };
+      return that;
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::multiplies_, boost::simd::tag::avx_
+                            , (A0)
+                            , ((simd_<float_<A0>,boost::simd::tag::avx_>))
+                              ((simd_<float_<A0>,boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A0 result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      A0 that = { _mm256_mul_ps(a0,a1) };
+      return that;
+    }
+  };
+} } }
+
+
+
+#endif
+#endif
