8d48d1c30a39396715bba53a508635b04ddeec59
Mathias Gaunard <mathias@gaunard.com>
2011-08-29 17:51:35 +0200

    workaround boost.math bug #5823 to make it behave correctly

--- modules/boost/simd/ieee/include/boost/simd/toolbox/ieee/functions/scalar/nextafter.hpp
+++ modules/boost/simd/ieee/include/boost/simd/toolbox/ieee/functions/scalar/nextafter.hpp
@@ -10,10 +10,36 @@
 #define BOOST_SIMD_TOOLBOX_IEEE_FUNCTIONS_SCALAR_NEXTAFTER_HPP_INCLUDED
 
 #include <boost/simd/include/functions/sign.hpp>
-#include <boost/simd/include/constants/digits.hpp>
+#include <boost/simd/include/constants/one.hpp>
 
 #include <boost/simd/toolbox/ieee/details/math.hpp>
 #include <boost/math/special_functions/next.hpp>
+#include <boost/simd/include/constants/inf.hpp>
+#include <boost/simd/include/constants/minf.hpp>
+
+// workaround for boost.math bug #5823
+namespace boost { namespace simd { namespace details
+{
+  template<class T>
+  T nextafter(T a0, T a1)
+  {
+    if(a0 > a1 && a0 == Inf<T>())
+      return Valmax<T>();
+    if(a0 < a1 && a0 == Minf<T>())
+      return Valmin<T>();
+
+    using namespace boost::math::policies;
+    typedef policy<
+      domain_error<errno_on_error>,
+      pole_error<errno_on_error>,
+      overflow_error<errno_on_error>,
+      evaluation_error<errno_on_error>,
+      rounding_error<errno_on_error>
+    > c_policy;
+
+    return boost::math::nextafter(a0, a1, c_policy());
+  }
+} } }
 
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type A0 is arithmetic_
@@ -56,7 +82,7 @@ namespace boost { namespace simd { namespace ext
     #elif defined(BOOST_SIMD_TOOLBOX_IEEE_HAS__NEXTAFTER)
       return ::_nextafter(a0, a1);
     #else
-      return boost::math::nextafter(a0, a1);
+      return details::nextafter(a0, a1);
     #endif
     }
   };
@@ -83,7 +109,7 @@ namespace boost { namespace simd { namespace ext
     #elif defined(BOOST_SIMD_TOOLBOX_IEEE_HAS__NEXTAFTERF)
       return ::_nextafterf(a0, a1);
     #else
-      return boost::math::nextafter(a0, a1);
+      return details::nextafter(a0, a1);
     #endif
     }
   };
