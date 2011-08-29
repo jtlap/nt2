9a23fe8b90f5bffd2f14237341c75a1ba1415f1c
pesterie <pesterie@gmail.com>
2011-08-29 17:52:17 +0200

    Cleaning new files for doc

--- modules/boost/simd/sdk/include/boost/simd/sdk/config/details/masks.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/config/details/masks.hpp
@@ -1,23 +1,24 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef BOOST_SIMD_SDK_CONFIG_DETAILS_MASKS_HPP_INCLUDED
 #define BOOST_SIMD_SDK_CONFIG_DETAILS_MASKS_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// Here is the registration of the cpuid masks coming from extensions headers.
-// All SIMD extension detections go down to file.
-// When a new SIMD extension needs to be detected at runtime, you can register
-// this extension by specializing cpuid_mask on the extension's tag and the 
-// vendor. You just need to provide a member function that describes how to 
-// detect the extension at runtime and return a boolean.
-// You need to follow the same way to add a new vendor detection at runtime. 
-////////////////////////////////////////////////////////////////////////////////
+/*!
+ *\file masks.hpp
+ *\brief Here is the registration of the cpuid masks coming from extensions headers.
+ * All SIMD extension detections go down to file.
+ * When a new SIMD extension needs to be detected at runtime, you can register
+ * this extension by specializing cpuid_mask on the extension's tag and the 
+ * vendor. You just need to provide a member function that describes how to 
+ * detect the extension at runtime and return a boolean.
+ * You need to follow the same way to add a new vendor detection at runtime. 
+ */
 
 #include <boost/simd/sdk/config/details/masks/forward.hpp>
 #include <boost/simd/sdk/config/details/masks/vendor.hpp>
--- modules/boost/simd/sdk/include/boost/simd/sdk/config/is_supported.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/config/is_supported.hpp
@@ -1,19 +1,21 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef BOOST_SIMD_SDK_CONFIG_IS_SUPPORTED_HPP_INCLUDED
 #define BOOST_SIMD_SDK_CONFIG_IS_SUPPORTED_HPP_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-// Runtime function returning the SIMD extension supported. 
-// For detecting a new extenion from a new vendor you need to add a vendor case
-// and provide the right specialization of cpuid_mask.  
-////////////////////////////////////////////////////////////////////////////////
+/*!
+ *\file is_supported.hpp
+ * \brief Runtime function returning the SIMD extension supported. 
+ * For detecting a new extenion from a new vendor you need to add a 
+ * vendor case and provide the right specialization of cpuid_mask.  
+*/
+
 #include <boost/simd/sdk/config/details/cpuid.hpp>
 #include <boost/simd/sdk/config/details/masks.hpp>
 #include <boost/simd/sdk/config/details/get_vendor.hpp>
