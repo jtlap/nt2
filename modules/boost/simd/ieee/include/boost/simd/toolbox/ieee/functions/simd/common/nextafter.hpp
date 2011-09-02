44fac59d1203398ec912b86996f2a6ba205cf4a6
Jt lapreste <jtlapreste@gmail.com>
2011-09-02 09:56:24 +0200

    nextafter common

--- modules/boost/simd/ieee/include/boost/simd/toolbox/ieee/functions/simd/common/nextafter.hpp
+++ modules/boost/simd/ieee/include/boost/simd/toolbox/ieee/functions/simd/common/nextafter.hpp
@@ -8,21 +8,16 @@
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_IEEE_FUNCTIONS_SIMD_COMMON_NEXTAFTER_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_IEEE_FUNCTIONS_SIMD_COMMON_NEXTAFTER_HPP_INCLUDED
-#include <boost/simd/include/constants/infinites.hpp>
-#include <boost/simd/include/constants/real.hpp>
-#include <boost/dispatch/meta/as_integer.hpp>
-#include <boost/simd/include/constants/properties.hpp>
-#include <boost/simd/include/constants/digits.hpp>
-#include <boost/simd/include/constants/eps_related.hpp>
-#include <boost/fusion/tuple.hpp>
-#include <boost/dispatch/meta/strip.hpp>
+#include <boost/simd/include/functions/is_equal.hpp>
+#include <boost/simd/include/functions/is_not_equal.hpp>
+#include <boost/simd/include/functions/is_greater.hpp>
+#include <boost/simd/include/functions/seladd.hpp>
+#include <boost/simd/include/functions/select.hpp>
 #include <boost/simd/include/functions/next.hpp>
 #include <boost/simd/include/functions/prev.hpp>
-#include <boost/simd/include/functions/is_finite.hpp>
-#include <boost/simd/include/functions/is_eqz.hpp>
-#include <boost/simd/include/functions/negif.hpp>
-#include <boost/simd/include/functions/copysign.hpp>
-
+#include <boost/simd/include/constants/mone.hpp>
+#include <boost/simd/include/constants/two.hpp>
+#include <boost/simd/include/constants/one.hpp>
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -85,17 +80,17 @@ namespace boost { namespace simd { namespace ext
 
     BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
     {
-      typedef typename dispatch::meta::as_integer<A0, signed>::type itype;
-      A0 m;
-      itype expon;
-      boost::fusion::tie(m, expon) = fast_frexp(a0);
-      expon =  seladd(boost::simd::is_equal(m, Mhalf<A0>()), expon, Mone<itype>());
-      A0 diff =  fast_ldexp(One<A0>(), expon-Nbdigits<A0>());
-      diff = b_and(sel(is_eqz(diff)||is_eqz(a0),  Mindenormal<A0>(), diff), is_finite(a0));
-      A0 r = copysign(sel(boost::simd::is_equal(a0, Minf<A0>()), Valmin<A0>(), a0), a0);
-      diff   =  b_and(negif(gt(a0, a1), diff), is_not_equal(a0, a1));
-      return r+diff;
-      //      return sel(islt(a0, a1), next(a0), sel(iseq(a0, a1),  a0, prev(a0)));
+//       typedef typename dispatch::meta::as_integer<A0, signed>::type itype;
+//       A0 m;
+//       itype expon;
+//       boost::fusion::tie(m, expon) = fast_frexp(a0);
+//       expon =  seladd(boost::simd::is_equal(m, Mhalf<A0>()), expon, Mone<itype>());
+//       A0 diff =  fast_ldexp(One<A0>(), expon-Nbdigits<A0>());
+//       diff = b_and(sel(is_eqz(diff)||is_eqz(a0),  Mindenormal<A0>(), diff), is_finite(a0));
+//       A0 r = copysign(sel(boost::simd::is_equal(a0, Minf<A0>()), Valmin<A0>(), a0), a0);
+//       diff   =  b_and(negif(gt(a0, a1), diff), is_not_equal(a0, a1));
+//       return r+diff;
+      return sel(lt(a0, a1), next(a0), select(eq(a0, a1),  a0, prev(a0)));
     }
   };
 } } }
