13e92b49e83590ae96cdd6e010b62305fb643869
jt lapresté <jtlapreste@gmail.com>
2011-09-05 14:17:58 +0200

    -Wall warning free euler cover

--- modules/euler/cover/simd/expni.cpp
+++ modules/euler/cover/simd/expni.cpp
@@ -70,9 +70,9 @@ NT2_TEST_CASE_TPL ( expni_real__2_0,  NT2_SIMD_REAL_TYPES)
         iT a0 = tab_a0[j];
         vT a1 = load<vT>(&tab_a1[0],j);
         r_t v = expni(a0,a1);
-        for(int i = 0; i< cardinal_of<n_t>::value; i++)
+        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
         {
-          int k = i+j*cardinal_of<n_t>::value;
+          nt2::uint32_t k = i+j*cardinal_of<n_t>::value;
           NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::expni (tab_a0[j],tab_a1[k])), 10);
           ulp0 = nt2::max(ulpd,ulp0);
         }
