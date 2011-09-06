4d99039b5c0bfc4d94da5afc4a4d9bc0bf7bb4b0
jt lapresté <jtlapreste@gmail.com>
2011-09-06 17:34:26 +0200

    unused variables in at_i.hpp

--- modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/scalar/at_i.hpp
+++ modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/scalar/at_i.hpp
@@ -20,7 +20,7 @@ namespace boost { namespace simd { namespace ext
                    )
   {
     typedef A0 result_type;
-    inline result_type operator()(A0 const&)const 
+    inline result_type operator()(A0 const& a0,A1 const&)const 
     {
       return a0;
     };
