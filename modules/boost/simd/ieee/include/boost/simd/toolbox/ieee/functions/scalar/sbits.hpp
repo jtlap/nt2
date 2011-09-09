be72adf623fddb66d9335fdd694c62489ee307b9
Joel Falcou <joel.falcou@metascale.org>
2011-09-09 09:58:40 +0200

    More missing include in no SIMD mode

--- modules/boost/simd/ieee/include/boost/simd/toolbox/ieee/functions/scalar/sbits.hpp
+++ modules/boost/simd/ieee/include/boost/simd/toolbox/ieee/functions/scalar/sbits.hpp
@@ -1,14 +1,16 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_IEEE_FUNCTIONS_SCALAR_SBITS_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_IEEE_FUNCTIONS_SCALAR_SBITS_HPP_INCLUDED
+
 #include <boost/dispatch/meta/as_integer.hpp>
+#include <boost/simd/sdk/details/bitwise_cast.hpp>
 
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type  is fundamental_
