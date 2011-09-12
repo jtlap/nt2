a328c1a3aef11684baeadc1ea5060d0bdd99703d
Joel Falcou <joel.falcou@lri.fr>
2011-09-12 17:40:12 +0200

    Added dereference function as part of the Buffer concept. Updated array adaptation

--- modules/core/include/nt2/core/container/memory/adapted/array.hpp
+++ modules/core/include/nt2/core/container/memory/adapted/array.hpp
@@ -9,26 +9,98 @@
 #ifndef NT2_CORE_CONTAINER_MEMORY_ADAPTED_ARRAY_HPP
 #define NT2_CORE_CONTAINER_MEMORY_ADAPTED_ARRAY_HPP
 
+#include <boost/mpl/min.hpp>
+#include <boost/mpl/size.hpp>
 #include <boost/mpl/size_t.hpp>
+#include <boost/mpl/assert.hpp>
+#include <boost/fusion/include/mpl.hpp>
+#include <boost/fusion/adapted/array.hpp>
+#include <nt2/core/container/meta/reference.hpp>
 #include <nt2/core/container/meta/dimensions_of.hpp>
 
 //==============================================================================
 // Forward declaration
 //==============================================================================
-namespace boost
-{  
-  template<class T, std::size_t N> class array;
-}
+namespace boost { template<class T, std::size_t N> class array; }
 
 //==============================================================================
 // Fill out the Buffer concepts for boost::array
 //==============================================================================
 namespace nt2 { namespace meta
 {
-  template<class T, std::size_t N>
+  //============================================================================
+  // dimensions_of specialization
+  //============================================================================
+  template<typename T, std::size_t N>
   struct  dimensions_of< boost::array<T,N> > 
         : boost::mpl::size_t<1 + dimensions_of<T>::value> 
-  {};
+  {};  
+
+  //============================================================================
+  // reference_ specialization
+  //============================================================================
+  template<typename T, std::size_t N, std::size_t Level> 
+  struct reference_<boost::array<T,N>,Level>
+  {
+    typedef typename boost::array<T,N>::value_type  base;
+    typedef typename reference_<base,Level-1>::type type;
+  };
+
+  template<typename T, std::size_t N> 
+  struct reference_<boost::array<T,N>,1UL>
+  {
+    typedef typename boost::array<T,N>::reference type;
+  };
+
+  //============================================================================
+  // const_reference_ specialization
+  //============================================================================
+  template<typename T, std::size_t N, std::size_t Level> 
+  struct const_reference_<boost::array<T,N>,Level>
+  {
+    typedef typename boost::array<T,N>::value_type        base;
+    typedef typename const_reference_<base,Level-1>::type type;
+  };
+
+  template<typename T, std::size_t N> 
+  struct const_reference_<boost::array<T,N>,1UL>
+  {
+    typedef typename boost::array<T,N>::const_reference type;
+  };
+} }
+
+namespace nt2 { namespace memory
+{
+  //============================================================================
+  // boost::array initialize - Part of Buffer Concept
+  //============================================================================
+  template<typename T, std::size_t N, typename Sizes, typename Bases>
+  inline void initialize( boost::array<T,N>&, Sizes const&, Bases const&)
+  {
+    //==========================================================================
+    // A boost::array is already initialized, we just check that we don't
+    // try to instanciated it with too few or too much dimensions.
+    //==========================================================================
+    BOOST_MPL_ASSERT_MSG
+    ( (   meta::dimensions_of<boost::array<T,N> >::value 
+      ==  boost::mpl::size<Sizes>::value
+      ) 
+    , NT2_CORE_CONTAINER_MEMORY_INITIALIZATION_SIZE_MISMATCH
+    , (Sizes, boost::array<T,N>)
+    );
+
+    //==========================================================================
+    // A boost::array is already initialized, we just check that we don't
+    // try to instanciated it with too few or too much base indices.
+    //==========================================================================
+    BOOST_MPL_ASSERT_MSG
+    ( (   meta::dimensions_of<boost::array<T,N> >::value 
+      ==  boost::mpl::size<Bases>::value
+      ) 
+    , NT2_CORE_CONTAINER_MEMORY_INITIALIZATION_BASES_MISMATCH
+    , (Bases, boost::array<T,N>)
+    );
+  }
 } }
 
 #endif
---
+++ modules/core/include/nt2/core/container/memory/dereference.hpp
@@ -0,0 +1,76 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_CONTAINER_MEMORY_DEREFERENCE_HPP
+#define NT2_CORE_CONTAINER_MEMORY_DEREFERENCE_HPP
+
+//==============================================================================
+/**
+  * \file 
+  * \brief Defines and implements the \c nt2::memory::dereference class
+  **/
+//==============================================================================
+namespace nt2 { namespace details
+{
+  template< std::size_t Level, std::size_t Dims>
+  struct dereference
+  {
+    template<typename Buffer, typename Position>
+    static inline typename meta::reference_<Buffer,Level>::type 
+    apply( Buffer& b, Position const& p )
+    {
+      return  dereference<Level-1,Dims>
+              ::apply(b[boost::fusion::at_c<Level-1>(p)], p);
+    }
+
+    template<typename Buffer, typename Position>
+    static inline typename meta::const_reference_<Buffer,Level>::type 
+    apply( Buffer const& b, Position const& p )
+    {
+      return  dereference<Level-1,Dims>
+              ::apply(b[boost::fusion::at_c<Level-1>(p)], p);
+    }
+  };
+
+  template<std::size_t Dims>
+  struct dereference<1,Dims>
+  {
+    template<typename Buffer, typename Position>
+    static inline typename meta::reference_<Buffer>::type 
+    apply( Buffer& b, Position const& p )
+    {
+      return b[boost::fusion::at_c<0>(p)];
+    }
+
+    template<typename Buffer, typename Position>
+    static inline typename meta::const_reference_<Buffer>::type 
+    apply( Buffer const& b, Position const& p )
+    {
+      return b[boost::fusion::at_c<0>(p)];
+    }
+  };
+} }
+
+namespace nt2 { namespace memory
+{
+  template<std::size_t Level, typename Buffer, typename Position>
+  inline typename meta::reference_<Buffer,Level>::type 
+  dereference( Buffer& b, Position const& p )
+  {
+    return details::dereference<Level,meta::dimensions_of<Buffer>::value>::apply(b,p);
+  }
+
+  template<std::size_t Level, typename Buffer, typename Position>
+  inline typename meta::const_reference_<Buffer,Level>::type 
+  dereference( Buffer const& b, Position const& p )
+  {
+    return details::dereference<Level,meta::dimensions_of<Buffer>::value>::apply(b,p);
+  }
+} }
+
+#endif
