1be5dc4d97934dcd12728bef57051bd2c2295adb
Mathias Gaunard <mathias@gaunard.com>
2011-09-20 15:15:31 +0200

    make True and False on scalar return a bool

--- modules/boost/simd/constant/include/boost/simd/toolbox/constant/constants/scalar/true.hpp
+++ modules/boost/simd/constant/include/boost/simd/toolbox/constant/constants/scalar/true.hpp
@@ -6,22 +6,21 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_SIMD_COMMON_TRUE_HPP_INCLUDED
-#define BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_SIMD_COMMON_TRUE_HPP_INCLUDED
+#ifndef BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_SCALAR_TRUE_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_CONSTANT_CONSTANTS_SCALAR_TRUE_HPP_INCLUDED
 
 #include <boost/simd/toolbox/constant/constants/true.hpp>
-#include <boost/simd/include/constants/allbits.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( simd::tag::True, tag::cpu_, (A0)(X)
-                                    , ((target_< simd_< arithmetic_<A0>,X> >))
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( simd::tag::True, tag::cpu_, (A0)
+                                    , ((target_< scalar_< arithmetic_<A0> > >))
                                     )
   {
-    typedef typename A0::type result_type;
+    typedef bool result_type;
     BOOST_DISPATCH_FORCE_INLINE result_type operator()(A0 const&) const
     {
-      return Allbits<result_type>();
+      return true;
     }
   };
 } } }
