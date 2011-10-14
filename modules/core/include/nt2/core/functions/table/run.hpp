98dbfeb77996e3bebc9b4f312096e6e48843036f
Joel Falcou <joel.falcou@lri.fr>
2011-10-14 14:37:50 +0200

    Specialization of extent and run for table and table expression

---
+++ modules/core/include/nt2/core/functions/table/run.hpp
@@ -0,0 +1,76 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_FUNCTIONS_TABLE_RUN_HPP_INCLUDED
+#define NT2_CORE_FUNCTIONS_TABLE_RUN_HPP_INCLUDED
+
+#include <nt2/include/functor.hpp>
+#include <nt2/dsl/functions/run.hpp>
+#include <nt2/core/container/table/table.hpp>
+#include <nt2/core/container/meta/runner.hpp>
+#include <nt2/core/container/meta/loop_nest.hpp>
+#include <nt2/core/container/meta/lhs_terminal.hpp>
+
+namespace nt2 { namespace ext
+{
+  //============================================================================
+  // When an assign(lhs,rhs) expression is run, we perform the evaluation of rhs
+  // then store it in lhs. Depending on the lhs nature (real terminal or a node
+  // containing a call to any indexing function, the result of the evaluation is
+  // returned, usually as non-const reference.
+  //============================================================================
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_, tag::cpu_
+                            , (A0)(S0)
+                            , ((expr_<  table_< unspecified_<A0>, S0 >
+                                     , nt2::container::domain
+                                     , nt2::tag::assign_
+                                     >
+                              ))
+                            )
+  {
+    typedef typename boost::dispatch::meta::
+            terminal_of < typename boost::dispatch::meta::
+                          semantic_of<A0>::type
+                        >::type                             result_type;
+
+    BOOST_DISPATCH_FORCE_INLINE result_type
+    operator()(A0 const& a0) const
+    {
+      meta::for_each(base_indices(a0), extent(a0), meta::runner<A0 const&>(a0));
+      return meta::lhs_terminal()(a0);
+    }
+  };
+
+  //============================================================================
+  // When an arbitrary expression is run, we perform its evaluation into a
+  // local temporary container of proper type.This temporary is then returned by
+  // value.
+  //============================================================================
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_, tag::cpu_
+                            , (A0)(S0)
+                            , ((ast_<table_< unspecified_<A0>, S0 > >))
+                            )
+  {
+    typedef typename boost::
+    remove_reference< typename boost::dispatch::meta::
+                      terminal_of< typename boost::dispatch::meta::
+                                   semantic_of<A0>::type
+                                 >::type
+                    >::type                                result_type;
+
+    BOOST_DISPATCH_FORCE_INLINE result_type
+    operator()(A0 const& a0) const
+    {
+      result_type tmp;
+      run(assign(tmp, a0));
+      return tmp;
+    }
+  };
+} }
+
+#endif
