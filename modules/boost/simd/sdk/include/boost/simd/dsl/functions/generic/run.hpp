55c970bd6c320d924b9f76511908e2db359240a9
Joel Falcou <joel.falcou@lri.fr>
2011-10-16 17:57:01 +0200

    Made geenric run function handle the full (Xpr,State,Data) case for proto expression

--- modules/boost/simd/sdk/include/boost/simd/dsl/functions/generic/run.hpp
+++ modules/boost/simd/sdk/include/boost/simd/dsl/functions/generic/run.hpp
@@ -17,6 +17,9 @@
 
 namespace boost { namespace simd { namespace ext
 {
+  //============================================================================
+  // Run an expression witohut state nor data
+  //============================================================================
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::run_, tag::formal_
                             , (A0)
                             , (ast_< unspecified_<A0> >)
@@ -49,7 +52,10 @@ namespace boost { namespace simd { namespace ext
       return transform()(a0);
     }
   };
-  
+
+  //============================================================================
+  // Run an expression with a state and no data
+  //============================================================================  
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::run_, tag::cpu_
                             , (A0)(State)
                             , (ast_< unspecified_<A0> >)
@@ -108,6 +114,74 @@ namespace boost { namespace simd { namespace ext
     operator()(A0_& a0, State_& state) const
     {
       return typename dispatch::make_functor<tag::terminal_, A0>::type()(a0, state);
+    }
+  };
+
+
+  //============================================================================
+  // Run an expression with a state and a data
+  //============================================================================  
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::run_, tag::cpu_
+                            , (A0)(State)(Data)
+                            , (ast_< unspecified_<A0> >)
+                              (unspecified_<State>)
+                              (unspecified_<Data>)
+                            )
+  {
+    typedef proto::
+    unpack< proto::
+            call< dispatch::
+                  functor< typename proto::tag_of<A0>::type >
+                > ( proto::
+                    when< proto::_
+                        , proto::call< typename dispatch::
+                                       make_functor< tag::run_, A0>::type( proto::_expr
+                                                                         , proto::_state
+                                                                         , proto::_data
+                                                                         )
+                                     >
+                        >
+                  )
+          >
+    transform;
+    
+    template<class Sig>
+    struct result;
+    
+    template<class This, class A0_, class State_, class Data_>
+    struct result<This(A0_, State_, Data_)>
+      : transform::template result<transform(A0_, State_, Data_)>
+    {
+    };
+    
+    template<class A0_, class State_, class Data_>
+    BOOST_DISPATCH_FORCE_INLINE typename result<implement(A0_&, State_&, Data_&)>::type
+    operator()(A0_& a0, State_& state, Data_& data) const
+    {
+      return transform()(a0, state, data);
+    }
+  };
+  
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::run_, tag::cpu_
+                            , (A0)(Domain)(State)(Data)
+                            , ((expr_< unspecified_<A0>, Domain, tag::terminal_ >))
+                              (unspecified_<State>)
+                              (unspecified_<Data>)
+                            )
+  {
+    template<class Sig>
+    struct result;
+    
+    template<class This, class A0_, class State_, class Data_>
+    struct result<This(A0_, State_, Data_)>
+      : dispatch::meta::call<tag::terminal_(A0_, State_, Data_)>
+    {};
+      
+    template<class A0_, class State_, class Data_>
+    BOOST_DISPATCH_FORCE_INLINE typename result<implement(A0_&, State_&, Data_&)>::type
+    operator()(A0_& a0, State_& state, Data_& data) const
+    {
+      return typename dispatch::make_functor<tag::terminal_, A0>::type()(a0, state,data);
     }
   };
   
