b2fc374720452196a8cc016c92ce89ce2f3ef7a7
Mathias Gaunard <mathias@gaunard.com>
2011-10-27 19:58:23 +0200

    small include fixes

--- modules/core/include/nt2/core/container/dsl/details/elementwise.hpp
+++ modules/core/include/nt2/core/container/dsl/details/elementwise.hpp
@@ -10,10 +10,10 @@
 #define NT2_CORE_CONTAINER_DSL_DETAILS_ELEMENTWISE_HPP_INCLUDED
 
 #include <nt2/core/container/dsl/generator.hpp>
+#include <nt2/sdk/error/assert.hpp>
 #include <boost/fusion/include/transform.hpp>
 #include <boost/fusion/include/fold.hpp>
 #include <boost/fusion/include/at_c.hpp>
-#include <boost/assert.hpp>
 
 namespace nt2 { namespace container { namespace ext
 {
