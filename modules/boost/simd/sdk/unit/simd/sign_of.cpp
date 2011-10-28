fd6a653efc426334f51d8fa0c67f5bb57c58842c
Joel Falcou <joel.falcou@lri.fr>
2011-10-28 16:21:06 +0200

    Fixed sign_of behavior with respect to logical

--- modules/boost/simd/sdk/unit/simd/sign_of.cpp
+++ modules/boost/simd/sdk/unit/simd/sign_of.cpp
@@ -1,37 +1,52 @@
-/*******************************************************************************
- *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
- *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #define NT2_UNIT_MODULE "boost::simd::meta::sign_of SIMD"
 
 #include <boost/simd/sdk/simd/native.hpp>
+#include <boost/simd/sdk/simd/logical.hpp>
 #include <boost/simd/sdk/simd/pack.hpp>
 #include <boost/dispatch/meta/sign_of.hpp>
-#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
 #include <nt2/sdk/unit/module.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
-// Test that scalar_of on SIMD
+// Test that sign_of on SIMD
 ////////////////////////////////////////////////////////////////////////////////
 NT2_TEST_CASE_TPL(sign_of_simd_native, BOOST_SIMD_TYPES)
 {
   using boost::simd::native;
   using boost::dispatch::meta::sign_of;
-  using boost::is_same;
+  using boost::mpl::_;
 
   typedef BOOST_SIMD_DEFAULT_EXTENSION      ext_t;
   typedef native<T,ext_t>                 native_t;
 
-  NT2_TEST( (is_same< typename sign_of<native_t>::type
-                    , typename sign_of<T>::type
-                    >::value
-            )
-          );
+  native_t a0;
+  NT2_TEST_EXPR_TYPE( a0, sign_of<_>, typename sign_of<T>::type );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Test that sign_of on SIMD logical
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE_TPL(sign_of_simd_native_logical, BOOST_SIMD_TYPES)
+{
+  using boost::simd::logical;
+  using boost::simd::native;
+  using boost::dispatch::meta::sign_of;
+  using boost::mpl::_;
+
+  typedef BOOST_SIMD_DEFAULT_EXTENSION      ext_t;
+  typedef native<logical<T>,ext_t>          native_t;
+
+  native_t a0;
+  NT2_TEST_EXPR_TYPE( a0, sign_of<_>, typename sign_of<T>::type );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -41,14 +56,11 @@ NT2_TEST_CASE_TPL(sign_of_simd, BOOST_SIMD_TYPES)
 {
   using boost::simd::pack;
   using boost::dispatch::meta::sign_of;
-  using boost::is_same;
+  using boost::mpl::_;
 
   typedef BOOST_SIMD_DEFAULT_EXTENSION      ext_t;
   typedef pack<T>                          pack_t;
 
-  NT2_TEST( (is_same< typename sign_of<pack_t>::type
-                    , typename sign_of<T>::type
-                    >::value
-            )
-          );
+  pack_t a0;
+  NT2_TEST_EXPR_TYPE( a0, sign_of<_>, typename sign_of<T>::type );
 }
