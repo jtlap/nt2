5b4d92c99bbebafc85e03c30c34821d352263142
Joel Falcou <joel.falcou@lri.fr>
2011-10-31 10:25:25 +0100

    Setup True/False test to handle boolean directly

--- modules/boost/simd/constant/unit/scalar/true_false.cpp
+++ modules/boost/simd/constant/unit/scalar/true_false.cpp
@@ -25,9 +25,8 @@ NT2_TEST_CASE_TPL( true_false_value, BOOST_SIMD_TYPES )
   NT2_TEST_EQUAL( boost::simd::False< logical<T> >(), logical<T>(false) );
   NT2_TEST_EQUAL( boost::simd::True< logical<T> >() , logical<T>(true) );
 
-  NT2_TEST_EQUAL( bool(boost::simd::False<T>()), (false) );
-  NT2_TEST_EQUAL( bool(boost::simd::True<T>()) , (true) );
-
-  NT2_TEST_EQUAL( bool(boost::simd::False< logical<T> >()), (false) );
-  NT2_TEST_EQUAL( bool(boost::simd::True< logical<T> >()) , (true) );
+  NT2_TEST_EQUAL( boost::simd::False<T>(), false );
+  NT2_TEST_EQUAL( boost::simd::True<T>() , true );
+  NT2_TEST_EQUAL( boost::simd::False< logical<T> >(), false );
+  NT2_TEST_EQUAL( boost::simd::True< logical<T> >() , true );
 }
