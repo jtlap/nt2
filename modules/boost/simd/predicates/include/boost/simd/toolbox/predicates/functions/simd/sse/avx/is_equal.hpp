2d63dbec26de76dcbe0cebb1765d6144d9357ad7
jt lapresté <jtlapreste@gmail.com>
2011-11-01 10:35:38 +0100

    the correct is_equal.hpp version for avx (to be generated by the special map

--- modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/simd/sse/avx/is_equal.hpp
+++ modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/simd/sse/avx/is_equal.hpp
@@ -12,9 +12,31 @@
 #include <boost/simd/sdk/simd/logical.hpp>
 #include <boost/simd/toolbox/predicates/functions/is_equal.hpp>
 #include <boost/simd/sdk/simd/native_cast.hpp>
-
+#include <boost/dispatch/meta/scalar_of.hpp>
 namespace boost { namespace simd { namespace ext
 {
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_equal_, boost::simd::tag::avx_
+                            , (A0)
+                            , ((simd_<arithmetic_<A0>,boost::simd::tag::avx_>))
+                              ((simd_<arithmetic_<A0>,boost::simd::tag::avx_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      typedef typename dispatch::meta::scalar_of<A0>::type   sctype;
+      typedef simd::native<sctype, boost::simd::tag::sse_ >  svtype;
+      svtype a00 = { _mm256_extractf128_si256(a0, 0)};
+      svtype a10 = { _mm256_extractf128_si256(a1, 0)};
+      svtype  r0 = native_cast<svtype>(eq(a00,a10));
+      result_type that  = {_mm256_insertf128_si256(that, r0, 0)};
+      svtype a01 = { _mm256_extractf128_si256(a0, 1)};
+      svtype a11 = { _mm256_extractf128_si256(a1, 1)};
+      svtype r1 = native_cast<svtype>(eq(a01,a11));
+      that = native_cast<result_type>(_mm256_insertf128_si256(that, r1, 1));
+      return that;
+    }
+  };
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_equal_, boost::simd::tag::avx_
                             , (A0)
                             , ((simd_<double_<A0>,boost::simd::tag::avx_>))
