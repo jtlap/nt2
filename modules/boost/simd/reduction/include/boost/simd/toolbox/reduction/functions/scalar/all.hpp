e17868c57015627c128f3ef0ea122a3bf977bf2d
Jt lapreste <jtlapreste@gmail.com>
2011-11-04 23:22:52 +0100

    scalar all and any for logical

--- modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/scalar/all.hpp
+++ modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/scalar/all.hpp
@@ -17,11 +17,19 @@ namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::all_, tag::cpu_,
                      (A0),
-                     (scalar_<fundamental_<A0> >)
+                     (scalar_<arithmetic_<A0> >)
                     )
   {
     typedef typename meta::as_logical<A0>::type result_type;
     BOOST_SIMD_FUNCTOR_CALL(1) { return is_nez(a0); };
   };
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::all_, tag::cpu_,
+                     (A0),
+                     (scalar_<logical_<A0> >)
+                    )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+    BOOST_SIMD_FUNCTOR_CALL(1) { return a0; };
+  };  
 } } }
 #endif
