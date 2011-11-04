2fb2a1e862f43cc9f987cb5868597062bf2a55ab
Jt lapreste <jtlapreste@gmail.com>
2011-11-04 15:25:05 +0100

    logical_xor.cpp

--- modules/arithmetic/unit/simd/logical_xor.cpp
+++ modules/arithmetic/unit/simd/logical_xor.cpp
@@ -57,12 +57,12 @@ NT2_TEST_CASE_TPL ( logical_xor_real__2_0,  NT2_SIMD_REAL_TYPES)
 
 
   // specific values tests
-  NT2_TEST_ULP_EQUAL(logical_xor(nt2::splat<vT>(0),nt2::splat<vT>(1))[0]!=0, true, 0);
-  NT2_TEST_ULP_EQUAL(logical_xor(nt2::splat<vT>(3),nt2::splat<vT>(0))[0]!=0, true, 0);
-  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Inf<vT>(), nt2::Inf<vT>())[0]!=0, false, 0);
-  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Minf<vT>(), nt2::Minf<vT>())[0]!=0, false, 0);
-  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Mone<vT>(), nt2::Mone<vT>())[0]!=0, false, 0);
-  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Nan<vT>(), nt2::Nan<vT>())[0]!=0, false, 0);
-  NT2_TEST_ULP_EQUAL(logical_xor(nt2::One<vT>(), nt2::One<vT>())[0]!=0, false, 0);
-  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Zero<vT>(), nt2::Zero<vT>())[0]!=0, false, 0);
+  NT2_TEST_ULP_EQUAL(logical_xor(nt2::splat<vT>(0),nt2::splat<vT>(1))[0], true, 0);
+  NT2_TEST_ULP_EQUAL(logical_xor(nt2::splat<vT>(3),nt2::splat<vT>(0))[0], true, 0);
+  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Inf<vT>(), nt2::Inf<vT>())[0], false, 0);
+  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Minf<vT>(), nt2::Minf<vT>())[0], false, 0);
+  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Mone<vT>(), nt2::Mone<vT>())[0], false, 0);
+  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Nan<vT>(), nt2::Nan<vT>())[0], false, 0);
+  NT2_TEST_ULP_EQUAL(logical_xor(nt2::One<vT>(), nt2::One<vT>())[0], false, 0);
+  NT2_TEST_ULP_EQUAL(logical_xor(nt2::Zero<vT>(), nt2::Zero<vT>())[0], false, 0);
 } // end of test for floating_
