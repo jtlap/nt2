53e49bc842518bd8408bb34708ea0cc4da402780
Mathias Gaunard <mathias@gaunard.com>
2011-11-09 18:16:33 +0100

    fix bitwise operators for SSE logicals

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/sse/sse2/bitwise_xor.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/sse/sse2/bitwise_xor.hpp
@@ -17,8 +17,8 @@ namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::bitwise_xor_, boost::simd::tag::sse2_
                             , (A0)(A1)
-                            , ((simd_<fundamental_<A0>,boost::simd::tag::sse_>))
-                              ((simd_<fundamental_<A1>,boost::simd::tag::sse_>))
+                            , ((simd_<arithmetic_<A0>,boost::simd::tag::sse_>))
+                              ((simd_<arithmetic_<A1>,boost::simd::tag::sse_>))
                             )
   {
     typedef A0 result_type;
