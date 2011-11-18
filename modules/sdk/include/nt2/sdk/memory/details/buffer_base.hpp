f57840e4e31f5cb16a075001d8d82d796c382058
montagne <montagne@lri.fr>
2011-11-18 10:04:06 +0100

    Moved buffer into core module

---
+++ modules/sdk/include/nt2/sdk/memory/details/buffer_base.hpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_SDK_MEMORY_BUFFER_BASE_HPP_INCLUDED
+#define NT2_SDK_MEMORY_BUFFER_BASE_HPP_INCLUDED
+
+#include <boost/simd/sdk/memory/details/buffer_base.hpp>
+
+namespace nt2 { namespace memory { namespace details
+{
+  using boost::simd::details::buffer_data;
+} } }
+
+#endif
