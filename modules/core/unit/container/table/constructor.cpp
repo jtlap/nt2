434b3065b64e54d9ece5659a95055fe169cd442b
Joel Falcou <joel.falcou@lri.fr>
2011-11-24 09:25:11 +0100

    Placeholder for table unit tests. To be filled soon.

---
+++ modules/core/unit/container/table/constructor.cpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#define NT2_UNIT_MODULE "nt2::table constructor checks"
+
+#include <nt2/table.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/basic.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+
+NT2_TEST_CASE( default_ctor )
+{
+  using nt2::table;
+
+}
