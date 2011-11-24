5e6773a055a1f5163bb699ef68d0538e4bc7e6ad
montagne <montagne@lri.fr>
2011-11-24 18:27:46 +0100

    Added value_of and model_of for table with unit test

--- modules/core/include/nt2/core/container/table/adapted/table.hpp
+++ modules/core/include/nt2/core/container/table/adapted/table.hpp
@@ -15,10 +15,8 @@
 #include <nt2/sdk/memory/no_padding.hpp>
 #include <boost/dispatch/meta/model_of.hpp>
 #include <boost/dispatch/meta/value_of.hpp>
-//#include <nt2/core/container/meta/dereference.hpp>
-//#include <nt2/core/container/memory/dereference.hpp>
 #include <nt2/core/container/meta/dimensions_of.hpp>
-//#include <nt2/core/container/memory/adapted/pointer.hpp>
+
 
 //==============================================================================
 // Forward declaration
@@ -34,134 +32,39 @@ namespace nt2 { namespace container
 //==============================================================================
 namespace nt2 { namespace meta
 {
-  template<class Type, class Settings>
-  struct dimensions_of< nt2::container::table<Type,Settings> > 
-         : boost::mpl::size_t<meta::option<Settings, tag::of_size_>::type::static_size> 
+  template<class T, class S>
+  struct dimensions_of< nt2::container::table<T,S> > 
+         : boost::mpl::size_t<meta::option<S, tag::of_size_>::type::static_size> 
   {};
 
-  // //============================================================================
-  // // dereference_ specialization
-  // //============================================================================
-  // template< typename T,  typename A, std::size_t Level>
-  // struct dereference_<memory::buffer<T,A>&,Level>
-  // {
-  //   typedef typename add_pointers<T,1-Level>::type&  type;
-  // };
-
-
-  // //============================================================================
-  // // dereference_ specialization
-  // //============================================================================
-  // template<typename T, typename A, std::size_t Level>
-  // struct dereference_<memory::buffer<T,A>,Level>
-  // {
-  //   typedef typename add_pointers<T,1-Level>::type  type;
-  // };
-
-  // template<typename T, typename A, std::size_t Level>
-  // struct dereference_<memory::buffer<T,A> const&,Level>
-  // {
-  //   typedef typename add_pointers<T,1-Level>::type const&  type;
-  // };
-
-  // template<typename T, typename A>
-  // struct dereference_<memory::buffer<T,A>&,1>
-  // {
-  //   typedef typename memory::buffer<T,A>::reference type;
-  // };
-
-  // template<typename T, typename A>
-  // struct dereference_<memory::buffer<T,A> const&,1>
-  // {
-  //   typedef typename memory::buffer<T,A>::const_reference type;
-  // };
-
-  // template<typename T, typename A>
-  // struct dereference_<memory::buffer<T,A>,1>
-  // {
-  //   typedef typename memory::buffer<T,A>::value_type type;
-  // };
 } }
 
-// namespace boost { namespace dispatch { namespace meta
-// {
-//   //============================================================================
-//   // value_of specialization
-//   //============================================================================
-//   template< typename T, typename A>
-//   struct value_of< nt2::memory::buffer<T,A> >
-//   {
-//     typedef T type;
-//   };
+namespace boost { namespace dispatch { namespace meta
+{
+  //============================================================================
+  // value_of specialization
+  //============================================================================
 
-//   //============================================================================
-//   // model_of specialization
-//   //============================================================================
-//   template<typename T, typename A>
-//   struct model_of< nt2::memory::buffer<T,A> >
-//   {
-//     struct type
-//     {
-//       template<class X> struct apply
-//       {
-//         typedef typename A::template rebind<X>::other alloc;
-//         typedef nt2::memory::buffer<X,alloc>    type;
-//       };
-//     };
-//   };
-// } } }
+  template<class T, class S>
+  struct value_of< nt2::container::table<T,S> >
+  {
+    typedef T type;
+  };
 
-// namespace nt2 { namespace memory
-// {
-//   //============================================================================
-//   // buffer initialize - Part of Buffer Concept
-//   //============================================================================
-//   template< typename T,  typename P, typename A
-//           , typename Sizes, typename Bases
-//           >
-//   inline void initialize( buffer<T,A>& v
-//                           , Sizes const& s, Bases const& b, P const& p
-//                           )
-//   {
-//     BOOST_ASSERT(boost::fusion::size(s) == 1);
-//     BOOST_ASSERT(boost::fusion::size(b) == 1);
-//     v.restructure( boost::fusion::at_c<0>(b)
-//                    ,slice<1>(s,p)
-//                  );
-//   }
-
-//   //============================================================================
-//   // buffer resize - Part of Buffer Concept
-//   //============================================================================
-//   template< typename T,  typename P, typename A
-//           , typename Sizes, typename Bases
-//           >
-//   inline void resize( buffer<T,A>& v
-//                       , Sizes const& s, Bases const& b, P const& p
-//                     )
-//   {
-//     BOOST_ASSERT(boost::fusion::size(s) == 1);
-//     BOOST_ASSERT(boost::fusion::size(b) == 1);
-//     v.restructure( boost::fusion::at_c<0>(b)
-//                    ,slice<1>(s,p)
-//                  );
-//   }
-
-
-//   //============================================================================
-//   // buffer share - Part of SharingBuffer Concept
-//   //============================================================================
-//   template<   typename T, typename P, typename A
-//               , typename Sizes, typename Bases
-//           >
-//   inline void share( buffer<T,A>& v
-//                      , Sizes const& s, Bases const& b, P const& p
-//                    )
-//   {
-//     initialize(v,s,b,p);
-//   }
-
-
-// } }
+  //============================================================================
+  // model_of specialization
+  //============================================================================
+  template<typename T, typename S>
+  struct model_of< nt2::container::table<T,S> >
+  {
+    struct type
+    {
+      template<class X> struct apply
+      {
+        typedef nt2::container::table<X,S>    type;
+      };
+    };
+  };
+} } }
 
 #endif
--- modules/core/unit/container/table/table.cpp
+++ modules/core/unit/container/table/table.cpp
@@ -9,11 +9,14 @@
 #define NT2_UNIT_MODULE "nt2::table "
 
 #include <nt2/table.hpp>
-#include <nt2/core/functions/of_size.hpp>
 
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+// table type has some dimensions
+////////////////////////////////////////////////////////////////////////////////
 
 NT2_TEST_CASE_TPL( table_dimensions ,NT2_TYPES)
 {
@@ -24,8 +27,56 @@ NT2_TEST_CASE_TPL( table_dimensions ,NT2_TYPES)
   using nt2::_4D;
   using nt2::meta::dimensions_of;
 
-  NT2_TEST_EQUAL( (dimensions_of<table<int,_1D> >::value), 1UL) ;
-  NT2_TEST_EQUAL( (dimensions_of<table<int,_2D> >::value), 2UL) ;
-  NT2_TEST_EQUAL( (dimensions_of<table<int,_3D> >::value), 3UL) ;
-  NT2_TEST_EQUAL( (dimensions_of<table<int,_4D> >::value), 4UL) ;
+  NT2_TEST_EQUAL( (dimensions_of<table<T,_1D> >::value), 1UL) ;
+  NT2_TEST_EQUAL( (dimensions_of<table<T,_2D> >::value), 2UL) ;
+  NT2_TEST_EQUAL( (dimensions_of<table<T,_3D> >::value), 3UL) ;
+  NT2_TEST_EQUAL( (dimensions_of<table<T,_4D> >::value), 4UL) ;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// table type has some value
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE_TPL( table_values, NT2_TYPES )
+{
+  using boost::is_same;
+  using boost::dispatch::meta::value_of;
+  using nt2::table;
+  using nt2::_1D;
+  using nt2::_2D;
+  using nt2::_3D;
+  using nt2::_4D;
+
+  NT2_TEST((is_same< typename value_of< table<T,_1D> >::type, T>::value ));
+  NT2_TEST((is_same< typename value_of< table<T,_2D> >::type, T>::value ));
+  NT2_TEST((is_same< typename value_of< table<T,_3D> >::type, T>::value ));
+  NT2_TEST((is_same< typename value_of< table<T,_4D> >::type, T>::value ));
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// table type has a model
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE_TPL( table_models, NT2_TYPES )
+{
+  using boost::is_same;
+  using boost::dispatch::meta::model_of;
+  using boost::mpl::apply;
+  using nt2::table;
+  using nt2::_1D;
+  using nt2::_2D;
+  using nt2::_3D;
+  using nt2::_4D;
+
+  typedef typename model_of< table<T,_1D> >::type model1d;
+  typedef typename model_of< table<T,_2D> >::type model2d;
+  typedef typename model_of< table<T,_3D> >::type model3d;
+  typedef typename model_of< table<T,_4D> >::type model4d;
+
+  NT2_TEST((is_same<typename apply<model1d,float>::type, table<float,_1D> >::value ));
+  NT2_TEST((is_same<typename apply<model2d,float>::type, table<float,_2D> >::value ));
+  NT2_TEST((is_same<typename apply<model3d,float>::type, table<float,_3D> >::value ));
+  NT2_TEST((is_same<typename apply<model4d,float>::type, table<float,_4D> >::value ));
+
+
 }
