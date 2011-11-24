748899d79b94a0170b3ae624a0b67a0e14d309dc
Jt lapreste <jtlapreste@gmail.com>
2011-11-24 16:45:06 +0100

    all for avx

--- modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/simd/sse/avx/all.hpp
+++ modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/simd/sse/avx/all.hpp
@@ -6,27 +6,33 @@
 //                 See accompanying file LICENSE.txt or copy at                 
 //                     http://www.boost.org/LICENSE_1_0.txt                     
 //==============================================================================
-#ifndef BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_SSE_SSE2_BITWISE_ANY_HPP_INCLUDED
-#define BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_SSE_SSE2_BITWISE_ANY_HPP_INCLUDED
-#ifdef BOOST_SIMD_HAS_SSE2_SUPPORT
+#ifndef BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_SSE_AVX_ALL_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_SSE_AVX_ALL_HPP_INCLUDED
+#ifdef BOOST_SIMD_HAS_AVX_SUPPORT
+#include <boost/simd/toolbox/reduction/functions/all.hpp>
 #include <boost/simd/sdk/simd/logical.hpp>
-#include <boost/simd/toolbox/reduction/functions/bitwise_any.hpp>
+#include <boost/simd/include/constants/false.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::bitwise_any_, boost::simd::tag::sse2_,
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::all_, boost::simd::tag::avx_,
                               (A0),
-                              ((simd_<type16_<A0>,boost::simd::tag::sse_>))
+                              ((simd_<type16_<A0>,boost::simd::tag::avx_>))
                             )
   {
     typedef typename meta::scalar_of<A0>::type sA0; 
     typedef typename meta::as_logical<sA0>::type result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return result_type(_mm_movemask_epi8(a0) != 0);
+      typedef typename meta::scalar_of<A0>::type             sctype;
+      typedef simd::native<sctype, boost::simd::tag::sse_ >  svtype;
+      svtype a00 = { _mm256_extractf128_si256(a0, 0)};
+      if  (_mm_movemask_epi8(a00) != 0xFFFF) return False<sA0>();
+      svtype a01 = { _mm256_extractf128_si256(a0, 1)};
+      return result_type(_mm_movemask_epi8(a01) == 0xFFFF);
     }
   };
-  
+
 } } }  
 #endif
 #endif
