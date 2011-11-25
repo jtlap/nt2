b7d5cba38e10004ef29fad5d011cc56bd392ec9e
montagne <montagne@lri.fr>
2011-11-25 16:28:48 +0100

    modified storage_order unit test

--- modules/core/unit/container/table/storage_order.cpp
+++ modules/core/unit/container/table/storage_order.cpp
@@ -11,7 +11,6 @@
 #include <nt2/table.hpp>
 #include <nt2/core/settings/storage_order.hpp>
 
-
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
@@ -26,12 +25,36 @@ NT2_TEST_CASE_TPL( storage_order, NT2_TYPES )
   using nt2::matlab_so_;
   using nt2::c_so_;
   using nt2::storage_order_;
+  using boost::mpl::int_;
+  int i = 0;
 
-  typedef boost::mpl::int_<2> size;
-  typedef boost::mpl::int_<1> dim;
 
-  NT2_TEST((is_same<storage_order_<matlab_so_, size, dim>::type, boost::mpl::int_<0>  >::value ));
-  NT2_TEST((is_same<storage_order_<c_so_, size, dim>::type, boost::mpl::int_<1>  >::value ));
+  typedef int_<4> size;
+  typedef int_<0> dim_0;
+  typedef int_<1> dim_1;
+  typedef int_<2> dim_2;
+  typedef int_<3> dim_3;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// table and matlab_storage order
+////////////////////////////////////////////////////////////////////////////////
+
+  NT2_TEST((is_same<storage_order_<matlab_so_, size, dim_0>::type, int_<3>  >::value ));
+  NT2_TEST((is_same<storage_order_<matlab_so_, size, dim_1>::type, int_<2>  >::value ));
+  NT2_TEST((is_same<storage_order_<matlab_so_, size, dim_2>::type, int_<1>  >::value ));
+  NT2_TEST((is_same<storage_order_<matlab_so_, size, dim_3>::type, int_<0>  >::value ));
+
+////////////////////////////////////////////////////////////////////////////////
+// table and c_storage order
+////////////////////////////////////////////////////////////////////////////////
+
+  NT2_TEST((is_same<storage_order_<c_so_, size, dim_0>::type, int_<0>  >::value ));
+  NT2_TEST((is_same<storage_order_<c_so_, size, dim_1>::type, int_<1>  >::value ));
+  NT2_TEST((is_same<storage_order_<c_so_, size, dim_2>::type, int_<2>  >::value ));
+  NT2_TEST((is_same<storage_order_<c_so_, size, dim_3>::type, int_<3>  >::value ));
+
+
 
 }
 
