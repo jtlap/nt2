1d09a40afb268b35ad008fb82496ddaabbebc855
Jt lapreste <jtlapreste@gmail.com>
2011-11-28 12:10:07 +0100

    is_nez sse2 version

--- modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/simd/sse/sse2/is_nez.hpp
+++ modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/simd/sse/sse2/is_nez.hpp
@@ -9,9 +9,10 @@
 #ifndef BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_SIMD_SSE_SSE2_IS_NEZ_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_SIMD_SSE_SSE2_IS_NEZ_HPP_INCLUDED
 #ifdef BOOST_SIMD_HAS_SSE2_SUPPORT
-
 #include <boost/simd/toolbox/predicates/functions/is_nez.hpp>
+//#include <boost/simd/include/functions/genmask.hpp>
 #include <boost/simd/include/functions/bitwise_or.hpp>
+#include <boost/simd/include/functions/logical_or.hpp>
 #include <boost/simd/sdk/simd/logical.hpp>
 #include <boost/simd/sdk/simd/native_cast.hpp>
 
@@ -27,16 +28,14 @@ namespace boost { namespace simd { namespace ext
                                     )
   {
     typedef typename meta::as_logical<A0>::type result_type; 
-
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      typedef typename dispatch::meta::downgrade<A0>::type          base; 
+      typedef typename dispatch::meta::downgrade<A0>::type          base;
+      typedef typename meta::as_logical<base>::type                lbase; 
       typedef typename dispatch::meta::downgrade<result_type>::type type; 
-
-      const type tmp1 = is_nez(boost::simd::native_cast<base>(a0));
-      const type tmp2 = {_mm_shuffle_epi32(tmp1, _MM_SHUFFLE(2, 3, 0, 1))};
-
-      return boost::simd::native_cast<result_type>(b_or(tmp1, tmp2));
+      const lbase tmp1 = is_nez(boost::simd::native_cast<base>(a0));
+      const lbase tmp2 = {_mm_shuffle_epi32(tmp1, _MM_SHUFFLE(2, 3, 0, 1))};
+      return boost::simd::native_cast<result_type>(l_or(tmp1, tmp2));
     }
   };
 } } }
