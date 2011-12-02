d12d1ee6927a999e895799c813ac67689056bdd1
Jt lapreste <jtlapreste@gmail.com>
2011-12-02 23:04:27 +0100

    cover adjustement for polynomials

--- modules/polynomials/cover/simd/hermite.cpp
+++ modules/polynomials/cover/simd/hermite.cpp
@@ -67,15 +67,14 @@ NT2_TEST_CASE_TPL ( hermite_real__2_0,  NT2_SIMD_REAL_TYPES)
     NT2_CREATE_BUF(tab_a0,iT, NR, iT(0), iT(10));
     NT2_CREATE_BUF(tab_a1,T, NR, T(-10), T(10));
     double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR/cardinal_of<n_t>::value; j++)
+    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
       {
         iT a0 = tab_a0[j];
         vT a1 = load<vT>(&tab_a1[0],j);
         r_t v = hermite(a0,a1);
         for(unsigned int i = 0; i< cardinal_of<n_t>::value; i++)
         {
-          unsigned int k = i+j*cardinal_of<n_t>::value;
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::hermite (tab_a0[j],tab_a1[k])), 13);
+          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::hermite (tab_a0[j],a1[i])), 13);
           ulp0 = nt2::max(ulpd,ulp0);
         }
       }
