9d8793997936b3f4edf214c196a46f16cfbcf01f
Jt lapreste <jtlapreste@gmail.com>
2011-12-02 20:44:05 +0100

    cover adjustement for bitwise

--- modules/bitwise/cover/simd/genmask.cpp
+++ modules/bitwise/cover/simd/genmask.cpp
@@ -62,14 +62,14 @@ NT2_TEST_CASE_TPL ( genmask_real__1_0,  NT2_SIMD_REAL_TYPES)
   {
     NT2_CREATE_BUF(tab_a0,T, NR, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
     double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR/cardinal_of<n_t>::value; j++)
+    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
       {
         vT a0 = load<vT>(&tab_a0[0],j);
         r_t v = genmask(a0);
         for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
         {
-          nt2::uint32_t k = i+j*cardinal_of<n_t>::value;
-          NT2_TEST_EQUAL( v[i],ssr_t(nt2::genmask (tab_a0[k])));
+          
+          NT2_TEST_EQUAL( v[i],ssr_t(nt2::genmask (a0[i])));
         }
       }
     
@@ -100,14 +100,14 @@ NT2_TEST_CASE_TPL ( genmask_integer__1_0,  NT2_SIMD_INTEGRAL_TYPES)
   {
     NT2_CREATE_BUF(tab_a0,T, NR, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
     double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR/cardinal_of<n_t>::value; j++)
+    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
       {
         vT a0 = load<vT>(&tab_a0[0],j);
         r_t v = genmask(a0);
         for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
         {
-          nt2::uint32_t k = i+j*cardinal_of<n_t>::value;
-          NT2_TEST_EQUAL( v[i],ssr_t(nt2::genmask (tab_a0[k])));
+          
+          NT2_TEST_EQUAL( v[i],ssr_t(nt2::genmask (a0[i])));
         }
       }
     
