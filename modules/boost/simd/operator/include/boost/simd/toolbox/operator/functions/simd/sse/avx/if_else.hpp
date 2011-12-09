43964f65d75cffb17c70eee65239244a432b53f9
jt lapresté <jtlapreste@gmail.com>
2011-12-09 12:56:17 +0100

    if_else avx version revisited

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/sse/avx/if_else.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/sse/avx/if_else.hpp
@@ -9,11 +9,10 @@
 #ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_AVX_SELECT_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_AVX_SELECT_HPP_INCLUDED
 #ifdef BOOST_SIMD_HAS_AVX_SUPPORT
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type  is arithmetic_
-/////////////////////////////////////////////////////////////////////////////
 #include <boost/simd/toolbox/operator/functions/if_else.hpp>
 #include <boost/simd/include/functions/genmask.hpp>
+#include <iostream>
+#include <nt2/sdk/details/type_id.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -30,10 +29,7 @@ namespace boost { namespace simd { namespace ext
       return boost::simd::bitwise_cast<A1>(_mm256_blendv_ps(a2, a1,  bitwise_cast<A1>(genmask(a0)))); 
     }
   };
-} } }
 
-namespace boost { namespace simd { namespace ext
-{
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
                             , (A0)(A1)
                             , ((simd_<arithmetic_<A0>, boost::simd::tag::avx_>))
@@ -47,9 +43,7 @@ namespace boost { namespace simd { namespace ext
       return boost::simd::bitwise_cast<A1>(_mm256_blendv_pd(a2, a1, bitwise_cast<A1>(genmask(a0)))); 
     }
   };
-} } }
-namespace boost { namespace simd { namespace ext
-{
+
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
                             , (A0)(A1)
                             , ((simd_<logical_<A0>, boost::simd::tag::avx_>))
@@ -63,10 +57,7 @@ namespace boost { namespace simd { namespace ext
       return boost::simd::bitwise_cast<A1>(_mm256_blendv_ps(a2, a1, bitwise_cast<A1>(a0))); 
     }
   };
-} } }
 
-namespace boost { namespace simd { namespace ext
-{
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
                             , (A0)(A1)
                             , ((simd_<logical_<A0>, boost::simd::tag::avx_>))
@@ -80,6 +71,181 @@ namespace boost { namespace simd { namespace ext
       return boost::simd::bitwise_cast<A1>(_mm256_blendv_pd(a2, a1, bitwise_cast<A1>(a0))); 
     }
   };
+  
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<logical_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<int32_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<int32_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef native < float, boost::simd::tag::avx_ > ctype;
+      typedef meta::as_logical<ctype>::type ltype; 
+      return bitwise_cast<result_type>(if_else(bitwise_cast<ltype>(a0), bitwise_cast<ctype>(a1),bitwise_cast<ctype>(a2)));
+    }
+  };
+  
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<arithmetic_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<int32_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<int32_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef native < float, boost::simd::tag::avx_ > ctype;
+      return bitwise_cast<result_type>(if_else(bitwise_cast<ctype>(a0), bitwise_cast<ctype>(a1),bitwise_cast<ctype>(a2))); 
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<logical_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<uint32_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<uint32_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef native < float, boost::simd::tag::avx_ > ctype;
+      typedef meta::as_logical<ctype>::type ltype; 
+      return bitwise_cast<result_type>(if_else(bitwise_cast<ltype>(a0), bitwise_cast<ctype>(a1),bitwise_cast<ctype>(a2)));
+    }
+  };
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<arithmetic_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<uint32_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<uint32_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef native < float, boost::simd::tag::avx_ > ctype;
+      return bitwise_cast<result_type>(if_else(bitwise_cast<ctype>(a0), bitwise_cast<ctype>(a1),bitwise_cast<ctype>(a2))); 
+    }
+  };
+
+   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<logical_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<int64_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<int64_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef native < float, boost::simd::tag::avx_ > ctype;
+      typedef meta::as_logical<ctype>::type ltype; 
+      return bitwise_cast<result_type>(if_else(bitwise_cast<ltype>(a0), bitwise_cast<ctype>(a1),bitwise_cast<ctype>(a2)));
+    }
+  };
+  
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<arithmetic_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<int64_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<int64_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef native < float, boost::simd::tag::avx_ > ctype;
+      return bitwise_cast<result_type>(if_else(bitwise_cast<ctype>(a0), bitwise_cast<ctype>(a1),bitwise_cast<ctype>(a2))); 
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<logical_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<uint64_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<uint64_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef native < float, boost::simd::tag::avx_ > ctype;
+      typedef meta::as_logical<ctype>::type ltype; 
+      return bitwise_cast<result_type>(if_else(bitwise_cast<ltype>(a0), bitwise_cast<ctype>(a1),bitwise_cast<ctype>(a2)));
+    }
+  };
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<arithmetic_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<uint64_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<uint64_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef native < float, boost::simd::tag::avx_ > ctype;
+      return bitwise_cast<result_type>(if_else(bitwise_cast<ctype>(a0), bitwise_cast<ctype>(a1),bitwise_cast<ctype>(a2))); 
+    }
+  };
+   
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<logical_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<arithmetic_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<arithmetic_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef typename meta::scalar_of<A1>::type             sctype;
+      typedef simd::native<sctype, boost::simd::tag::sse_ >  svtype;
+      typedef typename meta::as_logical<svtype>::type       lsvtype; 
+      lsvtype a00 = { _mm256_extractf128_si256(bitwise_cast<A1>(a0), 0)};
+      svtype a10 = { _mm256_extractf128_si256(a1, 0)};
+      svtype a20 = { _mm256_extractf128_si256(a2, 0)};
+      svtype  r0 = if_else(a00,a10,a20);
+      result_type that  = {_mm256_insertf128_si256(that, r0, 0)};
+      lsvtype a01 = { _mm256_extractf128_si256(bitwise_cast<A1>(a0), 1)};
+      svtype a11 = { _mm256_extractf128_si256(a1, 1)};
+      svtype a21 = { _mm256_extractf128_si256(a2, 1)};
+      svtype r1 = if_else(a01,a11,a21);
+      that = _mm256_insertf128_si256(that, r1, 1);
+      return that;       
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::if_else_, boost::simd::tag::avx_
+                            , (A0)(A1)
+                            , ((simd_<arithmetic_<A0>, boost::simd::tag::avx_>))
+                              ((simd_<arithmetic_<A1>, boost::simd::tag::avx_>))
+                              ((simd_<arithmetic_<A1>, boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A1 result_type;
+    inline result_type operator()(A0 const& a0,A1 const& a1,A1 const& a2) const
+    {
+      typedef typename meta::scalar_of<A1>::type             sctype;
+      typedef simd::native<sctype, boost::simd::tag::sse_ >  svtype;
+      svtype a00 = { _mm256_extractf128_si256(bitwise_cast<A1>(a0), 0)};
+      svtype a10 = { _mm256_extractf128_si256(a1, 0)};
+      svtype a20 = { _mm256_extractf128_si256(a2, 0)};
+      svtype  r0 = if_else(a00,a10,a20);
+      result_type that  = {_mm256_insertf128_si256(that, r0, 0)};
+      svtype a01 = { _mm256_extractf128_si256(bitwise_cast<A1>(a0), 1)};
+      svtype a11 = { _mm256_extractf128_si256(a1, 1)};
+      svtype a21 = { _mm256_extractf128_si256(a2, 1)};
+      svtype r1 = if_else(a01,a11,a21);
+      that = _mm256_insertf128_si256(that, r1, 1);
+      return that;       
+    }
+  };  
 } } }
 
 #endif
