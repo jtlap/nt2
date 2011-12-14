669b6bd0764d1ad839d35860710cf19cd933b98b
Mathias Gaunard <mathias@gaunard.com>
2011-12-14 15:08:02 +0100

    add a couple of missing includes in exponential

--- modules/exponential/include/nt2/toolbox/exponential/functions/simd/common/exp.hpp
+++ modules/exponential/include/nt2/toolbox/exponential/functions/simd/common/exp.hpp
@@ -8,10 +8,11 @@
 //==============================================================================
 #ifndef NT2_TOOLBOX_EXPONENTIAL_FUNCTIONS_SIMD_COMMON_EXP_HPP_INCLUDED
 #define NT2_TOOLBOX_EXPONENTIAL_FUNCTIONS_SIMD_COMMON_EXP_HPP_INCLUDED
-#include <nt2/sdk/meta/as_floating.hpp>
-//#include <nt2/sdk/simd/meta/is_real_convertible.hpp>
-#include <nt2/include/functions/tofloat.hpp>
+
+#include <nt2/toolbox/exponential/functions/exp.hpp>
 #include <nt2/toolbox/exponential/functions/simd/common/impl/expo.hpp>
+#include <nt2/include/functions/tofloat.hpp>
+#include <nt2/sdk/meta/as_floating.hpp>
 
 /////////////////////////////////////////////////////////////////////////////
 // Implementation when type  is arithmetic_
