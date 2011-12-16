53c38ced54934989e3afdcee951ff2c2aebd06a7
Mathias Gaunard <mathias@gaunard.com>
2011-12-16 17:08:21 +0100

    adding is_equal_with_equal_nans, is_not_equal_with_equal_nans and is_unord for altivec

---
+++ modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/simd/vmx/altivec/is_unord.hpp
@@ -0,0 +1,55 @@
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
+//                                                                              
+//          Distributed under the Boost Software License, Version 1.0.          
+//                 See accompanying file LICENSE.txt or copy at                 
+//                     http://www.boost.org/LICENSE_1_0.txt                     
+//==============================================================================
+#ifndef BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_SIMD_VMX_ALTIVEC_IS_UNORD_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_SIMD_VMX_ALTIVEC_IS_UNORD_HPP_INCLUDED
+#ifdef BOOST_SIMD_HAS_VMX_SUPPORT
+
+#include <boost/simd/toolbox/predicates/functions/is_unord.hpp>
+#include <boost/simd/include/functions/is_equal.hpp>
+#include <boost/simd/include/functions/is_greater.hpp>
+#include <boost/simd/include/functions/logical_or.hpp>
+#include <boost/simd/include/functions/logical_not.hpp>
+#include <boost/simd/include/constants/false.hpp>
+#include <boost/simd/sdk/simd/logical.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_unord_, boost::simd::tag::altivec_, (A0)
+                            , ((simd_<floating_<A0>, boost::simd::tag::altivec_>))
+                              ((simd_<floating_<A0>, boost::simd::tag::altivec_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+       return logical_not(
+         logical_or( is_equal(a, b)
+                   , logical_or( is_greater(b, a)
+                               , is_greater(a, b)
+                               )
+                   )
+       );
+    }
+  };
+  
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_unord_, boost::simd::tag::altivec_, (A0)
+                            , ((simd_<integer_<A0>, boost::simd::tag::altivec_>))
+                              ((simd_<integer_<A0>, boost::simd::tag::altivec_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+    BOOST_FORCEINLINE result_type operator()(A0 const&, A0 const&) const
+    {
+       return False<result_type>();
+    }
+  };
+} } }
+
+#endif
+#endif
