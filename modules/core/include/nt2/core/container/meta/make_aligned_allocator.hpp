640e8c338bd75a9b1a5ef67577c52badd0d15ff6
Joel Falcou <joel.falcou@lri.fr>
2011-12-19 22:16:34 +0100

    Added alignment traits for allocator and alignment adaptation meta-function

---
+++ modules/core/include/nt2/core/container/meta/make_aligned_allocator.hpp
@@ -0,0 +1,41 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_CONTAINER_META_MAKE_ALIGNED_ALLOCATOR_HPP_INCLUDED
+#define NT2_CORE_CONTAINER_META_MAKE_ALIGNED_ALLOCATOR_HPP_INCLUDED
+
+#include <boost/mpl/bool.hpp>
+#include <boost/simd/sdk/memory/allocator_adaptor.hpp>
+#include <nt2/core/container/meta/perform_aligned_allocation.hpp>
+
+namespace nt2 {  namespace details
+{
+  template< class Allocator
+          , bool Enable = meta::perform_aligned_allocation<Allocator>::value
+          >
+  struct make_aligned_allocator
+  {
+    typedef Allocator type;
+  };
+
+  template< class Allocator>
+  struct make_aligned_allocator<Allocator,false>
+  {
+    typedef typename Allocator::value_type value_type;
+    typedef boost::simd::memory::allocator_adaptor<value_type,Allocator> type;
+  };  
+} }
+
+namespace nt2 {  namespace meta
+{
+  template<class Allocator>
+  struct make_aligned_allocator : details::make_aligned_allocator<Allocator>
+  {};
+} }
+
+#endif
---
+++ modules/core/include/nt2/core/container/meta/perform_aligned_allocation.hpp
@@ -0,0 +1,47 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_CONTAINER_META_PERFORM_ALIGNED_ALLOCATION_HPP_INCLUDED
+#define NT2_CORE_CONTAINER_META_PERFORM_ALIGNED_ALLOCATION_HPP_INCLUDED
+
+#include <boost/mpl/bool.hpp>
+
+namespace boost { namespace simd {  namespace memory
+{
+  template<class T>                   struct allocator;
+  template<class T, class Allocator>  struct allocator_adaptor;
+} } }
+
+namespace nt2 {  namespace memory
+{
+  template<typename V, typename A> struct padded_allocator;
+} }
+
+namespace nt2 {  namespace meta
+{
+  template<class Allocator>
+  struct perform_aligned_allocation : boost::mpl::false_
+  {};
+
+  template<typename V, typename A>
+  struct  perform_aligned_allocation< memory::padded_allocator<V,A> >
+        : perform_aligned_allocation<A>
+  {};
+
+  template<class T>
+  struct  perform_aligned_allocation< boost::simd::memory::allocator<T> >
+        : boost::mpl::true_
+  {};
+
+  template<class T, class A>
+  struct  perform_aligned_allocation<boost::simd::memory::allocator_adaptor<T,A> >
+        : boost::mpl::true_
+  {};
+} }
+
+#endif
