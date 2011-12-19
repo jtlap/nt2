a63210440d3341c04c79d9159c8fafd6aa52c776
Joel Falcou <joel.falcou@lri.fr>
2011-12-19 16:25:10 +0100

    Fixed padding options to support globaland lead padding at the same time

--- modules/core/include/nt2/core/settings/padding.hpp
+++ modules/core/include/nt2/core/settings/padding.hpp
@@ -10,68 +10,51 @@
 #define NT2_CORE_SETTINGS_PADDING_HPP_INCLUDED
 
 #include <boost/mpl/int.hpp>
-#include <nt2/core/settings/option.hpp>
 #include <nt2/core/settings/misc.hpp>
+#include <nt2/core/settings/option.hpp>
+#include <boost/simd/sdk/memory/parameters.hpp>
 #include <boost/preprocessor/repetition/enum.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
 #include <boost/preprocessor/repetition/repeat_from_to.hpp>
-#include <boost/simd/sdk/memory/parameters.hpp>
 
 namespace nt2 
 { 
+  template<typename Value = boost::mpl::int_<1> >
+  struct global_padding_strategy_
+  {
+    typedef Value padding_value;
+  };
 
-  template<  typename Strat, typename Value
-           , typename Default = boost::mpl::int_<1> >
-  struct padding_strategy_ {};
+  template<typename Value = boost::mpl::int_<BOOST_SIMD_CONFIG_ALIGNMENT> >
+  struct lead_padding_strategy_
+  {
+    typedef Value padding_value;
+  };
 
-  //===========================================================================
-  /*! 
-   * Composite settings for container memory padding strategies.
-   **/
   //============================================================================
-  struct padding_ {};
-
-  //============================================================================
-  /*! Specify that current container doesn't pad its memory. 
-   *  Equivalent to padding_(none_).
+  /*!
+   * Specify that current container doesn't pad its memory. 
    **/
   //============================================================================
   struct no_padding_ {};
 
   //============================================================================
-  /*! Specify a global padding strategy will be used. 
-   *  Equivalent to padding_(global_). 
-   *  The padding value will have to be specified at runtime using 
-   *  the appropriate container constructor.
+  /*!
+   * Specify a global padding strategy will be used. 
+   * The padding value will have to be specified at runtime using the
+   * appropriate container constructor.
    **/
   //============================================================================
   struct global_padding_{};
 
   //============================================================================
-  /*! Specify a lead dimension padding strategy will be used. 
-   *  Equivalent to padding_(lead_). 
-   *  The padding value will have to be specified at runtime using 
-   *  the appropriate container constructor.
+  /*!
+   * Specify a lead dimension padding strategy will be used. 
+   * The padding value will have to be specified at runtime using 
+   * the appropriate container constructor.
    **/
   //============================================================================
-  struct lead_padding_{};
-
-  //============================================================================
-  /*! Specify a global padding strategy will be used. 
-   *  The padding value can either be specified at runtime 
-   *  (see nt2::global_padding_) or using nt2::with_
-   **/
-  //============================================================================
-  struct global_{};
-
-  //============================================================================
-  /*! Specify a lead dimension padding strategy will be used. 
-   *  The padding value can either be specified at runtime 
-   *  (see nt2::lead_padding_) or using nt2::with_
-   **/
-  //============================================================================
-  struct lead_{};
-
+  struct lead_padding_  {};
 
   namespace tag 
   { 
@@ -80,125 +63,84 @@ namespace nt2
      * Option tag for padding options
      **/
     //==========================================================================
-    struct padding_ {}; 
+    struct global_padding_  {};
+    struct lead_padding_    {}; 
   }
-
-
 }
 
 namespace nt2 { namespace meta
 {
   //============================================================================
-  /*! padding list of one argument return the padding strategy of 
-   * the argument option with a default value
-  **/
+  /*!
+   * global_padding_ option return padding_strategy of global
+   **/
   //============================================================================
-  template<class S0, class Default>
-  struct  option< padding_(S0), tag::padding_, Default >
+  template<class Default>
+  struct  option< global_padding_, tag::global_padding_, Default >
   {
-    typedef padding_strategy_<S0, boost::mpl::int_<1> > type;
+    typedef global_padding_strategy_< boost::mpl::int_<-1> > type;
   };
 
   //============================================================================
-  /*! global_padding_ option return padding_strategy of global 
-   *  with a default value
-  **/
+  /*!
+   * lead_padding_ option return padding_strategy of lead
+   **/
   //============================================================================
   template<class Default>
-  struct  option< global_padding_, tag::padding_, Default >
+  struct  option< lead_padding_, tag::lead_padding_, Default >
   {
-    typedef padding_strategy_<global_, boost::mpl::int_<-1> > type;
-  };
-
-  //============================================================================
-  /*! lead_padding_ option return padding_strategy of lead with a default value
-  **/
-  //============================================================================
-  template<class Default>
-  struct  option< lead_padding_, tag::padding_, Default >
-  {
-    typedef padding_strategy_<lead_, boost::mpl::int_<-1> > type;
+    typedef lead_padding_strategy_< boost::mpl::int_<-1> > type;
   };
 
   //============================================================================
   /*! global_padding_ with alignment value 
    *  return padding_strategy of global_ with the alignment value
-  **/
+   **/
   //============================================================================
   template<class Default, std::ptrdiff_t N>
-  struct  option< global_padding_(with_<N>), tag::padding_, Default >
+  struct  option< global_padding_(with_<N>), tag::global_padding_, Default >
   {
-    typedef padding_strategy_<  global_
-                              , boost::mpl::int_<-1>
-                              , boost::mpl::int_<N> 
-                              > type;
+    typedef global_padding_strategy_< boost::mpl::int_<N> > type;
   };
 
   //============================================================================
   /*! lead_padding_ with alignment value 
    *  return padding_strategy of lead_ with the alignment value
-  **/
+   **/
   //============================================================================
   template<class Default, std::ptrdiff_t N>
-  struct  option< lead_padding_(with_<N>), tag::padding_, Default >
+  struct  option< lead_padding_(with_<N>), tag::lead_padding_, Default >
   {
-    typedef padding_strategy_<  lead_
-                              , boost::mpl::int_<-1>
-                              , boost::mpl::int_<N> 
-                              > type;
+    typedef lead_padding_strategy_< boost::mpl::int_<N> > type;
   };
 
   //============================================================================
-  /*! no_padding_ option 
-   *  return padding_strategy of none_ with a default value
-  **/
+  /*!
+   * no_padding_ option 
+   **/
   //============================================================================
   template<class Default>
-  struct  option< no_padding_, tag::padding_, Default >
+  struct  option< no_padding_, tag::lead_padding_, Default >
   {
-    typedef padding_strategy_<none_, boost::mpl::int_<1>  > type;
+    typedef lead_padding_strategy_< boost::mpl::int_<1> > type;
   };
 
-  //============================================================================
-  /*! padding_ option with alignement value
-   *  return padding_strategy of lead_ with the alignement value
-  **/
-  //============================================================================
-  template<std::ptrdiff_t N, class Default>
-  struct  option< padding_(with_<N>), tag::padding_, Default >
+  template<class Default>
+  struct  option< no_padding_, tag::global_padding_, Default >
   {
-    typedef padding_strategy_<  lead_
-                              , boost::mpl::int_<-1>
-                              , boost::mpl::int_<N> 
-                              > type;
+    typedef global_padding_strategy_< boost::mpl::int_<1> > type;
   };
 
-  //============================================================================
-  /*! padding_ option with alignement value and padding type
-   *  return padding_strategy of padding type with the alignement value
-  **/
-  //============================================================================
-  template<class S0, std::ptrdiff_t N, class Default>
-  struct  option< padding_(with_<N>, S0),tag::padding_, Default >
+  template<class N, class Default>
+  struct  option<global_padding_strategy_<N>, tag::global_padding_, Default >
   {
-    typedef padding_strategy_<  S0
-                              , boost::mpl::int_<-1>
-                              , boost::mpl::int_<N> 
-                              > type;
+    typedef global_padding_strategy_<N> type;
   };
 
-  //============================================================================
-  /*! padding_ option with padding type and alignement value 
-   *  return padding_strategy of padding type with the alignement value
-  **/
-  //============================================================================
-  template<class S0, std::ptrdiff_t N, class Default>
-  struct  option< padding_(S0, with_<N>), tag::padding_, Default >
+  template<class N, class Default>
+  struct  option<lead_padding_strategy_<N>, tag::lead_padding_, Default >
   {
-    typedef padding_strategy_<  S0
-                              , boost::mpl::int_<-1>
-                              , boost::mpl::int_<N> 
-                              > type;
+    typedef lead_padding_strategy_<N> type;
   };
 } }
 
--- modules/core/unit/container/settings/padding.cpp
+++ modules/core/unit/container/settings/padding.cpp
@@ -19,176 +19,91 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Pass some padding_ as an option and check everythign go out properly
 ////////////////////////////////////////////////////////////////////////////////
-nt2::settings no_padd(nt2::padding_(nt2::lead_),nt2::padding_(nt2::none_));
+nt2::settings no_padd(nt2::no_padding_);
 
 NT2_TEST_CASE( no_padding )
 {
-   using nt2::none_;
-   using nt2::no_padding_;
-   using nt2::padding_;
-   using nt2::lead_;
-   using nt2::padding_strategy_;
-   using nt2::meta::option;
-   using boost::mpl::_;
-   using boost::mpl::int_;
-   using nt2::settings;
+  using nt2::no_padding_;
+  using nt2::lead_padding_strategy_;
+  using nt2::global_padding_strategy_;
+  using nt2::meta::option;
+  using boost::mpl::_;
+  using boost::mpl::int_;
+  using nt2::settings;
 
   NT2_TEST_EXPR_TYPE( no_padding_()
-                     ,(option< _, nt2::tag::padding_>)
-                      ,(padding_strategy_<none_, int_<1>, int_<1> >)
-                     );
+                    , (option< _, nt2::tag::lead_padding_>)
+                    , (lead_padding_strategy_< int_<1> >)
+                    );
 
-  //Pass some padding_ as option with a default
   NT2_TEST_EXPR_TYPE( no_padding_()
-                      ,(option< void, nt2::tag::padding_,_>)
-                      ,(padding_strategy_<none_, int_<1>, int_<1> >)
-                     );
+                    , (option< _, nt2::tag::global_padding_>)
+                    , (global_padding_strategy_< int_<1> >)
+                    );
 
   NT2_TEST_EXPR_TYPE( no_padd
-                      ,(option<_, nt2::tag::padding_>)
-                      ,(padding_strategy_<none_, int_<1>, int_<1> >)
+                    , (option< _, nt2::tag::lead_padding_>)
+                    , (lead_padding_strategy_< int_<1> >)
+                    );
+
+  NT2_TEST_EXPR_TYPE( no_padd
+                    , (option< _, nt2::tag::global_padding_>)
+                    , (global_padding_strategy_< int_<1> >)
+                    );
+}
+
+nt2::global_padding_  global_32(nt2::with_<32>);
+nt2::settings         global_set( nt2::global_padding_(nt2::with_<64>) );
+
+NT2_TEST_CASE( lead_padding )
+{
+  using nt2::global_padding_;
+  using nt2::global_padding_strategy_;
+  using nt2::meta::option;
+  using nt2::settings;
+  using boost::mpl::_;
+  using boost::mpl::int_;
+
+  NT2_TEST_EXPR_TYPE( global_padding_()
+                      ,(option< _, nt2::tag::global_padding_>)
+                      ,(global_padding_strategy_< int_<-1> >)
                      );
 
-  // Pass some padding_ as settings with a default
-  NT2_TEST_EXPR_TYPE( no_padding_()
-                      ,(option<settings(double,float), nt2::tag::padding_,_>)
-                      ,(padding_strategy_<none_, int_<1>, int_<1> >)
+  NT2_TEST_EXPR_TYPE( global_32
+                      ,(option< _, nt2::tag::global_padding_>)
+                      ,(global_padding_strategy_< int_<32> >)
+                     );
+
+  NT2_TEST_EXPR_TYPE( global_set
+                      ,(option<_, nt2::tag::global_padding_>)
+                      ,(global_padding_strategy_< int_<64> >)
                      );
 }
 
-nt2::padding_ global_32(nt2::global_, nt2::with_<32>);
-nt2::padding_ global_32_permute(nt2::with_<32>, nt2::global_);
-nt2::settings global_padd_sett_32(nt2::padding_(nt2::none_)
-                                  ,nt2::padding_(nt2::global_,nt2::with_<32>)
-                                  );
-nt2::settings global_padd_sett_32_permute(nt2::padding_(nt2::none_)
-                                         ,nt2::padding_(nt2::global_,nt2::with_<32>)
-                                         );
+nt2::lead_padding_  lead_32(nt2::with_<32>);
+nt2::settings       lead_set( nt2::lead_padding_(nt2::with_<64>) );
 
 NT2_TEST_CASE( global_padding )
 {
-   using nt2::global_;
-   using nt2::global_padding_;
-   using nt2::padding_;
-   using nt2::padding_strategy_;
-   using nt2::meta::option;
-   using nt2::settings;
-   using boost::mpl::_;
-   using boost::mpl::int_;
-
-  NT2_TEST_EXPR_TYPE( global_padding_()
-                      ,(option< _, nt2::tag::padding_>)
-                      ,(padding_strategy_<global_, int_<-1>, int_<1> >)
-                     );
-
-  NT2_TEST_EXPR_TYPE( global_32
-                      ,(option< _, nt2::tag::padding_>)
-                      ,(padding_strategy_<global_, int_<-1>, int_<32> >)
-                     );
-
-  NT2_TEST_EXPR_TYPE( global_32_permute
-                     ,(option< _, nt2::tag::padding_>)
-                      ,(padding_strategy_<global_, int_<-1>, int_<32> >)
-                     );
-
-  //Pass some padding_ as option with a default
-  NT2_TEST_EXPR_TYPE( global_32
-                      ,(option< void, nt2::tag::padding_,_>)
-                      ,(padding_strategy_<global_, int_<-1>, int_<32> >)
-                     );
-
-  NT2_TEST_EXPR_TYPE( global_32_permute
-                      ,(option< void, nt2::tag::padding_,_>)
-                      ,(padding_strategy_<global_, int_<-1>, int_<32> >)
-                     );
-
-  //Pass some padding_ as settings
-  NT2_TEST_EXPR_TYPE( global_padd_sett_32
-                      ,(option<_, nt2::tag::padding_>)
-                      ,(padding_strategy_<global_, int_<-1>, int_<32> >)
-                     );
-  NT2_TEST_EXPR_TYPE( global_padd_sett_32_permute
-                      ,(option<_, nt2::tag::padding_>)
-                      ,(padding_strategy_<global_, int_<-1>, int_<32> >)
-                     );
-
-  // Pass some padding_ as settings with a default
-  NT2_TEST_EXPR_TYPE( global_32
-                      ,(option< settings(double, float), nt2::tag::padding_,_>)
-                      ,(padding_strategy_<global_, int_<-1>, int_<32> >)
-                     );
-
-  NT2_TEST_EXPR_TYPE( global_32_permute
-                      ,(option< settings(double, float), nt2::tag::padding_,_>)
-                      ,(padding_strategy_<global_, int_<-1>, int_<32> >)
-                     );
-}
-
-nt2::padding_ lead_32(nt2::lead_, nt2::with_<32>);
-nt2::padding_ lead_32_permute(nt2::with_<32>, nt2::lead_);
-nt2::settings lead_padd_sett_32 ( nt2::padding_(nt2::none_)
-                                , nt2::padding_(nt2::lead_,nt2::with_<32>)
-                                );
-nt2::settings lead_padd_sett_32_permute ( nt2::padding_(nt2::none_)
-                                        , nt2::padding_(nt2::lead_,nt2::with_<32>)
-                                        );
-NT2_TEST_CASE( lead_padding )
-{
-   using nt2::padding_;
-   using nt2::lead_padding_;
-   using nt2::lead_;
-   using nt2::none_;
-   using nt2::padding_strategy_;
-   using nt2::meta::option;
-   using nt2::settings;
-   using boost::mpl::_;
-   using boost::mpl::int_;
+  using nt2::lead_padding_;
+  using nt2::lead_padding_strategy_;
+  using nt2::meta::option;
+  using nt2::settings;
+  using boost::mpl::_;
+  using boost::mpl::int_;
 
   NT2_TEST_EXPR_TYPE( lead_padding_()
-                     ,(option< _, nt2::tag::padding_>)
-                      ,(padding_strategy_<lead_, int_<-1>, int_<1> >)
+                      ,(option< _, nt2::tag::lead_padding_>)
+                      ,(lead_padding_strategy_< int_<-1> >)
                      );
 
   NT2_TEST_EXPR_TYPE( lead_32
-                     ,(option< _, nt2::tag::padding_>)
-                     ,(padding_strategy_<lead_, int_<-1>, int_<32> >)
+                      ,(option< _, nt2::tag::lead_padding_>)
+                      ,(lead_padding_strategy_< int_<32> >)
                      );
 
-  NT2_TEST_EXPR_TYPE( lead_32_permute
-                     ,(option< _, nt2::tag::padding_>)
-                     ,(padding_strategy_<lead_, int_<-1>, int_<32> >)
-                     );
-
-  //Pass some padding_ as option with a default
-  NT2_TEST_EXPR_TYPE( lead_32
-                      ,(option< void, nt2::tag::padding_,_>)
-                     ,(padding_strategy_<lead_, int_<-1>, int_<32> >)
-                     );
-
-  NT2_TEST_EXPR_TYPE( lead_32_permute
-                      ,(option< void, nt2::tag::padding_,_>)
-                     ,(padding_strategy_<lead_, int_<-1>, int_<32> >)
-                     );
-
-  //Pass some padding_ as settings
-  NT2_TEST_EXPR_TYPE( lead_padd_sett_32
-                      ,(option<_, nt2::tag::padding_>)
-                     ,(padding_strategy_<lead_, int_<-1>, int_<32> >)
-                     );
-
-  NT2_TEST_EXPR_TYPE( lead_padd_sett_32_permute
-                      ,(option<_, nt2::tag::padding_>)
-                     ,(padding_strategy_<lead_, int_<-1>, int_<32> >)
-                     );
-
-  // Pass some padding_ as settings with a default
-  NT2_TEST_EXPR_TYPE( lead_32
-                      ,(option< settings(double, float), nt2::tag::padding_,_>)
-                      ,(padding_strategy_<lead_, int_<-1>, int_<32> >)
-                     );
-
-  NT2_TEST_EXPR_TYPE( lead_32_permute
-                      ,(option< settings(double, float), nt2::tag::padding_,_>)
-                      ,(padding_strategy_<lead_, int_<-1>, int_<32> >)
+  NT2_TEST_EXPR_TYPE( lead_set
+                      ,(option<_, nt2::tag::lead_padding_>)
+                      ,(lead_padding_strategy_< int_<64> >)
                      );
 }
