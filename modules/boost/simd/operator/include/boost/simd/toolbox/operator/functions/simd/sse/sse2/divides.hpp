898dcd475c089b9623d9b4145996db58ce0ac5c1
Mathias Gaunard <mathias@gaunard.com>
2011-12-23 13:02:13 +0100

    disable GCC divides bug workaround for GCC 4.6.3+

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/sse/sse2/divides.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/sse/sse2/divides.hpp
@@ -9,12 +9,13 @@
 #ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_SSE2_DIVIDES_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_SSE2_DIVIDES_HPP_INCLUDED
 #ifdef BOOST_SIMD_HAS_SSE2_SUPPORT
+
 #include <boost/simd/toolbox/operator/functions/divides.hpp>
-#include <boost/simd/sdk/config/compiler.hpp>
 #include <boost/simd/include/functions/is_eqz.hpp>
 #include <boost/simd/include/functions/logical_and.hpp>
-#include <boost/simd/include/constants/nan.hpp>
 #include <boost/simd/include/functions/if_allbits_else.hpp>
+#include <boost/simd/include/constants/nan.hpp>
+#include <boost/simd/sdk/config/compiler.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -28,12 +29,9 @@ namespace boost { namespace simd { namespace ext
     BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
     {
       A0 const that = { _mm_div_pd(a0,a1) };
-#ifdef BOOST_SIMD_COMPILER_GCC
-      //================================================================
-      // this is a workaround for a possible gcc over-optimisation
-      // that produce zero/zero -> zero instead of nan
+#if defined(BOOST_SIMD_COMPILER_GCC) && __GNUC__ <= 4 && __GNUC_MINOR__ <= 6 && __GNUC_PATCHLEVEL__ < 3
+      // workaround for GCC bug #50396 fixed in 4.6.3
       return if_nan_else(logical_and(is_eqz(a0), is_eqz(a1)), that);
-      //================================================================
 #else
       return that;
 #endif
@@ -50,14 +48,11 @@ namespace boost { namespace simd { namespace ext
     BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
     {
       A0 const that = { _mm_div_ps(a0,a1) };
-#ifdef BOOST_SIMD_COMPILER_GCC
-      //================================================================
-      // this is a workaround for a possible gcc over-optimisation
-      // that produce zero/zero -> zero instead of nan
+#if defined(BOOST_SIMD_COMPILER_GCC) && __GNUC__ <= 4 && __GNUC_MINOR__ <= 6 && __GNUC_PATCHLEVEL__ < 3
+      // workaround for GCC bug #50396 fixed in 4.6.3
       return if_nan_else(logical_and(is_eqz(a0), is_eqz(a1)), that);
-      //================================================================
 #else
-     return that;
+      return that;
 #endif
     }
   };
