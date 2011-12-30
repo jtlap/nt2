da9592686bd09795736edb866da8e0d7311ec971
Mathias Gaunard <mathias@gaunard.com>
2011-12-31 00:38:24 +0100

    fix header guard in sign.hpp

--- modules/complex/ieee/include/nt2/toolbox/ieee/functions/complex/generic/sign.hpp
+++ modules/complex/ieee/include/nt2/toolbox/ieee/functions/complex/generic/sign.hpp
@@ -6,8 +6,8 @@
 //                 See accompanying file LICENSE.txt or copy at                 
 //                     http://www.boost.org/LICENSE_1_0.txt                     
 //==============================================================================
-#ifndef NT2_TOOLBOX_arithmetic_FUNCTIONS_COMPLEX_GENERIC_SIGN_HPP_INCLUDED
-#define NT2_TOOLBOX_arithmetic_FUNCTIONS_COMPLEX_GENERIC_SIGN_HPP_INCLUDED
+#ifndef NT2_TOOLBOX_IEEE_FUNCTIONS_COMPLEX_GENERIC_SIGN_HPP_INCLUDED
+#define NT2_TOOLBOX_IEEE_FUNCTIONS_COMPLEX_GENERIC_SIGN_HPP_INCLUDED
 #include <nt2/toolbox/ieee/functions/sign.hpp>
 #include <nt2/include/functions/real.hpp>
 #include <nt2/include/functions/imag.hpp>
