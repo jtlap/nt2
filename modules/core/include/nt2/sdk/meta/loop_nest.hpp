8695a2a97691e3cc8d86a10d370b3ddea1b4df0f
Joel Falcou <joel.falcou@lri.fr>
2012-01-08 17:40:57 +0100

    Fixed loop nest to handle expression evalaution in the new, 2D only indices based system.

--- modules/core/include/nt2/sdk/meta/loop_nest.hpp
+++ modules/core/include/nt2/sdk/meta/loop_nest.hpp
@@ -9,107 +9,114 @@
 #ifndef NT2_SDK_META_LOOP_NEST_HPP_INCLUDED
 #define NT2_SDK_META_LOOP_NEST_HPP_INCLUDED
 
+#include <boost/mpl/int.hpp>
+#include <nt2/sdk/meta/view_at.hpp>
 #include <boost/fusion/include/at.hpp>
 #include <boost/fusion/include/size.hpp>
-#include <boost/fusion/include/array.hpp>
-#include <boost/fusion/include/io.hpp>
-#include <boost/fusion/include/as_vector.hpp>
-#include <boost/mpl/int.hpp>
+#include <boost/fusion/include/fold.hpp>
+#include <boost/fusion/include/value_at.hpp>
+#include <boost/fusion/include/pop_front.hpp>
+#include <boost/fusion/include/vector_tie.hpp>
+#include <nt2/include/functions/multiplies.hpp>
 
-// Experimental OpenMP support
+// TODO: Move to modules/openmp
 #if defined(_OPENMP)
 #include <omp.h>
 #endif
 
 namespace nt2 { namespace meta
 {
-  //==============================================================================
+  //============================================================================
   // Loop nest generator
-  // Main case : generate loops over the Nth dimensions set and forward to next
-  // level as usual
-  //==============================================================================
-  template<std::size_t N,std::size_t MaxIter>
-  struct for_each_impl
+  // Handle unaligned bits if needed, OpenMP the outermost loop
+  //============================================================================
+  template<std::size_t N> struct for_each_impl
   {
-    template<class Bases, class Sizes, class Position, class Step, class F>
+    template<class Bases, class Sizes, class Step, class F>
     static BOOST_DISPATCH_FORCE_INLINE
-    void call(Bases const& bs, Sizes const& sz, Position& p, Step const& s, F const& f)
+    void call(Bases const& bs, Sizes const& sz, Step const& s, F const& f)
     {
-      std::size_t bound = boost::fusion::at_c<N-1>(bs) + boost::fusion::at_c<N-1>(sz);
+      typedef typename boost::fusion::result_of::
+              value_at<Sizes,boost::mpl::int_<0> >::type value_type;
 
-      for ( boost::fusion::at_c<N-1>(p)  = boost::fusion::at_c<N-1>(bs);
-            boost::fusion::at_c<N-1>(p) < bound;
-            ++boost::fusion::at_c<N-1>(p)
-          )
-        for_each_impl<N-1,MaxIter>::call(bs, sz, p, s, f);
+      #pragma omp parallel
+      {
+        std::ptrdiff_t ilow   = boost::fusion::at_c<0>(bs);
+        std::ptrdiff_t olow   = boost::fusion::at_c<1>(bs);
+        std::ptrdiff_t bound  = boost::fusion::at_c<0>(sz) + ilow;
+        std::ptrdiff_t ibound = ilow
+                              + (boost::fusion::at_c<0>(sz)/Step::value)
+                              * Step::value;
+
+        std::ptrdiff_t obound = olow
+                              + boost::fusion::fold
+                                ( boost::fusion::pop_front(sz)
+                                , value_type(1)
+                                , functor<tag::multiplies_>()
+                                );
+
+        #pragma omp for
+        for(std::ptrdiff_t j=olow;j < obound;++j)
+        {
+          for(std::ptrdiff_t i=ilow;i < ibound;i += s)
+          {
+            f( boost::fusion::vector_tie(i,j), s);
+          }
+
+          for(std::ptrdiff_t i=ibound;i < bound;++i)
+          {
+            f( boost::fusion::vector_tie(i,j), boost::mpl::int_<1>());
+          }
+        }
+      }
     }
   };
 
-  //==============================================================================
-  // Loop nest generator
-  // Outermost loop needs special care due to potential OpenMP support.
-  // To satisfy less-than-stellar C++ compliance, the loop and the index support
-  // are broken down to pieces then reconstructed under a classical for() loop
-  //==============================================================================
-  template<std::size_t N> struct for_each_impl<N,N>
+  //============================================================================
+  // 1D Loop nest generator
+  // Handle unaligned bits if needed, OpenMP the largest part
+  //============================================================================
+  template<> struct for_each_impl<1>
   {
-    template<class Bases, class Sizes, class Position, class Step, class F>
+    template<class Bases, class Sizes, class Step, class F>
     static BOOST_DISPATCH_FORCE_INLINE
-    void call(Bases const& bs, Sizes const& sz, Position& p, Step const& s, F const& f)
+    void call(Bases const& bs, Sizes const& sz, Step const& s, F const& f)
     {
-      std::size_t bound  = boost::fusion::at_c<N-1>(sz);
-      std::size_t low    = boost::fusion::at_c<N-1>(bs);
+      std::ptrdiff_t low   = boost::fusion::at_c<0>(bs);
+      std::ptrdiff_t bound = boost::fusion::at_c<0>(sz) + low;
+      std::ptrdiff_t aligned_bound  = low
+                                    + (boost::fusion::at_c<0>(sz)/Step::value)
+                                    * Step::value;
 
-      #pragma omp for
-      for(std::size_t i=0;i<bound;++i)
+      #pragma omp parallel for
+      for(std::ptrdiff_t i=low;i < aligned_bound;i += s)
       {
-        boost::fusion::at_c<N-1>(p) = i + low;
-        for_each_impl<N-1,N>::call(bs, sz, p, s, f);
+        f( boost::fusion::vector_tie(i), s);
+      }
+
+      for(std::ptrdiff_t i=aligned_bound;i < bound;++i)
+      {
+        f( boost::fusion::vector_tie(i), boost::mpl::int_<1>());
       }
     }
   };
 
   //==============================================================================
-  // Loop nest generator
-  // Innermost loop finally calls our functor taking care of the evaluation
-  //==============================================================================
-  template<std::size_t MaxIter> struct for_each_impl<1,MaxIter>
-  {
-    template<class Bases, class Sizes, class Position, class Step, class F>
-    static BOOST_DISPATCH_FORCE_INLINE
-    void call(Bases const& bs, Sizes const& sz, Position& p, Step const& s, F const& f)
-    {
-      std::size_t bound = boost::fusion::at_c<0>(bs) + boost::fusion::at_c<0>(sz);
-      std::size_t aligned_bound = boost::fusion::at_c<0>(bs) + boost::fusion::at_c<0>(sz)/Step::value*Step::value;
-      for( boost::fusion::at_c<0>(p) = boost::fusion::at_c<0>(bs); boost::fusion::at_c<0>(p) != aligned_bound; boost::fusion::at_c<0>(p) += s )
-        f(p, s);
-      for( ; boost::fusion::at_c<0>(p) != bound; ++boost::fusion::at_c<0>(p) )
-        f(p, boost::mpl::int_<1>());
-    }
-  };
-
-  //==============================================================================
-  // Loop nest generator
-  // Special 1-dimensional case, needed to remove ambiguity
-  //==============================================================================
-  template<> struct for_each_impl<1,1> : for_each_impl<1, 2> {};
-
-  //==============================================================================
   // Loop nest generator - Special scalar case
   //==============================================================================
-  template<> struct for_each_impl<0,0>
+  template<> struct for_each_impl<0>
   {
-    template<class Bases, class Sizes, class Position, class Step, class F>
+    template<class Bases, class Sizes, class Step, class F>
     static BOOST_DISPATCH_FORCE_INLINE
-    void call(Bases const&, Sizes const&, Position& pos, Step const&, F const& f)
+    void call(Bases const& bs, Sizes const&, Step const&, F const& f)
     {
-      f(pos, boost::mpl::int_<1>());
+      f( meta::view_at<0>(bs), boost::mpl::int_<1>() );
     }
   };
 
   //============================================================================
   /*!
-   * for_each generates a N dimensions loop nest from a dimensions set. This
+   * for_each generates a loop nest from a dimensions set. This
    * loop nest will take care of calling a custom functor on every position of
    * the iteration space.
    */
@@ -118,13 +125,8 @@ namespace nt2 { namespace meta
   BOOST_DISPATCH_FORCE_INLINE
   void for_each(Bases const& bases, Sizes const& sz, Step const & st, F const& f)
   {
-    static const std::size_t nb_dims = boost::fusion::result_of::size<Sizes>::value;
-
-    #pragma omp parallel
-    {
-      boost::array<std::size_t, nb_dims> position;
-      for_each_impl<nb_dims,nb_dims>::call(bases, sz, position, st, f);
-    }
+    for_each_impl<boost::fusion::result_of::size<Sizes>::value>
+                 ::call(bases, sz, st, f);
   }
 } }
 
