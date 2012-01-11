91e79f4d686090d19602584be3106134b59d67bd
Jt lapreste <jtlapreste@gmail.com>
2012-01-11 07:40:00 +0100

    is_included unit include pb

--- modules/complex/hyperbolic/include/nt2/toolbox/hyperbolic/functions/complex/generic/tanh.hpp
+++ modules/complex/hyperbolic/include/nt2/toolbox/hyperbolic/functions/complex/generic/tanh.hpp
@@ -10,6 +10,7 @@
 #define NT2_TOOLBOX_ARITHMETIC_FUNCTIONS_COMPLEX_GENERIC_TANH_HPP_INCLUDED
 #include <nt2/include/functions/sincos.hpp>
 #include <nt2/include/functions/sinhcosh.hpp>
+#include <nt2/include/functions/tan.hpp>
 #include <nt2/include/functions/real.hpp>
 #include <nt2/include/functions/imag.hpp>
 #include <nt2/include/functions/tanh.hpp>
@@ -21,7 +22,7 @@
 //tanh ( x + iy ) = tanh ( x ) + i . tan ( y ) 1 + i . tanh ( x ) tan ( y ) .
 namespace nt2 { namespace ext
 {
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::tan_, tag::cpu_, (A0)
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::tanh_, tag::cpu_, (A0)
                             , (generic_< complex_< arithmetic_<A0> > >)
                             )
   {
@@ -37,7 +38,7 @@ namespace nt2 { namespace ext
     }
   };
 
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::tan_, tag::cpu_, (A0)
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::tanh_, tag::cpu_, (A0)
                             , (generic_< imaginary_< arithmetic_<A0> > >)
                             )
   {
