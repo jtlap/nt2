2981b6ac2b3a72b2d8065ab56fef3633b37a8a8c
Jt lapreste <jtlapreste@gmail.com>
2012-01-11 07:45:05 +0100

    is_included unit include pb

--- modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/simd/common/is_included_c.hpp
+++ modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/simd/common/is_included_c.hpp
@@ -8,7 +8,7 @@
  ******************************************************************************/
 #ifndef BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_COMMON_IS_INCLUDED_C_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_COMMON_IS_INCLUDED_C_HPP_INCLUDED
-#include <boost/simd/toolbox/bitwise/functions/is_included_c.hpp>
+#include <boost/simd/toolbox/reduction/functions/is_included_c.hpp>
 #include <boost/simd/include/functions/bitwise_and.hpp>
 #include <boost/simd/include/functions/none.hpp>
 
