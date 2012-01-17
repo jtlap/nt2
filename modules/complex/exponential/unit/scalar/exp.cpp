2ae33af4f11bb336cccde5358dc344ad8dd68840
Jt lapreste <jtlapreste@gmail.com>
2012-01-17 22:16:11 +0100

    comletion of limit test for complex exp and log

--- modules/complex/exponential/unit/scalar/exp.cpp
+++ modules/complex/exponential/unit/scalar/exp.cpp
@@ -57,43 +57,35 @@ NT2_TEST_CASE_TPL ( exp_real__1_0,  NT2_REAL_TYPES)
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Zero<T>())), cT(nt2::One<T>()), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(0, nt2::Pi<T>())),  cT(nt2::Mone<T>()), 0.75);
   
-  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Zero<T>(), nt2::Zero<T>())),  nt2::One<cT>(), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Zero<T>(),  nt2::Zero<T>())), nt2::One<cT>(), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Mzero<T>(), nt2::Zero<T>())), nt2::One<cT>(), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), nt2::Zero<T>())), cT(nt2::Inf<T>(), nt2::Zero<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Nan  <T>(), nt2::Zero<T>())), cT(nt2::Nan <T>(), nt2::Zero<T>()), 0.75);
+  
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::One  <T>(), nt2::Inf <T>())), cT(nt2::Nan<T>(), nt2::Nan<T>()), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::One  <T>(), nt2::Nan <T>())), cT(nt2::Nan<T>(), nt2::Nan<T>()), 0.75);
-  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), nt2::Zero<T>())), cT(nt2::Inf<T>(), nt2::Zero<T>()), 0.75);
-  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), nt2::One <T>())), cT(nt2::Zero<T>()*exp(cT(nt2::Zero<T>(), nt2::One <T>()))), 0.75);
-  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), nt2::Mone<T>())), cT(nt2::Zero<T>()*exp(cT(nt2::Zero<T>(), nt2::Mone<T>()))), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), nt2::One <T>())), cT(nt2::Zero<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), nt2::Mone<T>())), cT(nt2::Zero<T>()), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), nt2::One <T>())), cT(nt2::Inf <T>()*exp(cT(nt2::Zero<T>(), nt2::One <T>()))), 0.75);   
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), nt2::Mone<T>())), cT(nt2::Inf <T>()*exp(cT(nt2::Zero<T>(), nt2::Mone<T>()))), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), nt2::Inf<T>())),  cT(nt2::Zero<T>()), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), nt2::Inf<T>())),  cT(nt2::Inf <T>(), nt2::Nan<T>()), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), nt2::Nan<T>())),  cT(nt2::Zero<T>()), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), nt2::Nan<T>())),  cT(nt2::Inf <T>(), nt2::Nan<T>()), 0.75);
-  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Nan  <T>(), nt2::Zero<T>())), cT(nt2::Nan <T>(), nt2::Zero<T>()), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Nan  <T>(), nt2::One<T>())),  cT(nt2::Nan <T>(), nt2::Nan<T>()), 0.75);
   NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Nan  <T>(), nt2::Nan<T>())),  cT(nt2::Nan <T>(), nt2::Nan<T>()), 0.75);
- } // end of test for floating_
 
-//  C99 standard
-//  cexp(conj(z)) = conj(cexp(z)).
-//  cexp(±0 + i0) returns 1 + i0.
-//  cexp(x + i $B!g(B) returns NaN + iNaN and raises the -F¡¡invalid¢¢ floating-point-A
-//                                                   exception, for finite x.
-//  cexp(x + iNaN) returns NaN + iNaN and optionally raises the -F¡¡invalid¢¢ floating--A
-//                                     point exception, for finite x.
-//  cexp(+$B!g(B + i0) returns +$B!g(B + i0.
-//  cexp($B!]!g(B + iy) returns +0 cis(y), for finite y.
-//  cexp(+$B!g(B + iy) returns +$B!g(B cis(y), for finite nonzero y.
-//  cexp($B!]!g(B + i $B!g(B) returns ±0 ± i0 (where the signs of the real and imaginary parts of
-//                                   the result are unspecified).
-//  cexp(+$B!g(B + i $B!g(B) returns ±$B!g(B + iNaN and raises the -F¡¡invalid¢¢ floating-point-A
-//                                    exception (where the sign of the real part of the result is unspecified).
-//  cexp($B!]!g(B + iNaN) returns ±0 ± i0 (where the signs of the real and imaginary parts
-//                                    of the result are unspecified).
-//  cexp(+$B!g(B + iNaN) returns ±$B!g(B + iNaN (where the sign of the real part of the result
-//                                      is unspecified).
-//  cexp(NaN + i0) returns NaN + i0.
-//  cexp(NaN + iy) returns NaN + iNaN and optionally raises the -F¡¡invalid¢¢ floating--A
-//                                    point exception, for all nonzero numbers y.
-//  cexp(NaN + iNaN) returns NaN + iNaN.
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::One  <T>(), -nt2::Inf <T>())), cT(nt2::Nan<T>(), nt2::Nan<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::One  <T>(), -nt2::Nan <T>())), cT(nt2::Nan<T>(), nt2::Nan<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), -nt2::One <T>())), cT(nt2::Zero<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), -nt2::Mone<T>())), cT(nt2::Zero<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), -nt2::One <T>())), cT(nt2::Inf <T>()*exp(cT(nt2::Zero<T>(), -nt2::One <T>()))), 0.75);   
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), -nt2::Mone<T>())), cT(nt2::Inf <T>()*exp(cT(nt2::Zero<T>(), -nt2::Mone<T>()))), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), -nt2::Inf<T>())),  cT(nt2::Zero<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), -nt2::Inf<T>())),  cT(nt2::Inf <T>(), -nt2::Nan<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Minf <T>(), -nt2::Nan<T>())),  cT(nt2::Zero<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Inf  <T>(), -nt2::Nan<T>())),  cT(nt2::Inf <T>(), -nt2::Nan<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Nan  <T>(), -nt2::One<T>())),  cT(nt2::Nan <T>(), -nt2::Nan<T>()), 0.75);
+  NT2_TEST_ULP_EQUAL(nt2::exp(cT(nt2::Nan  <T>(), -nt2::Nan<T>())),  cT(nt2::Nan <T>(), -nt2::Nan<T>()), 0.75);
+} // end of test for floating_
+
