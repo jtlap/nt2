d7f509cf14290e9bf81aaacbd2b55c4e2009166b
Mathias Gaunard <mathias@gaunard.com>
2012-01-25 12:28:34 +0100

    fix as_tuple and at on simd::tuple

--- modules/boost/simd/sdk/include/boost/simd/sdk/tuple.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/tuple.hpp
@@ -10,7 +10,6 @@
 #ifndef BOOST_SIMD_SDK_TUPLE_HPP_INCLUDED
 #define BOOST_SIMD_SDK_TUPLE_HPP_INCLUDED
 
-#include <boost/dispatch/details/parameters.hpp>
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/facilities/intercept.hpp>
 #include <boost/preprocessor/arithmetic/inc.hpp>
@@ -21,13 +20,18 @@
 #include <boost/preprocessor/repetition/enum.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
 #include <boost/preprocessor/repetition/enum_binary_params.hpp>
+#include <boost/dispatch/details/parameters.hpp>
+#include <boost/dispatch/attributes.hpp>
+#include <boost/dispatch/meta/identity.hpp>
+#include <boost/dispatch/meta/strip.hpp>
+#include <boost/mpl/bool.hpp>
 #include <boost/mpl/size_t.hpp>
 #include <boost/mpl/apply.hpp>
-#include <boost/dispatch/meta/identity.hpp>
 #include <boost/fusion/support.hpp>
 #include <boost/fusion/include/value_at.hpp>
-#include <boost/type_traits/remove_reference.hpp>
+#include <boost/fusion/include/size.hpp>
 
+#define TUPLE_TYPES(z, n, t) typedef T##n m##n##_type;
 #define TUPLE_MEMBERS(z, n, t) T##n m##n;
 #define TUPLE_CTORS(z, n, t) tuple(BOOST_PP_ENUM_BINARY_PARAMS(n, T, const& a)) BOOST_PP_EXPR_IF(n, :) BOOST_PP_ENUM(n, TUPLE_CTORS_, ~) {}
 #define TUPLE_CTORS_(z, n, t) m##n(a##n)
@@ -42,14 +46,18 @@ namespace boost { namespace simd
   template<BOOST_PP_ENUM_BINARY_PARAMS(BOOST_DISPATCH_MAX_ARITY, class T, = void BOOST_PP_INTERCEPT)>
   struct tuple;
   
+  namespace details
+  {
+    template<class Seq, class Lambda, int N>
+    struct as_tuple;
+  }
+  
   namespace meta
   {
     template<class Seq, class Lambda = boost::dispatch::identity>
     struct as_tuple
+     : details::as_tuple<Seq, Lambda, fusion::result_of::size<Seq>::type::value>
     {
-      #define M0(z, n, t) typename boost::mpl::apply<Lambda, typename fusion::result_of::value_at_c<Seq, n>::type>::type
-      typedef tuple<BOOST_PP_ENUM(BOOST_DISPATCH_MAX_ARITY, M0, ~)> type;
-      #undef M0
     };
   }
   
@@ -57,6 +65,24 @@ namespace boost { namespace simd
 
 namespace boost { namespace fusion { namespace extension
 {
+  template<>
+  struct is_sequence_impl<boost::simd::tag::tuple_>
+  {
+    template<class Seq>
+    struct apply : mpl::true_
+    {
+    };
+  };
+
+  template<>
+  struct is_view_impl<boost::simd::tag::tuple_>
+  {
+    template<class Seq>
+    struct apply : mpl::false_
+    {
+    };
+  };
+  
   template<>
   struct size_impl<boost::simd::tag::tuple_>
   {
@@ -82,9 +108,11 @@ namespace boost { namespace fusion { namespace extension
   template<>
   struct value_at_impl<boost::simd::tag::tuple_>
   {
+    template<class Seq, int N>
+    struct apply_c;
+    
     template<class Seq, class N>
-    struct apply
-     : remove_reference<typename at_impl<boost::simd::tag::tuple_>::template apply<Seq, N>::type>
+    struct apply : apply_c<Seq, N::value>
     {
     };
   };
@@ -96,7 +124,7 @@ namespace boost { namespace fusion { namespace extension
     struct apply
     {
       typedef boost::simd::at_iterator<Seq, 0> type;
-      static type call(Seq& seq)
+      BOOST_FORCEINLINE static type call(Seq& seq)
       {
         return type(seq);
       }
@@ -110,7 +138,7 @@ namespace boost { namespace fusion { namespace extension
     struct apply
     {
       typedef boost::simd::at_iterator<Seq, Seq::static_size> type;
-      static type call(Seq& seq)
+      BOOST_FORCEINLINE static type call(Seq& seq)
       {
         return type(seq);
       }
@@ -147,6 +175,7 @@ namespace boost { namespace simd
 #undef TUPLE_CTORS_
 #undef TUPLE_CTORS
 #undef TUPLE_MEMBERS
+#undef TUPLE_TYPES
 
 #endif
 
@@ -161,31 +190,50 @@ namespace boost { namespace simd
   {
     typedef tag::tuple_ fusion_tag;
     static const std::size_t static_size = N;
+    BOOST_PP_REPEAT(N, TUPLE_TYPES, ~)
     
     BOOST_PP_REPEAT(BOOST_PP_INC(N), TUPLE_CTORS, ~)
     BOOST_PP_REPEAT(N, TUPLE_MEMBERS, ~)
   };
 } }
 
+namespace boost { namespace simd { namespace details
+{
+  template<class Seq, class Lambda>
+  struct as_tuple<Seq, Lambda, N>
+  {
+    #define M0(z, n, t) typename boost::mpl::apply<Lambda, typename fusion::result_of::value_at_c<Seq, n>::type>::type
+    typedef tuple<BOOST_PP_ENUM(N, M0, ~)> type;
+    #undef M0
+  };
+} } }
+
 namespace boost { namespace fusion { namespace extension
 {
-  template<BOOST_PP_ENUM_PARAMS(BOOST_DISPATCH_MAX_ARITY, class T)>
-  struct at_impl<boost::simd::tag::tuple_>
-  ::apply_c< boost::simd::tuple<BOOST_PP_ENUM_PARAMS(BOOST_DISPATCH_MAX_ARITY, T)>, BOOST_PP_DEC(N) >
+  template<class Seq>
+  struct value_at_impl<boost::simd::tag::tuple_>
+  ::apply_c<Seq, BOOST_PP_DEC(N) >
   {
-    typedef BOOST_PP_CAT(T, BOOST_PP_DEC(N))& type;
-    static type call(boost::simd::tuple<BOOST_PP_ENUM_PARAMS(BOOST_DISPATCH_MAX_ARITY, T)>& seq)
+    typedef typename Seq::BOOST_PP_CAT(BOOST_PP_CAT(m, BOOST_PP_DEC(N)), _type) type;
+  };
+  
+  template<class Seq>
+  struct at_impl<boost::simd::tag::tuple_>
+  ::apply_c<Seq, BOOST_PP_DEC(N) >
+  {
+    typedef typename Seq::BOOST_PP_CAT(BOOST_PP_CAT(m, BOOST_PP_DEC(N)), _type)& type;
+    BOOST_FORCEINLINE static type call(Seq& seq)
     {
       return seq.BOOST_PP_CAT(m, BOOST_PP_DEC(N));
     }
   };
   
-  template<BOOST_PP_ENUM_PARAMS(BOOST_DISPATCH_MAX_ARITY, class T)>
+  template<class Seq>
   struct at_impl<boost::simd::tag::tuple_>
-  ::apply_c< boost::simd::tuple<BOOST_PP_ENUM_PARAMS(BOOST_DISPATCH_MAX_ARITY, T)> const, BOOST_PP_DEC(N) >
+  ::apply_c<Seq const, BOOST_PP_DEC(N) >
   {
-    typedef BOOST_PP_CAT(T, BOOST_PP_DEC(N)) const& type;
-    static type call(boost::simd::tuple<BOOST_PP_ENUM_PARAMS(BOOST_DISPATCH_MAX_ARITY, T)> const& seq)
+    typedef typename Seq::BOOST_PP_CAT(BOOST_PP_CAT(m, BOOST_PP_DEC(N)), _type) const& type;
+    BOOST_FORCEINLINE static type call(Seq const& seq)
     {
       return seq.BOOST_PP_CAT(m, BOOST_PP_DEC(N));
     }
