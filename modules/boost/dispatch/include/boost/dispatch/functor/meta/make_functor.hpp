2a265e14cdd74a746afa0f3f143efc3114e243cf
Joel Falcou <joel.falcou@lri.fr>
2012-01-27 09:30:45 +0100

    Added missing include in make_fucntor

--- modules/boost/dispatch/include/boost/dispatch/functor/meta/make_functor.hpp
+++ modules/boost/dispatch/include/boost/dispatch/functor/meta/make_functor.hpp
@@ -9,6 +9,8 @@
 #ifndef BOOST_DISPATCH_FUNCTOR_META_MAKE_FUNCTOR_HPP_INCLUDED
 #define BOOST_DISPATCH_FUNCTOR_META_MAKE_FUNCTOR_HPP_INCLUDED
 
+#include <boost/dispatch/functor/functor.hpp>
+
 /*!
  * \file
  * \brief Implements a delayed \ref boost::dispatch::functor type instanciation system
