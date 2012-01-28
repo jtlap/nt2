82b42431102054a356e46eb9a797bfcdfdfd171e
Joel Falcou <joel.falcou@lri.fr>
2012-01-28 17:20:41 +0100

    Fixed missing include in complex cscd

--- modules/complex/trigonometric/include/nt2/toolbox/trigonometric/functions/complex/generic/cscd.hpp
+++ modules/complex/trigonometric/include/nt2/toolbox/trigonometric/functions/complex/generic/cscd.hpp
@@ -1,18 +1,19 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef NT2_TOOLBOX_TRIGONOMETRIC_FUNCTIONS_COMPLEX_GENERIC_CSCD_HPP_INCLUDED
 #define NT2_TOOLBOX_TRIGONOMETRIC_FUNCTIONS_COMPLEX_GENERIC_CSCD_HPP_INCLUDED
-#include <nt2/include/functions/sin.hpp>
+
 #include <nt2/include/functions/rec.hpp>
-#include <nt2/sdk/complex/meta/as_complex.hpp>
-#include <nt2/sdk/complex/meta/as_real.hpp>
+#include <nt2/include/functions/sind.hpp>
 #include <nt2/sdk/complex/meta/as_dry.hpp>
+#include <nt2/sdk/complex/meta/as_real.hpp>
+#include <nt2/sdk/complex/meta/as_complex.hpp>
 
 //cscd(x+iy)=rec(sind(x+iy)).
 namespace nt2 { namespace ext
@@ -24,10 +25,10 @@ namespace nt2 { namespace ext
     typedef A0 result_type;
     NT2_FUNCTOR_CALL(1)
     {
-      return rec(nt2::sind(a0));     
+      return rec(nt2::sind(a0));
     }
   };
-  
+
 } }
 
 #endif
