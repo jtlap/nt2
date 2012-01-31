91b2c36864e5c26407aaa3364a7ad4192aa89b25
Mathias Gaunard <mathias@gaunard.com>
2012-01-31 17:50:27 +0100

    fix bad definition of flags in boost.simd.sdk dependencies

--- modules/boost/simd/sdk/cmake/nt2.boost.simd.sdk.dependencies.cmake
+++ modules/boost/simd/sdk/cmake/nt2.boost.simd.sdk.dependencies.cmake
@@ -13,14 +13,14 @@ MESSAGE( STATUS "[boost.simd.sdk] target system: ${NT2_OS} (${CMAKE_SYSTEM_NAME}
 MESSAGE( STATUS "[boost.simd.sdk] target processor: ${NT2_ARCH} (${CMAKE_SYSTEM_PROCESSOR})" )
 
 # Disable useless qualifier flag because of const-return trick used by Proto
-set(FLAGS)
+set(FLAGS ${NT2_SIMD_FLAGS})
 if(CMAKE_CXX_COMPILER MATCHES "[\\/]icpc[^\\/]*")
   if(UNIX)
-    set(FLAGS -wd2536)
+    set(FLAGS "${FLAGS} -wd2536")
   else()
-    set(FLAGS /wd2536)
+    set(FLAGS "${FLAGS} /wd2536")
   endif()
 endif()
 
-SET(NT2_BOOST.SIMD.SDK_FLAGS ${NT2_SIMD_FLAGS} ${FLAGS})
+SET(NT2_BOOST.SIMD.SDK_FLAGS ${FLAGS})
 SET(NT2_BOOST.SIMD.SDK_DEPENDENCIES_EXTRA boost.dispatch boost.simd.config)
