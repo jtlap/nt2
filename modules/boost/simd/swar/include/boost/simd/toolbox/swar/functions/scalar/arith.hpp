45d06259b5484c05989bc8e4214f310bc841941e
Jt lapreste <jtlapreste@gmail.com>
2012-02-06 11:37:39 +0100

    generation of arithmetic sequences in simd vectors

---
+++ modules/boost/simd/swar/include/boost/simd/toolbox/swar/functions/scalar/arith.hpp
@@ -0,0 +1,61 @@
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef BOOST_SIMD_TOOLBOX_SWAR_FUNCTIONS_SCALAR_ARITH_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_SWAR_FUNCTIONS_SCALAR_ARITH_HPP_INCLUDED
+
+#include <boost/simd/toolbox/swar/functions/arith.hpp>
+#include <boost/simd/include/constants/zero.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::arith_, tag::cpu_
+                                    , (T)
+                                    , (target_< scalar_< arithmetic_<T> > >)
+                                    )
+  {
+    typedef typename T::type result_type;
+
+    result_type operator()(T const& ) const
+    {
+      Zero<T>();
+    }
+  };
+
+   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::arith_, tag::cpu_
+                                    , (A0)(T)
+                                    , (scalar_< arithmetic_<A0> >)
+                                      (target_< scalar_< arithmetic_<T> > >)
+                                    )
+  {
+    typedef typename T::type result_type;
+
+    result_type operator()(A0 const& a0, T const& ) const
+    {
+      return static_cast<T>(a0);
+    }
+  };
+  
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::arith_, tag::cpu_
+                                      , (A0)(A1)(T)
+                                      , (scalar_< arithmetic_<A0> >)
+                                        (scalar_< arithmetic_<A1> >)
+                                        (target_< scalar_< arithmetic_<T> > >)
+                                    )
+  {
+    typedef typename T::type result_type;
+
+    result_type operator()(A0 const& a0, A1 const&, T const& ) const
+    {
+      return static_cast<T>(a0);
+    }
+  };
+    
+} } }
+
+#endif
--- modules/boost/simd/swar/unit/simd/arith.cpp
+++ modules/boost/simd/swar/unit/simd/arith.cpp
@@ -8,7 +8,7 @@
 //////////////////////////////////////////////////////////////////////////////
 #define NT2_UNIT_MODULE "nt2 boost.simd.swar toolbox - enumerate SIMD Mode"
 
-#include <boost/simd/toolbox/swar/include/functions/enumerate.hpp>
+#include <boost/simd/toolbox/swar/include/functions/arith.hpp>
 
 #include <boost/type_traits/is_same.hpp>
 #include <boost/dispatch/functor/meta/call.hpp>
@@ -18,33 +18,43 @@
 #include <boost/simd/toolbox/constant/constant.hpp>
 #include <boost/simd/include/functions/load.hpp>
 
-NT2_TEST_CASE_TPL ( enumerate, BOOST_SIMD_SIMD_TYPES)
+NT2_TEST_CASE_TPL ( arith, BOOST_SIMD_SIMD_TYPES)
 {
   using boost::simd::native;
-  using boost::simd::enumerate;
+  using boost::simd::arith;
   using boost::dispatch::meta::as_;
-  using boost::simd::tag::enumerate_;
-
+  using boost::simd::tag::arith_;
+  using boost::simd::arith;
+  
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
   typedef native<T,ext_t>               vT;
-  typedef typename boost::dispatch::meta::call<enumerate_(T, as_<vT> )>::type r_t;
+  typedef typename boost::dispatch::meta::call<arith_(T, as_<vT> )>::type r_t;
 
-  std::cout << enumerate<vT>(T(10)) << std::endl; 
+  std::cout << arith<vT>() << std::endl; 
   for(std::size_t i=0; i < vT::static_size;++i)
-    NT2_TEST_EQUAL(enumerate<vT>(T(10))[i], T(i+10));
+    NT2_TEST_EQUAL(arith<vT>()[i], T(i));
+  std::cout << arith<vT>(T(10)) << std::endl; 
+  for(std::size_t i=0; i < vT::static_size;++i)
+    NT2_TEST_EQUAL(arith<vT>(T(10))[i], T(i+10));
+  std::cout << arith<vT>(T(10), T(0.5)) << std::endl; 
+  for(std::size_t i=0; i < vT::static_size;++i)
+    NT2_TEST_EQUAL(arith<vT>(T(10), T(0.5))[i], T(10 + T(0.5)*i));
 }
 
-NT2_TEST_CASE_TPL ( enumerate_from_int, BOOST_SIMD_SIMD_TYPES)
+NT2_TEST_CASE_TPL ( arith_from_int, BOOST_SIMD_SIMD_TYPES)
 {
   using boost::simd::native;
-  using boost::simd::enumerate;
+  using boost::simd::arith;
   using boost::dispatch::meta::as_;
-  using boost::simd::tag::enumerate_;
+  using boost::simd::tag::arith_;
 
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t; 
   typedef native<T,ext_t>               vT;
-  typedef typename boost::dispatch::meta::call<enumerate_(int, as_<vT> )>::type r_t;
-  std::cout << enumerate<vT>(int(10)) << std::endl; 
+  typedef typename boost::dispatch::meta::call<arith_(int, as_<vT> )>::type r_t;
+  std::cout << arith<vT>(int(10)) << std::endl; 
   for(std::size_t i=0; i < vT::static_size;++i)
-    NT2_TEST_EQUAL(enumerate<vT>(int(10))[i], T(10 + i));
+    NT2_TEST_EQUAL(arith<vT>(int(10))[i], T(10 + i));
+  std::cout << arith<vT>(int(10), T(0.5)) << std::endl; 
+  for(std::size_t i=0; i < vT::static_size;++i)
+    NT2_TEST_EQUAL(arith<vT>(int(10), T(0.5))[i], T(10 + T(0.5)*i));
 }
