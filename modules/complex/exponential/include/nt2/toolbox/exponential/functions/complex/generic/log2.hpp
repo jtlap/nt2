d4732c0794a58437d78bfb29ffc53a5397b6c683
Jt lapreste <jtlapreste@gmail.com>
2012-02-09 09:24:26 +0100

    exponential simd complex. log is still buggy for some limits

--- modules/complex/exponential/include/nt2/toolbox/exponential/functions/complex/generic/log2.hpp
+++ modules/complex/exponential/include/nt2/toolbox/exponential/functions/complex/generic/log2.hpp
@@ -30,35 +30,7 @@ namespace nt2 { namespace ext
                          arg(a0)/Log_2<rtype>()); 
     }
   };
-  
- //  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::log2_, tag::cpu_
-//                             , (A0)
-//                             , (generic_< imaginary_<floating_<A0> > >)
-//                             )
-//   {
-//     typedef typename meta::as_real<A0>::type             rtype; 
-//     typedef typename meta::as_complex<rtype>::type result_type;
-//     NT2_FUNCTOR_CALL(1)
-//     {
-//       return result_type(nt2::log2(nt2::abs(a0)),
-//                          nt2::arg(a0)/Log_2<rtype>()); 
-//     }
-//   };
 
-//   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::log2_, tag::cpu_
-//                             , (A0)
-//                             , (generic_< dry_<floating_<A0> > >)
-//                             )
-//   {
-//     typedef typename meta::as_real<A0>::type             rtype; 
-//     typedef typename meta::as_complex<rtype>::type result_type;
-//     NT2_FUNCTOR_CALL(1)
-//     {
-//       return result_type(nt2::log2(nt2::abs(real(a0))),
-//                          nt2::arg(a0)/Log_2<rtype>()); 
-//     }
-//   };
-    
 } }
 
 
