070a7638e16799fe3e48ca17b4f5d443b0a86d62
Joel Falcou <joel.falcou@lri.fr>
2012-02-09 16:26:48 +0100

    Removed useless colon specialisation for extent, any unspecified has size _0D now

--- modules/core/include/nt2/core/functions/scalar/extent.hpp
+++ modules/core/include/nt2/core/functions/scalar/extent.hpp
@@ -16,7 +16,7 @@
 namespace nt2 { namespace ext
 {
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::extent_, tag::cpu_
-                            , (A0), (scalar_< unspecified_<A0> >)
+                            , (A0), (unspecified_<A0>)
                             )
   {
     typedef _0D result_type;
@@ -24,17 +24,6 @@ namespace nt2 { namespace ext
     BOOST_DISPATCH_FORCE_INLINE
     result_type operator()(const A0&) const { return result_type(); }
   };
-
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::extent_, tag::cpu_
-                            , (A0), (colon_< A0 >)
-                            )
-  {
-    typedef _0D result_type;
-
-    BOOST_DISPATCH_FORCE_INLINE
-    result_type operator()(const A0&) const { return result_type(); }
-  };
-
 } }
 
 #endif
