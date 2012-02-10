5814361aaba720581b8abb9eb33d16d74e645e39
Jt lapreste <jtlapreste@gmail.com>
2012-02-10 17:15:09 +0100

    safe_min and safe_max needed by somme trigo and hyper math functions

---
+++ modules/boost/simd/ieee/include/boost/simd/toolbox/ieee/functions/safe_min.hpp
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+/*!
+ * \file
+**/
+#ifndef BOOST_SIMD_TOOLBOX_IEEE_FUNCTIONS_SAFE_MIN_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_IEEE_FUNCTIONS_SAFE_MIN_HPP_INCLUDED
+#include <boost/simd/include/simd.hpp>
+#include <boost/dispatch/include/functor.hpp>
+
+/*!
+ * \ingroup boost_simd_ieee
+ * \defgroup boost_simd_ieee_safe_min safe_min
+ *
+ * \par Description
+ * Returns the safe_min relative to abs(a0),  ie the minimal
+ * value which will not underflow when divided by a0.
+ *
+ * \par Header file
+ * 
+ * \code
+ * #include <nt2/include/functions/safe_min.hpp>
+ * \endcode
+ * 
+ * 
+ * \synopsis
+ *
+ * \code
+ * namespace boost::simd
+ * {
+ *   template <class A0>
+ *     meta::call<tag::safe_min_(A0)>::type
+ *     safe_min(const A0 & a0);
+ * }
+ * \endcode
+ *
+ * \param a0 the unique parameter of safe_min
+ * 
+ * \return a value of the same type as the parameter
+ *  
+ * \par Notes
+ * In SIMD mode, this function acts elementwise on the inputs vectors elements
+ * \par
+ *  
+**/
+
+namespace boost { namespace simd { namespace tag
+  {         
+    /*!
+     * \brief Define the tag safe_min_ of functor safe_min 
+     *        in namespace boost::simd::tag for toolbox boost.simd.ieee
+    **/
+    struct safe_min_ : ext::elementwise_<safe_min_> { typedef ext::elementwise_<safe_min_> parent; };
+  }
+  BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::safe_min_, safe_min, 1)
+} }
+
+#endif
+
+// modified by jt the 25/12/2010
---
+++ modules/boost/simd/ieee/unit/scalar/safe_min.cpp
@@ -0,0 +1,55 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#define NT2_UNIT_MODULE "nt2 boost.simd.ieee toolbox - safe_min/scalar Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// unit test behavior of boost.simd.ieee components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+/// created by jt the 04/12/2010
+/// 
+#include <boost/simd/toolbox/ieee/include/functions/safe_min.hpp>
+#include <boost/simd/include/functions/ulpdist.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/dispatch/functor/meta/call.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <boost/simd/sdk/memory/buffer.hpp>
+#include <boost/simd/toolbox/constant/constant.hpp>
+
+
+NT2_TEST_CASE_TPL ( safe_min_real__1_0,  BOOST_SIMD_REAL_TYPES)
+{
+  
+  using boost::simd::safe_min;
+  using boost::simd::tag::safe_min_;
+  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
+  typedef typename boost::dispatch::meta::call<safe_min_(T)>::type r_t;
+  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
+  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
+  typedef typename boost::dispatch::meta::upgrade<T>::type u_t;
+  typedef typename boost::common_type<T>::type wished_r_t;
+
+
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+  ulpd=0.0;
+
+
+  // specific values tests
+  NT2_TEST_EQUAL(safe_min(boost::simd::Mone<T>()), boost::simd::Sqrtsmallestposval<r_t>());
+  NT2_TEST_EQUAL(safe_min(boost::simd::One<T>()), boost::simd::Sqrtsmallestposval<r_t>());
+  NT2_TEST_EQUAL(safe_min(boost::simd::Zero<T>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(safe_min(boost::simd::Inf<T>()), boost::simd::Inf<r_t>());
+  NT2_TEST_EQUAL(safe_min(boost::simd::Minf<T>()), boost::simd::Inf<r_t>());
+  NT2_TEST_EQUAL(safe_min(boost::simd::Nan<T>()), boost::simd::Nan<r_t>());
+} // end of test for floating_
+
+
