584bdfeee21289274bcca392787870f4a6417fe6
Joel Falcou <joel.falcou@lri.fr>
2012-02-10 20:06:09 +0100

    Fixed various warning on unused parameters in SIMD predicates

--- modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/generic/is_real.hpp
+++ modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/generic/is_real.hpp
@@ -1,10 +1,10 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_GENERIC_IS_REAL_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_GENERIC_IS_REAL_HPP_INCLUDED
@@ -19,7 +19,10 @@ namespace boost { namespace simd { namespace ext
                             )
   {
     typedef typename meta::as_logical<A0>::type result_type;
-    BOOST_SIMD_FUNCTOR_CALL(1) { return True<A0>(); }
+    BOOST_FORCEINLINE result_type operator()(A0 const&) const
+    {
+      return True<A0>();
+  }
   };
 } } }
 
