f6073013d8683dfa456bdb7a800f09d18e7915c4
Mathias Gaunard <mathias@gaunard.com>
2012-02-22 17:56:01 +0100

    add support for restrict pointers in boost::is_pointer, boost::pointee and boost::dispatch::meta::is_iterator. Should probably be integrated upstream in Boost.

--- modules/boost/dispatch/include/boost/dispatch/meta/is_iterator.hpp
+++ modules/boost/dispatch/include/boost/dispatch/meta/is_iterator.hpp
@@ -19,7 +19,38 @@
 #include <boost/utility/enable_if.hpp>
 #include <boost/dispatch/meta/enable_if_type.hpp>
 #include <boost/type_traits/is_function.hpp>
+#include <boost/type_traits/is_pointer.hpp>
 #include <boost/type_traits/remove_pointer.hpp>
+
+//============================================================================
+// Fix a couple of things for restrict pointers
+// FIXME: integrate upstream
+//============================================================================
+#if defined(_MSC_VER) || defined(__GNUC__)
+#ifdef _MSC_VER
+#define BOOST_DISPATCH_RESTRICT __restrict
+#else
+#define BOOST_DISPATCH_RESTRICT __restrict__
+#endif
+namespace boost
+{
+  template<class T>
+  struct is_pointer<T BOOST_DISPATCH_RESTRICT>
+   : mpl::true_
+  {
+  };
+
+  namespace detail
+  {
+    template<class T>
+    struct iterator_traits<T BOOST_DISPATCH_RESTRICT>
+     : boost::detail::iterator_traits<T>
+    {
+    };
+  }
+}
+#undef BOOST_DISPATCH_RESTRICT
+#endif
 
 namespace boost { namespace dispatch { namespace meta
 {
@@ -49,21 +80,7 @@ namespace boost { namespace dispatch { namespace meta
    */
   //============================================================================
   template<class T, class Enable=void>
-  struct  is_iterator : boost::mpl::false_  {};
-
-  //============================================================================
-  // Overload for pointers
-  //============================================================================
-  template<class T>
-  struct  is_iterator < T*
-                      , typename boost::
-                        enable_if_c< !boost::
-                                     is_function<typename boost::
-                                                 remove_pointer<T>::type
-                                                >::value
-                                   >::type
-                    >
-    : boost::mpl::true_   {};
+  struct  is_iterator : boost::is_pointer<T>  {};
 
   //============================================================================
   // Overload for function pointers (which are not iterator)
