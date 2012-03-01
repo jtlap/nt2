1006ccfa1b77c12843f778270e73e5085b56d522
Joel Falcou <joel.falcou@lri.fr>
2012-03-01 20:25:14 +0100

    Make cache_padding works OK with fusion view of non-const reference

--- modules/core/include/nt2/sdk/memory/cache_padding.hpp
+++ modules/core/include/nt2/sdk/memory/cache_padding.hpp
@@ -37,15 +37,15 @@ namespace nt2 { namespace memory
     template<class T> struct result_impl<T,0>   { typedef std::size_t type; };
 
     template<class T, class N, class V>
-    typename result<cache_padding(T const&, N const&, V const&)>::type
-    operator()(T const& t, N const& n, V const& v) const
+    typename result<cache_padding(T&, N const&, V const&)>::type
+    operator()(T& t, N const& n, V const& v) const
     {
       return eval(t,n,v,boost::mpl::bool_<N::value==0>());
     }
 
     template<class T, class N, class V>
-    typename result<cache_padding(T const&, N const&, V const&)>::type
-    eval(T const& t, N const&, V const&, boost::mpl::true_ const&) const
+    typename result<cache_padding(T&, N const&, V const&)>::type
+    eval(T& t, N const&, V const&, boost::mpl::true_ const&) const
     {
       static const std::size_t  tz = sizeof(typename V::type);
 
@@ -56,8 +56,8 @@ namespace nt2 { namespace memory
       return sz/tz;
     }
 
-    template<class T, class N, class V> T const&
-    eval(T const& t, N const&, V const&, boost::mpl::false_ const&) const
+    template<class T, class N, class V> T&
+    eval(T& t, N const&, V const&, boost::mpl::false_ const&) const
     {
       return t;
     }
