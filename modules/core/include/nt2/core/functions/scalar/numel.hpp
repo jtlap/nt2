4b4e1a0401f519cbe5bbdc9c27dd2d456a2e5652
Joel Falcou <joel.falcou@lri.fr>
2012-03-01 22:11:13 +0100

    Reverted numel changes

--- modules/core/include/nt2/core/functions/scalar/numel.hpp
+++ modules/core/include/nt2/core/functions/scalar/numel.hpp
@@ -9,9 +9,10 @@
 #ifndef NT2_CORE_FUNCTIONS_SCALAR_NUMEL_HPP_INCLUDED
 #define NT2_CORE_FUNCTIONS_SCALAR_NUMEL_HPP_INCLUDED
 
+#include <boost/mpl/size_t.hpp>
 #include <nt2/core/functions/numel.hpp>
-#include <boost/type_traits/common_type.hpp>
 #include <boost/fusion/include/fold.hpp>
+#include <nt2/include/functions/multiplies.hpp>
 
 namespace nt2 { namespace ext
 {
@@ -30,34 +31,18 @@ namespace nt2 { namespace ext
                             , (fusion_sequence_<A0>)
                             )
   {
-    struct mul_
-    {
-      template<class Sig> struct result;
-      template<class This,class A, class B>
-      struct result<This(A,B)>
-      {
-        typedef typename boost::common_type<A,B>::type  base;
-        typedef typename meta::strip<base>::type        type;
-      };
-
-      template<class A, class B>
-      BOOST_FORCEINLINE typename result<mul_(A const&,B const&)>::type
-      operator()(const A& a, const B& b) const
-      {
-        typedef typename result<mul_(A,B)>::type type;
-        return type(a)*type(b);
-      }
-    };
+    typedef typename  boost::dispatch::
+                      make_functor<tag::multiplies_,A0>::type func_t;
 
     typedef typename  boost::fusion::result_of::
-                      fold<A0,boost::mpl::size_t<1>,mul_>::type   result_type;
+                      fold<A0,boost::mpl::size_t<1>,func_t>::type   result_type;
 
     BOOST_DISPATCH_FORCE_INLINE
     result_type operator()(const A0& a0) const
     {
       return boost::fusion::fold( a0
                                 , boost::mpl::size_t<1>()
-                                , mul_()
+                                , func_t()
                                 );
     }
   };
