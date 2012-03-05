5d15634bded5a5215362bea5f99be97b3722ee71
Joel Falcou <joel.falcou@lri.fr>
2012-03-05 16:47:34 +0100

    Modified box semantic to carry over contents semantic. Added test for temporary passing of value

--- modules/core/unit/container/dsl/box.cpp
+++ modules/core/unit/container/dsl/box.cpp
@@ -38,11 +38,7 @@ NT2_TEST_CASE( semantic_of )
 NT2_TEST_CASE( value_semantic )
 {
   using nt2::box;
-  using nt2::_2D;
-  using boost::mpl::_;
   using nt2::tag::box_;
-  using nt2::meta::as_;
-  using boost::dispatch::meta::semantic_of;
 
   foo f = { 42 };
 
@@ -54,4 +50,33 @@ NT2_TEST_CASE( value_semantic )
 
   a0 = f;
   NT2_TEST_EQUAL( f.i, a0.value().i );
+}
+
+foo make_foo(int i) { foo f = {i}; return f; }
+
+// For the sake of it, let's pretend foo can do plus
+namespace boost { namespace simd { namespace ext
+{
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION
+  ( boost::simd::tag::plus_, tag::cpu_, (A0)
+  , (unspecified_< const foo >)(unspecified_<A0>)
+  )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2) { return a0; }
+  };
+} } }
+
+NT2_TEST_CASE( temp_semantic )
+{
+  using nt2::box;
+  using nt2::tag::box_;
+
+  box < foo > a0(make_foo(42));
+  NT2_TEST_EQUAL( 42, a0.value().i );
+
+  BOOST_AUTO(temporary_boxes, box<foo>(make_foo(42)) + box<foo>(make_foo(24)) );
+
+  NT2_TEST_EQUAL( 42, boost::proto::child_c<0>( temporary_boxes).value().i );
+  NT2_TEST_EQUAL( 24, boost::proto::child_c<1>( temporary_boxes ).value().i );
 }
