93ba6f2e5f13eea83f76bc16bf217949682b8912
Jt lapreste <jtlapreste@gmail.com>
2012-03-06 11:08:34 +0100

    issymetric

--- modules/core/unit/functions/issymetric.cpp
+++ modules/core/unit/functions/issymetric.cpp
@@ -15,6 +15,7 @@
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
 
+
 NT2_TEST_CASE( fundamental_issymetric )
 {
   NT2_TEST( nt2::issymetric('e') );
@@ -26,17 +27,17 @@ NT2_TEST_CASE( fundamental_issymetric )
  NT2_TEST_CASE( container_issymetric )
  {
 //        std::cout << nt2::ones(4)(1) << std::endl;
-//        std::cout << nt2::ones(4, 4)(1, 1) << std::endl;
+        std::cout << nt2::ones(4, 4)(1, 1) << std::endl;
 
-      NT2_TEST( !nt2::issymetric( nt2::ones(4, nt2::meta::as_<float>()))      );
-   //   NT2_TEST( nt2::issymetric( nt2::ones(4,4))     );
+      NT2_TEST( nt2::issymetric( nt2::ones(4, nt2::meta::as_<float>()))      );
+//       NT2_TEST( nt2::issymetric( nt2::ones(4,4))     );
    
-   //   NT2_TEST( !nt2::issymetric( nt2::ones(4,1,1))   );
-   //   NT2_TEST( !nt2::issymetric( nt2::ones(4,1,1,1)) );
+//       NT2_TEST( !nt2::issymetric( nt2::ones(4,1,1))   );
+//       NT2_TEST( !nt2::issymetric( nt2::ones(4,1,1,1)) );
    
-   //   NT2_TEST( !nt2::issymetric( nt2::ones(2,3))     );
-   //   NT2_TEST( !nt2::issymetric( nt2::ones(3,1,2))   );
-   //   NT2_TEST( !nt2::issymetric( nt2::ones(3,3,1,9)) );
+//       NT2_TEST( !nt2::issymetric( nt2::ones(2,3))     );
+//       NT2_TEST( !nt2::issymetric( nt2::ones(3,1,2))   );
+//       NT2_TEST( !nt2::issymetric( nt2::ones(3,3,1,9)) );
   
  }
  
