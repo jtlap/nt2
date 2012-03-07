c2129efae0e02b4d099773ed55608623e79489e1
Jt lapreste <jtlapreste@gmail.com>
2012-03-07 23:11:06 +0100

    issymetric.cpp

--- modules/core/unit/functions/issymetric.cpp
+++ modules/core/unit/functions/issymetric.cpp
@@ -15,7 +15,6 @@
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
 
-
 // NT2_TEST_CASE( fundamental_issymetric )
 // {
 //   NT2_TEST( nt2::issymetric('e') );
@@ -27,9 +26,9 @@
  NT2_TEST_CASE( container_issymetric )
  {
 //        std::cout << nt2::ones(4)(1) << std::endl;
-        std::cout << nt2::ones(4, 4)(1, 1) << std::endl;
+          std::cout << nt2::ones(4, 4)(1, 1) << std::endl;
 
-//      NT2_TEST( nt2::issymetric( nt2::ones(4, nt2::meta::as_<float>()))      );
+//       NT2_TEST( nt2::issymetric( nt2::ones(4, nt2::meta::as_<float>()))      );
 //       NT2_TEST( nt2::issymetric( nt2::ones(4,4))     );
    
 //       NT2_TEST( !nt2::issymetric( nt2::ones(4,1,1))   );
