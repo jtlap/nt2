ce2a836459042c432dd67fd7f33120049548d343
Mathias Gaunard <mathias@gaunard.com>
2012-03-12 21:55:42 +0100

    put old proto_value meta-function in container domain

--- modules/core/include/nt2/core/container/dsl/domain.hpp
+++ modules/core/include/nt2/core/container/dsl/domain.hpp
@@ -13,13 +13,53 @@
 #include <nt2/core/container/dsl/generator.hpp>
 #include <boost/proto/domain.hpp>
 
+#include <boost/dispatch/meta/as_ref.hpp>
+#include <boost/dispatch/meta/strip.hpp>
+#include <boost/type_traits/add_reference.hpp>
+#include <boost/utility/enable_if.hpp>
+
 namespace nt2 { namespace container
 {
+  template<class T, class Enable = void>
+  struct proto_value
+    : boost::dispatch::meta::as_ref<T>
+  {
+  };
+
+  template<class T, class Enable = void>
+  struct proto_value_impl
+   : boost::dispatch::meta::strip<T>
+  {
+  };
+
+  template<class T>
+  struct proto_value_impl<T, typename boost::enable_if< boost::is_same<typename boost::proto::tag_of<T>::type, boost::proto::tag::terminal> >::type>
+    : boost::add_reference<T>
+  {
+  };
+
+  template<class T>
+  struct proto_value<T, typename boost::enable_if< boost::proto::is_expr<T> >::type>
+   : proto_value_impl<T>
+  {
+  };
+
   struct  domain
         : boost::proto::domain< container::generator_transform<domain>
                               , container::grammar
                               >
-  {};
+  {
+    template<class T>
+    struct as_child : boost::proto::callable
+    {
+      typedef typename proto_value<T>::type result_type;
+      BOOST_FORCEINLINE result_type
+      operator()(T& t) const
+      {
+        return result_type(t);
+      }
+    };
+  };
 } }
 
 #endif
