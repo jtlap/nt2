6427e57b86c24ed567776be0aafbd3d84440dd99
Mathias Gaunard <mathias@gaunard.com>
2012-03-17 04:23:27 +0100

    build qbk doc with cmake instead of boost.build

---
+++ cmake/nt2.xsltproc.cmake
@@ -0,0 +1,11 @@
+################################################################################
+#         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+#         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+#
+#          Distributed under the Boost Software License, Version 1.0.
+#                 See accompanying file LICENSE.txt or copy at
+#                     http://www.boost.org/LICENSE_1_0.txt
+################################################################################
+
+set(ENV{XML_CATALOG_FILES} ${CATALOG})
+execute_process(COMMAND ${XSLTPROC_EXECUTABLE} --output ${OUTPUT} --xinclude --nonet ${ARGS})
