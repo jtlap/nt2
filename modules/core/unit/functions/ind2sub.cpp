937f673dde3a193491b9c625185ce6455c8d679a
Alexandre Borghi <xander.alex@gmail.com>
2012-03-21 15:15:28 +0100

    Add tests for ind2sub

--- modules/core/unit/functions/ind2sub.cpp
+++ modules/core/unit/functions/ind2sub.cpp
@@ -17,13 +17,53 @@
 #include <nt2/sdk/unit/tests/basic.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
 
+NT2_TEST_CASE( ind2sub_1D )
+{
+  using boost::fusion::make_vector;
+  using boost::mpl::int_;
+
+  boost::array<int, 1> a = nt2::ind2sub( make_vector(3), 2 );
+  boost::array<int, 1> b = {{2}};
+  for(int i=0;i<1;++i) NT2_TEST_EQUAL( a[i], b[i] );
+}
+
+NT2_TEST_CASE( ind2sub_1D_base1 )
+{
+  using boost::fusion::make_vector;
+  using boost::mpl::int_;
+
+  boost::array<int, 1> a = nt2::ind2sub( make_vector(3), 2, make_vector(1) );
+  boost::array<int, 1> b = {{2}};
+  for(int i=0;i<1;++i) NT2_TEST_EQUAL( a[i], b[i] );
+}
+
+NT2_TEST_CASE( ind2sub_1D_base )
+{
+  using boost::fusion::make_vector;
+  using boost::mpl::int_;
+
+  boost::array<int, 1> a = nt2::ind2sub( make_vector(4), 3, make_vector(2) );
+  boost::array<int, 1> b = {{3}};
+  for(int i=0;i<1;++i) NT2_TEST_EQUAL( a[i], b[i] );
+}
+
 NT2_TEST_CASE( ind2sub_2D )
 {
   using boost::fusion::make_vector;
   using boost::mpl::int_;
 
-  boost::array<int, 2> a = nt2::ind2sub( make_vector(3,3), int_<3>() );
+  boost::array<int, 2> a = nt2::ind2sub( make_vector(3,2), 3 );
   boost::array<int, 2> b = {{3,1}};
+  for(int i=0;i<2;++i) NT2_TEST_EQUAL( a[i], b[i] );
+}
+
+NT2_TEST_CASE( ind2sub_2D_base1 )
+{
+  using boost::fusion::make_vector;
+  using boost::mpl::int_;
+
+  boost::array<int, 2> a = nt2::ind2sub( make_vector(3,2), 4, make_vector(1,1) );
+  boost::array<int, 2> b = {{1,2}};
   for(int i=0;i<2;++i) NT2_TEST_EQUAL( a[i], b[i] );
 }
 
@@ -32,9 +72,39 @@ NT2_TEST_CASE( ind2sub_2D_base )
   using boost::fusion::make_vector;
   using boost::mpl::int_;
 
-  boost::array<int, 2> a = nt2::ind2sub( make_vector(3,3), int_<3>(), make_vector(1,1) );
-  boost::array<int, 2> b = {{3,1}};
+  boost::array<int, 2> a = nt2::ind2sub( make_vector(3,4), 5, make_vector(2,3) );
+  boost::array<int, 2> b = {{2,4}};
   for(int i=0;i<2;++i) NT2_TEST_EQUAL( a[i], b[i] );
+}
+
+NT2_TEST_CASE( ind2sub_3D )
+{
+  using boost::fusion::make_vector;
+  using boost::mpl::int_;
+
+  boost::array<int, 3> a = nt2::ind2sub( make_vector(2,3,5), 15 );
+  boost::array<int, 3> b = {{1,2,3}};
+  for(int i=0;i<3;++i) NT2_TEST_EQUAL( a[i], b[i] );
+}
+
+NT2_TEST_CASE( ind2sub_3D_base1 )
+{
+  using boost::fusion::make_vector;
+  using boost::mpl::int_;
+
+  boost::array<int, 3> a = nt2::ind2sub( make_vector(2,3,5), 15, make_vector(1,1,1) );
+  boost::array<int, 3> b = {{1,2,3}};
+  for(int i=0;i<3;++i) NT2_TEST_EQUAL( a[i], b[i] );
+}
+
+NT2_TEST_CASE( ind2sub_3D_base )
+{
+  using boost::fusion::make_vector;
+  using boost::mpl::int_;
+
+  boost::array<int, 3> a = nt2::ind2sub( make_vector(2,3,5), 15, make_vector(2,3,4) );
+  boost::array<int, 3> b = {{3,3,6}};
+  for(int i=0;i<3;++i) NT2_TEST_EQUAL( a[i], b[i] );
 }
 
 NT2_TEST_CASE( ind2sub_4D )
@@ -42,7 +112,17 @@ NT2_TEST_CASE( ind2sub_4D )
   using boost::fusion::make_vector;
   using boost::mpl::int_;
 
-  boost::array<int, 4> a = nt2::ind2sub( make_vector(7,5,3,2), int_<93>() );
+  boost::array<int, 4> a = nt2::ind2sub( make_vector(7,5,3,2), 93 );
+  boost::array<int, 4> b = {{2,4,3,1}};
+  for(int i=0;i<4;++i) NT2_TEST_EQUAL( a[i], b[i] );
+}
+
+NT2_TEST_CASE( ind2sub_4D_base1 )
+{
+  using boost::fusion::make_vector;
+  using boost::mpl::int_;
+
+  boost::array<int, 4> a = nt2::ind2sub( make_vector(7,5,3,2), 93, make_vector(1,1,1,1) );
   boost::array<int, 4> b = {{2,4,3,1}};
   for(int i=0;i<4;++i) NT2_TEST_EQUAL( a[i], b[i] );
 }
@@ -52,7 +132,7 @@ NT2_TEST_CASE( ind2sub_4D_base )
   using boost::fusion::make_vector;
   using boost::mpl::int_;
 
-  boost::array<int, 4> a = nt2::ind2sub( make_vector(7,5,3,2), int_<93>(), make_vector(1,1,1,1) );
-  boost::array<int, 4> b = {{2,4,3,1}};
+  boost::array<int, 4> a = nt2::ind2sub( make_vector(7,5,4,3), 93, make_vector(1,2,3,4) );
+  boost::array<int, 4> b = {{2,5,5,4}};
   for(int i=0;i<4;++i) NT2_TEST_EQUAL( a[i], b[i] );
 }
