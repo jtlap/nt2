95698490dbddb5455bafb4d77713a0694c31eab4
Mathias Gaunard <mathias@gaunard.com>
2012-03-29 18:11:16 +0200

    provide an implementation of ndims for fusion sequences; unlike for tables and the rest, always give the real result, not a minimum of 2

--- modules/core/include/nt2/core/functions/expr/ndims.hpp
+++ modules/core/include/nt2/core/functions/expr/ndims.hpp
@@ -9,10 +9,8 @@
 #ifndef NT2_CORE_FUNCTIONS_EXPR_NDIMS_HPP_INCLUDED
 #define NT2_CORE_FUNCTIONS_EXPR_NDIMS_HPP_INCLUDED
 
-#include <algorithm>
-#include <nt2/core/container/dsl.hpp>
 #include <nt2/core/functions/ndims.hpp>
-#include <nt2/core/functions/extent.hpp>
+#include <nt2/include/functions/extent.hpp>
 #include <nt2/core/container/category.hpp>
 
 namespace nt2 { namespace ext
@@ -23,27 +21,20 @@ namespace nt2 { namespace ext
                             )
   {
     typedef std::size_t result_type;
-
-    static bool local_match(std::size_t i) { return i != 1; }
     
     BOOST_DISPATCH_FORCE_INLINE
     result_type operator()(const A0& a0) const
     {
       typedef typename A0::extent_type extent_type;
       extent_type sz  = a0.extent();
-      std::size_t d,i = sz.size();
+      std::size_t i = a0.extent().size();
 
       // If less than 2D, then return 2 directly
       if(i < 2) return 2;
 
-      // Else find the first non-1 from the end
-      typename meta::strip<extent_type>::type::const_reverse_iterator
-      b = sz.rbegin(), e = sz.rend(),c;
-      c = std::find_if(b,e,local_match);
+      std::size_t d = nt2::ndims(a0.extent());
 
       // ndims is either 2, |c-e| or, if d is 0, sz.size()
-      d = std::distance(c,e);
-      
       return d ? ( (d < 2) ? 2 : d) : i;                     
     }
   };
