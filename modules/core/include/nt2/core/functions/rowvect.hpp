d0277c4130ed53761c194328f8de42648ec5e3a5
Jt lapreste <jtlapreste@gmail.com>
2012-04-10 09:57:29 +0200

    rowvect colvect

---
+++ modules/core/include/nt2/core/functions/rowvect.hpp
@@ -0,0 +1,55 @@
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_FUNCTIONS_ROWVECT_HPP_INCLUDED
+#define NT2_CORE_FUNCTIONS_ROWVECT_HPP_INCLUDED
+
+/*!
+ * \file
+ * \brief Defines and implements the nt2::rowvect function
+ */
+
+#include <nt2/include/functor.hpp>
+#include <nt2/core/container/dsl/details/reshaping.hpp>
+
+namespace nt2
+{
+  namespace tag
+  {
+    struct rowvect_ : ext::reshaping_<rowvect_>
+    {
+      typedef ext::reshaping_<rowvect_> parent;
+    };
+  }
+
+  //============================================================================
+  /*!
+   * superior triangular part of a 2D table.
+   *
+   * \param xpr 2D table (must verify is_matrix(a))
+   */
+  //============================================================================
+  NT2_FUNCTION_IMPLEMENTATION(nt2::tag::rowvect_       , rowvect, 1)
+}
+
+namespace nt2 { namespace container { namespace ext
+{
+  template<class Domain, class Expr>
+  struct  generator<nt2::tag::rowvect_,Domain,2,Expr>
+        : reshaping_generator<Expr>
+  {};
+
+
+  template<class Domain, class Expr>
+  struct  size_of<nt2::tag::rowvect_,Domain,2,Expr>
+        : reshaping_size_of<Expr>
+  {};
+
+} } }
+
+#endif
---
+++ modules/core/unit/functions/colvect.cpp
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#define NT2_UNIT_MODULE "nt2::colvect function"
+
+#include <nt2/table.hpp>
+#include <nt2/include/functions/colvect.hpp>
+#include <nt2/include/functions/size.hpp>
+#include <nt2/include/functions/ones.hpp>
+#include <nt2/include/functions/ndims.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/tests/exceptions.hpp>
+
+NT2_TEST_CASE_TPL( colvect_ofsize, (float) )
+{
+  nt2::table<T> r;
+  nt2::table<T, nt2::_2D> y( nt2::of_size(4,4) );
+
+  for(int j=1;j<=4;j++)
+    for(int i=1;i<=4;i++)
+      y(i,j) = T(i + 10*j);
+
+  r = nt2::colvect(y);
+
+  int k = 1; 
+  for(int j=1;j<=4;j++)
+    for(int i=1;i<=4;i++, k++)
+      NT2_TEST_EQUAL( T(r(k)) , T(y(i, j)) );
+  NT2_TEST_EQUAL( nt2::ndims(r)  , 2 );
+  NT2_TEST_EQUAL( nt2::size(r,1) , numel(y) );
+  NT2_TEST_EQUAL( nt2::size(r,2) , 1 );
+  NT2_TEST_EQUAL( nt2::size(r,3) , 1 );
+
+}
+
+NT2_TEST_CASE_TPL( colvect_size, (float) )
+{
+  nt2::table<T> r;
+  nt2::table<T, nt2::_2D> y( nt2::of_size(4,4) );
+
+  for(int j=1;j<=4;j++)
+    for(int i=1;i<=4;i++)
+      y(i,j) = T(i + 10*j);
+
+  r = nt2::colvect(y);
+  int k = 1; 
+  for(int j=1;j<=4;j++)
+    for(int i=1;i<=4;i++, k++)
+      NT2_TEST_EQUAL( T(r(k)) , T(y(i, j)) );
+  NT2_TEST_EQUAL( nt2::ndims(r)  , 2 );
+  NT2_TEST_EQUAL( nt2::size(r,1) , numel(y) );
+  NT2_TEST_EQUAL( nt2::size(r,2) , 1 );
+  NT2_TEST_EQUAL( nt2::size(r,3) , 1 );
+
+}
+
