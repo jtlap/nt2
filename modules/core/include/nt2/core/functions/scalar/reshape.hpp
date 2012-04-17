8d0a162c23296c6d5a34262b2e3b47f05de32235
Mathias Gaunard <mathias@gaunard.com>
2012-04-17 20:52:44 +0200

    fix references in reshape

--- modules/core/include/nt2/core/functions/scalar/reshape.hpp
+++ modules/core/include/nt2/core/functions/scalar/reshape.hpp
@@ -10,19 +10,23 @@
 #define NT2_CORE_FUNCTIONS_SCALAR_RESHAPE_HPP_INCLUDED
 
 #include <nt2/core/functions/reshape.hpp>
-
-#include <nt2/sdk/memory/copy.hpp>
 #include <nt2/core/container/dsl.hpp>
 #include <nt2/core/utility/box.hpp>
-#include <nt2/include/functions/length.hpp>
+
+#include <nt2/sdk/parameters.hpp>
+#include <boost/preprocessor/arithmetic/inc.hpp>
+#include <boost/preprocessor/repetition/repeat_from_to.hpp>
+#include <boost/preprocessor/repetition/enum_shifted_params.hpp>
+#include <boost/preprocessor/repetition/enum_shifted_binary_params.hpp>
 
 namespace nt2 { namespace ext
 {
   //============================================================================
   // Generates linearize_ from expression + N size value
   //============================================================================
-  #define M2(z,n,t) (BOOST_PP_CAT(A,n))
-  #define M1(z,n,t) (scalar_< integer_<BOOST_PP_CAT(A,n)> >)
+  #define M3(z,n,t) A##n const& a##n
+  #define M2(z,n,t) (A##n)
+  #define M1(z,n,t) (scalar_< integer_<A##n> >)
 
   #define M0(z,n,t)                                                           \
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::reshape_, tag::cpu_                   \
@@ -32,32 +36,33 @@ namespace nt2 { namespace ext
                               BOOST_PP_REPEAT_FROM_TO(1,BOOST_PP_INC(n),M1,~) \
                             )                                                 \
   {                                                                           \
-    typedef BOOST_PP_CAT(_,BOOST_PP_CAT(n,D)) sizes_t;                        \
+    typedef _##n##D sizes_t;                                                  \
     typedef typename  boost::proto::                                          \
                       result_of::make_expr< nt2::tag::reshape_                \
                                           , container::domain                 \
-                                          , A0 &                              \
+                                          , A0&                               \
                                           , box<sizes_t>                      \
                                           >::type   result_type;              \
                                                                               \
     BOOST_FORCEINLINE result_type                                             \
-    operator()(BOOST_PP_ENUM_BINARY_PARAMS(BOOST_PP_INC(n),A,& a)) const      \
+    operator()(A0& a0, BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS(BOOST_PP_INC(n),A,const& a)) const \
     {                                                                         \
       sizes_t sizee(BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_INC(n),a));         \
       BOOST_ASSERT_MSG                                                        \
       ( nt2::numel(a0) == nt2::numel(sizee)                                   \
       , "To RESHAPE the number of elements must not change."                  \
       );                                                                      \
-      return  boost::proto::                                                  \
-              make_expr < nt2::tag::reshape_                                  \
-                        , container::domain                                   \
-        > ( boost::reference_wrapper<A0>(a0), boxify(sizee) ); \
+      return boost::proto::                                                   \
+             make_expr< nt2::tag::reshape_                                    \
+                      , container::domain                                     \
+                      > ( boost::reference_wrapper<A0>(a0), boxify(sizee) );  \
     }                                                                         \
   };                                                                          \
   /**/
 
   BOOST_PP_REPEAT_FROM_TO(2,BOOST_PP_INC(NT2_MAX_DIMENSIONS),M0,~)
 
+  #undef M3
   #undef M2
   #undef M1
   #undef M0
