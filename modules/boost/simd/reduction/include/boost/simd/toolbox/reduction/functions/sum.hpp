7aeb5fd4270ae41744da2c7b7700404a46da42cc
Joel Falcou <joel.falcou@lri.fr>
2012-04-18 09:00:31 +0200

    Fixed improper dependencies of boost.simd over nt2 by pushing reduction geenrator calls to their proper plac ein nt2/core module

--- modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/sum.hpp
+++ modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/sum.hpp
@@ -15,9 +15,6 @@
 #include <boost/dispatch/include/functor.hpp>
 #include <boost/simd/toolbox/operator/functions/plus.hpp>
 #include <boost/simd/toolbox/constant/constants/zero.hpp>
-#include <nt2/sdk/memory/container.hpp>
-#include <nt2/core/container/dsl/details/reduction.hpp>
-#include <nt2/core/container/dsl/size.hpp>
 
 /*!
  * \ingroup boost_simd_reduction
@@ -27,12 +24,12 @@
  * Returns the sum of the elements of the SIMD vector
  *
  * \par Header file
- * 
+ *
  * \code
  * #include <nt2/include/functions/sum.hpp>
  * \endcode
- * 
- * 
+ *
+ *
  * \synopsis
  *
  * \code
@@ -45,9 +42,9 @@
  * \endcode
  *
  * \param a0 the unique parameter of sum
- * 
+ *
  * \return always a scalar value
- *  
+ *
  * \par Notes
  * \par
  * This is a reduction operation. As such it has not real interest outside
@@ -58,18 +55,18 @@
  * \par
  * If usable and used in scalar mode, it reduces to the operation as acting
  * on a one element vector.
- *  
+ *
 **/
 
 namespace boost { namespace simd { namespace tag
-  {         
+  {
     /*!
-     * \brief Define the tag sum_ of functor sum 
+     * \brief Define the tag sum_ of functor sum
      *        in namespace boost::simd::tag for toolbox boost.simd.reduction
     **/
-    struct sum_ : ext::reduction_<sum_, tag::plus_, tag::Zero> 
-    { 
-      typedef ext::reduction_<sum_, tag::plus_, tag::Zero> parent; 
+    struct sum_ : ext::reduction_<sum_, tag::plus_, tag::Zero>
+    {
+      typedef ext::reduction_<sum_, tag::plus_, tag::Zero> parent;
     };
   }
   BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::sum_, sum, 1)
@@ -77,22 +74,4 @@ namespace boost { namespace simd { namespace tag
 
 } }
 
-namespace nt2 { namespace container { namespace ext
-{
-  template<class Domain, class Expr>
-  struct size_of<boost::simd::tag::sum_,Domain,1,Expr> 
-    : reduction_size_of<boost::simd::tag::sum_, 1, Expr>{};
-
-  template<class Domain, class Expr>
-  struct size_of<boost::simd::tag::sum_,Domain,2,Expr> 
-    : reduction_size_of<boost::simd::tag::sum_, 2, Expr>{};
-
-
-  template<class Domain, int N, class Expr>
-  struct generator<boost::simd::tag::sum_,Domain,N,Expr> 
-    : reduction_generator<boost::simd::tag::sum_,N,Expr> {};
-} } }
-
 #endif
-
-// modified by jt the 25/12/2010
