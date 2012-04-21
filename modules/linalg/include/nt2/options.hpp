d67ef61898662aeae9cdb45fa14f66a7f16eac48
Joel Falcou <joel.falcou@lri.fr>
2012-04-21 18:33:17 +0200

    Added option objects to use as Matlab 'string' like options

---
+++ modules/linalg/include/nt2/options.hpp
@@ -0,0 +1,29 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_OPTIONS_HPP_INCLUDED
+#define NT2_OPTIONS_HPP_INCLUDED
+
+#include <nt2/sdk/meta/as.hpp>
+
+namespace nt2
+{
+  namespace details
+  {
+    // Markee for 'lower' option
+    struct lower_ {};
+
+    // Markee for in place option
+    struct in_place_ {};
+  }
+
+  meta::as_<details::lower_>    const lower_ = {};
+  meta::as_<details::in_place_> const in_place_ = {};
+}
+
+#endif
