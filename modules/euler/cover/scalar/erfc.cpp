3ad5fce6c972330b1bc5474c8b5475570619e9cf
jt <jtlapreste@gmail.com>
2012-04-25 14:27:23 +0200

    euler erfc wih unit bench and cover

---
+++ modules/euler/cover/scalar/erfc.cpp
@@ -0,0 +1,70 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#define NT2_UNIT_MODULE "nt2 euler toolbox - erfc/scalar Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// cover test behavior of euler components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+/// created  by jt the 22/02/2011
+/// 
+#include <nt2/toolbox/euler/include/functions/erfc.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/include/functions/max.hpp>
+#include <nt2/toolbox/boost_math/include/functions/erfc.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/as_floating.hpp>
+#include <nt2/sdk/meta/as_signed.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+#include <nt2/sdk/meta/downgrade.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
+#include <boost/dispatch/meta/as_floating.hpp>
+#include <boost/type_traits/common_type.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
+#include <nt2/toolbox/constant/constant.hpp>
+
+
+NT2_TEST_CASE_TPL ( erfc_real__1_0,  NT2_REAL_TYPES)
+{
+  
+  using nt2::erfc;
+  using nt2::tag::erfc_;
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef typename nt2::meta::call<erfc_(T)>::type r_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
+  typedef typename nt2::meta::upgrade<T>::type u_t;
+  typedef typename boost::dispatch::meta::as_floating<T>::type wished_r_t;
+
+
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+  ulpd=0.0;
+
+  // random verifications
+  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
+  {
+    NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(10));
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    T a0;
+    for(nt2::uint32_t j =0; j < NR; ++j )
+      {
+        std::cout << "for param "
+                  << "  a0 = "<< u_t(a0 = tab_a0[j])
+                  << std::endl;
+        NT2_TEST_ULP_EQUAL( nt2::erfc(a0),nt2::boost_math::erfc(a0),25.0);
+        ulp0=nt2::max(ulpd,ulp0);
+     }
+     std::cout << "max ulp found is: " << ulp0 << std::endl;
+   }
+} // end of test for floating_
