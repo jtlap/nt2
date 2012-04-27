3fdaf7f5d56190f5b88e0a25673d1583627ca029
jt <jtlapreste@gmail.com>
2012-04-27 17:14:10 +0200

    schur details

---
+++ modules/linalg/include/nt2/toolbox/linalg/functions/details/schur.hpp
@@ -0,0 +1,229 @@
+/*******************************************************************************
+ *         Copyright 2003-2012 LASMEA UMR 6602 CNRS/U.B.P
+ *         Copyright 2011-2012 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_DETAILS_SCHUR_HPP_INCLUDED
+#define NT2_TOOLBOX_LINALG_FUNCTIONS_DETAILS_SCHUR_HPP_INCLUDED
+
+#include <nt2/toolbox/linalg/details/utility/workspace.hpp>
+//#include <nt2/toolbox/linalg/details/utility/lower.hpp>
+#include <nt2/include/constants/eps.hpp>
+#include <nt2/include/functions/schur.hpp>
+#include <nt2/include/functions/of_size.hpp>
+#include <nt2/include/functions/min.hpp>
+#include <nt2/include/functions/isempty.hpp>
+#include <nt2/include/functions/height.hpp>
+#include <nt2/include/functions/width.hpp>
+#include <nt2/toolbox/linalg/details/lapack/geesx.hpp>
+#include <nt2/include/functions/expand.hpp>
+#include <nt2/include/functions/prod.hpp>
+#include <nt2/sdk/complex/meta/is_complex.hpp>
+#include <iostream>
+
+#include <nt2/table.hpp>
+//#include <iostream>
+//#include <nt2/include/functions/diag.hpp>
+
+//  schur  schur decomposition.
+//     [u,t] = schur(x) produces a quasitriangular schur matrix t and
+//     a unitary matrix u so that x = u*t*u' and u'*u = eye(size(u)).
+//     x must be square.
+ 
+//     t = schur(x) returns just the schur matrix t.
+ 
+//     if x is complex, the complex schur form is returned in matrix t.
+//     the complex schur form is upper triangular with the eigenvalues
+//     of x on the diagonal.
+ 
+//     if x is real, two different decompositions are available.
+//     schur(x,'real') has the real eigenvalues on the diagonal and the
+//     complex eigenvalues in 2-by-2 blocks on the diagonal.
+//     schur(x,'complex') is triangular and is complex if x has complex
+//     eigenvalues.  schur(x,'real') is the default.
+///////////// TO DO
+// The second style must be enforced in nt2 by converting input to complex
+// before calling schur
+///////////// 
+//     see rsf2csf to convert from real to complex schur form.
+namespace nt2 {
+  namespace details
+  {
+    template<class T,
+             class CPLX = typename nt2::details::is_complex<typename T::value_type >::type>
+    struct schur_result
+    {
+      typedef typename meta::strip<T>::type                   source_t;
+      typedef typename source_t::value_type                     type_t;
+      typedef typename source_t::index_type                     index_t;
+      typedef typename meta::as_real<type_t>::type              base_t;
+      typedef typename meta::as_integer<base_t, signed>::type  itype_t;
+      typedef T                                                data_t;
+      typedef nt2::table<type_t,nt2::matlab_index_>              tab_t;
+      typedef nt2::table<base_t,nt2::matlab_index_>             btab_t;
+      typedef nt2::table<itype_t,nt2::matlab_index_>            itab_t;
+      typedef nt2::details::workspace<type_t>              workspace_t;
+      typedef nt2::table<nt2_la_int,nt2::matlab_index_>         ibuf_t;
+      typedef nt2::table<type_t,index_t>                   result_type;
+      
+      
+      template<class Input>
+      schur_result ( Input& xpr
+                     , char jobvs = 'V'
+                     , char sort  = 'N'
+                     , char sense = 'N')
+        : jobvs_(jobvs)
+        , sort_(sort)
+        , sense_(sense)
+        , a_(xpr)
+        , aa_(xpr)
+        , n_(nt2::height(xpr))
+        , lda_(a_.leading_size())
+      {
+        jobvs_ = (sense_ == 'E' || sense_ == 'B') ? 'V':jobvs_;
+        sort_ = (sense_ == 'N') ? sort_ : 'S'; 
+        ldvs_ = (jobvs_ == 'N') ? n_ : 1;
+        w_.resize(nt2::of_size(n_, 1));
+        vs_.resize(of_size(ldvs_, ldvs_));
+        ldvs_ = vs_.leading_size(); 
+        
+        nt2::details::geesx(&jobvs_, &sort_, &nt2::details::selectall , &sense_, &n_, 
+                            aa_.raw(), &lda_, &sdim_, w_.raw(),
+                            vs_.raw(), &ldvs_,
+                            &rconde_, &rcondv_, 
+                            &info_, wrk_);
+      }
+      result_type values() const { return aa_; }
+      result_type     w () const { return from_diag(w);}
+      result_type     t () const { return aa_;     }
+      result_type     z () const 
+      {
+        BOOST_ASSERT_MSG( (jobvs_ == 'V'), "choose jobvs == 'V' to compute z"); 
+        return vs_;
+      }
+      nt2_la_int  status() const   { return info_;   }
+      base_t      rconde()
+      {
+        BOOST_ASSERT_MSG( (sense_ ==  'E' || sense_ ==  'B'), "choose sense in {'E', 'B'} to compute rconde"); 
+        return rconde_; 
+      }
+      base_t      rcondv()
+      {
+        BOOST_ASSERT_MSG( (sense_ ==  'E' || sense_ ==  'B'), "choose sense in {'E', 'B'} to compute rconde"); 
+        return rcondv_; 
+      }
+    private:
+      template<class S>
+      static tab_t from_diag(const S& w)
+      {
+        tab_t m = nt2::zeros(numel(w), numel(w), meta::as_<type_t>());
+        for (int i = 1; i <= numel(w); ++i) m(i, i) = w(i);
+        return m;
+      }
+      char                 jobvs_, sort_;
+      char                        sense_;
+      data_t                          a_;
+      tab_t                          aa_; 
+      nt2_la_int                      n_;
+      nt2_la_int                    lda_; 
+      nt2_la_int                   ldvs_;
+      nt2_la_int                   sdim_; 
+      base_t            rcondv_, rconde_;
+      tab_t                          vs_; 
+      tab_t                           w_; 
+      nt2_la_int                   info_; 
+      workspace_t                   wrk_;
+    }; 
+
+    template<class T>
+    struct schur_result < T, boost::mpl::false_ >
+    {
+      typedef typename meta::strip<T>::type                   source_t;
+      typedef typename source_t::value_type                     type_t;
+      typedef typename source_t::index_type                    index_t;
+      typedef typename meta::as_real<type_t>::type              base_t;
+      typedef typename meta::as_integer<base_t, signed>::type  itype_t;
+      typedef T                                                 data_t;
+      typedef nt2::table<type_t,nt2::matlab_index_>              tab_t;
+      typedef nt2::table<base_t,nt2::matlab_index_>             btab_t;
+      typedef nt2::table<itype_t,nt2::matlab_index_>            itab_t;
+      typedef nt2::details::workspace<type_t>              workspace_t;
+      typedef nt2::table<nt2_la_int,nt2::matlab_index_>         ibuf_t;
+      typedef nt2::table<type_t,index_t>                   result_type;
+      
+      
+      template<class Input>
+      schur_result ( Input& xpr
+                     , char jobvs = 'V'
+                     , char sort  = 'N'
+                     , char sense = 'N')
+        : jobvs_(jobvs)
+        , sort_(sort)
+        , sense_(sense)
+        , a_(xpr)
+        , aa_(xpr)
+        , n_(nt2::height(xpr))
+        , lda_(a_.leading_size())
+      {
+        std::cout << "icitte" << std::endl;
+        jobvs_ = (sense_ == 'E' || sense_ == 'B') ? 'V':jobvs_;
+        std::cout << "jobvs " << jobvs_ << std::endl; 
+        sort_ = (sense_ == 'N') ? sort_ : 'S'; 
+        ldvs_ = (jobvs_ == 'V') ? n_ : 1;
+        wr_.resize(nt2::of_size(n_, 1));
+        wi_.resize(nt2::of_size(n_, 1)); 
+        vs_.resize(of_size(ldvs_, ldvs_));
+        ldvs_ = vs_.leading_size(); 
+        
+        nt2::details::geesx(&jobvs_, &sort_, &nt2::details::selectall2 , &sense_, &n_, 
+                            aa_.raw(), &lda_, &sdim_, wr_.raw(), wi_.raw(), 
+                            vs_.raw(), &ldvs_,
+                            &rconde_, &rcondv_, 
+                            &info_, wrk_);
+      }
+      result_type values() const { return aa_; }
+      result_type     t () const { return aa_;     }
+      result_type     z () const 
+      {
+        BOOST_ASSERT_MSG( (jobvs_ == 'V'), "choose jobvs == 'V' to compute z"); 
+        return vs_;
+      }
+      nt2_la_int  status() const   { return info_;   }
+      base_t      rconde()
+      {
+        BOOST_ASSERT_MSG( (sense_ ==  'E' || sense_ ==  'B'), "choose sense in {'E', 'B'} to compute rconde"); 
+        return rconde_; 
+      }
+      base_t      rcondv()
+      {
+        BOOST_ASSERT_MSG( (sense_ ==  'E' || sense_ ==  'B'), "choose sense in {'E', 'B'} to compute rconde"); 
+        return rcondv_; 
+      }
+    private:
+      char                 jobvs_, sort_;
+      char                        sense_;
+      data_t                          a_;
+      tab_t                          aa_; 
+      nt2_la_int                      n_;
+      nt2_la_int                    lda_; 
+      nt2_la_int                   ldvs_;
+      nt2_la_int                   sdim_; 
+      base_t            rcondv_, rconde_;
+      tab_t                          vs_; 
+      tab_t                     wr_, wi_;  
+      nt2_la_int                   info_; 
+      workspace_t                   wrk_;
+      template <class S>
+      static tab_t from_diag(const S& w)
+      {
+        tab_t m = nt2::zeros(numel(w), numel(w), meta::as_<type_t>());
+        for (int i = 1; i <= numel(w); ++i) m(i, i) = w(i);
+        return m;
+      }
+    }; 
+  }
+}
+#endif
---
+++ modules/linalg/include/nt2/toolbox/linalg/functions/schur.hpp
@@ -0,0 +1,133 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_SCHUR_HPP_INCLUDED
+#define NT2_TOOLBOX_LINALG_FUNCTIONS_SCHUR_HPP_INCLUDED
+
+#include <nt2/options.hpp>
+#include <nt2/include/functor.hpp>
+#include <nt2/toolbox/linalg/functions/details/schur.hpp>
+
+namespace nt2
+{
+  namespace tag
+  {
+    namespace factorization
+    {
+      struct schur_ : ext::unspecified_<factorization::schur_>
+      {
+        typedef ext::unspecified_<factorization::schur_> parent;
+      };
+    }
+
+    struct schur_ : ext::unspecified_<schur_>
+    {
+      typedef ext::unspecified_<schur_>  parent;
+    };
+  }
+
+  /**
+   * @brief Perform Schur factorization
+   *
+   * For any given matrix expression, performs a Schur factorization of
+   * said matrix using the specified output layout for the Schurevsky method.
+   *
+   * Contrary to the nt2::factorization::schur function which performs a partial evaluation of
+   * said decomposition, nt2::schur returns a result similar to the Matlab interface,
+   * making it useful for Matlab like usage.
+   *
+   * @param  xpr  Matrix expression to factorize
+   *
+   * @return A tuple-like type containing the factorized matrix and an indicator
+   * of the success of the factorization
+   **/
+  NT2_FUNCTION_IMPLEMENTATION(tag::schur_, schur, 1)
+  NT2_FUNCTION_IMPLEMENTATION(tag::schur_, schur, 2)
+
+  // Those variant are used for the tied(x...) = schur(..) syntax
+  NT2_FUNCTION_IMPLEMENTATION_TPL ( tag::schur_
+                                  , schur
+                                  , (A0 const&)(A1 const&)(A2&)(A3&)
+                                  , 4
+                                  )
+
+  namespace factorization
+  {
+    /**
+     * @brief Initialize a Schur factorization
+     *
+     * For any given matrix expression, initialize a Schur factorization of
+     * said matrix using the specified output layout for the Schur method
+     * and return a precomputed factorization object.
+     *
+     * Contrary to the schur function which performs such a factorization and
+     * return a Matlab like output, factorization::schur returns an object
+     * containing the initial evaluation of said factorization. This object can
+     * then be used to fasten other algorithms implementation.
+     *
+     * @param  xpr  Matrix expression to factorize
+     * @param  econ Notify if schur should return an economy size decomposition
+     *
+     * @return A unspecified type containing the precomputed elements of the
+     * Schur factorization.
+     **/
+    NT2_FUNCTION_IMPLEMENTATION(tag::factorization::schur_, schur, 1)
+    NT2_FUNCTION_IMPLEMENTATION_SELF(tag::factorization::schur_, schur, 2)
+  }
+}
+
+// namespace nt2 { namespace container { namespace ext
+// {
+//   template<class Domain, int N, class Expr>
+//   struct  size_of<tag::schur_,Domain,N,Expr>
+//         ////: reduction_size_of<tag::sum_, 1, Expr>{};
+//   {
+//     // The size is contained in the first child
+//     typedef typename boost::proto::result_of::child_c<Expr&,0>::type seq_term;
+//     typedef typename meta::strip<seq_term>::type::extent_type        result_type;
+
+//     BOOST_FORCEINLINE result_type operator()(Expr& e) const
+//     {
+//       return boost::proto::child_c<0>(e).extent();
+//     }
+//   };
+
+//   template<class Domain, class Expr>
+//   struct  size_of<tag::schur_,Domain,1,Expr>
+//         ////: reduction_size_of<tag::sum_, 1, Expr>{};
+//   {
+//     // The size is contained in the first child
+//     typedef typename boost::proto::result_of::child_c<Expr&,0>::type seq_term;
+//     typedef typename meta::strip<seq_term>::type::extent_type        result_type;
+
+//     BOOST_FORCEINLINE result_type operator()(Expr& e) const
+//     {
+//       return boost::proto::child_c<0>(e).extent();
+//     }
+//   };
+
+//   template<class Domain, int N, class Expr>
+//   struct  generator<tag::schur_,Domain,N,Expr>
+//         ////: reduction_generator<tag::schur_,N,Expr> {};
+//   {
+//     typedef typename boost::proto::result_of::child_c<Expr&,0>::type seq_term;
+//     typedef typename boost::dispatch::meta::semantic_of<seq_term>::type sema_t;
+
+//     // Rebuidl proper expression type with semantic
+//     typedef expression< typename boost::remove_const<Expr>::type
+//                       , sema_t
+//                       >                                     result_type;
+
+//     BOOST_FORCEINLINE result_type operator()(Expr& e) const
+//     {
+//       return result_type(e);
+//     }
+//   };
+//} } }
+
+#endif
