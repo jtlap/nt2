b1271357318fea35748b19b57152457b2368ea5d
jt <jtlapreste@gmail.com>
2012-04-27 23:40:47 +0200

    symeig: symetric or hermitian eigenvalues

---
+++ modules/linalg/include/nt2/toolbox/linalg/functions/details/symeig.hpp
@@ -0,0 +1,139 @@
+/*******************************************************************************
+ *         Copyright 2003-2012 LASMEA UMR 6602 CNRS/U.B.P
+ *         Copyright 2011-2012 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_FACTORIZATIONS_SYMEIG_HPP_INCLUDED
+#define NT2_TOOLBOX_LINALG_FUNCTIONS_FACTORIZATIONS_SYMEIG_HPP_INCLUDED
+
+#include <nt2/include/functions/abs.hpp>
+#include <nt2/toolbox/linalg/details/utility/workspace.hpp>
+#include <nt2/toolbox/linalg/details/lapack/hsev.hpp>
+#include <nt2/include/functions/height.hpp>
+#include <nt2/include/functions/max.hpp>
+#include <nt2/include/functions/zeros.hpp>
+#include <nt2/include/functions/numel.hpp>
+#include <nt2/include/functions/issymetric.hpp>
+#include <nt2/include/constants/eps.hpp>
+#include <nt2/table.hpp>
+
+namespace nt2 { namespace details
+{
+  template<class T> struct symeig_result
+  {
+    typedef typename meta::strip<T>::type                   source_t;
+    typedef typename source_t::value_type                     type_t;
+    typedef typename meta::as_integer<type_t, signed>::type  itype_t;
+    typedef typename source_t::index_type                    index_t;
+    typedef typename meta::as_real<type_t>::type              base_t;
+    typedef T                                                 data_t;
+    typedef nt2::table<type_t,nt2::matlab_index_>              tab_t;
+    typedef nt2::table<base_t,nt2::matlab_index_>             btab_t;
+    //    typedef typename meta::as_dry<type_t>::type                dry_t;
+    //    typedef nt2::table<dry_t, nt2::matlab_index_>             dtab_t; 
+    typedef nt2::table<itype_t,nt2::matlab_index_>            itab_t; 
+    typedef nt2::details::workspace<type_t>              workspace_t;
+    typedef nt2::table<nt2_la_int,nt2::matlab_index_>         ibuf_t;
+    typedef nt2::table<type_t,index_t>                   result_type;
+    
+    template<class Input>
+    symeig_result ( Input& xpr, const char & jobz = 'V', const char & uplo = 'U')
+      :jobz_(jobz)
+      , uplo_(uplo)
+      , a_(xpr)
+      , aa_(xpr)
+      , n_( nt2::height(xpr)  )
+      , w_(of_size(1, n_))       
+      , lda_( aa_.leading_size() )
+      , info_(0)
+    {
+      BOOST_ASSERT_MSG(nt2::issymetric(a_), "input must be a symetric matrix"); 
+      nt2::details::hsev(&jobz_, &uplo_, &n_,
+                         aa_.raw(), &lda_, w_.raw(),
+                         &info_, wrk_);
+    }
+
+    symeig_result& operator=(symeig_result const& src)
+    {
+      a_      = src.a_; 
+      aa_     = src.aa_;
+      n_      = src.n_;
+      lda_    = src.lda_; 
+      info_   = src.info_;
+      wrk_    = src.wrk_; 
+      return *this;
+    }
+    
+    //==========================================================================
+    // Return raw values
+    //==========================================================================
+    result_type values() const { return aa_; }
+    
+    // /////////////////////////////////////////////////////////////////////////////
+    // return eigen vectors
+    // /////////////////////////////////////////////////////////////////////////////
+    tab_t v ()        { return aa_;     }
+    
+    // /////////////////////////////////////////////////////////////////////////////
+    // return eigen values (as vector of reals)
+    // /////////////////////////////////////////////////////////////////////////////
+    btab_t      eigen()          { return w_;      }
+    // /////////////////////////////////////////////////////////////////////////////
+    // return eigen values (as diagonal matrix of reals)
+    // /////////////////////////////////////////////////////////////////////////////
+    btab_t      w ()         { return from_diag(w_);}//TODO
+    
+    //     // /////////////////////////////////////////////////////////////////////////////
+    //     // return eigen values (as vector of dry)
+    //     // /////////////////////////////////////////////////////////////////////////////
+    //     dtab_t      deigen()          { return w_;      }
+    //     // /////////////////////////////////////////////////////////////////////////////
+    //     // return eigen values (as diagonal matrix of dry)
+    //     // /////////////////////////////////////////////////////////////////////////////
+    //     dtab_t      dw ()         { return diag_from(w_);}
+    
+    nt2_la_int status() const { return info_;   }
+    
+    // /////////////////////////////////////////////////////////////////////////////
+    // return condition number
+    // /////////////////////////////////////////////////////////////////////////////
+    base_t     cond() const {return  nt2::abs(w_(1)/w_(numel(w_))); }
+    // /////////////////////////////////////////////////////////////////////////////
+    // return rank up to epsi
+    // /////////////////////////////////////////////////////////////////////////////
+    size_t     rank(base_t epsi = nt2::Eps<base_t>())
+    {
+      int32_t r = 0;
+      btab_t m = nt2::max(nt2::abs(w_)); 
+      base_t thresh = n_*epsi*m(1); 
+      for(int i=1; i <= n_; ++i)
+        {
+          if(nt2::abs(w_(i)) > thresh) ++r; 
+        }
+      return r; 
+    }
+    
+  private:
+    template < class S>
+    static btab_t from_diag(const S& w)
+    {
+      btab_t m = nt2::zeros(numel(w), numel(w), meta::as_<type_t>());
+      for (int i = 1; i <= numel(w); ++i) m(i, i) = w(i);
+      return m;
+    }
+    char     jobz_, uplo_;
+    data_t             a_;
+    tab_t             aa_; 
+    nt2_la_int         n_;
+    btab_t             w_; 
+    nt2_la_int       lda_; 
+    nt2_la_int      info_; 
+    workspace_t      wrk_;
+  }; 
+} }
+
+
+#endif
---
+++ modules/linalg/include/nt2/toolbox/linalg/functions/symeig.hpp
@@ -0,0 +1,87 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_SYMEIG_HPP_INCLUDED
+#define NT2_TOOLBOX_LINALG_FUNCTIONS_SYMEIG_HPP_INCLUDED
+
+#include <nt2/options.hpp>
+#include <nt2/include/functor.hpp>
+#include <nt2/toolbox/linalg/functions/details/symeig.hpp>
+
+namespace nt2
+{
+  namespace tag
+  {
+    namespace factorization
+    {
+      struct symeig_ : ext::unspecified_<factorization::symeig_>
+      {
+        typedef ext::unspecified_<factorization::symeig_> parent;
+      };
+    }
+
+    struct symeig_ : ext::unspecified_<symeig_>
+    {
+      typedef ext::unspecified_<symeig_>  parent;
+    };
+  }
+
+  /**
+   * @brief Perform Symeig factorization
+   *
+   * For any given matrix expression, performs a Symeig factorization of
+   * said matrix using the specified output layout for the Symeig method.
+   *
+   * Contrary to the nt2::factorization::symeig function which performs a partial evaluation of
+   * said decomposition, nt2::symeig returns a result similar to the Matlab interface,
+   * making it useful for Matlab like usage.
+   *
+   * @param  xpr  Matrix expression to factorize
+   *
+   * @return A tuple-like type containing the factorized matrix and an indicator
+   * of the success of the factorization
+   **/
+  NT2_FUNCTION_IMPLEMENTATION(tag::symeig_, symeig, 1)
+  NT2_FUNCTION_IMPLEMENTATION(tag::symeig_, symeig, 2)
+
+  // Those variant are used for the tied(x...) = symeig(..) syntax
+  NT2_FUNCTION_IMPLEMENTATION_TPL ( tag::symeig_
+                                  , symeig
+                                  , (A0 const&)(A1 const&)(A2&)(A3&)(A4&)
+                                  , 5
+                                  )
+
+  namespace factorization
+  {
+    /**
+     * @brief Initialize a Symeig factorization
+     *
+     * For any given matrix expression, initialize a Symeig factorization of
+     * said matrix using the specified output layout for the Symeig method
+     * and return a precomputed factorization object.
+     *
+     * Contrary to the symeig function which performs such a factorization and
+     * return a Matlab like output, factorization::symeig returns an object
+     * containing the initial evaluation of said factorization. This object can
+     * then be used to fasten other algorithms implementation.
+     *
+     * @param  xpr  Matrix expression to factorize
+     * @param  ip   Notify if lu should be performed in-place over xpr
+     *
+     * @return A unspecified type containing the precomputed elements of the
+     * Lu factorization.
+     **/
+    NT2_FUNCTION_IMPLEMENTATION(tag::factorization::symeig_, symeig, 1)
+    NT2_FUNCTION_IMPLEMENTATION_SELF(tag::factorization::symeig_, symeig, 2)
+  }
+}
+namespace nt2 { namespace container { namespace ext
+{
+  //TODO
+} } }
+#endif
