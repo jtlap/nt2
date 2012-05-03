e76d721f92b1b26f9a6ed64802721d222f836155
Joel Falcou <joel.falcou@lri.fr>
2012-05-03 14:19:58 +0200

    Properly supprot cref qualifier in scalar_of

--- modules/boost/dispatch/include/boost/dispatch/meta/scalar_of.hpp
+++ modules/boost/dispatch/include/boost/dispatch/meta/scalar_of.hpp
@@ -9,7 +9,9 @@
 #ifndef BOOST_DISPATCH_META_SCALAR_OF_HPP_INCLUDED
 #define BOOST_DISPATCH_META_SCALAR_OF_HPP_INCLUDED
 
+#include <boost/type_traits/add_const.hpp>
 #include <boost/dispatch/meta/value_of.hpp>
+#include <boost/type_traits/add_reference.hpp>
 
 namespace boost { namespace dispatch { namespace meta
 {
@@ -24,7 +26,7 @@ namespace details
     : meta::scalar_of<U>
   {
   };
-  
+
   template<class T>
   struct scalar_of<T, T>
   {
@@ -35,9 +37,19 @@ namespace details
 namespace meta
 {
   template<class T>
-  struct scalar_of
-    : details::scalar_of<T>
+  struct  scalar_of
+        : details::scalar_of<T> {};
+
+  template<class T> struct scalar_of<T&>
   {
+    typedef typename scalar_of<T>::type               base;
+    typedef typename boost::add_reference<base>::type type;
+  };
+
+  template<class T> struct scalar_of<T const>
+  {
+    typedef typename scalar_of<T>::type           base;
+    typedef typename boost::add_const<base>::type type;
   };
 } } }
 
