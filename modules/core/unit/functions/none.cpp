1c8dc0e0cdce15acb5d6c5069f1f202b2476f637
Joel Falcou <joel.falcou@lri.fr>
2012-05-05 21:38:12 +0200

    Fix none test

--- modules/core/unit/functions/none.cpp
+++ modules/core/unit/functions/none.cpp
@@ -43,33 +43,33 @@ NT2_TEST_CASE_TPL( none_expr, NT2_TYPES )
   for(int j=1;j<=3;j++)
     for(int i=1;i<=5;i++)
       y(i,j) = i + 10*j;
-  disp("y", y); 
+  disp("y", y);
   sy = nt2::none(y);
   for(int j=1;j<=3;j++)
     {
-    bool z = true;  
-    for(int i=1;i<=5;i++) z= nt2::logical_andnot(z, y(i, j)); 
-    NT2_TEST_EQUAL(nt2::is_true(z), sy(j));
+    bool z = true;
+    for(int i=1;i<=5;i++) z= nt2::logical_andnot(z, y(i, j));
+    NT2_TEST_EQUAL(nt2::is_nez(z), sy(j));
     }
-        
+
 //   disp("sy", sy);
 //   sy = nt2::none(y, 1);
 //   for(int j=1;j<=3;j++)
 //     for(int i=1;i<=5;i++)
-//       NT2_TEST_LESSER_EQUAL(y(i, j), sy(j)); 
+//       NT2_TEST_LESSER_EQUAL(y(i, j), sy(j));
 //   disp("sy", sy);
 //   sy = nt2::none(y, 2);
 //   for(int j=1;j<=3;j++)
 //     for(int i=1;i<=5;i++)
-//       NT2_TEST_LESSER_EQUAL(y(i, j), sy(i)); 
+//       NT2_TEST_LESSER_EQUAL(y(i, j), sy(i));
 //   disp("sy", sy);
 //   sy = nt2::none(y, 3);
 //   for(int j=1;j<=3;j++)
 //     for(int i=1;i<=5;i++)
-//       NT2_TEST_LESSER_EQUAL(y(i, j), sy(i, j)); 
+//       NT2_TEST_LESSER_EQUAL(y(i, j), sy(i, j));
 //   disp("sy", sy);
   sy = nt2::none(y(_));
-  disp(sy); 
-  NT2_TEST_EQUAL(sy(1), True<T>()); 
+  disp(sy);
+  NT2_TEST_EQUAL(sy(1), True<T>());
 }
 
