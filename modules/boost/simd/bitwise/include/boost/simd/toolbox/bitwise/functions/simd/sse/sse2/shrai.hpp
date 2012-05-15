c164d265902485d97e8aada0201e22d721985d71
Joel Falcou <joel.falcou@lri.fr>
2012-05-15 20:54:34 +0200

    More templatize fix

--- modules/boost/simd/bitwise/include/boost/simd/toolbox/bitwise/functions/simd/sse/sse2/shrai.hpp
+++ modules/boost/simd/bitwise/include/boost/simd/toolbox/bitwise/functions/simd/sse/sse2/shrai.hpp
@@ -1,10 +1,10 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_BITWISE_FUNCTIONS_SIMD_SSE_SSE2_SHRAI_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_BITWISE_FUNCTIONS_SIMD_SSE_SSE2_SHRAI_HPP_INCLUDED
@@ -56,11 +56,12 @@ namespace boost { namespace simd { namespace ext
                               (scalar_< integer_<A1> >)
                             )
   {
-    typedef A0 result_type;
+    typedef A0                                              result_type;
+    typedef typename meta::make_dependent<int16_t,A0>::type sub_t;
+    typedef native<sub_t, boost::simd::tag::sse_>           gen_t;
     BOOST_SIMD_FUNCTOR_CALL(2)
     {
-      typedef native<typename boost::simd::meta::int16_t_<A0>::type, boost::simd::tag::sse_> gen_type;
-      gen_type a0h, a0l;
+      gen_t a0h, a0l;
       boost::fusion::tie(a0l, a0h) = split(a0);
       return bitwise_cast<A0>(group(shrai(a0l, a1),shrai(a0h, a1)));
     }
