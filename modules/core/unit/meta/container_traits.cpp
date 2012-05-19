44283a6fb520f1c94c7fa111d0da926e8c5b443c
Joel Falcou <joel.falcou@lri.fr>
2012-05-19 21:57:45 +0200

    Use NT2_TYPE_IS in old tests in place of NT2_TEST( is_same ...)

--- modules/core/unit/meta/container_traits.cpp
+++ modules/core/unit/meta/container_traits.cpp
@@ -15,6 +15,49 @@
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+// Check pointer_
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE( pointer )
+{
+  using nt2::meta::pointer_;
+  using boost::is_same;
+  using std::vector;
+
+  NT2_TEST_TYPE_IS( pointer_<float>::type
+                  , float*
+                  );
+
+  NT2_TEST_TYPE_IS( pointer_<float&>::type
+                  , float*
+                  );
+
+  NT2_TEST_TYPE_IS( pointer_<float const>::type
+                  , float const*
+                  );
+
+  NT2_TEST_TYPE_IS( pointer_<float const&>::type
+                  , float const*
+                  );
+
+  NT2_TEST_TYPE_IS( pointer_< vector<float> >::type
+                  , float*
+                  );
+
+  NT2_TEST_TYPE_IS( pointer_< vector<float>& >::type
+                  , float*
+                  );
+
+  NT2_TEST_TYPE_IS( pointer_< vector<float> const >::type
+                  , float const*
+                  );
+
+  NT2_TEST_TYPE_IS( pointer_< vector<float> const& >::type
+                  , float const*
+                  );
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Check reference_
@@ -25,8 +68,80 @@ NT2_TEST_CASE( reference )
   using boost::is_same;
   using std::vector;
 
-  NT2_TEST(( is_same < float&, reference_<float>::type >::value ));
-  NT2_TEST(( is_same < float&, reference_< vector<float> >::type >::value ));
+  NT2_TEST_TYPE_IS( reference_<float>::type
+                  , float&
+                  );
+
+  NT2_TEST_TYPE_IS( reference_<float&>::type
+                  , float&
+                  );
+
+  NT2_TEST_TYPE_IS( reference_<float const>::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( reference_<float const&>::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( reference_< vector<float> >::type
+                  , float&
+                  );
+
+  NT2_TEST_TYPE_IS( reference_< vector<float>& >::type
+                  , float&
+                  );
+
+  NT2_TEST_TYPE_IS( reference_< vector<float> const >::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( reference_< vector<float> const& >::type
+                  , float const&
+                  );
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Check pointer_
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE( const_pointer )
+{
+  using nt2::meta::const_pointer_;
+  using boost::is_same;
+  using std::vector;
+
+  NT2_TEST_TYPE_IS( const_pointer_<float>::type
+                  , float const*
+                  );
+
+  NT2_TEST_TYPE_IS( const_pointer_<float&>::type
+                  , float*
+                  );
+
+  NT2_TEST_TYPE_IS( const_pointer_<float const>::type
+                  , float const*
+                  );
+
+  NT2_TEST_TYPE_IS( const_pointer_<float const&>::type
+                  , float const*
+                  );
+
+  NT2_TEST_TYPE_IS( const_pointer_< vector<float> >::type
+                  , float const*
+                  );
+
+  NT2_TEST_TYPE_IS( const_pointer_< vector<float>& >::type
+                  , float const*
+                  );
+
+  NT2_TEST_TYPE_IS( const_pointer_< vector<float> const >::type
+                  , float const*
+                  );
+
+  NT2_TEST_TYPE_IS( const_pointer_< vector<float> const& >::type
+                  , float const*
+                  );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -38,8 +153,37 @@ NT2_TEST_CASE( const_reference )
   using boost::is_same;
   using std::vector;
 
-  NT2_TEST(( is_same < float const&, const_reference_<float>::type >::value ));
-  NT2_TEST(( is_same < float const&, const_reference_< vector<float> >::type >::value ));
+  NT2_TEST_TYPE_IS( const_reference_<float>::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( const_reference_<float&>::type
+                  , float&
+                  );
+
+  NT2_TEST_TYPE_IS( const_reference_<float const>::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( const_reference_<float const&>::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( const_reference_< vector<float> >::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( const_reference_< vector<float>& >::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( const_reference_< vector<float> const >::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( const_reference_< vector<float> const& >::type
+                  , float const&
+                  );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -51,6 +195,73 @@ NT2_TEST_CASE( value_type )
   using boost::is_same;
   using std::vector;
 
-  NT2_TEST(( is_same < float, value_type_<float>::type >::value ));
-  NT2_TEST(( is_same < float, value_type_< vector<float> >::type >::value ));
+  NT2_TEST_TYPE_IS( value_type_< float >::type
+                  , float
+                  );
+
+  NT2_TEST_TYPE_IS( value_type_< float& >::type
+                  , float&
+                  );
+
+  NT2_TEST_TYPE_IS( value_type_< float const >::type
+                  , float const
+                  );
+
+  NT2_TEST_TYPE_IS( value_type_< float const& >::type
+                  , float const&
+                  );
+
+  NT2_TEST_TYPE_IS( value_type_< vector<float> >::type
+                  , float
+                  );
+
+  NT2_TEST_TYPE_IS( value_type_< vector<float>& >::type
+                  , float
+                  );
+
+  NT2_TEST_TYPE_IS( value_type_< vector<float> const >::type
+                  , float
+                  );
+  NT2_TEST_TYPE_IS( value_type_< vector<float> const& >::type
+                  , float
+                  );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Check size_type_
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE( size_type )
+{
+  using nt2::meta::size_type_;
+  using boost::is_same;
+  using std::vector;
+
+  NT2_TEST_TYPE_IS( size_type_< float >::type
+                  , std::size_t
+                  );
+
+  NT2_TEST_TYPE_IS( size_type_< float& >::type
+                  , std::size_t
+                  );
+
+  NT2_TEST_TYPE_IS( size_type_< float const >::type
+                  , std::size_t
+                  );
+
+  NT2_TEST_TYPE_IS( size_type_< float const& >::type
+                  , std::size_t
+                  );
+
+  NT2_TEST_TYPE_IS( size_type_< vector<float> >::type
+                  , std::size_t
+                  );
+  NT2_TEST_TYPE_IS( size_type_< vector<float>& >::type
+                  , std::size_t
+                  );
+  NT2_TEST_TYPE_IS( size_type_< vector<float> const >::type
+                  , std::size_t
+                  );
+  NT2_TEST_TYPE_IS( size_type_< vector<float> const& >::type
+                  , std::size_t
+                  );
 }
