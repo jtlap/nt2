a10e06d18fd815a6f0ea0d14fc642cd0b611aec3
pesterie <pesterie@gmail.com>
2012-06-19 16:20:13 -0500

    Removing uneeded include that fix several errors

--- modules/boost/simd/sdk/include/boost/simd/sdk/simd/pack/store.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/simd/pack/store.hpp
@@ -11,7 +11,6 @@
 
 #include <boost/simd/include/functions/store.hpp>
 #include <boost/simd/include/functions/evaluate.hpp>
-#include <boost/simd/sdk/simd/pack/domain.hpp>
 
 // store forces evaluation due to side-effect
 namespace boost { namespace simd { namespace ext
