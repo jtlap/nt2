jtlap/nt2

commit 91098a785c4f327fc29389fd352a410b4db7aa58

Fix some ICE in rem_pio2 on gcc.

modules/trigonometric/include/nt2/toolbox/trigonometric/functions/simd/common/rem_pio2.hpp
@@ -1,10 +1,10 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef NT2_TOOLBOX_TRIGONOMETRIC_FUNCTIONS_SIMD_COMMON_REM_PIO2_HPP_INCLUDED
 #define NT2_TOOLBOX_TRIGONOMETRIC_FUNCTIONS_SIMD_COMMON_REM_PIO2_HPP_INCLUDED
@@ -28,9 +28,9 @@ namespace nt2 { namespace ext
                       ((simd_ < floating_<A0>,X > ))
                     )
   {
-    typedef typename meta::as_integer<A0>::type            itype;    
+    typedef typename meta::as_integer<A0>::type            itype;
     typedef boost::fusion::tuple<A0,A0,itype>        result_type;
-    
+
     inline result_type operator()(A0 const& a0) const
     {
       result_type res;
@@ -38,10 +38,10 @@ namespace nt2 { namespace ext
         nt2::rem_pio2(a0,
                       boost::fusion::at_c<0>(res),
                       boost::fusion::at_c<1>(res)
-                     ); 
-      return res; 
+                     );
+      return res;
     }
-  }; 
+  };
 
   NT2_FUNCTOR_IMPLEMENTATION(  nt2::tag::rem_pio2_, boost::simd::tag::simd_,(A0)(X)
                             , ((simd_<floating_<A0>,X>))
@@ -49,17 +49,21 @@ namespace nt2 { namespace ext
                               ((simd_<floating_<A0>,X>))
                             )
   {
-    typedef typename meta::as_integer<A0>::type result_type;    
+    typedef typename meta::as_integer<A0>::type result_type;
     inline result_type operator()(A0 const& a0,A0 & xr,A0 & xc) const
     {
       typedef typename meta::scalar_of<A0>::type          stype;
-      typedef typename meta::scalar_of<result_type>::type itype; 
+      typedef typename meta::scalar_of<result_type>::type itype;
       static const nt2::uint32_t size = meta::cardinal_of<result_type>::value;
       BOOST_SIMD_ALIGNED_TYPE(itype) tmp[size];
       BOOST_SIMD_ALIGNED_TYPE(stype) txr[size];
       BOOST_SIMD_ALIGNED_TYPE(stype) txc[size];
       for(nt2::uint32_t i=0; i!=size; ++i)
-        tmp[i] =  nt2::rem_pio2(a0[i], txr[i], txc[i]);
+      {
+        // This prevent the may_alias ICE
+        stype a = a0[i], r = txr[i], c = txc[i];
+        tmp[i] =  nt2::rem_pio2(a, r, c);
+      }
 
       xr = load<A0>(&txr[0], 0);
       xc = load<A0>(&txc[0], 0);

commit e01b682f3a06e9824ce2366ba4d12f06a0dc0f1c

fix rem_pio2 regression introduced in commit 91098a785c4f327fc29389fd352a410b4db7aa58

modules/trigonometric/include/nt2/toolbox/trigonometric/functions/simd/common/rem_pio2.hpp
@@ -63,6 +63,8 @@ namespace nt2 { namespace ext
         // This prevent the may_alias ICE
         stype a = a0[i], r = txr[i], c = txc[i];
         tmp[i] =  nt2::rem_pio2(a, r, c);
+        txr[i] = r;
+        txc[i] = c;
       }
 
       xr = load<A0>(&txr[0], 0);