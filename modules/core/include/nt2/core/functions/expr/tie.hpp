e640a464c3571b322043543db38851310e4eba6f
Mathias Gaunard <mathias@gaunard.com>
2012-07-02 03:20:52 +0200

    change tie assign/run_assign to something a bit better

--- modules/core/include/nt2/core/functions/expr/tie.hpp
+++ modules/core/include/nt2/core/functions/expr/tie.hpp
@@ -18,17 +18,16 @@
 namespace nt2 { namespace ext
 {
   //============================================================================
-  // Handles tie(...) = tieable_func(...)
+  // Call function for tie(...) = tieable_func(...)
   //============================================================================
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::assign_, tag::cpu_
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_assign_, tag::cpu_
                             , (A0)(N0)(A1)(T1)(N1)
                             , ((node_<A0, nt2::tag::tie_, N0>))
                               ((node_<A1, tieable_<T1>, N1>))
                             )
   {
     typedef A0&                                         result_type;
-    typedef typename meta::strip<A1>::type              dest_t;
-    typedef typename boost::proto::tag_of<dest_t>::type tag_t;
+    typedef typename boost::proto::tag_of<A1>::type     tag_t;
     typedef typename make_functor<tag_t,A0>::type       func_t;
 
     BOOST_FORCEINLINE result_type operator()(A0& a0, A1& a1) const
@@ -41,55 +40,45 @@ namespace nt2 { namespace ext
   };
 
   //============================================================================
-  // Handles terminal = tieable_func(...) by looking at it as a tie(terminal)
+  // Standalone tieable_func(...) -- required because schedule doesn't call assign
   //============================================================================
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::assign_, tag::cpu_
-                            , (A0)(T0)(A1)(T1)(N1)
-                            , ((node_<A0, T0 , boost::mpl::long_<0> >))
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_assign_, tag::cpu_
+                            , (A0)(T0)(N0)(A1)(T1)(N1)
+                            , ((node_<A0, elementwise_<T0>, N0>))
                               ((node_<A1, tieable_<T1>, N1>))
                             )
   {
     typedef A0&                                         result_type;
-    typedef typename meta::strip<A1>::type              dest_t;
-    typedef typename boost::proto::tag_of<dest_t>::type tag_t;
+    typedef typename boost::proto::tag_of<A1>::type     tag_t;
     typedef typename make_functor<tag_t,A0>::type       func_t;
 
     BOOST_FORCEINLINE result_type operator()(A0& a0, A1& a1) const
     {
+      // FIXME: schedule doesn't produce terminals usable by reference
       func_t callee;
-      callee( a1, nt2::tie(a0));
+      callee(a1, nt2::tie(a0));
 
       return a0;
     }
   };
 
   //============================================================================
-  // Handles elementwise = tieable_func(...) by looking at it as a tie(terminal)
-  // over a local memory block.
+  // Handles elementwise  = tieable_func(...) by transforming it to
+  //     tie(elementwise) = tieable_func(...)
   //============================================================================
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_assign_, tag::cpu_
-                            , (A0)(T0)(N0)(A1)(T1)(N1)
-                            , ((node_ <A0, elementwise_<T0>, N0 > ))
-                              ((node_ <A1, tieable_<T1>, N1>))
-                            )
+  NT2_FUNCTOR_IMPLEMENTATION_IF( nt2::tag::assign_, tag::cpu_
+                              , (A0)(T0)(N0)(A1)(T1)(N1)
+                              , (mpl::not_< is_same<T0, nt2::tag::tie_> >)
+                              , ((node_ <A0, elementwise_<T0>, N0 > ))
+                                ((node_ <A1, tieable_<T1>, N1>))
+                              )
   {
-    typedef typename boost::proto::result_of::child_c<A1&,0>::type      bound_t;
-    typedef typename boost::remove_reference<bound_t>::type::value_type value_type;
-    typedef A0& result_type;
-
-    typedef typename meta::strip<A1>::type              dest_t;
-    typedef typename boost::proto::tag_of<dest_t>::type tag_t;
-    typedef typename make_functor<tag_t,A0>::type       func_t;
+    typedef typename meta::call<tag::tie_(A0&)>::type tied;
+    typedef typename meta::call<tag::assign_(tied, A1&)>::type result_type;
 
     BOOST_FORCEINLINE result_type operator()(A0& a0, A1& a1) const
     {
-      nt2::container::table<value_type> bound = boost::proto::child_c<0>(a1);
-
-      func_t callee;
-      callee( a1, nt2::tie(bound) );
-
-      a0 = bound;
-      return a0;
+      return nt2::assign(nt2::tie(a0), a1);
     }
   };
 } }
