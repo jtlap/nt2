ea384bd8502d0cb9b01351371aaba9a538e9d0a1
psiha <psiha@psiha.org>
2012-07-03 02:03:15 -0700

    Added complex FFT testing. Changed test value range to [-1, +1]. Changed test value containers to boost::arrays to allow easier visual debugging in "less than capable debuggers" (e.g. Xcode). Minor other refactoring and stylistic changes.

--- modules/signal/unit/test.cpp
+++ modules/signal/unit/test.cpp
@@ -16,26 +16,57 @@
 
 #include <boost/simd/sdk/memory/allocator.hpp>
 
+#include <boost/array.hpp>
+#include <boost/foreach.hpp>
+
 NT2_TEST_CASE( test )
 {
-  typedef float T;
-  typedef nt2::static_fft<128, 8192, T> FFT;
-  static const std::size_t N = 4096;
+    static const std::size_t N = 4096;
+    typedef float T;
+    typedef BOOST_SIMD_ALIGN_ON( BOOST_SIMD_ARCH_ALIGNMENT ) boost::array<T, N> aligned_array;
+    typedef nt2::static_fft<128, 8192, T> FFT;
 
-  typedef std::vector<T, boost::simd::memory::allocator<T> > aligned_vector;
+    // Test complex transform(s):
+    {
+        aligned_array real_data;
+        aligned_array imag_data;
 
-  aligned_vector real_time_data(N);
-  for(std::size_t i=0; i!=N; ++i)
-    real_time_data[i] = roll<T>(0, 1);
+        BOOST_FOREACH( T & scalar, real_data ) { scalar = roll<T>( -1, 1 ); }
+        BOOST_FOREACH( T & scalar, imag_data ) { scalar = roll<T>( -1, 1 ); }
 
-  aligned_vector real_frequency_data(N);
-  aligned_vector imag_frequency_data(N);
-  FFT::real_forward_transform(&real_time_data[0], &real_frequency_data[0], &imag_frequency_data[0], N);
+        aligned_array const original_real_data( real_data );
+        aligned_array const original_imag_data( imag_data );
 
-  aligned_vector real_time_data2(N);
-  FFT::real_inverse_transform(&real_frequency_data[0], &imag_frequency_data[0], &real_time_data2[0], N);
+        FFT::forward_transform( &real_data[ 0 ], &imag_data[ 0 ], N );
+        FFT::inverse_transform( &real_data[ 0 ], &imag_data[ 0 ], N );
 
-  T norm = nt2::real_fft_normalization_factor<T>(N);
-  for(std::size_t i=0; i!=N; ++i)
-    NT2_TEST_ULP_EQUAL( real_time_data[i], real_time_data2[i] * norm, 1000 );
+        T const norm( nt2::complex_fft_normalization_factor<T>( N ) );
+        BOOST_FOREACH( T & scalar, real_data ) { scalar *= norm; }
+        BOOST_FOREACH( T & scalar, imag_data ) { scalar *= norm; }
+
+        for ( std::size_t i( 0 ); i != N; ++i )
+        {
+            NT2_TEST_ULP_EQUAL( real_data[ i ], original_real_data[ i ], 1000 );
+            NT2_TEST_ULP_EQUAL( imag_data[ i ], original_imag_data[ i ], 1000 );
+        }
+    }
+
+    // Test real transform(s):
+    {
+        aligned_array real_time_data;
+        BOOST_FOREACH( T & scalar, real_time_data ) { scalar = roll<T>( -1, 1 ); }
+
+        aligned_array real_frequency_data;
+        aligned_array imag_frequency_data;
+        FFT::real_forward_transform( &real_time_data[ 0 ], &real_frequency_data[ 0 ], &imag_frequency_data[ 0 ], N );
+
+        aligned_array real_time_data2;
+        FFT::real_inverse_transform( &real_frequency_data[ 0 ], &imag_frequency_data[ 0 ], &real_time_data2[ 0 ], N );
+
+        T const norm( nt2::real_fft_normalization_factor<T>(N) );
+        BOOST_FOREACH( T & scalar, real_time_data2 ) { scalar *= norm; }
+
+        for ( std::size_t i( 0 ); i != N; ++i )
+            NT2_TEST_ULP_EQUAL( real_time_data[ i ], real_time_data2[ i ], 1000 );
+    }
 }
