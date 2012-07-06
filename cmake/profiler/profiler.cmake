8978727cd4a0e9ea39c8ea6666ffedbf16ecd325
Joel Falcou <joel.falcou@lri.fr>
2012-07-06 18:56:41 +0200

    Simple empty file using nt2::plus for template instanciation profiling

--- cmake/profiler/profiler.cmake
+++ cmake/profiler/profiler.cmake
@@ -12,13 +12,15 @@
 ################################################################################
 FIND_FILE(PROFILER_PATH profiler ${CMAKE_MODULE_PATH})
 
+MESSAGE(STATUS "PROFILER: ${CMAKE_BINARY_DIR}")
+
 ADD_EXECUTABLE(template.profiler.filter ${PROFILER_PATH}/filter.cpp)
-set_property(TARGET template.profiler.filter PROPERTY RUNTIME_OUTPUT_DIRECTORY ${NT2_BINARY_DIR}/tools)
+set_property(TARGET template.profiler.filter PROPERTY RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/tools)
 
 INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIRS})
 ADD_EXECUTABLE(template.profiler.postprocess ${PROFILER_PATH}/postprocess.cpp)
 target_link_libraries(template.profiler.postprocess ${Boost_LIBRARIES})
-set_property(TARGET template.profiler.postprocess PROPERTY RUNTIME_OUTPUT_DIRECTORY ${NT2_BINARY_DIR}/tools)
+set_property(TARGET template.profiler.postprocess PROPERTY RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/tools)
 
 ################################################################################
 # Make a macro to compile foo and profile it
@@ -28,7 +30,7 @@ macro(template_profile target src)
   foreach(INCLUDE ${INCLUDES})
     list(APPEND INCLUDE_DIRECTORIES -I${INCLUDE})
   endforeach()
-  
+
   add_custom_command(OUTPUT ${target}.preprocessed.cpp
                      COMMAND ${CMAKE_CXX_COMPILER} ${CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}} ${INCLUDE_DIRECTORIES} -E ${CMAKE_CURRENT_SOURCE_DIR}/${src}
                            | ${PERL_EXECUTABLE} ${PROFILER_PATH}/preprocess.pl > ${target}.preprocessed.cpp
@@ -36,10 +38,10 @@ macro(template_profile target src)
                    )
   add_custom_command(OUTPUT ${target}.template_profile
                      COMMAND ${CMAKE_CXX_COMPILER} ${CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}} -I${PROFILER_PATH} -c -DPROFILE_TEMPLATES ${target}.preprocessed.cpp 2>&1
-                           | ${NT2_BINARY_DIR}/tools/template.profiler.filter ${ARGN} > ${target}.filtered && ${NT2_BINARY_DIR}/tools/template.profiler.postprocess ${ARGN} ${target}.filtered > ${target}.template_profile
+                           | ${CMAKE_BINARY_DIR}/tools/template.profiler.filter ${ARGN} > ${target}.filtered && ${CMAKE_BINARY_DIR}/tools/template.profiler.postprocess  --call-graph ${ARGN} ${target}.filtered > ${target}.template_profile
                      DEPENDS ${target}.preprocessed.cpp
                              template.profiler.filter
-                             template.profiler.postprocess 
+                             template.profiler.postprocess
                     )
   add_custom_target (${target}
                      DEPENDS ${target}.template_profile
