9437eda59d17dbe2f17288ef65a3f7dd0b008d3b
jtlap <jtlapreste@gmail.com>
2012-07-07 19:29:58 +0200

    linesstride is a functor computing the gap between two elements of an expression along a given direction

---
+++ modules/core/include/nt2/core/functions/expr/linesstride.hpp
@@ -0,0 +1,45 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_FUNCTIONS_EXPR_LINESSTRIDE_HPP_INCLUDED
+#define NT2_CORE_FUNCTIONS_EXPR_LINESSTRIDE_HPP_INCLUDED
+
+#include <nt2/core/functions/linesstride.hpp>
+#include <nt2/include/functions/size.hpp>
+#include <nt2/include/functions/min.hpp>
+#include <nt2/include/functions/isempty.hpp>
+
+namespace nt2 { namespace ext
+{
+
+
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::linesstride_, tag::cpu_
+                            , (A0)(A1)
+                            , (ast_<A0>)
+                              (scalar_< integer_<A1> >)
+                            )
+  {
+    typedef std::size_t result_type;
+
+    BOOST_DISPATCH_FORCE_INLINE
+    result_type operator()(const A0& a0,const A1& dim) const
+    {
+      if (isempty(a0)) return 1; 
+      size_t d =  nt2::min(size_t(dim), a0.extent().size()); 
+      size_t stride = 1;
+      for(size_t i=1; i < d; ++i)
+        {
+          stride *= size(a0, i); 
+        }
+      return stride; 
+    }
+  };
+} }
+
+#endif
+
---
+++ modules/core/unit/functions/linesstride.cpp
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#define NT2_UNIT_MODULE "nt2::linesstride function"
+
+#include <nt2/table.hpp>
+#include <nt2/include/functions/linesstride.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/basic.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+// linesstride of arithmetic types
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE( fundamental_linesstride )
+{
+  using nt2::linesstride;
+
+  NT2_TEST_EQUAL( linesstride( 3 , 1 ), 1u);
+  NT2_TEST_EQUAL( linesstride( 3 , 2 ), 1u);
+  NT2_TEST_EQUAL( linesstride( 3 , 3 ), 1u);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// linesstride of tables
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE( linesstride_table )
+{
+  using nt2::table;
+
+  table<float, nt2::settings(nt2::_3D)> x( nt2::of_size(4,5,6) );
+
+  NT2_TEST_EQUAL( nt2::linesstride( x , 1), 1u); 
+  NT2_TEST_EQUAL( nt2::linesstride( x , 2), 4u); 
+  NT2_TEST_EQUAL( nt2::linesstride( x , 3), 20u); 
+  NT2_TEST_EQUAL( nt2::linesstride( x , 4), 120u); 
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// linesstride of expression
+////////////////////////////////////////////////////////////////////////////////
+NT2_TEST_CASE( linesstride_expression )
+{
+  using nt2::table;
+
+  table<float, nt2::settings(nt2::_3D)> x( nt2::of_size(4,5,6) );
+
+  NT2_TEST_EQUAL( nt2::linesstride( x+x/3.f, 1),  1u);
+  NT2_TEST_EQUAL( nt2::linesstride( x+x/3.f, 2),  4u);
+  NT2_TEST_EQUAL( nt2::linesstride( x+x/3.f, 3),  20u);
+  NT2_TEST_EQUAL( nt2::linesstride( x+x/3.f, 4),  120u); 
+}
+
+
