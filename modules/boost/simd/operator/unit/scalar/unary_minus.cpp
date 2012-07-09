91da0a0eab43f9ff089235930de84fca8841fc59
jtlap <jtlapreste@gmail.com>
2012-07-09 13:00:34 +0200

    spurious ulpd in modules/boost/simd/operator/unit/

--- modules/boost/simd/operator/unit/scalar/unary_minus.cpp
+++ modules/boost/simd/operator/unit/scalar/unary_minus.cpp
@@ -39,10 +39,6 @@ NT2_TEST_CASE_TPL ( unary_minus_real__1_0,  BOOST_SIMD_REAL_TYPES)
   // return type conformity test 
   NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
   std::cout << std::endl; 
-  double ulpd;
-  ulpd=0.0;
-
-
   // specific values tests
   NT2_TEST_EQUAL(unary_minus(boost::simd::Inf<T>()), boost::simd::Minf<r_t>());
   NT2_TEST_EQUAL(unary_minus(boost::simd::Minf<T>()), boost::simd::Inf<r_t>());
