8de5e32b75f36bed7295f809dc6e38bb525366ef
psiha <psiha@psiha.org>
2012-07-17 04:07:41 -0700

    Refactored the twiddle factor calculation code so that the internally used angle units/sincos implementation can be configured. Added the following implementations: radians, degrees, pies and "x87 extended precision fsincos" (MSVC only).
    Removed an extra trailing line.

--- modules/signal/include/nt2/signal/twiddle_factors.hpp
+++ modules/signal/include/nt2/signal/twiddle_factors.hpp
@@ -17,11 +17,12 @@
 #include <nt2/signal/static_sincos.hpp>
 
 #include <nt2/include/functions/simd/sinecosine.hpp>
+#include <nt2/include/functions/simd/sincosd.hpp>
+#include <nt2/include/functions/simd/sincospi.hpp>
 
 #include <boost/simd/sdk/simd/extensions.hpp>
 #include <boost/simd/include/functions/simd/arith.hpp>
 
-#include <boost/concept_check.hpp>
 #include <boost/preprocessor/arithmetic/add.hpp>
 #include <boost/preprocessor/arithmetic/mul.hpp>
 #include <boost/preprocessor/repetition/repeat.hpp>
@@ -92,8 +93,46 @@ struct twiddles_interleaved;
 
 namespace detail
 {
-    template <typename Vector>
-    void calculate_single_twiddle_pair( twiddle_pair<Vector> & twiddles, int const index, long double const omega, int const omega_scale )
+    ////////////////////////////////////////////////////////////////////////////
+    // Twiddle calculation implementations.
+    ////////////////////////////////////////////////////////////////////////////
+    /// \note Simple radians based sinecosine calculation of twiddles does not
+    /// give maximally accurate values because rational values of Pi (a
+    /// transcendental number) cannot be represented accurately enough [this
+    /// becomes most adverse for inputs which should produce an exact zero, like
+    /// cos( Pi/2 )].
+    ///                                       (17.07.2012.) (Domagoj Saric)
+    //////////////////////////////////////////////////////////////////////////// 
+
+    //...zzz...for testing...
+    //#define NT2_FFT_USE_x87_TWIDDLES
+
+    struct radians
+    {
+        static long double full_circle() { return 2 * 3.1415926535897932384626433832795028841971693993751058209749445923078164062L; }
+        template <typename Vector>
+        static BOOST_FORCEINLINE Vector sincos( Vector const & input, Vector & cosine ) { return sinecosine<small>( input, cosine ); }
+    };
+
+    struct degrees
+    {
+        static long double full_circle() { return 2 * 180; }
+        template <typename Vector>
+        static BOOST_FORCEINLINE Vector sincos( Vector const & input, Vector & cosine ) { Vector sine; sincosd( input, sine, cosine ); return sine; }
+        //...zzz...fails to compile...static BOOST_FORCEINLINE Vector sincos( Vector const & input, Vector & cosine ) { return sincosd( input, cosine ); }
+    };
+
+    struct pies
+    {
+        static long double full_circle() { return 2 * 1; }
+        template <typename Vector>
+        static BOOST_FORCEINLINE Vector sincos( Vector const & input, Vector & cosine ) { Vector sine; sincospi( input, sine, cosine ); return sine; }
+        //...zzz...fails to compile...static BOOST_FORCEINLINE Vector sincos( Vector const & input, Vector & cosine ) { return sincospi( input, cosine ); }
+    };
+
+
+    template <typename Impl, typename Vector>
+    void calculate_single_twiddle_pair( twiddle_pair<Vector> & twiddles, int const index, long double const N, long double const omega_scale )
     {
         typedef          Vector               vector_t;
         typedef typename vector_t::value_type scalar_t;
@@ -101,15 +140,52 @@ namespace detail
         vector_t * BOOST_DISPATCH_RESTRICT const p_cosine( &twiddles.wr );
         vector_t * BOOST_DISPATCH_RESTRICT const p_sine  ( &twiddles.wi );
 
+    #if defined( NT2_FFT_USE_x87_TWIDDLES ) && defined( _MSC_VER ) && defined( _M_IX86 )
+
+        // http://software.intel.com/en-us/forums/showthread.php?t=74354
+        // http://www.devmaster.net/forums/showthread.php?t=5784
+
+        unsigned int const vector_size( vector_t::static_size );
+        unsigned int       local_index( index                 );
+	    __asm
+	    {
+		    mov ecx, vector_size
+		    mov edx, p_sine
+		    mov edi, p_cosine
+        sincos_loop:
+		    fldpi
+            fldpi
+            fadd
+            fmul [omega_scale]
+            fimul [local_index]
+            fdiv [N]
+		    fsincos
+		    fstp [edi]
+		    add edi, 4
+            fchs
+		    fstp [edx]
+		    add edx, 4
+
+            inc [local_index]
+		    dec ecx
+		    jnz sincos_loop
+	    }
+
+    #else // 32 bit x86 MSVC
+
+        long double const omega( omega_scale * Impl::full_circle() / N );
+
         long double const start_value( index * omega );
         long double const increment  (         omega );
 
         vector_t const input( boost::simd::arith<vector_t>( static_cast<scalar_t>( start_value ), static_cast<scalar_t>( increment ) ) );
 
-        *p_sine = sinecosine<small>( input, *p_cosine ) ^ Mzero<vector_t>();
+        *p_sine = Impl::sincos( input, *p_cosine ) ^ Mzero<vector_t>();
 
         BOOST_ASSERT( input[ vector_t::static_size - 1 ] <= omega_scale * 2 * M_PI / 4 );
         boost::ignore_unused_variable_warning( omega_scale );
+
+    #endif // 32 bit x86 MSVC
     }
 
     /// \note Regardless of the input data layout we always use twiddles
@@ -126,6 +202,10 @@ namespace detail
         unsigned int                                           const N_int
     )
     {
+    #if defined( _MSC_VER ) && ( defined( _M_IX86 ) || defined( _M_AMD64 ) || defined( _M_IA64 ) )
+        unsigned int const current_precision( ::_controlfp( _PC_64, _MCW_PC ) );
+    #endif // _MSC_VER
+
         split_radix_twiddles<Vector> * BOOST_DISPATCH_RESTRICT p_w( p_twiddles );
 
         /// \todo Since cos( a ) = sin( a + Pi/2 ) = -sin( a + 3*Pi/2 ) separate
@@ -139,20 +219,25 @@ namespace detail
         /// \note N/4 values are required for split-radix.
         ///                                   (21.05.2012.) (Domagoj Saric)
 
-        long double const N    ( static_cast<int>( N_int ) );
-        long double const omega( 2 * M_PI / N              );
+        long double const N( static_cast<int>( N_int ) );
 
         int i( 0 );
         while ( unsigned( i ) < ( N_int / 4 ) )
         {
-            calculate_single_twiddle_pair( p_w->w0, i, 1 * omega, 1 );
-            calculate_single_twiddle_pair( p_w->w3, i, 3 * omega, 3 );
+            typedef pies impl;
+
+            calculate_single_twiddle_pair<impl>( p_w->w0, i, N, 1 );
+            calculate_single_twiddle_pair<impl>( p_w->w3, i, N, 3 );
 
             i += Vector::static_size;
             ++p_w;
         }
 
         BOOST_ASSERT( p_w == &p_twiddles[ N_int / 4 / Vector::static_size ] );
+
+    #if defined( _MSC_VER ) && ( defined( _M_IX86 ) || defined( _M_AMD64 ) || defined( _M_IA64 ) )
+        ::_controlfp( current_precision, _MCW_PC );
+    #endif // _MSC_VER
     }
 
     template <typename Vector, unsigned N>
@@ -351,4 +436,3 @@ typename twiddles<fft_size, stride, T>::factors_t const                         
 } // namespace nt2
 //------------------------------------------------------------------------------
 #endif // twiddle_factors_hpp
-
