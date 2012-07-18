b265600f035d24de4dae21a2be0b6aa2596baa5a
Joel Falcou <joel.falcou@lri.fr>
2012-07-18 16:11:39 +0200

    Fix for Issue #220 - OpenMP performance bug
    This patch fixes issue 220 by providing a new code generation process for
    OpenMP transform. This is done by using a SPMD model of computation instead
    of the naive SIMD way using omp parallel for.
    
    The patch contains:
     - A new specialisation for nt2::transform that can now be called with
       a specified starting index and a specified number of element to process.
       This overloads is required to lessen the code duplication in openMP and
       to make the whole elementwise loop generation fully hierarchical.
    
     - An asymmetric table performance benchmark. This test compares the
       performance of the openMP+SIMD implementation of some elementwise
       operation on a N*N table versus a N²*1 tablle. This showcases the
       effect of the new openMP code generation process.

--- modules/core/include/nt2/core/functions/common/transform.hpp
+++ modules/core/include/nt2/core/functions/common/transform.hpp
@@ -10,34 +10,60 @@
 #define NT2_CORE_FUNCTIONS_COMMON_TRANSFORM_HPP_INCLUDED
 
 #include <nt2/core/functions/transform.hpp>
-#include <nt2/include/functor.hpp>
-#include <nt2/include/functions/run.hpp>
 #include <nt2/include/functions/numel.hpp>
-#include <nt2/core/container/table/table.hpp>
-#include <boost/fusion/include/pop_front.hpp>
-#include <boost/fusion/include/vector_tie.hpp>
+#include <nt2/include/functions/run.hpp>
 
 namespace nt2 { namespace ext
 {
+  //============================================================================
+  // Global version
+  //============================================================================
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::transform_, tag::cpu_
                             , (A0)(A1)
                             , (ast_<A0>)
                               (ast_<A1>)
                             )
   {
-    typedef void result_type;
+    typedef void                        result_type;
 
     typedef typename meta::
             strip< typename meta::
                    scalar_of<A0>::type
-                 >::type                                    stype;
+                 >::type                stype;
+
+    BOOST_FORCEINLINE result_type operator()(A0& a0, A1& a1) const
+    {
+      std::size_t bound = nt2::numel(a0);
+      for(std::size_t i=0; i != bound; ++i)
+        nt2::run(a0, i, nt2::run(a1, i, meta::as_<stype>()));
+    }
+  };
+
+  //============================================================================
+  // Partial version - Process in*out elements starting at element p
+  // Note that p should be properly set beforehand
+  //============================================================================
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::transform_, tag::cpu_
+                            , (A0)(A1)(A2)(A3)(A4)
+                            , (ast_<A0>)
+                              (ast_<A1>)
+                              (scalar_< integer_<A2> >)
+                              (scalar_< integer_<A3> >)
+                              (scalar_< integer_<A4> >)
+                            )
+  {
+    typedef void                        result_type;
+
+    typedef typename meta::
+            strip< typename meta::
+                   scalar_of<A0>::type
+                 >::type                stype;
 
     BOOST_FORCEINLINE result_type
-    operator()(A0& a0, A1& a1) const
+    operator()(A0& a0, A1& a1, A2 p, A3 in, A4 out) const
     {
-      std::size_t bound  = nt2::numel(a0);
-
-      for(std::size_t i=0; i != bound; ++i)
+      std::size_t bound = p+in*out;
+      for(std::size_t i=p; i != bound; ++i)
         nt2::run(a0, i, nt2::run(a1, i, meta::as_<stype>()));
     }
   };
--- modules/core/include/nt2/core/functions/simd/transform.hpp
+++ modules/core/include/nt2/core/functions/simd/transform.hpp
@@ -8,27 +8,27 @@
 //==============================================================================
 #ifndef NT2_CORE_FUNCTIONS_SIMD_TRANSFORM_HPP_INCLUDED
 #define NT2_CORE_FUNCTIONS_SIMD_TRANSFORM_HPP_INCLUDED
+#ifndef BOOST_SIMD_NO_SIMD
 
 #include <nt2/core/functions/transform.hpp>
 #include <nt2/include/functions/run.hpp>
 #include <nt2/include/functions/scalar/numel.hpp>
-#include <nt2/core/container/table/table.hpp>
 #include <boost/simd/sdk/simd/native.hpp>
 #include <boost/simd/sdk/meta/cardinal_of.hpp>
-#include <boost/fusion/include/vector_tie.hpp>
 #include <boost/fusion/include/pop_front.hpp>
 
-#ifndef BOOST_SIMD_NO_SIMD
 namespace nt2 { namespace ext
 {
-  // nD element-wise operation
+  //============================================================================
+  // Global nD element-wise transform
+  //============================================================================
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::transform_, boost::simd::tag::simd_
                             , (A0)(A1)
                             , (ast_<A0>)
                               (ast_<A1>)
                             )
   {
-    typedef void                                             result_type;
+    typedef void                                            result_type;
 
     typedef typename meta::
             strip< typename meta::
@@ -38,37 +38,38 @@ namespace nt2 { namespace ext
     typedef boost::simd::native<stype, BOOST_SIMD_DEFAULT_EXTENSION>
                                                             target_type;
 
-    BOOST_FORCEINLINE result_type
-    operator()(A0& a0, A1& a1) const
+    BOOST_FORCEINLINE result_type operator()(A0& a0, A1& a1) const
     {
       typename A0::extent_type e = a0.extent();
-      static const std::size_t N = boost::simd::meta::cardinal_of<target_type>::value;
+      static const std::size_t N = boost::simd::meta
+                                        ::cardinal_of<target_type>::value;
 
       std::size_t in_sz     = boost::fusion::at_c<0>(e);
-      std::size_t in_sz_bnd = (in_sz/N)*N;
+      std::size_t in_sz_bnd = in_sz & ~(N-1);
       std::size_t outer_sz  = nt2::numel(boost::fusion::pop_front(e));
 
       std::size_t it = 0;
       for(std::size_t j=0; j != outer_sz; ++j)
       {
         for(std::size_t m=it+in_sz_bnd; it != m; it+=N)
-          nt2::run(a0, it, nt2::run(a1, it, meta::as_<target_type>()));
+          nt2::run(a0, it, nt2::run(a1, it, meta::as_<target_type>()) );
 
         for(std::size_t m=it+in_sz-in_sz_bnd; it != m; ++it)
-          nt2::run(a0, it, nt2::run(a1, it, meta::as_<stype>()));
+          nt2::run(a0, it, nt2::run(a1, it, meta::as_<stype>()) );
       }
     }
   };
 
-  // 1D element-wise operation
-  NT2_FUNCTOR_IMPLEMENTATION_TPL( nt2::tag::transform_, boost::simd::tag::simd_
-                            , (class A0)(class A1)(class Shape)(class StorageKind)(std::ptrdiff_t Sz)(class T1)(class N1)
+  //============================================================================
+  // Partial nD element-wise transform with offset/size
+  //============================================================================
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::transform_, boost::simd::tag::simd_
+                            , (A0)(A1)(A2)(A3)(A4)
                             , (ast_<A0>)
-                              ((expr_< table_< unspecified_<A1>, nt2::settings(nt2::of_size_<Sz>, Shape, StorageKind)>
-                                     , T1
-                                     , N1
-                                     >
-                              ))
+                              (ast_<A1>)
+                              (scalar_< integer_<A2> >)
+                              (scalar_< integer_<A3> >)
+                              (scalar_< integer_<A4> >)
                             )
   {
     typedef void                                            result_type;
@@ -82,18 +83,23 @@ namespace nt2 { namespace ext
                                                             target_type;
 
     BOOST_FORCEINLINE result_type
-    operator()(A0& a0, A1& a1) const
+    operator()(A0& a0, A1& a1, A2 p, A3 in, A4 out) const
     {
-      static const std::size_t N = boost::simd::meta::cardinal_of<target_type>::value;
+      static const std::size_t N = boost::simd::meta
+                                        ::cardinal_of<target_type>::value;
 
-      std::size_t bound          = boost::fusion::at_c<0>(a0.extent());
-      std::size_t aligned_bound  = (bound/N)*N;
+      std::size_t aligned_in  = in & ~(N-1);
+      std::size_t outer       = out;
+      std::size_t it          = p;
 
-      for(std::size_t i=0; i != aligned_bound; i+=N)
-        nt2::run(a0, i, nt2::run(a1, i, meta::as_<target_type>()));
+      for(std::size_t j=0; j != outer; ++j)
+      {
+        for(std::size_t m=it+aligned_in; it != m; it+=N)
+          nt2::run(a0, it, nt2::run(a1, it, meta::as_<target_type>()) );
 
-      for(std::size_t i=aligned_bound; i != bound; ++i)
-        nt2::run(a0, i, nt2::run(a1, i, meta::as_<stype>()));
+        for(std::size_t m=it+(in-aligned_in); it != m; ++it)
+          nt2::run(a0, it, nt2::run(a1, it, meta::as_<stype>()) );
+      }
     }
   };
 } }
