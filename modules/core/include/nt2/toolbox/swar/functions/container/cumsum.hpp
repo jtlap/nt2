3854ee183bbd610f9c9afdc3ba6014f8a90df419
Mathias Gaunard <mathias@gaunard.com>
2012-07-21 12:50:42 +0200

    move cumsum.hpp from reduction to swar

--- modules/core/include/nt2/toolbox/swar/functions/container/cumsum.hpp
+++ modules/core/include/nt2/toolbox/swar/functions/container/cumsum.hpp
@@ -6,8 +6,8 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef NT2_TOOLBOX_REDUCTION_FUNCTIONS_CONTAINER_CUMSUM_HPP_INCLUDED
-#define NT2_TOOLBOX_REDUCTION_FUNCTIONS_CONTAINER_CUMSUM_HPP_INCLUDED
+#ifndef NT2_TOOLBOX_SWAR_FUNCTIONS_CONTAINER_CUMSUM_HPP_INCLUDED
+#define NT2_TOOLBOX_SWAR_FUNCTIONS_CONTAINER_CUMSUM_HPP_INCLUDED
 
 #include <nt2/toolbox/swar/functions/cumsum.hpp>
 #include <boost/simd/toolbox/swar/functions/cumsum.hpp>
