34c18241c017243f130f3a094998ad0a78974ec8
jtlap <jtlapreste@gmail.com>
2012-08-02 15:15:35 +0200

    asin and acos forgotten no_brace simplification

--- modules/trigonometric/include/nt2/toolbox/trigonometric/functions/simd/common/acos.hpp
+++ modules/trigonometric/include/nt2/toolbox/trigonometric/functions/simd/common/acos.hpp
@@ -53,12 +53,11 @@ namespace nt2 { namespace ext
                             )
   {
 
-    typedef typename meta::as_floating<A0>::type result_type;
+    typedef A0 result_type;
 
     NT2_FUNCTOR_CALL(1)
     {
-      const A0 r = impl::invtrig_base<A0,radian_tag, tag::simd_type>::acos(a0); 
-      return r; 
+      return impl::invtrig_base<A0,radian_tag, tag::simd_type>::acos(a0); 
     }
   };
 } }
