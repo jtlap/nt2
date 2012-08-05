380de0c82e48f0e24d47040bd11d9d4276c822f3
Joel Falcou <joel.falcou@lri.fr>
2012-08-05 16:47:12 +0200

    Added missing splat over native logical values

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/splat.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/splat.hpp
@@ -11,6 +11,8 @@
 
 #include <boost/simd/toolbox/operator/functions/splat.hpp>
 #include <boost/simd/include/functions/simd/insert.hpp>
+#include <boost/simd/include/functions/simd/mask2logical.hpp>
+#include <boost/simd/sdk/meta/as_arithmetic.hpp>
 #include <boost/simd/sdk/meta/cardinal_of.hpp>
 #include <boost/simd/sdk/meta/scalar_of.hpp>
 
@@ -32,27 +34,74 @@ namespace boost { namespace simd { namespace ext
       typedef typename meta::scalar_of<result_type>::type sA1;
 
       result_type tmp;
-      for(unsigned int i = 0; i != boost::simd::meta::cardinal_of<result_type>::value; ++i)
+      for(unsigned int i = 0; i != meta::cardinal_of<result_type>::value; ++i)
         tmp[i] = static_cast<sA1>(a0);
 
       return tmp;
     }
   };
+  //============================================================================
+  // Splatting a logical is splat the value + mask2logical
+  //============================================================================
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::splat_, tag::cpu_
+                                    , (A0)(A1)(X)
+                                    , (unspecified_<A0>)
+                                      ((target_< simd_< logical_<A1>, X > >))
+                                    )
+  {
+    typedef typename A1::type                               result_type;
+    typedef typename meta::as_arithmetic<result_type>::type n_t;
+    inline result_type operator()(const A0& a0, const A1&) const
+    {
+      return mask2logical( splat<n_t>(a0) );
+    }
+  };
 
   //============================================================================
-  // Splatting a SIMD value is identity
+  // Splatting a SIMD value is identity in arithmetic and logical cases
   //============================================================================
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::splat_, tag::cpu_
-                            , (A0)(A1)(X)
-                            , ((simd_< arithmetic_<A0>, X >))
-                              ((target_< simd_< arithmetic_<A1>, X > >))
-                            )
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::splat_, tag::cpu_
+                                    , (A0)(A1)(X)
+                                    , ((simd_< arithmetic_<A0>, X >))
+                                      ((target_< simd_< arithmetic_<A1>, X > >))
+                                    )
   {
     typedef A0 result_type;
 
     inline result_type operator()(const A0& a0, const A1&) const
     {
       return a0;
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::splat_, tag::cpu_
+                                    , (A0)(A1)(X)
+                                    , ((simd_< logical_<A0>, X >))
+                                      ((target_< simd_< logical_<A1>, X > >))
+                                    )
+  {
+    typedef A0 result_type;
+
+    inline result_type operator()(const A0& a0, const A1&) const
+    {
+      return a0;
+    }
+  };
+
+  //============================================================================
+  // Splatting a SIMD value into a logical is mask2logical
+  //============================================================================
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::splat_, tag::cpu_
+                            , (A0)(A1)(X)
+                            , ((simd_< fundamental_<A0>, X >))
+                              ((target_< simd_< logical_<A1>, X > >))
+                            )
+  {
+    typedef A0 result_type;
+
+    inline result_type operator()(const A0& a0, const A1&) const
+    {
+      return mask2logical(a0);
     }
   };
 } } }
