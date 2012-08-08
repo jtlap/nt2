efb59b157efcf33c0b189dd05097c46974ea23a9
jtlap <jtlapreste@gmail.com>
2012-08-08 16:46:38 +0200

    one fail un cat.cpp

--- modules/core/unit/functions/cat.cpp
+++ modules/core/unit/functions/cat.cpp
@@ -12,8 +12,10 @@
 #include <nt2/include/functions/size.hpp>
 #include <nt2/include/functions/cat.hpp>
 #include <nt2/include/functions/vertcat.hpp>
+#include <nt2/include/functions/horzcat.hpp>
 #include <nt2/include/functions/rif.hpp>
 #include <nt2/include/functions/cif.hpp>
+#include <nt2/include/functions/isequal.hpp>
 
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
@@ -30,15 +32,25 @@ NT2_TEST_CASE( cat_size )
     NT2_DISPLAY(b);
     nt2::table<float> d = cat(2, a, b);
     NT2_DISPLAY(d);
+    nt2::table<float> e = horzcat(a, b); 
+    NT2_DISPLAY(e);
+    NT2_TEST(isequal(d, e)); 
+    NT2_TEST(isequal(a, d(nt2::_, nt2::_(1u, size(a, 2)))));
+    NT2_TEST(isequal(b, d(nt2::_, nt2::_(size(a, 2)+1, nt2::end_)))); 
   }
   {
     nt2::table<float> a = nt2::rif(nt2::of_size(2, 3), nt2::meta::as_<float>());
     nt2::table<float> b = nt2::cif(nt2::of_size(4, 3), nt2::meta::as_<float>());
     NT2_DISPLAY(a);
     NT2_DISPLAY(b);
-    nt2::table<float> c = vertcat(a, b);
-    NT2_DISPLAY(c);
+    nt2::table<float> e = vertcat(a, b);
+    NT2_DISPLAY(e);
     nt2::table<float> d = cat(1, a, b);
+    NT2_TEST(isequal(d, e)); 
+    NT2_TEST(isequal(a, d(nt2::_(1u, size(a, 1)), nt2::_)));
+    NT2_DISPLAY(d(nt2::_(1u, size(a, 1)), nt2::_));
+    NT2_TEST(isequal(b, d(nt2::_(size(a, 1)+1, nt2::end_), nt2::_))); 
+    NT2_DISPLAY(d(nt2::_(size(a, 1)+1, nt2::end_), nt2::_));
     NT2_DISPLAY(d);
   }
   {
@@ -48,6 +60,8 @@ NT2_TEST_CASE( cat_size )
     NT2_DISPLAY(b);
     nt2::table<float> d = cat(3, a, b);
     NT2_DISPLAY(d);
+    NT2_TEST(isequal(a, d(nt2::_,nt2::_,1 )));
+    NT2_TEST(isequal(b, d(nt2::_,nt2::_,2 ))); 
   }
 }
 
