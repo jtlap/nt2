dbf3d0e3a9d829d6114e8897a56da2c37bde0410
Mathias Gaunard <mathias@gaunard.com>
2012-08-09 18:19:33 +0200

    adding mixed scalar/simd variants for bitwise_and, bitwise_or and bitwise_xor

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/bitwise_or.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/bitwise_or.hpp
@@ -10,6 +10,7 @@
 #define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_COMMON_BITWISE_OR_HPP_INCLUDED
 
 #include <boost/simd/toolbox/operator/functions/bitwise_or.hpp>
+#include <boost/simd/include/functions/simd/splat.hpp>
 #include <boost/simd/include/functions/simd/bitwise_cast.hpp>
 #include <boost/mpl/not.hpp>
 #include <boost/type_traits/is_same.hpp>
@@ -27,6 +28,33 @@ namespace boost { namespace simd { namespace ext
     BOOST_SIMD_FUNCTOR_CALL(2)
     {
       return bitwise_or(a0, simd::bitwise_cast<A0>(a1));
+    }
+  };
+
+  // mixed scalar/simd
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::bitwise_or_, tag::cpu_,
+                                    (A0)(A1)(X),
+                                    ((simd_<arithmetic_ <A0>,X>))((scalar_<arithmetic_<A1> >))
+                                   )
+  {
+    typedef A0 result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL(2)
+    {
+      return boost::simd::bitwise_or(a0, boost::simd::splat<A0>(a1));
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::bitwise_or_, tag::cpu_,
+                                     (A0)(A1)(X),
+                                     ((scalar_<arithmetic_<A0> >))((simd_<arithmetic_<A1>,X>))
+                                   )
+  {
+    typedef A1 result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL(2)
+    {
+      return boost::simd::bitwise_or(boost::simd::splat<A1>(a0), a1);
     }
   };
 } } }
