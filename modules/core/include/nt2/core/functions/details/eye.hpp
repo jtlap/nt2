3ee1fa2608c669551103b1fdec95e0361563951d
Joel Falcou <joel.falcou@lri.fr>
2012-08-09 20:54:53 +0200

    Fixed eye

--- modules/core/include/nt2/core/functions/details/eye.hpp
+++ modules/core/include/nt2/core/functions/details/eye.hpp
@@ -10,13 +10,11 @@
 #define NT2_CORE_FUNCTIONS_DETAILS_EYE_HPP_INCLUDED
 
 #include <nt2/include/functions/ind2sub.hpp>
-#include <nt2/include/functions/simd/splat.hpp>
 #include <nt2/include/functions/simd/if_else.hpp>
 #include <nt2/include/functions/simd/is_equal.hpp>
 #include <nt2/include/functions/simd/enumerate.hpp>
 #include <nt2/include/constants/one.hpp>
 #include <nt2/include/constants/zero.hpp>
-#include <nt2/core/utility/of_size.hpp>
 #include <nt2/sdk/meta/as_integer.hpp>
 
 namespace nt2 { namespace details
@@ -30,20 +28,17 @@ namespace nt2 { namespace details
     BOOST_FORCEINLINE typename Target::type
     operator()(Pos const& p, Size const& sz, Target const&) const
     {
-      typedef typename Target::type                                   type;
-      typedef typename meta::as_integer<type>::type                   i_t;
-      typedef typename meta::call<nt2::tag::ind2sub_(_2D,Pos)>::type  sub_t;
+      typedef typename Target::type                                         type;
+      typedef typename meta::as_integer<type>::type                         i_t;
+      typedef typename meta::
+                       call<nt2::tag::enumerate_(Pos,meta::as_<i_t>)>::type p_t;
+      typedef typename meta::call<nt2::tag::ind2sub_(_2D,p_t)>::type        s_t;
 
       // Retrieve 2D position from the linear index
-      sub_t const pos = ind2sub(sz,p);
+      s_t const pos = ind2sub(sz,nt2::enumerate<i_t>(p));
 
       // Return a diagonal of 1
-      return nt2::if_else ( nt2::eq ( nt2::enumerate<i_t>( pos[0] )
-                                    , nt2::splat<i_t>( pos[1] )
-                                    )
-                          , One<type>()
-                          , Zero<type>()
-                          );
+      return nt2::if_else(nt2::eq(pos[0],pos[1]), One<type>(), Zero<type>());
     }
   };
 } }
