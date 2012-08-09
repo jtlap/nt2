50fffbda746d97fb4b50cf92d15a5ec898139c98
Joel Falcou <joel.falcou@lri.fr>
2012-08-09 22:00:59 +0200

    Fixed band, triu/l, tri1u/l

--- modules/core/include/nt2/core/functions/common/triu.hpp
+++ modules/core/include/nt2/core/functions/common/triu.hpp
@@ -36,20 +36,20 @@ namespace nt2 { namespace ext
 
     typedef typename meta::strip<base_type>::type              result_type;
 
-    typedef typename meta::as_integer<result_type>::type              i_t;
-    typedef typename meta::call<nt2::tag::ind2sub_(_2D,State)>::type  sub_t;
+    typedef typename meta::as_integer<result_type>::type                    i_t;
+    typedef typename meta::
+                     call<nt2::tag::enumerate_(State,meta::as_<i_t>)>::type p_t;
+    typedef typename meta::call<nt2::tag::ind2sub_(_2D,p_t)>::type          s_t;
 
     BOOST_FORCEINLINE result_type
     operator()(A0 const& a0, State const& p, Data const& t) const
     {
       // Retrieve 2D position from the linear index
-      sub_t pos = ind2sub(_2D(a0.extent()),p);
+      s_t pos = ind2sub(_2D(a0.extent()),nt2::enumerate<i_t>(p));
 
       // Return the upper triangular section
       return nt2::if_else
-            ( nt2::le ( nt2::enumerate<i_t>( pos[0] )
-                      , nt2::splat<i_t>    ( pos[1] )
-                      )
+            ( nt2::le (pos[0], pos[1])
             , nt2::run(boost::proto::child_c<0>(a0),p,t)
             , Zero<result_type>()
             );
@@ -71,23 +71,22 @@ namespace nt2 { namespace ext
                 >::type                                               base_type;
 
     typedef typename meta::strip<base_type>::type                     result_type;
-    typedef typename meta::as_integer<result_type>::type              i_t;
-    typedef typename meta::call<nt2::tag::ind2sub_(_2D,State)>::type  sub_t;
+    typedef typename meta::as_integer<result_type>::type                    i_t;
+    typedef typename meta::
+                     call<nt2::tag::enumerate_(State,meta::as_<i_t>)>::type p_t;
+    typedef typename meta::call<nt2::tag::ind2sub_(_2D,p_t)>::type          s_t;
+    typedef typename s_t::value_type                                        sp_t;
 
     BOOST_FORCEINLINE result_type
     operator()(A0 const& a0, State const& p, Data const& t) const
     {
       // Retrieve 2D position from the linear index
-      sub_t pos = ind2sub(_2D(a0.extent()),p);
+      s_t pos = ind2sub(_2D(a0.extent()),nt2::enumerate<i_t>(p));
+      sp_t p1 = pos[1] - boost::proto::value(boost::proto::child_c<1>(a0));
 
       // Return the upper triangular section shifted by k element
       return nt2::if_else
-            ( nt2::le ( nt2::enumerate<i_t>( pos[0] )
-                      , nt2::splat<i_t>
-                        ( pos[1]
-                        - boost::proto::value(boost::proto::child_c<1>(a0))
-                        )
-                      )
+            ( nt2::le (pos[0], p1)
             , nt2::run(boost::proto::child_c<0>(a0),p,t)
             , Zero<result_type>()
             );
