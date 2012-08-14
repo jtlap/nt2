e07d3bea273f19c9c9abacfee43e0979d8f7e43f
jtlap <jtlapreste@gmail.com>
2012-08-14 15:25:49 +0200

    mean.cpp to test rec

--- modules/core/unit/functions/mean.cpp
+++ modules/core/unit/functions/mean.cpp
@@ -90,25 +90,28 @@ NT2_TEST_CASE_TPL( mean_2, NT2_REAL_TYPES )
     for(int i=1;i<=5;i++)
       y(i,j) = i + 10*j;
   sy = nt2::sum(y)/T(nt2::size(y, nt2::firstnonsingleton(y)));
-  syb= nt2::sum(y)*rec(T(nt2::size(y, nt2::firstnonsingleton(y)))); 
+  syb= nt2::sum(y)*nt2::rec(T(nt2::size(y, nt2::firstnonsingleton(y)))); 
   sy2 = nt2::mean(y);
   NT2_TEST(nt2::isequal(sy2,  nt2::mean(y)));
   NT2_TEST(nt2::isequal(syb,  nt2::mean(y)));
 
   sy = nt2::sum(y, 1)/T(nt2::size(y, 1));
   sy2 = nt2::mean(y, 1);
+  syb= nt2::sum(y, 1)*nt2::rec(T(nt2::size(y, 1))); 
   NT2_TEST(nt2::isequal(sy2,  nt2::mean(y, 1)));
   NT2_TEST(nt2::isequal(syb,  nt2::mean(y, 1)));
 
 
   sy = nt2::sum(y, 2)/T(nt2::size(y, 2));
   sy2 = nt2::mean(y, 2);
+  syb= nt2::sum(y, 2)*nt2::rec(T(nt2::size(y, 2))); 
   NT2_TEST(nt2::isequal(sy2,  nt2::mean(y, 2)));
   NT2_TEST(nt2::isequal(syb,  nt2::mean(y, 2)));
 
 
   sy = nt2::sum(y, 3)/T(nt2::size(y, 3));
   sy2 = nt2::mean(y, 3);
+  syb= nt2::sum(y, 3)*nt2::rec(T(nt2::size(y, 3))); 
   NT2_TEST(nt2::isequal(sy2,  nt2::mean(y, 3)));
   NT2_TEST(nt2::isequal(syb,  nt2::mean(y, 3)));
 }
