ca2e94240379d5d5f7c56739499a49b0e9306f71
jtlap <jtlapreste@gmail.com>
2012-08-21 22:45:55 +0200

    multind.cpp shows some regression in indexing in function_gather branch

--- modules/core/unit/container/table/multind.cpp
+++ modules/core/unit/container/table/multind.cpp
@@ -11,6 +11,8 @@
 #include <nt2/table.hpp>
 #include <nt2/include/functions/isequal.hpp>
 #include <nt2/include/functions/ones.hpp>
+#include <nt2/include/functions/rif.hpp>
+#include <nt2/include/constants/two.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
 #include <nt2/sdk/unit/module.hpp>
@@ -20,47 +22,52 @@ NT2_TEST_CASE_TPL( table_dimensions ,NT2_TYPES)
   using nt2::table;
   using nt2::_;
   using nt2::meta::as_; 
-  typedef T r_t;
   nt2::table<T> a(nt2::of_size(1, 10));
   nt2::table<T> b(nt2::of_size(1, 10));
   nt2::table<T> c(nt2::of_size(10, 1));
 
 // Ca c'est ok -------------------------------------
-//   a = _(T(1), T(10));
-//   NT2_DISPLAY(a); 
-//   a(_(1, 2, 10)) = T(1);
-//   NT2_DISPLAY(a); 
-//   a(_(2, 2, 10)) = T(1);
-//   NT2_DISPLAY(a);
-//   NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
+  a = _(T(1), T(10));
+  NT2_DISPLAY(a); 
+  a(_(1, 2, 10)) = T(1);
+  NT2_DISPLAY(a); 
+  a(_(2, 2, 10)) = T(1);
+  NT2_DISPLAY(a);
+  NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
 // fin ok ------------------------------------------
-
+  typedef typename nt2::meta::as_integer<T>::type itype; 
   // le reste ne passe pas,  décommente ce que tu veux
-  nt2::table<int> ind = _(1, 10);
+  nt2::table<itype> ind = _(itype(1), itype(10));
   NT2_DISPLAY(ind);
   a(ind) = T(1);
   NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
 
-//   b = a;
-//   c = nt2::ones(10, 1, as_<T>())
-//   nt2::table<int> ind = _(1, 10);
-//   NT2_DISPLAY(ind);
-//   a(ind) = b;
-//   NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
+  b = a;
+  c = nt2::ones(10, 1, as_<T>()); 
+  NT2_DISPLAY(ind);
+  a(ind) = b;
+  NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
 
-//   nt2::table<int> ind = _(1, 10);
-//   NT2_DISPLAY(ind);
-//   a(ind) = b(ind);
-//   NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
+  NT2_DISPLAY(ind);
+  a(ind) = b(ind);
+  NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
   
-//   nt2::table<int> ind = _(1, 10);
-//   NT2_DISPLAY(ind);
-//   a(_) = b(ind);
-//   NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
+  NT2_DISPLAY(ind);
+  a(_) = b(ind);
+  NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
   
-//   nt2::table<int> ind = _(1, 10);
-//   NT2_DISPLAY(ind);
-//   a(_) = b(ind);
-//   NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
+  NT2_DISPLAY(ind);
+  a(_) = b(ind);
+  NT2_TEST(isequal(a, nt2::ones(1, 10, as_<T>())));
 
+}
+NT2_TEST_CASE_TPL( table_2 ,NT2_TYPES)
+{
+  using nt2::table;
+  using nt2::_;
+  using nt2::meta::as_; 
+  nt2::table<T> c = nt2::rif(6, 1, nt2::meta::as_<T>());
+  typedef typename nt2::meta::as_integer<T>::type itype; 
+  nt2::table<itype> ind = nt2::Two<itype>()*nt2::ones(1, 10, nt2::meta::as_<itype>());
+  NT2_DISPLAY(c(ind)); 
 }
