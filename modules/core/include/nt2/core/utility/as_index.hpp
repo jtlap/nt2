9373c5f8a22d685a2639e78408aa68b9f6961900
Joel Falcou <joel.falcou@lri.fr>
2012-08-28 22:16:12 +0200

    Added as_index utility function.
    as_idnex repalce the fat sub2ind in internal position
    computation. as_index, like as_subscript is lighter and
    0-based.

---
+++ modules/core/include/nt2/core/utility/as_index.hpp
@@ -0,0 +1,113 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_UTILITY_AS_INDEX_HPP_INCLUDED
+#define NT2_CORE_UTILITY_AS_INDEX_HPP_INCLUDED
+
+#include <nt2/core/functions/sub2ind.hpp>
+#include <nt2/include/functions/simd/splat.hpp>
+#include <nt2/include/functions/simd/minus.hpp>
+#include <nt2/include/functions/simd/plus.hpp>
+#include <nt2/include/functions/simd/multiplies.hpp>
+#include <nt2/include/functions/simd/bitwise_cast.hpp>
+#include <nt2/include/constants/one.hpp>
+#include <nt2/include/constants/zero.hpp>
+#include <nt2/sdk/meta/as_unsigned.hpp>
+#include <boost/fusion/include/at.hpp>
+#include <boost/fusion/include/value_at.hpp>
+#include <boost/fusion/include/size.hpp>
+#include <boost/fusion/adapted/array.hpp>
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/identity.hpp>
+#include <boost/mpl/eval_if.hpp>
+#include <cstddef>
+
+namespace nt2 { namespace details
+{
+  template<class Seq>
+  struct sequence_value
+   : boost::dispatch::meta::
+     strip< typename boost::mpl::
+            eval_if_c< boost::fusion::result_of::size<Seq>::type::value == 0
+                     , boost::mpl::identity<std::size_t>
+                     , boost::fusion::result_of::value_at_c<Seq, 0>
+                     >::type
+          >
+  {
+  };
+
+  template<class Size, class Position>
+  struct as_index
+  {
+    typedef typename sequence_value<Position>::type               type;
+    typedef typename meta::as_unsigned<type>::type          result_type;
+
+    BOOST_DISPATCH_FORCE_INLINE result_type
+    operator()(const Size& size, const Position& pos) const
+    {
+      typedef typename boost::fusion::result_of::size<Position>::type dims;
+      return eval ( size, pos
+                  , boost::mpl::int_<0>()
+                  , boost::mpl::int_<dims::value-1>()
+                  );
+    }
+
+    template<class Idx, class Sz>
+    BOOST_DISPATCH_FORCE_INLINE result_type
+    eval(const Size& s, const Position& p, const Idx&, const Sz& sz) const
+    {
+      return  splat<result_type>( boost::fusion::at_c<Idx::value>(p) )
+            + splat<result_type>( boost::fusion::at_c<Idx::value>(s) )
+            * eval(s,p,boost::mpl::int_<Idx::value+1>(),sz);
+    }
+
+    template<class Sz>
+    BOOST_DISPATCH_FORCE_INLINE result_type
+    eval(const Size& s,const Position& p,const boost::mpl::int_<0>&,const Sz& sz) const
+    {
+      return  splat<result_type>( boost::fusion::at_c<0>(p) )
+            + splat<result_type>( boost::fusion::at_c<0>(s) )
+            * eval(s,p,boost::mpl::int_<1>(),sz);
+    }
+
+    template<class Sz>
+    BOOST_DISPATCH_FORCE_INLINE result_type
+    eval(const Size&, const Position& p, const Sz&, const Sz&) const
+    {
+      return splat<type>( boost::fusion::at_c<Sz::value>(p) );
+    }
+
+    BOOST_DISPATCH_FORCE_INLINE result_type
+    eval( const Size&, const Position& p
+        , const boost::mpl::int_<0>&, const boost::mpl::int_<0>&
+        ) const
+    {
+      return splat<result_type>( boost::fusion::at_c<0>(p) );
+    }
+
+    BOOST_DISPATCH_FORCE_INLINE result_type
+    eval( const Size&, const Position&
+        , const boost::mpl::int_<0>&, const boost::mpl::int_<-1>&
+        ) const
+    {
+      return Zero<result_type>();
+    }
+  };
+} }
+
+namespace nt2
+{
+  template<class Size,class Position>
+  typename details::as_index<Size,Position>::result_type
+  as_index(Size const& sz, Position const& p)
+  {
+    return details::as_index<Size,Position>()(sz,p);
+  }
+}
+
+#endif
---
+++ modules/core/unit/utility/as_index.cpp
@@ -0,0 +1,132 @@
+/*******************************************************************************
+ *         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+ *         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#define NT2_UNIT_MODULE "nt2::as_index function"
+
+#include <nt2/core/utility/as_index.hpp>
+#include <nt2/include/functions/enumerate.hpp>
+
+#include <boost/fusion/include/make_vector.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/basic.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+
+NT2_TEST_CASE( as_index_0D )
+{
+  using nt2::as_index;
+  using boost::fusion::make_vector;
+
+  NT2_TEST_EQUAL( as_index( make_vector(), make_vector() ), 0u);
+}
+
+NT2_TEST_CASE( as_index_1D )
+{
+  using nt2::as_index;
+  using boost::fusion::make_vector;
+
+  NT2_TEST_EQUAL( as_index( make_vector(5), make_vector(0) ), 0u);
+  NT2_TEST_EQUAL( as_index( make_vector(5), make_vector(1) ), 1u);
+  NT2_TEST_EQUAL( as_index( make_vector(5), make_vector(2) ), 2u);
+  NT2_TEST_EQUAL( as_index( make_vector(5), make_vector(3) ), 3u);
+  NT2_TEST_EQUAL( as_index( make_vector(5), make_vector(4) ), 4u);
+}
+
+NT2_TEST_CASE_TPL ( as_index_1D_simd
+                  , (nt2::int32_t)(nt2::uint32_t)(nt2::int64_t)(nt2::uint64_t)
+                  )
+{
+  using nt2::as_index;
+  using boost::simd::native;
+  using boost::fusion::make_vector;
+  using boost::mpl::int_;
+
+  typedef native<T, BOOST_SIMD_DEFAULT_EXTENSION> idx_t;
+  typedef typename boost::dispatch::meta::as_unsigned<idx_t>::type sidx_t;
+  static const std::size_t sz = boost::simd::meta::cardinal_of<idx_t>::value;
+
+  NT2_TEST_EQUAL( as_index( make_vector(16)
+                          , make_vector(nt2::enumerate<idx_t>(0))
+                          )
+                , nt2::enumerate<sidx_t>(0)
+                );
+}
+
+NT2_TEST_CASE( as_index_2D )
+{
+  using nt2::as_index;
+  using boost::fusion::make_vector;
+
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(0,0) ), 0u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(1,0) ), 1u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(2,0) ), 2u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(0,1) ), 3u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(1,1) ), 4u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(2,1) ), 5u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(0,2) ), 6u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(1,2) ), 7u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(2,2) ), 8u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(0,3) ), 9u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(1,3) ), 10u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(2,3) ), 11u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(0,4) ), 12u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(1,4) ), 13u);
+  NT2_TEST_EQUAL( as_index( make_vector(3,5), make_vector(2,4) ), 14u);
+}
+
+NT2_TEST_CASE( as_index_3D )
+{
+  using nt2::as_index;
+  using boost::fusion::make_vector;
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2), make_vector(0,0,0) ), 0u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2), make_vector(1,0,0) ), 1u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2), make_vector(0,1,0) ), 2u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2), make_vector(1,1,0) ), 3u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2), make_vector(0,0,1) ), 4u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2), make_vector(1,0,1) ), 5u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2), make_vector(0,1,1) ), 6u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2), make_vector(1,1,1) ), 7u);
+}
+
+NT2_TEST_CASE( as_index_4D )
+{
+  using nt2::as_index;
+  using boost::fusion::make_vector;
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(0,0,0,0) ), 0u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(1,0,0,0) ), 1u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(0,1,0,0) ), 2u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(1,1,0,0) ), 3u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(0,0,1,0) ), 4u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(1,0,1,0) ), 5u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(0,1,1,0) ), 6u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(1,1,1,0) ), 7u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(0,0,0,1) ), 8u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(1,0,0,1) ), 9u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(0,1,0,1) ), 10u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(1,1,0,1) ), 11u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(0,0,1,1) ), 12u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(1,0,1,1) ), 13u);
+
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(0,1,1,1) ), 14u);
+  NT2_TEST_EQUAL( as_index( make_vector(2,2,2,2), make_vector(1,1,1,1) ), 15u);
+}
