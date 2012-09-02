c643ac7061c0cb9b731f0ac32defd827f2615f90
jtlap <jtlapreste@gmail.com>
2012-09-02 10:04:26 +0200

    test for vandermonde

--- modules/linalg/unit/functions/vandermonde.cpp
+++ modules/linalg/unit/functions/vandermonde.cpp
@@ -13,12 +13,31 @@
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/exceptions.hpp>
+#include <nt2/include/functions/isulpequal.hpp>
+#include <nt2/sdk/unit/tests/basic.hpp>
 
 NT2_TEST_CASE_TPL ( vandermonde_ints, NT2_REAL_TYPES)
 {
   nt2::table<T> a0 = nt2::_(T(1), 3);
   nt2::table<T> v  =  nt2::vandermonde(a0); 
   nt2::display("vandermonde(a0)", v);
-  nt2::display("vandermonde(a0)", nt2::vandermonde(a0)); 
+  nt2::display("vandermonde(a0)", nt2::vandermonde(a0));
+  T bc[9] =  {
+    1, 1, 1, 
+    4, 2, 1, 
+    9, 3, 1
+  };
+  int k = 0;
+  nt2::table<T> a(nt2::of_size(3, 3));
+  for(int i=1; i <= 3; i++)
+    {
+      for(int j=1; j <= 3; j++)
+        {
+          a(i, j) = bc[k++];
+        }
+
+    }
+  NT2_DISPLAY(a); 
+  NT2_TEST(nt2::isulpequal(a, v));  
 }
 
