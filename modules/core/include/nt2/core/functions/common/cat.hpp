24be22dfbc3308b823e53616be9e30f165c954cc
Joel Falcou <joel.falcou@lri.fr>
2012-09-05 20:06:16 +0200

    Fix cat functions behavior
    cat was incorrectly accessing invalid address when selecting its value.
    This fix clamp the computed position so it never goes out of bounds.

--- modules/core/include/nt2/core/functions/common/cat.hpp
+++ modules/core/include/nt2/core/functions/common/cat.hpp
@@ -1,6 +1,7 @@
 //==============================================================================
-//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2011 - 2012   MetaScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -11,13 +12,13 @@
 
 #include <nt2/core/functions/cat.hpp>
 #include <nt2/include/functions/run.hpp>
-#include <nt2/include/constants/zero.hpp>
-#include <nt2/include/functions/splat.hpp>
-#include <nt2/include/functions/if_else.hpp>
-#include <nt2/include/functions/is_less.hpp>
-#include <nt2/include/functions/is_greater_equal.hpp>
-#include <nt2/include/functions/selsub.hpp>
-#include <nt2/include/functions/enumerate.hpp>
+#include <nt2/include/functions/simd/min.hpp>
+#include <nt2/include/functions/simd/splat.hpp>
+#include <nt2/include/functions/simd/if_else.hpp>
+#include <nt2/include/functions/simd/is_less.hpp>
+#include <nt2/include/functions/simd/is_greater_equal.hpp>
+#include <nt2/include/functions/simd/selsub.hpp>
+#include <nt2/include/functions/simd/enumerate.hpp>
 #include <nt2/core/utility/as_subscript.hpp>
 #include <nt2/core/utility/as_index.hpp>
 #include <nt2/sdk/meta/as_index.hpp>
@@ -25,32 +26,37 @@
 namespace nt2 { namespace ext
 {
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_, tag::cpu_
-                            , (A0)(State)(Data)(N)
+                            , (A0)(State)(Target)(N)
                             , ((node_<A0, nt2::tag::cat_, N>))
                               (generic_< integer_<State> >)
-                              ((unspecified_<Data>))
+                              ((target_< unspecified_<Target> >))
                             )
   {
-    typedef typename Data::type                                          result_type;
-    typedef typename A0::extent_type                                     ext_t;
-    typedef typename meta::as_index<result_type>::type                   i_t;
-    typedef typename details::as_subscript<ext_t,i_t>::result_type       sub_t;
+    typedef typename Target::type                                   result_type;
+    typedef typename A0::extent_type                                ext_t;
+    typedef typename meta::as_index<result_type>::type              i_t;
+    typedef typename details::as_subscript<ext_t,i_t>::result_type  sub_t;
 
     BOOST_FORCEINLINE result_type
-    operator()(A0 const& a0, State const& p, Data const& t) const
+    operator()(A0 const& a0, State const& p, Target const& t) const
     {
-      size_t const along =  boost::proto::child_c<2>(a0);
+      // Gather dimension to concatenate
+      std::size_t along = boost::proto::child_c<2>(a0);
 
+      // Gather size of a0 and a1
       ext_t ex0 = boost::proto::child_c<0>(a0).extent();
       ext_t ex1 = boost::proto::child_c<1>(a0).extent();
 
-      sub_t pos0 = as_subscript(a0.extent(),enumerate<i_t>(p));
-      sub_t pos1 = pos0;
-
+      // Gather indexing treshold
       i_t offset = splat<i_t>(ex0[along]);
+
+      sub_t pos  = as_subscript(a0.extent(),enumerate<i_t>(p));
+      sub_t pos0 = pos;
+      sub_t pos1 = pos;
+      pos0[along] = min( pos0[along], splat<i_t>(ex0[along]-1));
       pos1[along] = selsub( ge(pos1[along],offset), pos1[along], offset);
 
-      return if_else( lt(pos0[along], offset)
+      return if_else( lt(pos[along], offset)
                     , nt2::run( boost::proto::child_c<0>(a0)
                               , as_index(ex0, pos0)
                               , t
