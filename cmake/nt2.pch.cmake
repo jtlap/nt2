743a6dea349d85ea2c6dfbaf85b9408e64135425
Mathias Gaunard <mathias@gaunard.com>
2012-09-06 03:51:10 +0200

    change the way PCH is implemented so that it can work with Ninja too

--- cmake/nt2.pch.cmake
+++ cmake/nt2.pch.cmake
@@ -14,7 +14,7 @@ macro(nt2_pch name)
     message(FATAL_ERROR "[nt2.pch] precompiled header already set to ${NT2_PCH_TARGET}, cannot change to ${name}")
   endif()
 
-  if((CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX) AND CMAKE_GENERATOR MATCHES "Make")
+  if(NT2_WITH_PCH AND (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX) AND CMAKE_GENERATOR MATCHES "Make|Ninja")
 
     get_target_property(pch_exists pch EXCLUDE_FROM_ALL)
     if(pch_exists MATCHES "NOTFOUND$")
@@ -23,40 +23,28 @@ macro(nt2_pch name)
 
     foreach(BUILD_TYPE Debug Release NT2Test NT2TestDebug NT2Bench)
       string(TOUPPER ${BUILD_TYPE} BUILD_TYPE_U)
-      set(FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${BUILD_TYPE_U}}")
-      string(REPLACE " " ";" FLAGS ${FLAGS})
-
-      get_directory_property(COMPILE_DEFINITIONS COMPILE_DEFINITIONS)
-      foreach(DEFINITION ${COMPILE_DEFINITIONS})
-        list(APPEND ${FLAGS} -D${DEFINITION})
-      endforeach()
-
-      get_directory_property(INCLUDE_DIRECTORIES INCLUDE_DIRECTORIES)
-      set(INCLUDES)
-      foreach(INCLUDE ${INCLUDE_DIRECTORIES})
-        list(APPEND INCLUDES "-I${INCLUDE}")
-        if(EXISTS ${INCLUDE}/${name})
-          set(file ${INCLUDE}/${name})
-        endif()
-      endforeach()
-
       set(name_ "${name}/${BUILD_TYPE}")
       string(REPLACE "/" "_" pch_base ${name})
       string(REPLACE "/" "_" pch_base_ ${name_})
       string(REGEX REPLACE "\\.hpp$" "" rule ${pch_base})
       string(REGEX REPLACE "\\.hpp$" "_${BUILD_TYPE}" rule_ ${pch_base})
+
       file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${pch_base_} "#include <${name}>\n")
-      add_custom_command(OUTPUT ${pch_base_}.gch
-                         COMMAND ${CMAKE_CXX_COMPILER} ${FLAGS} ${INCLUDES} -x c++-header -c ${file} -o ${pch_base_}.gch
-                         IMPLICIT_DEPENDS CXX ${file}
-                         COMMENT "Precompiling header file ${name} for configuration ${BUILD_TYPE}..."
+      set_source_files_properties(${CMAKE_CURRENT_BINARY_DIR}/${pch_base_} PROPERTIES LANGUAGE CXX)
+
+      add_library(${rule_}.pch STATIC ${CMAKE_CURRENT_BINARY_DIR}/${pch_base_})
+      set_target_properties(${rule_}.pch PROPERTIES COMPILE_FLAGS "${CMAKE_CXX_FLAGS_${BUILD_TYPE_U}} -x c++-header")
+      add_custom_command(TARGET ${rule_}.pch POST_BUILD
+                         COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${rule_}.pch.dir/${pch_base_}.o
+                                                          ${CMAKE_CURRENT_BINARY_DIR}/${pch_base_}.gch
+                         COMMAND ${CMAKE_COMMAND} -E remove ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${rule_}.pch.dir/${pch_base_}.o
+                         COMMAND ${CMAKE_COMMAND} -E touch  ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${rule_}.pch.dir/${pch_base_}.o
+                         COMMAND ${CMAKE_COMMAND} -E remove ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${rule_}.pch${CMAKE_STATIC_LIBRARY_SUFFIX}
+                         COMMAND ${CMAKE_COMMAND} -E touch  ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${rule_}.pch${CMAKE_STATIC_LIBRARY_SUFFIX}
                         )
-      add_custom_target(${rule_}.pch
-                        DEPENDS ${pch_base_}.gch
-                       )
       add_dependencies(pch ${rule_}.pch)
       set(NT2_PCH_TARGET ${rule})
-      # Escape special characters. Unfortunately CMake doesn't already take care of it when using add_definitionss
+      # Escape special characters
       string(REGEX REPLACE "([ \\\\\"&|;#])" "\\\\\\1" path "${CMAKE_CURRENT_BINARY_DIR}/${pch_base_}")
       set(CMAKE_CXX_FLAGS_${BUILD_TYPE_U} "${CMAKE_CXX_FLAGS_${BUILD_TYPE_U}} -include ${path} -Winvalid-pch")
     endforeach()
