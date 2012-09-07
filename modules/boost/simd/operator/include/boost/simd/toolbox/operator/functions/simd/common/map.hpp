4b0cff4c33b1ddf0f34f712819f7fd1da01a6e16
Mathias Gaunard <mathias@gaunard.com>
2012-09-07 02:09:10 +0200

    changing the way result of map is computed, use vector_of with cardinal instead of picking the extension at random

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/map.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/map.hpp
@@ -8,27 +8,13 @@
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_COMMON_MAP_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_COMMON_MAP_HPP_INCLUDED
+
 #include <boost/simd/toolbox/operator/functions/map.hpp>
-#include <boost/simd/sdk/simd/category.hpp>
 #include <boost/simd/include/functions/simd/insert.hpp>
 #include <boost/simd/include/functions/simd/extract.hpp>
+#include <boost/simd/sdk/simd/meta/vector_of.hpp>
 #include <boost/simd/sdk/meta/cardinal_of.hpp>
 #include <boost/simd/sdk/meta/scalar_of.hpp>
-
-namespace boost { namespace simd { namespace details
-{
-  template<class X, class Y>
-  struct best_ext
-  {
-    typedef X type;
-  };
-
-  template<std::size_t N, class Y>
-  struct best_ext< tag::simd_emulation_<N>, Y >
-  {
-    typedef Y type;
-  };
-} } }
 
 #if !defined(BOOST_SIMD_DONT_USE_PREPROCESSED_FILES)
 #include <boost/simd/toolbox/operator/functions/simd/common/preprocessed/map.hpp>
@@ -45,21 +31,10 @@ namespace boost { namespace simd { namespace details
 #undef BOOST_FORCEINLINE
 #endif
 
-#define BINARY_TREE(n, f, name) BOOST_PP_CAT(BINARY_TREE_, n)(f, name)
-#define BINARY_TREE_0(f, name)
-#define BINARY_TREE_1(f, name) BOOST_PP_CAT(name, 0)
-#define BINARY_TREE_2(f, name) typename f<  BOOST_PP_CAT(name, 0), BOOST_PP_CAT(name, 1) >::type
-#define BINARY_TREE_3(f, name) typename f< BINARY_TREE_2(f, name), BOOST_PP_CAT(name, 2) >::type
-#define BINARY_TREE_4(f, name) typename f< BINARY_TREE_3(f, name), BOOST_PP_CAT(name, 3) >::type
-#define BINARY_TREE_5(f, name) typename f< BINARY_TREE_4(f, name), BOOST_PP_CAT(name, 4) >::type
-#define BINARY_TREE_6(f, name) typename f< BINARY_TREE_5(f, name), BOOST_PP_CAT(name, 5) >::type
-#define BINARY_TREE_7(f, name) typename f< BINARY_TREE_6(f, name), BOOST_PP_CAT(name, 6) >::type
-#define BINARY_TREE_8(f, name) typename f< BINARY_TREE_7(f, name), BOOST_PP_CAT(name, 7) >::type
-
-#define M0(z,n,t) ((simd_< unspecified_<A##n>, X##n >))
+#define M0(z,n,t) (generic_< unspecified_<A##n> >)
 #define M2(z,n,t) typename meta::scalar_of<_A##n>::type
-#define M3(z,n,t) a##n[i]
-#define M4(z,n,t) (A##n)(X##n)
+#define M3(z,n,t) extract(a##n, i)
+#define M4(z,n,t) (A##n)
 
 #define M5(z,n,t)                                                              \
 namespace boost { namespace simd { namespace ext                               \
@@ -83,7 +58,7 @@ namespace boost { namespace simd { namespace ext                               \
       template<class rtype, class Dummy = void>                                \
       struct impl                                                              \
       {                                                                        \
-        typedef simd::native<rtype, BINARY_TREE(n, details::best_ext, X)> type; \
+        typedef typename meta::vector_of<rtype, meta::cardinal_of<A0>::value>::type type; \
         static type call( Func const& f                                        \
                         , BOOST_PP_ENUM_BINARY_PARAMS(n, _A, & a)              \
                         )                                                      \
@@ -142,17 +117,6 @@ BOOST_PP_REPEAT_FROM_TO(1,BOOST_PP_INC(BOOST_DISPATCH_MAX_ARITY),M5,~)
 #undef M2
 #undef M1
 #undef M0
-
-#undef BINARY_TREE
-#undef BINARY_TREE_0
-#undef BINARY_TREE_1
-#undef BINARY_TREE_2
-#undef BINARY_TREE_3
-#undef BINARY_TREE_4
-#undef BINARY_TREE_5
-#undef BINARY_TREE_6
-#undef BINARY_TREE_7
-#undef BINARY_TREE_8
 
 #if defined(__WAVE__) && defined(BOOST_SIMD_CREATE_PREPROCESSED_FILES)
 #pragma wave option(output: null)
