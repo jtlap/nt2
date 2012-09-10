d33bb942a489f011dc3d942e28a46cfbbf28c7d0
jtlap <jtlapreste@gmail.com>
2012-09-10 10:09:09 +0200

    some tests for unifrnd

--- modules/statistics/unit/scalar/unifrnd.cpp
+++ modules/statistics/unit/scalar/unifrnd.cpp
@@ -15,6 +15,9 @@
 /// 
 #include <nt2/include/functions/unifrnd.hpp>
 #include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/include/functions/is_less_equal.hpp>
+#include <nt2/include/functions/is_greater_equal.hpp>
+#include <nt2/include/functions/globalall.hpp>
 #include <nt2/include/functions/ones.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
@@ -50,14 +53,24 @@ NT2_TEST_CASE_TPL ( unifrnd_real__2_1,  NT2_REAL_TYPES)
   // specific values tests
   nt2::table<T> a =  nt2::ones(3, 4, nt2::meta::as_<T>());
   nt2::table<T> b = unifrnd(T(-1.0), T(2.0));
+  NT2_TEST(nt2::globalall(ge(b, T(-1))));
+  NT2_TEST(nt2::globalall(le(b, T(2)))); 
   NT2_DISPLAY(b);
   nt2::table<T> c =  unifrnd(T(-1.0), T(2.0), 3);
+  NT2_TEST(nt2::globalall(ge(c, T(-1))));
+  NT2_TEST(nt2::globalall(le(c, T(2)))); 
   NT2_DISPLAY(c);
   nt2::table<T> d =  unifrnd(T(-1.0), T(2.0), 3, 5);
+  NT2_TEST(nt2::globalall(ge(d, T(-1))));
+  NT2_TEST(nt2::globalall(le(d, T(2)))); 
   NT2_DISPLAY(d);
   nt2::table<T> e =  unifrnd(T(-1.0), T(2.0), nt2::of_size(3, 2));
+  NT2_TEST(nt2::globalall(ge(e, T(-1))));
+  NT2_TEST(nt2::globalall(le(e, T(2)))); 
   NT2_DISPLAY(e);
   nt2::table<T> f =  unifrnd(T(-1.0), T(2.0), size(e));
+  NT2_TEST(nt2::globalall(ge(f, T(-1))));
+  NT2_TEST(nt2::globalall(le(f, T(2)))); 
   NT2_DISPLAY(f);
   
  } // end of test for floating_
