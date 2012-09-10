0adfa00634fcab45526cc9a4ae3b189de287ccb9
Mathias Gaunard <mathias@gaunard.com>
2012-09-10 22:55:21 +0200

    add CMAKE_CROSSCOMPILING_CMD to prefix test executables for cross-compilation

--- cmake/NT2Module.cmake
+++ cmake/NT2Module.cmake
@@ -158,7 +158,8 @@ macro(nt2_configure_tests)
   option(NT2_WITH_TESTS_FULL "Use one executable per test" ${NT2_WITH_TESTS_FULL_})
   option(NT2_WITH_TESTS_BENCH "Register benchmarks with ctest" OFF)
   option(NT2_WITH_TESTS_COVER "Enable cover tests" OFF)
-  set(CMAKE_CROSSCOMPILING_HOST $ENV{CMAKE_CROSSCOMPILING_HOST} CACHE STRING "Host name to connect to in order to run tests in a cross-compiling setup")
+  set(CMAKE_CROSSCOMPILING_CMD $ENV{CMAKE_CROSSCOMPILING_CMD})# CACHE STRING "Command to use to run test sin a cross-compiling setup")
+  set(CMAKE_CROSSCOMPILING_HOST $ENV{CMAKE_CROSSCOMPILING_HOST})# CACHE STRING "Host name to connect to in order to run tests in a cross-compiling setup")
 
   if(NT2_WITH_TESTS_FULL)
     set(NT2_WITH_TESTS ON CACHE BOOL "Enable benchmarks and unit tests" FORCE)
@@ -385,6 +386,8 @@ macro(nt2_module_add_tests name)
           add_test(${prefix}.${basename}-${suffix} /bin/sh -c
                    "scp \"${NT2_BINARY_DIR}/${suffix}/${exe}\" ${CMAKE_CROSSCOMPILING_HOST}:/tmp && ssh ${CMAKE_CROSSCOMPILING_HOST} /tmp/${exe} ${arg} && ssh ${CMAKE_CROSSCOMPILING_HOST} rm /tmp/${exe}"
                   )
+        elseif(CMAKE_CROSSCOMPILING AND CMAKE_CROSSCOMPILING_CMD)
+          add_test(${prefix}.${basename}-${suffix} ${CMAKE_CROSSCOMPILING_CMD} ${NT2_BINARY_DIR}/${suffix}/${exe} ${arg})
         else()
           add_test(${prefix}.${basename}-${suffix} ${NT2_BINARY_DIR}/${suffix}/${exe} ${arg})
         endif()
