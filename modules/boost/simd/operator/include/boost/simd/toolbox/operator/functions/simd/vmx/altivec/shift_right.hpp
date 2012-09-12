af4f1ca4fc15acc8e7793715d182e4b28d15f7ea
Mathias Gaunard <mathias@gaunard.com>
2012-09-12 19:05:33 +0200

    altivec shift_right should use vec_sra, not vec_sr

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/vmx/altivec/shift_right.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/vmx/altivec/shift_right.hpp
@@ -1,10 +1,10 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_VMX_ALTIVEC_SHIFT_RIGHT_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_VMX_ALTIVEC_SHIFT_RIGHT_HPP_INCLUDED
@@ -23,11 +23,11 @@ namespace boost { namespace simd { namespace ext
   {
     typedef A0 result_type;
 
-    BOOST_SIMD_FUNCTOR_CALL(2)  
-    { 
+    BOOST_SIMD_FUNCTOR_CALL(2)
+    {
      typedef typename dispatch::meta::as_unsigned<A1>::type type;
      type shift = simd::bitwise_cast<type>(a1);
-     return vec_sr(a0(), shift()); 
+     return vec_sra(a0(), shift());
     }
   };
 
@@ -38,12 +38,12 @@ namespace boost { namespace simd { namespace ext
   {
     typedef A0 result_type;
 
-    BOOST_SIMD_FUNCTOR_CALL(2) 
-   { 
+    BOOST_SIMD_FUNCTOR_CALL(2)
+   {
      typedef typename dispatch::meta::as_unsigned<A1>::type type;
      type shift = simd::bitwise_cast<type>(a1);
      type value = simd::bitwise_cast<type>(a0);
-     return simd::bitwise_cast<A0>( vec_sr(value(), shift()) );  
+     return simd::bitwise_cast<A0>( vec_sra(value(), shift()) );
    }
   };
 } } }
