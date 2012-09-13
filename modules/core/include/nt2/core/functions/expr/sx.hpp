3ef742232587b60e7d3d6295ab5723112518c5e5
jtlap <jtlapreste@gmail.com>
2012-09-13 17:05:16 +0200

    now sx relies directly on expand_to and no more on bsx tsx

--- modules/core/include/nt2/core/functions/expr/sx.hpp
+++ modules/core/include/nt2/core/functions/expr/sx.hpp
@@ -11,8 +11,9 @@
 
 #include <nt2/core/functions/sx.hpp>
 #include <nt2/core/container/dsl.hpp>
-#include <nt2/include/functions/bsxfun.hpp>
-#include <nt2/include/functions/tsxfun.hpp>
+#include <nt2/include/functions/expand_to.hpp>
+#include <nt2/core/utility/max_extent.hpp>
+
 
 namespace nt2 { namespace ext
 {
@@ -23,10 +24,15 @@ namespace nt2 { namespace ext
                               (unspecified_<A2>)
                               )
   {
-    typedef typename meta::call<tag::bsxfun_(nt2::functor<A0>,const A1&,const A2&)>::type result_type; 
+    typedef typename details::max_extent<A1, A2>::type ext_t;
+    typedef typename meta::call<tag::expand_to_(const A1&,ext_t)>::type t1_t;
+    typedef typename meta::call<tag::expand_to_(const A2&,ext_t)>::type t2_t;
+    typedef typename meta::call<A0(t1_t, t2_t)>::type result_type;
+    
     BOOST_FORCEINLINE result_type operator()(A0 const&, A1 const& a1, A2 const& a2) const
     {
-      return bsxfun(nt2::functor<A0>(), a1, a2);
+     ext_t s = nt2::max_extent(a1, a2); 
+     return nt2::functor<A0>()(nt2::expand_to(a1, s), nt2::expand_to(a2, s));
     }
   };
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::sx_, tag::cpu_,
@@ -37,11 +43,16 @@ namespace nt2 { namespace ext
                               (unspecified_<A3>)
                               )
   {
-    typedef typename meta::call<tag::tsxfun_(nt2::functor<A0>,const A1&,const A2&,const A3&)>::type result_type; 
+    typedef typename details::max_extent<A1, A2, A3>::type ext_t;
+    typedef typename meta::call<tag::expand_to_(const A1&,ext_t)>::type t1_t;
+    typedef typename meta::call<tag::expand_to_(const A2&,ext_t)>::type t2_t;
+    typedef typename meta::call<tag::expand_to_(const A3&,ext_t)>::type t3_t;
+    typedef typename meta::call<A0(t1_t, t2_t, t3_t)>::type result_type;
     BOOST_FORCEINLINE result_type operator()(A0 const&, A1 const& a1,
                                              A2 const& a2, A3 const& a3) const
     {
-      return tsxfun(nt2::functor<A0>(), a1, a2, a3);
+     ext_t s = nt2::max_extent(a1, a2, a3); 
+     return nt2::functor<A0>()(nt2::expand_to(a1, s), nt2::expand_to(a2, s), nt2::expand_to(a3, s));
     }
   };
 } }
