af2b315fb4190e17d787a71d355969293951f74e
jtlap <jtlapreste@gmail.com>
2012-09-14 10:19:42 +0200

    bad include guard in max_extent.hpp

--- modules/core/include/nt2/core/utility/max_extent.hpp
+++ modules/core/include/nt2/core/utility/max_extent.hpp
@@ -6,8 +6,8 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef NT2_CORE_UTILITY_AS_INDEX_HPP_INCLUDED
-#define NT2_CORE_UTILITY_AS_INDEX_HPP_INCLUDED
+#ifndef NT2_CORE_UTILITY_MAX_EXTENT_HPP_INCLUDED
+#define NT2_CORE_UTILITY_MAX_EXTENT_HPP_INCLUDED
 
 #include <nt2/core/utility/of_size.hpp>
 #include <nt2/include/functions/extent.hpp>
