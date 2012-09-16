dbb759e3620476155e5abd0a357c1c45e2f1a9bb
Mathias Gaunard <mathias@gaunard.com>
2012-09-16 18:20:39 +0200

    make bsxfun and tsxfun use as_subscript/as_index instead of ind2sub/sub2ind

--- modules/core/include/nt2/core/functions/common/bsxfun.hpp
+++ modules/core/include/nt2/core/functions/common/bsxfun.hpp
@@ -12,12 +12,12 @@
 
 #include <nt2/core/functions/bsxfun.hpp>
 #include <nt2/include/functions/run.hpp>
-#include <nt2/include/functions/min.hpp>
-#include <nt2/include/functions/splat.hpp>
-#include <nt2/include/functions/ind2sub.hpp>
-#include <nt2/include/functions/sub2ind.hpp>
+#include <nt2/include/functions/simd/min.hpp>
+#include <nt2/include/functions/simd/splat.hpp>
+#include <nt2/include/functions/simd/enumerate.hpp>
+#include <nt2/core/utility/as_subscript.hpp>
+#include <nt2/core/utility/as_index.hpp>
 #include <nt2/sdk/meta/as_index.hpp>
-#include <nt2/include/functions/enumerate.hpp>
 
 namespace nt2 { namespace ext
 {
@@ -40,17 +40,13 @@ namespace nt2 { namespace ext
     typedef typename Data::type                                     result_type;
 
     typedef typename meta::as_index<result_type>::type                      i_t;
-    typedef typename meta::call < nt2::tag
-                                  ::enumerate_(State,meta::as_<i_t>)
-                                >::type                                     p_t;
-    typedef typename meta::call<nt2::tag::ind2sub_(ext_t,p_t)>::type        s_t;
-    typedef typename s_t::value_type                                        w_t;
+    typedef typename details::as_subscript<ext_t,i_t>::result_type          s_t;
 
     BOOST_FORCEINLINE result_type
     operator()(A0 const& a0, State const& p, Data const& t) const
     {
       // Grab position and size for each children
-      s_t pos0  = ind2sub(a0.extent(),enumerate<i_t>(p));
+      s_t pos0  = as_subscript(a0.extent(),enumerate<i_t>(p));
       ext_t sz0 = boost::proto::child_c<0>(a0).extent();
       ext_t sz1 = boost::proto::child_c<1>(a0).extent();
       s_t pos1  = pos0;
@@ -58,14 +54,14 @@ namespace nt2 { namespace ext
       // If you're a singleton, you're always the smallest
       for(std::size_t i = 0; i != ext_t::size(); ++i)
       {
-        pos0[i] = nt2::min(splat<w_t>(sz0[i]),pos0[i]);
-        pos1[i] = nt2::min(splat<w_t>(sz1[i]),pos1[i]);
+        pos0[i] = nt2::min(splat<i_t>(sz0[i]-1u),pos0[i]);
+        pos1[i] = nt2::min(splat<i_t>(sz1[i]-1u),pos1[i]);
       }
 
       // Call f on each value potential repeated
       return  boost::proto::value(boost::proto::child_c<2>(a0))
-              ( run(boost::proto::child_c<0>(a0), sub2ind(sz0, pos0), ad0_t())
-              , run(boost::proto::child_c<1>(a0), sub2ind(sz1, pos1), ad1_t())
+              ( run(boost::proto::child_c<0>(a0), as_index(sz0, pos0), ad0_t())
+              , run(boost::proto::child_c<1>(a0), as_index(sz1, pos1), ad1_t())
               );
     }
   };
