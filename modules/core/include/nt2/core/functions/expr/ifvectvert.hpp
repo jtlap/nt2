f8a3ac80b7545c76c72f0ed0dc24bd0dad101b42
Mathias Gaunard <mathias@gaunard.com>
2012-09-17 17:18:17 +0200

    fix ifvectvert size computation so that it works correctly when input is statically-sized, remove redundant code

--- modules/core/include/nt2/core/functions/expr/ifvectvert.hpp
+++ modules/core/include/nt2/core/functions/expr/ifvectvert.hpp
@@ -11,9 +11,10 @@
 #define NT2_CORE_FUNCTIONS_EXPR_IFVECTVERT_HPP_INCLUDED
 
 #include <nt2/core/functions/ifvectvert.hpp>
-#include <nt2/core/container/dsl.hpp>
 #include <nt2/include/functions/numel.hpp>
 #include <nt2/include/functions/isvector.hpp>
+#include <nt2/core/container/dsl.hpp>
+#include <nt2/core/utility/of_size/predef.hpp>
 
 namespace nt2 { namespace ext
 {
@@ -22,52 +23,23 @@ namespace nt2 { namespace ext
   struct size_of<nt2::tag::ifvectvert_,Domain,N,Expr>
   {
     typedef typename boost::proto::result_of::child_c<Expr&,0>::value_type  c0_t;
-    typedef typename c0_t::extent_type                               result_type;
+    // TODO: static size
+    typedef typename nt2::make_size<c0_t::extent_type::static_size>::type result_type;
 
     BOOST_FORCEINLINE result_type operator()(Expr& e) const
     {
       result_type siz;
-      siz[0] = nt2::numel(boost::proto::child_c<0>(e)); 
+      siz[0] = nt2::numel(boost::proto::child_c<0>(e));
       if(nt2::isvector(boost::proto::child_c<0>(e)))
         return siz;
       else
-        return extent(boost::proto::child_c<0>(e)); 
+        return extent(boost::proto::child_c<0>(e));
     }
   };
-
-  /// INTERNAL ONLY
-  template<class Domain, int N, class Expr>
-  struct  value_type<nt2::tag::ifvectvert_,Domain,N,Expr>
-        : meta::reshaping_value<Expr>
-  {};
 } }
 
 namespace nt2 { namespace ext
 {
-  /// INTERNAL ONLY
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::ifvectvert_, tag::cpu_
-                            , (A0)
-                            , (ast_<A0>)
-                            )
-  {
-    typedef typename meta::call<tag::numel_(A0&)>::type num;
-    typedef nt2::of_size_< mpl_value<num>::value > sz;
-
-    typedef typename  boost::proto::
-                      result_of::make_expr< nt2::tag::ifvectvert_
-                                          , container::domain
-                                          , A0&
-                                          >::type             result_type;
-
-    BOOST_FORCEINLINE result_type operator()(A0& a0) const
-    {
-      return  boost::proto::
-              make_expr < nt2::tag::ifvectvert_
-                        , container::domain
-                        > ( boost::ref(a0)  );
-    }
-  };
-
   /// INTERNAL ONLY
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::ifvectvert_, tag::cpu_
                             , (A0)
--- modules/core/unit/functions/ifvectvert.cpp
+++ modules/core/unit/functions/ifvectvert.cpp
@@ -17,27 +17,36 @@
 #include <nt2/sdk/unit/tests/basic.hpp>
 #include <nt2/sdk/unit/tests/exceptions.hpp>
 
-NT2_TEST_CASE_TPL( ifvectvert_scalar, NT2_TYPES )
+NT2_TEST_CASE_TPL( ifvectvert_scalar, (float) )
 {
   NT2_TEST_EQUAL(nt2::ifvectvert(T(1)), T(1));
 }
 
 
-NT2_TEST_CASE_TPL( ifvectvert,NT2_TYPES )
+NT2_TEST_CASE_TPL( ifvectvert, (float) )
 {
-  nt2::table<T> r;
+  nt2::table<T> r, r2;
   nt2::table<T, nt2::_2D> y( nt2::of_size(4,4) );
+  nt2::table<T, nt2::of_size_<4, 4> > y2;
+  nt2::table<T, nt2::of_size_<1, 16> > y3;
 
   for(int j=1;j<=4;j++)
     for(int i=1;i<=4;i++)
       y(i,j) = T(i + 10*j);
 
   r = nt2::ifvectvert(y);
-  NT2_TEST(nt2::isequal(y, r)); 
-  y =  nt2::reshape(y, 1, 16); 
+  r2 = nt2::ifvectvert(y2);
+  NT2_TEST(nt2::isequal(y, r));
+  NT2_TEST(nt2::isequal(y2, r2));
+
+  y = nt2::reshape(y, 1, 16);
+  y3 = nt2::reshape(y2, 1, 16);
   r = nt2::ifvectvert(y);
+  r2 = nt2::ifvectvert(y3);
   NT2_TEST(nt2::isequal(y(nt2::_), r));
+  NT2_TEST(nt2::isequal(y2(nt2::_), r2));
+
   y = y(nt2::_);
   r = nt2::ifvectvert(y);
-  NT2_TEST(nt2::isequal(y, r));  
+  NT2_TEST(nt2::isequal(y, r));
 }
