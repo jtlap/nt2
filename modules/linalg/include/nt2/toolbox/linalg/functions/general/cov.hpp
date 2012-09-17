ee08136e645a90eef34f62942a2a49b431828ca7
jtlap <jtlapreste@gmail.com>
2012-09-17 10:18:07 +0200

    cov is now working properly

--- modules/linalg/include/nt2/toolbox/linalg/functions/general/cov.hpp
+++ modules/linalg/include/nt2/toolbox/linalg/functions/general/cov.hpp
@@ -21,8 +21,11 @@
 #include <nt2/include/functions/conj.hpp>
 #include <nt2/include/functions/center.hpp>
 #include <nt2/include/functions/horzcat.hpp>
+#include <nt2/include/functions/ifvectvert.hpp>
 #include <nt2/include/constants/zero.hpp>
 #include <nt2/include/functions/zeros.hpp>
+
+//strange dispatching is to mimick matlab cov function
 
 namespace nt2 { namespace ext
 {
@@ -31,7 +34,8 @@ namespace nt2 { namespace ext
                               (ast_<A0>)(scalar_<integer_<A1> > )
                               )
   {
-    typedef typename meta::call<tag::center_(A0 const&, size_t)>::type T1; 
+    typedef typename meta::call<tag::ifvectvert_(A0 const&)>::type T0; 
+    typedef typename meta::call<tag::center_(T0, size_t)>::type T1; 
     typedef typename meta::call<tag::conj_(T1)>::type T2; 
     typedef typename meta::call<tag::trans_(T2)>::type T3;
     typedef typename meta::call<tag::mtimes_(T3, T1)>::type T4;
@@ -39,18 +43,10 @@ namespace nt2 { namespace ext
     typedef typename meta::call<tag::multiplies_(T4,value_type)>::type result_type; 
     BOOST_FORCEINLINE result_type operator()(A0 const& a0, A1 const & a1) const
     {
-      T1 a0_c = nt2::center(a0, size_t(1));
-      value_type n = nt2::size(a0, 1);
-      std::cout << "n " << n << std::endl;
-      NT2_DISPLAY(a0);
-      NT2_DISPLAY(nt2::mtimes(trans(conj(a0)), a0)); 
-      NT2_DISPLAY(a0_c); 
+      T0 a00 = nt2::ifvectvert(a0); 
+      T1 a0_c = nt2::center(a00, size_t(1));
+      value_type n = nt2::size(a00, 1);
       value_type f = (n <= One<value_type>())? One<value_type>() : nt2::rec(n-(a1?value_type(0):value_type(1)));
-      std::cout << "f " << f << std::endl; 
-      NT2_DISPLAY(trans(conj(a0_c)));
-      NT2_DISPLAY(nt2::mtimes(trans(conj(a0_c)), a0_c));
-      NT2_DISPLAY(nt2::multiplies(nt2::mtimes(trans(conj(a0_c)), a0_c), f));
-      std::cout << "why the return of cov (just following) is not identical to this (just above) result"<< std::endl; 
       return  nt2::multiplies(nt2::mtimes(trans(conj(a0_c)), a0_c), f); 
     }
 
@@ -61,7 +57,8 @@ namespace nt2 { namespace ext
                               (ast_<A0>)
                               )
   {
-    typedef typename meta::call<tag::center_(A0, size_t)>::type T1; 
+    typedef typename meta::call<tag::ifvectvert_(A0 const&)>::type T0; 
+    typedef typename meta::call<tag::center_(T0, size_t)>::type T1; 
     typedef typename meta::call<tag::conj_(T1)>::type T2; 
     typedef typename meta::call<tag::trans_(T2)>::type T3;
     typedef typename meta::call<tag::mtimes_(T3, T1)>::type T4;
@@ -69,8 +66,9 @@ namespace nt2 { namespace ext
     typedef typename meta::call<tag::multiplies_(T4, value_type)>::type result_type; 
     BOOST_FORCEINLINE result_type operator()(A0 const& a0) const
     {
-      T1 a0_c = nt2::center(a0, size_t(1));
-      value_type n = nt2::size(a0, 1); 
+      T0 a00 = nt2::ifvectvert(a0); 
+      T1 a0_c = nt2::center(a00, size_t(1));
+      value_type n = nt2::size(a00, 1); 
       value_type f = (n <= One<value_type>())? One<value_type>() : nt2::rec(n-1); 
       return  nt2::multiplies(nt2::mtimes(trans(conj(a0_c)), a0_c), f); 
     }   
@@ -81,17 +79,9 @@ namespace nt2 { namespace ext
                               (ast_<A0>)(ast_<A1>)
                               )
   {
-    typedef typename meta::call<tag::function_(A0 const&, tag::colon_)>::type T0;
-    typedef typename meta::call<tag::function_(A1 const&, tag::colon_)>::type T1;
-    typedef typename meta::call<tag::horzcat_(T0, T1)>::type H;
-    typedef typename meta::call<tag::cov_(H)>::type result_type; 
-    //     typedef typename A0::value_type value_type;
-    //     typedef typename A0::index_type index_type; 
-    //     typedef table<value_type, index_type> result_type; 
-    BOOST_FORCEINLINE result_type operator()(A0 const& a0, const A1& a1) const
-    {
-      return nt2::cov(horzcat(a0(_), a1(_))); 
-    }
+    BOOST_DISPATCH_RETURNS(2, (A0 const& a0, A1 const& a1),
+                           nt2::cov(nt2::horzcat(a0(nt2::_), a1(nt2::_)))
+                           )
   };
 
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::cov_, tag::cpu_,
@@ -99,13 +89,9 @@ namespace nt2 { namespace ext
                               (ast_<A0>)(ast_<A1>)(scalar_<integer_<A2> > )
                               )
   {
-    typedef typename A0::value_type value_type;
-    typedef typename A0::index_type index_type; 
-    typedef table<value_type, index_type> result_type; 
-    BOOST_FORCEINLINE result_type operator()(A0 const& a0, const A1& a1, const A2& a2) const
-    {
-      return nt2::cov(nt2::horzcat(a0(_), a1(_)), a2); 
-    }
+    BOOST_DISPATCH_RETURNS(3, (A0 const& a0, A1 const& a1, A2 const& a2),
+                           nt2::cov(nt2::horzcat(a0(nt2::_), a1(nt2::_)), a2)
+                           )
   };
 
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::cov_, tag::cpu_,
@@ -138,12 +124,9 @@ namespace nt2 { namespace ext
                               (scalar_<floating_<A1> >)
                               )
   {
-    typedef A0 value_type;
-    typedef table<value_type> result_type; 
-    BOOST_FORCEINLINE result_type operator()(A0 const&, A1 const &) const
-    {
-      return zeros(2, 2, meta::as_<A0>()); 
-    }
+    BOOST_DISPATCH_RETURNS(2, (A0 const& a0, A1 const& a1),
+                           nt2::zeros(2, 2, nt2::meta::as_<A0>())
+                           )
   };
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::cov_, tag::cpu_,
                               (A0)(A1)(A2),
@@ -151,13 +134,10 @@ namespace nt2 { namespace ext
                               (scalar_<floating_<A1> >)
                               (scalar_<integer_<A2> >)
                               )
-    {
-    typedef A0 value_type;
-    typedef table<value_type> result_type; 
-    BOOST_FORCEINLINE result_type operator()(A0 const&, A1 const &,  A2 const &) const
-    {
-      return zeros(2, 2, meta::as_<A0>()); 
-    }
+  {
+    BOOST_DISPATCH_RETURNS(3, (A0 const& a0, A1 const& a1, A2 const& a2),
+                           nt2::zeros(2, 2, nt2::meta::as_<A0>())
+                           )
   };
   
 } }
--- modules/linalg/unit/general/cov.cpp
+++ modules/linalg/unit/general/cov.cpp
@@ -10,24 +10,20 @@
  
 #include <nt2/table.hpp>
 #include <nt2/include/functions/cov.hpp>
-#include <nt2/include/functions/sum.hpp>
-#include <nt2/include/functions/center.hpp>
-#include <nt2/include/functions/sqr_abs.hpp>
-#include <nt2/include/functions/size.hpp>
 #include <nt2/include/functions/rif.hpp>
 #include <nt2/include/functions/reshape.hpp>
 #include <nt2/include/functions/var.hpp>
 #include <nt2/include/functions/zeros.hpp>
 #include <nt2/include/functions/isequal.hpp>
-#include <nt2/include/functions/sqr.hpp>
-#include <nt2/include/functions/trans.hpp>
-#include <nt2/include/functions/mtimes.hpp>
+#include <nt2/include/functions/isulpequal.hpp>  
+#include <nt2/include/functions/repnum.hpp>
+
 
 #include <nt2/sdk/unit/module.hpp> 
 #include <nt2/sdk/unit/tests/relation.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
 
-NT2_TEST_CASE_TPL( cov_scalar, (float)(double))//NT2_TYPES )
+NT2_TEST_CASE_TPL( cov_scalar, NT2_REAL_TYPES )
 {
   T x = nt2::cov(T(42));
   NT2_TEST_EQUAL( x, T(0) );
@@ -37,39 +33,63 @@ NT2_TEST_CASE_TPL( cov_scalar, (float)(double))//NT2_TYPES )
 
   nt2::table<T> y = nt2::cov(T(42),T(1));
   NT2_TEST( isequal(y, nt2::zeros(2, 2, nt2::meta::as_<T>())));
-
-
 }
 
-NT2_TEST_CASE_TPL( cov, (float)(double))//NT2_TYPES )
+NT2_TEST_CASE_TPL( cov, NT2_REAL_TYPES )
 {
   nt2::table<T> y;
   nt2::table<T> c, v; 
-  
-//   for(int i=1;i<=5;i++)
-//     y(i) = i;
-//  display("y", y);
-//   c = nt2::cov(y, y);
-//   display("c", c);
-
-  //  v = nt2::var(y);
-  //  NT2_TEST(nt2::isequal(c, v)); 
- 
   y = nt2::reshape(nt2::sqr(nt2::_(T(1), T(15))), 3, 5);
-//   display("y", y); 
-//   nt2::table<T> yc =  nt2::center(yc, 1);
-//   display("nt2::center(yc, 1)", yc); 
-//   nt2::table<T> tyc =  nt2::trans(yc);
-//   display("trans(yc) ", tyc); 
-//   nt2::table<T> z = nt2::mtimes(tyc, yc); 
-//   display("nt2::mtimes(tyc, yc)", z); 
-  
-// //   y = nt2::reshape(nt2::_(T(1), T(16)), 4, 4); 
-    display("y", y);
-    c = nt2::cov(y, 0);
-    display("cov", c);
-    c = nt2::cov(y, 1);
-    display("cov", c);
+  display("y", y);
+  c = nt2::cov(y, 0);
+  display("cov", c);
+  c = nt2::cov(y, 1);
+  display("cov", c);
+  y = nt2::reshape(nt2::_(T(1), T(15)), 3, 5);
+  display("y", y);
+  c = nt2::cov(y, 0);
+  display("cov", c);
+  NT2_TEST(nt2::isulpequal(c, nt2::repnum(T(1), 5, 5))); 
+  c = nt2::cov(y, 1);
+  display("cov", c);
+  NT2_TEST(nt2::isulpequal(c, nt2::repnum(T(2)/T(3), 5, 5))); 
 
+  
 }
 
+NT2_TEST_CASE_TPL( cov_2, NT2_REAL_TYPES )
+{
+  nt2::table<T> y;
+  nt2::table<T> c, v; 
+  y = nt2::reshape(nt2::_(T(1), T(15)), 3, 5);
+  display("y", y);
+  c = nt2::cov(y, y);
+  display("cov", c);
+  NT2_TEST(nt2::isulpequal(c, nt2::repnum(T(20), 2, 2))); 
+  c = nt2::cov(y, y, 1);
+  display("cov", c);
+  NT2_TEST(nt2::isulpequal(c, nt2::repnum(T(56)/T(3), 2, 2))); 
+
+  
+}
+NT2_TEST_CASE_TPL( cov_3, NT2_REAL_TYPES )
+{
+  nt2::table<T> y;
+  nt2::table<T> c, v; 
+  y = nt2::_(T(1), T(15));
+  display("y", y);
+  c = nt2::cov(y);
+  display("cov", c);
+  NT2_TEST(nt2::isulpequal(c, nt2::repnum(T(20), 1))); 
+  c = nt2::cov(y, 1);
+  display("cov", c);
+  NT2_TEST(nt2::isulpequal(c, nt2::repnum(T(56)/T(3), 1))); 
+  c = nt2::cov(y(nt2::_));
+  display("cov", c);
+  NT2_TEST(nt2::isulpequal(c, nt2::repnum(T(20), 1))); 
+  c = nt2::cov(y(nt2::_), 1);
+  display("cov", c);
+  NT2_TEST(nt2::isulpequal(c, nt2::repnum(T(56)/T(3), 1))); 
+
+  
+}
