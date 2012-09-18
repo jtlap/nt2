272e1f50703f0a1fa7dfa9ad41759d9a9e0a074b
Mathias Gaunard <mathias@gaunard.com>
2012-09-18 23:50:14 +0200

    restore isempty specialization removed in f8a3ac80b7545c76c72f0ed0dc24bd0dad101b42, needed by display

---
+++ modules/core/include/nt2/core/functions/common/isempty.hpp
@@ -0,0 +1,32 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2011 - 2012   MetaScale SAS
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_FUNCTIONS_COMMON_ISEMPTY_HPP_INCLUDED
+#define NT2_CORE_FUNCTIONS_COMMON_ISEMPTY_HPP_INCLUDED
+
+#include <nt2/core/functions/isempty.hpp>
+
+namespace nt2 { namespace ext
+{
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::isempty_, tag::cpu_
+                            , (A0)
+                            , (unspecified_<A0>)
+                            )
+  {
+    typedef bool result_type;
+
+    BOOST_DISPATCH_FORCE_INLINE
+    result_type operator()(const A0&) const
+    {
+      return false;
+    }
+  };
+} }
+
+#endif
