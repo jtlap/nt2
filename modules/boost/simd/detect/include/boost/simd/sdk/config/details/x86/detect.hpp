f759830ff42a06f6afc951694996d64f4b1dbd81
Pierre Esterie <pesterie@gmail.com>
2012-09-19 20:12:24 +0200

    Modifyng detect and fix #274

--- modules/boost/simd/detect/include/boost/simd/sdk/config/details/x86/detect.hpp
+++ modules/boost/simd/detect/include/boost/simd/sdk/config/details/x86/detect.hpp
@@ -19,13 +19,6 @@
  *\file detect.hpp
 */
 
-#define BOOST_SIMD_DECLARE_X86_DETECTION_CALL(x, y, z)                         \
-static const int bit = x, function = y, register_id = z;                       \
-int regs_x86[4] = {0x00000000,0x00000000,0x00000000,0x00000000};               \
-config::x86::cpuid(regs_x86, function);                                        \
-return has_bit_set(regs_x86[register_id-1], bit);                              \
-/**/
-
 namespace boost { namespace simd { namespace config { namespace details
 {
   inline bool has_bit_set(int value, int bit)
@@ -33,73 +26,92 @@ namespace boost { namespace simd { namespace config { namespace details
     return (value & (1<<bit)) != 0;
   }
 
+  inline bool x86_detection(int const& bit, int const& function, int const& register_id)
+  {
+    int regs_x86[4] = {0x00000000,0x00000000,0x00000000,0x00000000};
+    config::x86::cpuid(regs_x86, function);
+    return has_bit_set(regs_x86[register_id-1], bit);
+  }
+
   inline bool detect(tag::sse_ const&)
   {
     if(get_vendor() == intel || get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(25,0x00000001,4) }
+      return x86_detection(25,0x00000001,4);
     else return false;
   }
 
   inline bool detect(tag::sse2_ const&)
   {
     if(get_vendor() == intel || get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(26,0x00000001,4) }
+      return x86_detection(26,0x00000001,4); 
     else return false;
   }
 
   inline bool detect(tag::sse3_ const&)
   {
     if(get_vendor() == intel || get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(0,0x00000001,3) }
+      return x86_detection(0,0x00000001,3);
     else return false;
   }
 
   inline bool detect(tag::ssse3_ const&)
   {
     if(get_vendor() == intel || get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(9,0x00000001,3) }
+      return x86_detection(9,0x00000001,3);
     else return false;
   }
 
   inline bool detect(tag::sse4a_ const&)
   {
     if(get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(6,0x80000001,3); }
+      return x86_detection(6,0x80000001,3);
     else return false;
   }
 
   inline bool detect(tag::sse4_1_ const&)
   {
     if(get_vendor() == intel || get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(19,0x00000001,3) }
+      return x86_detection(19,0x00000001,3);
     else return false;
   }
 
   inline bool detect(tag::sse4_2_ const&)
   {
     if(get_vendor() == intel || get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(20,0x00000001,3) }
+      return x86_detection(20,0x00000001,3);
     else return false;
   }
 
   inline bool detect(tag::avx_ const&)
   {
     if(get_vendor() == intel || get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(28,0x00000001,3) }
+    { 
+      if(x86_detection(28,0x00000001,3) && x86_detection(27,0x00000001,3)) 
+        return true;
+      else return false;
+    }
     else return false;
   }
 
   inline bool detect(tag::fma4_ const&)
   {
     if(get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(16,0x80000001,3) }
+    { 
+      if(x86_detection(16,0x80000001,3) && x86_detection(27,0x00000001,3)) 
+        return true;
+      else return false;
+    }
     else return false;
   }
 
   inline bool detect(tag::xop_ const&)
   {
     if(get_vendor() == amd)
-    { BOOST_SIMD_DECLARE_X86_DETECTION_CALL(11,0x80000001,3) }
+    {
+      if(x86_detection(11,0x80000001,3) && x86_detection(27,0x00000001,3)) 
+        return true;
+      else return false;
+    }
     else return false;
   }
 
