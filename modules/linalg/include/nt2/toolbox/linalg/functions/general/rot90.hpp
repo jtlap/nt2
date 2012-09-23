8c89d7c18df86c29d9a8d26512cd24684eccf768
jtlap <jtlapreste@gmail.com>
2012-09-23 18:07:02 +0200

    rot90(a,n) now uses run_assign, but rot90(a) returns an expression

--- modules/linalg/include/nt2/toolbox/linalg/functions/general/rot90.hpp
+++ modules/linalg/include/nt2/toolbox/linalg/functions/general/rot90.hpp
@@ -12,11 +12,13 @@
 #include <nt2/include/functions/trans.hpp>
 #include <nt2/include/functions/flipud.hpp>
 #include <nt2/include/functions/fliplr.hpp>
+#include <nt2/core/container/table/table.hpp>
+#include <nt2/include/functions/assign.hpp>
 #include <nt2/table.hpp>
 
 namespace nt2{ namespace ext 
 {
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::rot90_, tag::cpu_,
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::rot90_0_, tag::cpu_,
                                      (A0)(A1), 
                                      ((scalar_<arithmetic_<A0> > ))
                                      (scalar_<integer_<A1> > )
@@ -29,7 +31,7 @@ namespace nt2{ namespace ext
       }
   };
 
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::rot90_, tag::cpu_,
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::rot90_0_, tag::cpu_,
                                      (A0), 
                                      ((scalar_<arithmetic_<A0> > ))
                                      )
@@ -41,25 +43,7 @@ namespace nt2{ namespace ext
       }
   };
 
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::rot90_, tag::cpu_,
-                              (A0)(A1), 
-                              ((ast_<A0>))
-                              (scalar_<integer_<A1> > )
-                              )
-  {
-    typedef typename A0::value_type value_type;
-    typedef table<value_type> result_type; 
-    NT2_FUNCTOR_CALL(2)
-      {
-        int k = ((a1%4)+4)%4; 
-        if(k == 1)  return nt2::fliplr(nt2::trans(a0));
-        if(k == 2)  return nt2::fliplr(nt2::flipud(a0));
-        if(k == 3)  return nt2::trans(nt2::flipud(a0));
-        return a0;
-      }
-  };
-  
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::rot90_, tag::cpu_,
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::rot90_0_, tag::cpu_,
                               (A0), 
                               ((ast_<A0>))
                               )
@@ -67,7 +51,29 @@ namespace nt2{ namespace ext
     BOOST_DISPATCH_RETURNS(1, (A0 const& a0),
                            (nt2::fliplr(nt2::trans(a0)))
                            )
-      };
+  };
+
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_assign_, tag::cpu_
+                              , (A0)(A1)(N)
+                              , (ast_<A0>)
+                                ((node_<A1, nt2::tag::rot90_, N> ))
+                            )
+  {
+    typedef typename boost::proto::result_of::child_c<A1&,0>::type       Out0;
+    typedef A0&                                                   result_type;
+
+    result_type operator()(A0& out, const A1& in) const
+    {
+      out.resize(in.extent());
+      int k = ((boost::proto::child_c<1>(in)%4)+4)%4;
+      if(k == 1)       out = nt2::fliplr(nt2::trans(boost::proto::child_c<0>(in)));
+      else if(k == 2)  out = nt2::fliplr(nt2::flipud(boost::proto::child_c<0>(in)));
+      else if(k == 3)  out = nt2::trans(nt2::flipud(boost::proto::child_c<0>(in)));
+      else out = boost::proto::child_c<0>(in); 
+      return out; 
+    }
+  }; 
+
 } }
 
 
