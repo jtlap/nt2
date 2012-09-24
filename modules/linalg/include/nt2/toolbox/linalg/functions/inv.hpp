f922c9668050038d15fbb4afef1645d17f7c1d54
jtlap <jtlapreste@gmail.com>
2012-09-24 10:45:55 +0200

    inv is now run_assign

--- modules/linalg/include/nt2/toolbox/linalg/functions/inv.hpp
+++ modules/linalg/include/nt2/toolbox/linalg/functions/inv.hpp
@@ -12,32 +12,20 @@
 #include <boost/simd/include/simd.hpp>
 #include <boost/dispatch/include/functor.hpp>
 #include <nt2/sdk/memory/container.hpp>
+#include <nt2/sdk/meta/size_as.hpp>
+#include <nt2/sdk/meta/value_as.hpp>
+#include <nt2/core/container/dsl/value_type.hpp>
+#include <nt2/core/container/dsl/size.hpp>
+#include <nt2/include/functions/issquare.hpp>
 
 /*!
- * \ingroup algebra
- * \defgroup algebra_inv inv
+ * @brief inv  rotate matrix 90 degrees.
+ * inv(x) is the inverse of the square matrix expression x.
  *
- * \par Description
- * Elementary Least square
- *
- * \par Header file
- * 
- * \code
- * #include <nt2/include/functions/inv.hpp>
- * \endcode
- * 
- * 
- * \synopsis
- *
- * \param a the matrix or vector expression a 
- *
- * \param type of inv required
- * 
- * \par Notes
- *   Call the dedicated lapack routines available on the target.
- * \par
- *  
-**/
+ * Avoid to use this function if you do not have a very good reason
+ * and whatsoever never use it to solve linear systems.
+ * 99% of the time a solver is a better choice.
+ **/
 
 namespace nt2 { namespace tag
   {         
@@ -45,12 +33,35 @@ namespace nt2 { namespace tag
      * \brief Define the tag inv_ of functor inv
      *        in namespace nt2::tag for toolbox algebra
     **/
-    struct inv_ :  tag::formal_ { typedef tag::formal_ parent; };
+    struct inv_ :  ext::unspecified_<inv_> { typedef ext::unspecified_<inv_> parent; };
   }
   
   BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::inv_, inv, 1)
-
+  BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::inv_, inv, 2)
 }
+
+namespace nt2 { namespace ext
+{
+  template<class Domain, class Expr,  int N> //N == 1 only
+  struct  size_of<tag::inv_, Domain, N, Expr> 
+  {
+    typedef typename boost::proto::result_of::child_c<Expr&,0>::value_type  c0_t;
+    typedef typename c0_t::extent_type                               result_type;
+    BOOST_FORCEINLINE result_type operator()(Expr& e) const
+    {
+      result_type sizee = boost::proto::child_c<0>(e).extent();
+      BOOST_ASSERT_MSG(issquare(boost::proto::child_c<0>(e)),
+                       "inverse of non square matrix expression requested");
+      return sizee; 
+    }
+  };
+
+ template <class Domain, class Expr,  int N>
+ struct value_type < tag::inv_, Domain,N,Expr>
+  : meta::value_as<Expr,0>
+ {};
+} }
+
 
 #endif
 
--- modules/linalg/unit/general/inv.cpp
+++ modules/linalg/unit/general/inv.cpp
@@ -13,12 +13,15 @@
 #include <nt2/include/functions/eye.hpp>
 #include <nt2/include/constants/one.hpp>
 #include <nt2/include/constants/ten.hpp>
-
+#include <nt2/include/constants/eps.hpp>
+#include <nt2/include/constants/two.hpp>
+#include <nt2/include/functions/rec.hpp>
 #include <nt2/include/functions/ulpdist.hpp>
 #include <nt2/include/functions/isulpequal.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/exceptions.hpp>
+
 
 NT2_TEST_CASE_TPL(inv, NT2_REAL_TYPES)
 {
@@ -30,8 +33,41 @@ NT2_TEST_CASE_TPL(inv, NT2_REAL_TYPES)
   NT2_DISPLAY(n);
   NT2_DISPLAY(invn);
   NT2_TEST(nt2::isulpequal(invn, n, 0.5));
-  for(int i=1; i <= 100; i++)
-    {
-      NT2_TEST_ULP_EQUAL(invn(i),n(i), 0.5);
-    }
+}
+
+NT2_TEST_CASE_TPL(inv_1, NT2_REAL_TYPES)
+{
+  typedef T r_t;
+  using nt2::inv;
+  using nt2::tag::inv_;
+  nt2::table<T> n = nt2::eye(10, 10, nt2::meta::as_<T>()), nn;
+  nn = n; 
+  n(1, 1) = nt2::Eps<T>()*nt2::Half<T>();
+  nn(1, 1) = nt2::rec(n(1, 1)); 
+  nt2::table<T> invn = nt2::inv(n);
+  NT2_DISPLAY(n);
+  NT2_DISPLAY(invn);
+  NT2_TEST(nt2::isulpequal(invn, nn, 0.5));
+ }
+NT2_TEST_CASE_TPL(inv_2, NT2_REAL_TYPES)
+{
+  typedef T r_t;
+  using nt2::inv;
+  using nt2::tag::inv_;
+  nt2::table<T> n = nt2::eye(10, 8, nt2::meta::as_<T>()), nn;
+  NT2_TEST_ASSERT(nt2::inv(n));
+ }
+NT2_TEST_CASE_TPL(inv_nowarn, NT2_REAL_TYPES)
+{
+  typedef T r_t;
+  using nt2::inv;
+  using nt2::tag::inv_;
+  nt2::table<T> n = nt2::eye(10, 10, nt2::meta::as_<T>()), nn;
+  nn = n; 
+  n(1, 1) = nt2::Eps<T>()*nt2::Half<T>();
+  nn(1, 1) = nt2::rec(n(1, 1)); 
+  nt2::table<T> invn = nt2::inv(n, false);
+  NT2_DISPLAY(n);
+  NT2_DISPLAY(invn);
+  NT2_TEST(nt2::isulpequal(invn, nn, 0.5));
  }
