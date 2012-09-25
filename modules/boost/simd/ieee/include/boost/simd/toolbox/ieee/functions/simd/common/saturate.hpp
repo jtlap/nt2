aad875ed1483dd639e5720c37c6723dc4895630a
Mathias Gaunard <mathias@gaunard.com>
2012-09-25 20:45:51 +0200

    saturate float/something-else simd common maps scalar version

--- modules/boost/simd/ieee/include/boost/simd/toolbox/ieee/functions/simd/common/saturate.hpp
+++ modules/boost/simd/ieee/include/boost/simd/toolbox/ieee/functions/simd/common/saturate.hpp
@@ -14,11 +14,31 @@
 #include <boost/simd/include/functions/simd/is_less.hpp>
 #include <boost/simd/include/functions/simd/if_else.hpp>
 #include <boost/simd/include/functions/simd/splat.hpp>
+#include <boost/simd/include/functions/simd/insert.hpp>
+#include <boost/simd/include/functions/simd/extract.hpp>
 #include <boost/simd/include/constants/valmax.hpp>
 #include <boost/simd/include/constants/valmin.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::saturate_, tag::cpu_
+                                    , (A0)(T)(X)
+                                    , ((simd_< arithmetic_<A0>,X>))
+                                      (target_< generic_<arithmetic_<T> > >)
+                                    )
+  {
+    typedef A0 result_type;
+    typedef typename meta::scalar_of<T>::type target_t;
+
+    BOOST_FORCEINLINE result_type operator()(A0 const& a0, T const& ) const
+    {
+      result_type that;
+      for(std::size_t i=0; i!=result_type::static_size; ++i)
+        that[i] = saturate<target_t>(a0[i]);
+      return that;
+    }
+  };
+
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::saturate_, tag::cpu_
                                     , (A0)(T)(X)
                                     , ((simd_<int_<A0>,X>))
