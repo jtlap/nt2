51e12249c743227696b0744aa64e0f7e266c7a5a
Mathias Gaunard <mathias@gaunard.com>
2012-09-25 18:51:01 +0200

    add floating/xxx version of scalar saturate + make common group saturate like other group specializations

--- modules/boost/simd/swar/include/boost/simd/toolbox/swar/functions/simd/common/group.hpp
+++ modules/boost/simd/swar/include/boost/simd/toolbox/swar/functions/simd/common/group.hpp
@@ -1,16 +1,17 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_SWAR_FUNCTIONS_SIMD_COMMON_GROUP_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_SWAR_FUNCTIONS_SIMD_COMMON_GROUP_HPP_INCLUDED
 
 #include <boost/simd/toolbox/swar/functions/group.hpp>
 #include <boost/simd/include/functions/simd/load.hpp>
+#include <boost/simd/include/functions/scalar/saturate.hpp>
 #include <boost/simd/sdk/memory/aligned_type.hpp>
 #include <boost/simd/sdk/meta/cardinal_of.hpp>
 #include <boost/simd/sdk/meta/scalar_of.hpp>
@@ -31,14 +32,14 @@ namespace boost { namespace simd { namespace ext
   {
 
     typedef typename dispatch::meta::downgrade<A0>::type result_type;
-    
+
     BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
     {
       static const size_t size = boost::simd::meta::cardinal_of<A0>::value;
       typedef typename meta::scalar_of<result_type>::type sR;
       BOOST_SIMD_ALIGNED_TYPE(sR) tmp[size*2];
-      for(size_t i = 0; i != size; ++i)  tmp[i]      = static_cast<sR>(a0[i]);
-      for(size_t i = 0; i != size; ++i)  tmp[i+size] = static_cast<sR>(a1[i]);
+      for(size_t i = 0; i != size; ++i)  tmp[i]      = static_cast<sR>(saturate<sR>(a0[i]));
+      for(size_t i = 0; i != size; ++i)  tmp[i+size] = static_cast<sR>(saturate<sR>(a1[i]));
       return load<result_type>(&tmp[0], 0);
     }
   };
