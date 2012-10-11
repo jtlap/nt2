4edc20fe1b553126096961429967c813836eb7ed
Joel Falcou <joel.falcou@lri.fr>
2012-10-11 15:07:51 +0200

    Fixed improper use of & instead of && in is_not_equal_with_equal_nans

--- modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/scalar/is_not_equal_with_equal_nans.hpp
+++ modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/scalar/is_not_equal_with_equal_nans.hpp
@@ -1,10 +1,10 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_SCALAR_IS_NOT_EQUAL_WITH_EQUAL_NANS_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_SCALAR_IS_NOT_EQUAL_WITH_EQUAL_NANS_HPP_INCLUDED
@@ -27,9 +27,10 @@ namespace boost { namespace simd { namespace ext
     }
   };
 
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_not_equal_with_equal_nans_
-                                   , tag::cpu_, (A0)
-                                   , (scalar_< floating_<A0> >)(scalar_< floating_<A0> >)
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::is_not_equal_with_equal_nans_
+                                    , tag::cpu_, (A0)
+                                    , (scalar_< floating_<A0> >)
+                                      (scalar_< floating_<A0> >)
                                    )
   {
 
@@ -37,7 +38,7 @@ namespace boost { namespace simd { namespace ext
 
     BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
     {
-      return   result_type((a0 != a1) & !(boost::simd::is_nan(a0) & boost::simd::is_nan(a1)));
+      return result_type(a0 != a1) && !(boost::simd::is_nan(a0) && boost::simd::is_nan(a1));
     }
   };
 } } }
