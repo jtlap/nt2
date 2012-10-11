04e63c63d7d9ed7d6126b342946bb7ba54e50228
jtlap <jtlapreste@gmail.com>
2012-10-11 23:31:50 +0200

    pow2 for complex

---
+++ modules/type/complex/exponential/unit/scalar/pow2.cpp
@@ -0,0 +1,61 @@
+//////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+//////////////////////////////////////////////////////////////////////////////
+#define NT2_UNIT_MODULE "nt2 complex.exponential toolbox - pow2/scalar Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// unit test behavior of exponential components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+/// created by jt the 08/12/2010
+/// 
+#include <nt2/include/functions/pow2.hpp>
+#include <nt2/include/functions/ulpdist.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/as_floating.hpp>
+#include <nt2/sdk/meta/as_signed.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+#include <nt2/sdk/meta/downgrade.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
+#include <boost/dispatch/meta/as_floating.hpp>
+#include <boost/type_traits/common_type.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/memory/buffer.hpp>
+#include <nt2/toolbox/constant/constant.hpp>
+
+
+NT2_TEST_CASE_TPL ( pow2_real__1_0,  NT2_REAL_TYPES)
+{
+  
+  using nt2::pow2;
+  using nt2::tag::pow2_;
+  typedef std::complex<T> cT; 
+  typedef typename nt2::meta::as_integer<T>::type iT;
+  typedef typename nt2::meta::call<pow2_(cT)>::type r_t;
+  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
+  typedef typename nt2::meta::upgrade<T>::type u_t;
+  typedef typename nt2:: meta::as_complex<T>::type wished_r_t;
+  typedef typename nt2:: meta::as_dry<T>::type dT; 
+
+  // return type conformity test 
+  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
+  std::cout << std::endl; 
+  double ulpd;
+  ulpd=0.0;
+
+  // specific values tests
+  NT2_TEST_ULP_EQUAL(pow2(cT(nt2::Inf<T>())), cT(nt2::Inf<T>()), 0);
+  NT2_TEST_EQUAL(pow2(cT(nt2::Minf<T>())), cT(nt2::Zero<T>()));
+  NT2_TEST_EQUAL(pow2(cT(nt2::Mone<T>())), cT(nt2::Half<T>()));
+  NT2_TEST_EQUAL(pow2(cT(nt2::Nan<T>())), cT(nt2::Nan<T>()));
+  NT2_TEST_EQUAL(pow2(cT(nt2::One<T>())), cT(nt2::Two<T>()));
+  NT2_TEST_EQUAL(pow2(cT(nt2::Zero<T>())), cT(nt2::One<T>()));
+  NT2_TEST_EQUAL(pow2(cT(nt2::One<T>(), nt2::One<T>()), iT(1)), dT(2)); 
+} // end of test for floating_
