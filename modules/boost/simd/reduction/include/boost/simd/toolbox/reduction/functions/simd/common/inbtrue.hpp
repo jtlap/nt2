a80ae0143ed6674a3d30b1cb3d9f2dd82d1f9f29
Joel Falcou <joel.falcou@lri.fr>
2012-10-14 15:27:15 +0200

    inbture correctives for issue #304 - Fix hyperbolic errors

--- modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/simd/common/inbtrue.hpp
+++ modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/simd/common/inbtrue.hpp
@@ -1,10 +1,10 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_COMMON_INBTRUE_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_COMMON_INBTRUE_HPP_INCLUDED
@@ -14,10 +14,10 @@
 
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::inbtrue_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<fundamental_<A0>,X>))
-                            )
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::inbtrue_, tag::cpu_
+                                    , (A0)(X)
+                                    , ((simd_<fundamental_<A0>,X>))
+                                    )
   {
     typedef std::size_t result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
@@ -25,16 +25,17 @@ namespace boost { namespace simd { namespace ext
       result_type z = a0[0] != 0;
       for(size_t i = 1; i< boost::simd::meta::cardinal_of<A0>::value; ++i)
       {
-          z += a0[i] != 0;
+        z += bool(a0[i] != 0);
       }
       return z;
     }
   };
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::inbtrue_, tag::cpu_
-                                     , (A0)(A1)(X)
-                                     , ((simd_<fundamental_<A0>,X>))
-                                     (scalar_< integer_<A1> > )
-                                     )
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::inbtrue_, tag::cpu_
+                                    , (A0)(A1)(X)
+                                    , ((simd_<fundamental_<A0>,X>))
+                                      (scalar_< integer_<A1> > )
+                                    )
   {
     typedef std::size_t result_type;
     inline result_type operator()(A0 const & a0, A1 const &) const
@@ -42,7 +43,7 @@ namespace boost { namespace simd { namespace ext
       result_type z = a0[0] != 0;
       for(size_t i = 1; i< boost::simd::meta::cardinal_of<A0>::value; ++i)
       {
-          z += a0[i] != 0;
+        z += bool(a0[i] != 0);
       }
       return z;
     }
