d7502330d32e70f22d6a5c86f3ddc3305172d5ec
Mathias Gaunard <mathias@gaunard.com>
2012-10-16 17:42:29 +0200

    add specialization to vector_of for fusion sequences, merge some code with make_soa

--- modules/boost/simd/sdk/include/boost/simd/sdk/simd/meta/vector_of.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/simd/meta/vector_of.hpp
@@ -12,8 +12,12 @@
 #include <boost/simd/sdk/simd/extensions.hpp>
 #include <boost/simd/sdk/simd/native_fwd.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/dispatch/meta/strip.hpp>
+#include <boost/fusion/include/fold.hpp>
 #include <boost/mpl/sizeof.hpp>
-#include <boost/array.hpp>
+#include <boost/mpl/greater.hpp>
+#include <boost/mpl/if.hpp>
+#include <boost/mpl/apply.hpp>
 
 /* We use boost::mpl::sizeof_ rather than sizeof because MSVC has trouble
  * with sizeof of dependent names in SFINAE contexts */
@@ -36,12 +40,52 @@ struct vector_of< T ,Card                                               \
 
 namespace boost { namespace simd { namespace details
 {
-  template<class T,std::size_t Card, class Enable=void>
+  template<class F>
+  struct max_value_impl
+  {
+    template<class Sig>
+    struct result;
+
+    template<class This, class T, class U>
+    struct result<This(T, U)>
+    {
+      typedef typename dispatch::meta::strip<T>::type sT;
+      typedef typename mpl::apply1<F, sT>::type vT;
+      typedef typename dispatch::meta::strip<T>::type sU;
+      typedef typename mpl::apply1<F, sT>::type vU;
+      typedef typename mpl::if_< mpl::greater<vU, vT>
+                               , sU
+                               , sT
+                               >::type type;
+    };
+  };
+
+  template<class T, class F>
+  struct max_value
+       : fusion::result_of::
+         fold< T
+             , typename fusion::result_of::
+               value_of< typename fusion::result_of::begin<T>::type
+                       >::type
+             , max_value_impl<F>
+             >
+  {
+  };
+
+  template<class T, std::size_t Card, class Enable=void>
   struct vector_of
   {
     typedef simd::native<T, tag::simd_emulation_< sizeof(T)*Card > > type;
   };
-  
+
+  template<class T, std::size_t Card>
+  struct vector_of<T, Card, typename boost::enable_if< fusion::traits::is_sequence<T> >::type>
+  {
+    typedef typename max_value<T, mpl::sizeof_<mpl::_> >::type mT;
+    typedef typename vector_of<mT, Card>::type nT;
+    typedef simd::native<T, typename nT::extension_type> type;
+  };
+
   #ifdef BOOST_SIMD_DETECTED
   BOOST_PP_SEQ_FOR_EACH(BOOST_SIMD_LOCAL,~,BOOST_SIMD_TAG_SEQ)
 #endif
