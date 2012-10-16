591ce104213f58cad8f18efb1986ad62574b83cd
Mathias Gaunard <mathias@gaunard.com>
2012-10-16 10:45:51 +0200

    remove redundant code in logical operators, fix includes

--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/logical_and.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/logical_and.hpp
@@ -1,11 +1,10 @@
-
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_COMMON_LOGICAL_AND_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_COMMON_LOGICAL_AND_HPP_INCLUDED
@@ -15,7 +14,6 @@
 #include <boost/simd/include/functions/simd/genmask.hpp>
 #include <boost/simd/include/functions/simd/mask2logical.hpp>
 #include <boost/simd/sdk/simd/logical.hpp>
-#include <boost/assert.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -23,19 +21,6 @@ namespace boost { namespace simd { namespace ext
                             , (A0)(A1)(X)
                             , ((simd_<fundamental_<A0>,X>))
                               ((simd_<fundamental_<A1>,X>))
-                            )
-  {
-    typedef typename meta::as_logical<A0>::type result_type;
-    BOOST_SIMD_FUNCTOR_CALL(2)
-    {
-      return mask2logical(b_and(genmask(a0), genmask(a1)));
-    }
-  };
-  
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::logical_and_, tag::cpu_
-                            , (A0)(A1)(X)
-                            , ((simd_<logical_<A0>,X>))
-                              ((simd_<logical_<A1>,X>))
                             )
   {
     typedef typename meta::as_logical<A0>::type result_type;
--- modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/logical_or.hpp
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/common/logical_or.hpp
@@ -1,10 +1,10 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_COMMON_LOGICAL_OR_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_COMMON_LOGICAL_OR_HPP_INCLUDED
@@ -21,18 +21,6 @@ namespace boost { namespace simd { namespace ext
                             , (A0)(A1)(X)
                             , ((simd_<fundamental_<A0>,X>))
                               ((simd_<fundamental_<A1>,X>))
-                            )
-  {
-    typedef typename meta::as_logical<A0>::type result_type;
-    BOOST_SIMD_FUNCTOR_CALL(2)
-    {
-      return mask2logical(b_or(genmask(a0), genmask(a1)));
-    }
-  };
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::logical_or_, tag::cpu_
-                            , (A0)(A1)(X)
-                            , ((simd_<logical_<A0>,X>))
-                              ((simd_<logical_<A1>,X>))
                             )
   {
     typedef typename meta::as_logical<A0>::type result_type;
