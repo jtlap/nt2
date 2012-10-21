5709c7c38315daea680f0a59f631b50555766139
jtlap <jtlapreste@gmail.com>
2012-10-21 16:18:49 +0200

    find.cpp

--- modules/core/utility/unit/functions/find.cpp
+++ modules/core/utility/unit/functions/find.cpp
@@ -11,8 +11,10 @@
 #include <nt2/table.hpp>
 #include <nt2/include/functions/of_size.hpp>
 #include <nt2/include/functions/find.hpp>
+#include <nt2/include/functions/globalfind.hpp>
 #include <nt2/include/functions/is_nez.hpp>
 #include <nt2/include/functions/isequal.hpp>
+#include <nt2/include/constants/one.hpp>
  
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
@@ -40,15 +42,27 @@ NT2_TEST_CASE( find )
             T(0),    T(57),   T(0)
           };
 
+  U out[]={ U(1),             U(3),
+            U(4),    U(5),    U(6),
+            U(7),             U(9),
+            U(10),   U(11),
+            U(13),   U(14),   U(15),
+                              U(18),
+            U(19),   U(20),   U(21),
+            U(22),   U(23),   U(24),
+            U(25),            U(27),
+                     U(29)
+          };
   U bo[] = { 1, 3, 4, 5};
   U co[] = { 29, 27, 25, 24}; 
   table<T, nt2::shared_> in2(of_size(3, 5, 2), nt2::share(&in[0], in + sizeof in/sizeof *in));
+  table<U, nt2::shared_> c(of_size(sizeof out/sizeof *out), nt2::share(&out[0], out + sizeof out/sizeof *out));
   table<U, nt2::shared_> bo2(of_size(4, 1), nt2::share(&bo[0], bo + sizeof bo/sizeof *bo));
   table<U, nt2::shared_> co2(of_size(4, 1), nt2::share(&co[0], co + sizeof co/sizeof *co));
 
   table<T> a = in2, v;
   table<U> b = find(a), e, f;
-  NT2_TEST(isequal(b, in2)); 
+  NT2_TEST(isequal(b, c)); 
   NT2_DISPLAY(b);
   b =  find(a, 4);
   NT2_TEST(isequal(b, bo2)); 
@@ -65,5 +79,6 @@ NT2_TEST_CASE( find )
    NT2_DISPLAY(e); 
    NT2_DISPLAY(f);
    NT2_DISPLAY(v);
-
+   b =  globalfind(a)+nt2::One<U>();
+   NT2_DISPLAY(b);
 }
