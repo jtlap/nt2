93f65a1fdd6b9b638a4889166d9d1d09dca52685
Mathias Gaunard <mathias@gaunard.com>
2012-11-07 23:45:59 +0100

    sb2b fix

--- modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/scalar/sb2b.hpp
+++ modules/boost/simd/predicates/include/boost/simd/toolbox/predicates/functions/scalar/sb2b.hpp
@@ -1,10 +1,10 @@
 //==============================================================================
-//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II         
-//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI         
-//                                                                              
-//          Distributed under the Boost Software License, Version 1.0.          
-//                 See accompanying file LICENSE.txt or copy at                 
-//                     http://www.boost.org/LICENSE_1_0.txt                     
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #ifndef BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_SCALAR_SB2B_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_PREDICATES_FUNCTIONS_SCALAR_SB2B_HPP_INCLUDED
@@ -42,10 +42,10 @@ namespace boost { namespace simd { namespace ext
 
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return result_type(a0!= boost::simd::False<result_type>());
+      return result_type(a0!= boost::simd::False<A0>());
     }
   };
-  
+
 } } }
 
 
