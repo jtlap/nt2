6e257f2d372310bd17f93cd120764487931c1ec9
jtlap <jtlapreste@gmail.com>
2012-11-09 10:49:52 +0100

    polyfit works with complex

--- modules/core/linalg/unit/solvers/chol_result_solve.cpp
+++ modules/core/linalg/unit/solvers/chol_result_solve.cpp
@@ -43,3 +43,24 @@ NT2_TEST_CASE_TPL(chol_result, NT2_REAL_TYPES)
   std::cout << nt2::globalmax(nt2::ulpdist(bb, mtimes(aa, f.x()))) << std::endl; ; 
   NT2_TEST(nt2::isulpequal(bb, mtimes(aa, f.x())));   
 }
+NT2_TEST_CASE_TPL(chol_resultc, NT2_REAL_TYPES)
+{
+  std::cout << std::setprecision(20); 
+  typedef std::complex<T> cT;
+  typedef nt2::table<cT> ct_t;
+  ct_t a =       nt2::ones (4, 4, nt2::meta::as_<cT>())
+        + T(10)*nt2::eye  (4, 4, nt2::meta::as_<cT>());
+  ct_t b = nt2::ones(4, 1, nt2::meta::as_<cT>());
+  ct_t aa = a;
+  ct_t bb = b; 
+  nt2::display("a     ", a);
+  nt2::display("b     ", b);
+  nt2::details::chol_solve_result<ct_t> f(a, b, 'L');
+
+  nt2::display("x", f.x());
+  NT2_DISPLAY(a);
+  NT2_DISPLAY(b);
+  NT2_DISPLAY(mtimes(aa, f.x()));
+  std::cout << nt2::globalmax(nt2::ulpdist(bb, mtimes(aa, f.x()))) << std::endl; ; 
+  NT2_TEST(nt2::isulpequal(bb, mtimes(aa, f.x())));   
+}
--- modules/core/polynom/unit/scalar/polyfit.cpp
+++ modules/core/polynom/unit/scalar/polyfit.cpp
@@ -123,3 +123,25 @@ NT2_TEST_CASE_TPL ( plevl_real__3_0,  NT2_REAL_TYPES)
 } // end of test for floating_
 
 
+NT2_TEST_CASE_TPL ( plevl_real__4_0,  NT2_REAL_TYPES)
+{
+  
+  using nt2::polyfit;
+  using nt2::tag::polyfit_;
+  typedef std::complex<T> cT; 
+  cT a [] = {0.0, 1.0, 2.0, 3.0,  4.0,  5.0};
+  cT b [] = {0.0, 0.8, 0.9, 0.1, -0.8, -1.0};
+  nt2::table<cT> x(nt2::of_size(1,6));
+  nt2::table<cT> y(nt2::of_size(1,6));
+  
+ for(int i=0; i < 6; ++i)
+   {
+     x(i+1) = cT(a[i]);
+     y(i+1) = cT(b[i]); 
+   }
+  NT2_DISPLAY(x);
+  NT2_DISPLAY(y);
+  nt2::table<cT> z = polyfit(x, y, 3);
+   NT2_DISPLAY(z);  
+} // end of test for floating_
+
