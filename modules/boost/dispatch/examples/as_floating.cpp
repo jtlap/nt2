5e8a27010ab1ee750df1ffb667d9cf568018d2ae
Joel Falcou <joel.falcou@lri.fr>
2012-11-12 16:48:29 +0100

    Fixed as_floatign examples to not use unsigned long

--- modules/boost/dispatch/examples/as_floating.cpp
+++ modules/boost/dispatch/examples/as_floating.cpp
@@ -1,12 +1,14 @@
 #include <boost/mpl/assert.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <boost/dispatch/meta/as_floating.hpp>
+#include <boost/dispatch/meta/make_integer.hpp>
 
 using boost::is_same;
+using boost::dispatch::meta::make_integer;
 
 int main()
 {
   BOOST_MPL_ASSERT(( is_same<float  , boost::dispatch::meta::as_floating<int>::type>  ));
   BOOST_MPL_ASSERT(( is_same<float  , boost::dispatch::meta::as_floating<unsigned int>::type> ));
-  BOOST_MPL_ASSERT(( is_same<double , boost::dispatch::meta::as_floating<unsigned long >::type> ));
+  BOOST_MPL_ASSERT(( is_same<double , boost::dispatch::meta::as_floating<make_integer<8,unsigned>::type>::type> ));
 }
