fa739a24aff0c1b1a7689ee6c14280ccf2432864
jtlap <jtlapreste@gmail.com>
2012-11-14 15:58:51 +0100

    splitting reduction tests real/complex

--- modules/type/complex/reduction/unit/table/globalsum.cpp
+++ modules/type/complex/reduction/unit/table/globalsum.cpp
@@ -22,23 +22,13 @@
 #include <nt2/sdk/unit/tests/exceptions.hpp>
 #include <nt2/table.hpp>
 
-NT2_TEST_CASE_TPL( globalsum, NT2_TYPES )
-{
-  nt2::table<T> a = nt2::reshape(nt2::_(T(1), T(9)), 3, 3);
-  NT2_TEST_EQUAL( nt2::globalsum(a), T(45));
-  NT2_TEST_EQUAL( nt2::globalsum(T(1)), T(1)); 
-  a(3, 3) = T(0); 
-  NT2_TEST_EQUAL( nt2::globalsum(a), T(45-9));
-  NT2_TEST_EQUAL( nt2::globalsum(T(0)), T(0));
-}
 
 NT2_TEST_CASE_TPL( globalsumc, NT2_REAL_TYPES )
 {
-  nt2::table<cT> a = nt2::reshape(nt2::_(T(1), T(9)), 3, 3);
-  
-  NT2_TEST_EQUAL( nt2::globalsum(a), cT(45));
-  NT2_TEST_EQUAL( nt2::globalsum(cT(1)), cT(1)); 
-  a(3, 3) = T(0); 
-  NT2_TEST_EQUAL( nt2::globalsum(a), cT(45-9));
-  NT2_TEST_EQUAL( nt2::globalsum(cT(0)), cT(0));
+  typedef std::complex<T> cT; 
+  nt2::table<cT> a = nt2::reshape(nt2::ones(1, 9, nt2::meta::as_<cT>()), 3, 3);
+  NT2_DISPLAY(a);
+  NT2_DISPLAY(nt2::colvect(a)); 
+  NT2_TEST_EQUAL( nt2::globalsum(a), cT(9));
+
 }
