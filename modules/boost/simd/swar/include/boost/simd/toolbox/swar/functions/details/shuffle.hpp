80055b965265ea24c463f402d2a98f361a2de603
Pierre Esterie <pesterie@gmail.com>
2012-11-21 18:06:13 +0100

    AVX on the go

--- modules/boost/simd/swar/include/boost/simd/toolbox/swar/functions/details/shuffle.hpp
+++ modules/boost/simd/swar/include/boost/simd/toolbox/swar/functions/details/shuffle.hpp
@@ -103,6 +103,17 @@ __m256d shuffle(__m256d const input)
   return shuffle<lower_i0, lower_i1, upper_i0, upper_i1>(input, input);
 }
 
+template< unsigned int lower_i0, unsigned int lower_i1
+          , unsigned int upper_i0, unsigned int upper_i1>
+  BOOST_FORCEINLINE
+  __m256i shuffle(__m256i const lower, __m256i const upper)
+  {
+    return _mm256_castpd_si256(
+      _mm256_shuffle_pd( _mm256_castsi256_pd(lower), _mm256_castsi256_pd(upper)
+                    , _MM_SHUFFLE(upper_i1, upper_i0, lower_i1, lower_i0)
+                    )
+    );
+  }
 #endif
 } } }
 
