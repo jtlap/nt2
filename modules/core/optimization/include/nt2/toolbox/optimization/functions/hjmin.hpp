4e106eb3fe593e3612b25b35302ddebb8e4a0f2f
jtlap <jtlapreste@gmail.com>
2012-11-23 08:05:29 +0100

    some comments in hjmin

--- modules/core/optimization/include/nt2/toolbox/optimization/functions/hjmin.hpp
+++ modules/core/optimization/include/nt2/toolbox/optimization/functions/hjmin.hpp
@@ -30,16 +30,27 @@ namespace nt2
 
   //============================================================================
   /*!
-   * Apply hjmin algorithm to find local minimum of a function
+   * Apply hjmin (Hooke-Jeeves) algorithm to find local minimum of a function
    *
    * \param func  Function to optimize
-   * \param x0    Initial value
-   * \param h     Initial step(s) inevery (each) direction
+   * \param init  Initial value
+   * \param stps  Initial step(s) in every (each) direction
    * \param opt   Options pack related to the minimization handling
    *
    * \return  a tuple containing the result of the minimization, the value of the
    *          function at minimum, the number of required iteration and a boolean
    *          notifying success of convergence.
+   *
+   * Hooke-Jeeves Pattern Moves is in a sense the simplest of all optimization
+   * algorithms. At each iteration, this method first defines a pattern of
+   * points by moving each parameter one by one, so as to optimize the
+   * current loss function. The entire pattern of points is then shifted or
+   * moved to a new location; this new location is determined by
+   * extrapolating the line from the old base point in the m dimensional
+   * parameter space to the new base point. The step sizes in this process
+   * are constantly adjusted to "zero in" on the respective optimum. This
+   * method is usually quite effective, and should be tried if both the
+   * quasi-Newton and Simplex methods fail to produce reasonable estimates.
    */
   //============================================================================
   template<class F, class A, class H> BOOST_FORCEINLINE
@@ -49,12 +60,12 @@ namespace nt2
                                     , details::optimization_settings<typename A::value_type> const&
                                   )
                       >::type
-  hjmin(F f, A aa, H hh)
+  hjmin(F f, A init, H stps)
   {
     typename boost::dispatch::make_functor<tag::hjmin_, F>::type callee;
     return callee ( f
-                  , aa
-                  , hh
+                  , init
+                  , stps
                   , details::optimization_settings<typename A::value_type>()
                   );
   }
@@ -67,12 +78,12 @@ namespace nt2
                                   , details::optimization_settings<typename A::value_type> const&
                                   )
                       >::type
-  hjmin(F f, A aa, H hh, nt2::details::option_expr<Xpr> const& opt)
+  hjmin(F f, A init, H stps, nt2::details::option_expr<Xpr> const& opt)
   {
     typename boost::dispatch::make_functor<tag::hjmin_, F>::type callee;
     return callee ( f
-                  , aa
-                  , hh
+                  , init
+                  , stps
                   , details::optimization_settings<typename A::value_type>(opt)
                   );
   }
