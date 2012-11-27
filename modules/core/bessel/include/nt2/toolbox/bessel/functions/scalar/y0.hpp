76337bf48cbf37780a57524861e2cdb080136113
jtlap <jtlapreste@gmail.com>
2012-11-27 15:26:13 +0100

    bsd indentation for some bessel functions

--- modules/core/bessel/include/nt2/toolbox/bessel/functions/scalar/y0.hpp
+++ modules/core/bessel/include/nt2/toolbox/bessel/functions/scalar/y0.hpp
@@ -61,13 +61,13 @@ namespace nt2 { namespace ext
       if (is_ltz(a0)||is_nan(a0)) return Nan<result_type>();
       if (is_inf(a0)) return Zero<result_type>();
       if (is_eqz(a0)) return Minf<result_type>();
-    #if defined(BOOST_SIMD_HAS__Y0)
+#if defined(BOOST_SIMD_HAS__Y0)
       return ::_y0(a0);
-    #elif defined(BOOST_SIMD_HAS_Y0)
+#elif defined(BOOST_SIMD_HAS_Y0)
       return ::y0(a0);
-    #else
-      #error y0 not supported
-    #endif
+#else
+#error y0 not supported
+#endif
     }
   };
 } }
@@ -93,43 +93,43 @@ namespace nt2 { namespace ext
       if (is_inf(a0)) return Zero<result_type>();
       if (is_eqz(a0)) return Minf<result_type>();
       if (a0 <= Two<A0>())
-        {
-          A0 z = sqr(a0);
-          A0 p2 = (z-single_constant<A0, 0x3edd4b3a>())*
-            horner< NT2_HORNER_COEFF_T(stype, 5,
-                                       (0x33cb0920,
-                                        0xb71ded71,
-                                        0x3a0c1a3e,
-                                        0xbc81c8f4,
-                                        0x3e2edb4f
-                                        ) ) > (z);
-          return p2+single_constant<A0, 0x3f22f983>()*nt2::log(a0)*nt2::j0(a0);
-        }
+      {
+        A0 z = sqr(a0);
+        A0 p2 = (z-single_constant<A0, 0x3edd4b3a>())*
+          horner< NT2_HORNER_COEFF_T(stype, 5,
+                                     (0x33cb0920,
+                                      0xb71ded71,
+                                      0x3a0c1a3e,
+                                      0xbc81c8f4,
+                                      0x3e2edb4f
+                                       ) ) > (z);
+        return p2+single_constant<A0, 0x3f22f983>()*nt2::log(a0)*nt2::j0(a0);
+      }
       A0 q = rec(a0);
       A0 w = nt2::sqrt(q);
       A0 p3 = w *
-         horner< NT2_HORNER_COEFF_T(stype, 8,
-                                    (0xbd8c100e,
-                                     0x3e3ef887,
-                                     0xbe5ba616,
-                                     0x3df54214,
-                                     0xbb69539e,
-                                     0xbd4b8bc1,
-                                     0xb6612dc2,
-                                     0x3f4c422a
-                                     ) ) > (q);
-      w = sqr(q);
-      A0 xn =  q*
         horner< NT2_HORNER_COEFF_T(stype, 8,
-                                   (0x4201aee0,
-                                    0xc2113945,
-                                    0x418c7f6a,
-                                    0xc09f3306,
-                                    0x3f8040aa,
-                                    0xbe46a57f,
-                                    0x3d84ed6e,
-                                    0xbdffff97
-                                    ) ) > (w)-Pio_4<A0>();
+                                   (0xbd8c100e,
+                                    0x3e3ef887,
+                                    0xbe5ba616,
+                                    0x3df54214,
+                                    0xbb69539e,
+                                    0xbd4b8bc1,
+                                    0xb6612dc2,
+                                    0x3f4c422a
+                                     ) ) > (q);
+       w = sqr(q);
+       A0 xn =  q*
+         horner< NT2_HORNER_COEFF_T(stype, 8,
+                                    (0x4201aee0,
+                                     0xc2113945,
+                                     0x418c7f6a,
+                                     0xc09f3306,
+                                     0x3f8040aa,
+                                     0xbe46a57f,
+                                     0x3d84ed6e,
+                                     0xbdffff97
+                                      ) ) > (w)-Pio_4<A0>();
       return p3*nt2::sin(xn+a0);
     }
   };
