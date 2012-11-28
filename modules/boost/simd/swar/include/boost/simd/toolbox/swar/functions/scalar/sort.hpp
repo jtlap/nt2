21523e9f3646bfc4099529b52a9c326cc32df82a
jtlap <jtlapreste@gmail.com>
2012-11-28 23:51:40 +0100

    sort test corrections

--- modules/boost/simd/swar/include/boost/simd/toolbox/swar/functions/scalar/sort.hpp
+++ modules/boost/simd/swar/include/boost/simd/toolbox/swar/functions/scalar/sort.hpp
@@ -24,7 +24,32 @@ namespace boost { namespace simd { namespace ext
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-       return a0;
+      return a0;
+    }
+  };
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::sort_, tag::cpu_
+                                     , (A0)(A1)
+                                     , (scalar_< arithmetic_<A0> >)
+                                     (scalar_< integer_<A1> >)
+    )
+  {
+    typedef A0 result_type;
+    result_type operator()(A0 const& a0, A1 const&) const
+    {
+      return a0;
+    }
+  };
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::sort_, tag::cpu_
+                                     , (A0)(A1)(A2)
+                                     , (scalar_< arithmetic_<A0> >)
+                                     (scalar_< integer_<A1> >)
+                                     (scalar_< int8_<A2> >)
+    )
+  {
+    typedef A0 result_type;
+    result_type operator()(A0 const& a0, A1 const&, A2 const&) const
+    {
+      return a0;
     }
   };
 } } }
