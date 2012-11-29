a8e2c53a73eb9a78ba52c2cbb038c3a0aa5a2eb8
jtlap <jtlapreste@gmail.com>
2012-11-29 10:09:32 +0100

    is_prime test

--- modules/core/combinatorial/unit/expr/is_prime.cpp
+++ modules/core/combinatorial/unit/expr/is_prime.cpp
@@ -15,23 +15,14 @@
 ///
 #include <nt2/toolbox/combinatorial/include/functions/is_prime.hpp>
 #include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/include/functions/min.hpp>
-#include <nt2/include/functions/saturate.hpp>
-
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/as_floating.hpp>
-#include <nt2/sdk/meta/as_signed.hpp>
-#include <nt2/sdk/meta/upgrade.hpp>
-#include <nt2/sdk/meta/downgrade.hpp>
-#include <nt2/sdk/meta/scalar_of.hpp>
-#include <boost/dispatch/meta/as_floating.hpp>
-#include <boost/type_traits/common_type.hpp>
+
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/toolbox/constant/constant.hpp>
+#include <nt2/include/functions/is_nez.hpp>
 #include <nt2/table.hpp>
 
 NT2_TEST_CASE_TPL ( is_prime_real__1_0,  NT2_REAL_TYPES)
@@ -41,10 +32,15 @@ NT2_TEST_CASE_TPL ( is_prime_real__1_0,  NT2_REAL_TYPES)
   using nt2::tag::is_prime_;
   typedef typename nt2::meta::as_logical<T>::type bT;
   nt2::table<T> a = nt2::_(T(1), T(30));
+  int aa[] = {false, true, true, false, true, false, true, false, false, false,
+               true, false, true, false, false, false, true, false, true, false,
+               false, false, true, false, false, false, false, false, true, false};
+  nt2::table<bT> ba(nt2::of_size(1, 30));
+
+  for(int i=0; i < 30; i++) ba(i+1) = nt2::is_nez(aa[i]);
   NT2_DISPLAY( is_prime(a));
-  //  nt2::table<bT> p = is_prime(a);
-  // specific values tests
-  //  NT2_TEST(p(2));
+  nt2::table<bT> p = is_prime(a);
+  NT2_TEST_EQUAL(p, ba);
 
 } // end of test for floating_
 
