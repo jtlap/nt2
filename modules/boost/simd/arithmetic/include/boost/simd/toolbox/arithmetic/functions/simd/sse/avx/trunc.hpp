ea874862efaba532f0abf4fcf1887a62a2bc46bd
jtlap <jtlapreste@gmail.com>
2012-12-03 20:58:22 +0100

    trunc using simd calls in avx ans sse_4.1
    round floor and ceil have already been coded in sse4.1 and avx as mm_round/ceil/floor_/ps/pd intrinsics
    but trunc has been forgotten because it has to go through a two parameters mm_round_ps/pd call

---
+++ modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/simd/sse/avx/trunc.hpp
@@ -0,0 +1,41 @@
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_SSE_AVX_TRUNC_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_SSE_AVX_TRUNC_HPP_INCLUDED
+#ifdef BOOST_SIMD_HAS_AVX_SUPPORT
+
+namespace boost { namespace simd { namespace ext
+{
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::trunc_, boost::simd::tag::avx_
+                            , (A0)
+                            , ((simd_<double_<A0>,boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL(1)
+    {
+      return _mm256_round_pd(a0, _MM_FROUND_TO_ZERO);
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::trunc_, boost::simd::tag::avx_
+                            , (A0)
+                            , ((simd_<single_<A0>,boost::simd::tag::avx_>))
+                            )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL(1)
+    {
+      return _mm256_round_ps(a0, _MM_FROUND_TO_ZERO);
+    }
+  };
+} } }
+
+#endif
+#endif
