bb662c6e821292273c65cf0b7e8626f84d2fe825
Mathias Gaunard <mathias@gaunard.com>
2012-12-03 20:10:30 +0100

    missing include in store scalar test

--- modules/boost/simd/operator/unit/scalar/store.cpp
+++ modules/boost/simd/operator/unit/scalar/store.cpp
@@ -9,6 +9,7 @@
 #define NT2_UNIT_MODULE "nt2 operator toolbox - store/scalar Mode"
 
 #include <boost/simd/toolbox/operator/include/functions/store.hpp>
+#include <boost/simd/include/functions/load.hpp>
 #include <boost/fusion/include/at_c.hpp>
 #include <boost/fusion/include/make_vector.hpp>
 #include <boost/fusion/include/vector.hpp>
