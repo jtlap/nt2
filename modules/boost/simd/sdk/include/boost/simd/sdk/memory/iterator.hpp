ed93b23125c98ecb658e2725b45feec6f128cb88
Mathias Gaunard <mathias@gaunard.com>
2012-12-06 04:24:48 +0100

    fix header guard in iterator

--- modules/boost/simd/sdk/include/boost/simd/sdk/memory/iterator.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/memory/iterator.hpp
@@ -6,8 +6,8 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#ifndef BOOST_SIMD_SDK_MEMORY_DETAILS_CATEGORY_HPP_INCLUDED
-#define BOOST_SIMD_SDK_MEMORY_DETAILS_CATEGORY_HPP_INCLUDED
+#ifndef BOOST_SIMD_SDK_MEMORY_ITERATOR_HPP_INCLUDED
+#define BOOST_SIMD_SDK_MEMORY_ITERATOR_HPP_INCLUDED
 
 ////////////////////////////////////////////////////////////////////////////////
 // Various memory hierarchy stuff
