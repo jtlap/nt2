a215a603b1c0338326f48dbca739f00ac435eef7
Mathias Gaunard <mathias@gaunard.com>
2012-12-11 23:05:17 +0100

    rewrite SIMD iterators without reinterpret cast, simplify code a bit

--- modules/boost/simd/sdk/include/boost/simd/sdk/simd/iterator.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/simd/iterator.hpp
@@ -10,241 +10,208 @@
 #define BOOST_SIMD_SDK_SIMD_ITERATOR_HPP_INCLUDED
 
 #include <boost/simd/sdk/simd/pack.hpp>
+#include <boost/simd/sdk/meta/cardinal_of.hpp>
+#include <boost/simd/include/functions/load.hpp>
 #include <boost/simd/include/functions/store.hpp>
-#include <boost/simd/sdk/memory/align_on.hpp>
 #include <boost/simd/sdk/memory/is_aligned.hpp>
-#include <boost/dispatch/meta/strip.hpp>
 #include <boost/iterator/iterator_adaptor.hpp>
-#include <boost/pointee.hpp>
 #include <boost/assert.hpp>
-#include <boost/utility/enable_if.hpp>
-#include <boost/type_traits/is_same.hpp>
-#include <boost/mpl/and.hpp>
-#include <boost/mpl/bool.hpp>
 
 namespace boost { namespace simd
 {
-////////////////////////////////////////////////////////////////////////////////
-// simd::input_iterator reference a pack of N elements of type T that can only
-// be read.
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // simd::input_iterator reference a pack of N elements of type T that can only
+  // be read.
+  ////////////////////////////////////////////////////////////////////////////////
   template<class T, std::size_t C = meta::cardinal_of< pack<T> >::value >
   struct  input_iterator
-        : public  boost
-                  ::iterator_adaptor< input_iterator<T,C>
-                                    , typename pack< typename dispatch::meta::
-                                                     strip<T>::type
-                                                   , C
-                                                   >::data_type const*
-                                    , pack< typename dispatch::meta::
-                                            strip<T>::type
-                                          , C
-                                          >
-                                    , boost::random_access_traversal_tag
-                                    , pack< typename dispatch::meta::
-                                            strip<T>::type
-                                          , C
-                                          >
-                                    >
+        : boost::
+          iterator_adaptor< input_iterator<T, C>
+                          , T const*
+                          , pack<T, C>
+                          , std::random_access_iterator_tag
+                          , pack<T, C> const
+                          >
   {
-    private:
-    struct enabler {};
+  private:
+    typedef pack<T,C>                               pack_type;
 
-    public:
-    typedef typename dispatch::meta::strip<T>::type   strip_t;
-    typedef pack<strip_t,C>                         pack_type;
-    typedef typename pack<strip_t,C>::data_type   native_type;
-    typedef native_type*                             iterator;
-    typedef native_type const*                 const_iterator;
 
+  public:
     input_iterator() : input_iterator::iterator_adaptor_(0) {}
 
-    explicit input_iterator(const strip_t* p)
+    explicit input_iterator(T const* p)
     : input_iterator::
-      iterator_adaptor_(reinterpret_cast<const_iterator>(p))
+      iterator_adaptor_(p)
     {
       BOOST_ASSERT_MSG
-      ( boost::simd::is_aligned(p,sizeof(native_type))
-      , "The constructor of iterator<T,C> has been called on a pointer"
-        "which alignment is not compatible with current SIMD extension."
+      ( boost::simd::is_aligned(p, C*sizeof(T))
+      , "The constructor of iterator<T,C> has been called on a pointer "
+        "which alignment is not compatible with the current SIMD extension."
       );
     }
 
-    template<class U, std::size_t V>
-    input_iterator( input_iterator<U,V> const& other
-                  , typename
-                    boost::enable_if< boost::mpl::and_< boost::is_same<T,U>
-                                                      , boost::mpl::bool_<C==V>
-                                                      >
-                                    , enabler
-                                    >::type = enabler()
-                  ) : input_iterator::iterator_adaptor_(other.base()) {}
-
-    private:
+  private:
     friend class boost::iterator_core_access;
 
-    BOOST_DISPATCH_FORCE_INLINE
+    BOOST_FORCEINLINE
     typename input_iterator::reference dereference() const
     {
-      pack_type that;
-      that = boost::simd::
-             load<pack_type>(reinterpret_cast<const_iterator>(this->base()),0);
-      return that;
+      return boost::simd::load<pack_type>(this->base());
+    }
+
+    BOOST_FORCEINLINE void increment()
+    {
+      this->base_reference() += C;
+    }
+
+    BOOST_FORCEINLINE void decrement()
+    {
+      this->base_reference() -= C;
+    }
+
+    BOOST_FORCEINLINE void advance(typename input_iterator::difference_type n)
+    {
+      this->base_reference() += n*C;
+    }
+
+    BOOST_FORCEINLINE
+    typename input_iterator::difference_type distance_to(input_iterator const& other) const
+    {
+      return (other.base() - this->base()) / typename input_iterator::difference_type(C);
     }
   };
 
-////////////////////////////////////////////////////////////////////////////////
-// simd::input_begin()/simd::input_end() functions wrap scalar begin/end
-// iterators for input_iterator.
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // simd::input_begin()/simd::input_end() functions wrap scalar begin/end
+  // iterators for input_iterator.
+  ////////////////////////////////////////////////////////////////////////////////
   template<class Iterator>
   input_iterator<typename std::iterator_traits<Iterator>::value_type>
   input_begin(Iterator p)
   {
-    typedef typename std::iterator_traits<Iterator>::value_type value_type;
-    value_type const* tmp = &(*p);
-    return input_iterator<value_type>(tmp);
+    return input_iterator<typename std::iterator_traits<Iterator>::value_type>(&*p);
   }
 
-  template<class Iterator, std::size_t C>
-  input_iterator<typename std::iterator_traits<Iterator>::value_type,C>
+  template<std::size_t C, class Iterator>
+  input_iterator<typename std::iterator_traits<Iterator>::value_type, C>
   input_begin(Iterator p)
   {
-    typedef typename std::iterator_traits<Iterator>::value_type value_type;
-    value_type const* tmp = &(*p);
-    return input_iterator<value_type,C>(tmp);
+    return input_iterator<typename std::iterator_traits<Iterator>::value_type, C>(&*p);
   }
 
   template<class Iterator>
   input_iterator<typename std::iterator_traits<Iterator>::value_type>
   input_end(Iterator p)
   {
-    typedef typename std::iterator_traits<Iterator>::value_type value_type;
-    value_type const* tmp = &(*(p-1));
-    return input_iterator<value_type>(tmp+1);
+    return input_iterator<typename std::iterator_traits<Iterator>::value_type>(&*(p-1) + 1);
   }
 
-  template<class Iterator, std::size_t C>
+  template<std::size_t C, class Iterator>
   input_iterator<typename std::iterator_traits<Iterator>::value_type,C>
   input_end(Iterator p)
   {
-    typedef typename std::iterator_traits<Iterator>::value_type value_type;
-    value_type const* tmp = &(*(p-1));
-    return input_iterator<value_type,C>(tmp+1);
+    return input_iterator<typename std::iterator_traits<Iterator>::value_type, C>(&*(p-1) + 1);
   }
 
-////////////////////////////////////////////////////////////////////////////////
-// simd::output_iterator reference a pack of N elements of type T that can only
-// be write.
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // simd::output_iterator reference a pack of N elements of type T that can only
+  // be write.
+  ////////////////////////////////////////////////////////////////////////////////
   template<class T, std::size_t C = meta::cardinal_of< pack<T> >::value >
   struct  output_iterator
-    : public  boost
-              ::iterator_adaptor< output_iterator<T,C>
-                                , typename pack< typename dispatch::meta::
-                                                 strip<T>::type
-                                                 , C
-                                                 >::data_type*
-                                , pack< typename dispatch::meta::
-                                        strip<T>::type
-                                      , C
-                                      >
-                                , boost::random_access_traversal_tag
-                                , output_iterator<T,C>
-                                >
+        :  boost::
+           iterator_adaptor< output_iterator<T,C>
+                           , T*
+                           , pack<T, C>
+                           , std::random_access_iterator_tag
+                           , output_iterator<T,C> const
+                           >
   {
   private:
-    struct enabler {};
+    typedef pack<T,C> pack_type;
 
   public:
-
-    typedef pack<T,C> pack_type;
-    typedef typename pack<T,C>::data_type native_;
-    typedef typename native_::native_type native_type;
-
     output_iterator() : output_iterator::iterator_adaptor_(0) {}
 
     explicit output_iterator(T* p)
-      : output_iterator::iterator_adaptor_(reinterpret_cast<native_*>(p))
+      : output_iterator::iterator_adaptor_(p)
     {
       BOOST_ASSERT_MSG
-        ( boost::simd::is_aligned(p,sizeof(native_type))
-        , "The constructor of iterator<T,C> has been called on a pointer"
+        ( boost::simd::is_aligned(p, C*sizeof(T))
+        , "The constructor of iterator<T,C> has been called on a pointer "
           "which alignment is not compatible with current SIMD extension."
         );
     }
 
-    template<class U, std::size_t V>
-    output_iterator( output_iterator<U,V> const& other
-                   , typename
-                     boost::enable_if< boost::mpl::and_< boost::is_same<T,U>
-                                                       , boost::mpl::bool_<C==V>
-                                                       >
-                                    , enabler
-                                    >::type = enabler()
-                   ) : output_iterator::iterator_adaptor_(other.base()) {}
-
-    BOOST_DISPATCH_FORCE_INLINE
-    void operator=(pack_type const& right)
-    {
-      boost::simd::store(right, reinterpret_cast<T*>(this->base()), 0);
-    }
-
     template<class Expr>
-    BOOST_DISPATCH_FORCE_INLINE
-    void operator=(Expr const& right)
+    BOOST_FORCEINLINE
+    void operator=(Expr const& right) const
     {
-      boost::simd::store(right, reinterpret_cast<T*>(this->base()), 0);
+      boost::simd::store(right, this->base());
     }
 
   private:
     friend class boost::iterator_core_access;
 
-    BOOST_DISPATCH_FORCE_INLINE
+    BOOST_FORCEINLINE
     typename output_iterator::reference dereference() const
     {
       return *this;
     }
+
+    BOOST_FORCEINLINE void increment()
+    {
+      this->base_reference() += C;
+    }
+
+    BOOST_FORCEINLINE void decrement()
+    {
+      this->base_reference() -= C;
+    }
+
+    BOOST_FORCEINLINE void advance(typename output_iterator::difference_type n)
+    {
+      this->base_reference() += n*C;
+    }
+
+    BOOST_FORCEINLINE
+    typename output_iterator::difference_type distance_to(output_iterator const& other) const
+    {
+      return (other.base() - this->base()) / typename input_iterator::difference_type(C);
+    }
   };
 
-////////////////////////////////////////////////////////////////////////////////
-// simd::output_begin()/simd::output_end() functions wrap scalar begin/end
-// iterators for output_iterator.
-////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  // simd::output_begin()/simd::output_end() functions wrap scalar begin/end
+  // iterators for output_iterator.
+  ////////////////////////////////////////////////////////////////////////////////
   template<class Iterator>
   output_iterator<typename std::iterator_traits<Iterator>::value_type>
   output_begin(Iterator p)
   {
-    typedef typename std::iterator_traits<Iterator>::value_type value_type;
-    value_type* tmp = &(*p);
-    return output_iterator<value_type>(tmp);
+    return output_iterator<typename std::iterator_traits<Iterator>::value_type>(&*p);
   }
 
-  template<class Iterator, std::size_t C>
-  output_iterator<typename std::iterator_traits<Iterator>::value_type,C>
+  template<std::size_t C, class Iterator>
+  output_iterator<typename std::iterator_traits<Iterator>::value_type, C>
   output_begin(Iterator p)
   {
-    typedef typename std::iterator_traits<Iterator>::value_type value_type;
-    value_type* tmp = &(*p);
-    return output_iterator<value_type,C>(tmp);
+    return output_iterator<typename std::iterator_traits<Iterator>::value_type, C>(&*p);
   }
 
   template<class Iterator>
   output_iterator<typename std::iterator_traits<Iterator>::value_type>
   output_end(Iterator p)
   {
-    typedef typename std::iterator_traits<Iterator>::value_type value_type;
-    value_type* tmp = &(*(p-1));
-    return output_iterator<value_type>(tmp+1);
+    return output_iterator<typename std::iterator_traits<Iterator>::value_type>(&*(p-1) + 1);
   }
 
-  template<class Iterator, std::size_t C>
+  template<std::size_t C, class Iterator>
   output_iterator<typename std::iterator_traits<Iterator>::value_type,C>
   output_end(Iterator p)
   {
-    typedef typename std::iterator_traits<Iterator>::value_type value_type;
-    value_type* tmp = &(*(p-1));
-    return output_iterator<value_type,C>(tmp+1);
+    return output_iterator<typename std::iterator_traits<Iterator>::value_type, C>(&*(p-1) + 1);
   }
 
 } }
