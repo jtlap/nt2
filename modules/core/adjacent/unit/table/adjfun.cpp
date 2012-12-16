0478c810879271d8f99455c0dffb6777bbbd52aa
Mathias Gaunard <mathias@gaunard.com>
2012-12-16 22:52:55 +0100

    small adjfun fix for operations past the max number of dimensions

--- modules/core/adjacent/unit/table/adjfun.cpp
+++ modules/core/adjacent/unit/table/adjfun.cpp
@@ -11,11 +11,12 @@
 #include <nt2/table.hpp>
 #include <nt2/include/functions/adjfun.hpp>
 #include <nt2/include/functions/isempty.hpp>
-#include <nt2/include/functions/isequal.hpp>
 #include <nt2/include/functions/size.hpp>
+#include <nt2/include/functions/ones.hpp>
 
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
 
 NT2_TEST_CASE_TPL( adjfun, (double)(float)(int)(short)(char) )
 {
@@ -32,20 +33,17 @@ NT2_TEST_CASE_TPL( adjfun, (double)(float)(int)(short)(char) )
     for(std::size_t i=1;i <= nt2::size(y, 1);i++)
       y(i,j) = T(i + j)*T(i+j);
 
-  NT2_TEST( isequal( adjfun(functor<nt2::tag::minus_>(), y)
-                        , y(_(2, end_), _)-y(_(1, end_-1), _)
-                        )
-          );
+  NT2_TEST_EQUAL( adjfun(functor<nt2::tag::minus_>(), y)
+                , y(_(2, end_), _)-y(_(1, end_-1), _)
+                );
 
-  NT2_TEST( isequal( adjfun(functor<nt2::tag::minus_>(), y, 1)
-                        , y(_(2, end_), _)-y(_(1, end_-1), _)
-                        )
-          );
+  NT2_TEST_EQUAL( adjfun(functor<nt2::tag::minus_>(), y, 1)
+                , y(_(2, end_), _)-y(_(1, end_-1), _)
+                );
 
-  NT2_TEST( isequal( adjfun(functor<nt2::tag::minus_>(), y, 2)
-                        , y(_,_(2, end_), _)-y(_,_(1, end_-1), _)
-                        )
-          );
+  NT2_TEST_EQUAL( adjfun(functor<nt2::tag::minus_>(), y, 2)
+                , y(_,_(2, end_), _)-y(_,_(1, end_-1), _)
+                );
 
   NT2_TEST(isempty( adjfun(functor<nt2::tag::minus_>(), y, 3) ));
 }
@@ -59,4 +57,7 @@ NT2_TEST_CASE_TPL( adjfun_scalar, NT2_TYPES )
   NT2_TEST(nt2::isempty(nt2::adjfun(nt2::functor<nt2::tag::minus_>(), T(1),1) ));
   NT2_TEST(nt2::isempty(nt2::adjfun(nt2::functor<nt2::tag::minus_>(), T(1),2) ));
   NT2_TEST(nt2::isempty(nt2::adjfun(nt2::functor<nt2::tag::minus_>(), T(1),3) ));
+
+  nt2::table<T, nt2::_2D> a2 = nt2::ones(3, 5, nt2::meta::as_<T>());
+  NT2_TEST(nt2::isempty(nt2::adjfun(nt2::functor<nt2::tag::minus_>(), a2, 3) ));
 }
