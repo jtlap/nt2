9cf6ac3a43b7de669f4000d5e4126b43667fbf89
jtlap <jtlapreste@gmail.com>
2013-01-09 19:31:46 +0100

    more tests for norminv

--- modules/core/statistics/unit/scalar/norminv.cpp
+++ modules/core/statistics/unit/scalar/norminv.cpp
@@ -18,13 +18,14 @@
 #include <nt2/include/functions/eye.hpp>
 #include <nt2/include/functions/ones.hpp>
 #include <nt2/include/functions/abs.hpp>
+#include <nt2/include/functions/repnum.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/memory/buffer.hpp>
 #include <nt2/toolbox/constant/constant.hpp>
 #include <nt2/table.hpp>
 
-NT2_TEST_CASE_TPL ( norminv_1,  (float)(double))//NT2_REAL_TYPES)
+NT2_TEST_CASE_TPL ( norminv_1,  NT2_REAL_TYPES)
 {
   using nt2::norminv;
   using nt2::tag::norminv_;
@@ -48,5 +49,31 @@ NT2_TEST_CASE_TPL ( norminv_2,  NT2_REAL_TYPES)
   NT2_TEST_ULP_EQUAL(norminv(T(0.158655253931457051414767454368), nt2::One<T>()), nt2::Zero<T>(), 6.5);
   NT2_TEST_ULP_EQUAL(norminv(nt2::One<T>(), nt2::One<T>()), nt2::Inf<T>(), 0.5);
   NT2_TEST_ULP_EQUAL(norminv(nt2::Zero<T>(), nt2::One<T>()), nt2::Minf<T>(), 0.5);
+} // end of test for floating_
+
+NT2_TEST_CASE_TPL ( norminv_1v,  NT2_REAL_TYPES)
+{
+  using nt2::norminv;
+  using nt2::tag::norminv_;
+  using nt2::_;
+  // specific values tests
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(nt2::Nan<T>(), 10, 2)), nt2::repnum(nt2::Nan<T>(), 10, 2), 0.5);
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(T(0.841344746068542948585232545632), 10, 2)), nt2::repnum(nt2::One<T>(), 10, 2), 3);
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(T(0.158655253931457051414767454368), 10, 2)), nt2::repnum(nt2::Mone<T>(), 10, 2), 6.5);
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(nt2::Zero<T>(), 10, 2)), nt2::repnum(nt2::Minf<T>(), 10, 2), 0.5);
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(nt2::One<T>(), 10, 2)), nt2::repnum(nt2::Inf<T>(), 10, 2), 0.5);
+}
+
+NT2_TEST_CASE_TPL ( norminv_2v,  NT2_REAL_TYPES)
+{
+  using nt2::norminv;
+  using nt2::tag::norminv_;
+  using nt2::_;
+  // specific values tests
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(nt2::Nan<T>(), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Nan<T>(), 10, 2), 0.5);
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(T(0.841344746068542948585232545632), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Two<T>(), 10, 2), 3);
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(T(0.158655253931457051414767454368), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Zero<T>(), 10, 2), 6.5);
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(nt2::One<T>(), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Inf<T>(), 10, 2), 0.5);
+  NT2_TEST_ULP_EQUAL(norminv(nt2::repnum(nt2::Zero<T>(), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Minf<T>(), 10, 2), 0.5);
 } // end of test for floating_
 
