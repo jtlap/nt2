7accb062feff31f5b3591727ef482058ec6ac90e
pesterie <pesterie@gmail.com>
2013-01-10 16:43:26 +0100

    Adding special cases for shuffle optimization

--- modules/boost/simd/swar/include/boost/simd/toolbox/swar/functions/simd/sse/sse2/shuffle.hpp
+++ modules/boost/simd/swar/include/boost/simd/toolbox/swar/functions/simd/sse/sse2/shuffle.hpp
@@ -328,8 +328,13 @@ namespace boost { namespace simd
       typedef typename P::type                permutation_t;
 
       template< int I0,int I1,int I2,int I3
-              , bool Some0 = (I0==-1) || (I1==-1) || (I2==-1) || (I3==-1)
-              , int  Num0  = (I0==-1?1:0)+(I1==-1?1:0)+(I2==-1?1:0)+(I3==-1?1:0)
+              , bool AorB   = (I0<4&&I1<4&&I2<4&&I3<4)
+                            ? true : (I0>=4&&I1>=4&&I2>=4&&I3>=4)
+                                    ? false : false
+              , bool Invert = (I0<4&&I1<4&&I2>=4&&I3>=4&&I0!=-1&&I1!=-1&&I2!=-1&&I3!=-1)
+                            ? false : (I0>=4&&I1>=4&&I2<4&&I3<4&&I0!=-1&&I1!=-1&&I2!=-1&&I3!=-1)
+                                    ? true : false
+              , bool Some0  = (I0==-1) || (I1==-1) || (I2==-1) || (I3==-1)
               >
       struct sel
       {};
@@ -357,67 +362,269 @@ namespace boost { namespace simd
         return eval(a0,a1,sel<i0_t::value,i1_t::value,i2_t::value,i3_t::value>());
       }
 
+      template<int I0,int I1,int I2,int I3>
       BOOST_FORCEINLINE result_type eval( A0 const& a0, A0 const&
-                                        , sel<0,1,2,3,false,0> const&) const
+                                        , sel<I0,I1,I2,I3,true,false> const&) const
       {
-        return a0;
+        return shuffle<I0,I1,I2,I3>(a0);
+      }
+
+      template<int I0,int I1,int I2,int I3>
+      BOOST_FORCEINLINE result_type eval( A0 const&, A0 const& a1
+                                        , sel<I0,I1,I2,I3,false,false> const&) const
+      {
+        return shuffle<I0,I1,I2,I3>(a1);
       }
 
       BOOST_FORCEINLINE result_type eval( A0 const&, A0 const&
-                                        , sel<-1,-1,-1,-1> const&) const
+                                        , sel<-1,-1,-1,-1,true,false,true> const&) const
       {
         return Zero<result_type>();
       }
 
       template<int I0,int I1,int I2,int I3> BOOST_FORCEINLINE
       result_type eval( A0 const& a0, A0 const& a1
-                      , sel<I0,I1,I2,I3,false> const&) const
+                      , sel<I0,I1,I2,I3,false,false,false> const&) const
       {
         return details::shuffle<I0&3, I1&3, I2&3, I3&3>(a0,a1);
       }
 
       template<int I0,int I1,int I2,int I3> BOOST_FORCEINLINE
       result_type eval( A0 const& a0, A0 const& a1
-                      , sel<I0,I1,I2,I3,true> const&) const
+                      , sel<I0,I1,I2,I3,false,true,false> const&) const
+      {
+        return details::shuffle<I0&3, I1&3, I2&3, I3&3>(a1,a0);
+      }
+
+      template<int I0,int I1,int I2,int I3> BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<I0,I1,I2,I3,false,false,true> const&) const
       {
         typedef typename dispatch::meta::as_integer<A0,unsigned>::type i_t;
         // Mask the shuffled equivalent
         return  simd::bitwise_and
-                ( eval( a0, a1, sel<I0<0?-2:I0,I1<0?-2:I1,I2<0?-2:I2,I3<0?-2:I3>() )
+                ( eval( a0, a1, sel< I0<0?-1:I0,I1<0?-1:I1,I2<0?-1:I2,I3<0?-1:I3
+                                   >() )
                 , simd::make<i_t> ( -int(I0>=0), -int(I1>=0)
                                   , -int(I2>=0), -int(I3>=0)
                                   )
                 );
       }
 
-      template<int I0,int I1> BOOST_FORCEINLINE
-      result_type eval( A0 const& a0, A0 const& a1
-                      , sel<I0,I1,-1,-1,true,2> const&) const
+      template<int I0,int I1,int I2,int I3> BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<I0,I1,I2,I3,false,true,true> const&) const
       {
-        return details::shuffle<(I0&3), (I1&3),2,3>(a0,Zero<result_type>());
+        typedef typename dispatch::meta::as_integer<A0,unsigned>::type i_t;
+        // Mask the shuffled equivalent
+        return  simd::bitwise_and
+                ( eval( a0, a1, sel< I0<0?-1:I0,I1<0?-1:I1,I2<0?-1:I2,I3<0?-1:I3
+                                   >() )
+                , simd::make<i_t> ( -int(I0>=0), -int(I1>=0)
+                                  , -int(I2>=0), -int(I3>=0)
+                                  )
+                );
       }
 
-      template<int I2,int I3> BOOST_FORCEINLINE
-      result_type eval( A0 const& a0, A0 const& a1
-                      , sel<-1,-1,I2,I3,true,2> const&) const
+      template<int I0,int I1,int I2,int I3> BOOST_FORCEINLINE
+      result_type eval( __m128i const a0, __m128i const a1
+                      , sel<I0,I1,I2,I3,false,false,true> const&) const
       {
-        return details::shuffle<0, 1, (I2&3), (I3&3)>(Zero<result_type>(), a1);
+        typedef typename dispatch::meta::as_integer<A0,unsigned>::type i_t;
+        // Mask the shuffled equivalent
+        return  simd::bitwise_and
+                ( eval( a0, a1, sel< I0<0?-1:I0,I1<0?-1:I1,I2<0?-1:I2,I3<0?-1:I3
+                                   >() )
+                , simd::make<i_t> ( -int(I0>=0), -int(I1>=0)
+                                  , -int(I2>=0), -int(I3>=0)
+                                  )
+                );
+      }
+
+      template<int I0,int I1,int I2,int I3> BOOST_FORCEINLINE
+      result_type eval( __m128i const a0, __m128i const a1
+                      , sel<I0,I1,I2,I3,false,true,true> const&) const
+      {
+        typedef typename dispatch::meta::as_integer<A0,unsigned>::type i_t;
+        // Mask the shuffled equivalent
+        return  simd::bitwise_and
+                ( eval( a0, a1, sel< I0<0?-1:I0,I1<0?-1:I1,I2<0?-1:I2,I3<0?-1:I3
+                                   >() )
+                , simd::make<i_t> ( -int(I0>=0), -int(I1>=0)
+                                  , -int(I2>=0), -int(I3>=0)
+                                  )
+                );
       }
 
       BOOST_FORCEINLINE
       result_type eval( __m128 const a0, __m128 const a1
-                      , sel<0,1,0,1,false> const&) const
+                      , sel<0,1,4,5,false,false,false> const&) const
       {
         return _mm_movelh_ps(a0,a1);
       }
 
       BOOST_FORCEINLINE
       result_type eval( __m128 const a0, __m128 const a1
-                      , sel<2,3,2,3,false> const&) const
+                      , sel<2,3,6,7,false,false,false> const&) const
       {
         return _mm_movehl_ps(a1,a0);
       }
 
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<4,5,0,1,false,true,false> const&) const
+      {
+        return _mm_movelh_ps(a1,a0);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<6,7,2,3,false,true,false> const&) const
+      {
+        return _mm_movehl_ps(a1,a0);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<-1,-1,0,1,true,false,true> const&) const
+      {
+        return _mm_movelh_ps(Zero<result_type>(),a0);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<-1,-1,4,5,false,false,true> const&) const
+      {
+        return _mm_movelh_ps(Zero<result_type>(),a1);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<-1,-1,2,3,true,false,true> const&) const
+      {
+        return _mm_movehl_ps(Zero<result_type>(),a0);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<-1,-1,6,7,false,false,true> const&) const
+      {
+        return _mm_movehl_ps(Zero<result_type>(),a1);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<0,1,-1,-1,true,false,true> const&) const
+      {
+        return _mm_movelh_ps(a0,Zero<result_type>());
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<4,5,-1,-1,false,false,true> const&) const
+      {
+        return _mm_movelh_ps(a1,Zero<result_type>());
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<2,3,-1,-1,true,false,true> const&) const
+      {
+        return _mm_movehl_ps(a0,Zero<result_type>());
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<6,7,-1,-1,false,false,true> const&) const
+      {
+        return _mm_movehl_ps(a1,Zero<result_type>());
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<0,4,1,5,false,false,false> const&) const
+      {
+        return _mm_unpacklo_ps(a0,a1);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<2,6,3,7,false,false,false> const&) const
+      {
+        return _mm_unpackhi_ps(a0,a1);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<4,0,5,1,false,false,false> const&) const
+      {
+        return _mm_unpacklo_ps(a1,a0);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<6,2,7,3,false,false,false> const&) const
+      {
+        return _mm_unpackhi_ps(a1,a0);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<0,-1,1,-1,true,false,true> const&) const
+      {
+        return _mm_unpacklo_ps(a0,Zero<result_type>());
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<4,-1,5,-1,false,false,true> const&) const
+      {
+        return _mm_unpacklo_ps(a1,Zero<result_type>());
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<2,-1,3,-1,true,false,true> const&) const
+      {
+        return _mm_unpackhi_ps(a0,Zero<result_type>());
+
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<6,-1,7,-1,false,false,true> const&) const
+      {
+        return _mm_unpackhi_ps(a1,Zero<result_type>());
+
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<-1,0,-1,1,true,false,true> const&) const
+      {
+        return _mm_unpacklo_ps(Zero<result_type>(),a0);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<-1,4,-1,5,false,false,true> const&) const
+      {
+        return _mm_unpacklo_ps(Zero<result_type>(),a1);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<-1,2,-1,3,true,false,true> const&) const
+      {
+        return _mm_unpackhi_ps(Zero<result_type>(),a0);
+      }
+
+      BOOST_FORCEINLINE
+      result_type eval( __m128 const a0, __m128 const a1
+                      , sel<-1,6,-1,7,false,false,true> const&) const
+      {
+        return _mm_unpackhi_ps(Zero<result_type>(),a1);
+      }
     };
   }
 } }
