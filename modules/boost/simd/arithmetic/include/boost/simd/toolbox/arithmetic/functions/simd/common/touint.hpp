fdfec403b52960e98ab7a682cc787da8cbe4f536
jtlap <jtlapreste@gmail.com>
2013-01-11 15:42:31 +0100

    touint had some lacks. Constants computations have still to be improved

--- modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/simd/common/touint.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/simd/common/touint.hpp
@@ -9,12 +9,22 @@
 #ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_TOUINT_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_TOUINT_HPP_INCLUDED
 #include <boost/simd/toolbox/arithmetic/functions/touint.hpp>
+#include <boost/simd/include/functions/if_else_zero.hpp>
+#include <boost/simd/include/functions/if_else.hpp>
+#include <boost/simd/include/functions/is_gez.hpp>
+#include <boost/simd/include/functions/is_greater_equal.hpp>
+#include <boost/simd/include/functions/oneplus.hpp>
+#include <boost/simd/include/functions/tofloat.hpp>
+#include <boost/simd/include/functions/tofloat.hpp>
+#include <boost/simd/include/functions/bitwise_cast.hpp>
+#include <boost/simd/include/constants/valmax.hpp>
+#include <boost/dispatch/meta/as_integer.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::touint_, tag::cpu_
                             , (A0)(X)
-                            , ((simd_<integer_<A0>,X>))
+                            , ((simd_<unsigned_<A0>,X>))
                             )
   {
     typedef A0 result_type;
@@ -30,7 +40,40 @@ namespace boost { namespace simd { namespace ext
       return a0;
     }
   };
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::touint_, tag::cpu_
+                            , (A0)(X)
+                            , ((simd_<signed_<A0>,X>))
+                            )
+  {
+    typedef typename dispatch::meta::as_integer<A0, unsigned>::type result_type;
 
+    BOOST_FORCEINLINE
+    result_type
+    operator()(A0 const& a0) const
+    {
+      return if_else_zero(is_gez(a0), bitwise_cast<result_type>(a0));
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::touint_, tag::cpu_
+                            , (A0)(X)
+                            , ((simd_<floating_<A0>,X>))
+                            )
+  {
+    typedef typename dispatch::meta::as_integer<A0, unsigned>::type result_type;
+    typedef typename dispatch::meta::as_integer<A0>::type si_type;
+
+    BOOST_FORCEINLINE
+    result_type
+    operator()(A0 const& a0) const
+    {
+      A0 r = if_else_zero(is_gez(a0), a0);
+      return if_else(ge(a0,tofloat(Valmax<si_type>())),
+                     bitwise_cast<result_type>(toint(a0-tofloat(Valmax<si_type>())))+oneplus(bitwise_cast<result_type>(Valmax<si_type>())),
+                     bitwise_cast<result_type>(toint(r))
+        );
+    }
+  };
 
 } } }
 
