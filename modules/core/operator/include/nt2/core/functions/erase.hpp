911912fed0886df249790367ab3665be17f7c7d3
Mathias Gaunard <mathias@gaunard.com>
2013-01-18 19:20:48 +0100

    add erase(a, i, dim) that uses along_index

--- modules/core/operator/include/nt2/core/functions/erase.hpp
+++ modules/core/operator/include/nt2/core/functions/erase.hpp
@@ -10,17 +10,16 @@
 #define NT2_CORE_FUNCTIONS_ERASE_HPP_INCLUDED
 
 #include <nt2/include/functions/function_index.hpp>
+#include <nt2/include/functions/along_index.hpp>
 #include <nt2/include/functions/run.hpp>
 #include <nt2/core/functions/aggregate.hpp>
-#include <boost/typeof/typeof.hpp>
+#include <nt2/sdk/parameters.hpp>
 
 namespace nt2
 {
-  template<class A, class Idx>
-  void erase(A& a, Idx const& idx)
+  template<std::size_t N, class A, class Phi>
+  void erase_impl(A& a, Phi const& phi)
   {
-    BOOST_AUTO_TPL(phi, nt2::function_index(idx, a.extent(), meta::as_<typename A::indexes_type>()));
-
     std::size_t j = nt2::run(phi, 0u, meta::as_<std::size_t>());
     std::size_t k = 0u;
 
@@ -35,7 +34,7 @@ namespace nt2
     }
 
     // compute size: if only one size changes (+ trailing ones) then keep the shape, otherwise linearize
-    typedef typename nt2::make_size<Idx::proto_arity_c>::type size_type;
+    typedef typename nt2::make_size<N>::type size_type;
     size_type sz     = a.extent();
     size_type phi_sz = phi.extent();
 
@@ -60,6 +59,18 @@ namespace nt2
     if(found == -1 || found == 0)
       sz = nt2::of_size(1u, k);
     a.resize(sz);
+  }
+
+  template<class A, class Idx>
+  void erase(A& a, Idx const& idx)
+  {
+    erase_impl<Idx::proto_arity_c>(a, nt2::function_index(idx, a.extent(), meta::as_<typename A::indexes_type>()));
+  }
+
+  template<class A, class Idx>
+  void erase(A& a, Idx const& idx, std::size_t dim)
+  {
+    erase_impl<NT2_MAX_DIMENSIONS>(a, nt2::along_index(idx, dim, a.extent()));
   }
 }
 
