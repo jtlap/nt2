586791b9ccede009f1c0e2a8f231993f3a3dd9e2
jtlap <jtlapreste@gmail.com>
2013-01-28 00:17:25 +0100

    its seems that alias power for pow is a problem

--- modules/core/exponential/include/nt2/toolbox/exponential/functions/pow.hpp
+++ modules/core/exponential/include/nt2/toolbox/exponential/functions/pow.hpp
@@ -61,7 +61,6 @@ namespace nt2 { namespace tag
     struct pow_ : ext::elementwise_<pow_> { typedef ext::elementwise_<pow_> parent; };
   }
   NT2_FUNCTION_IMPLEMENTATION(tag::pow_, pow, 2)
-  NT2_FUNCTION_IMPLEMENTATION(tag::pow_, power, 2)
 }
 
 #endif
