604dc064365d0ebef271c9e4aa5a62fd06a03aa3
Mathias Gaunard <mathias@gaunard.com>
2013-01-29 19:05:06 +0100

    change safe_at_c to be more generic by taking the value to use if none exists. Add mpl_safe_at_c that does the same for MPL sequences

--- modules/core/sdk/include/nt2/sdk/meta/safe_at.hpp
+++ modules/core/sdk/include/nt2/sdk/meta/safe_at.hpp
@@ -9,47 +9,44 @@
 #ifndef NT2_SDK_META_SAFE_AT_HPP_INCLUDED
 #define NT2_SDK_META_SAFE_AT_HPP_INCLUDED
 
-#include <boost/mpl/size_t.hpp>
-#include <boost/utility/enable_if.hpp>
 #include <boost/dispatch/attributes.hpp>
+#include <boost/fusion/include/at.hpp>
+#include <boost/mpl/size_t.hpp>
+#include <boost/mpl/size.hpp>
+#include <boost/mpl/at.hpp>
+#include <boost/utility/enable_if.hpp>
 
 namespace nt2 { namespace meta
 {
-  template<int N, int D, class T> BOOST_FORCEINLINE
+  template<int N, class D, class T> BOOST_FORCEINLINE
   typename boost::lazy_enable_if_c
   < (0 <= N && N < boost::fusion::result_of::size<T>::type::value)
     , boost::fusion::result_of::at_c<const T, N>
-  >::type default_at_c(const T& t)
+  >::type safe_at_c(const T& t, const D& = D())
   {
     return boost::fusion::at_c<N>(t);
   }
 
-  template<int N, int D, class T> BOOST_FORCEINLINE
+  template<int N, class D, class T> BOOST_FORCEINLINE
   typename boost::disable_if_c
   < (0 <= N && N < boost::fusion::result_of::size<T>::type::value)
-    , boost::mpl::size_t<D>
-  >::type default_at_c(const T&)
+    , D
+  >::type safe_at_c(const T&, const D& d = D())
   {
-    return boost::mpl::size_t<D>();
+    return d;
   }
 
-  template<int N, class T> BOOST_FORCEINLINE
-  typename boost::lazy_enable_if_c
-  < (0 <= N && N < boost::fusion::result_of::size<T>::type::value)
-    , boost::fusion::result_of::at_c<const T, N>
-  >::type safe_at_c(const T& t)
+  template<class Seq, int N, class D, class Enable = void>
+  struct mpl_safe_at_c
   {
-    return boost::fusion::at_c<N>(t);
-  }
+    typedef D type;
+  };
 
-  template<int N, class T> BOOST_FORCEINLINE
-  typename boost::disable_if_c
-  < (0 <= N && N < boost::fusion::result_of::size<T>::type::value)
-    , boost::mpl::size_t<1>
-  >::type safe_at_c(const T&)
+  template<class Seq, int N, class D>
+  struct mpl_safe_at_c<Seq, N, D, typename boost::enable_if_c< N < boost::mpl::size<Seq>::type::value >::type>
+       : boost::mpl::at_c<Seq, N>
   {
-    return boost::mpl::size_t<1>();
-  }
+  };
 } }
 
 #endif
