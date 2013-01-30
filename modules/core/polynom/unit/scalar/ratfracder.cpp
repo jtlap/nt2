c5f9949dbee695bc768945c7c346f68a32320a94
jtlap <jtlapreste@gmail.com>
2013-01-30 21:21:18 +0100

    ratfracder correction

--- modules/core/polynom/unit/scalar/ratfracder.cpp
+++ modules/core/polynom/unit/scalar/ratfracder.cpp
@@ -24,8 +24,6 @@
 #include <nt2/include/constants/real.hpp>
 #include <nt2/table.hpp>
 
-#include <boost/array.hpp>
-
 
 NT2_TEST_CASE_TPL ( ratfracder_real__1_0, NT2_REAL_TYPES)
 {
@@ -36,14 +34,26 @@ NT2_TEST_CASE_TPL ( ratfracder_real__1_0, NT2_REAL_TYPES)
   nt2::table<T> b =  nt2::_(T(1), T(3));
   nt2::table<T> c(nt2::of_size(1, 0));
   nt2::table<T> n, d;
-//   T daab[] = { 5, 16, 30, 32, 17 };
-//   T daa [] = { 3, 4, 3};
-//   nt2::table<T> dab(nt2::of_size(1, 5), &daab[0], &daab[5]);
-//   nt2::table<T> da (nt2::of_size(1, 3), &daa[0], &daa[3]);
+  T rn[] = {  1, 4, 10, 4, 1  };
+  T rd [] = {  1, 4, 10, 12, 9 };
+  T dab[] = {  1, 4, 10, 4, 1  };
+  T da [] = { 3, 4, 3};
+  nt2::table<T> rdab(nt2::of_size(1, 5), &dab[0], &dab[5]);
+  NT2_DISPLAY(rdab);
+  nt2::table<T> rda (nt2::of_size(1, 3), &da[0], &da[3]);
+  NT2_DISPLAY(rda);
+  nt2::table<T> rnn (nt2::of_size(1, 5), &rn[0], &rn[5]);
+  NT2_DISPLAY(rnn);
+  nt2::table<T> rdd (nt2::of_size(1, 5), &rd[0], &rd[5]);
+  NT2_DISPLAY(rdd);
+  NT2_TEST_EQUAL(rda, ratfracder(a));
   NT2_DISPLAY(ratfracder(a));
+  NT2_TEST_EQUAL(rdab, ratfracder(a, b));
   NT2_DISPLAY(ratfracder(a, b));
   nt2::tie(n, d) = ratfracder(a, b);
+  NT2_TEST_EQUAL(rnn, n);
   NT2_DISPLAY(n);
+  NT2_TEST_EQUAL(rdd, d);
   NT2_DISPLAY(d);
 } // end of test for floating_
 
