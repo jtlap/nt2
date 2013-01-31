382c67f63075268ef41651b7cecf114a871e689e
Mathias Gaunard <mathias@gaunard.com>
2013-01-31 15:39:57 +0100

    clean up includes and formatting in ratfracder and conv

--- modules/core/polynom/include/nt2/toolbox/polynom/functions/expr/ratfracder.hpp
+++ modules/core/polynom/include/nt2/toolbox/polynom/functions/expr/ratfracder.hpp
@@ -8,14 +8,13 @@
 //==============================================================================
 #ifndef NT2_TOOLBOX_POLYNOM_FUNCTIONS_EXPR_RATFRACDER_HPP_INCLUDED
 #define NT2_TOOLBOX_POLYNOM_FUNCTIONS_EXPR_RATFRACDER_HPP_INCLUDED
+
 #include <nt2/toolbox/polynom/functions/ratfracder.hpp>
+#include <nt2/include/functions/minus.hpp>
 #include <nt2/include/functions/polyder.hpp>
 #include <nt2/include/functions/conv.hpp>
-#include <nt2/include/functions/colon.hpp>
-#include <nt2/include/functions/multiplies.hpp>
 #include <nt2/include/functions/numel.hpp>
-#include <nt2/include/constants/mone.hpp>
-#include <nt2/include/constants/one.hpp>
+#include <nt2/include/functions/extent.hpp>
 #include <nt2/include/functions/tie.hpp>
 
 /////////////////////////////////////////////////////////////////////////////
@@ -29,8 +28,8 @@ namespace nt2 { namespace ext
   // pair as inputs
   //============================================================================
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::ratfracder_, tag::cpu_
-                              , (A0)(N0)(A1)(N1)
-                              , ((node_<A0, nt2::tag::ratfracder_, N0, nt2::container::domain>))
+                            , (A0)(N0)(A1)(N1)
+                            , ((node_<A0, nt2::tag::ratfracder_, N0, nt2::container::domain>))
                               ((node_<A1, nt2::tag::tie_ , N1, nt2::container::domain>))
                             )
   {
@@ -51,19 +50,18 @@ namespace nt2 { namespace ext
       typedef typename boost::proto::result_of::child_c<A1&,1>::type       d_type;
       typedef typename boost::proto::result_of::child_c<A0&,0>::type       a_type;
       typedef typename boost::proto::result_of::child_c<A0&,1>::type       b_type;
-      a_type & a = boost::proto::child_c<0>(a0);
-      b_type & b = boost::proto::child_c<1>(a0);
-      n_type & n = boost::proto::child_c<0>(a1);
-      d_type & d = boost::proto::child_c<1>(a1);
+      a_type a = boost::proto::child_c<0>(a0);
+      b_type b = boost::proto::child_c<1>(a0);
+      n_type n = boost::proto::child_c<0>(a1);
+      d_type d = boost::proto::child_c<1>(a1);
       _2D sizee;
       n.resize(extent(a0));
-      n =  nt2::conv(nt2::polyder(a),b)-nt2::conv(nt2::polyder(b),a);
+      n = nt2::conv(nt2::polyder(a),b)-nt2::conv(nt2::polyder(b),a);
       sizee[0] = 1;
       sizee[1] = numel(b);
       sizee[1] = sizee[1] ? 2*sizee[1]-1:0;
       d.resize(sizee);
-      nt2::container::table<value_type, nt2::_2D> dd = nt2::conv(b, b);
-      d = dd;
+      d = nt2::conv(b, b);
     }
     BOOST_FORCEINLINE
     void val(A0& a0, A1& a1,
@@ -72,10 +70,10 @@ namespace nt2 { namespace ext
       typedef typename boost::proto::result_of::child_c<A1&,0>::type       n_type;
       typedef typename boost::proto::result_of::child_c<A0&,0>::type       a_type;
       typedef typename boost::proto::result_of::child_c<A0&,1>::type       b_type;
-      a_type & a = boost::proto::child_c<0>(a0);
-      b_type & b = boost::proto::child_c<1>(a0);
-      n_type & n = boost::proto::child_c<0>(a1);
-      n =  nt2::conv(nt2::polyder(a),b)-nt2::conv(nt2::polyder(b),a);
+      a_type a = boost::proto::child_c<0>(a0);
+      b_type b = boost::proto::child_c<1>(a0);
+      n_type n = boost::proto::child_c<0>(a1);
+      n = nt2::conv(nt2::polyder(a),b)-nt2::conv(nt2::polyder(b),a);
     }
     BOOST_FORCEINLINE
     void val(A0& a0, A1& a1,
@@ -83,9 +81,9 @@ namespace nt2 { namespace ext
     {
       typedef typename boost::proto::result_of::child_c<A1&,0>::type       n_type;
       typedef typename boost::proto::result_of::child_c<A0&,0>::type       a_type;
-      a_type & a = boost::proto::child_c<0>(a0);
-      n_type & n = boost::proto::child_c<0>(a1);
-      n =  polyder(a);
+      a_type a = boost::proto::child_c<0>(a0);
+      n_type n = boost::proto::child_c<0>(a1);
+      n = polyder(a);
     }
   };
 } }
