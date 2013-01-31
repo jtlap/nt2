a59244f1e38808458b009110b043ef456856fd18
Mathias Gaunard <mathias@gaunard.com>
2013-01-31 16:27:50 +0100

    removing unneeded includes in conv and ratfracder tests

--- modules/core/polynom/unit/scalar/conv.cpp
+++ modules/core/polynom/unit/scalar/conv.cpp
@@ -6,45 +6,32 @@
 ///                 See accompanying file LICENSE.txt or copy at
 ///                     http://www.boost.org/LICENSE_1_0.txt
 //////////////////////////////////////////////////////////////////////////////
-#define NT2_UNIT_MODULE "nt2 polynom toolbox - conv/scalar Mode"
+#define NT2_UNIT_MODULE "nt2 polynom toolbox - conv/table Mode"
 
 //////////////////////////////////////////////////////////////////////////////
-// unit test behavior of polynom components in scalar mode
+// unit test behavior of polynom components in table mode
 //////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 06/03/2011
-///
-#include <nt2/include/functions/conv.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/include/functions/isequal.hpp>
-#include <boost/type_traits/is_same.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/unit/tests.hpp>
-#include <nt2/sdk/unit/module.hpp>
-#include <nt2/sdk/memory/buffer.hpp>
-#include <nt2/include/constants/real.hpp>
+
 #include <nt2/table.hpp>
+#include <nt2/include/functions/conv.hpp>
 
-#include <boost/array.hpp>
-
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
 
 NT2_TEST_CASE_TPL ( conv_real__1_0,  NT2_REAL_TYPES)
 {
 
   using nt2::conv;
   using nt2::tag::conv_;
-  nt2::table<T> a =  nt2::_(T(1), T(4));
-  nt2::table<T> b =  nt2::_(T(1), T(3));
-   NT2_DISPLAY(a);
-   NT2_DISPLAY(b);
+  nt2::table<T> a = nt2::_(T(1), T(4));
+  nt2::table<T> b = nt2::_(T(1), T(3));
   nt2::table<T> c(nt2::of_size(1, 0));
+
   T aab[] = { 1, 4, 10, 16, 17, 12};
   nt2::table<T> ab(nt2::of_size(1, 6), &aab[0], &aab[6]);
-   NT2_DISPLAY(ab);
-   NT2_DISPLAY(conv(a, b));
-   NT2_TEST(nt2::isequal(ab,conv(a, b)));
-   NT2_DISPLAY(conv(a, c));
-   NT2_DISPLAY(conv(c, b));
-   NT2_DISPLAY(conv(b, b));
-} // end of test for floating_
+
+ NT2_TEST_EQUAL(ab, conv(a, b));
+
+}
 
 
