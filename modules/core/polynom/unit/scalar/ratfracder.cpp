2c5313d9f06c122ea681e64466f24905981ae2a7
Mathias Gaunard <mathias@gaunard.com>
2013-01-31 16:27:50 +0100

    removing unneeded includes in conv and ratfracder tests

--- modules/core/polynom/unit/scalar/ratfracder.cpp
+++ modules/core/polynom/unit/scalar/ratfracder.cpp
@@ -6,23 +6,19 @@
 ///                 See accompanying file LICENSE.txt or copy at
 ///                     http://www.boost.org/LICENSE_1_0.txt
 //////////////////////////////////////////////////////////////////////////////
-#define NT2_UNIT_MODULE "nt2 polynom toolbox - ratfracder/scalar Mode"
+#define NT2_UNIT_MODULE "nt2 polynom toolbox - ratfracder/table Mode"
 
 //////////////////////////////////////////////////////////////////////////////
 // unit test behavior of polynomials components in scalar mode
 //////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 06/03/2011
-///
-#include <nt2/include/functions/ratfracder.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
-#include <nt2/include/functions/isequal.hpp>
-#include <boost/type_traits/is_same.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/unit/tests.hpp>
-#include <nt2/sdk/unit/module.hpp>
-#include <nt2/sdk/memory/buffer.hpp>
-#include <nt2/include/constants/real.hpp>
+
 #include <nt2/table.hpp>
+#include <nt2/include/functions/ratfracder.hpp>
+#include <nt2/include/functions/tie.hpp>
+#include <nt2/include/functions/of_size.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
 
 
 NT2_TEST_CASE_TPL ( ratfracder_real__1_0, NT2_REAL_TYPES)
@@ -34,27 +30,21 @@ NT2_TEST_CASE_TPL ( ratfracder_real__1_0, NT2_REAL_TYPES)
   nt2::table<T> b =  nt2::_(T(1), T(3));
   nt2::table<T> c(nt2::of_size(1, 0));
   nt2::table<T> n, d;
-  T rn[] = {  1, 4, 10, 4, 1  };
-  T rd [] = {  1, 4, 10, 12, 9 };
-  T dab[] = {  1, 4, 10, 4, 1  };
-  T da [] = { 3, 4, 3};
+  T rn[]  = { 1, 4, 10,  4, 1 };
+  T rd [] = { 1, 4, 10, 12, 9 };
+  T dab[] = { 1, 4, 10,  4, 1 };
+  T da [] = { 3, 4, 3 };
   nt2::table<T> rdab(nt2::of_size(1, 5), &dab[0], &dab[5]);
-  NT2_DISPLAY(rdab);
   nt2::table<T> rda (nt2::of_size(1, 3), &da[0], &da[3]);
-  NT2_DISPLAY(rda);
   nt2::table<T> rnn (nt2::of_size(1, 5), &rn[0], &rn[5]);
-  NT2_DISPLAY(rnn);
   nt2::table<T> rdd (nt2::of_size(1, 5), &rd[0], &rd[5]);
-  NT2_DISPLAY(rdd);
+
   NT2_TEST_EQUAL(rda, ratfracder(a));
-  NT2_DISPLAY(ratfracder(a));
   NT2_TEST_EQUAL(rdab, ratfracder(a, b));
-  NT2_DISPLAY(ratfracder(a, b));
+
   nt2::tie(n, d) = ratfracder(a, b);
   NT2_TEST_EQUAL(rnn, n);
-  NT2_DISPLAY(n);
   NT2_TEST_EQUAL(rdd, d);
-  NT2_DISPLAY(d);
-} // end of test for floating_
+}
 
 
