53f904cbe2c070e18cbd84fa7f7cdcfbe9bb41fb
Mathias Gaunard <mathias@gaunard.com>
2013-02-08 22:58:58 +0100

    fix computation of default alignment in aligned_array: must be in bytes (so multiply by sizeof(T)) + must be a power of 2

--- modules/boost/simd/sdk/include/boost/simd/sdk/memory/aligned_array_fwd.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/memory/aligned_array_fwd.hpp
@@ -13,9 +13,23 @@
 
 namespace boost { namespace simd { namespace memory
 {
+  /* Default alignment is BOOST_SIMD_ARCH_ALIGNMENT or the largest power of 2
+   * smaller than N*sizeof(T), whichever is smaller */
   template< class T
           , std::size_t N
-          , std::size_t Align = (BOOST_SIMD_ARCH_ALIGNMENT>N)?N:BOOST_SIMD_ARCH_ALIGNMENT>
+          , std::size_t Align = (BOOST_SIMD_ARCH_ALIGNMENT > (N*sizeof(T)))
+                              ? (  ( ( (N*sizeof(T))
+                                     | (N*sizeof(T)) >> 1
+                                     | (N*sizeof(T)) >> 2
+                                     | (N*sizeof(T)) >> 4
+                                     | (N*sizeof(T)) >> 8
+                                     | (N*sizeof(T)) >> 16
+                                     )
+                                     >> 1
+                                   ) + 1
+                                )
+                              : BOOST_SIMD_ARCH_ALIGNMENT
+          >
   struct aligned_array;
 } } }
 
