d1def98c8198ead253fecfd3a13ed29008202b82
jtlap <jtlapreste@gmail.com>
2013-02-15 13:41:37 +0100

    jordbloc for gallery

---
+++ modules/core/linalg/include/nt2/toolbox/linalg/functions/gallery/jordbloc.hpp
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ *         Copyright 2003-2012 LASMEA UMR 6602 CNRS/U.B.P
+ *         Copyright 2011-2012 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_GALLERY_VANDERMONDE_HPP_INCLUDED
+#define NT2_TOOLBOX_LINALG_FUNCTIONS_GALLERY_VANDERMONDE_HPP_INCLUDED
+#include <nt2/toolbox/linalg/functions/vandermonde.hpp>
+#include <nt2/include/functions/eye.hpp>
+#include <nt2/include/functions/from_diag.hpp>
+#include <nt2/include/functions/ones.hpp>
+
+namespace nt2 {namespace ext
+{
+
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::jordbloc_, tag::cpu_,
+                              (A0)(A1),
+                              (scalar_<integer_<A0> >)
+                              (scalar_<floating_<A1> >)
+                            )
+  {
+    BOOST_DISPATCH_RETURNS(2, (A0 const& n, A1 const& lambda),
+                           (lambda*nt2::eye(n, meta::as_<A1>())+ nt2::from_diag(ones(n-1,1, meta::as_<A1>()),1))
+                          )
+  };
+
+} }
+
+#endif
---
+++ modules/core/linalg/unit/functions/jordbloc.cpp
@@ -0,0 +1,43 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#define NT2_UNIT_MODULE "nt2 linalg toolbox - jordbloc function"
+
+#include <nt2/table.hpp>
+#include <nt2/include/functions/jordbloc.hpp>
+#include <nt2/include/functions/trans.hpp>
+#include <nt2/include/functions/ones.hpp>
+#include <nt2/include/functions/cons.hpp>
+#include <nt2/include/functions/cast.hpp>
+#include <nt2/include/constants/i.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/exceptions.hpp>
+
+NT2_TEST_CASE_TPL ( jordbloc, NT2_REAL_TYPES)
+{
+  nt2::table<T> t3 =nt2::trans(nt2::reshape(nt2::cons(
+                                              T(1), T(1), T(0),
+                                              T(0), T(1), T(1),
+                                              T(0), T(0), T(1)),
+                                            nt2::of_size(3, 3)));
+  NT2_DISPLAY(t3);
+  nt2::table<T> v = nt2::jordbloc(3, T(1.0));
+  NT2_DISPLAY(v);
+  nt2::table<T> v1=  nt2::jordbloc<T>(3);
+  NT2_DISPLAY(v1);
+  nt2::table<T> v2=  nt2::jordbloc(3, 1, nt2::meta::as_<T>());
+  NT2_DISPLAY(v2);
+  NT2_TEST_EQUAL(v, t3);
+  NT2_TEST_EQUAL(v1, t3);
+  NT2_TEST_EQUAL(v2, t3);
+
+}
+
+
+
