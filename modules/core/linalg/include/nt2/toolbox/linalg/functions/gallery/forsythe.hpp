b95941c5f52356efe406d0af4121cd1bf68e3241
jtlap <jtlapreste@gmail.com>
2013-02-16 11:56:05 +0100

    jordbloc and forsythe for gallery

--- modules/core/linalg/include/nt2/toolbox/linalg/functions/gallery/forsythe.hpp
+++ modules/core/linalg/include/nt2/toolbox/linalg/functions/gallery/forsythe.hpp
@@ -6,24 +6,25 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_GALLERY_JORDBLOC_HPP_INCLUDED
-#define NT2_TOOLBOX_LINALG_FUNCTIONS_GALLERY_JORDBLOC_HPP_INCLUDED
-#include <nt2/toolbox/linalg/functions/jordbloc.hpp>
-#include <nt2/include/functions/eye.hpp>
+#ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_GALLERY_FORSYTHE_HPP_INCLUDED
+#define NT2_TOOLBOX_LINALG_FUNCTIONS_GALLERY_FORSYTHE_HPP_INCLUDED
+#include <nt2/toolbox/linalg/functions/forsythe.hpp>
+#include <nt2/include/functions/jordbloc.hpp>
 #include <nt2/include/functions/from_diag.hpp>
 #include <nt2/include/functions/ones.hpp>
 
 namespace nt2 {namespace ext
 {
 
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::jordbloc_, tag::cpu_,
-                              (A0)(A1),
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::forsythe_, tag::cpu_,
+                              (A0)(A1)(A2),
                               (scalar_<integer_<A0> >)
                               (scalar_<floating_<A1> >)
+                              (scalar_<floating_<A2> >)
                             )
   {
-    BOOST_DISPATCH_RETURNS(2, (A0 const& n, A1 const& lambda),
-                           (lambda*nt2::eye(n, meta::as_<A1>())+ nt2::from_diag(ones(n-1,1, meta::as_<A1>()),1))
+    BOOST_DISPATCH_RETURNS(3, (A0 const& n, A1 const& alpha, A2 const& l),
+                           (nt2::jordbloc(n, l)+alpha*nt2::from_diag(nt2::ones(1, 1, meta::as_<A1>()),-ptrdiff_t(n-1)))
                           )
   };
 
