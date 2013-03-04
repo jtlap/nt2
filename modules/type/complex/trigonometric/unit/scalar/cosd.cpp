bc7d01bd1cf4d286652bff031b77a1ccc67ac730
Mathias Gaunard <mathias@gaunard.com>
2013-03-04 19:22:27 +0100

    normalizing license header in all C++ files

--- modules/type/complex/trigonometric/unit/scalar/cosd.cpp
+++ modules/type/complex/trigonometric/unit/scalar/cosd.cpp
@@ -1,11 +1,11 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #define NT2_UNIT_MODULE "nt2 cosdonential toolbox - cosd/scalar Mode"
 
 //////////////////////////////////////////////////////////////////////////////
