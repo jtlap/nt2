bc701605bc46a583e949c0ba5c93814ac454bb74
jtlap <jtlapreste@gmail.com>
2013-03-11 09:13:52 +0100

    ris for gallery

---
+++ modules/core/linalg/include/nt2/toolbox/linalg/functions/gallery/ris.hpp
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ *         Copyright 2003-2012 LASMEA UMR 6602 CNRS/U.B.P
+ *         Copyright 2011-2012 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+ *
+ *          Distributed under the Boost Software License, Version 1.0.
+ *                 See accompanying file LICENSE.txt or copy at
+ *                     http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+#ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_GALLERY_RIS_HPP_INCLUDED
+#define NT2_TOOLBOX_LINALG_FUNCTIONS_GALLERY_RIS_HPP_INCLUDED
+#include <nt2/toolbox/linalg/functions/ris.hpp>
+#include <nt2/include/functions/cauchy.hpp>
+#include <nt2/include/functions/colon.hpp>
+#include <nt2/include/functions/fma.hpp>
+#include <nt2/include/constants/mtwo.hpp>
+
+namespace nt2
+{
+
+  namespace ext
+  {
+
+    NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::ris_, tag::cpu_,
+                                (A0)(T),
+                                (scalar_<integer_<A0> >)
+                                (target_<scalar_<floating_<T> > > )
+                              )
+    {
+      typedef typename T::type t_t;
+      BOOST_DISPATCH_RETURNS(2, (A0 const& n, T const& t),
+                             (nt2::cauchy(nt2::fma(nt2::Mtwo<t_t>(), nt2::_(t_t(1), t_t(n)),(t_t(n+1.5)))))
+                            )
+        };
+
+  }
+
+  meta::call<tag::ris_(const ptrdiff_t &, meta::as_<double> const &)>::type
+  ris(ptrdiff_t n)
+  {
+    return nt2::ris(n,  meta::as_<double>());
+  }
+  template<class T>
+  typename meta::call<tag::ris_(const ptrdiff_t &, typename meta::as_<T> const &)>::type
+  ris(ptrdiff_t n)
+  {
+    return nt2::ris(n,  meta::as_<T>());
+  }
+
+}
+
+
+#endif
