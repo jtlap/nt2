1db0104272f2ee0cd7a60d8056619bef8558d85a
Mathias Gaunard <mathias@gaunard.com>
2013-03-12 15:54:47 +0100

    normalizing more license headers

--- modules/core/linalg/include/nt2/toolbox/linalg/functions/details/lsq_lse_solve.hpp
+++ modules/core/linalg/include/nt2/toolbox/linalg/functions/details/lsq_lse_solve.hpp
@@ -1,11 +1,11 @@
-/*******************************************************************************
- *         Copyright 2003-2012 LASMEA UMR 6602 CNRS/U.B.P
- *         Copyright 2011-2012 LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_DETAILS_LSQ_LSE_SOLVE_HPP_INCLUDED
 #define NT2_TOOLBOX_LINALG_FUNCTIONS_DETAILS_LSQ_LSE_SOLVE_HPP_INCLUDED
 
--- modules/core/trigonometric/include/nt2/toolbox/trigonometric/functions/scalar/impl/selection_tags.hpp
+++ modules/core/trigonometric/include/nt2/toolbox/trigonometric/functions/scalar/impl/selection_tags.hpp
@@ -1,11 +1,11 @@
-/*******************************************************************************
- *         Copyright 2003-2010 LASMEA UMR 6602 CNRS/U.B.P
- *         Copyright 2009-2010 LRI    UMR 8623 CNRS/Univ Paris Sud XI
- *
- *          Distributed under the Boost Software License, Version 1.0.
- *                 See accompanying file LICENSE.txt or copy at
- *                     http://www.boost.org/LICENSE_1_0.txt
- ******************************************************************************/
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_TOOLBOX_TRIGONOMETRIC_FUNCTIONS_SCALAR_IMPL_SELECTION_TAGS_HPP_INCLUDED
 #define NT2_TOOLBOX_TRIGONOMETRIC_FUNCTIONS_SCALAR_IMPL_SELECTION_TAGS_HPP_INCLUDED
 #include <nt2/sdk/meta/tags.hpp>
