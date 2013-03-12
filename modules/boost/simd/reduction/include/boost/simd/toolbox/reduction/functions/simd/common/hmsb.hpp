ebd5365c38c517dfa7d3cfddee2af0389babb977
Joel Falcou <joel.falcou@lri.fr>
2013-03-12 19:27:49 +0100

    Added support for native<logical> in hmsb

--- modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/simd/common/hmsb.hpp
+++ modules/boost/simd/reduction/include/boost/simd/toolbox/reduction/functions/simd/common/hmsb.hpp
@@ -9,18 +9,21 @@
 #ifndef BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_COMMON_HMSB_HPP_INCLUDED
 #define BOOST_SIMD_TOOLBOX_REDUCTION_FUNCTIONS_SIMD_COMMON_HMSB_HPP_INCLUDED
 #include <boost/simd/toolbox/reduction/functions/hmsb.hpp>
+#include <boost/simd/include/functions/bitwise_cast.hpp>
 #include <boost/simd/include/functions/simd/bits.hpp>
 #include <boost/simd/sdk/meta/cardinal_of.hpp>
+#include <boost/simd/sdk/simd/logical.hpp>
 #include <climits>
+#include <cstddef>
 
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::hmsb_, tag::cpu_,
-                       (A0)(X),
-                       ((simd_<arithmetic_<A0>,X>))
-                      )
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::hmsb_, tag::cpu_
+                                    , (A0)(X)
+                                    , ((simd_<arithmetic_<A0>,X>))
+                                    )
   {
-    typedef size_t result_type;
+    typedef std::size_t result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
       typedef typename meta::scalar_of<A0>::type stype;
@@ -34,5 +37,19 @@ namespace boost { namespace simd { namespace ext
       return z;
     }
   };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::hmsb_, tag::cpu_
+                                    , (A0)(X)
+                                    , ((simd_<logical_<A0>,X>))
+                                    )
+  {
+    typedef std::size_t result_type;
+    BOOST_SIMD_FUNCTOR_CALL(1)
+    {
+      typedef typename meta::as_arithmetic<A0>::type tgt_t;
+      return boost::simd::hmsb(bitwise_cast<tgt_t>(a0));
+    }
+  };
 } } }
+
 #endif
--- modules/core/reduction/unit/simd/hmsb.cpp
+++ modules/core/reduction/unit/simd/hmsb.cpp
@@ -6,102 +6,83 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 reduction toolbox - hmsb/simd Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// unit test behavior of reduction components in simd mode
-//////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 24/02/2011
-///
 #include <nt2/toolbox/reduction/include/functions/hmsb.hpp>
 #include <boost/simd/sdk/simd/native.hpp>
+#include <boost/simd/sdk/simd/logical.hpp>
 #include <nt2/include/functions/bits.hpp>
 #include <nt2/include/functions/shri.hpp>
 
-#include <boost/type_traits/is_same.hpp>
 #include <boost/dispatch/functor/meta/call.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
 #include <nt2/sdk/unit/module.hpp>
-#include <boost/simd/toolbox/constant/constant.hpp>
-#include <boost/dispatch/details/ignore_unused.hpp>
-#include <boost/simd/sdk/memory/is_aligned.hpp>
-#include <boost/simd/sdk/memory/aligned_type.hpp>
-#include <boost/simd/include/functions/load.hpp>
-#include <boost/simd/toolbox/constant/constant.hpp>
-#include <boost/dispatch/details/ignore_unused.hpp>
-
+#include <nt2/toolbox/constant/constant.hpp>
 
 NT2_TEST_CASE_TPL ( hmsb_real__1_0,  BOOST_SIMD_SIMD_REAL_TYPES)
 {
-  using boost::simd::hmsb;
-  using boost::simd::tag::hmsb_;
-  using boost::simd::load;
+  using nt2::hmsb;
+  using nt2::tag::hmsb_;
   using boost::simd::native;
   using boost::simd::meta::cardinal_of;
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename boost::dispatch::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
+  typedef native<T,ext_t>                        vT;
   typedef typename boost::dispatch::meta::call<hmsb_(vT)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
 
   // specific values tests
-  NT2_TEST_EQUAL(hmsb(boost::simd::Allbits<vT>()), sr_t((1ull << boost::simd::meta::cardinal_of<vT>::value) - 1));
-  NT2_TEST_EQUAL(hmsb(boost::simd::Inf<vT>()), boost::simd::Zero<sr_t>());
-  NT2_TEST_EQUAL(hmsb(boost::simd::Minf<vT>()), sr_t(boost::simd::shri(boost::simd::Mone<boost::simd::int32_t>(),32-boost::simd::meta::cardinal_of<vT>::value)));
-  NT2_TEST_EQUAL(hmsb(boost::simd::Mone<vT>()), sr_t(boost::simd::shri(boost::simd::Mone<boost::simd::int32_t>(),32-boost::simd::meta::cardinal_of<vT>::value)));
-  NT2_TEST_EQUAL(hmsb(boost::simd::Nan<vT>()),  sr_t(boost::simd::shri(boost::simd::Mone<boost::simd::int32_t>() ,32-boost::simd::meta::cardinal_of<vT>::value)));
-  NT2_TEST_EQUAL(hmsb(boost::simd::One<vT>()), boost::simd::Zero<sr_t>());
-  NT2_TEST_EQUAL(hmsb(boost::simd::Signmask<vT>()), sr_t((1ull << boost::simd::meta::cardinal_of<vT>::value) - 1));
-  NT2_TEST_EQUAL(hmsb(boost::simd::Zero<vT>()), boost::simd::Zero<sr_t>());
+  NT2_TEST_EQUAL(hmsb(nt2::Allbits<vT>()), r_t((1ull << cardinal_of<vT>::value) - 1));
+  NT2_TEST_EQUAL(hmsb(nt2::Inf<vT>()), nt2::Zero<r_t>());
+  NT2_TEST_EQUAL(hmsb(nt2::Minf<vT>()), r_t(nt2::shri(nt2::Mone<nt2::int32_t>(),32-cardinal_of<vT>::value)));
+  NT2_TEST_EQUAL(hmsb(nt2::Mone<vT>()), r_t(nt2::shri(nt2::Mone<nt2::int32_t>(),32-cardinal_of<vT>::value)));
+  NT2_TEST_EQUAL(hmsb(nt2::Nan<vT>()), r_t(nt2::shri(nt2::Mone<nt2::int32_t>(),32-cardinal_of<vT>::value)));
+  NT2_TEST_EQUAL(hmsb(nt2::One<vT>()), nt2::Zero<r_t>());
+  NT2_TEST_EQUAL(hmsb(nt2::Signmask<vT>()), r_t((1ull << cardinal_of<vT>::value) - 1));
+  NT2_TEST_EQUAL(hmsb(nt2::Zero<vT>()), nt2::Zero<r_t>());
 } // end of test for real_
 
 NT2_TEST_CASE_TPL ( hmsb_signed_int__1_0,  BOOST_SIMD_SIMD_INTEGRAL_SIGNED_TYPES)
 {
-  using boost::simd::hmsb;
-  using boost::simd::tag::hmsb_;
-  using boost::simd::load;
+  using nt2::hmsb;
+  using nt2::tag::hmsb_;
   using boost::simd::native;
   using boost::simd::meta::cardinal_of;
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename boost::dispatch::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
+  typedef native<T,ext_t>                        vT;
   typedef typename boost::dispatch::meta::call<hmsb_(vT)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
 
   // specific values tests
-  NT2_TEST_EQUAL(hmsb(boost::simd::Allbits<vT>()), sr_t((1ull << boost::simd::meta::cardinal_of<vT>::value) - 1));
-  NT2_TEST_EQUAL(hmsb(boost::simd::One<vT>()), boost::simd::Zero<sr_t>());
-  NT2_TEST_EQUAL(hmsb(boost::simd::Signmask<vT>()), sr_t((1ull << boost::simd::meta::cardinal_of<vT>::value) - 1));
-  NT2_TEST_EQUAL(hmsb(boost::simd::Zero<vT>()), boost::simd::Zero<sr_t>());
+  NT2_TEST_EQUAL(hmsb(nt2::Allbits<vT>()), r_t((1ull << cardinal_of<vT>::value) - 1));
+  NT2_TEST_EQUAL(hmsb(nt2::One<vT>()), nt2::Zero<r_t>());
+  NT2_TEST_EQUAL(hmsb(nt2::Signmask<vT>()), r_t((1ull << cardinal_of<vT>::value) - 1));
+  NT2_TEST_EQUAL(hmsb(nt2::Zero<vT>()), nt2::Zero<r_t>());
 } // end of test for signed_int_
 
 NT2_TEST_CASE_TPL ( hmsb_unsigned_int__1_0,  BOOST_SIMD_SIMD_UNSIGNED_TYPES)
 {
-  using boost::simd::hmsb;
-  using boost::simd::tag::hmsb_;
-  using boost::simd::load;
+  using nt2::hmsb;
+  using nt2::tag::hmsb_;
   using boost::simd::native;
   using boost::simd::meta::cardinal_of;
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename boost::dispatch::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
+  typedef native<T,ext_t>                        vT;
   typedef typename boost::dispatch::meta::call<hmsb_(vT)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
 
   // specific values tests
-  NT2_TEST_EQUAL(hmsb(boost::simd::Allbits<vT>()), sr_t((1ull << boost::simd::meta::cardinal_of<vT>::value) - 1));
-  NT2_TEST_EQUAL(hmsb(boost::simd::One<vT>()), boost::simd::Zero<sr_t>());
-  NT2_TEST_EQUAL(hmsb(boost::simd::Zero<vT>()), boost::simd::Zero<sr_t>());
+  NT2_TEST_EQUAL(hmsb(nt2::Allbits<vT>()), r_t((1ull << cardinal_of<vT>::value) - 1));
+  NT2_TEST_EQUAL(hmsb(nt2::One<vT>()), nt2::Zero<r_t>());
+  NT2_TEST_EQUAL(hmsb(nt2::Zero<vT>()), nt2::Zero<r_t>());
 } // end of test for unsigned_int_
+
+NT2_TEST_CASE_TPL ( hmsb_logical, BOOST_SIMD_SIMD_TYPES)
+{
+  using nt2::hmsb;
+  using nt2::tag::hmsb_;
+  using boost::simd::native;
+  using boost::simd::logical;
+  using boost::simd::meta::cardinal_of;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<logical<T>,ext_t>                          vT;
+  typedef typename boost::dispatch::meta::call<hmsb_(vT)>::type r_t;
+
+  // specific values tests
+  NT2_TEST_EQUAL(hmsb(nt2::True<vT>())  , r_t((1ull << cardinal_of<vT>::value) - 1));
+  NT2_TEST_EQUAL(hmsb(nt2::False<vT>()) , nt2::Zero<r_t>());
+}
