596f95d87b5d2ef4e06d854468b2852cf70e2e8e
jtlap <jtlapreste@gmail.com>
2013-03-25 09:06:14 +0100

    correcting ulps for randjorth test

--- modules/core/linalg/unit/functions/randjorth.cpp
+++ modules/core/linalg/unit/functions/randjorth.cpp
@@ -18,6 +18,7 @@
 #include <nt2/include/functions/globalmax.hpp>
 #include <nt2/include/functions/eye.hpp>
 #include <nt2/include/constants/sqrteps.hpp>
+#include <nt2/include/constants/two.hpp>
 #include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
 #include <nt2/sdk/unit/module.hpp>
@@ -33,26 +34,26 @@ NT2_TEST_CASE_TPL ( randjorth, NT2_REAL_TYPES)
   rc = nt2::randjorth(2, 2, nt2::rec(nt2::Sqrteps<T>()), 1);
   p  = nt2::mtimes( nt2::mtimes(nt2::transpose(rc), j), rc);
   NT2_TEST_LESSER(nt2::abs(nt2::cond(rc)-nt2::rec(nt2::Sqrteps<T>())), T(1));
-  NT2_TEST_LESSER(nt2::globalmax(nt2::abs(j-p)), nt2::Sqrteps<T>());
+  NT2_TEST_LESSER(nt2::globalmax(nt2::abs(j-p)), nt2::Two<T>()*nt2::Sqrteps<T>());
 
   rc = nt2::randjorth(2, 2, nt2::rec(nt2::Sqrteps<T>()), 0);
   p  = nt2::mtimes( nt2::mtimes(nt2::transpose(rc), j), rc);
   NT2_TEST_LESSER(nt2::abs(nt2::cond(rc)-nt2::rec(nt2::Sqrteps<T>())), T(1));
-  NT2_TEST_LESSER(nt2::globalmax(nt2::abs(j-p)), nt2::Sqrteps<T>());
+  NT2_TEST_LESSER(nt2::globalmax(nt2::abs(j-p)),  nt2::Two<T>()*nt2::Sqrteps<T>());
 
   rc = nt2::randjorth(3, 4, T(2));
   j = nt2::blkdiag(nt2::eye(3, nt2::meta::as_<T>()),
                    -nt2::eye(4, nt2::meta::as_<T>()));
   p  = nt2::mtimes( nt2::mtimes(nt2::transpose(rc), j), rc);
   NT2_TEST_LESSER(nt2::abs(nt2::cond(rc)-T(2)), T(1.0e-3));
-  NT2_TEST_LESSER(nt2::globalmax(nt2::abs(j-p)), nt2::Sqrteps<T>());
+  NT2_TEST_LESSER(nt2::globalmax(nt2::abs(j-p)),  nt2::Two<T>()*nt2::Sqrteps<T>());
 
   rc = nt2::randjorth(4, 3, T(2));
   j = nt2::blkdiag(nt2::eye(4, nt2::meta::as_<T>()),
                    -nt2::eye(3, nt2::meta::as_<T>()));
   p  = nt2::mtimes( nt2::mtimes(nt2::transpose(rc), j), rc);
   NT2_TEST_LESSER(nt2::abs(nt2::cond(rc)-T(2)), T(1.0e-3));
-  NT2_TEST_LESSER(nt2::globalmax(nt2::abs(j-p)), nt2::Sqrteps<T>());
+  NT2_TEST_LESSER(nt2::globalmax(nt2::abs(j-p)),  nt2::Two<T>()*nt2::Sqrteps<T>());
 
 }
 
