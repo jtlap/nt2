d6b58d4aee8f86685f70fb4c5c2e017b8629fbcb
Ian Masliah <ian.masliah@u-psud.fr>
2013-03-28 16:15:37 +0100

    Clean-up and refurbishing of LU decomposition
    Implementation of lu now uses an architecture-dependent version
    of trf, which is a direct mapping to the underlying LAPACK kernel.
    This allows LU to use MAGMA and other specific version of BLAS/LAPACK.
    
    In addition, proper allocation avoiding scheme is now in place
    whenever possible for calls of tie(l,u,...) = lu(a). Previous code
    was overcopying in some situations (like non-square input).
    
    This change deprecates the factorization namespace as the internals
    of the factorization and the memory handling is done by trf or by
    specific version of LU itself. Functions depending on other members
    of the defunct lu_result type will be rewrote later using trf.

--- modules/core/linalg/include/nt2/linalg/functions/tied/lu.hpp
+++ modules/core/linalg/include/nt2/linalg/functions/tied/lu.hpp
@@ -10,63 +10,134 @@
 #define NT2_LINALG_FUNCTIONS_TIED_LU_HPP_INCLUDED
 
 #include <nt2/linalg/functions/lu.hpp>
-#include <nt2/linalg/functions/factorizations/lu.hpp>
 #include <nt2/include/functions/assign.hpp>
 #include <nt2/include/functions/tie.hpp>
+#include <nt2/include/functions/trf.hpp>
+#include <nt2/include/functions/triu.hpp>
+#include <nt2/include/functions/tri1l.hpp>
+#include <nt2/include/functions/colon.hpp>
+#include <nt2/include/functions/zeros.hpp>
+#include <nt2/include/functions/width.hpp>
+#include <nt2/include/functions/numel.hpp>
+#include <nt2/include/functions/height.hpp>
+#include <nt2/include/functions/issquare.hpp>
+#include <nt2/sdk/meta/concrete.hpp>
+#include <boost/dispatch/meta/terminal_of.hpp>
 
 namespace nt2 { namespace ext
 {
-  //============================================================================
-  // Capture a tie(l, u, p) = lu(...) at assign time and resolve to optimized call
-  //============================================================================
+  /// INTERNAL ONLY - D = LU(A)
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::lu_, tag::cpu_
-                            , (A0)(N0)(A1)(N1)
-                            , ((node_<A0, nt2::tag::lu_, N0, nt2::container::domain>))
-                              ((node_<A1, nt2::tag::tie_ , N1, nt2::container::domain>))
+                            , (A0)(N0)(A1)
+                            , ((node_ < A0, nt2::tag::lu_
+                                      , N0, nt2::container::domain
+                                      >
+                              ))
+                              ((node_ < A1, nt2::tag::tie_
+                                      , boost::mpl::long_<1>
+                                      , nt2::container::domain
+                                      >
+                              ))
                             )
   {
-    typedef void                                                    result_type;
-    typedef typename boost::proto::result_of::child_c<A1&,0>::type  child0;
-    typedef typename meta::
-            call< nt2::tag::
-                  factorization::lu_(child0,nt2::details::in_place_)
-                >::type                                             fact_t;
+    typedef void  result_type;
 
     BOOST_FORCEINLINE result_type operator()( A0& a0, A1& a1 ) const
     {
-      // Copy data in output first
-      boost::proto::child_c<0>(a1) = boost::proto::child_c<0>(a0);
+      nt2::table<nt2_la_int> ip;
 
-      // Factorize in place
-      fact_t f = factorization::lu(boost::proto::child_c<0>(a1),in_place_);
+      BOOST_AUTO_TPL( out, concrete(boost::proto::child_c<0>(a1)) );
+      out = boost::proto::child_c<0>(a0);
+      nt2::trf(out,ip);
 
-      decomp(f, a1, N1());
+      boost::proto::child_c<0>(a1) = out;
+    }
+  };
+
+  /// INTERNAL ONLY - [L,U,P*] = LU(A)
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::lu_, tag::cpu_
+                            , (A0)(N0)(A1)(N1)
+                            , ((node_ < A0, nt2::tag::lu_
+                                      , N0, nt2::container::domain
+                                      >
+                              ))
+                              ((node_ < A1, nt2::tag::tie_
+                                      , N1, nt2::container::domain
+                                      >
+                              ))
+                            )
+  {
+    typedef void                                              result_type;
+    typedef typename boost::proto::result_of
+                                 ::child_c<A1&,0>::value_type c0_t;
+    typedef typename boost::dispatch::meta::
+            terminal_of< typename boost::dispatch
+                                       ::meta::semantic_of<c0_t>::type
+                       >::type                                tab0_t;
+
+    BOOST_FORCEINLINE result_type operator()( A0& a0, A1& a1 ) const
+    {
+      nt2::table<nt2_la_int> ip;
+      decomp(a0, a1, ip, N1());
     }
 
-    private:
-    //==========================================================================
-    // INTERNAL ONLY
-    // fill the args out
-    //==========================================================================
+    /// INTERNAL ONLY - [L,U] = LU(A)
     BOOST_FORCEINLINE
-    void decomp(fact_t & f, A1 & a1, boost::mpl::long_<1> const&) const
+    void decomp ( A0& a0, A1& a1, nt2::table<nt2_la_int>& ip
+                , boost::mpl::long_<2> const&
+                ) const
     {
-       boost::proto::child_c<0>(a1) = f.values();
+      if( nt2::issquare(boost::proto::child_c<0>(a0)) )
+      {
+        BOOST_AUTO_TPL( work, concrete(boost::proto::child_c<0>(a1)) );
+        work = boost::proto::child_c<0>(a0);
+        nt2::trf(work,ip);
+
+        extract_lu(a1,work);
+      }
+      else
+      {
+        tab0_t work = boost::proto::child_c<0>(a0);
+        nt2::trf(work,ip);
+        extract_lu(a1,work);
+      }
     }
 
+    /// INTERNAL ONLY - [L,U,P] = LU(A)
     BOOST_FORCEINLINE
-    void decomp(fact_t & f, A1 & a1, boost::mpl::long_<2> const&) const
+    void decomp ( A0& a0, A1& a1, nt2::table<nt2_la_int>& ip
+                , boost::mpl::long_<3> const&
+                ) const
     {
-      boost::proto::child_c<0>(a1) = f.l();
-      boost::proto::child_c<1>(a1) = f.u();
+      decomp(a0,a1,ip,boost::mpl::long_<2>());
+      extract_p(a1,ip);
     }
 
-    BOOST_FORCEINLINE
-    void decomp(fact_t & f, A1 & a1, boost::mpl::long_<3> const&) const
+    /// INTERNAL ONLY - Helper for L/U extraction
+    template<typename W>
+    BOOST_FORCEINLINE void extract_lu(A1& a1, W& work) const
     {
-      boost::proto::child_c<0>(a1) = f.l();
-      boost::proto::child_c<1>(a1) = f.u();
-      boost::proto::child_c<2>(a1) = f.p();
+      std::size_t d  = dim(work);
+      boost::proto::child_c<1>(a1) = nt2::triu( work( _(1, d), _) );
+      boost::proto::child_c<0>(a1) = nt2::tri1l( work(_, _(1, d) ) );
+    }
+
+    /// INTERNAL ONLY - Size of L/U
+    template<typename W>
+    BOOST_FORCEINLINE std::size_t dim(W const& work) const
+    {
+      return std::min(nt2::height(work),nt2::width(work));
+    }
+
+    /// INTERNAL ONLY
+    BOOST_FORCEINLINE
+    void extract_p(A1& a1, nt2::table<nt2_la_int>& ip) const
+    {
+      std::size_t d = nt2::numel(ip);
+      boost::proto::child_c<2>(a1) = nt2::zeros(d);
+
+      for(std::size_t i = 1; i<= d;++i)
+        boost::proto::child_c<2>(a1)(i,ip(i)) = 1;
     }
   };
 } }
