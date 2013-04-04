3afd080b9138225ccdb8c674aba414e141b31ec7
Mathias Gaunard <mathias@gaunard.com>
2013-04-04 22:16:43 +0200

    missing as_floating include in spence

--- modules/core/euler/include/nt2/toolbox/euler/functions/simd/common/spence.hpp
+++ modules/core/euler/include/nt2/toolbox/euler/functions/simd/common/spence.hpp
@@ -8,11 +8,8 @@
 //==============================================================================
 #ifndef NT2_TOOLBOX_EULER_FUNCTIONS_SIMD_COMMON_SPENCE_HPP_INCLUDED
 #define NT2_TOOLBOX_EULER_FUNCTIONS_SIMD_COMMON_SPENCE_HPP_INCLUDED
+
 #include <nt2/toolbox/euler/functions/spence.hpp>
-#include <nt2/sdk/simd/meta/is_real_convertible.hpp>
-#include <nt2/include/constants/digits.hpp>
-#include <nt2/include/constants/real.hpp>
-#include <nt2/sdk/meta/strip.hpp>
 #include <nt2/include/functions/simd/tofloat.hpp>
 #include <nt2/include/functions/simd/log.hpp>
 #include <nt2/include/functions/simd/polevl.hpp>
@@ -21,6 +18,9 @@
 #include <nt2/include/functions/simd/sqr.hpp>
 #include <nt2/include/functions/simd/if_else.hpp>
 #include <nt2/include/functions/simd/logical_or.hpp>
+#include <nt2/include/constants/digits.hpp>
+#include <nt2/include/constants/real.hpp>
+#include <nt2/sdk/meta/as_floating.hpp>
 #include <nt2/sdk/simd/logical.hpp>
 
 
