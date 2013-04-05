a6f1fcc6ec21b2dc67a84f84d3d184e8202949fe
Mathias Gaunard <mathias@gaunard.com>
2013-04-05 12:20:52 +0200

    rem_2pi, some case did not pass by the parameter by reference

--- modules/core/trigonometric/include/nt2/toolbox/trigonometric/functions/generic/rem_2pi.hpp
+++ modules/core/trigonometric/include/nt2/toolbox/trigonometric/functions/generic/rem_2pi.hpp
@@ -59,7 +59,7 @@ namespace nt2 { namespace ext
     )
   {
     typedef A0 result_type;
-    inline result_type operator()(A0 const a0, A1 const&) const
+    inline result_type operator()(A0 const& a0, A1 const&) const
     {
       typedef typename A1::type selector;
       return rem2pi<selector, void>::rem(a0);
