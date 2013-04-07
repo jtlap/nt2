42d4813fa2abb89a5f2029ca32ad2ab32791c402
jtlap <jtlapreste@gmail.com>
2013-04-07 14:43:02 +0200

    modifying acsch.cpp to fit the policy: integer 0 is considered as floating +0 in math calls

--- modules/core/hyperbolic/unit/scalar/acsch.cpp
+++ modules/core/hyperbolic/unit/scalar/acsch.cpp
@@ -79,7 +79,7 @@ NT2_TEST_CASE_TPL ( acsch_unsigned_int__1_0,  NT2_UNSIGNED_TYPES)
 
 
   // specific values tests
-  NT2_TEST_ULP_EQUAL(acsch(nt2::Zero<T>()), nt2::Nan<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(acsch(nt2::Zero<T>()), nt2::Inf<r_t>(), 0.5);
 } // end of test for unsigned_int_
 
 NT2_TEST_CASE_TPL ( acsch_signed_int__1_0,  NT2_INTEGRAL_SIGNED_TYPES)
@@ -102,5 +102,5 @@ NT2_TEST_CASE_TPL ( acsch_signed_int__1_0,  NT2_INTEGRAL_SIGNED_TYPES)
 
 
   // specific values tests
-  NT2_TEST_ULP_EQUAL(acsch(nt2::Zero<T>()), nt2::Nan<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(acsch(nt2::Zero<T>()), nt2::Inf<r_t>(), 0.5);
 } // end of test for signed_int_
