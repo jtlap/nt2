d43ea08ed0f36d35415d53f67a68b867cc139060
Mathias Gaunard <mathias@gaunard.com>
2013-04-24 14:32:38 +0200

    remove redundant code in sse4.1 min/max

--- modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/simd/sse/sse4_1/max.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/simd/sse/sse4_1/max.hpp
@@ -10,44 +10,10 @@
 #define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_SSE_SSE4_1_MAX_HPP_INCLUDED
 #ifdef BOOST_SIMD_HAS_SSE4_1_SUPPORT
 
+#include <boost/simd/toolbox/arithmetic/functions/max.hpp>
+
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::max_, boost::simd::tag::sse4_1_, (A0)
-                            , ((simd_<double_<A0>,boost::simd::tag::sse_>))
-                              ((simd_<double_<A0>,boost::simd::tag::sse_>))
-                            )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      return _mm_max_pd(a0,a1);
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::max_, boost::simd::tag::sse4_1_, (A0)
-                            , ((simd_<single_<A0>,boost::simd::tag::sse_>))
-                              ((simd_<single_<A0>,boost::simd::tag::sse_>))
-                            )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      return _mm_max_ps(a0,a1);
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::max_, boost::simd::tag::sse4_1_, (A0)
-                            , ((simd_<uint8_<A0>,boost::simd::tag::sse_>))
-                              ((simd_<uint8_<A0>,boost::simd::tag::sse_>))
-                            )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-         return _mm_max_epu8(a0,a1);
-    }
-  };
-
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::max_, boost::simd::tag::sse4_1_, (A0)
                             , ((simd_<int8_<A0>,boost::simd::tag::sse_>))
                               ((simd_<int8_<A0>,boost::simd::tag::sse_>))
@@ -70,18 +36,6 @@ namespace boost { namespace simd { namespace ext
     BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
     {
       return _mm_max_epu16(a0,a1);
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::max_, boost::simd::tag::sse4_1_, (A0)
-                            , ((simd_<int16_<A0>,boost::simd::tag::sse_>))
-                              ((simd_<int16_<A0>,boost::simd::tag::sse_>))
-                            )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
-    {
-      return _mm_max_epi16(a0,a1);
     }
   };
 
