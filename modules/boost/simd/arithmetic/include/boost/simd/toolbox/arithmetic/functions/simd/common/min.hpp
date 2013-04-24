cced7b36a023d46feeb53bd7eda14897d5e8f79f
Mathias Gaunard <mathias@gaunard.com>
2013-04-24 10:23:54 +0200

    move simd min common code to common file, fix includes

---
+++ modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/simd/common/min.hpp
@@ -0,0 +1,33 @@
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_MIN_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_MIN_HPP_INCLUDED
+
+#include <boost/simd/toolbox/arithmetic/functions/min.hpp>
+#include <boost/simd/include/functions/simd/seladd.hpp>
+#include <boost/simd/include/functions/simd/is_greater.hpp>
+#include <boost/simd/include/functions/simd/minus.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::min_, tag::cpu_, (A0)(X)
+                                   , ((simd_<arithmetic_<A0>,X>))
+                                     ((simd_<arithmetic_<A0>,X>))
+                                   )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+       return seladd(gt(a0,a1),a0,a1-a0);
+    }
+  };
+} } }
+
+
+#endif
