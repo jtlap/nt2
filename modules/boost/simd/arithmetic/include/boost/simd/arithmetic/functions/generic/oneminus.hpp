84a1db8a45647eeb5d23ee846a4c67aa4049465c
jtlap <jtlapreste@gmail.com>
2013-04-26 10:45:22 +0200

    oneminus is generic in boost.simd.arithmetic

--- modules/boost/simd/arithmetic/include/boost/simd/arithmetic/functions/generic/oneminus.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/arithmetic/functions/generic/oneminus.hpp
@@ -6,37 +6,38 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_SCALAR_ONEMINUS_HPP_INCLUDED
-#define BOOST_SIMD_ARITHMETIC_FUNCTIONS_SCALAR_ONEMINUS_HPP_INCLUDED
+#ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_GENERIC_ONEMINUS_HPP_INCLUDED
+#define BOOST_SIMD_ARITHMETIC_FUNCTIONS_GENERIC_ONEMINUS_HPP_INCLUDED
+
 #include <boost/simd/arithmetic/functions/oneminus.hpp>
+#include <boost/simd/include/functions/simd/min.hpp>
+#include <boost/simd/include/functions/simd/minus.hpp>
 #include <boost/simd/include/constants/one.hpp>
-#include <boost/simd/include/functions/scalar/min.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::oneminus_, tag::cpu_,
-                       (A0),
-                       (scalar_<signed_<A0> > )
-                       )
+                                    (A0),
+                                    ((generic_<signed_<A0> >))
+                                   )
   {
     typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL(1)
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(1)
     {
       return One<result_type>()-a0;
     }
   };
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::oneminus_, tag::cpu_,
-                       (A0),
-                       (scalar_<unsigned_<A0> > )
-                       )
+                                    (A0),
+                                    ((generic_<unsigned_<A0> >))
+                                   )
   {
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return One<result_type>()-min(a0, One<result_type>());
+      return One<result_type>()-simd::min(a0, One<result_type>());
     }
   };
 } } }
-
 #endif
