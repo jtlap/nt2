db685e44c249465a7de6ca298fc6b9b0e869e843
jtlap <jtlapreste@gmail.com>
2013-04-26 11:18:20 +0200

    sqrt is generic in boost.simd.arithmetic

--- modules/boost/simd/arithmetic/include/boost/simd/arithmetic/functions/generic/rsqrt.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/arithmetic/functions/generic/rsqrt.hpp
@@ -6,26 +6,27 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_COMMON_RSQRT_HPP_INCLUDED
-#define BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_COMMON_RSQRT_HPP_INCLUDED
+#ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_GENERIC_RSQRT_HPP_INCLUDED
+#define BOOST_SIMD_ARITHMETIC_FUNCTIONS_GENERIC_RSQRT_HPP_INCLUDED
 #include <boost/simd/arithmetic/functions/rsqrt.hpp>
+#include <boost/simd/include/functions/simd/rec.hpp>
 #include <boost/simd/include/functions/simd/sqrt.hpp>
 #include <boost/simd/include/functions/simd/tofloat.hpp>
-#include <boost/simd/include/functions/simd/divides.hpp>
-#include <boost/simd/include/constants/one.hpp>
 #include <boost/dispatch/meta/as_floating.hpp>
+
+//TO DO some asks for a fast inaccurate version
 
 namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::rsqrt_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<arithmetic_<A0>,X>))
+                            , (A0)
+                            , (generic_< arithmetic_<A0> >)
                             )
   {
-    typedef typename dispatch::meta::as_floating<A0>::type result_type;
+    typedef typename boost::dispatch::meta::as_floating<A0>::type result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return One<result_type>()/boost::simd::sqrt(tofloat(a0));
+      return rec(simd::sqrt(tofloat(a0)));
     }
   };
 } } }
