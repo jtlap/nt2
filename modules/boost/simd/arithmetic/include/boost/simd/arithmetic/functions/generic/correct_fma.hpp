43b0b9873af9192d08ccc617d6bd7451de625034
jtlap <jtlapreste@gmail.com>
2013-04-26 08:31:22 +0200

    correct is generic in boost.simd.arithmetic except for float for which upgrade can be used

---
+++ modules/boost/simd/arithmetic/include/boost/simd/arithmetic/functions/generic/correct_fma.hpp
@@ -0,0 +1,53 @@
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_GENERIC_CORRECT_FMA_HPP_INCLUDED
+#define BOOST_SIMD_ARITHMETIC_FUNCTIONS_GENERIC_CORRECT_FMA_HPP_INCLUDED
+#include <boost/simd/arithmetic/functions/correct_fma.hpp>
+#include <boost/simd/include/functions/simd/two_prod.hpp>
+#include <boost/simd/include/functions/simd/two_add.hpp>
+#include <boost/simd/include/functions/simd/plus.hpp>
+#include <boost/simd/include/functions/simd/multiplies.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::correct_fma_, tag::cpu_
+                                   , (A0)
+                                   , (generic_< integer_<A0> >)
+                                     (generic_< integer_<A0> >)
+                                     (generic_< integer_<A0> >)
+                                   )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(3)
+    {
+      return a0*a1+a2;
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::correct_fma_, tag::cpu_
+                                   , (A0)
+                                   , (generic_< floating_<A0> >)
+                                     (generic_< floating_<A0> >)
+                                     (generic_< floating_<A0> >)
+                                   )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(3)
+    {
+      result_type p, rp, s, rs;
+      two_prod(a0, a1, p, rp);
+      two_add(p, a2, s, rs);
+      return s+(rp+rs);
+    }
+  };
+
+} } }
+
+
+#endif
--- modules/boost/simd/arithmetic/include/boost/simd/arithmetic/functions/simd/common/correct_fma.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/arithmetic/functions/simd/common/correct_fma.hpp
@@ -11,47 +11,12 @@
 #include <boost/simd/arithmetic/functions/correct_fma.hpp>
 #include <boost/simd/include/functions/simd/multiplies.hpp>
 #include <boost/simd/include/functions/simd/plus.hpp>
-#include <boost/simd/include/functions/simd/two_prod.hpp>
-#include <boost/simd/include/functions/simd/two_add.hpp>
 #include <boost/simd/include/functions/simd/split.hpp>
 #include <boost/simd/include/functions/simd/group.hpp>
 #include <boost/dispatch/meta/upgrade.hpp>
 
-/////////////////////////////////////////////////////////////////////////////
-// Implementation when type A0 is arithmetic_
-/////////////////////////////////////////////////////////////////////////////
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::correct_fma_, tag::cpu_
-                                   , (A0)(X)
-                                   , ((simd_<arithmetic_<A0>,X>))
-                                     ((simd_<arithmetic_<A0>,X>))
-                                     ((simd_<arithmetic_<A0>,X>))
-                                   )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(3)
-    {
-      return a0*a1+a2;
-    }
-  };
-
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::correct_fma_, tag::cpu_
-                                   , (A0)(X)
-                                   , ((simd_<floating_<A0>,X>))
-                                     ((simd_<floating_<A0>,X>))
-                                     ((simd_<floating_<A0>,X>))
-                                   )
-  {
-    typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL_REPEAT(3)
-    {
-      A0 p, rp, s, rs;
-      two_prod(a0, a1, p, rp);
-      two_add(p, a2, s, rs);
-      return s+(rp+rs);
-    }
-  };
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::correct_fma_, tag::cpu_
                                    , (A0)(X)
