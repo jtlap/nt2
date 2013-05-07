ed0054cfd4bd3b7908ce8aa4a5829988eaf61f12
Mathias Gaunard <mathias@gaunard.com>
2013-05-07 11:28:27 +0200

    remove unnecessary displays and indentity tests in flipdim test

--- modules/core/restructuring/unit/table/flipdim.cpp
+++ modules/core/restructuring/unit/table/flipdim.cpp
@@ -10,7 +10,7 @@
 
 #include <nt2/table.hpp>
 #include <nt2/include/functions/flipdim.hpp>
-#include <nt2/include/functions/isequal.hpp>
+
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
@@ -33,19 +33,11 @@ NT2_TEST_CASE_TPL( flipdim, NT2_TYPES )
   x = nt2::flipdim(y, 2);
   display("x", x);
 
- std::cout << "y" << std::endl;
-  for(int i=1;i<=5;i++)
-    {
-      for(int j=1;j<=3;j++)
-        std::cout << T(y(i,j)) << "\t";
-      std::cout << std::endl;
-    }
-  std::cout << std::endl;
-
   for(int j=1;j<=3;j++)
     for(int i=1;i<=5;i++)
       NT2_TEST_EQUAL( T(x(i,j)),T(y(i,4-j)) );
 }
+
 NT2_TEST_CASE_TPL( flipud, NT2_TYPES )
 {
   nt2::table<T> x,x2,y( nt2::of_size(5,3) );
@@ -59,15 +51,6 @@ NT2_TEST_CASE_TPL( flipud, NT2_TYPES )
   x2 = nt2::flipdim(y, 1)(nt2::_);
   display("x", x);
 
- std::cout << "y" << std::endl;
-  for(int i=1;i<=5;i++)
-    {
-      for(int j=1;j<=3;j++)
-        std::cout << T(y(i,j)) << "\t";
-      std::cout << std::endl;
-    }
-  std::cout << std::endl;
-
   for(int j=1;j<=3;j++)
   {
     for(int i=1;i<=5;i++)
@@ -77,6 +60,7 @@ NT2_TEST_CASE_TPL( flipud, NT2_TYPES )
     }
   }
 }
+
 NT2_TEST_CASE_TPL( noflip, NT2_TYPES )
 {
   nt2::table<T> x,y( nt2::of_size(5,3) );
@@ -86,22 +70,9 @@ NT2_TEST_CASE_TPL( noflip, NT2_TYPES )
       y(i,j) = T(i + 10*j);
   display("y", y);
 
-  x = nt2::flipdim(y, 3);
-  display("x", x);
-
- std::cout << "y" << std::endl;
-  for(int i=1;i<=5;i++)
-    {
-      for(int j=1;j<=3;j++)
-        std::cout << T(y(i,j)) << "\t";
-      std::cout << std::endl;
-    }
-  std::cout << std::endl;
-
-  for(int j=1;j<=3;j++)
-    for(int i=1;i<=5;i++)
-      NT2_TEST_EQUAL( T(x(i,j)),T(y(i,j)) );
+  NT2_TEST_EQUAL( nt2::flipdim(y, 3) , y);
 }
+
 NT2_TEST_CASE_TPL( flipflip, NT2_TYPES )
 {
   nt2::table<T> x,y( nt2::of_size(5,3) );
@@ -111,20 +82,13 @@ NT2_TEST_CASE_TPL( flipflip, NT2_TYPES )
       y(i,j) = T(i + 10*j);
   display("y", y);
 
-
   x = nt2::flipdim(y, 1);
-  display("x", x);
-
-  NT2_TEST(nt2::isequal( x,  nt2::flipdim(y, 1)));
-  NT2_TEST(nt2::isequal( y,  nt2::flipdim(x, 1)));
+  NT2_TEST_EQUAL( y,  nt2::flipdim(x, 1) );
 
   x = nt2::flipdim(y, 2);
-  display("x", x);
-
-  NT2_TEST( nt2::isequal( x,  nt2::flipdim(y, 2)));
-  NT2_TEST( nt2::isequal( y,  nt2::flipdim(x, 2)));
-
+  NT2_TEST_EQUAL( y,  nt2::flipdim(x, 2) );
 }
+
 NT2_TEST_CASE_TPL( flipflip1, NT2_TYPES )
 {
   nt2::table<T> xr,yr( nt2::_(T(1), T(5)));
@@ -132,7 +96,7 @@ NT2_TEST_CASE_TPL( flipflip1, NT2_TYPES )
   xr = nt2::flipdim(yr);
   display("xr", xr);
 
-  NT2_TEST(nt2::isequal( xr,  nt2::flipdim(yr, 2)));
+  NT2_TEST_EQUAL( xr,  nt2::flipdim(yr, 2) );
 
   nt2::table<T> xl,yl(nt2::_(T(1), T(5)));
   yl.resize(nt2::of_size(5, 1));
@@ -140,6 +104,6 @@ NT2_TEST_CASE_TPL( flipflip1, NT2_TYPES )
   xl = nt2::flipdim(yl);
   display("xl", xl);
 
-  NT2_TEST(nt2::isequal( xl,  nt2::flipdim(yl, 1)));
+  NT2_TEST_EQUAL( xl,  nt2::flipdim(yl, 1) );
 
 }
