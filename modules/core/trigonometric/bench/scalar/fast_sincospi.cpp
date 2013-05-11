5e4371da6247ca785e201221702de8f9d08df4f5
jtlap <jtlapreste@gmail.com>
2013-05-11 13:59:21 +0200

    range correction for some trigo benches

--- modules/core/trigonometric/bench/scalar/fast_sincospi.cpp
+++ modules/core/trigonometric/bench/scalar/fast_sincospi.cpp
@@ -31,12 +31,12 @@ using nt2::tag::fast_sincospi_;
 namespace n1 {
   typedef float T;
   typedef boost::dispatch::meta::as_integer<T>::type iT;
-  NT2_TIMING(fast_sincospi_,(RS(T,T(0.25),T(0.25))))
+  NT2_TIMING(fast_sincospi_,(RS(T,T(-0.25),T(0.25))))
 }
 namespace n2 {
   typedef double T;
   typedef boost::dispatch::meta::as_integer<T>::type iT;
-  NT2_TIMING(fast_sincospi_,(RS(T,T(0.25),T(0.25))))
+  NT2_TIMING(fast_sincospi_,(RS(T,T(-0.25),T(0.25))))
 }
 
 #undef RS
