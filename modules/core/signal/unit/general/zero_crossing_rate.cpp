50dadafd222779a2537fb4ea6e92b3f8d04d97b9
jtlap <jtlapreste@gmail.com>
2013-05-20 10:56:40 +0200

    modifying tests for zero crosing rate
    In fact zero_crossing rate is devoted to real noisy signal and can be very touchy for inexact values around
    zero. But in fact this is not very relevant. The problematc test is related to the fact that cospi(half_int)
    can be not exactly computed as 0 depending of the system.

--- modules/core/signal/unit/general/zero_crossing_rate.cpp
+++ modules/core/signal/unit/general/zero_crossing_rate.cpp
@@ -11,12 +11,14 @@
 #include <nt2/signal/include/functions/zero_crossing_rate.hpp>
 #include <nt2/include/functions/cospi.hpp>
 #include <nt2/include/functions/colon.hpp>
+#include <nt2/include/functions/round.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <nt2/sdk/functor/meta/call.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/table.hpp>
+
 
 
 NT2_TEST_CASE_TPL ( zero_crossing_rate_real__1_0,  NT2_REAL_TYPES)
@@ -29,6 +31,6 @@ NT2_TEST_CASE_TPL ( zero_crossing_rate_real__1_0,  NT2_REAL_TYPES)
   NT2_TEST_ULP_EQUAL(zero_crossing_rate(s)(1), T(1), 0.5);
   NT2_TEST_ULP_EQUAL(zero_crossing_rate(nt2::_(T(0), T(10))), T(0), 0.5);
   NT2_TEST_ULP_EQUAL(zero_crossing_rate(nt2::cospi(nt2::_(T(e), T(e+0.5), T(e+10)))), T(0.5), 0.5);
-  NT2_TEST_ULP_EQUAL(zero_crossing_rate(nt2::cospi(nt2::_(T(0), T(0.5), T(10)))), T(0), 0.5);
+  NT2_TEST_ULP_EQUAL(zero_crossing_rate(nt2::round(T(2)*nt2::cospi(nt2::_(T(0), T(0.5), T(10))))/T(2)), T(0), 0.5);
 
 }
