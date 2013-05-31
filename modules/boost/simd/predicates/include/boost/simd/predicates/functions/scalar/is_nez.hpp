dc64a6e03fcd2003f7a52af453f1f22c893d2742
Mathias Gaunard <mathias@gaunard.com>
2013-05-31 20:34:07 +0200

    define is_nez on bool

--- modules/boost/simd/predicates/include/boost/simd/predicates/functions/scalar/is_nez.hpp
+++ modules/boost/simd/predicates/include/boost/simd/predicates/functions/scalar/is_nez.hpp
@@ -16,7 +16,7 @@ namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_nez_, tag::cpu_
                             , (A0)
-                            , (scalar_<fundamental_<A0> >)
+                            , (scalar_<arithmetic_<A0> >)
                             )
   {
     typedef typename meta::as_logical<A0>::type result_type;
@@ -25,9 +25,22 @@ namespace boost { namespace simd { namespace ext
       return result_type(a0 != Zero<A0>());
     }
   };
+
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_nez_, tag::cpu_
                             , (A0)
                             , (scalar_<logical_<A0> >)
+                            )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL(1)
+    {
+      return a0;
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_nez_, tag::cpu_
+                            , (A0)
+                            , (scalar_<bool_<A0> >)
                             )
   {
     typedef A0 result_type;
