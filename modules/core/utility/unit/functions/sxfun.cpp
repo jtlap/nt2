671dda9b64d2864b46f4a3df6ea6391c9a1335ba
jtlap <jtlapreste@gmail.com>
2013-06-02 22:41:10 +0200

    testing all arities with sxfun

--- modules/core/utility/unit/functions/sxfun.cpp
+++ modules/core/utility/unit/functions/sxfun.cpp
@@ -18,6 +18,7 @@
 #include <nt2/include/functions/colvect.hpp>
 #include <nt2/include/functions/colon.hpp>
 #include <nt2/include/functions/plus.hpp>
+#include <nt2/include/functions/fma.hpp>
 #include <nt2/include/functions/cons.hpp>
 
 #include <nt2/sdk/unit/module.hpp>
@@ -89,4 +90,76 @@ NT2_TEST_CASE_TPL( sxfun_7, NT2_TYPES)
     b = nt2::ones(nt2::of_size(1, 3, 1,3), nt2::meta::as_<T>());
 
   NT2_TEST_EQUAL( sxfun(plus_pfo(2),  a, b), T(4)*ones(nt2::of_size(3, 3, 3, 3),  nt2::meta::as_<T>()));
+}
+
+
+struct fma_pfo
+{
+  fma_pfo(int value_) : value(value_)
+  {
+  }
+
+  template<class Sig>
+  struct result;
+
+  template<class This, class T, class U, class V>
+  struct result<This(T, U, V)>
+    : boost::dispatch::meta::strip<T>
+  {
+  };
+
+  template<class T>
+  T operator()(T const& a, T const& b, T const& c) const
+  {
+    return a*b + c + nt2::splat<T>(value);
+  }
+
+  int value;
+};
+
+
+
+
+NT2_TEST_CASE_TPL( sxfun_8, NT2_TYPES )
+{
+  nt2::table<T> a = nt2::_(T(1), T(4));
+  nt2::table<T> b =  nt2::reshape(a, 4, 1);
+  nt2::table<T> c = nt2::_(T(1), T(4));
+  nt2::table<T> d(nt2::of_size(4, 4));
+
+  for(int i=1; i <= 4 ; ++i)
+  {
+    for(int j=1; j <= 4 ; ++j)
+    {
+      d(i, j) = i*j+j;
+    }
+  }
+  NT2_TEST_EQUAL(nt2::sxfun(nt2::functor<nt2::tag::fma_>(), a, b, c), d);
+  NT2_TEST_EQUAL(nt2::sxfun(fma_pfo(0), a, b, c), d);
+}
+
+NT2_TEST_CASE_TPL( sxfun_9, NT2_TYPES )
+{
+  nt2::table<T> a = nt2::_(T(1), T(4));
+  nt2::table<T> b =  nt2::reshape(a, 4, 1);
+  nt2::table<T> c = nt2::_(T(1), T(4));
+  nt2::table<T> d(nt2::of_size(4, 4));
+
+  for(int i=1; i <= 4 ; ++i)
+  {
+    for(int j=1; j <= 4 ; ++j)
+    {
+      d(i, j) = i*j+j+1;
+    }
+  }
+  NT2_TEST_EQUAL(nt2::sxfun(fma_pfo(1), a, b, c), d);
+}
+
+NT2_TEST_CASE_TPL( sxfun_10, NT2_TYPES)
+{
+  nt2::table<T> a = nt2::ones(nt2::of_size(3, 1,3,1), nt2::meta::as_<T>()),
+    b = nt2::ones(nt2::of_size(1, 3, 1,3), nt2::meta::as_<T>()),
+    c = nt2::ones(nt2::of_size(3, 1, 1, 3), nt2::meta::as_<T>());
+
+  NT2_TEST_EQUAL( sxfun(fma_pfo(2),  a, b, c), T(4)*ones(nt2::of_size(3, 3, 3, 3),  nt2::meta::as_<T>()));
 }
