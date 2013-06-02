90db03e99a0de533f9843b25ee6d60e7975f6247
jtlap <jtlapreste@gmail.com>
2013-06-02 16:42:48 +0200

    these simd cover tests do not compile with the new macros

--- modules/core/arithmetic/cover/simd/two_add.cpp
+++ modules/core/arithmetic/cover/simd/two_add.cpp
@@ -11,49 +11,44 @@
 //////////////////////////////////////////////////////////////////////////////
 // cover test behavior of arithmetic components in simd mode
 //////////////////////////////////////////////////////////////////////////////
-/// created by jt the 01/12/2010
-///
+
 #include <nt2/arithmetic/include/functions/two_add.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <nt2/include/functions/max.hpp>
-#include <boost/type_traits/is_same.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/as_floating.hpp>
-#include <nt2/sdk/meta/as_signed.hpp>
-#include <nt2/sdk/meta/upgrade.hpp>
-#include <nt2/sdk/meta/downgrade.hpp>
-#include <nt2/sdk/meta/scalar_of.hpp>
-#include <boost/dispatch/meta/as_floating.hpp>
-#include <boost/type_traits/common_type.hpp>
+#include <vector>
+#include <nt2/include/constants/valmin.hpp>
+#include <nt2/include/constants/valmax.hpp>
 #include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/module.hpp>
-
-#include <nt2/constant/constant.hpp>
-#include <nt2/sdk/meta/cardinal_of.hpp>
-#include <nt2/include/functions/splat.hpp>
-
-#include <nt2/include/functions/load.hpp>
-#include <nt2/constant/constant.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
 
 
-NT2_TEST_CASE_TPL ( two_add_real__2_0,  NT2_SIMD_REAL_TYPES)
+template < class T1, class T2>   std::ostream&
+operator<<(std::ostream& os, std::pair<T1, T2> const& xpr)
+{
+  os << "[" << xpr.first << ",  " << xpr.second << "]";
+  return os;
+}
+
+NT2_TEST_CASE_TPL ( two_add_real_types,  NT2_SIMD_REAL_TYPES)
 {
   using nt2::two_add;
   using nt2::tag::two_add_;
-  using nt2::load;
   using boost::simd::native;
-  using nt2::meta::cardinal_of;
   typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<two_add_(vT,vT)>::type r_t;
-  typedef typename nt2::meta::call<two_add_(T,T)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
+  typedef native<T,ext_t>                nT;
 
-} // end of test for floating_
+  typedef typename nt2::meta::call<two_add_(T, T)>::type r_t;
+
+  // random verifications
+  nt2::uint32_t NR  = NT2_NB_RANDOM_TEST;
+  std::vector<T> in1(NR), in2(NR);
+  nt2::roll(in1, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
+  nt2::roll(in2, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
+
+  std::vector<r_t> ref(NR);
+  for(nt2::uint32_t i=0; i < NR ; ++i) ref[i] = two_add(in1[i], in2[i]);
+  NT2_COVER_ULP_EQUAL(two_add_, ((nT, in1))((nT, in2)), ref, 0);
+
+}
+
