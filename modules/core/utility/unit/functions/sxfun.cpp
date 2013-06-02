bd5b136a0a6cd8f7d00ab2af0e369e3a7360b4bb
jtlap <jtlapreste@gmail.com>
2013-06-02 20:04:51 +0200

    sx.cpp works well but sxfun.cpp does not compile with pfo_plus

---
+++ modules/core/utility/unit/functions/sxfun.cpp
@@ -0,0 +1,96 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#define NT2_UNIT_MODULE "nt2::sxfun function"
+
+#include <nt2/table.hpp>
+#include <nt2/include/functions/size.hpp>
+#include <nt2/include/functions/sxfun.hpp>
+#include <nt2/include/functions/rif.hpp>
+#include <nt2/include/functions/cif.hpp>
+#include <nt2/include/functions/ones.hpp>
+#include <nt2/include/functions/mean.hpp>
+#include <nt2/include/functions/colvect.hpp>
+#include <nt2/include/functions/colon.hpp>
+#include <nt2/include/functions/plus.hpp>
+#include <nt2/include/functions/cons.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+
+struct plus_pfo
+{
+  plus_pfo(int value_) : value(value_)
+  {
+  }
+  plus_pfo() : value(0)
+  {
+  }
+
+  template<class Sig>
+  struct result;
+
+  template<class This, class T>
+  struct result<This(T)>
+    : boost::dispatch::meta::strip<T>
+  {
+  };
+
+  template<class T>
+  T operator()(T const& a, T const& b) const
+  {
+    return a + b + nt2::splat<T>(value);
+  }
+
+  int value;
+};
+
+
+
+
+NT2_TEST_CASE_TPL( sxfun_5, NT2_TYPES )
+{
+  nt2::table<T> a = nt2::_(T(1), T(4));
+  nt2::table<T> b =  nt2::reshape(a, 4, 1);
+  nt2::table<T> c(nt2::of_size(4, 4));
+
+  for(int i=1; i <= 4 ; ++i)
+  {
+    for(int j=1; j <= 4 ; ++j)
+    {
+      c(i, j) = i+j+1;
+    }
+  }
+  NT2_TEST_EQUAL(nt2::sxfun(nt2::functor<nt2::tag::plus_>(), a, b), c);
+  NT2_TEST_EQUAL(nt2::sxfun(plus_pfo(0), a, b), c);
+}
+
+NT2_TEST_CASE_TPL( sxfun_6, NT2_TYPES )
+{
+  nt2::table<T> a = nt2::_(T(1), T(4));
+  nt2::table<T> b =  nt2::reshape(a, 4, 1);
+  nt2::table<T> c(nt2::of_size(4, 4));
+
+  for(int i=1; i <= 4 ; ++i)
+  {
+    for(int j=1; j <= 4 ; ++j)
+    {
+      c(i, j) = i+j+1;
+    }
+  }
+  NT2_TEST_EQUAL(nt2::sxfun(nt2::functor<nt2::tag::plus_>(), a, b), c);
+  NT2_TEST_EQUAL(nt2::sxfun(plus_pfo(), a, b), c);
+}
+
+NT2_TEST_CASE_TPL( sxfun_7, NT2_TYPES)
+{
+  nt2::table<T> a = nt2::ones(nt2::of_size(3, 1,3,1), nt2::meta::as_<T>()),
+    b = nt2::ones(nt2::of_size(1, 3, 1,3), nt2::meta::as_<T>());
+
+  NT2_TEST_EQUAL( sxfun(plus_pfo(2),  a, b), T(4)*ones(nt2::of_size(3, 3, 3, 3),  nt2::meta::as_<T>()));
+}
