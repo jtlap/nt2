5b7823f004ec17254e4e0c177cc4d25b1c7815de
Mathias Gaunard <mathias@gaunard.com>
2013-06-04 17:19:44 +0200

    normalize rshl/rshr code, make it work with simd/scalar

--- modules/boost/simd/base/include/boost/simd/bitwise/functions/simd/common/rshl.hpp
+++ modules/boost/simd/base/include/boost/simd/bitwise/functions/simd/common/rshl.hpp
@@ -8,23 +8,48 @@
 //==============================================================================
 #ifndef BOOST_SIMD_BITWISE_FUNCTIONS_SIMD_COMMON_RSHL_HPP_INCLUDED
 #define BOOST_SIMD_BITWISE_FUNCTIONS_SIMD_COMMON_RSHL_HPP_INCLUDED
+
 #include <boost/simd/bitwise/functions/rshl.hpp>
-#include <boost/simd/include/functions/simd/shli.hpp>
-#include <boost/simd/include/functions/simd/shri.hpp>
 #include <boost/simd/include/functions/simd/is_gtz.hpp>
+#include <boost/simd/include/functions/simd/if_else.hpp>
+#include <boost/simd/include/functions/simd/shift_left.hpp>
+#include <boost/simd/include/functions/simd/shift_right.hpp>
+#include <boost/simd/include/functions/simd/unary_minus.hpp>
+#include <boost/simd/sdk/meta/cardinal_of.hpp>
+#include <boost/mpl/equal_to.hpp>
+
 namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION_IF ( boost::simd::tag::rshl_, tag::cpu_, (A0)(A1)(X)
-                                , (boost::mpl::equal_to < boost::simd::meta::cardinal_of<A0>
-                                                        , boost::simd::meta::cardinal_of<A1>
-                                                        >
-                                  )
-                                , ((simd_<arithmetic_<A0>,X>))
-                                  ((simd_<integer_<A1>,X>))
-                                )
+                                       , (boost::mpl::equal_to < boost::simd::meta::cardinal_of<A0>
+                                                               , boost::simd::meta::cardinal_of<A1>
+                                                               >
+                                         )
+                                       , ((simd_<arithmetic_<A0>,X>))
+                                         ((generic_<integer_<A1> >))
+                                       )
   {
     typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL(2) { return map(typename dispatch::make_functor<boost::simd::tag::rshl_, A0>::type(), a0, a1); }
+    BOOST_SIMD_FUNCTOR_CALL(2)
+    {
+      return if_else(is_gtz(a1),shl(a0, a1),shr(a0, -a1));
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION_IF ( boost::simd::tag::rshl_, tag::cpu_, (A0)(A1)(X)
+                                       , (boost::mpl::equal_to < boost::simd::meta::cardinal_of<A0>
+                                                               , boost::simd::meta::cardinal_of<A1>
+                                                               >
+                                       )
+                                       , ((simd_<arithmetic_<A0>,X>))
+                                         ((generic_<unsigned_<A1> ))
+                                       )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL(2)
+    {
+      return shl(a0, a1);
+    }
   };
 
 } } }
