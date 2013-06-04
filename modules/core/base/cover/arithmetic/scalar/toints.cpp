55487ec52d2a4b8239615e948709f2929804a1be
jtlap <jtlapreste@gmail.com>
2013-06-04 22:18:09 +0200

    cover for toints

---
+++ modules/core/base/cover/arithmetic/scalar/toints.cpp
@@ -0,0 +1,43 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#define NT2_UNIT_MODULE "nt2 arithmetic toolbox - toints/scalar Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// cover test behavior of arithmetic components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+
+#include <nt2/arithmetic/include/functions/toints.hpp>
+#include <vector>
+#include <nt2/include/constants/valmin.hpp>
+#include <nt2/include/constants/valmax.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
+
+
+NT2_TEST_CASE_TPL ( toints_real,  NT2_REAL_TYPES)
+{
+
+  using nt2::toints;
+  using nt2::tag::toints_;
+  typedef typename nt2::meta::call<toints_(T)>::type r_t;
+
+  nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
+  std::vector<T> in1(NR);
+  nt2::roll(in1, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
+  std::vector<r_t>  ref(NR);
+  for(nt2::uint32_t i=0; i < NR ; ++i)
+  {
+    ref[i] = in1[i];
+  }
+
+  NT2_COVER_ULP_EQUAL(toints_, ((T, in1)), ref, 0);
+}
