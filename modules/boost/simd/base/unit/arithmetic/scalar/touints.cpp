af6352979442ef74a1757bab7c4e298af684855d
jtlap <jtlapreste@gmail.com>
2013-06-04 18:04:36 +0200

    unit tests for boost/simd/base/unit/arithmetic/ toints

---
+++ modules/boost/simd/base/unit/arithmetic/scalar/touints.cpp
@@ -0,0 +1,74 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#define NT2_UNIT_MODULE "nt2 boost.simd.arithmetic toolbox - touints/scalar Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// unit test behavior of boost.simd.arithmetic components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+
+#include <boost/simd/arithmetic/include/functions/touints.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <boost/dispatch/functor/meta/call.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/include/constants/inf.hpp>
+#include <nt2/include/constants/zero.hpp>
+#include <nt2/include/constants/one.hpp>
+#include <nt2/include/constants/mone.hpp>
+#include <nt2/include/constants/minf.hpp>
+#include <nt2/include/constants/nan.hpp>
+
+
+NT2_TEST_CASE_TPL ( touints_real,  BOOST_SIMD_REAL_TYPES)
+{
+
+  using boost::simd::touints;
+  using boost::simd::tag::touints_;
+  typedef typename boost::dispatch::meta::call<touints_(T)>::type              r_t;
+  typedef typename boost::dispatch::meta::as_integer<T, unsigned>::type wished_r_t;
+
+  NT2_TEST_TYPE_IS( r_t, wished_r_t );
+  // specific values tests
+  NT2_TEST_EQUAL(touints(boost::simd::Inf<T>()) , boost::simd::Inf<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Minf<T>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Mone<T>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Nan<T>()) , boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::One<T>()) , boost::simd::One<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Zero<T>()), boost::simd::Zero<r_t>());
+ } // end of test for floating_
+
+NT2_TEST_CASE_TPL ( touints_unsigned_int,  BOOST_SIMD_UNSIGNED_TYPES)
+{
+
+  using boost::simd::touints;
+  using boost::simd::tag::touints_;
+  typedef typename boost::dispatch::meta::call<touints_(T)>::type               r_t;
+  typedef typename boost::dispatch::meta::as_integer<T, unsigned>::type wished_r_t;
+
+  NT2_TEST_TYPE_IS( r_t, wished_r_t );
+  // specific values tests
+  NT2_TEST_EQUAL(touints(boost::simd::One<T>()), boost::simd::One<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Zero<T>()), boost::simd::Zero<r_t>());
+} // end of test for unsigned_int_
+
+NT2_TEST_CASE_TPL ( touints_signed_int,  BOOST_SIMD_INTEGRAL_SIGNED_TYPES)
+{
+
+  using boost::simd::touints;
+  using boost::simd::tag::touints_;
+  typedef typename boost::dispatch::meta::call<touints_(T)>::type               r_t;
+  typedef typename boost::dispatch::meta::as_integer<T, unsigned>::type wished_r_t;
+
+  NT2_TEST_TYPE_IS( r_t, wished_r_t );
+//  NT2_TEST_EQUAL(touints(boost::simd::Mone<T>()), boost::simd::Valmax<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Mone<T>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::One<T>()), boost::simd::One<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Zero<T>()), boost::simd::Zero<r_t>());
+} // end of test for signed_int_
---
+++ modules/boost/simd/base/unit/arithmetic/simd/touints.cpp
@@ -0,0 +1,79 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#define NT2_UNIT_MODULE "nt2 boost.simd.arithmetic toolbox - touints/simd Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// unit test behavior of boost.simd.arithmetic components in simd mode
+//////////////////////////////////////////////////////////////////////////////
+
+#include <boost/simd/arithmetic/include/functions/touints.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <boost/dispatch/functor/meta/call.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/include/constants/inf.hpp>
+#include <nt2/include/constants/zero.hpp>
+#include <nt2/include/constants/one.hpp>
+#include <nt2/include/constants/mone.hpp>
+#include <nt2/include/constants/minf.hpp>
+#include <nt2/include/constants/nan.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
+
+
+
+
+NT2_TEST_CASE_TPL ( touints_real,  BOOST_SIMD_SIMD_REAL_TYPES)
+{
+  using boost::simd::touints;
+  using boost::simd::tag::touints_;
+  using boost::simd::native;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>                  vT;
+  typedef typename boost::dispatch::meta::as_integer<vT, unsigned>::type ivT;
+  typedef typename boost::dispatch::meta::call<touints_(vT)>::type       r_t;
+
+  // specific values tests
+  NT2_TEST_EQUAL(touints(boost::simd::Inf<vT>()), boost::simd::Inf<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Minf<vT>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Mone<vT>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Nan<vT>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::One<vT>()), boost::simd::One<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Zero<vT>()), boost::simd::Zero<r_t>());
+} // end of test for floating_
+
+NT2_TEST_CASE_TPL ( touints_integral,  BOOST_SIMD_SIMD_INTEGRAL_TYPES)
+{
+  using boost::simd::touints;
+  using boost::simd::tag::touints_;
+  using boost::simd::native;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>                  vT;
+  typedef typename boost::dispatch::meta::as_integer<vT, unsigned>::type ivT;
+  typedef typename boost::dispatch::meta::call<touints_(vT)>::type       r_t;
+
+  // specific values tests
+  NT2_TEST_EQUAL(touints(boost::simd::One<vT>()), boost::simd::One<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Zero<vT>()), boost::simd::Zero<r_t>());
+} // end of test for floating_
+NT2_TEST_CASE_TPL ( touints_signed_int,  BOOST_SIMD_SIMD_INTEGRAL_SIGNED_TYPES)
+{
+  using boost::simd::touints;
+  using boost::simd::tag::touints_;
+  using boost::simd::native;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>                  vT;
+  typedef typename boost::dispatch::meta::as_integer<vT, unsigned>::type ivT;
+  typedef typename boost::dispatch::meta::call<touints_(vT)>::type       r_t;
+
+  // specific values tests
+  NT2_TEST_EQUAL(touints(boost::simd::Mone<vT>()), boost::simd::Valmin<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::One<vT>()), boost::simd::One<r_t>());
+  NT2_TEST_EQUAL(touints(boost::simd::Zero<vT>()), boost::simd::Zero<r_t>());
+}
