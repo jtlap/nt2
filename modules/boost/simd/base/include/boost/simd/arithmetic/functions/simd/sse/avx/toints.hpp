d2343bedc61ca73dfefbda49f3594380823671eb
jtlap <jtlapreste@gmail.com>
2013-06-04 18:09:27 +0200

    adding saturated conversions from floats to signed ints toints

---
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/sse/avx/toints.hpp
@@ -0,0 +1,59 @@
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_SSE_AVX_TOINTS_HPP_INCLUDED
+#define BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_SSE_AVX_TOINTS_HPP_INCLUDED
+#ifdef BOOST_SIMD_HAS_AVX_SUPPORT
+#include <boost/simd/arithmetic/functions/toints.hpp>
+#include <boost/simd/include/functions/simd/make.hpp>
+// #include <boost/simd/include/functions/simd/is_nan.hpp>
+// #include <boost/simd/include/functions/simd/bitwise_andnot.hpp>
+// #include <boost/simd/include/functions/simd/is_equal.hpp>
+// #include <boost/simd/include/functions/simd/if_else.hpp>
+// #include <boost/simd/include/functions/simd/if_zero_else.hpp>
+// #include <boost/simd/include/constants/inf.hpp>
+#include <boost/simd/sdk/meta/scalar_of.hpp>
+#include <boost/dispatch/meta/as_integer.hpp>
+#include <boost/simd/include/functions/simd/make.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::toints_, boost::simd::tag::avx_,
+                        (A0),
+                        ((simd_<double_<A0>,boost::simd::tag::avx_>))
+                       )
+  {
+    typedef typename dispatch::meta::as_integer<A0>::type result_type;
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(1)
+    {
+      typedef typename meta::scalar_of<result_type>::type stype;
+      const A0 aa0 = if_zero_else(is_nan(a0), a0);
+      return select(eq(aa0, Inf<A0>()),
+                    Inf<result_type>(),
+                    make<result_type>(aa0[0],aa0[1],aa0[2],aa0[3])
+                   );
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION(boost::simd::tag::toints_, boost::simd::tag::avx_,
+                        (A0),
+                        ((simd_<single_<A0>,boost::simd::tag::avx_>))
+                       )
+  {
+    typedef typename dispatch::meta::as_integer<A0>::type result_type;
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(1)
+    {
+      const A0 aa0 = if_zero_else(is_nan(a0), a0);
+      result_type that = _mm256_cvttps_epi32(aa0);
+      return  select(eq(aa0, Inf<A0>()), Inf<result_type>(), that);
+    }
+  };
+} } }
+
+#endif
+#endif
---
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/toints.hpp
@@ -0,0 +1,76 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+/*!
+ * \file
+**/
+#ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_TOINTS_HPP_INCLUDED
+#define BOOST_SIMD_ARITHMETIC_FUNCTIONS_TOINTS_HPP_INCLUDED
+#include <boost/simd/include/functor.hpp>
+#include <boost/dispatch/include/functor.hpp>
+
+/*!
+ * \ingroup boost_simd_arithmetic
+ * \defgroup boost_simd_arithmetic_toints toints
+ *
+ * \par Description
+ * convert a floating entry to signed integer by truncation.
+ * The Inf -Inf and Nan value are treated properly and go to respectively to
+ * Valmax, Valmin and Zero of the destination integral type which has the
+ * All values superior (resps) less than Valmax (resp Valmin) of the return type
+ * are saturated accordingly.
+ * same bit size as the entry.
+ * On integral typed values the functor acts as identity keeping the entry type.
+ *
+ * \par Header file
+ *
+ * \code
+ * #include <nt2/include/functions/toints.hpp>
+ * \endcode
+ *
+ * \par Aliases
+ * \arg ifix
+ * \arg itrunc
+ *
+ * \synopsis
+ *
+ * \code
+ * namespace boost::simd
+ * {
+ *   template <class A0>
+ *     meta::call<tag::toints_(A0)>::type
+ *     toints(const A0 & a0);
+ * }
+ * \endcode
+ *
+ * \param a0 the unique parameter of toints
+ *
+ * \return a value of the same type as the parameter
+ *
+ * \par Notes
+ * In SIMD mode, this function acts elementwise on the inputs vectors elements
+ * \par
+ *
+**/
+
+namespace boost { namespace simd { namespace tag
+  {
+    /*!
+     * \brief Define the tag toints_ of functor toints
+     *        in namespace boost::simd::tag for toolbox boost.simd.arithmetic
+    **/
+    struct toints_ : ext::elementwise_<toints_> { typedef ext::elementwise_<toints_> parent; };
+  }
+  BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::toints_, toints, 1)
+  BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::toints_, ifix, 1)
+  BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::toints_, itrunc, 1)
+} }
+
+#include <boost/simd/operator/specific/common.hpp>
+
+#endif
