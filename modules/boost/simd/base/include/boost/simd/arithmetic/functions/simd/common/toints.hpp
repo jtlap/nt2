cf705f6e070f022d4aa5ff6db769de235fe3d37d
jtlap <jtlapreste@gmail.com>
2013-06-05 14:46:36 +0200

    making simd calls to toints in a common calling toint

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/common/toints.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/common/toints.hpp
@@ -9,6 +9,7 @@
 #ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_COMMON_TOINTS_HPP_INCLUDED
 #define BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_COMMON_TOINTS_HPP_INCLUDED
 #include <boost/simd/arithmetic/functions/toints.hpp>
+#include <boost/simd/include/functions/simd/toints.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -32,6 +33,19 @@ namespace boost { namespace simd { namespace ext
   };
 
 
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::toints_, tag::cpu_,
+                                     (A0)(X)
+                                   , ((simd_<floating_<A0>,X>))
+                                   )
+  {
+    typedef typename dispatch::meta::as_integer<A0>::type result_type;
+    BOOST_SIMD_FUNCTOR_CALL(1)
+    {
+      A0 aa0 = if_zero_else(is_nan(a0), a0);
+      result_type that = toint(aa0);
+      return  select(eq(aa0, Inf<A0>()), Inf<result_type>(), that);
+    }
+  };
 } } }
 
 #endif
