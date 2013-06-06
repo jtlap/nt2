54ce03396792924c65ad771969707c13aa08ba3a
Mathias Gaunard <mathias@gaunard.com>
2013-06-06 19:12:16 +0200

    small optimization for scalar genmask

--- modules/boost/simd/base/include/boost/simd/bitwise/functions/scalar/genmask.hpp
+++ modules/boost/simd/base/include/boost/simd/bitwise/functions/scalar/genmask.hpp
@@ -10,20 +10,19 @@
 #define BOOST_SIMD_BITWISE_FUNCTIONS_SCALAR_GENMASK_HPP_INCLUDED
 
 #include <boost/simd/bitwise/functions/genmask.hpp>
-#include <boost/simd/include/constants/allbits.hpp>
-#include <boost/simd/include/constants/zero.hpp>
+#include <boost/simd/include/functions/scalar/unary_minus.hpp>
 #include <boost/simd/sdk/meta/as_arithmetic.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::genmask_, tag::cpu_, (A0)
-                            , (scalar_< fundamental_<A0> >)
-                            )
+                                   , (scalar_< fundamental_<A0> >)
+                                   )
   {
     typedef typename meta::as_arithmetic<A0>::type result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return a0 ? Allbits<result_type>() : Zero<result_type>();
+      return unary_minus(result_type(a0 != 0));
     }
   };
 } } }
