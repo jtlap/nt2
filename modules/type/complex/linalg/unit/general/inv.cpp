5b7a315061723d614f851a331178d2f5cc7a3031
jtlap <jtlapreste@gmail.com>
2013-06-12 22:52:37 +0200

    puttingcorrect macros test in unit test for complex inv

--- modules/type/complex/linalg/unit/general/inv.cpp
+++ modules/type/complex/linalg/unit/general/inv.cpp
@@ -16,7 +16,6 @@
 #include <nt2/include/constants/eps.hpp>
 #include <nt2/include/constants/two.hpp>
 #include <nt2/include/functions/rec.hpp>
-#include <nt2/include/functions/isulpequal.hpp>
 #include <nt2/include/functions/cons.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
@@ -32,7 +31,7 @@ NT2_TEST_CASE_TPL(inv, NT2_REAL_TYPES)
   using nt2::tag::inv_;
   nt2::table<cT> n = nt2::eye(10, 10, nt2::meta::as_<cT>());
   nt2::table<cT> invn = nt2::inv(n);
-  NT2_TEST(nt2::isulpequal(invn, n, 0.5));
+  NT2_TEST_ULP_EQUAL(invn, n, 0.5);
 }
 
 NT2_TEST_CASE_TPL(inv_1, NT2_REAL_TYPES)
@@ -46,7 +45,7 @@ NT2_TEST_CASE_TPL(inv_1, NT2_REAL_TYPES)
   n(1, 1) = nt2::Eps<T>()*nt2::Half<cT>();
   nn(1, 1) = nt2::rec(n(1, 1));
   nt2::table<cT> invn = nt2::inv(n);
-  NT2_TEST(nt2::isulpequal(invn, nn, 0.5));
+  NT2_TEST_ULP_EQUAL(invn, nn, 0.5);
  }
 NT2_TEST_CASE_TPL(inv_2, NT2_REAL_TYPES)
 {
@@ -68,7 +67,7 @@ NT2_TEST_CASE_TPL(inv_nowarn, NT2_REAL_TYPES)
   n(1, 1) = nt2::Eps<T>()*nt2::Half<cT>();
   nn(1, 1) = nt2::rec(n(1, 1));
   nt2::table<cT> invn = nt2::inv(n, false);
-  NT2_TEST(nt2::isulpequal(invn, nn, 0.5));
+  NT2_TEST_ULP_EQUAL(invn, nn, 0.5);
  }
 NT2_TEST_CASE_TPL(inv_3, NT2_REAL_TYPES)
 {
@@ -82,5 +81,5 @@ NT2_TEST_CASE_TPL(inv_3, NT2_REAL_TYPES)
   nt2::table<cT> inva = nt2::cons(nt2::of_size(2, 2),
                                   cT(1, -1), cT(0, 1),
                                   cT(-0.5, -0.5), cT(1));
-  NT2_TEST(nt2::isulpequal(inva, inv(a), 0.5));
+  NT2_TEST_ULP_EQUAL(inva, inv(a), 0.5);
  }
