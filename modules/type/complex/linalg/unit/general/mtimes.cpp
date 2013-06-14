eb188ac94d89d7b6f251311b2a0ddce38e4d86bb
jtlap <jtlapreste@gmail.com>
2013-06-14 13:07:38 +0200

    mtimes unit  test for complex

---
+++ modules/type/complex/linalg/unit/general/mtimes.cpp
@@ -0,0 +1,41 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#define NT2_UNIT_MODULE "nt2::mtimes function"
+
+#include <nt2/table.hpp>
+#include <nt2/include/functions/mtimes.hpp>
+#include <nt2/include/functions/reshape.hpp>
+#include <nt2/include/functions/zeros.hpp>
+#include <nt2/include/functions/cif.hpp>
+#include <nt2/include/functions/rif.hpp>
+#include <nt2/include/functions/transpose.hpp>
+#include <nt2/include/functions/cons.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+
+#include <nt2/sdk/complex/meta/as_complex.hpp>
+#include <nt2/include/constants/i.hpp>
+
+NT2_TEST_CASE_TPL( mtimes_4, NT2_REAL_TYPES )
+{
+  typedef typename nt2::meta::as_complex<T>::type  cT;
+  nt2::table<cT> y = nt2::cif(3, 4, nt2::meta::as_ < cT>())+nt2::I<cT>()*nt2::rif(3, 4, nt2::meta::as_ < cT>());
+  nt2::table<cT> d = nt2::mtimes(y, trans(y));
+  NT2_DISPLAY(d);
+  NT2_DISPLAY(y);
+  nt2::table<cT> c = nt2::trans(nt2::cons(nt2::of_size(3, 3),
+                                          cT(26,20), cT(22,30), cT(18,40),
+                                          cT(22,30), cT(14,40), cT(6,50),
+                                          cT(18,40), cT(6,50), cT(-6,60)
+                                         ));
+  NT2_TEST_EQUAL(c, d);
+
+ }
