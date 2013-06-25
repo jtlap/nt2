bf8db30ec8b4ab4ea367a74bbb21dd29e0f5b6b7
jtlap <jtlapreste@gmail.com>
2013-06-25 22:29:18 +0200

    adding globalnone

---
+++ modules/core/reduction/include/nt2/core/functions/globalnone.hpp
@@ -0,0 +1,62 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2011 - 2012   MetaScale SAS
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_FUNCTIONS_GLOBALNONE_HPP_INCLUDED
+#define NT2_CORE_FUNCTIONS_GLOBALNONE_HPP_INCLUDED
+
+/*!
+  @file
+  @brief Define and implements the globalnone function
+**/
+
+#include <nt2/include/functor.hpp>
+#include <nt2/include/functions/none.hpp>
+#include <nt2/include/functions/global.hpp>
+
+namespace nt2
+{
+  namespace tag
+  {
+    /*!
+      @brief Tag for the globalnone functor
+    **/
+    struct globalnone_ : boost::dispatch::tag::formal_
+    {
+      typedef boost::dispatch::tag::formal_ parent;
+    };
+  }
+
+  /*!
+    @brief Checks that none element of an expression is non-zero
+
+    @param a0 Expression to check
+  **/
+  NT2_FUNCTION_IMPLEMENTATION(nt2::tag::globalnone_       , globalnone, 1)
+}
+
+namespace nt2 { namespace ext
+{
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::globalnone_
+                            , tag::cpu_
+                            , (A0)
+                            , (unspecified_<A0>)
+                            )
+  {
+    typedef typename meta::call<tag::global_( nt2::functor<tag::none_>
+                                            , const A0&
+                                            )>::type                result_type;
+
+    BOOST_FORCEINLINE result_type operator()(A0 const& a0) const
+    {
+       return global(nt2::functor<tag::none_>(), a0);
+    }
+  };
+} }
+
+#endif
