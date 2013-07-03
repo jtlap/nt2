54543cc58d3077e4f4f38a54218a2d192b8d2fd6
jtlap <jtlapreste@gmail.com>
2013-07-03 17:43:26 +0200

    inappropriate casting for simd type in toint

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/common/toint.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/common/toint.hpp
@@ -9,6 +9,7 @@
 #ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_COMMON_TOINT_HPP_INCLUDED
 #define BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_COMMON_TOINT_HPP_INCLUDED
 #include <boost/simd/arithmetic/functions/toint.hpp>
+#include <boost/simd/include/functions/simd/bitwise_cast.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -20,7 +21,7 @@ namespace boost { namespace simd { namespace ext
     typedef typename dispatch::meta::as_integer<A0, signed> ::type result_type;
     result_type operator()(A0 const& a0) const
     {
-      return result_type(a0);
+      return bitwise_cast<result_type>(a0);
     }
   };
 
