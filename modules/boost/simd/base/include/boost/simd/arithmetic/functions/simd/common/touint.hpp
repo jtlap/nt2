2856613af36db6cd5d091dcfb1cc0da7880c8882
Mathias Gaunard <mathias@gaunard.com>
2013-07-03 14:19:12 +0200

    common implement for touint depending on toint for architectures that don't have it

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/common/touint.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/common/touint.hpp
@@ -8,8 +8,16 @@
 //==============================================================================
 #ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_COMMON_TOUINT_HPP_INCLUDED
 #define BOOST_SIMD_ARITHMETIC_FUNCTIONS_SIMD_COMMON_TOUINT_HPP_INCLUDED
+
 #include <boost/simd/arithmetic/functions/touint.hpp>
-#include <boost/simd/include/functions/bitwise_cast.hpp>
+#include <boost/simd/include/functions/simd/bitwise_cast.hpp>
+#include <boost/simd/include/functions/simd/if_else.hpp>
+#include <boost/simd/include/functions/simd/splat.hpp>
+#include <boost/simd/include/functions/simd/is_less.hpp>
+#include <boost/simd/include/functions/simd/toint.hpp>
+#include <boost/simd/include/functions/simd/minus.hpp>
+#include <boost/simd/include/functions/simd/plus.hpp>
+#include <boost/simd/sdk/meta/scalar_of.hpp>
 #include <boost/dispatch/meta/as_integer.hpp>
 
 namespace boost { namespace simd { namespace ext
@@ -28,6 +36,22 @@ namespace boost { namespace simd { namespace ext
     }
   };
 
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::touint_, tag::cpu_
+                            , (A0)(X)
+                            , ((simd_<floating_<A0>,X>))
+                            )
+  {
+    typedef typename dispatch::meta::as_integer<A0, unsigned> ::type result_type;
+    BOOST_FORCEINLINE
+    result_type
+    operator()(A0 const& a0) const
+    {
+      typedef typename dispatch::meta::as_integer<typename meta::scalar_of<A0>::type, signed>::type signed_integer;
+      A0 sign_f = splat<A0>(Valmax<signed_integer>())+1;
+      result_type sign_i = splat<result_type>(Valmax<signed_integer>())+1;
+      return if_else(a0 < sign_f, bitwise_cast<result_type>(toint(a0)), bitwise_cast<result_type>(toint(a0 - sign_f)) + sign_i);
+    }
+  };
 
 } } }
 
