cf013cfa4e6267952d7b604b16191c8358e1ecb4
Mathias Gaunard <mathias@gaunard.com>
2013-07-08 18:33:09 +0200

    re-order nt2/table.hpp includes to avoid instantiating colon too early

--- modules/core/container/table/include/nt2/table.hpp
+++ modules/core/container/table/include/nt2/table.hpp
@@ -12,16 +12,16 @@
 //==============================================================================
 // Include and injects table into nt2 namespace with all required features
 //==============================================================================
-#include <nt2/core/utility/share.hpp>
-#include <nt2/core/container/table/semantic.hpp>
-#include <nt2/include/functions/of_size.hpp>
-#include <nt2/operator/operator.hpp>
 #include <nt2/core/container/table/table.hpp>
-#include <nt2/core/container/colon/colon.hpp>
-#include <nt2/core/container/extremum/extremum.hpp>
+#include <nt2/core/container/table/semantic.hpp>
+#include <nt2/core/utility/share.hpp>
+#include <nt2/include/functions/of_size.hpp>
 #include <nt2/include/functions/function.hpp>
 #include <nt2/include/functions/display.hpp>
 #include <nt2/include/functions/disp.hpp>
+#include <nt2/operator/operator.hpp>
+#include <nt2/core/container/extremum/extremum.hpp>
+#include <nt2/core/container/colon/colon.hpp>
 
 namespace nt2
 {
