94ee15ede06f583b8f3045d441c2de8165b5a5b1
Mathias Gaunard <mathias@gaunard.com>
2013-07-08 15:57:53 +0200

    median and medianad only make sense on real values, enforcing it in scalar definitions and removing test in type.complex. Fix #483

--- modules/core/reduction/include/nt2/core/functions/scalar/medianad.hpp
+++ modules/core/reduction/include/nt2/core/functions/scalar/medianad.hpp
@@ -16,7 +16,7 @@ namespace nt2 { namespace ext
 {
 
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::medianad_, tag::cpu_, (A0)
-                            , (scalar_< unspecified_<A0> >)
+                            , (scalar_< arithmetic_<A0> >)
                             )
   {
     typedef A0 result_type;
@@ -27,7 +27,7 @@ namespace nt2 { namespace ext
   };
 
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::medianad_, tag::cpu_, (A0)(A1)
-                            , (scalar_< unspecified_<A0> >)
+                            , (scalar_< arithmetic_<A0> >)
                               (scalar_< integer_<A1> > )
                             )
   {
