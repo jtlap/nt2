db9937b05d4b2fce92d1c6f8eb1eea9dd2e32dc0
Mathias Gaunard <mathias@gaunard.com>
2013-07-09 18:21:36 +0200

    change the way we check the semantic of assignment for tieable functions

--- modules/core/utility/include/nt2/core/functions/common/tie.hpp
+++ modules/core/utility/include/nt2/core/functions/common/tie.hpp
@@ -13,22 +13,80 @@
 #include <nt2/include/functions/assign.hpp>
 #include <boost/simd/sdk/meta/iterate.hpp>
 
+#include <nt2/core/container/dsl/value_type.hpp>
 #include <boost/dispatch/meta/value_of.hpp>
+#include <boost/dispatch/meta/scalar_of.hpp>
 #include <boost/dispatch/meta/is_scalar.hpp>
+#include <boost/dispatch/meta/semantic_of.hpp>
 #include <boost/fusion/include/vector.hpp>
+#include <boost/fusion/include/is_sequence.hpp>
 #include <boost/mpl/assert.hpp>
 
 namespace boost { namespace dispatch { namespace meta
 {
+  template<class A0>
+  struct value_of< boost::fusion::vector1<A0> >
+  {
+    typedef boost::fusion::vector1< typename value_of<A0>::type > type;
+  };
+
+  // necessary because otherwise scalar_of doesn't know that vector1< complex<T> > is a scalar
+  template<class A0>
+  struct scalar_of< boost::fusion::vector1<A0> >
+  {
+    typedef boost::fusion::vector1< typename scalar_of<A0>::type > type;
+  };
+
   template<class A0, class A1>
   struct value_of< boost::fusion::vector2<A0, A1> >
   {
     typedef boost::fusion::vector2< typename value_of<A0>::type, typename value_of<A1>::type > type;
   };
+
+  // necessary because otherwise scalar_of doesn't know that vector2< complex<T>, complex<T> > is a scalar
+  template<class A0, class A1>
+  struct scalar_of< boost::fusion::vector2<A0, A1> >
+  {
+    typedef boost::fusion::vector2< typename scalar_of<A0>::type, typename scalar_of<A1>::type > type;
+  };
 } } }
 
 namespace nt2 { namespace ext
 {
+  template<class Domain, class Expr>
+  struct value_type<tag::assign_, Domain, 2, Expr>
+  {
+    // the usual; assignment behaves like assignment on the scalar values
+    template<class Tag, bool B, class Dummy = void>
+    struct impl
+         : meta::call<tag::assign_( typename meta::scalar_of< typename boost::dispatch::meta::semantic_of< typename boost::proto::result_of::child_c<Expr&, 0>::type >::type >::type
+                                  , typename meta::scalar_of< typename boost::dispatch::meta::semantic_of< typename boost::proto::result_of::child_c<Expr&, 1>::type >::type >::type
+                                  )
+                     >
+    {
+    };
+
+    // if tie is used, we assume the semantic of assigning a value to
+    // a fusion sequence is always valid if assigning the first element is
+    // TODO: make nodes specify their semantic per output arity?
+    template<class Dummy>
+    struct impl<tag::tie_, false, Dummy>
+         : meta::call<tag::assign_( typename meta::scalar_of< typename boost::fusion::result_of::at_c< typename boost::dispatch::meta::semantic_of< typename boost::proto::result_of::child_c<Expr&, 0>::type >::type, 0 >::type >::type
+                                  , typename meta::scalar_of< typename boost::dispatch::meta::semantic_of< typename boost::proto::result_of::child_c<Expr&, 1>::type >::type >::type
+                                  )
+                     >
+    {
+    };
+
+    typedef impl< typename Expr::proto_child0::proto_tag
+                , boost::fusion::traits::
+                  is_sequence< typename boost::dispatch::meta::
+                               semantic_of< typename Expr::proto_child1 >::type
+                             >::value
+                > impl_;
+    typedef typename impl_::type type;
+  };
+
   // resizing a tie resizes all arguments
   template<class Domain, int N, class Expr>
   struct resize<tag::tie_, Domain, N, Expr>
@@ -55,19 +113,6 @@ namespace nt2 { namespace ext
     }
   };
 
-  // semantic of assigning a value to a fusion sequence is assumed to always
-  // be valid if assigning the first element is
-  // TODO: make nodes specify their semantic per output arity?
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::assign_, tag::cpu_
-                            , (A0)(A1)
-                            , (fusion_sequence_<A0>)
-                              (generic_< unspecified_<A1> >)
-                            )
-  {
-    typedef typename boost::fusion::result_of::at_c<A0, 0>::type first;
-    typedef typename meta::call<tag::assign_(first, A1 const&)>::type result_type;
-  };
-
   // semantic of assigning a fusion sequence to a fusion sequence is assumed
   // to always be valid if assigning the first element is
   // TODO: check other elements?
@@ -82,8 +127,8 @@ namespace nt2 { namespace ext
     typedef typename meta::call<tag::assign_(first, first2)>::type result_type;
   };
 
-  // semantic of assigning a fusion sequence to a value only takes
-  // first value
+  // semantic of assigning a fusion sequence to a value can
+  // only take the first value
   // Not allowed if the fusion sequence is actually a scalar
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::assign_, tag::cpu_
                             , (A0)(A1)
