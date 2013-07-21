15ad075926a75993ef976fe3d02244fdb528bf58
jtlap <jtlapreste@gmail.com>
2013-07-21 09:40:53 +0200

    prefixing abs in abs cover

--- modules/core/base/cover/arithmetic/simd/abs.cpp
+++ modules/core/base/cover/arithmetic/simd/abs.cpp
@@ -32,7 +32,7 @@ NT2_TEST_CASE_TPL ( abs_real_types,  NT2_SIMD_REAL_TYPES)
   nt2::roll(in1, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
 
   std::vector<r_t>  ref(NR);
-  for(nt2::uint32_t i=0; i < NR ; ++i) ref[i] = abs(in1[i]);
+  for(nt2::uint32_t i=0; i < NR ; ++i) ref[i] = nt2::abs(in1[i]);
   NT2_COVER_ULP_EQUAL(abs_, ((nT, in1)), ref, 0);
 }
 
