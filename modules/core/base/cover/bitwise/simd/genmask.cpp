bc7c1c8e0791e56a4ca9dfef8b7462195d955271
jtlap <jtlapreste@gmail.com>
2013-07-21 15:32:25 +0200

    updating simd cover for base/bitwise/simd

--- modules/core/base/cover/bitwise/simd/genmask.cpp
+++ modules/core/base/cover/bitwise/simd/genmask.cpp
@@ -6,109 +6,32 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 bitwise toolbox - genmask/simd Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// cover test behavior of bitwise components in simd mode
-//////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 18/02/2011
-///
 #include <nt2/bitwise/include/functions/genmask.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <nt2/include/functions/max.hpp>
-#include <boost/type_traits/is_same.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/as_floating.hpp>
-#include <nt2/sdk/meta/as_signed.hpp>
-#include <nt2/sdk/meta/upgrade.hpp>
-#include <nt2/sdk/meta/downgrade.hpp>
-#include <nt2/sdk/meta/scalar_of.hpp>
-#include <boost/dispatch/meta/as_floating.hpp>
-#include <boost/type_traits/common_type.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/include/constants/valmin.hpp>
+#include <nt2/include/constants/valmax.hpp>
+#include <nt2/sdk/unit/tests/cover.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/module.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
+#include <vector>
 
-#include <nt2/constant/constant.hpp>
-#include <nt2/sdk/meta/cardinal_of.hpp>
-#include <nt2/include/functions/splat.hpp>
-
-#include <nt2/include/functions/aligned_load.hpp>
-#include <nt2/constant/constant.hpp>
-
-
-NT2_TEST_CASE_TPL ( genmask_real__1_0,  NT2_SIMD_REAL_TYPES)
+NT2_TEST_CASE_TPL ( genmask_real_types,  NT2_SIMD_TYPES)
 {
   using nt2::genmask;
   using nt2::tag::genmask_;
-  using nt2::aligned_load;
   using boost::simd::native;
-  using nt2::meta::cardinal_of;
   typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<genmask_(vT)>::type r_t;
-  typedef typename nt2::meta::call<genmask_(T)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
+  typedef native<T,ext_t>                nT;
+
+  typedef typename nt2::meta::call<genmask_(T)>::type r_t;
 
   // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
-      {
-        vT a0 = aligned_load<vT>(&tab_a0[0],j);
-        r_t v = genmask(a0);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
-        {
+  nt2::uint32_t NR  = NT2_NB_RANDOM_TEST;
+  std::vector<T> in1(NR);
+  nt2::roll(in1, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
 
-          NT2_TEST_EQUAL( v[i],ssr_t(nt2::genmask (a0[i])));
-        }
-      }
-
-  }
-} // end of test for floating_
-
-NT2_TEST_CASE_TPL ( genmask_integer__1_0,  NT2_SIMD_INTEGRAL_TYPES)
-{
-  using nt2::genmask;
-  using nt2::tag::genmask_;
-  using nt2::aligned_load;
-  using boost::simd::native;
-  using nt2::meta::cardinal_of;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<genmask_(vT)>::type r_t;
-  typedef typename nt2::meta::call<genmask_(T)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
-
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
-      {
-        vT a0 = aligned_load<vT>(&tab_a0[0],j);
-        r_t v = genmask(a0);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
-        {
-
-          NT2_TEST_EQUAL( v[i],ssr_t(nt2::genmask (a0[i])));
-        }
-      }
-
-  }
-} // end of test for integer_
+  std::vector<r_t>  ref(NR);
+  for(nt2::uint32_t i=0; i < NR ; ++i) ref[i] = nt2::genmask(in1[i]);
+  NT2_COVER_ULP_EQUAL(genmask_, ((nT, in1)), ref, 0);
+}
--- modules/core/base/cover/bitwise/simd/lo.cpp
+++ modules/core/base/cover/bitwise/simd/lo.cpp
@@ -6,115 +6,32 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 bitwise toolbox - lo/simd Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// cover test behavior of bitwise components in simd mode
-//////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 18/02/2011
-///
 #include <nt2/bitwise/include/functions/lo.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <nt2/include/functions/max.hpp>
-#include <nt2/sdk/meta/downgrade.hpp>
-
-#include <boost/type_traits/is_same.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/as_floating.hpp>
-#include <nt2/sdk/meta/as_signed.hpp>
-#include <nt2/sdk/meta/upgrade.hpp>
-#include <nt2/sdk/meta/downgrade.hpp>
-#include <nt2/sdk/meta/scalar_of.hpp>
-#include <boost/dispatch/meta/as_floating.hpp>
-#include <boost/type_traits/common_type.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/include/constants/valmin.hpp>
+#include <nt2/include/constants/valmax.hpp>
+#include <nt2/sdk/unit/tests/cover.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/module.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
+#include <vector>
 
-#include <nt2/constant/constant.hpp>
-#include <nt2/sdk/meta/cardinal_of.hpp>
-#include <nt2/include/functions/splat.hpp>
-
-#include <nt2/include/functions/aligned_load.hpp>
-#include <nt2/constant/constant.hpp>
-
-
-NT2_TEST_CASE_TPL ( lo_real__1_0,  NT2_SIMD_REAL_TYPES)
+NT2_TEST_CASE_TPL ( lo_real_types,  NT2_SIMD_TYPES)
 {
   using nt2::lo;
   using nt2::tag::lo_;
-  using nt2::aligned_load;
   using boost::simd::native;
-  using nt2::meta::cardinal_of;
-  typedef typename nt2::meta::as_integer<T,unsigned>::type ir_t;
   typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<lo_(vT)>::type r_t;
-  typedef typename nt2::meta::call<lo_(T)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
+  typedef native<T,ext_t>                nT;
+
+  typedef typename nt2::meta::call<lo_(T)>::type r_t;
 
   // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    typedef typename nt2::meta::as_integer<T,unsigned>::type ir_t;
-    NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(10));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
-      {
-        vT a0 = aligned_load<vT>(&tab_a0[0],j);
-        r_t v = lo(a0);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
-        {
+  nt2::uint32_t NR  = NT2_NB_RANDOM_TEST;
+  std::vector<T> in1(NR);
+  nt2::roll(in1, nt2::Valmin<T>()/2, nt2::Valmax<T>()/2);
 
-          NT2_TEST_EQUAL( v[i],ssr_t(nt2::lo (a0[i])));
-        }
-      }
-
-  }
-} // end of test for floating_
-
-NT2_TEST_CASE_TPL ( lo_sintgt_8__1_0,  NT2_SIMD_SIGNED_INT_GT_8_TYPES)
-{
-  using nt2::lo;
-  using nt2::tag::lo_;
-  using nt2::aligned_load;
-  using boost::simd::native;
-  using nt2::meta::cardinal_of;
-  typedef typename nt2::meta::as_integer<T,unsigned>::type ir_t;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<lo_(vT)>::type r_t;
-  typedef typename nt2::meta::call<lo_(T)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
-
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    typedef typename nt2::meta::as_integer<T,unsigned>::type ir_t;
-    NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(10));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
-      {
-        vT a0 = aligned_load<vT>(&tab_a0[0],j);
-        r_t v = lo(a0);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
-        {
-
-          NT2_TEST_EQUAL( v[i],ssr_t(nt2::lo (a0[i])));
-        }
-      }
-
-  }
-} // end of test for sintgt_8_
+  std::vector<r_t>  ref(NR);
+  for(nt2::uint32_t i=0; i < NR ; ++i) ref[i] = nt2::lo(in1[i]);
+  NT2_COVER_ULP_EQUAL(lo_, ((nT, in1)), ref, 0);
+}
