b14a0129ae484c0c9e9bd9f58fdf7b5546693399
Mathias Gaunard <mathias@gaunard.com>
2013-07-23 13:55:40 +0200

    iterator_traits needs a specialization for T* __restrict const

--- modules/boost/dispatch/include/boost/dispatch/meta/is_iterator.hpp
+++ modules/boost/dispatch/include/boost/dispatch/meta/is_iterator.hpp
@@ -52,6 +52,16 @@ namespace boost
       typedef T& BOOST_DISPATCH_RESTRICT reference;
 #endif
     };
+
+    template<class T>
+    struct iterator_traits<T* BOOST_DISPATCH_RESTRICT const>
+     : boost::detail::iterator_traits<T* const>
+    {
+      typedef T* BOOST_DISPATCH_RESTRICT pointer;
+#ifndef BOOST_DISPATCH_NO_RESTRICT_REFERENCES
+      typedef T& BOOST_DISPATCH_RESTRICT reference;
+#endif
+    };
   }
 }
 
@@ -61,6 +71,16 @@ namespace std
   template<class T>
   struct iterator_traits<T* BOOST_DISPATCH_RESTRICT>
    : iterator_traits<T*>
+  {
+    typedef T* BOOST_DISPATCH_RESTRICT pointer;
+#ifndef BOOST_DISPATCH_NO_RESTRICT_REFERENCES
+    typedef T& BOOST_DISPATCH_RESTRICT reference;
+#endif
+  };
+
+  template<class T>
+  struct iterator_traits<T* BOOST_DISPATCH_RESTRICT const>
+   : iterator_traits<T* const>
   {
     typedef T* BOOST_DISPATCH_RESTRICT pointer;
 #ifndef BOOST_DISPATCH_NO_RESTRICT_REFERENCES
