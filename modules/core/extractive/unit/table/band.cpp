4ab6a0c2c4479d0948ac1f0d757fc036e4358144
Joel Falcou <joel.falcou@lri.fr>
2013-08-01 13:48:08 +0200

    Fix band tests

--- modules/core/extractive/unit/table/band.cpp
+++ modules/core/extractive/unit/table/band.cpp
@@ -11,70 +11,112 @@
 
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
-#include <nt2/include/functions/reshape.hpp>
-#include <nt2/include/functions/colon.hpp>
-#include <nt2/include/functions/whereij.hpp>
-#include <nt2/include/functions/is_equal.hpp>
-#include <nt2/include/functions/logical_and.hpp>
-#include <nt2/include/functions/is_greater_equal.hpp>
-#include <nt2/include/functions/is_less_equal.hpp>
-#include <nt2/include/functions/minusone.hpp>
-#include <nt2/include/functions/oneplus.hpp>
-#include <nt2/include/functions/plus.hpp>
-#include <nt2/include/constants/two.hpp>
-#include <nt2/sdk/meta/as_logical.hpp>
+#include <nt2/sdk/unit/tests/exceptions.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
 
-struct fct1
+NT2_TEST_CASE_TPL( band_types, NT2_TYPES )
 {
-  template < class A0, class A1>
-  typename nt2::meta::as_logical<A0>::type
-  operator ()(const A0& i, const A1& j) const
-  {
-    return nt2::logical_and(nt2::ge(i, nt2::minusone(j)),
-                            nt2::le(i, nt2::oneplus(j))
-                           );
-  }
-};
+  using boost::mpl::_;
+  using nt2::meta::value_type_;
 
-struct fct2
-{
-  template < class A0, class A1>
-  typename nt2::meta::as_logical<A0>::type
-  operator ()(const A0& i, const A1& j) const
-  {
-    return  nt2::logical_and(nt2::ge(i, nt2::minusone(j)),
-                             nt2::le(i, nt2::Two<A1>()+j)
-                            );
-  }
-};
-
-
-NT2_TEST_CASE_TPL( band, NT2_TYPES )
-{
-  nt2::table<T> xx, x, y = nt2::reshape(nt2::_(T(1), T(20)), 4, 5);
-  x = nt2::diagonal(y);
-  xx = nt2::whereij(nt2::functor<nt2::tag::is_equal_>(),  y, T(0));
-  NT2_TEST_EQUAL(x, xx);
+  nt2::table<T> x( nt2::of_size(4,4) );
+  NT2_TEST_EXPR_TYPE( nt2::band(x)      , (value_type_<_>), (T));
+  NT2_TEST_EXPR_TYPE( nt2::band(x,1)    , (value_type_<_>), (T));
+  NT2_TEST_EXPR_TYPE( nt2::band(x,-1,1) , (value_type_<_>), (T));
 }
 
 NT2_TEST_CASE_TPL( band_scalar, NT2_TYPES )
 {
-  T x =  T(4.5);
-  nt2::table<T> xx = T(4.5);
-  NT2_TEST_EQUAL(nt2::band(x), x);
-  NT2_TEST_EQUAL(nt2::band(x), xx);
-  NT2_TEST_EQUAL(nt2::band(x), band(xx));
-  NT2_TEST_EQUAL(nt2::band(x, 3), band(xx, 3));
-  NT2_TEST_EQUAL(nt2::band(x, 3, 4), band(xx, 3, 4)) ;
+  // Diretc diagonal
+  T x = nt2::band(T(42));
+  NT2_TEST_EQUAL( x, T(42) );
+
+  x = nt2::diagonal(T(69));
+  NT2_TEST_EQUAL( x, T(69) );
+
+  // Include some amount of subdiagonal
+  x = nt2::band(T(42),-1);
+  NT2_TEST_EQUAL( x, T(0) );
+
+  x = nt2::band(T(42),0);
+  NT2_TEST_EQUAL( x, T(42) );
+
+  x = nt2::band(T(42),1);
+  NT2_TEST_EQUAL( x, T(42) );
+
+  // Include some amount of sub/superdiagonal
+
+  x = nt2::band(T(42),2,2);
+  NT2_TEST_EQUAL( x, T(0) );
+
+
+  x = nt2::band(T(42),-2,2);
+  NT2_TEST_EQUAL( x, T(42) );
+
+  x = nt2::band(T(42),-2,-2);
+  NT2_TEST_EQUAL( x, T(0) );
 }
 
-NT2_TEST_CASE_TPL( offset_band1, NT2_TYPES )
+NT2_TEST_CASE( band_assert )
 {
-  nt2::table<T> xx, x, y = nt2::reshape(nt2::_(T(1), T(20)), 4, 5);
+  NT2_TEST_ASSERT(nt2::band(13,2,-2));
+
+  nt2::table<double> x( nt2::of_size(2,5,3) );
+  NT2_TEST_ASSERT(nt2::band(x));
+  NT2_TEST_ASSERT(nt2::band(x,1));
+  NT2_TEST_ASSERT(nt2::band(x,1,2));
+
+  nt2::table<double> y( nt2::of_size(4,4) );
+  NT2_TEST_ASSERT(nt2::band(y,-1));
+  NT2_TEST_ASSERT(nt2::band(y,1,-2));
+}
+
+NT2_TEST_CASE_TPL( band, NT2_TYPES )
+{
+  nt2::table<T>   x
+                , y( nt2::of_size(4,5) )
+                , ref( nt2::of_size(4,5) );
+
+  for(int j=1;j<=5;j++)
+    for(int i=1;i<=4;i++)
+      y(i,j) = i + 10*j;
+
+  for(int j=1;j<=5;j++)
+    for(int i=1;i<=4;i++)
+      ref(i,j) = (i==j) ? y(i,j) : T(0);
+
+  x = nt2::diagonal(y);
+
+  NT2_TEST_EQUAL( x, ref );
+
+  x = nt2::band(y,0);
+  NT2_TEST_EQUAL( x, ref );
+
+  for(int j=1;j<=5;j++)
+    for(int i=1;i<=4;i++)
+      ref(i,j) = ((i>=(j-1))&&(i<=j+1)) ? y(i,j) : T(0);
+
   x = nt2::band(y,1);
-  xx = nt2::whereij(fct1(),  y, T(0));
-  NT2_TEST_EQUAL(x, xx);
-  x = nt2::band(y,1,2);
-  xx = nt2::whereij(fct2(),  y, T(0));
-  NT2_TEST_EQUAL(x, xx);
+  NT2_TEST_EQUAL( x, ref );
+
+  for(int j=1;j<=5;j++)
+    for(int i=1;i<=4;i++)
+      ref(i,j) = ((i>=(j-3))&&(i<=j-1)) ? y(i,j) : T(0);
+
+  x = nt2::band(y,1,3);
+  NT2_TEST_EQUAL( x, ref );
+
+  for(int j=1;j<=5;j++)
+    for(int i=1;i<=4;i++)
+      ref(i,j) = ((i>=(j-1))&&(i<=j+1)) ? y(i,j) : T(0);
+
+  x = nt2::band(y,-1,1);
+  NT2_TEST_EQUAL( x, ref );
+
+  for(int j=1;j<=5;j++)
+    for(int i=1;i<=4;i++)
+      ref(i,j) = ((i>=(j+1))&&(i<=j+3)) ? y(i,j) : T(0);
+
+  x = nt2::band(y,-3,-1);
+  NT2_TEST_EQUAL( x, ref );
 }
