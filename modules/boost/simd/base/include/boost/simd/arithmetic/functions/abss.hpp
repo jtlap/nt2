4437f71028699ec5c87c73483b6779a2f40bfad4
jtlap <jtlapreste@gmail.com>
2013-08-14 15:03:03 +0200

    oxygen correction in abss.hpp

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/abss.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/abss.hpp
@@ -94,7 +94,7 @@ namespace boost { namespace simd {
 
     saturated_abs
 
-    @param @c a0    value whose absolute value will be returned.
+    @param  a0    value whose absolute value will be returned.
 
     @return         a value of the same type as the input.
 
