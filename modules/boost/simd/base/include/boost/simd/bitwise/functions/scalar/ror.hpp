7ec8adf62214a4956cf0081cc05c7c916311c3de
Joel Falcou <joel.falcou@lri.fr>
2013-08-21 09:41:28 +0200

    Fix ror scalar implementation

--- modules/boost/simd/base/include/boost/simd/bitwise/functions/scalar/ror.hpp
+++ modules/boost/simd/base/include/boost/simd/bitwise/functions/scalar/ror.hpp
@@ -10,35 +10,38 @@
 #define BOOST_SIMD_BITWISE_FUNCTIONS_SCALAR_ROR_HPP_INCLUDED
 
 #include <boost/simd/bitwise/functions/ror.hpp>
-#include <boost/simd/include/functions/scalar/shift_left.hpp>
 #include <boost/simd/include/functions/scalar/shr.hpp>
+#include <boost/simd/include/functions/scalar/shift_left.hpp>
 #include <boost/simd/include/functions/scalar/bitwise_cast.hpp>
 #include <boost/dispatch/meta/as_integer.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::ror_, tag::cpu_, (A0)(A1)
-                            , (scalar_< arithmetic_<A0> >)
-                              (scalar_< integer_<A1> >)
-                            )
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::ror_, tag::cpu_
+                                    , (A0)(A1)
+                                    , (scalar_< arithmetic_<A0> >)
+                                      (scalar_< integer_<A1> >)
+                                    )
   {
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(2)
     {
-      static const std::size_t width = sizeof(A0)*CHAR_BIT;
+      std::size_t const width = sizeof(A0)*CHAR_BIT;
       return shri(a0, a1) | shli(a0, (width-a1) & (width-1));
     }
   };
 
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::ror_, tag::cpu_, (A0)(A1)
-                            , (scalar_< floating_<A0> >)(scalar_< integer_<A1> >)
-                            )
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::ror_, tag::cpu_
+                                    , (A0)(A1)
+                                    , (scalar_< floating_<A0> >)
+                                      (scalar_< integer_<A1> >)
+                                    )
   {
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(2)
     {
       typedef typename dispatch::meta::as_integer<A0, unsigned>::type itype;
-      return bitwise_cast<result_type>(ror(bitwise_cast<itype>(a0)));
+      return bitwise_cast<result_type>(ror(bitwise_cast<itype>(a0),a1));
     }
   };
 } } }
--- modules/boost/simd/base/unit/bitwise/scalar/ror.cpp
+++ modules/boost/simd/base/unit/bitwise/scalar/ror.cpp
@@ -6,43 +6,46 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 boost.simd.bitwise toolbox - ror/scalar Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// unit test behavior of boost.simd.bitwise components in scalar mode
-//////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 18/02/2011
-///
 #include <boost/simd/bitwise/include/functions/ror.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <boost/type_traits/is_same.hpp>
 #include <boost/dispatch/functor/meta/call.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+
 #include <nt2/sdk/unit/module.hpp>
-#include <boost/simd/constant/constant.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
 
-
-NT2_TEST_CASE_TPL ( ror_integer__2_0,  BOOST_SIMD_INTEGRAL_TYPES)
+NT2_TEST_CASE_TPL ( ror_integer, BOOST_SIMD_INTEGRAL_TYPES)
 {
-
   using boost::simd::ror;
   using boost::simd::tag::ror_;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
-  typedef typename boost::dispatch::meta::call<ror_(T,iT)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
-  typedef T wished_r_t;
-
 
   // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
+  NT2_TEST_TYPE_IS( typename boost::dispatch::meta::call<ror_(T,int)>::type
+                  , T
+                  );
 
+  std::size_t w = sizeof(T)*CHAR_BIT;
 
-  // specific values tests
-  NT2_TEST_EQUAL(ror(T(2),T(2)), r_t(boost::simd::One<T>()<<(sizeof(T)*8-1)));
-  NT2_TEST_EQUAL(ror(boost::simd::Mone<T>(),T(1)), boost::simd::Mone<T>());
-  NT2_TEST_EQUAL(ror(boost::simd::Mone<T>(),T(5)), boost::simd::Mone<T>());
-  NT2_TEST_EQUAL(ror(boost::simd::One<T>(),T(1)), r_t(boost::simd::One<T>()<<(sizeof(T)*8-1)));
-  NT2_TEST_EQUAL(ror(boost::simd::Zero<T>(),T(1)), boost::simd::Zero<T>());
-} // end of test for integer_
+  for(std::size_t i=0;i<w;++i)
+    NT2_TEST_EQUAL(ror(T(1),i), T(T(1)<<((w-i) & (w-1))));
+}
+
+NT2_TEST_CASE_TPL ( ror_real, BOOST_SIMD_REAL_TYPES)
+{
+  using boost::simd::ror;
+  using boost::simd::tag::ror_;
+  using boost::simd::bitwise_cast;
+
+  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
+
+  // return type conformity test
+  NT2_TEST_TYPE_IS( typename boost::dispatch::meta::call<ror_(T,int)>::type
+                  , T
+                  );
+
+  std::size_t w = sizeof(T)*CHAR_BIT;
+
+  for(std::size_t i=0;i<w;++i)
+    NT2_TEST_EQUAL( ror(bitwise_cast<T>(iT(1)),i)
+                  , bitwise_cast<T>(iT(1)<<((w-i) & (w-1)))
+                  );
+}
