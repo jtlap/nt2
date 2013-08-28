764c54fe4215e4dac7b3c37bc4293250f51bfd2d
jtlap <jtlapreste@gmail.com>
2013-08-28 17:38:39 +0200

    benches for sinc

---
+++ modules/core/trigonometric/bench/simd/sinc.cpp
@@ -0,0 +1,69 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#define NT2_BENCH_MODULE "nt2 trigonometric toolbox - sinc/simd Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// timing Test behavior of trigonometric components in simd mode
+//////////////////////////////////////////////////////////////////////////////
+#include <nt2/trigonometric/include/functions/sinc.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <nt2/sdk/bench/benchmark.hpp>
+#include <nt2/sdk/bench/timing.hpp>
+#include <boost/dispatch/meta/as_integer.hpp>
+#include <cmath>
+typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+
+//////////////////////////////////////////////////////////////////////////////
+// simd runtime benchmark for functor<sinc_> from trigonometric
+//////////////////////////////////////////////////////////////////////////////
+using nt2::tag::sinc_;
+
+//////////////////////////////////////////////////////////////////////////////
+// range macro
+//////////////////////////////////////////////////////////////////////////////
+#define RS(T,V1,V2) (T, (V1) ,(V2))
+
+namespace n1 {
+  typedef float T;
+  typedef boost::dispatch::meta::as_integer<T>::type iT;
+  typedef boost::simd::native<T,ext_t> vT;
+  NT2_TIMING(sinc_,(RS(vT,T(-40),T(40))))
+}
+namespace n2 {
+  typedef double T;
+  typedef boost::dispatch::meta::as_integer<T>::type iT;
+  typedef boost::simd::native<T,ext_t> vT;
+  NT2_TIMING(sinc_,(RS(vT,T(-40),T(40))))
+}
+namespace n3 {
+  typedef nt2::int32_t T;
+  typedef boost::dispatch::meta::as_integer<T>::type iT;
+  typedef boost::simd::native<T,ext_t> vT;
+  NT2_TIMING(sinc_,(RS(vT,T(-40),T(40))))
+}
+namespace n4 {
+  typedef nt2::int64_t T;
+  typedef boost::dispatch::meta::as_integer<T>::type iT;
+  typedef boost::simd::native<T,ext_t> vT;
+  NT2_TIMING(sinc_,(RS(vT,T(-40),T(40))))
+}
+namespace n5 {
+  typedef nt2::uint32_t T;
+  typedef boost::dispatch::meta::as_integer<T>::type iT;
+  typedef boost::simd::native<T,ext_t> vT;
+  NT2_TIMING(sinc_,(RS(vT,T(-40),T(40))))
+}
+namespace n6 {
+  typedef nt2::uint64_t T;
+  typedef boost::dispatch::meta::as_integer<T>::type iT;
+  typedef boost::simd::native<T,ext_t> vT;
+  NT2_TIMING(sinc_,(RS(vT,T(-40),T(40))))
+}
+
+#undef RS
