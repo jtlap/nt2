f7400e254f02a5853ac4fe7bcd8be6d267bb5ba9
jtlap <jtlapreste@gmail.com>
2013-08-28 17:35:38 +0200

    sinc and sincpi cover for small values

---
+++ modules/core/trigonometric/cover/scalar/sinc.cpp
@@ -0,0 +1,64 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#define NT2_UNIT_MODULE "nt2 trigonometric toolbox - sinc/scalar Mode"
+
+//////////////////////////////////////////////////////////////////////////////
+// cover test behavior of trigonometric components in scalar mode
+//////////////////////////////////////////////////////////////////////////////
+/// created  by jt the 11/02/2011
+///
+#include <nt2/trigonometric/include/functions/sinc.hpp>
+#include <nt2/include/functions/max.hpp>
+#include <nt2/trigonometric/constants.hpp>
+
+#include <boost/type_traits/is_same.hpp>
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/meta/as_floating.hpp>
+#include <nt2/sdk/meta/as_signed.hpp>
+#include <nt2/sdk/meta/upgrade.hpp>
+#include <nt2/sdk/meta/downgrade.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
+#include <boost/dispatch/meta/as_floating.hpp>
+#include <boost/type_traits/common_type.hpp>
+#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <boost/math/special_functions/sinc.hpp>
+#include <nt2/constant/constant.hpp>
+
+
+NT2_TEST_CASE_TPL ( sinc_real,  NT2_REAL_TYPES)
+{
+
+  using nt2::sinc;
+  using nt2::tag::sinc_;
+  typedef T u_t;
+  typedef typename nt2::meta::call<sinc_(T)>::type r_t;
+  typedef typename boost::dispatch::meta::as_floating<T>::type wished_r_t;
+
+  double ulpd;
+  ulpd=0.0;
+
+  // random verifications
+  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
+  {
+    NT2_CREATE_BUF(tab_a0,T, NR, -nt2::Eps<T>(), nt2::Eps<T>());
+    double ulp0, ulpd ; ulpd=ulp0=0.0;
+    T a0;
+    for(nt2::uint32_t j =0; j < NR; ++j )
+      {
+        std::cout << "for param "
+                  << "  a0 = "<< u_t(a0 = tab_a0[j])
+                  << std::endl;
+        NT2_TEST_ULP_EQUAL( nt2::sinc(a0),boost::math::sinc_pi(a0),1.0);
+        ulp0=nt2::max(ulpd,ulp0);
+     }
+     std::cout << "max ulp found is: " << ulp0 << std::endl;
+   }
+} // end of test for floating_
