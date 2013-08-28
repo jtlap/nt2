8bd58a7256cdbba5bb9b3582eac399271077b015
Mathias Gaunard <mathias@gaunard.com>
2013-08-28 20:05:22 +0200

    remove unused template parameter in avx sum

--- modules/boost/simd/reduction/include/boost/simd/reduction/functions/simd/sse/avx/sum.hpp
+++ modules/boost/simd/reduction/include/boost/simd/reduction/functions/simd/sse/avx/sum.hpp
@@ -33,7 +33,7 @@ namespace boost { namespace simd { namespace ext
   };
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::sum_, tag::cpu_
-                                    , (A0)(X)
+                                    , (A0)
                                     , ((simd_<single_<A0>,boost::simd::tag::avx_>))
                                     )
   {
