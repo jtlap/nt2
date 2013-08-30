8105e839387a0a26c9b07c4dccf0e1888aa89750
jtlap <jtlapreste@gmail.com>
2013-08-30 23:56:28 +0200

    suppressing integer yests from some trigo test files

--- modules/core/hyperbolic/cover/scalar/tanh.cpp
+++ modules/core/hyperbolic/cover/scalar/tanh.cpp
@@ -70,42 +70,4 @@ NT2_TEST_CASE_TPL ( tanh_real__1_0,  NT2_REAL_TYPES)
    }
 } // end of test for floating_
 
-NT2_TEST_CASE_TPL ( tanh_unsigned_int__1_0,  NT2_UNSIGNED_TYPES)
-{
 
-  using nt2::tanh;
-  using nt2::tag::tanh_;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef typename nt2::meta::call<tanh_(T)>::type r_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef typename boost::dispatch::meta::as_floating<T>::type wished_r_t;
-
-
-  // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
-  double ulpd;
-  ulpd=0.0;
-
-} // end of test for unsigned_int_
-
-NT2_TEST_CASE_TPL ( tanh_signed_int__1_0,  NT2_INTEGRAL_SIGNED_TYPES)
-{
-
-  using nt2::tanh;
-  using nt2::tag::tanh_;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef typename nt2::meta::call<tanh_(T)>::type r_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  typedef typename nt2::meta::upgrade<T>::type u_t;
-  typedef typename boost::dispatch::meta::as_floating<T>::type wished_r_t;
-
-
-  // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
-  double ulpd;
-  ulpd=0.0;
-
-} // end of test for signed_int_
