8047d62056bc05a9f0ada3993604cd6ad390d93c
Mathias Gaunard <mathias@gaunard.com>
2013-08-31 12:57:00 +0200

    fix bad computation of cardinal for map with hybrid scalar/simd

--- modules/boost/simd/base/include/boost/simd/operator/functions/simd/common/map.hpp
+++ modules/boost/simd/base/include/boost/simd/operator/functions/simd/common/map.hpp
@@ -22,10 +22,11 @@
 #else
 #include <boost/dispatch/details/parameters.hpp>
 #include <boost/dispatch/functor/preprocessor/call.hpp>
-#include <boost/preprocessor/cat.hpp>
-#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/facilities/intercept.hpp>
+#include <boost/preprocessor/repetition/repeat.hpp>
 #include <boost/preprocessor/repetition/enum.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/preprocessor/repetition/enum_binary_params.hpp>
 #include <boost/preprocessor/repetition/repeat_from_to.hpp>
 #if defined(__WAVE__) && defined(BOOST_SIMD_CREATE_PREPROCESSED_FILES) && __INCLUDE_LEVEL__ == 0
 #pragma wave option(preserve: 2, line: 0, output: "preprocessed/map.hpp")
@@ -38,81 +39,92 @@
 #define M4(z,n,t) (A##n)
 #define M5(z,n,t) typename dispatch::meta::as_ref<_A##n>::type a##n
 
-#define M0(z,n,t)                                                              \
-namespace boost { namespace simd { namespace ext                               \
-{                                                                              \
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::map_, tag::cpu_         \
-                            , (Func)BOOST_PP_REPEAT(n, M4, ~)                  \
-                            , (unspecified_<Func>)BOOST_PP_REPEAT(n,M1,~)      \
-                            )                                                  \
-  {                                                                            \
-    template<class Sig>                                                        \
-    struct result;                                                             \
-                                                                               \
-    template<class This, class _Func, BOOST_PP_ENUM_PARAMS(n, class _A)>       \
-    struct result<This(_Func, BOOST_PP_ENUM_PARAMS(n, _A))>                    \
-    {                                                                          \
-      typedef typename dispatch::meta::                                        \
-      result_of< Func const( BOOST_PP_ENUM(n,M2,~) )                           \
-               >::type                                                         \
-      rtype;                                                                   \
-                                                                               \
-      template<class rtype, class Dummy = void>                                \
-      struct impl                                                              \
-      {                                                                        \
-        typedef typename meta::                                                \
-                vector_of< rtype, meta::cardinal_of<A0>::value >::type type;   \
-        static type call( Func const& f                                        \
-                        , BOOST_PP_ENUM(n, M5, ~)                              \
-                        )                                                      \
-        {                                                                      \
-          type that;                                                           \
-                                                                               \
-          for(size_t i = 0; i != meta::cardinal_of<A0>::value; ++i)            \
-            insert(f(BOOST_PP_ENUM(n, M3, ~)), that, i);                       \
-                                                                               \
-          return that;                                                         \
-        }                                                                      \
-      };                                                                       \
-                                                                               \
-      template<class Dummy>                                                    \
-      struct impl<void, Dummy>                                                 \
-      {                                                                        \
-        typedef void type;                                                     \
-        static type call( Func const& f                                        \
-                        , BOOST_PP_ENUM(n, M5, ~)                              \
-                        )                                                      \
-        {                                                                      \
-          for(size_t i = 0; i != meta::cardinal_of<A0>::value; ++i)            \
-            f(BOOST_PP_ENUM(n, M3, ~));                                        \
-        }                                                                      \
-                                                                               \
-      };                                                                       \
-                                                                               \
-      typedef impl<rtype> impl_;                                               \
-      typedef typename impl_::type type;                                       \
-    };                                                                         \
-                                                                               \
-    template<BOOST_PP_ENUM_PARAMS(n, class _A)>                                \
-    BOOST_FORCEINLINE                                                          \
-    typename result< implement( Func const&                                    \
-                              , BOOST_PP_ENUM_BINARY_PARAMS(n, _A, & a)        \
-                              )                                                \
-                   >::type                                                     \
-    operator()(Func const& f, BOOST_PP_ENUM_BINARY_PARAMS(n, _A, & a))         \
-    {                                                                          \
-      return result< implement( Func const&                                    \
-                              , BOOST_PP_ENUM_BINARY_PARAMS(n, _A, & a)        \
-                              )                                                \
-                   >::impl_::call(f, BOOST_PP_ENUM_PARAMS(n, a));              \
-                                                                               \
-                                                                               \
-    }                                                                          \
-  };                                                                           \
-} } }                                                                          \
-/**/
+namespace boost { namespace simd { namespace ext
+{
+  template<BOOST_PP_ENUM_BINARY_PARAMS(BOOST_DISPATCH_MAX_ARITY, class A, = int BOOST_PP_INTERCEPT)>
+  struct map_cardinal_size
+  {
+    #define M0(z,n,t) meta::cardinal_of<A##n>::value != 1u ? meta::cardinal_of<A##n>::value :
+    static const std::size_t value = BOOST_PP_REPEAT(BOOST_DISPATCH_MAX_ARITY, M0, ~) 1u;
+    #undef M0
+  };
 
-BOOST_PP_REPEAT_FROM_TO(1,BOOST_PP_INC(BOOST_DISPATCH_MAX_ARITY),M0,~)
+  #define M0(z,n,t)                                                                                \
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::map_, tag::cpu_                             \
+                            , (Func)BOOST_PP_REPEAT(n, M4, ~)                                      \
+                            , (unspecified_<Func>)BOOST_PP_REPEAT(n,M1,~)                          \
+                            )                                                                      \
+  {                                                                                                \
+    template<class Sig>                                                                            \
+    struct result;                                                                                 \
+                                                                                                   \
+    template<class This, class _Func, BOOST_PP_ENUM_PARAMS(n, class _A)>                           \
+    struct result<This(_Func, BOOST_PP_ENUM_PARAMS(n, _A))>                                        \
+    {                                                                                              \
+      typedef typename dispatch::meta::                                                            \
+      result_of< Func const( BOOST_PP_ENUM(n,M2,~) )                                               \
+               >::type                                                                             \
+      rtype;                                                                                       \
+                                                                                                   \
+      template<class rtype, class Dummy = void>                                                    \
+      struct impl                                                                                  \
+      {                                                                                            \
+        typedef typename meta::                                                                    \
+                vector_of< rtype                                                                   \
+                         , map_cardinal_size<BOOST_PP_ENUM_PARAMS(n, A)>::value                    \
+                         >::type type;                                                             \
+        static type call( Func const& f                                                            \
+                        , BOOST_PP_ENUM(n, M5, ~)                                                  \
+                        )                                                                          \
+        {                                                                                          \
+          type that;                                                                               \
+                                                                                                   \
+          for(size_t i = 0; i != map_cardinal_size<BOOST_PP_ENUM_PARAMS(n, A)>::value; ++i)        \
+            insert(f(BOOST_PP_ENUM(n, M3, ~)), that, i);                                           \
+                                                                                                   \
+          return that;                                                                             \
+        }                                                                                          \
+      };                                                                                           \
+                                                                                                   \
+      template<class Dummy>                                                                        \
+      struct impl<void, Dummy>                                                                     \
+      {                                                                                            \
+        typedef void type;                                                                         \
+        static type call( Func const& f                                                            \
+                        , BOOST_PP_ENUM(n, M5, ~)                                                  \
+                        )                                                                          \
+        {                                                                                          \
+          for(size_t i = 0; i != map_cardinal_size<BOOST_PP_ENUM_PARAMS(n, A)>::value; ++i)        \
+            f(BOOST_PP_ENUM(n, M3, ~));                                                            \
+        }                                                                                          \
+                                                                                                   \
+      };                                                                                           \
+                                                                                                   \
+      typedef impl<rtype> impl_;                                                                   \
+      typedef typename impl_::type type;                                                           \
+    };                                                                                             \
+                                                                                                   \
+    template<BOOST_PP_ENUM_PARAMS(n, class _A)>                                                    \
+    BOOST_FORCEINLINE                                                                              \
+    typename result< implement( Func const&                                                        \
+                              , BOOST_PP_ENUM_BINARY_PARAMS(n, _A, & a)                            \
+                              )                                                                    \
+                   >::type                                                                         \
+    operator()(Func const& f, BOOST_PP_ENUM_BINARY_PARAMS(n, _A, & a))                             \
+    {                                                                                              \
+      return result< implement( Func const&                                                        \
+                              , BOOST_PP_ENUM_BINARY_PARAMS(n, _A, & a)                            \
+                              )                                                                    \
+                   >::impl_::call(f, BOOST_PP_ENUM_PARAMS(n, a));                                  \
+                                                                                                   \
+                                                                                                   \
+    }                                                                                              \
+  };                                                                                               \
+  /**/
+
+  BOOST_PP_REPEAT_FROM_TO(1,BOOST_PP_INC(BOOST_DISPATCH_MAX_ARITY),M0,~)
+
+} } }
 
 #undef M5
 #undef M4
