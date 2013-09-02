6ef9cb13d27e5f9723b4ad507ca51e8961e726ad
jtlap <jtlapreste@gmail.com>
2013-09-02 12:29:06 +0200

    correcting oxygenation relative to return types

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/sqrt.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/sqrt.hpp
@@ -29,7 +29,6 @@ namespace boost { namespace simd { namespace tag
     Computes the square root of its parameter.
 
     @par semantic:
-
     For any given value @c x of floating type @c T:
 
     @code
