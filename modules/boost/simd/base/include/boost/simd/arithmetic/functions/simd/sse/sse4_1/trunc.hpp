93c42db1cb3fec94de06a589b173266745423ee2
jtlap <jtlapreste@gmail.com>
2013-09-02 05:29:56 +0200

    naming immediate in trunc and round2even intrinsic calls

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/sse/sse4_1/trunc.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/sse/sse4_1/trunc.hpp
@@ -20,7 +20,7 @@ namespace boost { namespace simd { namespace ext
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return _mm_round_pd(a0, 3);
+      return _mm_round_pd(a0,  _MM_FROUND_TO_ZERO|_MM_FROUND_NO_EXC);
     }
   };
 
@@ -32,7 +32,7 @@ namespace boost { namespace simd { namespace ext
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return _mm_round_ps(a0, 3);
+      return _mm_round_ps(a0,  _MM_FROUND_TO_ZERO|_MM_FROUND_NO_EXC);
     }
   };
 } } }
