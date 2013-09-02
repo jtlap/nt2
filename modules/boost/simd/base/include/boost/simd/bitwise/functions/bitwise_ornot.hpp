6d6f8243ce3a969b7257706a8a1b3e7c0f8b8543
jtlap <jtlapreste@gmail.com>
2013-09-02 13:34:46 +0200

    oxygenation of some arithmetic bitwise functions

--- modules/boost/simd/base/include/boost/simd/bitwise/functions/bitwise_ornot.hpp
+++ modules/boost/simd/base/include/boost/simd/bitwise/functions/bitwise_ornot.hpp
@@ -6,64 +6,54 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-/*!
- * \file
-**/
 #ifndef BOOST_SIMD_BITWISE_FUNCTIONS_BITWISE_ORNOT_HPP_INCLUDED
 #define BOOST_SIMD_BITWISE_FUNCTIONS_BITWISE_ORNOT_HPP_INCLUDED
 #include <boost/simd/include/functor.hpp>
 #include <boost/dispatch/include/functor.hpp>
 
-/*!
- * \ingroup boost_simd_bitwise
- * \defgroup boost_simd_bitwise_bitwise_ornot bitwise_ornot
- *
- * \par Description
- * return the bitwise or of the first parameter
- * with the bitwise complement of the second parameter.
- * \par
- * The operands must share the same bit size and
- * the result type is the one of the first operand
- *
- * \par Header file
- *
- * \code
- * #include <nt2/include/functions/bitwise_ornot.hpp>
- * \endcode
- *
- * \par Alias
- * \arg b_ornot
- *
- * \synopsis
- *
- * \code
- * namespace boost::simd
- * {
- *   template <class A0>
- *     meta::call<tag::bitwise_ornot_(A0,A0)>::type
- *     bitwise_ornot(const A0 & a0,const A0 & a1);
- * }
- * \endcode
- *
- * \param a0 the first parameter of bitwise_ornot
- * \param a1 the second parameter of bitwise_ornot
- *
- * \return a value of the common type of the parameters
- *
- * \par Notes
- * In SIMD mode, this function acts elementwise on the inputs vectors elements
- * \par
- *
-**/
-
 namespace boost { namespace simd { namespace tag
   {
     /*!
-     * \brief Define the tag bitwise_ornot_ of functor bitwise_ornot
-     *        in namespace boost::simd::tag for toolbox boost.simd.bitwise
+      @brief  bitwise_ornot generic tag
+
+      Represents the bitwise_ornot function in generic contexts.
+
+      @par Models:
+      Hierarchy
     **/
-    struct bitwise_ornot_ : ext::elementwise_<bitwise_ornot_> { typedef ext::elementwise_<bitwise_ornot_> parent; };
+    struct bitwise_ornot_ : ext::elementwise_<bitwise_ornot_>
+    {
+      /// @brief Parent hierarchy
+      typedef ext::elementwise_<bitwise_ornot_> parent;
+    };
   }
+  /*!
+    Computes the bitwise and not of its parameters.
+
+    @par semantic:
+    For any given value @c x, of type @c T1 @c y of type @c T2
+    of same memory size:
+
+    @code
+    T r = bitwise_ornot(x, y);
+    @endcode
+
+    The code is equivalent to:
+
+    @code
+    T r = x | ~y;
+    @endcode
+
+    @par Alias
+
+    b_ornot
+
+    @param  a0
+    @param  a1
+
+    @return      a value of the same type as the input.
+
+  **/
   BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::bitwise_ornot_, bitwise_ornot, 2)
   BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::bitwise_ornot_, b_ornot, 2)
 } }
--- modules/boost/simd/base/include/boost/simd/bitwise/functions/bitwise_select.hpp
+++ modules/boost/simd/base/include/boost/simd/bitwise/functions/bitwise_select.hpp
@@ -6,67 +6,59 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-/*!
- * \file
-**/
 #ifndef BOOST_SIMD_BITWISE_FUNCTIONS_BITWISE_SELECT_HPP_INCLUDED
 #define BOOST_SIMD_BITWISE_FUNCTIONS_BITWISE_SELECT_HPP_INCLUDED
 #include <boost/simd/include/functor.hpp>
 #include <boost/dispatch/include/functor.hpp>
-
-/*!
- * \ingroup boost_simd_bitwise
- * \defgroup boost_simd_bitwise_bitwise_select bitwise_select
- *
- * \par Description
- * Returns the bitwise selection of bits from the second
- * or third operand according to the setting of the bits
- * of the first operand
- * \par
- * The operands must share the same bit size and
- * the result type is the same.
- *
- * \par Header file
- *
- * \code
- * #include <nt2/include/functions/bitwise_select.hpp>
- * \endcode
- *
- *
- * \synopsis
- *
- * \code
- * namespace boost::simd
- * {
- *   template <class A0,class A1,class A2>
- *     meta::call<tag::bitwise_select_(A0,A1,A2)>::type
- *     bitwise_select(const A0 & a0,const A1 & a1,const A2 & a2);
- * }
- * \endcode
- *
- * \param a0 the first parameter of bitwise_select
- * \param a1 the second parameter of bitwise_select
- * \param a2 the third parameter of bitwise_select
- *
- * \return a value of the common type of the parameters
- *
- * \par Notes
- * In SIMD mode, this function acts elementwise on the inputs vectors elements
- * \par
- *
-**/
-
 namespace boost { namespace simd { namespace tag
   {
     /*!
-     * \brief Define the tag bitwise_select_ of functor bitwise_select
-     *        in namespace boost::simd::tag for toolbox boost.simd.bitwise
+      @brief  bitwise_select generic tag
+
+      Represents the bitwise_select function in generic contexts.
+
+      @par Models:
+      Hierarchy
     **/
-    struct bitwise_select_ : ext::elementwise_<bitwise_select_> { typedef ext::elementwise_<bitwise_select_> parent; };
+    struct bitwise_select_ : ext::elementwise_<bitwise_select_>
+    {
+      /// @brief Parent hierarchy
+      typedef ext::elementwise_<bitwise_select_> parent;
+    };
   }
+  /*!
+    Returns the bitwise selection of bits from the second
+    or third operand according to the setting of the bits
+    of the first operand
+
+    @par semantic:
+    For any given value @c x, of type @c T1 @c y of type @c T2
+    of same memory size:
+
+    @code
+    T r = bitwise_select(x, y, z);
+    @endcode
+
+    The code is equivalent to:
+
+    @code
+    T r = (x&y)|(z&~y);
+    @endcode
+
+    @par Alias
+
+    b_select
+
+    @param  a0 selector
+    @param  a1 bits selected for a0 bits that are on
+    @param  a2 bits selected for a0 bits that are off
+
+    @return      a value of the same type as the input.
+
+  **/
   BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::bitwise_select_, bitwise_select, 3)
+  BOOST_DISPATCH_FUNCTION_IMPLEMENTATION(tag::bitwise_select_, b_select, 3)
 } }
 
 #endif
 
-// modified by jt the 25/12/2010
