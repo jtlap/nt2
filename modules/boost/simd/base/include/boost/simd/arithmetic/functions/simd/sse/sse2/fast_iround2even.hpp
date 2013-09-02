d9695120685b40132c9700fb6b854d4380d48e07
jtlap <jtlapreste@gmail.com>
2013-09-02 14:41:50 +0200

    adding a sse2 version of fast_iround2even for double using _mm_cvtpd_epi32 with an assert checking range

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/sse/sse2/fast_iround2even.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/simd/sse/sse2/fast_iround2even.hpp
@@ -13,6 +13,10 @@
 #include <boost/simd/include/functions/simd/is_ltz.hpp>
 #include <boost/simd/include/functions/simd/genmask.hpp>
 #include <boost/simd/include/functions/simd/bitwise_or.hpp>
+#include <boost/simd/include/functions/simd/any.hpp>
+#include <boost/simd/include/functions/simd/is_not_less_equal.hpp>
+#include <boost/simd/include/functions/simd/splat.hpp>
+#include <boost/simd/include/constants/valmax.hpp>
 #include <boost/dispatch/meta/as_integer.hpp>
 #include <boost/dispatch/meta/downgrade.hpp>
 #include <boost/simd/swar/functions/details/shuffle.hpp>
@@ -28,6 +32,23 @@ namespace boost { namespace simd { namespace ext
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
       return  _mm_cvtps_epi32(a0);
+    }
+  };
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::fast_iround2even_, boost::simd::tag::sse2_
+                            , (A0)
+                            , ((simd_<double_<A0>,boost::simd::tag::sse_>))
+                            )
+  {
+    typedef typename dispatch::meta::as_integer<A0>::type result_type;
+    BOOST_SIMD_FUNCTOR_CALL(1)
+    {
+      BOOST_ASSERT_MSG(any(is_nle(abs(a0)), splat<Valmax<int32_t>()), "value out of range for fast_iround2even");
+      typedef typename dispatch::meta::downgrade<result_type>::type di_type;
+      di_type z0 =  _mm_cvtpd_epi32(a0);
+      di_type z1 = details::shuffle<0, 2, 1, 3>(z0);
+      di_type z2 = details::shuffle<1, 0, 3, 2>(genmask(is_ltz(z1)));
+      return bitwise_cast<result_type>( bitwise_or(z1, z2));
+//    return bitwise_cast<result_type>(  _mm_unpacklo_epi32(z0, genmask(is_ltz(z0))));
     }
   };
 } } }
