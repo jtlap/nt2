b0326896d261da6c93c7e593a66bd129d3be90d5
jtlap <jtlapreste@gmail.com>
2013-09-03 15:18:17 +0200

    making scalar rol and ror fit the doc and adding test also for rror and rrol

--- modules/boost/simd/base/include/boost/simd/bitwise/functions/scalar/rol.hpp
+++ modules/boost/simd/base/include/boost/simd/bitwise/functions/scalar/rol.hpp
@@ -10,6 +10,7 @@
 #define BOOST_SIMD_BITWISE_FUNCTIONS_SCALAR_ROL_HPP_INCLUDED
 
 #include <boost/simd/bitwise/functions/rol.hpp>
+#include <boost/simd/include/functions/scalar/abs.hpp>
 #include <boost/simd/include/functions/scalar/shift_left.hpp>
 #include <boost/simd/include/functions/scalar/shr.hpp>
 #include <boost/simd/include/functions/scalar/bitwise_cast.hpp>
@@ -18,8 +19,22 @@
 namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::rol_, tag::cpu_, (A0)(A1)
-                            , (scalar_< arithmetic_<A0> >)
-                              (scalar_< integer_<A1> >)
+                            , (scalar_< integer_<A0> >)
+                              (scalar_< int_<A1> >)
+                            )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL(2)
+    {
+      static const std::size_t width = sizeof(A0)*CHAR_BIT;
+      A1 aa1 = boost::simd::abs(a1);
+      return shli(a0, aa1) | shri(a0, (width-aa1) & (width-1));
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::rol_, tag::cpu_, (A0)(A1)
+                            , (scalar_< integer_<A0> >)
+                              (scalar_< uint_<A1> >)
                             )
   {
     typedef A0 result_type;
@@ -31,8 +46,9 @@ namespace boost { namespace simd { namespace ext
   };
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::rol_, tag::cpu_, (A0)(A1)
-                            , (scalar_< floating_<A0> >)(scalar_< integer_<A1> >)
-                            )
+                                   , (scalar_< floating_<A0> >)
+                                     (scalar_< integer_<A1> >)
+                                   )
   {
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(2)
