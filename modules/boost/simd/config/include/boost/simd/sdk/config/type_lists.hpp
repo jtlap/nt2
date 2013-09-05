07c9411b242c18652f9cf782a687e83aa6a5127c
Mathias Gaunard <mathias@gaunard.com>
2013-09-05 15:52:07 +0200

    reducing the number of type list macros

--- modules/boost/simd/config/include/boost/simd/sdk/config/type_lists.hpp
+++ modules/boost/simd/config/include/boost/simd/sdk/config/type_lists.hpp
@@ -6,103 +6,42 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-
-
 #ifndef BOOST_SIMD_SDK_CONFIG_TYPE_LISTS_HPP_INCLUDED
 #define BOOST_SIMD_SDK_CONFIG_TYPE_LISTS_HPP_INCLUDED
 
-#if !defined(BOOST_SIMD_TYPES)
-#define BOOST_SIMD_TYPES  (boost::simd::uint64_t)                              \
-    (boost::simd::int64_t)(double)                                             \
-    (boost::simd::uint32_t)(boost::simd::int32_t)(float)                       \
-    (boost::simd::uint16_t)(boost::simd::int16_t)                              \
-    (boost::simd::uint8_t) (boost::simd::int8_t)                               \
-/**/
-#endif
+#include <boost/simd/sdk/config/types.hpp>
 
-#if !defined(BOOST_SIMD_REAL_TYPES)
-#define BOOST_SIMD_REAL_TYPES                                                  \
-  (double)                                                                     \
-  (float)                                                                      \
-/**/
-#define BOOST_SIMD_REAL BOOST_SIMD_REAL_TYPES
-#endif
+#define BOOST_SIMD_REAL_TYPES (double)(float)
 
-#if !defined(BOOST_SIMD_REAL_CONVERTIBLE_TYPES)
-#define BOOST_SIMD_REAL_CONVERTIBLE_TYPES                                      \
-  (boost::simd::uint64_t)(boost::simd::int64_t)(double)                        \
-    (boost::simd::uint32_t)(boost::simd::int32_t)(float)                       \
+#define BOOST_SIMD_INTEGRAL_SIGNED_TYPES (boost::simd::int64_t)(boost::simd::int32_t)              \
+                                         (boost::simd::int16_t)(boost::simd::int8_t)               \
 /**/
-#endif
 
-#if !defined(BOOST_SIMD_INTEGRAL_SIGNED_TYPES)
-#define BOOST_SIMD_INTEGRAL_SIGNED_TYPES  (boost::simd::int64_t)               \
-    (boost::simd::int32_t)                                                     \
-    (boost::simd::int16_t)                                                     \
-    (boost::simd::int8_t)                                                      \
+#define BOOST_SIMD_INTEGRAL_UNSIGNED_TYPES (boost::simd::uint64_t)(boost::simd::uint32_t)          \
+                                           (boost::simd::uint16_t)(boost::simd::uint8_t)           \
 /**/
-#endif
-
-#if !defined(BOOST_SIMD_UNSIGNED_TYPES)
-#define BOOST_SIMD_UNSIGNED_TYPES  (boost::simd::uint64_t)                     \
-    (boost::simd::uint32_t)                                                    \
-    (boost::simd::uint16_t)                                                    \
-    (boost::simd::uint8_t)                                                     \
-/**/
-#endif
-
-#if !defined(BOOST_SIMD_INTEGRAL_TYPES)
-#define BOOST_SIMD_INTEGRAL_TYPES  (boost::simd::uint64_t)                     \
-    (boost::simd::int64_t)(boost::simd::uint32_t)                              \
-    (boost::simd::int32_t)(boost::simd::uint16_t)(boost::simd::int16_t)        \
-    (boost::simd::int8_t)(boost::simd::uint8_t)                                \
-/**/
-#endif
-
-#if !defined(BOOST_SIMD_SIGNED_TYPES)
-#define BOOST_SIMD_SIGNED_TYPES (boost::simd::int64_t)(double)                 \
-    (boost::simd::int32_t)(float)                                              \
-    (boost::simd::int16_t)                                                     \
-    (boost::simd::int8_t)                                                      \
-/**/
-#endif
 
 #define BOOST_SIMD_INT_CONVERT_TYPES  (boost::simd::int32_t)(boost::simd::int64_t)
+#define BOOST_SIMD_UINT_CONVERT_TYPES (boost::simd::uint32_t)(boost::simd::uint64_t)
 
-#define BOOST_SIMD_UINT_CONVERT_TYPES  (boost::simd::uint32_t)(boost::simd::uint64_t)
+#define BOOST_SIMD_REAL_GROUPABLE_TYPES (double)
+#define BOOST_SIMD_REAL_SPLITABLE_TYPES (float)
 
-#define BOOST_SIMD_SIGNED_INT_GT_8  (boost::simd::int16_t)(boost::simd::int32_t)(boost::simd::int64_t)
+#define BOOST_SIMD_GROUPABLE_TYPES (boost::simd::int16_t)(boost::simd::uint16_t)                   \
+                                   (boost::simd::int32_t)(boost::simd::uint32_t)                   \
+                                   (boost::simd::int64_t)(boost::simd::uint64_t)                   \
+                                   (double)                                                        \
+/**/
+#define BOOST_SIMD_SPLITABLE_TYPES (boost::simd::int8_t)(boost::simd::uint8_t)                     \
+                                   (boost::simd::int16_t)(boost::simd::uint16_t)                   \
+                                   (boost::simd::int32_t)(boost::simd::uint32_t)                   \
+                                   (float)                                                         \
+/**/
 
-#define BOOST_SIMD_UNSIGNED_INT_GT_8  (boost::simd::int16_t)(boost::simd::int32_t)(boost::simd::int64_t)
-
-#define BOOST_SIMD_INT_64_TYPES (boost::simd::int64_t)(boost::simd::uint64_t)
-
-#define BOOST_SIMD_INT_32_TYPES  (boost::simd::int32_t)(boost::simd::uint32_t)
-
-#define BOOST_SIMD_INT_16_TYPES  (boost::simd::int16_t)(boost::simd::uint16_t)
-
-#define BOOST_SIMD_INT_8_TYPES  (boost::simd::int8_t)(boost::simd::uint8_t)
-
-#define BOOST_SIMD_GROUPABLE_TYPES  (boost::simd::int16_t)(boost::simd::uint16_t)(boost::simd::int32_t)(boost::simd::uint32_t)(boost::simd::int64_t)(boost::simd::uint64_t)(double)
-
-#define BOOST_SIMD_REAL_GROUPABLE_TYPES  (double)
-
-#define BOOST_SIMD_REAL_SPLITABLE_TYPES  (float)
-
-#define BOOST_SIMD_SPLITABLE_TYPES  (boost::simd::int8_t)(boost::simd::uint8_t)(boost::simd::int16_t)(boost::simd::uint16_t)(boost::simd::int32_t)(boost::simd::uint32_t)(float)
-
-#define BOOST_SIMD_INT_GT8_TYPES  (boost::simd::int16_t)(boost::simd::uint16_t)(boost::simd::int32_t)(boost::simd::uint32_t)(boost::simd::int64_t)(boost::simd::uint64_t)
-
-#define BOOST_SIMD_SIGNED_INT_GT_8_TYPES  (boost::simd::int16_t)(boost::simd::int32_t)(boost::simd::int64_t)
-
-#define BOOST_SIMD_UNSIGNED_INT_GT_8_TYPES  (boost::simd::uint16_t)(boost::simd::uint32_t)(boost::simd::uint64_t)
-
-#define BOOST_SIMD_LT64_TYPES  (boost::simd::int16_t)(boost::simd::uint16_t)(boost::simd::int32_t)(boost::simd::uint32_t)(boost::simd::int8_t)(boost::simd::uint8_t)(float)
-
-#define BOOST_SIMD_GT16_TYPES  (boost::simd::int32_t)(boost::simd::uint32_t)(boost::simd::int64_t)(boost::simd::uint64_t)(float)(double)
-
-#define BOOST_SIMD_SIGNED_INT_GT_16_TYPES  (boost::simd::int32_t)(boost::simd::int64_t)
-
-#define BOOST_SIMD_UNSIGNED_INT_GT_16_TYPES  (boost::simd::uint32_t)(boost::simd::uint64_t)
+#define BOOST_SIMD_INTEGRAL_TYPES BOOST_SIMD_INTEGRAL_UNSIGNED_TYPES BOOST_SIMD_INTEGRAL_SIGNED_TYPES
+#define BOOST_SIMD_UNSIGNED_TYPES BOOST_SIMD_INTEGRAL_UNSIGNED_TYPES
+#define BOOST_SIMD_SIGNED_TYPES BOOST_SIMD_REAL_TYPES BOOST_SIMD_INTEGRAL_SIGNED_TYPES
+#define BOOST_SIMD_REAL_CONVERTIBLE_TYPES  BOOST_SIMD_INT_CONVERT_TYPES BOOST_SIMD_UINT_CONVERT_TYPES BOOST_SIMD_REAL_TYPES
+#define BOOST_SIMD_TYPES BOOST_SIMD_REAL_TYPES BOOST_SIMD_INTEGRAL_TYPES
 
 #endif
--- modules/boost/simd/sdk/include/boost/simd/sdk/simd/extensions/powerpc/types.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/simd/extensions/powerpc/types.hpp
@@ -11,22 +11,7 @@
 
 #include <boost/simd/sdk/config/type_lists.hpp>
 
-#define BOOST_SIMD_SIMD_TYPES  (float)(boost::simd::uint32_t)(boost::simd::int32_t)  \
-                        (boost::simd::uint16_t)(boost::simd::int16_t)         \
-                        (boost::simd::uint8_t)(boost::simd::int8_t)           \
-/**/
-
 #define BOOST_SIMD_SIMD_REAL_TYPES (float)
-/**/
-
-#define BOOST_SIMD_SIMD_UNSIGNED_TYPES (boost::simd::uint32_t)(boost::simd::uint16_t)(boost::simd::uint8_t)  \
-/**/
-
-#define BOOST_SIMD_SIMD_INTEGRAL_TYPES (boost::simd::uint32_t)(boost::simd::uint16_t)(boost::simd::uint8_t)  \
-                                (boost::simd::int32_t)(boost::simd::int16_t)(boost::simd::int8_t)     \
-/**/
-
-#define BOOST_SIMD_SIMD_SIGNED_TYPES (boost::simd::int32_t)(float)(boost::simd::int16_t)(boost::simd::int8_t)  \
 /**/
 
 #define BOOST_SIMD_SIMD_INTEGRAL_UNSIGNED_TYPES  (boost::simd::uint32_t)(boost::simd::uint16_t)(boost::simd::uint8_t)  \
@@ -35,44 +20,10 @@
 #define BOOST_SIMD_SIMD_INTEGRAL_SIGNED_TYPES  (boost::simd::int32_t)(boost::simd::int16_t)(boost::simd::int8_t) \
 /**/
 
-#define BOOST_SIMD_SIMD_REAL_CONVERTIBLE_TYPES  (boost::simd::int32_t)(boost::simd::uint32_t)(float) \
-/**/
-
 #define BOOST_SIMD_SIMD_INT_CONVERT_TYPES  (boost::simd::int32_t) \
 /**/
 
 #define BOOST_SIMD_SIMD_UINT_CONVERT_TYPES  (boost::simd::uint32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_SIGNED_INT_GT_8  (boost::simd::int16_t)(boost::simd::int32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_8  (boost::simd::int16_t)(boost::simd::int32_t) \
-/**/
-
-
-#define BOOST_SIMD_SIMD__INT_CONVERT_TYPES  (boost::simd::int32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD__UINT_CONVERT_TYPES  (boost::simd::uint32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_SIGNED_INT_GT_8  (boost::simd::int16_t)(boost::simd::int32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_8  (boost::simd::int16_t)(boost::simd::int32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_INT_64_TYPES  \
-/**/
-
-#define BOOST_SIMD_SIMD_INT_32_TYPES  (boost::simd::int32_t)(boost::simd::uint32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_INT_16_TYPES  (boost::simd::int16_t)(boost::simd::uint16_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_INT_8_TYPES  (boost::simd::int8_t)(boost::simd::uint8_t) \
 /**/
 
 #define BOOST_SIMD_SIMD_GROUPABLE_TYPES  (boost::simd::int16_t)(boost::simd::uint16_t)(boost::simd::int32_t)(boost::simd::uint32_t)\
@@ -81,31 +32,7 @@
 #define BOOST_SIMD_SIMD_SPLITABLE_TYPES  (boost::simd::int8_t)(boost::simd::uint8_t)(boost::simd::int16_t)(boost::simd::uint16_t) \
 /**/
 
-#define BOOST_SIMD_SIMD_REAL_GROUPABLE_TYPES \
-/**/
-
-#define BOOST_SIMD_SIMD_REAL_SPLITABLE_TYPES \
-/**/
-
-#define BOOST_SIMD_SIMD_INT_GT8_TYPES  (boost::simd::int16_t)(boost::simd::uint16_t)(boost::simd::int32_t)(boost::simd::uint32_t)\
-/**/
-
-#define BOOST_SIMD_SIMD_SIGNED_INT_GT_8_TYPES  (boost::simd::int16_t)(boost::simd::int32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_8_TYPES  (boost::simd::uint16_t)(boost::simd::uint32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_LT64_TYPES  (boost::simd::int16_t)(boost::simd::uint16_t)(boost::simd::int32_t)(boost::simd::uint32_t)(boost::simd::int8_t)(boost::simd::uint8_t)(float) \
-/**/
-
-#define BOOST_SIMD_SIMD_GT16_TYPES  (boost::simd::int32_t)(boost::simd::uint32_t)(float) \
-/**/
-
-#define BOOST_SIMD_SIMD_SIGNED_INT_GT_16_TYPES  (boost::simd::int32_t) \
-/**/
-
-#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_16_TYPES  (boost::simd::uint32_t) \
-/**/
+#define BOOST_SIMD_SIMD_REAL_GROUPABLE_TYPES
+#define BOOST_SIMD_SIMD_REAL_SPLITABLE_TYPES
 
 #endif
