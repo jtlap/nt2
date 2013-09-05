91ffc69f64c1a506995f76e69fa37f30e38b3930
Mathias Gaunard <mathias@gaunard.com>
2013-09-05 22:31:11 +0200

    assert that type in native is not a reference or const qualified

--- modules/boost/simd/sdk/include/boost/simd/sdk/simd/native.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/simd/native.hpp
@@ -22,6 +22,8 @@
 #include <boost/fusion/adapted/boost_array.hpp>
 #include <boost/mpl/assert.hpp>
 #include <boost/type_traits/is_same.hpp>
+#include <boost/type_traits/is_const.hpp>
+#include <boost/type_traits/is_reference.hpp>
 
 namespace boost { namespace simd
 {
@@ -41,6 +43,12 @@ namespace boost { namespace simd
       (!is_same<native_type, dispatch::meta::na_>::value),
       BOOST_SIMD_NATIVE_TYPE_UNSUPPORTED_ON_EXTENSION,
       (Scalar, Extension)
+    );
+
+    BOOST_MPL_ASSERT_MSG(
+      (!is_const<Scalar>::value && !is_reference<Scalar>::value),
+      BOOST_SIMD_NATIVE_TYPE_MUST_NOT_BE_REF_OR_QUALIFIED,
+      (Scalar)
     );
 
     ////////////////////////////////////////////////////////////////////////////
