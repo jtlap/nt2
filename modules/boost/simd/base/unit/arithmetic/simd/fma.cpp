69354e2cb75c8eeb547b5815f96610d0db90dd7d
Joel Falcou <joel.falcou@lri.fr>
2013-09-11 10:13:25 +0200

    Fix include in simd fma test

--- modules/boost/simd/base/unit/arithmetic/simd/fma.cpp
+++ modules/boost/simd/base/unit/arithmetic/simd/fma.cpp
@@ -13,6 +13,8 @@
 #include <nt2/sdk/unit/module.hpp>
 #include <boost/simd/include/constants/zero.hpp>
 #include <boost/simd/include/constants/one.hpp>
+#include <boost/simd/include/constants/two.hpp>
+#include <boost/simd/include/constants/three.hpp>
 #include <boost/simd/include/constants/mone.hpp>
 #include <boost/simd/include/constants/inf.hpp>
 #include <boost/simd/include/constants/minf.hpp>
