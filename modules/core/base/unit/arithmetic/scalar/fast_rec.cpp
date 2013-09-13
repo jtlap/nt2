fa999da9b1999c4c7d9fad595b471b83b2361e9b
Mathias Gaunard <mathias@gaunard.com>
2013-09-13 18:36:24 +0200

    missing r_t typedef in fast_rec scalar unit test

--- modules/core/base/unit/arithmetic/scalar/fast_rec.cpp
+++ modules/core/base/unit/arithmetic/scalar/fast_rec.cpp
@@ -20,9 +20,8 @@ NT2_TEST_CASE_TPL ( fast_rec,  BOOST_SIMD_SIMD_REAL_TYPES)
   using nt2::fast_rec;
   using nt2::tag::fast_rec_;
 
-  NT2_TEST_TYPE_IS( typename boost::dispatch::meta::call<fast_rec_(T)>::type
-                  , T
-                  );
+  typedef typename boost::dispatch::meta::call<fast_rec_(T)>::type r_t;
+  NT2_TEST_TYPE_IS( r_t, T );
 
   NT2_TEST_ULP_EQUAL(fast_rec(boost::simd::Mone<T>()), boost::simd::Mone<r_t>(), 0.5);
   NT2_TEST_ULP_EQUAL(fast_rec(boost::simd::Mzero<T>()), boost::simd::Minf<r_t>(), 0);
