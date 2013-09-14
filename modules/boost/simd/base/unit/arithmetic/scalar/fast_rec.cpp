07623f374e2253c8865b1d6a068e17810b211ff3
jtlap <jtlapreste@gmail.com>
2013-09-14 10:26:12 +0200

    using constants in fast_rec test

--- modules/boost/simd/base/unit/arithmetic/scalar/fast_rec.cpp
+++ modules/boost/simd/base/unit/arithmetic/scalar/fast_rec.cpp
@@ -15,27 +15,25 @@
 #include <nt2/sdk/meta/as_floating.hpp>
 #include <boost/simd/include/constants/zero.hpp>
 #include <boost/simd/include/constants/two.hpp>
+#include <boost/simd/include/constants/half.hpp>
 #include <boost/simd/include/constants/one.hpp>
 #include <boost/simd/include/constants/mone.hpp>
+#include <boost/simd/include/constants/ten.hpp>
+#include <boost/simd/include/constants/three.hpp>
+#include <boost/simd/include/constants/third.hpp>
+#include <boost/simd/include/constants/oneo_10.hpp>
 
 NT2_TEST_CASE_TPL ( fast_rec_real,  BOOST_SIMD_REAL_TYPES)
 {
   using boost::simd::fast_rec;
   using boost::simd::tag::fast_rec_;
+  typedef typename boost::dispatch::meta::call<fast_rec_(T)>::type r_t;
 
-  NT2_TEST_TYPE_IS( typename boost::dispatch::meta::call<fast_rec_(T)>::type
-                  , T
-                  );
+  NT2_TEST_TYPE_IS(T,r_t);
 
-  // 1/+-1 = +-1
-  NT2_TEST_ULP_EQUAL(fast_rec(boost::simd::Mone<T>()), boost::simd::Mone<T>(), 0.5);
-  NT2_TEST_ULP_EQUAL(fast_rec(boost::simd::One<T>()), boost::simd::One<T>(), 0.5);
-
-  NT2_TEST_ULP_EQUAL(fast_rec( T(2) ), T(0.5)  , 0.5);
-  NT2_TEST_ULP_EQUAL(fast_rec( T(10)), T(0.1)  , 0.5);
-  NT2_TEST_ULP_EQUAL(fast_rec( T(3) ), T(1./3.), 0.5);
-
-  // 1/(1/x) = x
-  NT2_TEST_ULP_EQUAL(fast_rec(fast_rec(T(10))), T(10), 0.5);
-
+  NT2_TEST_ULP_EQUAL(fast_rec(boost::simd::Two<T>()), boost::simd::Half<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(fast_rec(boost::simd::Mone<T>()), boost::simd::Mone<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(fast_rec(boost::simd::One<T>()), boost::simd::One<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(fast_rec(boost::simd::Ten<T>()), boost::simd::Oneo_10<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(fast_rec(boost::simd::Three<T>()), boost::simd::Third<r_t>(), 0.5);
 }
