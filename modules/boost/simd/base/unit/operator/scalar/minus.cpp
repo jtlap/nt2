d354ed7ad1c2c402d4f3399050ca5fd1239f2fef
jtlap <jtlapreste@gmail.com>
2013-09-25 16:48:52 +0200

    operator scalar unit tests remaining

--- modules/boost/simd/base/unit/operator/scalar/minus.cpp
+++ modules/boost/simd/base/unit/operator/scalar/minus.cpp
@@ -6,37 +6,32 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 boost.simd.operator toolbox - minus/scalar Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// unit test behavior of boost.simd.operator components in scalar mode
-//////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 18/02/2011
-///
 #include <boost/simd/operator/include/functions/minus.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <boost/type_traits/is_same.hpp>
 #include <boost/dispatch/functor/meta/call.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
 #include <nt2/sdk/unit/module.hpp>
-#include <boost/simd/constant/constant.hpp>
+#include <boost/simd/sdk/config.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
 
+#include <boost/simd/include/constants/one.hpp>
+#include <boost/simd/include/constants/mone.hpp>
+#include <boost/simd/include/constants/zero.hpp>
+#include <boost/simd/include/constants/three.hpp>
+#include <boost/simd/include/constants/inf.hpp>
+#include <boost/simd/include/constants/minf.hpp>
+#include <boost/simd/include/constants/nan.hpp>
+#include <boost/simd/include/constants/valmax.hpp>
 
-NT2_TEST_CASE_TPL ( minus_real__2_0,  BOOST_SIMD_REAL_TYPES)
+NT2_TEST_CASE_TPL ( minus_real,  BOOST_SIMD_REAL_TYPES)
 {
-
   using boost::simd::minus;
   using boost::simd::tag::minus_;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
   typedef typename boost::dispatch::meta::call<minus_(T,T)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
-  typedef r_t wished_r_t;
-
+  typedef T wished_r_t;
 
   // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
+  NT2_TEST_TYPE_IS(r_t, wished_r_t);
 
   // specific values tests
   NT2_TEST_EQUAL(minus(boost::simd::Inf<T>(), boost::simd::Inf<T>()), boost::simd::Nan<r_t>());
@@ -47,21 +42,16 @@ NT2_TEST_CASE_TPL ( minus_real__2_0,  BOOST_SIMD_REAL_TYPES)
   NT2_TEST_EQUAL(minus(boost::simd::Zero<T>(),boost::simd::One<T>()), boost::simd::Mone<r_t>());
 } // end of test for floating_
 
-NT2_TEST_CASE_TPL ( minus_integer__2_0,  BOOST_SIMD_INTEGRAL_TYPES)
+NT2_TEST_CASE_TPL ( minus_integer,  BOOST_SIMD_INTEGRAL_TYPES)
 {
 
   using boost::simd::minus;
   using boost::simd::tag::minus_;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
   typedef typename boost::dispatch::meta::call<minus_(T,T)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
-  typedef r_t wished_r_t;
-
+  typedef T wished_r_t;
 
   // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
+  NT2_TEST_TYPE_IS(r_t, wished_r_t);
 
   // specific values tests
   NT2_TEST_EQUAL(minus(boost::simd::One<T>(), boost::simd::One<T>()), boost::simd::Zero<r_t>());
--- modules/boost/simd/base/unit/operator/scalar/unary_plus.cpp
+++ modules/boost/simd/base/unit/operator/scalar/unary_plus.cpp
@@ -6,37 +6,35 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 boost.simd.operator toolbox - unary_plus/scalar Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// unit test behavior of boost.simd.operator components in scalar mode
-//////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 18/02/2011
-///
 #include <boost/simd/operator/include/functions/unary_plus.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <boost/type_traits/is_same.hpp>
+
 #include <boost/dispatch/functor/meta/call.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
 #include <nt2/sdk/unit/module.hpp>
-#include <boost/simd/constant/constant.hpp>
+#include <boost/simd/sdk/config.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
 
+#include <boost/simd/include/constants/one.hpp>
+#include <boost/simd/include/constants/mone.hpp>
+#include <boost/simd/include/constants/mzero.hpp>
+#include <boost/simd/include/constants/zero.hpp>
+#include <boost/simd/include/constants/two.hpp>
+#include <boost/simd/include/constants/inf.hpp>
+#include <boost/simd/include/constants/minf.hpp>
+#include <boost/simd/include/constants/nan.hpp>
+#include <boost/simd/include/constants/valmax.hpp>
 
-NT2_TEST_CASE_TPL ( unary_plus_real__1_0,  BOOST_SIMD_REAL_TYPES)
+NT2_TEST_CASE_TPL ( unary_plus_real,  BOOST_SIMD_REAL_TYPES)
 {
 
   using boost::simd::unary_plus;
   using boost::simd::tag::unary_plus_;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
   typedef typename boost::dispatch::meta::call<unary_plus_(T)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
-  typedef r_t wished_r_t;
-
+  typedef T wished_r_t;
 
   // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
+  NT2_TEST_TYPE_IS(r_t, wished_r_t);
 
   // specific values tests
   NT2_TEST_EQUAL(unary_plus(boost::simd::Inf<T>()), boost::simd::Inf<r_t>());
@@ -46,21 +44,18 @@ NT2_TEST_CASE_TPL ( unary_plus_real__1_0,  BOOST_SIMD_REAL_TYPES)
   NT2_TEST_EQUAL(unary_plus(boost::simd::Zero<T>()), boost::simd::Zero<r_t>());
 } // end of test for floating_
 
-NT2_TEST_CASE_TPL ( unary_plus_integer__1_0,  BOOST_SIMD_INTEGRAL_TYPES)
+NT2_TEST_CASE_TPL ( unary_plus_integer,  BOOST_SIMD_INTEGRAL_TYPES)
 {
 
   using boost::simd::unary_plus;
   using boost::simd::tag::unary_plus_;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
   typedef typename boost::dispatch::meta::call<unary_plus_(T)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
-  typedef r_t wished_r_t;
+  typedef T wished_r_t;
 
 
   // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
+  NT2_TEST_TYPE_IS(r_t, wished_r_t);
+
   // specific values tests
   NT2_TEST_EQUAL(unary_plus(boost::simd::One<T>()), boost::simd::One<T>());
   NT2_TEST_EQUAL(unary_plus(boost::simd::Two<T>()), boost::simd::Two<T>());
