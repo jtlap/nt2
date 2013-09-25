2f33421997207157c1f7294d203177b8979b8f4c
jtlap <jtlapreste@gmail.com>
2013-09-25 15:36:50 +0200

    operator scalar unit tests

--- modules/boost/simd/base/unit/operator/scalar/complement.cpp
+++ modules/boost/simd/base/unit/operator/scalar/complement.cpp
@@ -6,60 +6,52 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 boost.simd.operator toolbox - complement/scalar Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// unit test behavior of boost.simd.operator components in scalar mode
-//////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 18/02/2011
-///
 #include <boost/simd/operator/include/functions/complement.hpp>
 #include <boost/simd/sdk/simd/native.hpp>
 #include <boost/simd/include/functions/shift_left.hpp>
 
-#include <boost/type_traits/is_same.hpp>
 #include <boost/dispatch/functor/meta/call.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
 #include <nt2/sdk/unit/module.hpp>
-#include <boost/simd/constant/constant.hpp>
+#include <boost/simd/sdk/config.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
 
+#include <boost/simd/include/constants/one.hpp>
+#include <boost/simd/include/constants/mone.hpp>
+#include <boost/simd/include/constants/zero.hpp>
+#include <boost/simd/include/constants/three.hpp>
+#include <boost/simd/include/constants/inf.hpp>
+#include <boost/simd/include/constants/minf.hpp>
+#include <boost/simd/include/constants/nan.hpp>
+#include <boost/simd/include/constants/valmax.hpp>
 
-NT2_TEST_CASE_TPL ( complement_real__1_0,  BOOST_SIMD_REAL_TYPES)
+NT2_TEST_CASE_TPL ( complement_real,  BOOST_SIMD_REAL_TYPES)
 {
-
   using boost::simd::complement;
   using boost::simd::tag::complement_;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
   typedef typename boost::dispatch::meta::call<complement_(T)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
   typedef T wished_r_t;
 
-
   // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
+  NT2_TEST_TYPE_IS(r_t, T);
 
   // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
   NT2_TEST_EQUAL(complement(boost::simd::Nan<T>()), boost::simd::Zero<r_t>());
+#endif
   NT2_TEST_EQUAL(complement(boost::simd::Zero<T>()), boost::simd::Nan<r_t>());
 } // end of test for floating_
 
-NT2_TEST_CASE_TPL ( complement_integer__1_0,  BOOST_SIMD_INTEGRAL_TYPES)
+NT2_TEST_CASE_TPL ( complement_integer,  BOOST_SIMD_INTEGRAL_TYPES)
 {
-
   using boost::simd::complement;
   using boost::simd::tag::complement_;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
   typedef typename boost::dispatch::meta::call<complement_(T)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
   typedef T wished_r_t;
 
-
   // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
+  NT2_TEST_TYPE_IS(r_t, T);
 
   // specific values tests
   NT2_TEST_EQUAL(complement(boost::simd::One<T>()), boost::simd::shli(boost::simd::Mone<r_t>(),1));
--- modules/boost/simd/base/unit/operator/scalar/if_else.cpp
+++ modules/boost/simd/base/unit/operator/scalar/if_else.cpp
@@ -6,64 +6,58 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 boost.simd.operator toolbox - if_else/scalar Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// unit test behavior of boost.simd.operator components in scalar mode
-//////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 18/02/2011
-///
 #include <boost/simd/operator/include/functions/if_else.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <boost/type_traits/is_same.hpp>
+
 #include <boost/dispatch/functor/meta/call.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
 #include <nt2/sdk/unit/module.hpp>
-#include <boost/simd/constant/constant.hpp>
+#include <boost/simd/sdk/config.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
 
+#include <boost/simd/include/constants/one.hpp>
+#include <boost/simd/include/constants/two.hpp>
+#include <boost/simd/include/constants/mone.hpp>
+#include <boost/simd/include/constants/zero.hpp>
+#include <boost/simd/include/constants/three.hpp>
+#include <boost/simd/include/constants/inf.hpp>
+#include <boost/simd/include/constants/minf.hpp>
+#include <boost/simd/include/constants/nan.hpp>
+#include <boost/simd/include/constants/valmax.hpp>
 
-NT2_TEST_CASE_TPL ( if_else_integer__3_0,  BOOST_SIMD_INTEGRAL_TYPES)
+NT2_TEST_CASE_TPL ( if_else_integer,  BOOST_SIMD_INTEGRAL_TYPES)
 {
-
   using boost::simd::if_else;
   using boost::simd::tag::if_else_;
   using boost::simd::logical;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
   typedef typename boost::dispatch::meta::call<if_else_(logical<T>,T,T)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
   typedef T wished_r_t;
 
-
   // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
+  NT2_TEST_TYPE_IS(r_t, T);
 
   // specific values tests
-  NT2_TEST_EQUAL(if_else( boost::simd::True< boost::simd::logical<T> >(), boost::simd::One<T>(), boost::simd::One<T>()), boost::simd::One<r_t>());
-  NT2_TEST_EQUAL(if_else( boost::simd::True< boost::simd::logical<T> >(), boost::simd::Zero<T>(),boost::simd::Two<T>()), boost::simd::Zero<r_t>());
-  NT2_TEST_EQUAL(if_else( boost::simd::False< boost::simd::logical<T> >(), boost::simd::One<T>(), boost::simd::Zero<T>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(if_else( boost::simd::True< logical<T> >(), boost::simd::One<T>(), boost::simd::One<T>()), boost::simd::One<r_t>());
+  NT2_TEST_EQUAL(if_else( boost::simd::True< logical<T> >(), boost::simd::Zero<T>(),boost::simd::Two<T>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(if_else( boost::simd::False< logical<T> >(), boost::simd::One<T>(), boost::simd::Zero<T>()), boost::simd::Zero<r_t>());
 } // end of test for integer_
 
-NT2_TEST_CASE_TPL ( if_else_real__3_0,  BOOST_SIMD_REAL_TYPES)
+NT2_TEST_CASE_TPL ( if_else_real,  BOOST_SIMD_REAL_TYPES)
 {
 
   using boost::simd::if_else;
   using boost::simd::tag::if_else_;
   using boost::simd::logical;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
   typedef typename boost::dispatch::meta::call<if_else_(logical<T>,T,T)>::type r_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
   typedef T wished_r_t;
 
-
   // return type conformity test
-  NT2_TEST( (boost::is_same < r_t, wished_r_t >::value) );
-  std::cout << std::endl;
+  NT2_TEST_TYPE_IS(r_t, T);
 
   // specific values tests
-  NT2_TEST_EQUAL(if_else( boost::simd::True< boost::simd::logical<T> >(), boost::simd::Nan<T>(), boost::simd::Nan<T>()), boost::simd::Nan<r_t>());
-  NT2_TEST_EQUAL(if_else( boost::simd::True< boost::simd::logical<T> >(),boost::simd::Zero<T>(),boost::simd::Two<T>()), boost::simd::Zero<r_t>());
-  NT2_TEST_EQUAL(if_else( boost::simd::False< boost::simd::logical<T> >(), boost::simd::Zero<T>(), boost::simd::Zero<T>()), boost::simd::Zero<r_t>());
+#ifndef BOOST_SIMD_NO_INVALIDS
+  NT2_TEST_EQUAL(if_else( boost::simd::True< logical<T> >(), boost::simd::Nan<T>(), boost::simd::Nan<T>()), boost::simd::Nan<r_t>());
+#endif
+  NT2_TEST_EQUAL(if_else( boost::simd::True< logical<T> >(),boost::simd::Zero<T>(),boost::simd::Two<T>()), boost::simd::Zero<r_t>());
+  NT2_TEST_EQUAL(if_else( boost::simd::False< logical<T> >(), boost::simd::Zero<T>(), boost::simd::Zero<T>()), boost::simd::Zero<r_t>());
 } // end of test for floating_
