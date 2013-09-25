4d3f2c6b01d3b7992df3cc94328818a685ffcecf
Joel Falcou <joel.falcou@lri.fr>
2013-09-25 13:17:46 +0200

    Fix split test type range

--- modules/boost/simd/swar/unit/simd/split.cpp
+++ modules/boost/simd/swar/unit/simd/split.cpp
@@ -20,7 +20,7 @@
 #include <nt2/sdk/unit/tests/relation.hpp>
 #include <nt2/sdk/unit/tests/type_expr.hpp>
 
-NT2_TEST_CASE_TPL( split, BOOST_SIMD_SPLITABLE_TYPES )
+NT2_TEST_CASE_TPL( split, BOOST_SIMD_SIMD_SPLITABLE_TYPES )
 {
   using boost::simd::split;
   using boost::simd::tag::split_;
