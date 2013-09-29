a368c92d068cb758bdf4dc6a893b7812052bdf62
Mathias Gaunard <mathias@gaunard.com>
2013-09-29 16:09:15 +0200

    fixing a couple of bad macro types in cover tests

--- modules/core/polynomials/cover/simd/polevl.cpp
+++ modules/core/polynomials/cover/simd/polevl.cpp
@@ -28,7 +28,7 @@
 #include <nt2/include/functions/aligned_load.hpp>
 //COMMENTED
 
-NT2_TEST_CASE_TPL ( polevl_real__2_0,  NT2_REAL_TYPES)
+NT2_TEST_CASE_TPL ( polevl_real__2_0,  NT2_SIMD_REAL_TYPES)
 {
 //   using nt2::polevl;
 //   using nt2::tag::polevl_;
