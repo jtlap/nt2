d5aab8d66c7f8c689263e6dbf36976685fdbd1c0
Antoine TRAN TAN <antoine.tran-tan@live.fr>
2013-10-07 15:44:41 +0200

    Modify openmp transform to use openmp worker

--- modules/arch/openmp/include/nt2/core/functions/openmp/outer_fold.hpp
+++ modules/arch/openmp/include/nt2/core/functions/openmp/outer_fold.hpp
@@ -49,24 +49,21 @@ namespace nt2 { namespace ext
                                 )
   {
     typedef void                                                              result_type;
-    typedef typename A0::value_type                                           value_type;
     typedef typename boost::remove_reference<A1>::type::extent_type           extent_type;
-    typedef boost::simd::native<value_type,BOOST_SIMD_DEFAULT_EXTENSION>      target_type;
 
     BOOST_FORCEINLINE result_type
     operator()(A0& out, A1& in, A2 const& neutral, A3 const& bop, A4 const& ) const
     {
       extent_type ext = in.extent();
-      static const std::size_t N = boost::simd::meta::cardinal_of<target_type>::value;
-      std::size_t ibound  = boost::fusion::at_c<0>(ext);
-      std::size_t mbound =  boost::fusion::at_c<1>(ext);
       std::ptrdiff_t obound = boost::fusion::at_c<2>(ext);
-      std::size_t id;
 
-      std::size_t cache_line_size = nt2::config::top_cache_line_size(2); // in byte
-      std::size_t nb_vec = cache_line_size/(sizeof(value_type)*N);
-      std::size_t cache_bound = (nb_vec)*N;
-      std::size_t bound = boost::simd::align_under(ibound, cache_bound);
+
+      std::size_t nblocs   = omp_get_num_threads();
+      std::size_t grain    = obound / nblocs;
+      std::size_t leftover = obound % nblocs;
+
+      nt2::worker<tag::outer_fold_,tag::openmp_<Site>,A0,A1,A2,A3>
+      vecworker( out, in, neutral, bop);
 
 #ifndef BOOST_NO_EXCEPTIONS
       boost::exception_ptr exception;
@@ -74,32 +71,17 @@ namespace nt2 { namespace ext
 
 #pragma omp parallel
       {
-#pragma omp for schedule(static) private(id)
-        for(std::ptrdiff_t o = 0; o < obound; ++o)
+
+        #pragma omp for schedule(static)
+        for(std::ptrdiff_t n=0;n<(std::ptrdiff_t)nblocs;++n)
         {
-          std::size_t o_ = o*ibound;
 #ifndef BOOST_NO_EXCEPTIONS
           try
           {
 #endif
-            for(std::size_t i = 0; i < bound; i+=cache_bound)
-            {
-              id = i+o_;
 
-              for (std::size_t k = 0, k_ = id; k < nb_vec; ++k, k_+=N)
-                nt2::run(out, k_, neutral(nt2::meta::as_<target_type>()));
+            vecworker((std::size_t)n*grain,grain);
 
-              for(std::size_t m = 0; m < mbound; ++m)
-              {
-                std::size_t m_ = m*ibound;
-                for (std::size_t k = 0, k_ = id; k < nb_vec; ++k, k_+=N)
-                  nt2::run( out, k_
-                          , bop( nt2::run(out, k_, meta::as_<target_type>())
-                               , nt2::run(in, k_+m_, meta::as_<target_type>())
-                               )
-                          );
-              }
-            }
 #ifndef BOOST_NO_EXCEPTIONS
           }
           catch(...)
@@ -109,21 +91,13 @@ namespace nt2 { namespace ext
             exception = boost::current_exception();
           }
 #endif
-          // scalar part
-          for(std::size_t i = bound; i < ibound; ++i)
-          {
-            id = i+o_;
-            nt2::run(out, id, neutral(nt2::meta::as_<value_type>()));
-            for(std::size_t m = 0, m_ = 0; m < mbound; ++m, m_+=ibound)
-            {
-              nt2::run( out, id
-                      , bop( nt2::run(out, id,meta::as_<value_type>())
-                           , nt2::run(in, id+m_,meta::as_<value_type>())
-                           )
-                      );
-            }
-          }
         }
+
+        #pragma omp single nowait
+        {
+          if(leftover) vecworker(nblocs*grain,leftover);
+        }
+
 #ifndef BOOST_NO_EXCEPTIONS
         if(exception)
           boost::rethrow_exception(exception);
