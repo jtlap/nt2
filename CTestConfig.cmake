d8c2a684375221ffa32200a388e8d741a7600bab
Mathias Gaunard <mathias@gaunard.com>
2013-10-14 13:21:18 +0200

    increase test timeout to 3 minutes

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,7 +8,7 @@
 ################################################################################
 set(CTEST_PROJECT_NAME "NT2")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
-set(CTEST_TEST_TIMEOUT "60")
+set(CTEST_TEST_TIMEOUT "180")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.lri.fr")
