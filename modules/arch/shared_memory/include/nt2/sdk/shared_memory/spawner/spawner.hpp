7f36034ba514dcb8d83e350a0a267a480e664c99
Antoine TRAN TAN <antoine.tran-tan@live.fr>
2013-10-19 12:09:06 +0200

    Create spawner template class and parfor tag

---
+++ modules/arch/shared_memory/include/nt2/sdk/shared_memory/spawner/spawner.hpp
@@ -0,0 +1,10 @@
+#ifndef NT2_SDK_SHARED_MEMORY_SPAWNER_SPAWNER_HPP_INCLUDED
+#define NT2_SDK_SHARED_MEMORY_SPAWNER_SPAWNER_HPP_INCLUDED
+
+namespace nt2
+{
+  template< class Tag, class Arch >
+  struct spawner;
+}
+#endif
+
