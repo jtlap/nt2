fe5d690870a6316fc6d160b4b754e895afff216e
Antoine TRAN TAN <antoine.tran-tan@live.fr>
2013-10-19 12:11:49 +0200

    Create specialization of parfor spawner for openmp and tbb

---
+++ modules/arch/openmp/include/nt2/sdk/openmp/spawner.hpp
@@ -0,0 +1,78 @@
+#include <nt2/sdk/openmp/openmp.hpp>
+
+#if defined(_OPENMP) && _OPENMP >= 200203 /* OpenMP 2.0 */
+
+#include <nt2/sdk/shared_memory/spawner/spawner.hpp>
+#include <nt2/sdk/shared_memory/spawner/parfor.hpp>
+
+#ifndef BOOST_NO_EXCEPTIONS
+#include <boost/exception_ptr.hpp>
+#endif
+
+namespace nt2
+{
+  template<class Site>
+  struct spawner< tag::parfor_, tag::openmp_<Site> >
+  {
+
+    spawner() {}
+
+    template<typename Worker>
+    void operator()(Worker worker_, std::size_t begin_, std::size_t size_, std::size_t grain_)
+    {
+#ifndef BOOST_NO_EXCEPTIONS
+      boost::exception_ptr exception;
+#endif
+
+      std::size_t nblocks  = size_/grain_;
+      std::size_t ibound   = nblocks * grain_;
+      std::size_t leftover = size_ % grain_;
+
+      #pragma omp parallel
+      {
+        // Dispatch group of blocks over each threads
+        #pragma omp for schedule(dynamic)
+        for(std::ptrdiff_t n=0;n<(std::ptrdiff_t)nblocks;++n)
+        {
+#ifndef BOOST_NO_EXCEPTIONS
+          try
+          {
+#endif
+            // Call operation
+            worker_(begin_+(std::size_t)n*grain_,grain_);
+
+#ifndef BOOST_NO_EXCEPTIONS
+          }
+          catch(...)
+          {
+            #pragma omp critical
+            exception = boost::current_exception();
+          }
+#endif
+        }
+
+        #pragma omp single nowait
+        {
+
+#ifndef BOOST_NO_EXCEPTIONS
+          try
+          {
+#endif
+          if(leftover) worker_(begin_+ibound,leftover);
+
+#ifndef BOOST_NO_EXCEPTIONS
+          }
+          catch(...)
+          {
+            #pragma omp critical
+            exception = boost::current_exception();
+          }
+#endif
+       }
+
+     }
+   }
+ };
+}
+
+#endif
