3648a8d0afda65ef9403c1cc420f18f1a4182699
jtlap <jtlapreste@gmail.com>
2013-10-22 22:19:47 +0200

    much faster log10 for integer types using horizontal simd

--- modules/core/exponential/include/nt2/exponential/functions/scalar/log10.hpp
+++ modules/core/exponential/include/nt2/exponential/functions/scalar/log10.hpp
@@ -6,8 +6,8 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef NT2_EXPONENTIAL_FUNCTIONS_GENERIC_LOG10_HPP_INCLUDED
-#define NT2_EXPONENTIAL_FUNCTIONS_GENERIC_LOG10_HPP_INCLUDED
+#ifndef NT2_EXPONENTIAL_FUNCTIONS_SCALAR_LOG10_HPP_INCLUDED
+#define NT2_EXPONENTIAL_FUNCTIONS_SCALAR_LOG10_HPP_INCLUDED
 
 #include <nt2/exponential/functions/log10.hpp>
 #include <nt2/exponential/functions/scalar/impl/logs.hpp>
@@ -20,19 +20,31 @@
 
 namespace nt2 { namespace ext
 {
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::log10_, tag::cpu_
+                            , (A0)
+                            , (scalar_< int_<A0> >)
+                            )
+  {
+    typedef A0 result_type;
+    NT2_FUNCTOR_CALL(1)
+    {
+      return toint(nt2::log10(nt2::tofloat(a0)));
+    }
+  };
+
 
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::log10_, tag::cpu_
                             , (A0)
-                            , (generic_< floating_<A0> >)
+                            , (scalar_< uint_<A0> >)
                             )
   {
-    typedef typename boost::dispatch::meta::as_floating<A0>::type result_type;
-    typedef typename boost::simd::meta::is_native<A0>::type is_native_t;
+    typedef A0 result_type;
     NT2_FUNCTOR_CALL(1)
     {
-      return details::logarithm<A0,is_native_t>::log10(a0);
+      return touint(nt2::log10(nt2::tofloat(a0)));
     }
   };
+
 } }
 
 
