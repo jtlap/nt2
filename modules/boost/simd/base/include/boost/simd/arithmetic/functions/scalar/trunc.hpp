a7a1ddffa548639d1454a9f8c718924aa00e2a68
jtlap <jtlapreste@gmail.com>
2013-10-23 11:24:12 +0200

    using _mm_cvttss_si32 if available to implement trunc

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/scalar/trunc.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/scalar/trunc.hpp
@@ -19,7 +19,7 @@ namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::trunc_, tag::cpu_
                             , (A0)
-                            , (scalar_< arithmetic_<A0> >)
+                            , (scalar_< integer_<A0> >)
                             )
   {
 
