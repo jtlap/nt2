0d6735e34dcb6a34ab60f761109bfd509cb8ea35
Joel Falcou <joel.falcou@lri.fr>
2013-10-30 16:31:06 +0100

    Complete the fused multiply/{add,sub} familly
    fma and fma is now followed by fms, fsm, fnma, fnam, fnms and fnsm.

---
+++ modules/boost/simd/base/unit/arithmetic/scalar/fnam.cpp
@@ -0,0 +1,27 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <boost/simd/arithmetic/include/functions/fnam.hpp>
+#include <boost/dispatch/functor/meta/call.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
+
+NT2_TEST_CASE_TPL( fnam,  BOOST_SIMD_TYPES)
+{
+  using boost::simd::fnam;
+  using boost::simd::tag::fnma_;
+
+  NT2_TEST_TYPE_IS(typename boost::dispatch::meta::call<fnma_(T,T,T)>::type, T);
+
+  NT2_TEST_EQUAL(fnam(T(4),T(2),T(2)) , T(-8));
+  NT2_TEST_EQUAL(fnam(T(4),T(-2),T(2)), T(0));
+  NT2_TEST_EQUAL(fnam(T(4),T(2),T(-2)), T(0));
+  NT2_TEST_EQUAL(fnam(T(4),T(-2),T(-2)), T(-8));
+}
---
+++ modules/boost/simd/base/unit/arithmetic/simd/fnsm.cpp
@@ -0,0 +1,35 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <boost/simd/arithmetic/include/functions/fnsm.hpp>
+#include <boost/simd/include/functions/simd/enumerate.hpp>
+#include <boost/simd/include/functions/simd/multiplies.hpp>
+#include <boost/simd/include/functions/simd/minus.hpp>
+#include <boost/dispatch/functor/meta/call.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
+
+NT2_TEST_CASE_TPL( fnsm,  BOOST_SIMD_SIMD_TYPES)
+{
+  using boost::simd::fnsm;
+  using boost::simd::enumerate;
+  using boost::simd::native;
+
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>                  vT;
+
+  vT a = enumerate<vT>(0,10);
+  vT b = enumerate<vT>(0,2);
+  vT c = enumerate<vT>(0,3);
+
+  NT2_TEST_EQUAL(fnsm(a,b,c) , b*c-a );
+}
