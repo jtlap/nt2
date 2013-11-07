6464cb3482d682d42af1eaa5186d6582ec3b4a9f
jtlap <jtlapreste@gmail.com>
2013-11-07 11:49:41 +0100

    suppressing intermediates in sse2 int8 multiply

--- modules/boost/simd/base/include/boost/simd/operator/functions/simd/sse/sse2/multiplies.hpp
+++ modules/boost/simd/base/include/boost/simd/operator/functions/simd/sse/sse2/multiplies.hpp
@@ -56,15 +56,18 @@ namespace boost { namespace simd { namespace ext
     {
       typedef typename dispatch::meta::upgrade<A0,signed>::type  type;
 
-      type mask1 = integral_constant<type,0x00FF>();
-      type a0_16 = a0();
-      type a1_16 = a1();
-      type al    = _mm_srli_epi16(a0_16, 8);
-      type bl    = _mm_srli_epi16(a1_16, 8);
-      type abh   = _mm_and_si128(mask1, _mm_mullo_epi16(a0,a1));
-      type ab    = _mm_mullo_epi16(al,bl);
-      type abl   = _mm_slli_epi16(_mm_and_si128(mask1, ab), 8);
-      return _mm_or_si128(abh, abl);
+      type mask = integral_constant<type,0x00FF>();
+      return _mm_or_si128(
+        _mm_and_si128(mask, _mm_mullo_epi16(a0,a1)),
+        _mm_slli_epi16(
+          _mm_and_si128(mask,
+                        _mm_mullo_epi16(_mm_srli_epi16(a0(), 8),
+                                        _mm_srli_epi16(a1(), 8)
+                                       )
+                       ),
+          8
+        )
+      );
     }
   };
 
