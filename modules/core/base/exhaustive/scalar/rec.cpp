538cb34c59d9ca0a5a88f57c1bfd84607c7cfcbe
Joel Falcou <joel.falcou@lri.fr>
2013-11-21 09:22:43 +0100

    [#596] Additional rec/fast_rec/raw_rec exhaustive tests

---
+++ modules/core/base/exhaustive/scalar/rec.cpp
@@ -0,0 +1,32 @@
+//==============================================================================
+//         Copyright 2009 - 2013 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2013        MetaScale SAS
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <nt2/sdk/unit/exhaustive.hpp>
+#include <nt2/include/functions/rec.hpp>
+
+#include <nt2/include/constants/valmin.hpp>
+#include <nt2/include/constants/valmax.hpp>
+
+struct raw_rec
+{
+  float operator()(float x) const
+  {
+    return 1.f/x;
+  }
+};
+
+int main()
+{
+  nt2::exhaustive_test<float> ( nt2::Valmin<float>()
+                              , nt2::Valmax<float>()
+                              , nt2::functor<nt2::tag::rec_>()
+                              , raw_rec()
+                              );
+
+  return 0;
+}
---
+++ modules/core/base/exhaustive/simd/raw_rec.cpp
@@ -0,0 +1,36 @@
+//==============================================================================
+//         Copyright 2009 - 2013 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2013        MetaScale SAS
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <nt2/sdk/unit/exhaustive.hpp>
+#include <nt2/include/functions/raw_rec.hpp>
+
+#include <boost/simd/sdk/simd/native.hpp>
+#include <nt2/include/constants/valmin.hpp>
+#include <nt2/include/constants/valmax.hpp>
+
+struct raw_rec
+{
+  float operator()(float x) const
+  {
+    return 1.f/x;
+  }
+};
+
+int main()
+{
+  typedef BOOST_SIMD_DEFAULT_EXTENSION             ext_t;
+  typedef boost::simd::native<float,ext_t>           n_t;
+
+  nt2::exhaustive_test<n_t> ( nt2::Valmin<float>()
+                            , nt2::Valmax<float>()
+                            , nt2::functor<nt2::tag::raw_rec_>()
+                            , raw_rec()
+                            );
+
+  return 0;
+}
