9cc94a73fed33d74529596577da799da9e0ab792
jtlap <jtlapreste@gmail.com>
2013-12-10 23:20:37 +0100

    rif.cpp commented by mistake

--- modules/type/complex/generative/unit/table/rif.cpp
+++ modules/type/complex/generative/unit/table/rif.cpp
@@ -12,62 +12,62 @@
 #include <nt2/sdk/unit/tests/relation.hpp>
 
 
-// NT2_TEST_CASE_TPL(rif0, NT2_REAL_TYPES)
-// {
-//   typedef std::complex<T> cT;
-//   {
-//     nt2::table<cT> ref( nt2::of_size(3,1) );
-//     for(int i=1;i<= 3;++i)
-//       ref(i) = cT(i);
+NT2_TEST_CASE_TPL(rif0, NT2_REAL_TYPES)
+{
+  typedef std::complex<T> cT;
+  {
+    nt2::table<cT> ref( nt2::of_size(3,1) );
+    for(int i=1;i<= 3;++i)
+      ref(i) = cT(i);
 
-//     nt2::table<cT> x0 = nt2::rif(3, 1, nt2::meta::as_<cT>());
-//     nt2::table<cT> x1 = nt2::rif(3, 1, nt2::meta::as_<T>());
+    nt2::table<cT> x0 = nt2::rif(3, 1, nt2::meta::as_<cT>());
+    nt2::table<cT> x1 = nt2::rif(3, 1, nt2::meta::as_<T>());
 
-//     NT2_TEST_EQUAL( x0,ref );
-//     NT2_TEST_EQUAL( x1,ref );
-//   }
-//    {
-//     nt2::table<cT> ref( nt2::of_size(1,3) );
-//     for(int i=1;i<= 3;++i)
-//       ref(1, i) = cT(1);
+    NT2_TEST_EQUAL( x0,ref );
+    NT2_TEST_EQUAL( x1,ref );
+  }
+   {
+    nt2::table<cT> ref( nt2::of_size(1,3) );
+    for(int i=1;i<= 3;++i)
+      ref(1, i) = cT(1);
 
-//     nt2::table<cT> x0 = nt2::rif(1, 3, nt2::meta::as_<cT>());
-//     nt2::table<cT> x1 = nt2::rif(1, 3, nt2::meta::as_<T>());
+    nt2::table<cT> x0 = nt2::rif(1, 3, nt2::meta::as_<cT>());
+    nt2::table<cT> x1 = nt2::rif(1, 3, nt2::meta::as_<T>());
 
-//     NT2_TEST_EQUAL( x0,ref );
-//     NT2_TEST_EQUAL( x1,ref );
-//   }
-// }
+    NT2_TEST_EQUAL( x0,ref );
+    NT2_TEST_EQUAL( x1,ref );
+  }
+}
 
-// NT2_TEST_CASE_TPL(rif1, NT2_REAL_TYPES)
-// {
-//   {
-//     typedef std::complex<T> cT;
-//     nt2::table<cT> ref( nt2::of_size(3,2) );
-//     for(int i=1;i<= 3;++i)
-//       for(int j=1;j<= 2;++j)
-//         ref(i,j) = cT(i);
+NT2_TEST_CASE_TPL(rif1, NT2_REAL_TYPES)
+{
+  {
+    typedef std::complex<T> cT;
+    nt2::table<cT> ref( nt2::of_size(3,2) );
+    for(int i=1;i<= 3;++i)
+      for(int j=1;j<= 2;++j)
+        ref(i,j) = cT(i);
 
-//     nt2::table<cT> x0 = nt2::rif(3, 2, nt2::meta::as_<cT>());
-//     nt2::table<cT> x1 = nt2::rif(3, 2, nt2::meta::as_<T>());
+    nt2::table<cT> x0 = nt2::rif(3, 2, nt2::meta::as_<cT>());
+    nt2::table<cT> x1 = nt2::rif(3, 2, nt2::meta::as_<T>());
 
-//     NT2_TEST_EQUAL( x0,ref );
-//     NT2_TEST_EQUAL( x1,ref );
-//   }
-//   {
-//     typedef std::complex<T> cT;
-//     nt2::table<cT> ref( nt2::of_size(2, 3) );
-//     for(int i=1;i<= 2;++i)
-//       for(int j=1;j<= 3;++j)
-//         ref(i,j) = cT(i);
+    NT2_TEST_EQUAL( x0,ref );
+    NT2_TEST_EQUAL( x1,ref );
+  }
+  {
+    typedef std::complex<T> cT;
+    nt2::table<cT> ref( nt2::of_size(2, 3) );
+    for(int i=1;i<= 2;++i)
+      for(int j=1;j<= 3;++j)
+        ref(i,j) = cT(i);
 
-//     nt2::table<cT> x0 = nt2::rif(2, 3, nt2::meta::as_<cT>());
-//     nt2::table<cT> x1 = nt2::rif(2, 3, nt2::meta::as_<T>());
+    nt2::table<cT> x0 = nt2::rif(2, 3, nt2::meta::as_<cT>());
+    nt2::table<cT> x1 = nt2::rif(2, 3, nt2::meta::as_<T>());
 
-//     NT2_TEST_EQUAL( x0,ref );
-//     NT2_TEST_EQUAL( x1,ref );
-//   }
-// }
+    NT2_TEST_EQUAL( x0,ref );
+    NT2_TEST_EQUAL( x1,ref );
+  }
+}
 
 NT2_TEST_CASE_TPL(rif2, NT2_REAL_TYPES)
 {
