1a4d7e7c0bdd13b43dbfc7f04120be2857cbb652
Mathias Gaunard <mathias@gaunard.com>
2013-12-10 16:03:14 +0100

    complex extract is redundant

--- modules/type/complex/sdk/include/nt2/memory/functions/complex/simd/common/extract.hpp
+++ modules/type/complex/sdk/include/nt2/memory/functions/complex/simd/common/extract.hpp
@@ -11,24 +11,11 @@
 
 #include <nt2/memory/functions/extract.hpp>
 #include <nt2/include/functions/real.hpp>
-#include <nt2/include/functions/imag.hpp>
 #include <nt2/include/functions/bitwise_cast.hpp>
 #include <nt2/sdk/meta/scalar_of.hpp>
 
 namespace nt2 { namespace ext
 {
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::extract_, tag::cpu_, (A0)(A1)(X)
-                            , ((simd_< complex_ < arithmetic_<A0> >, X >))
-                              (scalar_< integer_<A1> >)
-                            )
-  {
-    typedef typename meta::scalar_of<A0>::type result_type;
-    BOOST_FORCEINLINE result_type operator()(A0 const& a0, A1 a1) const
-    {
-      return result_type(extract(nt2::real(a0), a1), extract(nt2::imag(a0), a1));
-    }
-  };
-
   NT2_FUNCTOR_IMPLEMENTATION( boost::simd::tag::extract_, tag::cpu_, (A0)(A1)(X)
                             , ((simd_< dry_ < arithmetic_<A0> >, X >))
                               (scalar_< integer_<A1> >)
