182a8a1693d65ead92b31c50b19578964e5ed81a
alankelly <alankelly@live.com>
2013-12-12 17:19:18 +0100

    simd helloworld added with updated documentation

---
+++ demo/helloworld/simd/helloworld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/simd/sdk/simd/io.hpp>
+#include <boost/simd/sdk/simd/pack.hpp>
+#include <boost/simd/include/functions/splat.hpp>
+#include <boost/simd/include/functions/plus.hpp>
+#include <boost/simd/include/functions/multiplies.hpp>
+
+int main(int argc, const char *argv[])
+{
+  typedef boost::simd::pack<float> p_t;
+
+  p_t res;
+  p_t u(10);
+  p_t r = boost::simd::splat<p_t>(11);
+
+  res = (u + r) * 2.f;
+
+  std::cout << res << std::endl;
+
+  return 0;
+}
