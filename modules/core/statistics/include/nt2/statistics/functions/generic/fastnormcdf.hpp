ed7a2e7eb8ba73198ffd2f0e5f991e9904eeeca8
Mathias Gaunard <mathias@gaunard.com>
2013-12-13 19:30:30 +0100

    fixing formatting of fastnormcdf, in particular documentation

--- modules/core/statistics/include/nt2/statistics/functions/generic/fastnormcdf.hpp
+++ modules/core/statistics/include/nt2/statistics/functions/generic/fastnormcdf.hpp
@@ -1,6 +1,7 @@
 //==============================================================================
 //         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
 //         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2012 - 2013   MetaScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -8,22 +9,23 @@
 //==============================================================================
 #ifndef NT2_STATISTICS_FUNCTIONS_GENERIC_FASTNORMCDF_HPP_INCLUDED
 #define NT2_STATISTICS_FUNCTIONS_GENERIC_FASTNORMCDF_HPP_INCLUDED
+
 #include <nt2/statistics/functions/fastnormcdf.hpp>
 #include <nt2/polynomials/functions/scalar/impl/horner.hpp>
 #include <nt2/include/functions/simd/abs.hpp>
 #include <nt2/include/functions/simd/rec.hpp>
 #include <nt2/include/functions/simd/fma.hpp>
 #include <nt2/include/functions/simd/seldec.hpp>
-#include <nt2/include/constants/one.hpp>
-#include <nt2/include/constants/half.hpp>
-#include <nt2/include/constants/invsqrt_2pi.hpp>
 #include <nt2/include/functions/simd/sqr.hpp>
 #include <nt2/include/functions/simd/exp.hpp>
 #include <nt2/include/functions/simd/is_gtz.hpp>
 #include <nt2/include/functions/simd/splat.hpp>
 #include <nt2/include/functions/simd/unary_minus.hpp>
 #include <nt2/include/functions/simd/multiplies.hpp>
-
+#include <nt2/include/constants/one.hpp>
+#include <nt2/include/constants/half.hpp>
+#include <nt2/include/constants/invsqrt_2pi.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
 
 namespace nt2 { namespace ext
 {
@@ -44,7 +46,7 @@ namespace nt2 { namespace ext
                                                    , 0x3ea385fa
                                                    ))>(k);
       w*=Invsqrt_2pi<result_type>()*exp(-sqr(l)*Half<result_type>());
-      return(seldec(is_gtz(a0),w));
+      return seldec(is_gtz(a0),w);
     }
   };
 } }
