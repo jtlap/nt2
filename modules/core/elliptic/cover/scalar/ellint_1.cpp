6bf0b60725c218bcd9d6c4ddfc7c85f17ce878e3
jtlap <jtlapreste@gmail.com>
2013-12-13 10:47:16 +0100

    correcting ulps for cover ellint_1.cpp

--- modules/core/elliptic/cover/scalar/ellint_1.cpp
+++ modules/core/elliptic/cover/scalar/ellint_1.cpp
@@ -59,7 +59,7 @@ NT2_TEST_CASE_TPL ( ellint_1_real__1_0,  NT2_REAL_TYPES)
         std::cout << "for param "
                   << "  a0 = "<< u_t(a0 = tab_a0[j])
                   << std::endl;
-        NT2_TEST_ULP_EQUAL( nt2::ellint_1(a0),boost::math::ellint_1(a0),3.5);
+        NT2_TEST_ULP_EQUAL( nt2::ellint_1(a0),boost::math::ellint_1(a0),4);
         ulp0=nt2::max(ulpd,ulp0);
      }
      std::cout << "max ulp found is: " << ulp0 << std::endl;
