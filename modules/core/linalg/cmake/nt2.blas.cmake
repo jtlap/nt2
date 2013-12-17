209bb1f367fe278d76f34849dc4d4caf83fc4e97
Mathias Gaunard <mathias@gaunard.com>
2013-12-17 16:30:33 +0100

    various improvements to finding static MKL and switching between static and dynamic version

--- modules/core/linalg/cmake/nt2.blas.cmake
+++ modules/core/linalg/cmake/nt2.blas.cmake
@@ -26,6 +26,10 @@ if(NT2_BLAS_STATIC)
   set(OLD_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
   set(CMAKE_FIND_LIBRARY_SUFFIXES ".a;.lib")
 endif()
+set(STATIC)
+if(NT2_BLAS_STATIC)
+  set(STATIC _static)
+endif()
 
 # Intel MKL
 if(NT2_BLAS_VENDOR STREQUAL "Intel" OR (NOT DEFINED NT2_BLAS_VENDOR AND NOT NT2_BLAS_FOUND))
@@ -36,12 +40,13 @@ if(NT2_BLAS_VENDOR STREQUAL "Intel" OR (NOT DEFINED NT2_BLAS_VENDOR AND NOT NT2_
 
   # workaround for Ubuntu 13.10
   if(NOT DEFINED NT2_BLAS_STATIC)
-    execute_process(COMMAND lsb_release -rs RESULT_VARIABLE RESULT_VAR OUTPUT_VARIABLE RESULT)
+    execute_process(COMMAND lsb_release -rs RESULT_VARIABLE RESULT_VAR OUTPUT_VARIABLE RESULT ERROR_QUIET)
     if(RESULT_VAR EQUAL 0 AND NOT RESULT VERSION_LESS 13.10)
       message(STATUS "[nt2.blas] Ubuntu 13.10+ detected, using static MKL as workaround")
-      set(NT2_BLAS_STATIC 1)
+      set(NT2_BLAS_STATIC 1 CACHE BOOL "Whether BLAS/LAPACK should be linked statically or not")
       set(OLD_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
       set(CMAKE_FIND_LIBRARY_SUFFIXES ".a;.lib")
+      set(STATIC _static)
     endif()
   endif()
 
@@ -54,79 +59,82 @@ if(NT2_BLAS_VENDOR STREQUAL "Intel" OR (NOT DEFINED NT2_BLAS_VENDOR AND NOT NT2_
   set(NT2_MKL_LIBRARY_DIR ${MKLROOT}/lib)
   if(NT2_ARCH_X86_64)
     set(NT2_MKL_LIBRARY_SUFFIXES intel64)
-    find_library(NT2_MKL_LP64 NAMES mkl_intel_lp64 mkl_intel_lp64_dll
+    find_library(NT2_MKL_LP64${STATIC} NAMES mkl_intel_lp64_dll mkl_intel_lp64
                  PATHS ${NT2_MKL_LIBRARY_DIR}
                  PATH_SUFFIXES ${NT2_MKL_LIBRARY_SUFFIXES}
                 )
-    set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_LP64})
+    set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_LP64${STATIC}})
   else()
     set(NT2_MKL_LIBRARY_SUFFIXES ia32)
     set(NT2_MKL_LIBRARY_DIR ${NT2_BLAS_ROOT} ${MKLROOT}/lib/ia32)
-    find_library(NT2_MKL_32 NAMES mkl_intel mkl_intel_c_dll
+    find_library(NT2_MKL_32${STATIC} NAMES mkl_intel_c_dll mkl_intel_c mkl_intel
                  PATHS ${NT2_MKL_LIBRARY_DIR}
                  PATH_SUFFIXES ${NT2_MKL_LIBRARY_SUFFIXES}
                 )
-    set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_32})
+    set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_32${STATIC}})
   endif()
 
-  if(NT2_MKL_LP64 OR NT2_MKL_32)
+  if(NT2_MKL_LP64${STATIC} OR NT2_MKL_32${STATIC})
     if(NOT DEFINED NT2_BLAS_MULTICORE)
       nt2_module_tool(is_multicore RESULT_VARIABLE RESULT_VAR OUTPUT_QUIET)
-      if(RESULT_VAR EQUAL 0)
+      if(RESULT_VAR GREATER -1)
         set(NT2_BLAS_MULTICORE 1)
       endif()
     endif()
 
     if(NOT NT2_BLAS_MULTICORE)
-      find_library(NT2_MKL_SEQ NAMES mkl_sequential mkl_sequential_dll
+      find_library(NT2_MKL_SEQ${STATIC} NAMES mkl_sequential_dll mkl_sequential
                    PATHS ${NT2_MKL_LIBRARY_DIR}
                    PATH_SUFFIXES ${NT2_MKL_LIBRARY_SUFFIXES}
                   )
-      set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_SEQ})
+      set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_SEQ${STATIC}})
     else()
       # avoid re-checking for OpenMP every time
       if(NOT DEFINED OpenMP_CXX_FLAGS)
         find_package(OpenMP QUIET)
       endif()
       if(NT2_COMPILER_ICC OR NT2_COMPILER_MSVC OR NOT OpenMP_CXX_FLAGS)
-        unset(NT2_MKL_GNU_THREAD)
-        find_library(NT2_MKL_INTEL_THREAD NAMES mkl_intel_thread mkl_intel_thread_dll
+        unset(NT2_MKL_GNU_THREAD${STATIC})
+        find_library(NT2_MKL_INTEL_THREAD${STATIC} NAMES mkl_intel_thread_dll mkl_intel_thread
                      PATHS ${NT2_MKL_LIBRARY_DIR}
                      PATH_SUFFIXES ${NT2_MKL_LIBRARY_SUFFIXES}
                     )
         file(TO_CMAKE_PATH "$ENV{LIBRARY_PATH}" LIBRARY_PATH)
-        find_library(NT2_INTEL_OMP NAMES iomp5 iomp5md libiomp5md
+        find_library(NT2_INTEL_OMP${STATIC} NAMES libiomp5md iomp5md iomp5
                      PATHS ${LIBRARY_PATH}
                      PATH_SUFFIXES ${NT2_MKL_LIBRARY_SUFFIXES}
                     )
-        set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_INTEL_THREAD} ${NT2_INTEL_OMP})
+        set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_INTEL_THREAD${STATIC}} ${NT2_INTEL_OMP${STATIC}})
       elseif(NT2_COMPILER_GCC_LIKE)
-        unset(NT2_MKL_INTEL_THREAD)
-        unset(NT2_INTEL_OMP)
-        find_library(NT2_MKL_GNU_THREAD NAMES mkl_gnu_thread
+        unset(NT2_MKL_INTEL_THREAD${STATIC})
+        unset(NT2_INTEL_OMP${STATIC})
+        find_library(NT2_MKL_GNU_THREAD${STATIC} NAMES mkl_gnu_thread
                      PATHS ${NT2_MKL_LIBRARY_DIR}
                      PATH_SUFFIXES ${NT2_MKL_LIBRARY_SUFFIXES}
                     )
-        set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_GNU_THREAD})
+        set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_GNU_THREAD${STATIC}})
         set(NT2_BLAS_LINK_FLAGS ${OpenMP_CXX_FLAGS})
-      endif()
-
-      if(UNIX)
-        find_package(Threads)
-        set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
       endif()
     endif()
 
-    find_library(NT2_MKL_CORE NAMES mkl_core mkl_core_dll
+    find_library(NT2_MKL_CORE${STATIC} NAMES mkl_core_dll mkl_core
                  PATHS ${NT2_MKL_LIBRARY_DIR}
                  PATH_SUFFIXES ${NT2_MKL_LIBRARY_SUFFIXES}
                 )
-    set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_CORE})
+    set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} ${NT2_MKL_CORE${STATIC}})
 
-    if(NT2_MKL_SEQ AND NT2_MKL_CORE)
+    if(UNIX)
+        set(NT2_BLAS_LIBRARIES -Wl,--start-group ${NT2_BLAS_LIBRARIES} -Wl,--end-group)
+        if(NT2_BLAS_STATIC)
+          set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} -ldl)
+        endif()
+        set(NT2_BLAS_LIBRARIES ${NT2_BLAS_LIBRARIES} -lpthread)
+      endif()
+
+    if(NT2_MKL_SEQ${STATIC} AND NT2_MKL_CORE${STATIC})
       set(NT2_BLAS_FOUND TRUE)
       set(NT2_LAPACK_FOUND TRUE)
-    elseif(NT2_MKL_GNU_THREAD OR (NT2_MKL_INTEL_THREAD AND NT2_INTEL_OMP))
+    elseif(NT2_MKL_GNU_THREAD${STATIC} OR (NT2_MKL_INTEL_THREAD${STATIC} AND NT2_INTEL_OMP${STATIC}))
       if(NT2_MKL_CORE)
         set(NT2_BLAS_FOUND TRUE)
         set(NT2_LAPACK_FOUND TRUE)
@@ -146,11 +154,11 @@ if(NT2_BLAS_VENDOR STREQUAL "Goto2" OR (NOT DEFINED NT2_BLAS_VENDOR AND NOT NT2_
     message(STATUS "[nt2.blas] trying Goto2...")
   endif()
 
-  find_library(NT2_GOTO2 NAMES goto2
+  find_library(NT2_GOTO2${STATIC} NAMES goto2
                PATHS ${NT2_BLAS_ROOT})
-  if(NT2_GOTO2)
+  if(NT2_GOTO2${STATIC})
     set(NT2_BLAS_FOUND TRUE)
-    set(NT2_BLAS_LIBRARIES ${NT2_GOTO2})
+    set(NT2_BLAS_LIBRARIES ${NT2_GOTO2${STATIC}})
 
     if(NOT DEFINED NT2_BLAS_VENDOR)
       set(NT2_BLAS_VENDOR Goto2)
@@ -164,11 +172,11 @@ if(NT2_BLAS_VENDOR STREQUAL "Netlib" OR (NOT DEFINED NT2_BLAS_VENDOR AND NOT NT2
     message(STATUS "[nt2.blas] trying Netlib...")
   endif()
 
-  find_library(NT2_NETLIB NAMES blas_LINUX
+  find_library(NT2_NETLIB${STATIC} NAMES blas_LINUX
                PATHS ${NT2_BLAS_ROOT})
   if(NT2_NETLIB)
     set(NT2_BLAS_FOUND TRUE)
-    set(NT2_BLAS_LIBRARIES ${NT2_NETLIB})
+    set(NT2_BLAS_LIBRARIES ${NT2_NETLIB${STATIC}})
 
     if(NOT DEFINED NT2_BLAS_VENDOR)
       set(NT2_BLAS_VENDOR Netlib)
@@ -183,10 +191,10 @@ if(NOT NT2_BLAS_FOUND)
     message(STATUS "[nt2.blas] trying built-in...")
   endif()
 
-  find_library(BLAS_LIBRARY NAMES blas libblas PATHS ${NT2_BLAS_ROOT})
-  if(BLAS_LIBRARY)
+  find_library(BLAS_LIBRARY${STATIC} NAMES blas libblas PATHS ${NT2_BLAS_ROOT})
+  if(BLAS_LIBRARY${STATIC})
     set(NT2_BLAS_FOUND 1)
-    set(NT2_BLAS_LIBRARIES ${BLAS_LIBRARY})
+    set(NT2_BLAS_LIBRARIES ${BLAS_LIBRARY${STATIC}})
   endif()
 
   if(NOT NT2_BLAS_FOUND)
@@ -206,10 +214,10 @@ endif()
 if(NOT NT2_LAPACK_FOUND) # Find lapack if not MKL
   message(STATUS "[nt2.linalg] looking for LAPACK...")
 
-  find_library(LAPACK_LIBRARY NAMES lapack liblapack PATHS ${NT2_BLAS_ROOT})
+  find_library(LAPACK_LIBRARY${STATIC} NAMES lapack liblapack PATHS ${NT2_BLAS_ROOT})
   if(LAPACK_LIBRARY)
     set(NT2_LAPACK_FOUND 1)
-    set(NT2_LAPACK_LIBRARIES ${LAPACK_LIBRARY})
+    set(NT2_LAPACK_LIBRARIES ${LAPACK_LIBRARY${STATIC}})
   endif()
 
   if(NOT NT2_LAPACK_FOUND)
