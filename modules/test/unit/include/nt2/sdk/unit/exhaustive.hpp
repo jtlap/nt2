52ab18e68584a93964abe2581abb68d91850ab3f
Mathias Gaunard <mathias@gaunard.com>
2014-01-04 19:34:49 +0100

    try to improve exhaustive.hpp: restore fixed example, fix includes, improve formatting

--- modules/test/unit/include/nt2/sdk/unit/exhaustive.hpp
+++ modules/test/unit/include/nt2/sdk/unit/exhaustive.hpp
@@ -9,23 +9,24 @@
 #ifndef NT2_SDK_UNIT_EXHAUSTIVE_HPP_INCLUDED
 #define NT2_SDK_UNIT_EXHAUSTIVE_HPP_INCLUDED
 
-#include <nt2/include/functions/load.hpp>
-#include <nt2/include/functions/extract.hpp>
-#include <nt2/include/functions/is_invalid.hpp>
-#include <nt2/include/functions/successor.hpp>
-#include <nt2/include/functions/ulpdist.hpp>
+#include <nt2/include/functions/simd/load.hpp>
+#include <nt2/include/functions/simd/extract.hpp>
+#include <nt2/include/functions/simd/successor.hpp>
+#include <nt2/include/functions/simd/splat.hpp>
+#include <nt2/include/functions/scalar/is_invalid.hpp>
+#include <nt2/include/functions/scalar/ulpdist.hpp>
+#include <nt2/include/functions/scalar/iround.hpp>
+#include <nt2/include/functions/scalar/ilog2.hpp>
+#include <nt2/include/functions/scalar/min.hpp>
 #include <nt2/include/constants/valmin.hpp>
-#include <nt2/include/functions/iround.hpp>
-#include <nt2/include/functions/splat.hpp>
-#include <nt2/include/functions/ilog2.hpp>
-#include <nt2/include/constants/zero.hpp>
-#include <nt2/include/functions/min.hpp>
+#include <nt2/include/constants/valmax.hpp>
 #include <nt2/include/constants/nan.hpp>
 #include <boost/simd/sdk/meta/cardinal_of.hpp>
 #include <nt2/sdk/meta/type_id.hpp>
 #include <iostream>
 #include <iomanip>
 #include <cstdio>
+#include <cstddef>
 
 #define NT2_ASSERTS_AS_TRAP
 #include <nt2/sdk/error/assert_as_trap.hpp>
@@ -51,8 +52,49 @@ namespace nt2
     @param  maxi Upper    bound of the test interval
     @param  test_f        Function to test
     @param  reference_f   Reference function to compare to
-  **/
 
+    @par Example:
+
+    Here is an example to test SIMD single-precision nt2::log against scalar double-precision std::log.
+
+    @code
+    #include <boost/simd/sdk/simd/native.hpp>
+    #include <nt2/include/functions/log.hpp>
+    #include <nt2/include/constants/zero.hpp>
+    #include <nt2/include/constants/valmax.hpp>
+
+    #include <nt2/sdk/unit/exhaustive.hpp>
+    #include <cmath>
+    #include <cstdlib>
+
+    // function object used for the reference
+    struct std_log
+    {
+      float operator()(float x) const
+      {
+        return float(std::log(double(x)));
+      }
+    };
+
+    int main(int argc, char* argv[])
+    {
+      // define boundaries from command-line arguments,
+      // fallback to default values if not provided
+      float mini = argc >= 2 ? std::atof(argv[1]) : nt2::Zero<float>();
+      float mini = argc >= 3 ? std::atof(argv[2]) : nt2::Valmin<float>();
+
+      // type to call the function object to test with
+      typedef boost::simd::native<float, BOOST_SIMD_DEFAULT_EXTENSION> n_t;
+
+      // run the test
+      nt2::exhaustive_test<n_t>( mini
+                               , maxi
+                               , nt2::functor<nt2::tag::log_>() // function object to test
+                               , std_log()
+                               );
+    }
+    @endcode
+  **/
 
   template<typename Type, typename TestF, typename RefF> inline
   void exhaustive_test(float mini, float maxi, TestF test_f, RefF reference_f)
@@ -93,7 +135,7 @@ namespace nt2
     std::cout << "Exhaustive test for: " << nt2::type_id(test_f)      << "\n"
               << "             versus: " << nt2::type_id(reference_f) << "\n"
               << "             With T: " << nt2::type_id<Type>()      << "\n"
-              << "    in the interval: [" << mini << ",  " << maxi << "]" << "\n";
+              << "           in range: [" << mini << ", " << maxi << "]" << "\n";
     std::cout << std::endl;
     std::cout << "[" << std::flush;
 
@@ -168,12 +210,12 @@ namespace nt2
     {
       if(histo[i])
       {
-        printf("%10u values (%.2f%%)\twithin %1.1f ULPs.\t"
+        printf("%10u values (%.2f%%)\twithin %1.1f ULPs\t"
               , histo[i], (histo[i]*100.0/k), (d < 2 ? 0 : d/4)
               );
         if(i)
-          std::cout << std::scientific << std::setprecision(7)
-                    << "All samples values are in = [" << minin[i] << ",  "<< maxin[i] << "]"
+          std::cout << std::scientific << std::setprecision(9)
+                    << "in range [" << minin[i] << ", "<< maxin[i] << "]" << "."
                     << " Example: "<< minin[i] << " returns " << minval[i]
                     << " instead of " << minref[i];
         std::cout << std::endl;
@@ -185,8 +227,8 @@ namespace nt2
         printf("%10u values (%.2f%%)\twith invalid result.\t"
               , histo[M+1], (histo[M+1]*100.0/k)
               );
-        std::cout << std::scientific << std::setprecision(7)
-                  << "All samples value in = [" << minin[M+1] << ",  "<< maxin[M+1] << "]"
+        std::cout << std::scientific << std::setprecision(9)
+                  << "in range [" << minin[M+1] << ", "<< maxin[M+1] << "]" << "."
                   << " Example: "<< minin[M+1] << " returns " << minval[M+1]
                   << " instead of " << minref[M+1]
                   << std::endl;
