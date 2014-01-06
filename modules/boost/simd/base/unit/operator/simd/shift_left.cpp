d45ab83afe8c643f75f9ba39d8e573c2d86e5450
alankelly <alankelly@live.com>
2014-01-06 18:05:04 +0100

    added additional unit tests to shift_left.cpp to test the changes made to the arch arm version of this which can now accept both signed and unsigned arguements

--- modules/boost/simd/base/unit/operator/simd/shift_left.cpp
+++ modules/boost/simd/base/unit/operator/simd/shift_left.cpp
@@ -1,6 +1,7 @@
 //==============================================================================
 //         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
 //         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2012 - 2014   MetaScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -29,8 +30,15 @@ NT2_TEST_CASE_TPL ( shift_left_integer,  BOOST_SIMD_SIMD_INTEGRAL_TYPES)
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
   typedef native<T,ext_t>                  vT;
   typedef typename boost::dispatch::meta::as_integer<vT>::type ivT;
+  typedef typename boost::dispatch::meta::as_unsigned<vT>::type sivT;
   typedef typename boost::dispatch::meta::call<shift_left_(vT,ivT)>::type r_t;
+  typedef typename boost::dispatch::meta::call<shift_left_(vT,sivT)>::type sr_t;
 
+  // tests for signed and unsigned arguments
+  NT2_TEST_EQUAL(shift_left(boost::simd::One<vT>(),boost::simd::One<sivT>()), boost::simd::Two<sr_t>());
+  NT2_TEST_EQUAL(shift_left(boost::simd::One<vT>(),boost::simd::Zero<sivT>()), boost::simd::One<sr_t>());
+  NT2_TEST_EQUAL(shift_left(boost::simd::splat<vT>(17),boost::simd::splat<sivT>(2)), boost::simd::splat<sr_t>(68));
+  NT2_TEST_EQUAL(shift_left(boost::simd::Zero<vT>(),boost::simd::One<sivT>()), boost::simd::Zero<sr_t>());
   // specific values tests
   NT2_TEST_EQUAL(shift_left(boost::simd::One<vT>(),boost::simd::One<ivT>()), boost::simd::Two<r_t>());
   NT2_TEST_EQUAL(shift_left(boost::simd::One<vT>(),boost::simd::Zero<ivT>()), boost::simd::One<r_t>());
