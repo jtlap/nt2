e3a329d0a22cc7afa36b597b4a3007a7ec5e33a5
jtlap <jtlapreste@gmail.com>
2014-01-07 17:48:08 +0100

    adding two simd exhaustive tests for log2 and log10

---
+++ modules/core/exponential/exhaustive/simd/log10.cpp
@@ -0,0 +1,45 @@
+//==============================================================================
+//         Copyright 2009 - 2013 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2013        MetaScale SAS
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <boost/simd/sdk/simd/native.hpp>
+#include <nt2/include/functions/log10.hpp>
+#include <nt2/include/constants/zero.hpp>
+#include <nt2/include/constants/valmax.hpp>
+
+#include <nt2/sdk/unit/exhaustive.hpp>
+
+#define NT2_ASSERTS_AS_TRAP
+#include <nt2/sdk/error/assert_as_trap.hpp>
+
+#include <cmath>
+#include <cstdlib>
+
+struct std_log10
+{
+  float operator()(float x) const
+  {
+    return float(std::log10(double(x)));
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  typedef BOOST_SIMD_DEFAULT_EXTENSION             ext_t;
+  typedef boost::simd::native<float,ext_t>           n_t;
+  float mini = nt2::Zero<float>();
+  float maxi = nt2::Valmax<float>();
+  if(argc >= 2) mini = std::atof(argv[1]);
+  if(argc >= 3) maxi = std::atof(argv[2]);
+
+  nt2::exhaustive_test<n_t> ( mini
+                            , maxi
+                            , nt2::functor<nt2::tag::log10_>()
+                            , std_log10()
+                            );
+  return 0;
+}
