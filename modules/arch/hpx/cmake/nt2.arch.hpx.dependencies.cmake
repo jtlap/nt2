fb8d4c51ca30ece20e4f2d38e1c393ba4bb3afc2
Mathias Gaunard <mathias@gaunard.com>
2014-01-21 14:59:46 +0100

    prefer hpx/external in HPX_ROOT than in /usr/local/include

--- modules/arch/hpx/cmake/nt2.arch.hpx.dependencies.cmake
+++ modules/arch/hpx/cmake/nt2.arch.hpx.dependencies.cmake
@@ -31,6 +31,7 @@ if(HPX_ROOT)
 endif()
 
 if(NOT HPX_INCLUDE_EXTERNAL_DIRS)
+  find_file(HPX_INCLUDE_EXTERNAL_DIRS hpx/external PATHS ${HPX_ROOT} NO_DEFAULT_PATH)
   find_file(HPX_INCLUDE_EXTERNAL_DIRS hpx/external)
 endif()
 
