dd6ea60ef5149c620a95ced78ca5acc5094694a0
alankelly <alankelly@live.com>
2014-01-23 17:59:54 +0100

    boolean/functions cleaned

--- modules/boost/simd/base/include/boost/simd/boolean/functions/scalar/if_zero_else.hpp
+++ modules/boost/simd/base/include/boost/simd/boolean/functions/scalar/if_zero_else.hpp
@@ -1,6 +1,7 @@
 //==============================================================================
 //         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
 //         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2012 - 2014 MetaScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -8,30 +9,38 @@
 //==============================================================================
 #ifndef BOOST_SIMD_BOOLEAN_FUNCTIONS_SCALAR_IF_ZERO_ELSE_HPP_INCLUDED
 #define BOOST_SIMD_BOOLEAN_FUNCTIONS_SCALAR_IF_ZERO_ELSE_HPP_INCLUDED
-
 #include <boost/simd/boolean/functions/if_zero_else.hpp>
 #include <boost/simd/include/functions/is_eqz.hpp>
 #include <boost/simd/include/constants/zero.hpp>
+#include <boost/dispatch/attributes.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_zero_else_, tag::cpu_, (A0)(A1)
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_zero_else_
+                                    , tag::cpu_
+                                    , (A0)(A1)
                                     , (scalar_< logical_<A0> >)
                                       (scalar_< unspecified_<A1> >)
                                     )
   {
     typedef A1 result_type;
-    inline A1 operator()(const A0 & a0,const A1 & a1) const
-    { return is_eqz(a0) ? a1 : Zero<result_type>(); }
+    BOOST_FORCEINLINE BOOST_SIMD_FUNCTOR_CALL(2)
+    {
+      return is_eqz(a0) ? a1 : Zero<result_type>();
+    }
   };
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_zero_else_, tag::cpu_, (A0)(A1)
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_zero_else_
+                                    , tag::cpu_
+                                    , (A0)(A1)
                                     , (scalar_< arithmetic_<A0> >)
                                       (scalar_< arithmetic_<A1> >)
                                     )
   {
     typedef A1 result_type;
-    inline A1 operator()(const A0 & a0,const A1 & a1) const
-    { return is_eqz(a0) ?  a1 :Zero<result_type>(); }
+    BOOST_FORCEINLINE BOOST_SIMD_FUNCTOR_CALL(2)
+    {
+      return is_eqz(a0) ?  a1 :Zero<result_type>();
+    }
   };
 } } }
 
