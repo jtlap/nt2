0964e92f9f46a51bac59a554bf0881160a654898
alankelly <alankelly@live.com>
2014-01-29 12:00:03 +0100

    added missing common to macro parameter

--- modules/boost/simd/base/include/boost/simd/ieee/functions/simd/common/sign.hpp
+++ modules/boost/simd/base/include/boost/simd/ieee/functions/simd/common/sign.hpp
@@ -39,7 +39,7 @@ namespace boost { namespace simd { namespace ext
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::sign_, tag::cpu_
                                     , (A0)(X)
-                                      ((simd_<unsigned_<A0>,X>))
+                                    , ((simd_<unsigned_<A0>,X>))
                                     )
   {
     typedef A0 result_type;
