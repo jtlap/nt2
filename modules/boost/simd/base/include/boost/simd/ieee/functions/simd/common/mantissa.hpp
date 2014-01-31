3bb6872ddf939321091ef9daccdab3bdf18362b6
Mathias Gaunard <mathias@gaunard.com>
2014-01-31 15:18:39 +0100

    moving logical_or include to BOOST_SIMD_NO_INVALIDS in mantissa

--- modules/boost/simd/base/include/boost/simd/ieee/functions/simd/common/mantissa.hpp
+++ modules/boost/simd/base/include/boost/simd/ieee/functions/simd/common/mantissa.hpp
@@ -12,7 +12,6 @@
 
 #include <boost/simd/ieee/functions/mantissa.hpp>
 #include <boost/simd/include/functions/simd/is_eqz.hpp>
-#include <boost/simd/include/functions/simd/logical_or.hpp>
 #include <boost/simd/include/functions/simd/if_else.hpp>
 #include <boost/simd/include/functions/simd/bitwise_and.hpp>
 #include <boost/simd/include/functions/simd/bitwise_or.hpp>
@@ -21,6 +20,7 @@
 #include <boost/simd/sdk/config.hpp>
 
 #ifndef BOOST_SIMD_NO_INVALIDS
+#include <boost/simd/include/functions/simd/logical_or.hpp>
 #include <boost/simd/include/functions/simd/is_invalid.hpp>
 #endif
 
