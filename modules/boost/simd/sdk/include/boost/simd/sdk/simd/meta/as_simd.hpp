ca72be04ad4385a441db412d5cbdac5173eb57e8
Mathias Gaunard <mathias@gaunard.com>
2014-02-02 23:06:16 +0100

    workaround for weird bug with gcc in split on 32-bit platforms

--- modules/boost/simd/sdk/include/boost/simd/sdk/simd/meta/as_simd.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/simd/meta/as_simd.hpp
@@ -14,6 +14,8 @@
 #include <boost/simd/sdk/simd/preprocessor/repeat.hpp>
 #include <boost/simd/sdk/config/type_lists.hpp>
 #include <boost/simd/sdk/config/types.hpp>
+#include <boost/simd/sdk/config/compiler.hpp>
+#include <boost/simd/sdk/config/arch.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/dispatch/meta/na.hpp>
 #include <boost/type_traits/is_fundamental.hpp>
@@ -54,7 +56,13 @@ namespace boost { namespace simd { namespace meta
     typedef t type __attribute__((__vector_size__(n)));                        \
   };                                                                           \
   /**/
+
+// GCC bug with 64-bit integer types on i686
+#if defined(BOOST_SIMD_COMPILER_GCC) && defined(BOOST_SIMD_ARCH_X86) && !defined(BOOST_SIMD_ARCH_X86_64)
+  #define M1(z,n,t) BOOST_PP_SEQ_FOR_EACH(M0, n, BOOST_SIMD_SPLITABLE_TYPES(double))
+#else
   #define M1(z,n,t) BOOST_PP_SEQ_FOR_EACH(M0, n, BOOST_SIMD_TYPES)
+#endif
 #ifdef __GNUC__
   M0(0, 1, boost::simd::int8_t)
   M0(0, 1, boost::simd::uint8_t)
