2fbce362084d456aa37ce0eae21055888befeb12
Mathias Gaunard <mathias@gaunard.com>
2014-02-03 19:11:33 +0100

    silence warnings in nearest/linear/pchip unit tests

--- modules/core/interpol/unit/scalar/nearest.cpp
+++ modules/core/interpol/unit/scalar/nearest.cpp
@@ -12,6 +12,8 @@
 #include <nt2/include/functions/nearest.hpp>
 #include <nt2/include/functions/linspace.hpp>
 #include <nt2/include/functions/isequal.hpp>
+#include <boost/dispatch/meta/ignore_unused.hpp>
+
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
 #include <nt2/sdk/unit/tests/basic.hpp>
@@ -92,6 +94,7 @@ NT2_TEST_CASE_TPL( nearest3, NT2_REAL_TYPES )
   NT2_DISPLAY(y);
   NT2_DISPLAY(xi);
   nt2::table<T> y0;
+  boost::dispatch::ignore_unused(y0);
   NT2_TEST_ASSERT(y0=nt2::nearest(x, y, xi));
   NT2_TEST_ASSERT(y0=nt2::nearest(T(3), y, xi));
   NT2_TEST_ASSERT(y0=nt2::nearest(T(3), y, xi, T(32)));
@@ -107,6 +110,7 @@ NT2_TEST_CASE_TPL( nearest4, NT2_REAL_TYPES )
   nt2::table<T> y =  T(2);
   nt2::table<T> xi=  nt2::linspace(T(1),  T(4),4);
   nt2::table<T> y0;
+  boost::dispatch::ignore_unused(y0);
   NT2_TEST_ASSERT(y0=nt2::nearest(x, y, xi));
   NT2_TEST_ASSERT(y0=nt2::nearest(x, y, xi, T(32)));
   NT2_TEST_ASSERT(y0=nt2::nearest(x, y, xi, true));
