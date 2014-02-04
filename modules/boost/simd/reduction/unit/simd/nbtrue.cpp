75286ddcf602d62f40c8486bc714a9d88c978702
Mathias Gaunard <mathias@gaunard.com>
2014-02-04 01:28:27 +0100

    remove unused typedefs from (i)nbtrue tests

--- modules/boost/simd/reduction/unit/simd/nbtrue.cpp
+++ modules/boost/simd/reduction/unit/simd/nbtrue.cpp
@@ -20,16 +20,11 @@ NT2_TEST_CASE_TPL ( nbtrue_real__1_0,  BOOST_SIMD_SIMD_REAL_TYPES)
   using boost::simd::tag::nbtrue_;
   using boost::simd::native;
   using boost::simd::meta::cardinal_of;
-  typedef typename boost::dispatch::meta::scalar_of<T>::type sT;
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename boost::dispatch::meta::upgrade<T>::type   u_t;
   typedef native<T,ext_t>                        n_t;
   typedef n_t                                     vT;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
   typedef typename boost::dispatch::meta::call<nbtrue_(vT)>::type r_t;
   typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
 
   // specific values tests
   NT2_TEST_EQUAL(nbtrue(boost::simd::Inf<vT>()) , cardinal_of<vT>::value);
@@ -46,16 +41,11 @@ NT2_TEST_CASE_TPL ( nbtrue_integer,  BOOST_SIMD_SIMD_INTEGRAL_TYPES)
   using boost::simd::tag::nbtrue_;
   using boost::simd::native;
   using boost::simd::meta::cardinal_of;
-  typedef typename boost::dispatch::meta::scalar_of<T>::type sT;
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename boost::dispatch::meta::upgrade<T>::type   u_t;
   typedef native<T,ext_t>                        n_t;
   typedef n_t                                     vT;
-  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
   typedef typename boost::dispatch::meta::call<nbtrue_(vT)>::type r_t;
   typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
-  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
 
   // specific values tests
   NT2_TEST_EQUAL(nbtrue(boost::simd::Mone<vT>()), sr_t(cardinal_of<vT>::value));
