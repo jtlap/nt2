fdd06315867933ca679f1343755f7ccdd6fa1996
Mathias Gaunard <mathias@gaunard.com>
2014-02-04 09:51:53 +0100

    wrong simplification in if_zero_else

--- modules/boost/simd/base/include/boost/simd/boolean/functions/scalar/if_zero_else.hpp
+++ modules/boost/simd/base/include/boost/simd/boolean/functions/scalar/if_zero_else.hpp
@@ -11,7 +11,6 @@
 #define BOOST_SIMD_BOOLEAN_FUNCTIONS_SCALAR_IF_ZERO_ELSE_HPP_INCLUDED
 
 #include <boost/simd/boolean/functions/if_zero_else.hpp>
-#include <boost/simd/include/functions/scalar/is_eqz.hpp>
 #include <boost/simd/include/constants/zero.hpp>
 #include <boost/dispatch/attributes.hpp>
 
@@ -20,27 +19,14 @@ namespace boost { namespace simd { namespace ext
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_zero_else_
                                     , tag::cpu_
                                     , (A0)(A1)
-                                    , (scalar_< logical_<A0> >)
-                                      (scalar_< unspecified_<A1> >)
+                                    , (scalar_< fundamental_<A0> >)
+                                      (scalar_< fundamental_<A1> >)
                                     )
   {
     typedef A1 result_type;
     BOOST_FORCEINLINE BOOST_SIMD_FUNCTOR_CALL(2)
     {
-      return a0 ? a1 : Zero<result_type>();
-    }
-  };
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_zero_else_
-                                    , tag::cpu_
-                                    , (A0)(A1)
-                                    , (scalar_< arithmetic_<A0> >)
-                                      (scalar_< arithmetic_<A1> >)
-                                    )
-  {
-    typedef A1 result_type;
-    BOOST_FORCEINLINE BOOST_SIMD_FUNCTOR_CALL(2)
-    {
-      return is_eqz(a0) ? a1 : Zero<result_type>();
+      return a0 ? Zero<result_type>() : a1;
     }
   };
 } } }
