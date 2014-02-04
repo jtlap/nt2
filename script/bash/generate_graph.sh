6c08ff91fe6b3a723bb3a12825affe13ba2c4e28
alankelly <alankelly@live.com>
2014-02-04 14:54:02 +0100

    new scripts to create benchmarks

---
+++ script/bash/generate_graph.sh
@@ -0,0 +1,159 @@
+nb_data_types=$1
+NUM_TEST=$2
+nb_sizes=$3
+LEGEND=$4
+TITLE=$5
+PLOT_NAME=$6
+kk=$7
+
+i=0
+filename="create_plot${kk}.m"
+count=0
+for ((jj=0;jj<${NUM_TEST};jj++));do
+  for ((ll=1;ll<=${nb_sizes};ll++));do
+    ((pos=${jj}*${nb_sizes}*${nb_data_types}+${ll}+${kk}*${nb_sizes}))
+    SIZE[count]=$(sed -n ${pos}p res.txt | tr "(" "\n" | tr ")" "\n" | sed -n 2p)
+    CYC[count]=$(sed -n ${pos}p res.txt | cut -f3 )
+    UNIT=$(sed -n ${pos}p res.txt | cut -f4 | tr " " "\n" | sed -n 1p)
+    ((count++))
+  done
+done
+
+for ((ii=10#0;ii<${nb_sizes};ii++));do
+  for ((jj=10#0;jj<(${NUM_TEST});jj++));do
+    q=(${ii}+${jj}*${nb_sizes})
+    CYC2[${jj}+${ii}*(${NUM_TEST})]=${CYC[q]}
+  done
+done
+
+for ((ii=0;ii<${nb_sizes};ii++));do
+  declare "cyc${ii}=cyc${ii}"
+done
+((size_x_axis=${nb_sizes}))
+for ((ii=0;ii<${NUM_TEST};ii++));do
+  eval cyc${ii}='cyc${ii}=['
+done
+
+echo "hold on" > ${filename}
+echo "CYC2=[">>${filename}
+for ((ii=10#0;ii<${nb_sizes};ii++));do
+  for ((jj=10#0;jj<(${NUM_TEST});jj++));do
+    echo ${CYC2[${jj}+${ii}*(${NUM_TEST})]}>>${filename}
+  done
+done
+echo "]">>${filename}
+X="x=["
+
+for ((i = 1 ; i <= ${size_x_axis} ; i++)); do
+  X="$X $i"
+done
+for ((ii=10#0;ii<(${NUM_TEST});ii++));do
+  for ((jj=10#0;jj<${nb_sizes};jj++));do
+    ((q= ${jj} + ${ii}*${nb_sizes}))
+    var=cyc${ii}
+    temp="${!var} ${CYC[${q}]}"
+    eval cyc${ii}='$temp'
+    var=cyc${ii}
+  done
+done
+
+for ((ii=0;ii<${NUM_TEST};ii++));do
+  var=cyc${ii}
+  temp="${!var} ]"
+  eval cyc${ii}='$temp'
+done
+
+for ((ii=10#0;ii<(${NUM_TEST});ii++));do
+  var=cyc${ii}
+  echo ${!var} >> ${filename}
+done
+echo ${TITLE[kk]}  >> ${filename}
+
+X="$X ];"
+echo $X>>${filename}
+
+echo "num_y_ticks = round(max(CYC2))">>${filename}
+((q=${gap}+1))
+echo "num_div=1;">>${filename}
+
+echo "max_y=max(CYC2)*1.2">>${filename}
+echo "y_inc = max_y/num_y_ticks">>${filename}
+echo "x_points=ones(1,num_y_ticks);">>${filename}
+echo "y_points=zeros(1,num_y_ticks);">>${filename}
+echo "  y_dist=0.5;">>${filename}
+echo "if (max(CYC2)>1000)">>${filename}
+echo "  num_div=100;">>${filename}
+echo "  y_dist=3;">>${filename}
+echo "elseif (max(CYC2)>160)">>${filename}
+echo "  num_div=16;">>${filename}
+echo "  y_dist=2;">>${filename}
+echo "elseif (max(CYC2)>80)">>${filename}
+echo "  num_div=8;">>${filename}
+echo "elseif (max(CYC2)>40)">>${filename}
+echo "  num_div=4;">>${filename}
+echo "elseif (max(CYC2)>20)">>${filename}
+echo "  num_div=2;">>${filename}
+echo "elseif (max(CYC2)>10)">>${filename}
+echo "  num_div=1;">>${filename}
+echo "endif">>${filename}
+echo "num_y_ticks/=num_div;">>${filename}
+echo "y_points(1)=y_inc*num_div;">>${filename}
+echo "for i=2:num_y_ticks">>${filename}
+echo "  y_points(i)=y_points(i-1)+y_inc*num_div;">>${filename}
+echo "endfor">>${filename}
+
+echo "w=0.4;">>${filename}
+
+COLORS[0]=" '+r' "
+COLORS[1]=" '+g' "
+COLORS[2]=" '+b' "
+COLORS[3]=" '+c' "
+COLORS[4]=" '+y' "
+COLORS[5]=" \"+4\" "
+COLORS[6]=" '+','color',[255/255,140/255,0] "
+COLORS[7]=" '+','color',[0,0,0] "
+COLORS[8]=" '+','color',[34/255,139/255,34/255] "
+COLORS[9]=" '+','color',[218/255,165/255,32/255] "
+COLORS[10]=" '+','color',[255/255,140/255,0] "
+j=0
+for ((ii=0;ii<(${NUM_TEST});ii++));do
+    echo "plot(x,cyc${ii},${COLORS[${ii}]},\"linestyle\",'-',\"linewidth\",2);">>${filename}
+done
+
+for ((ii=0;ii<${nb_sizes};ii++));do
+  for ((jj=${gap}+1;jj<${gap}+(${NUM_TEST});jj++));do
+    ((q= ${jj} + 1 + ${ii}*(${gap}+(${NUM_TEST}))))
+    ((r=(${ii})*(${gap}+(${NUM_TEST}))+${gap}+1))
+  done
+done
+
+echo "max_x = max(x)+0.25;">>${filename}
+echo ${LEGEND}>>${filename}
+echo "xx=[1 max_x max_x 1 1];">>${filename}
+echo "text(y_dist,max_y/2-num_div,\"${UNIT}\",\"rotation\",90)">>${filename}
+echo "yy=[0 0 max_y max_y 0];">>${filename}
+echo "plot(xx,yy,'k')">>${filename}
+echo "ylim([-num_div*0.5 max_y])">>${filename}
+echo "axis off" >>${filename}
+echo "xlim([0.5 max_x]);">>${filename}
+
+for ((ii=1;ii<=${nb_sizes};ii++));do
+  echo "text(x(${ii}),-num_div*0.3,\"${SIZE[ii-1]}\")">>${filename}
+done
+
+echo "box on" >>${filename}
+echo "plot(x_points,y_points,'+k')" >>${filename}
+echo "for i=1:num_y_ticks" >>${filename}
+echo "  text(0.75,y_points(i),mat2str(i*num_div));" >>${filename}
+#echo "  text(x_points(i)-0.1,y_points(i),\"potato\");" >>${filename}
+echo "endfor" >>${filename}
+echo "print ('-dpng','-color',\"${PLOT_NAME}.png\")">>${filename}
+echo "h = figure;">>${filename}
+echo "W = 12; H = 3;">>${filename}
+echo "set(h,'PaperUnits','inches')">>${filename}
+echo "set(h,'PaperOrientation','portrait');">>${filename}
+echo "set(h,'PaperSize',[H,W])">>${filename}
+echo "set(h,'PaperPosition',[0,0,W,H])">>${filename}
+echo "refresh">>${filename}
+
+octave ${filename} &>  /dev/null; display "${PLOT_NAME}.png" &
---
+++ script/bash/plot_benchmarks.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+if [ $# -lt 1 ]
+then
+    echo "Usage: `basename $0` PLOT_TYPE <program>..."
+    echo "PLOT_TYPE is either bar or graph"
+    echo "If program is -, use content of existing output.txt file"
+    echo "RUN_CMD may be defined to an arbitrary prefix command"
+    echo "RUN_ARGS may be defined to an arbitrary list of arguments"
+    exit 65
+fi
+
+# run all programs, filter undesired output
+# count number of experiments and sizes
+# validate number of sizes
+# compute legend
+
+prev_nb_sizes=0
+nb_sizes=0
+prev_nb_experiments=0
+nb_experiments=0
+LEGEND=""
+plot=$1
+shift
+#separators
+data_type_seperator="With" #how experiments with diferent data types are separated
+arch_separator="Arch"
+start_data="CTEST_FULL_OUTPUT|---|Benchmark" #number of lines printed before the experiment results
+
+#positions
+arch_pos=2 #position of architecture
+type_pos=4 #position of data type
+name_pos=1 #position of name
+
+if [ "$1" != "-" ]
+then
+  rm -f output.txt
+  for i in  "$@"
+  do
+    $RUN_CMD $i $RUN_ARGS | tee -a output.txt
+  done
+fi
+
+cat output.txt | grep -Ev "${start_data}" > res.txt #remove all the lines containing anything in $start_data
+nb_experiments=$(grep -c ${arch_separator} output.txt) #count the number of times $arch_separator appears - this is the number of experiments
+((nb_data_types=$(grep -c "${data_type_seperator}" res.txt)/${nb_experiments})) #the data types are printed for every experiment for every type so divide by the number of experiments
+
+# benchmark name is the longest prefix of all experiments without the last _
+grep -v ${arch_separator} res.txt > res2.txt #res2.txt contains the benchmark results and the data types
+grep -Ev "${data_type_seperator}|${arch_separator}" res.txt > res3.txt #res3.txt only contains the benchmark results
+prefix=""
+while read l
+do
+  p=$(echo "$l" | cut -d$'\t' -f1)
+  if [ "$prefix" == "" ]
+  then
+    prefix=$p
+  else
+    prefix=$(printf "%s\n%s\n" $prefix $p | sed -e 'N;s/^\(.*\).*\n\1.*$/\1/')
+  fi
+done < res3.txt
+
+BENCH_NAME=$(echo "$prefix" | sed -r 's/_[^_]*//')
+#bash array containing T for each experiment
+data_types=($(grep ${data_type_seperator} output.txt | awk -v temp=${type_pos} ' { print $temp } ' | tr "[|]" "\000"))
+
+#check that the sizes are correct
+sizes=($(grep -n ${data_type_seperator}  res2.txt | cut -f1 -d:)) #assuming we have more than one experiment, count the number of lines between each occurence of $data_type_separator.
+
+#types is the number of data types in the benchmarks. If types is zero, then the benchmark gave no informaion about data types.
+types=$(grep -c "${data_type_seperator}" res.txt)
+if [ ${types} == "0" ];then
+  data_types=""
+  ((nb_sizes=$(wc -l < res.txt)/${nb_experiments}))
+  nb_data_types=1
+else
+  ((nb_sizes=${sizes[1]}-${sizes[0]}))
+fi
+ii=2
+while ((ii<${nb_data_types}*${nb_experiments}));do
+  ((pos=$ii-1))
+  ((my_nb_sizes=${sizes[ii]}-${sizes[pos]}))
+  if [ $nb_sizes -ne $my_nb_sizes ]
+    then
+    echo "inconsistent number of sizes, has $my_nb_sizes, previous had $nb_sizes" >&2
+    exit 1
+  fi
+  ((ii++))
+done
+((nb_sizes--)) #$nb_sizes counts the line containing $arch_separator
+arch_lines=($(grep -n ${arch_separator}  res.txt | cut -f1 -d:)) #find the line numbers on which architecture is printed
+
+#the legend contains the benchmark name and the architecture type
+LEGEND="legend(\" $(sed -n 3p res.txt  | awk -v temp=${name_pos} '{ print $temp }' | tr "_" " " | sed "s/$BENCH_NAME//g") $(sed -n 1p res.txt | awk -v temp=${arch_pos} '{ print $temp }')\" "
+
+for ((ii=1;ii<${nb_experiments};ii++));do
+  ((pos=${arch_lines[ii]}+2))
+  LEGEND="${LEGEND} , \"$(sed  -n ${pos}p res.txt | awk -v temp=${name_pos} ' { print $temp }' | tr "_" " " | sed "s/$BENCH_NAME//g") $(sed -n ${arch_lines[ii]}p res.txt | awk -v temp=${arch_pos} '{ print $temp }')\" "
+done
+LEGEND="${LEGEND} ) "
+NUM_TEST=${nb_experiments}
+
+mv res3.txt res.txt
+
+for ((ii=0;ii<${nb_experiments};ii++));do
+    ((pos=${ii}*${nb_sizes}+1))
+    BENCH_NAME[ii]=$(sed -n ${pos}p res.txt | awk '{ print $1 }' | tr "_" "\n"| sed -n 1p)
+    PLOT_NAME[ii]="${BENCH_NAME[${ii}]}_${data_types[ii]}"
+    TITLE[ii]="title(\"${BENCH_NAME[${ii}]} ${data_types[ii]}\")"
+done
+
+# generate M-file
+if [ $plot == "bar" ]; then
+  for ((kk=0;kk<${nb_data_types};kk++));do
+    generate_bar_chart.sh $nb_data_types $NUM_TEST $nb_sizes "${LEGEND[@]}" "${TITLE[kk]}" "${PLOT_NAME[kk]}" $kk
+  done
+elif [ $plot == "graph" ]; then
+  for ((kk=0;kk<${nb_data_types};kk++));do
+    generate_graph.sh $nb_data_types $NUM_TEST $nb_sizes "${LEGEND[@]}" "${TITLE[kk]}" "${PLOT_NAME[kk]}" $kk
+  done
+fi
