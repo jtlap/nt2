c977acd9351b2d5a3022e96c1a7ff469c8f4f6a2
Mathias Gaunard <mathias@gaunard.com>
2014-02-06 14:32:05 +0100

    make scalar genmask branch-free for all cases

--- modules/boost/simd/base/include/boost/simd/bitwise/functions/scalar/genmaskc.hpp
+++ modules/boost/simd/base/include/boost/simd/bitwise/functions/scalar/genmaskc.hpp
@@ -11,9 +11,9 @@
 
 #include <boost/simd/bitwise/functions/genmaskc.hpp>
 #include <boost/simd/include/functions/scalar/unary_minus.hpp>
-#include <boost/simd/include/constants/allbits.hpp>
-#include <boost/simd/include/constants/zero.hpp>
+#include <boost/simd/include/functions/scalar/bitwise_cast.hpp>
 #include <boost/simd/sdk/meta/as_arithmetic.hpp>
+#include <boost/dispatch/meta/as_integer.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -22,20 +22,22 @@ namespace boost { namespace simd { namespace ext
                                    )
   {
     typedef typename meta::as_arithmetic<A0>::type result_type;
-    BOOST_SIMD_FUNCTOR_CALL(1)
+    BOOST_FORCEINLINE BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return a0 ?  Zero<result_type>() : Allbits<result_type>();
+      typedef typename dispatch::meta::as_integer<result_type>::type itype;
+      return bitwise_cast<result_type>(unary_minus(itype(!a0)));
     }
   };
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::genmaskc_, tag::cpu_, (A0)
-                                   , (scalar_< integer_<A0> >)
+                                   , (scalar_< arithmetic_<A0> >)
                                    )
   {
     typedef typename meta::as_arithmetic<A0>::type result_type;
-    BOOST_SIMD_FUNCTOR_CALL(1)
+    BOOST_FORCEINLINE BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return unary_minus(result_type(a0 == 0));
+      typedef typename dispatch::meta::as_integer<result_type>::type itype;
+      return bitwise_cast<result_type>(unary_minus(itype(a0 == 0)));
     }
   };
 } } }
