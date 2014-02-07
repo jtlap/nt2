10e9dfb2a6bb532c0e3daa6e6b860c482907f543
jtlap <jtlapreste@gmail.com>
2014-02-07 10:17:23 +0100

    suppressing int in exponential covers

--- modules/core/exponential/cover/simd/nthroot.cpp
+++ modules/core/exponential/cover/simd/nthroot.cpp
@@ -1,163 +1,48 @@
 //==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 exponential toolbox - nthroot/simd Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// cover test behavior of exponential components in simd mode
-//////////////////////////////////////////////////////////////////////////////
-/// created by jt the 08/12/2010
-///
 #include <nt2/exponential/include/functions/nthroot.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <nt2/include/functions/max.hpp>
-#include <nt2/include/functions/sqr.hpp>
-#include <nt2/include/functions/abs.hpp>
 
-#include <boost/type_traits/is_same.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/as_floating.hpp>
-#include <nt2/sdk/meta/as_signed.hpp>
-#include <nt2/sdk/meta/upgrade.hpp>
-#include <nt2/sdk/meta/downgrade.hpp>
-#include <nt2/sdk/meta/scalar_of.hpp>
-#include <boost/dispatch/meta/as_floating.hpp>
-#include <boost/type_traits/common_type.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/cover.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/module.hpp>
-
-#include <nt2/constant/constant.hpp>
 #include <nt2/sdk/meta/cardinal_of.hpp>
-#include <nt2/include/functions/splat.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
 #include <nt2/include/functions/aligned_load.hpp>
-#include <nt2/constant/constant.hpp>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <iostream>
 
-
-NT2_TEST_CASE_TPL ( nthroot_real__2_0,  NT2_SIMD_REAL_TYPES)
+NT2_TEST_CASE_TPL ( nthroot,  NT2_SIMD_REAL_TYPES)
 {
   using nt2::nthroot;
   using nt2::tag::nthroot_;
-  using nt2::aligned_load;
   using boost::simd::native;
   using nt2::meta::cardinal_of;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
+  using nt2::aligned_load;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>                  vT;
   typedef typename nt2::meta::as_integer<T>::type iT;
   typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<nthroot_(vT,ivT)>::type r_t;
-  typedef typename nt2::meta::call<nthroot_(T,iT)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
 
-  // random verifications
   static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(10));
     NT2_CREATE_BUF(tab_a1,iT, NR, iT(-10), iT(10));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
+    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<vT>::value)
       {
         vT a0 = aligned_load<vT>(&tab_a0[0],j);
         ivT a1 = aligned_load<ivT>(&tab_a1[0],j);
-        r_t v = nthroot(a0,a1);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
+        vT v = nthroot(a0,a1);
+        for(nt2::uint32_t i = 0; i< cardinal_of<vT>::value; i++)
         {
 
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::nthroot (a0[i],a1[i])), 1);
-          ulp0 = nt2::max(ulpd,ulp0);
+          NT2_TEST_ULP_EQUAL( v[i],T(nt2::nthroot (a0[i],a1[i])), 1);
         }
       }
-    std::cout << "max ulp found is: " << ulp0 << std::endl;
   }
 } // end of test for floating_
-
-NT2_TEST_CASE_TPL ( nthroot_sintgt_16__2_0,  NT2_SIMD_INT_CONVERT_TYPES)
-{
-  using nt2::nthroot;
-  using nt2::tag::nthroot_;
-  using nt2::aligned_load;
-  using boost::simd::native;
-  using nt2::meta::cardinal_of;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<nthroot_(vT,ivT)>::type r_t;
-  typedef typename nt2::meta::call<nthroot_(T,iT)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
-
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, -100, 100);
-    NT2_CREATE_BUF(tab_a1,iT, NR, -100, 100);
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
-      {
-        vT a0 = aligned_load<vT>(&tab_a0[0],j);
-        ivT a1 = aligned_load<ivT>(&tab_a1[0],j);
-        r_t v = nthroot(a0,a1);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
-        {
-
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::nthroot (a0[i],a1[i])), 1);
-          ulp0 = nt2::max(ulpd,ulp0);
-        }
-      }
-    std::cout << "max ulp found is: " << ulp0 << std::endl;
-  }
-} // end of test for sintgt_16_
-
-NT2_TEST_CASE_TPL ( nthroot_uintgt_16__2_0,  NT2_SIMD_UINT_CONVERT_TYPES)
-{
-  using nt2::nthroot;
-  using nt2::tag::nthroot_;
-  using nt2::aligned_load;
-  using boost::simd::native;
-  using nt2::meta::cardinal_of;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<nthroot_(vT,ivT)>::type r_t;
-  typedef typename nt2::meta::call<nthroot_(T,iT)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
-
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, 0, 100);
-    NT2_CREATE_BUF(tab_a1,iT, NR, 0, 100);
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
-      {
-        vT a0 = aligned_load<vT>(&tab_a0[0],j);
-        ivT a1 = aligned_load<ivT>(&tab_a1[0],j);
-        r_t v = nthroot(a0,a1);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
-        {
-
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::nthroot (a0[i],a1[i])), 1);
-          ulp0 = nt2::max(ulpd,ulp0);
-        }
-      }
-    std::cout << "max ulp found is: " << ulp0 << std::endl;
-  }
-} // end of test for uintgt_16_
--- modules/core/exponential/cover/simd/pow.cpp
+++ modules/core/exponential/cover/simd/pow.cpp
@@ -1,167 +1,45 @@
 //==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 exponential toolbox - pow/simd Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// cover test behavior of exponential components in simd mode
-//////////////////////////////////////////////////////////////////////////////
-/// created by jt the 08/12/2010
-///
 #include <nt2/exponential/include/functions/pow.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <nt2/include/functions/max.hpp>
-#include <nt2/include/functions/sqr.hpp>
-#include <nt2/include/functions/sqrt.hpp>
-extern "C" { long double cephes_powl(long double,long double); }
 
-#include <boost/type_traits/is_same.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/as_floating.hpp>
-#include <nt2/sdk/meta/as_signed.hpp>
-#include <nt2/sdk/meta/upgrade.hpp>
-#include <nt2/sdk/meta/downgrade.hpp>
-#include <nt2/sdk/meta/scalar_of.hpp>
-#include <boost/dispatch/meta/as_floating.hpp>
-#include <boost/type_traits/common_type.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/cover.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/module.hpp>
-
-#include <nt2/constant/constant.hpp>
 #include <nt2/sdk/meta/cardinal_of.hpp>
-#include <nt2/include/functions/splat.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
 #include <nt2/include/functions/aligned_load.hpp>
-
-extern "C" { long double cephes_powil(long double,int); }
-
-
+#include <iostream>
 
 NT2_TEST_CASE_TPL ( pow_real__2_0,  NT2_SIMD_REAL_TYPES)
 {
   using nt2::pow;
   using nt2::tag::pow_;
-  using nt2::aligned_load;
   using boost::simd::native;
   using nt2::meta::cardinal_of;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<pow_(vT,vT)>::type r_t;
-  typedef typename nt2::meta::call<pow_(T,T)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
+  using nt2::aligned_load;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>                  vT;
 
-  // random verifications
   static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(0), T(10));
     NT2_CREATE_BUF(tab_a1,T, NR, T(0), T(10));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
+    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<vT>::value)
       {
         vT a0 = aligned_load<vT>(&tab_a0[0],j);
         vT a1 = aligned_load<vT>(&tab_a1[0],j);
-        r_t v = pow(a0,a1);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
+        vT v = pow(a0,a1);
+        for(nt2::uint32_t i = 0; i< cardinal_of<vT>::value; i++)
         {
 
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::pow (a0[i],a1[i])), 12.5);
-          ulp0 = nt2::max(ulpd,ulp0);
+          NT2_TEST_ULP_EQUAL( v[i],T(nt2::pow (a0[i],a1[i])), 12.5);
         }
       }
-    std::cout << "max ulp found is: " << ulp0 << std::endl;
-  }
-} // end of test for floating_
-
-NT2_TEST_CASE_TPL ( pow_real__2_1,  NT2_SIMD_REAL_TYPES)
-{
-  using nt2::pow;
-  using nt2::tag::pow_;
-  using nt2::aligned_load;
-  using boost::simd::native;
-  using nt2::meta::cardinal_of;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<pow_(vT,ivT)>::type r_t;
-  typedef typename nt2::meta::call<pow_(T,iT)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
-
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
-    NT2_CREATE_BUF(tab_a1,iT, NR, T(-10), T(10));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
-      {
-        vT a0 = aligned_load<vT>(&tab_a0[0],j);
-        ivT a1 = aligned_load<ivT>(&tab_a1[0],j);
-        r_t v = pow(a0,a1);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
-        {
-
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::pow (a0[i],a1[i])), 2.5);
-          ulp0 = nt2::max(ulpd,ulp0);
-        }
-      }
-    std::cout << "max ulp found is: " << ulp0 << std::endl;
-  }
-} // end of test for floating_
-
-
-NT2_TEST_CASE_TPL ( powi_real__2_0,  NT2_SIMD_REAL_TYPES)
-{
-  using nt2::pow;
-  using nt2::tag::pow_;
-  using nt2::aligned_load;
-  using boost::simd::native;
-  using nt2::meta::cardinal_of;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<pow_(vT,iT)>::type r_t;
-  typedef typename nt2::meta::call<pow_(T,iT)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
-
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
-    NT2_CREATE_BUF(tab_a1,iT, NR, T(-10), T(10));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
-      {
-        vT a0 = aligned_load<vT>(&tab_a0[0],j);
-        iT a1 = tab_a1[j];
-        r_t v = pow(a0,a1);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
-        {
-
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::pow (a0[i],tab_a1[j])), 2.5);
-          ulp0 = nt2::max(ulpd,ulp0);
-        }
-      }
-    std::cout << "max ulp found is: " << ulp0 << std::endl;
   }
 } // end of test for floating_
