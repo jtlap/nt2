4e8efc4b6dd839effb679f805a4df2ffa400628b
alankelly <alankelly@live.com>
2014-02-12 11:36:25 +0100

    fix for issue 683 with unit test for splat modified to account for it and cleaned (joel)

--- modules/boost/simd/base/unit/operator/simd/splat.cpp
+++ modules/boost/simd/base/unit/operator/simd/splat.cpp
@@ -1,6 +1,7 @@
 //==============================================================================
 //         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2009 - 2014   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2014   MetaScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -29,8 +30,10 @@ NT2_TEST_CASE_TPL ( splat,  BOOST_SIMD_SIMD_TYPES)
   typedef typename boost::dispatch::meta::call<splat_(T, boost::dispatch::meta::as_<vT>)>::type r_t;
 
   r_t value = splat<r_t>(42);
+  r_t ref;
 
-  for(size_t i=0;i<cardinal_of<r_t>::value;++i) NT2_TEST_EQUAL(value[i], T(42) );
+  for(size_t i=0;i<cardinal_of<r_t>::value;++i) ref[i] = T(42);
+  NT2_TEST_EQUAL(value, ref );
 }
 
 NT2_TEST_CASE_TPL ( splat_id,  BOOST_SIMD_SIMD_TYPES)
@@ -38,7 +41,7 @@ NT2_TEST_CASE_TPL ( splat_id,  BOOST_SIMD_SIMD_TYPES)
   using boost::simd::splat;
   using boost::simd::tag::splat_;
   using boost::simd::native;
-  using boost::simd::meta::cardinal_of;
+
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
   typedef native<T,ext_t>                        vT;
   typedef typename boost::dispatch::meta::call<splat_(vT, boost::dispatch::meta::as_<vT>)>::type r_t;
@@ -46,5 +49,22 @@ NT2_TEST_CASE_TPL ( splat_id,  BOOST_SIMD_SIMD_TYPES)
   r_t base = splat<r_t>(42);
   r_t value = splat<r_t>(base);
 
-  for(size_t i=0;i<cardinal_of<r_t>::value;++i) NT2_TEST_EQUAL(value[i], base[i] );
+  NT2_TEST_EQUAL(value, base);
+}
+
+NT2_TEST_CASE_TPL ( far_too_big_splat,  BOOST_SIMD_SIMD_TYPES)
+{
+  using boost::simd::splat;
+  using boost::simd::tag::splat_;
+  using boost::simd::meta::vector_of;
+  using boost::simd::meta::cardinal_of;
+
+  typedef typename vector_of<T,BOOST_SIMD_BYTES*4>::type                        vT;
+  typedef typename boost::dispatch::meta::call<splat_(T, boost::dispatch::meta::as_<vT>)>::type r_t;
+
+  r_t ref, value = splat<r_t>(42);
+
+  for(size_t i=0;i<cardinal_of<r_t>::value;++i) ref[i] = T(42);
+
+  NT2_TEST_EQUAL(value, ref );
 }
--- modules/boost/simd/sdk/include/boost/simd/preprocessor/make.hpp
+++ modules/boost/simd/sdk/include/boost/simd/preprocessor/make.hpp
@@ -1,6 +1,6 @@
 //==============================================================================
-//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
-//         Copyright 2012 - 2013   MetaScale SAS
+//         Copyright 2009 - 2014   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2012 - 2014   MetaScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -10,6 +10,7 @@
 #define BOOST_SIMD_PREPROCESSOR_MAKE_HPP_INCLUDED
 
 #include <boost/simd/sdk/simd/preprocessor/repeat.hpp>
+#include <boost/simd/sdk/meta/iterate.hpp>
 #include <boost/preprocessor/repetition/enum.hpp>
 #include <boost/preprocessor/repetition/enum_binary_params.hpp>
 #include <boost/preprocessor/tuple/elem.hpp>
@@ -30,6 +31,11 @@ result_type eval( BOOST_PP_ENUM_BINARY_PARAMS ( BOOST_PP_TUPLE_ELEM(2,0,Args)  \
 }                                                                              \
 /**/
 
+
+#define BOOST_SIMD_PP_GENERIC_MAKE_ARGS(z,n,t) a ## n(p ## n)
+#define BOOST_SIMD_PP_GENERIC_MAKE_TYPES(z,n,t) const X ## n & a ##n;
+#define BOOST_SIMD_PP_GENERIC_MAKE_TPL(z,n,t) typename X ## n
+
 /*!
   @brief make-based implementation generator
 
@@ -46,6 +52,41 @@ BOOST_SIMD_PP_REPEAT_POWER_OF_2_FROM( 2                                        \
                                     , BOOST_SIMD_PP_DETAILS_MAKE               \
                                     , (Args,Function)                          \
                                     )                                          \
+                                                                               \
+template<typename T                                                            \
+        ,std::size_t N                                                         \
+        ,BOOST_PP_ENUM(Args,BOOST_SIMD_PP_GENERIC_MAKE_TPL,~)>                 \
+struct generic_make                                                            \
+{                                                                              \
+  generic_make(BOOST_PP_ENUM_BINARY_PARAMS(Args, const X,& p), T& t)           \
+             : that(t)                                                         \
+             , BOOST_PP_ENUM(Args,BOOST_SIMD_PP_GENERIC_MAKE_ARGS,~) {}        \
+                                                                               \
+  template<int I> BOOST_FORCEINLINE void operator()() const                    \
+  {                                                                            \
+    that[I] = Function(~,I,N);                                                 \
+  }                                                                            \
+                                                                               \
+  T&  that;                                                                    \
+  BOOST_PP_REPEAT(Args, BOOST_SIMD_PP_GENERIC_MAKE_TYPES, ~)                   \
+  private:                                                                     \
+  generic_make& operator=(generic_make const&);                                \
+};                                                                             \
+                                                                               \
+template<std::size_t N>                                                        \
+result_type eval( BOOST_PP_ENUM_BINARY_PARAMS ( Args                           \
+                                              , const A,& a                    \
+                                              )                                \
+                , boost::mpl::size_t<N> const&                                 \
+                ) const                                                        \
+{                                                                              \
+  result_type that;                                                            \
+  meta::iterate<N>( generic_make< result_type                                  \
+                                 , N                                           \
+                                 , BOOST_PP_ENUM_PARAMS(Args,A)                \
+                                 >(BOOST_PP_ENUM_PARAMS(Args,a),that) );       \
+  return that;                                                                 \
+}                                                                              \
 /**/
 
 #include <boost/simd/include/functions/simd/make.hpp>
