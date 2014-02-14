1d6ab0f1b879bf501f3dbaa402be59678820007c
Mathias Gaunard <mathias@gaunard.com>
2014-02-14 18:10:25 +0100

    add release making scripts

---
+++ script/bash/do_release/do_release.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#NT2_SOURCE_ROOT=../../..
+BUILDNAME=Linux-x86_64
+BLACKLISTED="arch.cuda;arch.opencl;arch.arm;arch.x86;arch.mic;matlab;extern"
+
+VERSION=$(git -C "$NT2_SOURCE_ROOT" describe --tags)
+
+rm -rf install nt2 boost-simd source *.tgz
+mkdir -p install/nt2
+mkdir -p install/boost-simd
+
+mkdir nt2 && cd nt2
+cmake $NT2_SOURCE_ROOT -G Ninja -DCMAKE_INSTALL_PREFIX=../install/nt2 -DNT2_INSTALL_SHARE_DIR:STRING=. -DNT2_MODULES_BLACKLIST="$BLACKLISTED"
+ninja
+ninja -j1 doc && ninja -j1 doc
+ninja install
+cd ..
+cd install
+echo $VERSION > nt2/tagname
+tar -zcf ../nt2-$VERSION-$BUILDNAME.tgz nt2
+cd ..
+
+mkdir boost-simd && cd boost-simd
+cmake $NT2_SOURCE_ROOT -G Ninja -DCMAKE_INSTALL_PREFIX=../install/boost-simd -DNT2_INSTALL_SHARE_DIR:STRING=. -DNT2_MODULES="boost.simd" -DNT2_DOC_MAIN="boost.simd_doc" -DNT2_MODULES_BLACKLIST="$BLACKLISTED"
+ninja
+ninja -j1 doc && ninja -j1 doc
+ninja install
+cd ..
+cd install
+tar -zcf ../boost-simd-$VERSION.tgz boost-simd
+cd ..
+
+mkdir -p source
+cp -r $NT2_SOURCE_ROOT source/nt2
+
+OIFS=$IFS
+IFS=';'
+for i in $BLACKLISTED
+do
+  rm -rf "source/nt2/modules/$(echo "$i" | tr '.' '/')"
+done
+IFS=$OIFS
+
+rm -rf source/nt2/.git
+cd source
+echo $VERSION > nt2/tagname
+tar -zcf ../nt2-$VERSION-source.tgz nt2
+cd ..
---
+++ script/bash/do_release/do_release_win32.bat
@@ -0,0 +1,24 @@
+::set NT2_SOURCE_ROOT=..\..\..
+set BUILDNAME=Windows-i686-vc100
+set BLACKLISTED="arch.cuda;arch.opencl;arch.arm;arch.x86;arch.mic;matlab;extern"
+
+del VERSION.txt
+git -C "%NT2_SOURCE_ROOT%" describe --tags > VERSION.txt
+set /p VERSION=<VERSION.txt
+
+rmdir /S /Q install nt2_msvc
+mkdir install\nt2
+
+mkdir nt2_msvc && cd nt2_msvc
+cmake "%NT2_SOURCE_ROOT%" -G Ninja -DCMAKE_INSTALL_PREFIX=../install/nt2 -DNT2_MODULES_BLACKLIST="%BLACKLISTED%"
+ninja
+xcopy /S /I ..\nt2\doc\html doc\html
+ninja package
+copy nt2-%VERSION%.exe ..\nt2-%VERSION%-%BUILDNAME%.exe
+ninja install
+cd ..
+cd install
+echo %VERSION% > nt2/tagname
+del ..\nt2-%VERSION%-%BUILDNAME%.zip
+"C:\Program Files\7-Zip\7z.exe" a ..\nt2-%VERSION%-%BUILDNAME%.zip nt2
+cd ..
