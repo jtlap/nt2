6a3c6a0fac7c74c3f153c1fbfcbcb1e15b7a80cb
jtlap <jtlapreste@gmail.com>
2014-02-19 13:46:41 +0100

    suppressing int calls from exponential benches

--- modules/core/exponential/bench/simd/log.cpp
+++ modules/core/exponential/bench/simd/log.cpp
@@ -1,31 +1,33 @@
 //==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #include <nt2/exponential/include/functions/log.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
+
 #include <nt2/sdk/bench/benchmark.hpp>
 #include <nt2/sdk/bench/timing.hpp>
-#include <boost/dispatch/meta/as_integer.hpp>
-#include <cmath>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <boost/simd/sdk/simd/meta/vector_of.hpp>
+
+
 typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+
 using nt2::tag::log_;
 
 #define RS(T,V1,V2) (T, (V1) ,(V2))
 
+
 namespace n1 {
   typedef float T;
-  typedef boost::dispatch::meta::as_integer<T>::type iT;
   typedef boost::simd::meta::vector_of<T, BOOST_SIMD_BYTES/sizeof(T)>::type vT;
   NT2_TIMING(log_,(RS(vT,T(0.1),T(10))))
 }
 namespace n2 {
   typedef double T;
-  typedef boost::dispatch::meta::as_integer<T>::type iT;
   typedef boost::simd::meta::vector_of<T, BOOST_SIMD_BYTES/sizeof(T)>::type vT;
   NT2_TIMING(log_,(RS(vT,T(0.1),T(10))))
 }
--- modules/core/exponential/bench/simd/nthroot.cpp
+++ modules/core/exponential/bench/simd/nthroot.cpp
@@ -1,75 +1,29 @@
 //==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_BENCH_MODULE "nt2 exponential toolbox - nthroot/simd Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// timing Test behavior of exponential components in simd mode
-//////////////////////////////////////////////////////////////////////////////
 #include <nt2/exponential/include/functions/nthroot.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
+
 #include <nt2/sdk/bench/benchmark.hpp>
 #include <nt2/sdk/bench/timing.hpp>
-#include <boost/dispatch/meta/as_integer.hpp>
-#include <cmath>
-typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
+#include <boost/simd/sdk/simd/native.hpp>
+#include <boost/simd/sdk/simd/meta/vector_of.hpp>
 
-//////////////////////////////////////////////////////////////////////////////
-// simd runtime benchmark for functor<nthroot_> from exponential
-//////////////////////////////////////////////////////////////////////////////
 using nt2::tag::nthroot_;
 
-//////////////////////////////////////////////////////////////////////////////
-// range macro
-//////////////////////////////////////////////////////////////////////////////
 #define RS(T,V1,V2) (T, (V1) ,(V2))
+
 
 namespace n1 {
   typedef float T;
-  typedef boost::dispatch::meta::as_integer<T>::type iT;
   typedef boost::simd::meta::vector_of<T, BOOST_SIMD_BYTES/sizeof(T)>::type vT;
-  typedef boost::simd::native<iT,ext_t> viT;
-  NT2_TIMING(nthroot_,(RS(vT,T(0),T(10)))(RS(viT,iT(-10),iT(10))))
 }
 namespace n2 {
   typedef double T;
-  typedef boost::dispatch::meta::as_integer<T>::type iT;
   typedef boost::simd::meta::vector_of<T, BOOST_SIMD_BYTES/sizeof(T)>::type vT;
-  typedef boost::simd::native<iT,ext_t> viT;
-  NT2_TIMING(nthroot_,(RS(vT,T(0),T(10)))(RS(viT,iT(-10),iT(10))))
 }
-namespace n3 {
-  typedef nt2::int32_t T;
-  typedef boost::dispatch::meta::as_integer<T>::type iT;
-  typedef boost::simd::meta::vector_of<T, BOOST_SIMD_BYTES/sizeof(T)>::type vT;
-  typedef boost::simd::native<iT,ext_t> viT;
-  NT2_TIMING(nthroot_,(RS(vT,-100,100))(RS(viT,-100,100)))
-}
-namespace n4 {
-  typedef nt2::int64_t T;
-  typedef boost::dispatch::meta::as_integer<T>::type iT;
-  typedef boost::simd::meta::vector_of<T, BOOST_SIMD_BYTES/sizeof(T)>::type vT;
-  typedef boost::simd::native<iT,ext_t> viT;
-  NT2_TIMING(nthroot_,(RS(vT,-100,100))(RS(viT,-100,100)))
-}
-namespace n5 {
-  typedef nt2::uint32_t T;
-  typedef boost::dispatch::meta::as_integer<T>::type iT;
-  typedef boost::simd::meta::vector_of<T, BOOST_SIMD_BYTES/sizeof(T)>::type vT;
-  typedef boost::simd::native<iT,ext_t> viT;
-  NT2_TIMING(nthroot_,(RS(vT,0,100))(RS(viT,0,100)))
-}
-namespace n6 {
-  typedef nt2::uint64_t T;
-  typedef boost::dispatch::meta::as_integer<T>::type iT;
-  typedef boost::simd::meta::vector_of<T, BOOST_SIMD_BYTES/sizeof(T)>::type vT;
-  typedef boost::simd::native<iT,ext_t> viT;
-  NT2_TIMING(nthroot_,(RS(vT,0,100))(RS(viT,0,100)))
-}
-
 #undef RS
