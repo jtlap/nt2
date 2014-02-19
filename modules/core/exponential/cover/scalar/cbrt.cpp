ac562a22326c16cd5f79c357ded8e11899875b16
jtlap <jtlapreste@gmail.com>
2014-02-19 13:47:22 +0100

    suppressing int calls from exponential covers

--- modules/core/exponential/cover/scalar/cbrt.cpp
+++ modules/core/exponential/cover/scalar/cbrt.cpp
@@ -1,36 +1,27 @@
 //==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 exponential toolbox - cbrt/scalar Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// cover test behavior of exponential components in scalar mode
-//////////////////////////////////////////////////////////////////////////////
-/// created by jt the 08/12/2010
-///
 #include <nt2/exponential/include/functions/cbrt.hpp>
-#include <nt2/include/functions/sqr.hpp>
-extern "C" { long double cephes_cbrtl(long double);}
 
-#include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/tests/cover.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/module.hpp>
 
-#include <nt2/constant/constant.hpp>
+#include <iostream>
 
+#include <nt2/include/functions/sqr.hpp>
+extern "C" { long double cephes_cbrtl(long double);}
+extern "C" { long double cephes_cbrtl(long double);}
 
-NT2_TEST_CASE_TPL ( cbrt_real,  NT2_REAL_TYPES)
+NT2_TEST_CASE_TPL ( cbrt,  NT2_REAL_TYPES)
 {
-
   using nt2::cbrt;
   using nt2::tag::cbrt_;
-
-  // random verifications
   static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
@@ -41,8 +32,7 @@ NT2_TEST_CASE_TPL ( cbrt_real,  NT2_REAL_TYPES)
                   << "  a0 = "<< (a0 = tab_a0[j])
                   << std::endl;
         NT2_TEST_ULP_EQUAL( nt2::cbrt(a0),T(::cephes_cbrtl(a0)),2);
-        NT2_TEST_ULP_EQUAL( nt2::sqr(nt2::cbrt(a0))*nt2::cbrt(a0),a0,3);
+        NT2_TEST_ULP_EQUAL( nt2::sqr(nt2::cbrt(a0))*nt2::cbrt(a0),T(a0),3);
      }
    }
-} // end of test for floating_
-
+}
--- modules/core/exponential/cover/simd/cbrt.cpp
+++ modules/core/exponential/cover/simd/cbrt.cpp
@@ -1,72 +1,43 @@
 //==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 exponential toolbox - cbrt/simd Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// cover test behavior of exponential components in simd mode
-//////////////////////////////////////////////////////////////////////////////
-/// created by jt the 08/12/2010
-///
 #include <nt2/exponential/include/functions/cbrt.hpp>
-#include <boost/simd/sdk/simd/native.hpp>
-#include <nt2/include/functions/max.hpp>
-#include <nt2/include/functions/sqr.hpp>
-extern "C" { long double cephes_cbrtl(long double);}
 
-#include <boost/type_traits/is_same.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/as_floating.hpp>
-#include <nt2/sdk/meta/as_signed.hpp>
-#include <nt2/sdk/meta/upgrade.hpp>
-#include <nt2/sdk/meta/downgrade.hpp>
-#include <nt2/sdk/meta/scalar_of.hpp>
-#include <boost/dispatch/meta/as_floating.hpp>
-#include <boost/type_traits/common_type.hpp>
-#include <nt2/sdk/unit/tests.hpp>
+#include <nt2/sdk/unit/tests/cover.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/module.hpp>
-
-#include <nt2/constant/constant.hpp>
 #include <nt2/sdk/meta/cardinal_of.hpp>
-#include <nt2/include/functions/splat.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
 #include <nt2/include/functions/aligned_load.hpp>
+#include <iostream>
 
-NT2_TEST_CASE_TPL ( cbrt_real__1_0,  NT2_SIMD_REAL_TYPES)
+NT2_TEST_CASE_TPL ( cbrt,  NT2_SIMD_REAL_TYPES)
 {
   using nt2::cbrt;
   using nt2::tag::cbrt_;
   using boost::simd::native;
-  using boost::simd::aligned_load;
   using nt2::meta::cardinal_of;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
-  typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<cbrt_(vT)>::type r_t;
-  typedef typename nt2::meta::call<cbrt_(T)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
 
-  // random verifications
+  using nt2::aligned_load;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>                  vT;
+
   static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
   {
     NT2_CREATE_BUF(tab_a0,T, NR, T(-10), T(10));
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
+    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<vT>::value)
       {
         vT a0 = aligned_load<vT>(&tab_a0[0],j);
-        r_t v = cbrt(a0);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
+        vT v = cbrt(a0);
+        for(nt2::uint32_t i = 0; i< cardinal_of<vT>::value; i++)
         {
-
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::cbrt (a0[i])), 2.5);
+          NT2_TEST_ULP_EQUAL( v[i],nt2::cbrt (a0[i]), 2.5);
         }
       }
   }
-} // end of test for floating_
+}
