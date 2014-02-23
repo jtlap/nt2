31811069e50657129eee955a2da487ac97af9535
Joel Falcou <joel.falcou@lri.fr>
2014-02-23 19:03:22 +0100

    Altivec interleave_even

--- modules/boost/simd/swar/unit/simd/interleave_even.cpp
+++ modules/boost/simd/swar/unit/simd/interleave_even.cpp
@@ -6,11 +6,10 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 boost.simd.swar toolbox - interleave_even"
-
-#include <boost/simd/sdk/simd/native.hpp>
-#include <boost/simd/sdk/meta/cardinal_of.hpp>
 #include <boost/simd/include/functions/interleave_even.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
+#include <boost/simd/sdk/meta/cardinal_of.hpp>
 #include <nt2/sdk/unit/tests.hpp>
 #include <nt2/sdk/unit/module.hpp>
 
@@ -23,18 +22,21 @@ NT2_TEST_CASE_TPL(interleave_even, BOOST_SIMD_SIMD_TYPES)
   typedef native<T,ext_t>                      vT;
 
   const std::size_t card = cardinal_of<vT>::value;
-  vT a,b,c;
+  vT a,b,c,ref;
 
-  for(std::size_t i=1; i<=card; ++i)
+  for(std::size_t i=0; i<card; ++i)
   {
-    a[i-1]=T(i);
-    b[i-1]=T(i*10);
+    a[i]=T(i+1);
+    b[i]=T((i+1)*10);
+  }
+
+  for(std::size_t i=0;i<card;i+= 2)
+  {
+    ref[i]   = a[i];
+    ref[i+1] = b[i];
   }
 
   c = boost::simd::interleave_even(a,b);
 
-  for(std::size_t i=0; i<card; ++i)
-  {
-    NT2_TEST_EQUAL(c[i],(i%2==0)?a[i]:b[i-1]);
-  }
+  NT2_TEST_EQUAL(c,ref);
 }
