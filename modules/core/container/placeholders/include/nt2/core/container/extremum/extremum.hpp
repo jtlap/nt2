f44a08b7018dea7e79f3228a95f7cf39d4a5df1c
Joel Falcou <joel.falcou@lri.fr>
2014-03-01 19:35:51 +0100

    Move fix_index to core.sdk
    Only expression use fix_index so it has to be there.

--- modules/core/container/placeholders/include/nt2/core/container/extremum/extremum.hpp
+++ modules/core/container/placeholders/include/nt2/core/container/extremum/extremum.hpp
@@ -9,7 +9,7 @@
 #ifndef NT2_CORE_CONTAINER_EXTREMUM_EXTREMUM_HPP_INCLUDED
 #define NT2_CORE_CONTAINER_EXTREMUM_EXTREMUM_HPP_INCLUDED
 
-#include <nt2/core/utility/fix_index.hpp>
+#include <nt2/sdk/meta/fix_index.hpp>
 
 namespace nt2 { namespace container
 {
