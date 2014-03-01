afbfd826b83fee3becb15a4f35efe56df7552d5f
jtlap <jtlapreste@gmail.com>
2014-03-01 17:37:26 +0100

    updating bessel covers

--- modules/core/bessel/cover/simd/jni.cpp
+++ modules/core/bessel/cover/simd/jni.cpp
@@ -1,81 +1,49 @@
 //==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2003 - 2014   LASMEA UMR 6602 CNRS/UBP
+//         Copyright 2009 - 2014   LRI    UMR 8623 CNRS/Univ Paris Sud XI
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#define NT2_UNIT_MODULE "nt2 bessel toolbox - jni/simd Mode"
-
-//////////////////////////////////////////////////////////////////////////////
-// cover test behavior of bessel components in simd mode
-//////////////////////////////////////////////////////////////////////////////
-/// created  by jt the 16/02/2011
-///
+// cover for functor jni in simd mode
 #include <nt2/bessel/include/functions/jni.hpp>
+
+#include <boost/simd/sdk/simd/io.hpp>
 #include <boost/simd/sdk/simd/native.hpp>
-#include <nt2/include/functions/max.hpp>
+#include <cmath>
+#include <iostream>
+#include <nt2/sdk/meta/as_integer.hpp>
+#include <nt2/sdk/unit/args.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/cover.hpp>
+#include <vector>
 extern "C" {long double cephes_jnl(int,long double);}
 
-#include <boost/type_traits/is_same.hpp>
-#include <nt2/sdk/functor/meta/call.hpp>
-#include <nt2/sdk/meta/as_integer.hpp>
-#include <nt2/sdk/meta/as_floating.hpp>
-#include <nt2/sdk/meta/as_signed.hpp>
-#include <nt2/sdk/meta/upgrade.hpp>
-#include <nt2/sdk/meta/downgrade.hpp>
-#include <nt2/sdk/meta/scalar_of.hpp>
-#include <boost/dispatch/meta/as_floating.hpp>
-#include <boost/type_traits/common_type.hpp>
-#include <nt2/sdk/unit/tests.hpp>
-#include <nt2/sdk/unit/module.hpp>
-
-#include <nt2/constant/constant.hpp>
-#include <nt2/sdk/meta/cardinal_of.hpp>
-#include <nt2/include/functions/splat.hpp>
-
-#include <nt2/include/functions/aligned_load.hpp>
-#include <nt2/constant/constant.hpp>
-
-
-NT2_TEST_CASE_TPL ( jni_real__2_0,  NT2_SIMD_REAL_TYPES)
+NT2_TEST_CASE_TPL(jni_0,  NT2_SIMD_REAL_TYPES)
 {
-  using nt2::jni;
-  using nt2::tag::jni_;
-  using nt2::aligned_load;
   using boost::simd::native;
-  using nt2::meta::cardinal_of;
-  typedef NT2_SIMD_DEFAULT_EXTENSION  ext_t;
-  typedef typename nt2::meta::upgrade<T>::type   u_t;
-  typedef native<T,ext_t>                        n_t;
-  typedef n_t                                     vT;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>                  vT;
+
+  using nt2::unit::args;
+  const std::size_t NR = args("samples", NT2_NB_RANDOM_TEST);
+  const double ulpd = args("ulpd",  2000);
+
   typedef typename nt2::meta::as_integer<T>::type iT;
-  typedef native<iT,ext_t>                       ivT;
-  typedef typename nt2::meta::call<jni_(iT,vT)>::type r_t;
-  typedef typename nt2::meta::call<jni_(iT,T)>::type sr_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  double ulpd;
-  ulpd=0.0;
+  const iT min0 = args("min0", 1);
+  const iT max0 = args("max0", 10);
+  std::cout << "Argument samples randomly chosen in [" << min0 << ",  " << max0 << "]" << std::endl;
+  NT2_CREATE_BUF(a0,iT, NR,min0, max0);
+  const T min1 = args("min1", T(0));
+  const T max1 = args("max1", T(10));
+  std::cout << "Argument samples randomly chosen in [" << min1 << ",  " << max1 << "]" << std::endl;
+  NT2_CREATE_BUF(a1,T, NR,min1, max1);
 
-  // random verifications
-  static const nt2::uint32_t NR = NT2_NB_RANDOM_TEST;
-  {
-    NT2_CREATE_BUF(tab_a0,iT, NR, 1, 10);
-    NT2_CREATE_BUF(tab_a1,T, NR, T(0), T(10));
-    double ulp0, ulpd ; ulpd=ulp0=0.0;
-    for(nt2::uint32_t j = 0; j < NR;j+=cardinal_of<n_t>::value)
-      {
-        iT a0 = tab_a0[j];
-        vT a1 = aligned_load<vT>(&tab_a1[0],j);
-        r_t v = jni(a0,a1);
-        for(nt2::uint32_t i = 0; i< cardinal_of<n_t>::value; i++)
-        {
+  std::vector<T> ref(NR);
+  for(std::size_t i=0; i!=NR; ++i)
+    ref[i] = nt2::jni(a0[i], a1[i]);
 
-          NT2_TEST_ULP_EQUAL( v[i],ssr_t(nt2::jni (tab_a0[j],a1[i])), 2000);
-          ulp0 = nt2::max(ulpd,ulp0);
-        }
-      }
-    std::cout << "max ulp found is: " << ulp0 << std::endl;
-  }
-} // end of test for floating_
+  NT2_COVER_ULP_EQUAL(nt2::tag::jni_, ((iT, a0))((vT, a1)), ref, ulpd);
+
+}
