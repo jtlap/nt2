a5fed822ef5c2d6846eac5d8d4d095656dd62ec7
jtlap <jtlapreste@gmail.com>
2014-03-06 14:06:55 +0100

    adding exhaustive tests in euler for spence (also erf and erfc)

---
+++ modules/core/euler/exhaustive/simd/erfc.cpp
@@ -0,0 +1,46 @@
+//==============================================================================
+//         Copyright 2009 - 2013 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2013        MetaScale SAS
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <boost/simd/sdk/simd/native.hpp>
+#include <nt2/include/functions/erfc.hpp>
+#include <nt2/include/constants/minlog.hpp>
+#include <nt2/include/constants/maxlog.hpp>
+
+#include <nt2/sdk/unit/exhaustive.hpp>
+
+#define NT2_ASSERTS_AS_TRAP
+#include <nt2/sdk/error/assert_as_trap.hpp>
+
+#include <cmath>
+#include <cstdlib>
+
+struct raw_erfc
+{
+  float operator()(float x) const
+  {
+    return float(::erfc(double(x)));
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  typedef BOOST_SIMD_DEFAULT_EXTENSION             ext_t;
+  typedef boost::simd::native<float,ext_t>           n_t;
+  float mini = -6.0f;
+  float maxi = 6.0f;
+  if(argc >= 2) mini = std::atof(argv[1]);
+  if(argc >= 3) maxi = std::atof(argv[2]);
+
+  nt2::exhaustive_test<n_t> ( mini
+                            , maxi
+                            , nt2::functor<nt2::tag::erfc_>()
+                            , raw_erfc()
+                            );
+
+  return 0;
+}
