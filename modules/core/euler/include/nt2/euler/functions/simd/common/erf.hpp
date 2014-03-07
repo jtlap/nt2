370c6b6af0528624870fad8d3059522e4386a1f3
jtlap <jtlapreste@gmail.com>
2014-03-07 13:39:53 +0100

    erf and erfc are co-improved and a join details kernel file is added

--- modules/core/euler/include/nt2/euler/functions/simd/common/erf.hpp
+++ modules/core/euler/include/nt2/euler/functions/simd/common/erf.hpp
@@ -10,106 +10,46 @@
 #define NT2_EULER_FUNCTIONS_SIMD_COMMON_ERF_HPP_INCLUDED
 
 #include <nt2/euler/functions/erf.hpp>
-#include <nt2/include/functions/simd/tofloat.hpp>
-#include <nt2/include/functions/simd/sqr.hpp>
+#include <boost/simd/sdk/config.hpp>
+#include <nt2/euler/functions/details/erf_kernel.hpp>
+#include <nt2/include/constants/twothird.hpp>
+#include <nt2/include/constants/zero.hpp>
 #include <nt2/include/functions/simd/abs.hpp>
-#include <nt2/include/functions/simd/splat.hpp>
-#include <nt2/include/functions/simd/is_less.hpp>
-#include <nt2/include/functions/simd/inbtrue.hpp>
-#include <nt2/include/functions/simd/polevl.hpp>
-#include <nt2/include/functions/simd/multiplies.hpp>
 #include <nt2/include/functions/simd/divides.hpp>
-#include <nt2/include/functions/simd/oneminus.hpp>
-#include <nt2/include/functions/simd/logical_andnot.hpp>
-#include <nt2/include/functions/simd/unary_minus.hpp>
 #include <nt2/include/functions/simd/exp.hpp>
 #include <nt2/include/functions/simd/if_else.hpp>
-#include <nt2/include/functions/simd/is_ltz.hpp>
-#include <nt2/include/functions/simd/sign.hpp>
+#include <nt2/include/functions/simd/inbtrue.hpp>
+#include <nt2/include/functions/simd/is_greater_equal.hpp>
 #include <nt2/include/functions/simd/is_inf.hpp>
-#include <nt2/include/constants/zero.hpp>
-#include <nt2/include/constants/pi.hpp>
-#include <nt2/include/constants/three.hpp>
-#include <nt2/include/constants/four.hpp>
-#include <nt2/include/constants/eight.hpp>
-#include <nt2/sdk/meta/cardinal_of.hpp>
+#include <nt2/include/functions/simd/is_less.hpp>
+#include <nt2/include/functions/simd/is_ltz.hpp>
+#include <nt2/include/functions/simd/logical_andnot.hpp>
+#include <nt2/include/functions/simd/minus.hpp>
+#include <nt2/include/functions/simd/multiplies.hpp>
+#include <nt2/include/functions/simd/negif.hpp>
+#include <nt2/include/functions/simd/oneminus.hpp>
+#include <nt2/include/functions/simd/oneplus.hpp>
+#include <nt2/include/functions/simd/plus.hpp>
+#include <nt2/include/functions/simd/sign.hpp>
+#include <nt2/include/functions/simd/splat.hpp>
+#include <nt2/include/functions/simd/sqr.hpp>
+#include <nt2/include/functions/simd/unary_minus.hpp>
 #include <nt2/sdk/meta/as_floating.hpp>
+#include <nt2/sdk/meta/as_logical.hpp>
+#include <nt2/sdk/meta/cardinal_of.hpp>
+#include <nt2/sdk/meta/scalar_of.hpp>
 
 namespace nt2 { namespace ext
 {
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::erf_, tag::cpu_
                             , (A0)(X)
-                            , ((simd_<arithmetic_<A0>,X>))
-                            )
-  {
-    typedef typename meta::as_floating<A0>::type result_type;
-    NT2_FUNCTOR_CALL(1)
-    {
-      return nt2::erf(nt2::tofloat(a0));
-   }
-  };
-
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::erf_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<floating_<A0>,X>))
+                            , ((simd_<double_<A0>,X>))
                             )
   {
     typedef typename meta::as_floating<A0>::type result_type;
     typedef typename meta::scalar_of<A0>::type sA0;
     NT2_FUNCTOR_CALL(1)
     {
-      static const boost::array<sA0, 5 > erf0_P4 = {{
-          sA0(6.49254556481904E-05),
-          sA0(1.20339380863079E-03),
-          sA0(4.03259488531795E-02),
-          sA0(0.135894887627278   ),
-          sA0(1.12837916709551    )
-        }};
-      static const boost::array<sA0, 5 > erf0_Q4 = {{
-          sA0(3.64915280629351E-04),
-          sA0(8.49717371168693E-03),
-          sA0(8.69936222615386E-02),
-          sA0(0.453767041780003   ),
-          sA0(1                   )
-        }};
-
-      static const boost::array<sA0, 7 > erfc1_P5 = {{
-          sA0(0                   ),
-          sA0(7.06940843763253E-03),
-          sA0(7.14193832506776E-02),
-          sA0(0.331899559578213   ),
-          sA0(0.878115804155882   ),
-          sA0(1.33154163936765    ),
-          sA0(0.999999992049799   )
-        }};
-      static const boost::array<sA0, 7 > erfc1_Q5 = {{
-          sA0(1.25304936549413E-02),
-          sA0(0.126579413030178   ),
-          sA0(0.594651311286482   ),
-          sA0(1.61876655543871    ),
-          sA0(2.65383972869776    ),
-          sA0(2.45992070144246    ),
-          sA0(1                   )
-        }};
-
-      static const boost::array<sA0, 7 > erfc2_P5 = {{
-          sA0(0                   ),
-          sA0(2.25716982919218E-02),
-          sA0(0.157289620742839   ),
-          sA0(0.581528574177741   ),
-          sA0(1.26739901455873    ),
-          sA0(1.62356584489367    ),
-          sA0(0.99992114009714    )
-        }};
-      static const boost::array<sA0, 7 > erfc2_Q5 = {{
-          sA0(4.00072964526861E-02),
-          sA0(0.278788439273629   ),
-          sA0(1.05074004614827    ),
-          sA0(2.38574194785344    ),
-          sA0(3.37367334657285    ),
-          sA0(2.75143870676376    ),
-          sA0(1                   )
-        }};
       typedef typename meta::as_logical<A0>::type bA0;
 
       A0 x =  nt2::abs(a0);
@@ -122,8 +62,8 @@ namespace nt2 { namespace ext
 
       if(nb)
       {
-        r1 = a0*nt2::polevl( xx, erf0_P4)/nt2::polevl( xx, erf0_Q4 );
-        if (nb >= meta::cardinal_of<A0>::value) return r1;
+        r1 = a0*details::erf_kernel<A0>::erf1(xx);
+      if (nb >= meta::cardinal_of<A0>::value) return r1;
       }
       bA0 test2 = nt2::lt(x, lim2);
       bA0 test3 = nt2::logical_andnot(test2, test1);
@@ -132,15 +72,54 @@ namespace nt2 { namespace ext
       A0 ex = nt2::exp(-xx);
       if(nb1)
       {
-        A0 z = nt2::oneminus(ex*nt2::polevl(x, erfc1_P5)/nt2::polevl( x, erfc1_Q5));
+        A0 z = oneminus(exp(-xx)*details::erf_kernel<A0>::erfc2(x));
         A0 r2 = nt2::negif(is_ltz(a0), z);
         r1 = nt2::if_else(test1, r1, r2);
         nb+= nb1;
         if (nb >= meta::cardinal_of<A0>::value) return r1;
       }
-      A0 z = nt2::negif(nt2::is_ltz(a0), nt2::oneminus(ex*polevl(x, erfc2_P5)/nt2::polevl( x, erfc2_Q5)));
-      return nt2::if_else(nt2::is_inf(a0), nt2::sign(a0), nt2::if_else(test2, r1, z));
+      A0 z = nt2::negif(
+        nt2::is_ltz(a0),
+        nt2::oneminus(exp(-xx)*details::erf_kernel<A0>::erfc3(x)));
+      #ifndef BOOST_SIMD_NO_INFINITIES
+      z = nt2::if_else(nt2::is_inf(a0), nt2::sign(a0), z);
+      #endif
+      return nt2::if_else(nt2::is_inf(a0), nt2::sign(a0),
+                          nt2::if_else(test2, r1, z));
     }
   };
+
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::erf_, tag::cpu_
+                            , (A0)(X)
+                            , ((simd_<single_<A0>,X>))
+                            )
+  {
+    typedef typename meta::as_floating<A0>::type result_type;
+    typedef typename meta::scalar_of<A0>::type sA0;
+    NT2_FUNCTOR_CALL(1)
+    {
+      typedef typename meta::as_logical<A0>::type bA0;
+      A0 x =  nt2::abs(a0);
+      A0 r1 = nt2::Zero<A0>();
+      bA0 test1 = nt2::lt(x, Twothird<A0>());
+      std::size_t nb = 0;
+      if ((nb = (nt2::inbtrue(test1) > 0)))
+      {
+        r1 =  a0*details::erf_kernel<A0>::erf1(a0, sqr(x));
+        if (nb >= meta::cardinal_of<A0>::value)
+          return r1;
+      }
+      A0 z = x/oneplus(x);
+      z-= nt2::splat<A0>(0.4);
+      A0 r2 =   oneminus(exp(-sqr(x))*details::erf_kernel<A0>::erfc2(z));
+      r2 = nt2::negif(is_ltz(a0), r2);
+      r1 = if_else(test1, r1, r2);
+      #ifndef BOOST_SIMD_NO_INFINITIES
+      r1 = nt2::if_else(nt2::is_inf(a0), nt2::sign(a0), r1);
+      #endif
+      return r1;
+    }
+  };
+
 } }
 #endif
