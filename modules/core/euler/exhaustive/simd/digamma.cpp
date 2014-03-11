c85df9c659a8efb5b529ecc2229776584c206b42
jtlap <jtlapreste@gmail.com>
2014-03-11 18:20:31 +0100

    improving digamma

---
+++ modules/core/euler/exhaustive/simd/digamma.cpp
@@ -0,0 +1,47 @@
+//==============================================================================
+//         Copyright 2009 - 2013 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2013        MetaScale SAS
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <boost/simd/sdk/simd/native.hpp>
+#include <nt2/include/functions/digamma.hpp>
+#include <nt2/include/constants/minlog.hpp>
+#include <nt2/include/constants/maxlog.hpp>
+
+#include <nt2/sdk/unit/exhaustive.hpp>
+
+#define NT2_ASSERTS_AS_TRAP
+#include <nt2/sdk/error/assert_as_trap.hpp>
+
+#include <cmath>
+#include <cstdlib>
+#include <boost/math/special_functions/digamma.hpp>
+
+struct raw_digamma
+{
+  float operator()(float x) const
+  {
+    return float(boost::math::digamma(double(x)));
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  typedef BOOST_SIMD_DEFAULT_EXTENSION             ext_t;
+  typedef boost::simd::native<float,ext_t>           n_t;
+  float mini = -6.0f;
+  float maxi = 6.0f;
+  if(argc >= 2) mini = std::atof(argv[1]);
+  if(argc >= 3) maxi = std::atof(argv[2]);
+
+  nt2::exhaustive_test<n_t> ( mini
+                            , maxi
+                            , nt2::functor<nt2::tag::digamma_>()
+                            , raw_digamma()
+                            );
+
+  return 0;
+}
--- modules/core/euler/include/nt2/euler/constants/digammalargelim.hpp
+++ modules/core/euler/include/nt2/euler/constants/digammalargelim.hpp
@@ -17,8 +17,8 @@ namespace nt2
   namespace tag
   {
     BOOST_SIMD_CONSTANT_REGISTER( Digammalargelim, double
-                                , 0, 0x41200000
-                                , 0x4034000000000000ll
+                                , 0, 0x41200000LL
+                                , 0x4034000000000000ULL
                                 );
   }
 
