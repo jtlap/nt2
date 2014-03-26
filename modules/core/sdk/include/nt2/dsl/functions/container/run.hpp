56d105dc46742af91d31deb69c8b2853c36a0f35
Joel Falcou <joel.falcou@lri.fr>
2014-03-26 21:06:01 +0100

    Split elementwise run

--- modules/core/sdk/include/nt2/dsl/functions/container/run.hpp
+++ modules/core/sdk/include/nt2/dsl/functions/container/run.hpp
@@ -12,7 +12,6 @@
 #include <nt2/dsl/functions/run.hpp>
 #include <nt2/include/functions/assign.hpp>
 #include <nt2/include/functions/of_size.hpp>
-#include <nt2/include/functions/transform.hpp>
 #include <nt2/include/functions/terminal.hpp>
 #include <nt2/sdk/memory/category.hpp>
 #include <nt2/sdk/meta/is_container.hpp>
@@ -24,48 +23,6 @@
 
 namespace nt2 { namespace ext
 {
-  //============================================================================
-  // Element-wise operations go to transform
-  //============================================================================
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_assign_, tag::cpu_
-                            , (A0)(T0)(N0)(A1)(T1)(N1)
-                            , ((node_<A0, elementwise_<T0>, N0, nt2::container::domain>))
-                              ((node_<A1, elementwise_<T1>, N1, nt2::container::domain>))
-                            )
-  {
-    typedef A0&                                             result_type;
-
-    BOOST_FORCEINLINE result_type
-    operator()(A0& a0, A1& a1) const
-    {
-      a0.resize(a1.extent());
-      nt2::transform(container::as_view(a0), a1);
-      return a0;
-    }
-  };
-
-  //============================================================================
-  // Copies go to transform as well, but we check for self-assignment.
-  // This is not a pessimization, some code patterns rely on this!
-  //============================================================================
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_assign_, tag::cpu_
-                            , (A0)(T0)
-                            , ((node_<A0, elementwise_<T0>, boost::mpl::long_<0> , nt2::container::domain>))
-                              ((node_<A0, elementwise_<T0>, boost::mpl::long_<0> , nt2::container::domain>))
-                            )
-  {
-    typedef A0&                                             result_type;
-
-    BOOST_FORCEINLINE result_type
-    operator()(A0& a0, A0& a1) const
-    {
-      a0.resize(a1.extent());
-      if(a0.raw() != a1.raw())
-        nt2::transform(container::as_view(a0), a1);
-      return a0;
-    }
-  };
-
   //============================================================================
   // Non-assign table expressions are reduced to assign expressions
   //============================================================================
