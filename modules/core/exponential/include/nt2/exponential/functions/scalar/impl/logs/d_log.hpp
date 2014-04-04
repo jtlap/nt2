9432ece1f0902ef9d8bbca10099e686d4dc72c58
Joel Falcou <joel.falcou@lri.fr>
2014-04-04 15:39:38 +0200

    Missing half constant include in log

--- modules/core/exponential/include/nt2/exponential/functions/scalar/impl/logs/d_log.hpp
+++ modules/core/exponential/include/nt2/exponential/functions/scalar/impl/logs/d_log.hpp
@@ -31,6 +31,7 @@
 #include <nt2/include/constants/zero.hpp>
 #include <nt2/include/constants/two.hpp>
 #include <nt2/include/constants/minf.hpp>
+#include <nt2/include/constants/half.hpp>
 #include <nt2/include/constants/nan.hpp>
 #include <boost/simd/sdk/config.hpp>
 
