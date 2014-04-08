90725f4b0627c0f56d4469cc4ce2b4582c20944e
alankelly <alankelly@live.com>
2014-04-08 14:27:08 +0200

    avx masked aligned_load/store added

--- modules/boost/simd/sdk/include/boost/simd/memory/functions/simd/sse/avx/aligned_load.hpp
+++ modules/boost/simd/sdk/include/boost/simd/memory/functions/simd/sse/avx/aligned_load.hpp
@@ -1,6 +1,7 @@
 //==============================================================================
 //         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
 //         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2014               MetaScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -17,11 +18,40 @@
 #include <boost/simd/sdk/simd/category.hpp>
 #include <boost/simd/memory/is_aligned.hpp>
 #include <boost/dispatch/meta/mpl.hpp>
+#include <boost/dispatch/attributes.hpp>
 #include <boost/mpl/if.hpp>
-#include <boost/assert.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
+  /// INTERNAL ONLY - Regular SIMD mask load for double without offset
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::aligned_load_
+                                    , boost::simd::tag::avx_
+                                    , (A0)(A2)(A3)(A4)
+                                    , (iterator_< scalar_< double_<A0> > >)
+                                      ((target_ < simd_ < double_<A2>
+                                                        , boost::simd::tag::avx_
+                                                        >
+                                                >
+                                      ))
+                                      ((simd_<double_<A3>
+                                             , boost::simd::tag::avx_
+                                             >
+                                      ))
+                                      ((simd_< logical_<A4>
+                                             , boost::simd::tag::avx_
+                                             >
+                                      ))
+                                    )
+  {
+    typedef typename A2::type result_type;
+
+    BOOST_FORCEINLINE result_type operator()(A0 a0, const A2&, const A3& a3, const A4& a4) const
+    {
+      BOOST_SIMD_DETAILS_CHECK_PTR(a0, sizeof(result_type));
+      return _mm256_blendv_pd(a3,_mm256_load_pd(a0),a4);
+    }
+  };
+
   /// INTERNAL ONLY - Regular SIMD load for double without offset
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::aligned_load_
                                     , boost::simd::tag::avx_
@@ -40,6 +70,35 @@ namespace boost { namespace simd { namespace ext
     {
       BOOST_SIMD_DETAILS_CHECK_PTR(a0, sizeof(result_type));
       return _mm256_load_pd(a0);
+    }
+  };
+
+  /// INTERNAL ONLY - Regular SIMD mask load for single without offset
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::aligned_load_
+                                    , boost::simd::tag::avx_
+                                    , (A0)(A2)(A3)(A4)
+                                    , (iterator_< scalar_< single_<A0> > >)
+                                      ((target_ < simd_ < single_<A2>
+                                                        , boost::simd::tag::avx_
+                                                        >
+                                                >
+                                      ))
+                                      ((simd_<single_<A3>
+                                             , boost::simd::tag::avx_
+                                             >
+                                      ))
+                                      ((simd_< logical_<A4>
+                                             , boost::simd::tag::avx_
+                                             >
+                                      ))
+                                    )
+  {
+    typedef typename A2::type result_type;
+
+    BOOST_FORCEINLINE result_type operator()(A0 a0, const A2&, const A3& a3, const A4& a4) const
+    {
+      BOOST_SIMD_DETAILS_CHECK_PTR(a0, sizeof(result_type));
+      return _mm256_blendv_ps(a3,_mm256_load_ps(a0),a4);
     }
   };
 
@@ -88,6 +147,51 @@ namespace boost { namespace simd { namespace ext
     {
       BOOST_SIMD_DETAILS_CHECK_PTR(a0, sizeof(result_type));
       return _mm256_load_si256(reinterpret_cast<__m256i const*>(a0));
+    }
+  };
+
+    /// INTERNAL ONLY - Masked misaligned load without offset
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION_IF( boost::simd::tag::aligned_load_
+                                      , boost::simd::tag::avx_
+                                      , (A0)(A2)(A3)(A4)(A5)
+                                      , ( simd::meta::is_pointing_to
+                                          < A0
+                                          , typename A2::type::value_type
+                                          >
+                                        )
+                                      , (iterator_< scalar_< arithmetic_<A0> > >)
+                                        ((target_ < simd_ < arithmetic_<A2>
+                                                          , boost::simd::tag::avx_
+                                                          >
+                                                  >
+                                        ))
+                                        (mpl_integral_< scalar_< integer_<A3> > >)
+                                        ((simd_<arithmetic_<A4>
+                                               , boost::simd::tag::avx_
+                                               >
+                                        ))
+                                        ((simd_< logical_<A5>
+                                               , boost::simd::tag::avx_
+                                               >
+                                        ))
+                                      )
+  {
+    typedef typename A2::type result_type;
+
+    typedef typename A3::value_type align_t;
+    static const std::size_t cardinal    = meta::cardinal_of<result_type>::value;
+    static const align_t     unalignment = A3::value % cardinal;
+
+    typedef typename boost::mpl::if_c < unalignment
+                                      , boost::simd::tag::load_
+                                      , boost::simd::tag::aligned_load_
+                                      >::type           func_t;
+
+    BOOST_FORCEINLINE result_type operator()(A0 a0, const A2& a2, const A3&, const A4& a4, const A5& a5) const
+    {
+      BOOST_SIMD_DETAILS_CHECK_PTR(a0 - A3::value, sizeof(result_type));
+      typename boost::dispatch::make_functor<func_t, A0>::type callee;
+      return callee( a0, a2, a4, a5 );
     }
   };
 
--- modules/boost/simd/sdk/include/boost/simd/memory/functions/simd/sse/avx/aligned_store.hpp
+++ modules/boost/simd/sdk/include/boost/simd/memory/functions/simd/sse/avx/aligned_store.hpp
@@ -1,6 +1,7 @@
 //==============================================================================
 //         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
 //         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2014               MetaScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -19,6 +20,30 @@
 
 namespace boost { namespace simd { namespace ext
 {
+  /// INTERNAL ONLY - masked AVX double SIMD store without offset
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::aligned_store_
+                                    , boost::simd::tag::avx_
+                                    , (A0)(A1)(A2)
+                                    , ((simd_ < double_<A0>
+                                              , boost::simd::tag::avx_
+                                              >
+                                      ))
+                                      (iterator_< scalar_< double_<A1> > >)
+                                      ((simd_< logical_<A2>
+                                             , boost::simd::tag::mic_
+                                             >
+                                      ))
+                                    )
+  {
+    typedef void result_type;
+
+    BOOST_FORCEINLINE result_type operator()(__m256d a0, A1 a1, const A2& a2) const
+    {
+      BOOST_SIMD_DETAILS_CHECK_PTR(a1, sizeof(__m256d));
+      _mm256_maskstore_pd(a1,a2,a0);
+    }
+  };
+
   /// INTERNAL ONLY - AVX double SIMD store without offset
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::aligned_store_
                                     , boost::simd::tag::avx_
@@ -36,6 +61,30 @@ namespace boost { namespace simd { namespace ext
     {
       BOOST_SIMD_DETAILS_CHECK_PTR(a1, sizeof(__m256d));
       _mm256_store_pd(a1,a0);
+    }
+  };
+
+  /// INTERNAL ONLY - masked AVX single SIMD store without offset
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::aligned_store_
+                                    , boost::simd::tag::avx_
+                                    , (A0)(A1)(A2)
+                                    , ((simd_ < single_<A0>
+                                              , boost::simd::tag::avx_
+                                              >
+                                      ))
+                                      (iterator_< scalar_< single_<A1> > >)
+                                      ((simd_< logical_<A2>
+                                             , boost::simd::tag::mic_
+                                             >
+                                      ))
+                                    )
+  {
+    typedef void result_type;
+
+    BOOST_FORCEINLINE result_type operator()(__m256d a0, A1 a1, const A2& a2) const
+    {
+      BOOST_SIMD_DETAILS_CHECK_PTR(a1, sizeof(__m256d));
+      _mm256_maskstore_ps(a1,a2,a0);
     }
   };
 
