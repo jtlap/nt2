8842d5957c7c8d39fe63970c9d915fecb15be737
jtlap <jtlapreste@gmail.com>
2014-04-09 15:32:35 +0200

    spurious comments in gammaln_kernel.hpp

--- modules/core/euler/include/nt2/euler/functions/details/gammaln_kernel.hpp
+++ modules/core/euler/include/nt2/euler/functions/details/gammaln_kernel.hpp
@@ -10,7 +10,6 @@
 #define NT2_EULER_FUNCTIONS_DETAILS_GAMMALN_KERNEL_HPP_INCLUDED
 #include <nt2/polynomials/functions/scalar/impl/horner.hpp>
 #include <nt2/sdk/meta/scalar_of.hpp>
-
 #include <boost/dispatch/attributes.hpp>
 
 namespace nt2 { namespace details
@@ -24,18 +23,7 @@ namespace nt2 { namespace details
     typedef typename meta::scalar_of<A0>::type sA0;
     static BOOST_FORCEINLINE A0 gammalnB( const A0& x)
     {
-      /* log gamma(x+2), -.5 < x < .5 */
-//       static const boost::array<sA0, 8 > B = {{
-//           6.055172732649237E-004,
-//           -1.311620815545743E-003,
-//           2.863437556468661E-003,
-//           -7.366775108654962E-003,
-//           2.058355474821512E-002,
-//           -6.735323259371034E-002,
-//           3.224669577325661E-001,
-//           4.227843421859038E-001
-//         }};
-//       return polevl(x, B);
+      // log gamma(x+2), -.5 < x < .5
      return horner<NT2_HORNER_COEFF_T(sA0, 8,
                                        (0x3a1ebb94, //    6.055172732649237E-004,
                                         0xbaabeab1, //   -1.311620815545743E-003,
@@ -50,18 +38,7 @@ namespace nt2 { namespace details
     }
     static BOOST_FORCEINLINE A0 gammalnC(const A0& x)
     {
-//       /* log gamma(x+1), -.25 < x < .25 */
-//       static const boost::array<sA0, 8 > C = {{
-//           1.369488127325832E-001,
-//           -1.590086327657347E-001,
-//           1.692415923504637E-001,
-//           -2.067882815621965E-001,
-//           2.705806208275915E-001,
-//           -4.006931650563372E-001,
-//           8.224670749082976E-001,
-//           -5.772156501719101E-001
-//         }};
-//       return polevl(x, C);
+      // log gamma(x+1), -.25 < x < .25
      return horner<NT2_HORNER_COEFF_T(sA0, 8,
                                       (0x3e0c3c4f,  //    1.369488127325832E-001,
                                        0xbe22d329,  //   -1.590086327657347E-001,
@@ -76,12 +53,6 @@ namespace nt2 { namespace details
     }
      static BOOST_FORCEINLINE A0 gammaln2(const A0& p)
     {
-//       static const boost::array<sA0, 3 > A = {{
-//            6.789774945028216E-004f,
-//           -2.769887652139868E-003f,
-//            8.333316229807355E-002f
- //         }};
-//       return polevl(p, A);
       return horner<NT2_HORNER_COEFF_T(sA0, 3,
                                        (0x3a31fd69, //   6.789774945028216E-004f,
                                         0xbb358701, //  -2.769887652139868E-003f,
@@ -97,60 +68,34 @@ namespace nt2 { namespace details
     typedef typename meta::scalar_of<A0>::type sA0;
     static BOOST_FORCEINLINE A0 gammaln1(const A0& x)
     {
-//       static const boost::array<sA0, 6 > B = {{
-//  -1.37825152569120859100E3,
-//  -3.88016315134637840924E4,
-//  -3.31612992738871184744E5,
-//  -1.16237097492762307383E6,
-//  -1.72173700820839662146E6,
-//  -8.53555664245765465627E5
-//         }};
-//        static const boost::array<sA0, 7 > C = {
-//         1.00000000000000000000E0,
-//         -3.51815701436523470549E2,
-//         -1.70642106651881159223E4,
-//         -2.20528590553854454839E5,
-//         -1.13933444367982507207E6,
-//         -2.53252307177582951285E6,
-//         -2.01889141433532773231E6
-//       };
-//        return polevl(x,B) / polevl(x,C);
-
-       return NT2_HORNER_RAT(sA0, 6, 7, x,
-                             (0xc09589018ff36761ll, //  -1.37825152569120859100E3,
-                              0xc0e2f234355bb93ell, //  -3.88016315134637840924E4,
-                              0xc1143d73f89089e5ll, //  -3.31612992738871184744E5,
-                              0xc131bc82f994db51ll, //  -1.16237097492762307383E6,
-                              0xc13a45890219f20bll, //  -1.72173700820839662146E6,
-                              0xc12a0c675418055ell  //  -8.53555664245765465627E5
-                             ),
-                             (0x3ff0000000000000ll, //   1.00000000000000000000E0,
-                              0xc075fd0d1cf312b2ll, //  -3.51815701436523470549E2,
-                              0xc0d0aa0d7b89d757ll, //  -1.70642106651881159223E4,
-                              0xc10aeb84b9744c9bll, //  -2.20528590553854454839E5,
-                              0xc131628671950043ll, //  -1.13933444367982507207E6,
-                              0xc1435255892ff34cll, //  -2.53252307177582951285E6,
-                              0xc13ece4b6a11e14all  //  -2.01889141433532773231E6
-                             )
-                            );
+      return NT2_HORNER_RAT(sA0, 6, 7, x,
+                            (0xc09589018ff36761ll, //  -1.37825152569120859100E3,
+                             0xc0e2f234355bb93ell, //  -3.88016315134637840924E4,
+                             0xc1143d73f89089e5ll, //  -3.31612992738871184744E5,
+                             0xc131bc82f994db51ll, //  -1.16237097492762307383E6,
+                             0xc13a45890219f20bll, //  -1.72173700820839662146E6,
+                             0xc12a0c675418055ell  //  -8.53555664245765465627E5
+                            ),
+                            (0x3ff0000000000000ll, //   1.00000000000000000000E0,
+                             0xc075fd0d1cf312b2ll, //  -3.51815701436523470549E2,
+                             0xc0d0aa0d7b89d757ll, //  -1.70642106651881159223E4,
+                             0xc10aeb84b9744c9bll, //  -2.20528590553854454839E5,
+                             0xc131628671950043ll, //  -1.13933444367982507207E6,
+                             0xc1435255892ff34cll, //  -2.53252307177582951285E6,
+                             0xc13ece4b6a11e14all  //  -2.01889141433532773231E6
+                            )
+                           );
 
     }
-   static BOOST_FORCEINLINE A0 gammalnA(const A0& p)
+
+    static BOOST_FORCEINLINE A0 gammalnA(const A0& p)
     {
-//       static const boost::array<sA0, 5 > A = {{
-//           8.11614167470508450300E-4,
-//          -5.95061904284301438324E-4,
-//           7.93650340457716943945E-4,
-//          -2.77777777730099687205E-3,
-//           8.33333333333331927722E-2
-//         }};
-//        return polevl(p, A);
       return horner<NT2_HORNER_COEFF_T(sA0, 5,
-                                       (0x3f4a985027336661ll,
-                                        0xbf437fbdb580e943ll,
-                                        0x3f4a019f20dc5ebbll,
-                                        0xbf66c16c16b0a5a1ll,
-                                        0x3fb555555555554bll
+                                       (0x3f4a985027336661ll,//    8.11614167470508450300E-4,
+                                        0xbf437fbdb580e943ll,//   -5.95061904284301438324E-4,
+                                        0x3f4a019f20dc5ebbll,//    7.93650340457716943945E-4,
+                                        0xbf66c16c16b0a5a1ll,//   -2.77777777730099687205E-3,
+                                        0x3fb555555555554bll //    8.33333333333331927722E-2
                                        )
                                       )> (p);
     }
