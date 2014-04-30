dae54a081a876bbd527d88f42634ba5cdb69f6ea
alankelly <alankelly@live.com>
2014-04-30 18:30:12 +0200

    if_else added to common for arithmetic logical logical

--- modules/boost/simd/base/include/boost/simd/operator/functions/simd/common/if_else.hpp
+++ modules/boost/simd/base/include/boost/simd/operator/functions/simd/common/if_else.hpp
@@ -49,6 +49,20 @@ namespace boost { namespace simd { namespace ext
     }
   };
 
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_else_, tag::cpu_, (A0)(A1)(X)
+                                    , ((simd_< arithmetic_<A0>, X >))
+                                      ((simd_< logical_<A1>, X >))
+                                      ((simd_< logical_<A1>, X >))
+                                    )
+  {
+    typedef A1 result_type;
+    BOOST_FORCEINLINE result_type operator()(const A0& a0, const A1& a1, const A1&a2) const
+    {
+      return if_else( is_nez(a0), a1, a2 );
+    }
+  };
+
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_else_, tag::cpu_, (A0)(A1)(X)
                                     , ((simd_< logical_<A0>, X >))
                                       ((simd_< logical_<A1>, X >))
