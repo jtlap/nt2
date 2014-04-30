5b35b0c93e5efe8fda4b77a43bcf1ec3470b5c59
NT2 <contact@metascale.org>
2014-04-30 18:13:26 +0200

    if_else for logical types added to common

--- modules/boost/simd/base/include/boost/simd/operator/functions/simd/common/if_else.hpp
+++ modules/boost/simd/base/include/boost/simd/operator/functions/simd/common/if_else.hpp
@@ -13,7 +13,13 @@
 #include <boost/simd/include/functions/simd/bitwise_select.hpp>
 #include <boost/simd/include/functions/simd/is_nez.hpp>
 #include <boost/simd/include/functions/simd/genmask.hpp>
+#include <boost/simd/include/functions/simd/logical_or.hpp>
+#include <boost/simd/include/functions/simd/logical_and.hpp>
+#include <boost/simd/include/functions/simd/logical_andnot.hpp>
+#include <boost/simd/include/functions/simd/bitwise_cast.hpp>
+#include <boost/simd/sdk/meta/as_arithmetic.hpp>
 #include <boost/dispatch/attributes.hpp>
+#include <boost/utility/enable_if.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -29,6 +35,7 @@ namespace boost { namespace simd { namespace ext
       return bitwise_select(genmask(a0), a1, a2);
     }
   };
+
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_else_, tag::cpu_, (A0)(A1)(X)
                                     , ((simd_< arithmetic_<A0>, X >))
                                       ((simd_< arithmetic_<A1>, X >))
@@ -41,6 +48,31 @@ namespace boost { namespace simd { namespace ext
       return if_else( is_nez(a0), a1, a2 );
     }
   };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::if_else_, tag::cpu_, (A0)(A1)(X)
+                                    , ((simd_< logical_<A0>, X >))
+                                      ((simd_< logical_<A1>, X >))
+                                      ((simd_< logical_<A1>, X >))
+                                    )
+  {
+    typedef A1 result_type;
+
+    template<class A0_>
+    typename enable_if< meta::is_bitwise_logical<A0_>, result_type>::type
+    BOOST_FORCEINLINE operator()(const A0_& a0, const A1& a1, const A1&a2) const
+    {
+      typedef typename meta::as_arithmetic<A1>::type atype;
+      return bitwise_cast<result_type>(if_else(a0,bitwise_cast<atype>(a1),bitwise_cast<atype>(a2)));
+    }
+
+    template<class A0_>
+    typename disable_if< meta::is_bitwise_logical<A0_>, result_type>::type
+    BOOST_FORCEINLINE operator()(const A0_& a0, const A1& a1, const A1&a2) const
+    {
+      return logical_or(logical_and(a1,a0),logical_andnot(a2,a0));
+    }
+  };
+
 } } }
 
 #endif
