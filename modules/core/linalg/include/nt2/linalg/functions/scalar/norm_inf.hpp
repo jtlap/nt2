1fbe102d1f7b3b3d973e3fc67a7e59bcbbd4cc49
jtlap <jtlapreste@gmail.com>
2014-06-07 10:33:10 +0200

    norm_inf to answer issue #758

---
+++ modules/core/linalg/include/nt2/linalg/functions/scalar/norm_inf.hpp
@@ -0,0 +1,90 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_LINALG_FUNCTIONS_SCALAR_NORM_INF_HPP_INCLUDED
+#define NT2_LINALG_FUNCTIONS_SCALAR_NORM_INF_HPP_INCLUDED
+#include <nt2/linalg/functions/norm_inf.hpp>
+#include <nt2/include/functions/ismatrix.hpp>
+#include <nt2/include/functions/isvector.hpp>
+#include <nt2/include/functions/globalmax.hpp>
+#include <nt2/include/functions/asum1.hpp>
+#include <nt2/include/functions/abs.hpp>
+#include <nt2/include/constants/nan.hpp>
+#include <boost/assert.hpp>
+#include <nt2/linalg/options.hpp>
+#include <nt2/sdk/meta/as_real.hpp>
+#include <nt2/core/container/dsl.hpp>
+#include <nt2/sdk/meta/property_of.hpp>
+
+namespace nt2 { namespace ext
+{
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::norm_inf_, tag::cpu_,
+                              (A0),
+                              ((ast_<A0, nt2::container::domain>))
+                            )
+  {
+    typedef typename A0::value_type                   type_t;
+    typedef typename meta::as_real<type_t>::type result_type;
+
+    NT2_FUNCTOR_CALL(1)
+    {
+      if (isvector(a0))
+      {
+        return nt2::globalmax(nt2::abs(a0));
+      }
+      else if (ismatrix(a0))
+      {
+        return nt2::globalmax(nt2::asum1(a0, 2));
+      }
+      else
+      {
+        BOOST_ASSERT_MSG(false, "a0 is not matrix nor vector");
+        return Nan<result_type>();
+       }
+    }
+  };
+
+
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::norm_inf_, tag::cpu_,
+                              (A0)(A1),
+                              ((ast_<A0, nt2::container::domain>))
+                              (unspecified_<A1>)
+
+                            )
+  {
+    typedef typename A0::value_type                   type_t;
+    typedef typename meta::as_real<type_t>::type result_type;
+
+    NT2_FUNCTOR_CALL(2)
+    {
+      return eval(a0, a1);
+    }
+
+    BOOST_FORCEINLINE result_type eval( const A0 & a0
+                                      , const nt2::policy<ext::matrix_> &) const
+    {
+      return nt2::globalmax(nt2::asum1(a0, 2));
+    }
+
+    BOOST_FORCEINLINE result_type eval( const A0 & a0
+                                      , const nt2::policy<ext::vector_> &) const
+    {
+      return nt2::globalmax(nt2::abs(a0));
+    }
+
+    template < class T >
+    BOOST_FORCEINLINE result_type eval(const A0 & a0, const T &)
+    {
+      BOOST_ASSERT_MSG(false, "unknown option to norm_inf");
+      return Nan<result_type>();
+    }
+
+  };
+} }
+
+#endif
---
+++ modules/core/linalg/unit/scalar/norm_inf.cpp
@@ -0,0 +1,32 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <nt2/table.hpp>
+#include <nt2/include/functions/norm_inf.hpp>
+#include <nt2/include/functions/ones.hpp>
+#include <nt2/include/constants/one.hpp>
+#include <nt2/include/constants/ten.hpp>
+#include <nt2/linalg/options.hpp>
+
+#include <nt2/sdk/unit/tests/ulp.hpp>
+#include <nt2/sdk/unit/module.hpp>
+
+
+NT2_TEST_CASE_TPL(norm, NT2_REAL_TYPES)
+{
+  using nt2::norm_inf;
+  using nt2::tag::norm_inf_;
+  nt2::table<T> n = nt2::ones(1, 10, nt2::meta::as_<T>());
+  NT2_TEST_ULP_EQUAL(nt2::norm_inf(n)              ,  nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(norm_inf(n, nt2::vector_),  nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(norm_inf(n, nt2::matrix_),  nt2::Ten<T>(), 0);
+  nt2::table<T> a = nt2::ones(10, 10, nt2::meta::as_<T>());
+  NT2_TEST_ULP_EQUAL(norm_inf(a), nt2::Ten<T>(), 0);
+  NT2_TEST_ULP_EQUAL(norm_inf(a,nt2::vector_), nt2::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(norm_inf(a,nt2::matrix_), nt2::Ten<T>(), 0);
+}
