a2e7fc6a35fa54bed936ea1a876e1f8bdde1b034
jtlap <jtlapreste@gmail.com>
2014-06-26 15:44:35 +0200

    this commit contains only the files mnomxxx.hpp and cpp and the Cmakelist. these files doesnot modify any master files and are purely matricial norms some of them optimized in the case of vectors both staticly or dynamicly detected. The modification of norm and the suppression of vecnorm will occur elsewhere. These functors are NOT reductions and do not replace any of the normxxx and summxxx functions having a distinct semantic

---
+++ modules/core/linalg/unit/scalar/mnorm.cpp
@@ -0,0 +1,54 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <nt2/table.hpp>
+#include <nt2/include/functions/mnorm.hpp>
+#include <nt2/include/functions/ones.hpp>
+#include <nt2/include/constants/one.hpp>
+#include <nt2/include/constants/three.hpp>
+#include <nt2/include/constants/nine.hpp>
+#include <nt2/linalg/options.hpp>
+
+#include <nt2/sdk/unit/tests/ulp.hpp>
+#include <nt2/sdk/unit/module.hpp>
+
+
+NT2_TEST_CASE_TPL(norm, NT2_REAL_TYPES)
+{
+  using nt2::mnorm;
+  using nt2::tag::mnorm_;
+  NT2_TEST_ULP_EQUAL(mnorm(T(-3)),  nt2::Three<T>(), 0);
+  nt2::table<T> n = nt2::ones(1, 9, nt2::meta::as_<T>());
+  NT2_TEST_ULP_EQUAL(mnorm(n),  nt2::Three<T>(), 0);
+  nt2::table<T> n1 = nt2::ones(9, 1, nt2::meta::as_<T>());
+  NT2_TEST_ULP_EQUAL(mnorm(n1),  nt2::Three<T>(), 0);
+  nt2::table<T> a = nt2::ones(9, 9, nt2::meta::as_<T>());
+  NT2_TEST_ULP_EQUAL(mnorm(a), nt2::Nine<T>(), 0.5);
+  nt2::table<T, nt2::of_size_<1,9> > ns=  n;
+  NT2_TEST_ULP_EQUAL(mnorm(ns),  nt2::Three<T>(), 0);
+  nt2::table<T, nt2::of_size_<9,1> > ns1 = n1;
+  NT2_TEST_ULP_EQUAL(mnorm(ns1),  nt2::Three<T>(), 0);
+}
+
+NT2_TEST_CASE_TPL(norm_c, NT2_REAL_TYPES)
+{
+  using nt2::mnorm;
+  using nt2::tag::mnorm_;
+  typedef std::complex<T> cT;
+  NT2_TEST_ULP_EQUAL(mnorm(cT(-3)),  nt2::Three<T>(), 0);
+  nt2::table<cT> n = nt2::ones(1, 9, nt2::meta::as_<cT>());
+  NT2_TEST_ULP_EQUAL(mnorm(n),  nt2::Three<T>(), 0);
+  nt2::table<cT> n1 = nt2::ones(9, 1, nt2::meta::as_<cT>());
+  NT2_TEST_ULP_EQUAL(mnorm(n1),  nt2::Three<T>(), 0);
+  nt2::table<cT> a = nt2::ones(9, 9, nt2::meta::as_<cT>());
+  NT2_TEST_ULP_EQUAL(mnorm(a), nt2::Nine<T>(), 0.5);
+  nt2::table<cT, nt2::of_size_<1,9> > ns=  n;
+  NT2_TEST_ULP_EQUAL(mnorm(ns),  nt2::Three<T>(), 0);
+  nt2::table<cT, nt2::of_size_<9,1> > ns1 = n1;
+  NT2_TEST_ULP_EQUAL(mnorm(ns1),  nt2::Three<T>(), 0);
+}
