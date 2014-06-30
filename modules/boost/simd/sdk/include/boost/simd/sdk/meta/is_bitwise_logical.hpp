ee131b23e1694b2f4b5a4fe522862a489bf3dd75
alankelly <alankelly@live.com>
2014-06-30 11:05:22 +0200

    Doc for is_logical_mask and is_bitwise_logical updated. is_logical_mask now inherites from is_bitwise_logical - an overload must be provided for architectures which logical SIMD types are bitwise_logical but are not represented by all 0 or ~0

--- modules/boost/simd/sdk/include/boost/simd/sdk/meta/is_bitwise_logical.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/meta/is_bitwise_logical.hpp
@@ -1,6 +1,7 @@
 //==============================================================================
-//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2003 - 2014   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2014   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2014          NumScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -13,6 +14,18 @@
 #include <boost/simd/sdk/meta/as_arithmetic.hpp>
 #include <boost/mpl/equal_to.hpp>
 #include <boost/mpl/sizeof.hpp>
+
+/*!
+  @brief Check if logical and arithmetic datatypes use the same registers
+
+  This metafunction determines if a logical SIMD vector uses the same
+  register types as arithmetic vector types.
+
+  @par semantic
+  - boost::simd::meta::is_bitwise_logical<T>::value = true/false
+
+  @param T - a SIMD register on the target architecture
+*/
 
 namespace boost { namespace simd { namespace meta
 {
