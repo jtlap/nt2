3297faaade278251e346ffbe749e53d8d495f580
jtlap <jtlapreste@gmail.com>
2014-07-25 09:18:20 +0200

    typo in rcond

--- modules/core/linalg/include/nt2/linalg/functions/scalar/rcond.hpp
+++ modules/core/linalg/include/nt2/linalg/functions/scalar/rcond.hpp
@@ -54,7 +54,7 @@ namespace nt2{ namespace ext
     }
   };
 
-/+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::rcond_, tag::cpu_
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::rcond_, tag::cpu_
                             , (A0)(A1)
                             , ((ast_<A0, nt2::container::domain>))
                               (unspecified_<A1>)
