69602af88b3c082603a00e65039484a0948f0432
alankelly <alankelly@live.com>
2014-08-06 09:43:20 +0200

    gather_avx unit test moved to gather.cpp in the main repo

--- modules/boost/simd/sdk/unit/memory/simd/gather.cpp
+++ modules/boost/simd/sdk/unit/memory/simd/gather.cpp
@@ -194,3 +194,139 @@ NT2_TEST_CASE_TPL ( load_gather64_logical
                                                 )
                         )
 }
+
+
+template<typename Type, typename Target, typename Index>
+void gather_indices()
+{
+  using boost::simd::load;
+  using boost::simd::tag::load_;
+  using boost::dispatch::meta::as_;
+  using boost::simd::meta::cardinal_of;
+
+  NT2_TEST_TYPE_IS( (typename boost::dispatch::meta
+                        ::call<load_( Type*, as_<Target>, Index)>::type
+                    )
+                  , Target
+                  );
+  static const std::size_t cd = cardinal_of<Target>::value;
+
+  Type data[cd*3];
+
+  for (std::size_t ii=0;ii<3*cd;ii++)
+    data[ii]=ii;
+
+  int ind[cardinal_of<Index>::value];
+  for (std::size_t ii=0;ii<cardinal_of<Index>::value;ii++)
+    ind[ii]=3*ii;
+
+  Index index = load<Index>(&ind[0]);
+
+  Target loaded = load<Target>(&data[0],index);
+
+  Target ref = boost::simd::splat<Target>(Type(0));
+  for (std::size_t ii=0;ii<cardinal_of<Index>::value;ii++)
+    ref[ii] = data[ind[ii]];
+
+  NT2_TEST_EQUAL(loaded,ref);
+}
+
+template<typename Type, typename Target, typename Index, typename Mask>
+void gather_indices_mask()
+{
+  using boost::simd::load;
+  using boost::simd::extract;
+  using boost::simd::if_else;
+  using boost::simd::tag::load_;
+  using boost::dispatch::meta::as_;
+  using boost::simd::meta::cardinal_of;
+  using boost::simd::meta::scalar_of;
+
+  NT2_TEST_TYPE_IS( (typename boost::dispatch::meta
+                        ::call<load_( Type*, as_<Target>, Index, Mask, Target)>::type
+                    )
+                  , Target
+                  );
+
+  static const std::size_t cd = cardinal_of<Target>::value;
+
+  typedef typename scalar_of<Mask>::type l_type;
+
+  Type data[cd*3];
+
+  for (std::size_t ii=0;ii<3*cd;ii++)
+    data[ii]=ii;
+
+  int ind[cardinal_of<Index>::value];
+  for (std::size_t ii=0;ii<cardinal_of<Index>::value;ii++)
+    ind[ii]=3*ii;
+
+  Target ref = boost::simd::splat<Target>(Type(0));
+  Target old = boost::simd::splat<Target>(Type(65));
+
+  time_t seed = time(NULL);
+  std::cout<<" Seed for random number generator "<<seed<<'\n';
+  srand(seed);
+
+  Mask mask;
+
+  for(size_t ii=0;ii<cd;++ii)
+    insert(l_type(rand()%2), mask, ii);
+
+  for (std::size_t ii=0;ii<cardinal_of<Index>::value;ii++)
+    ref[ii] = if_else(extract(mask,ii),data[ind[ii]],extract(old,ii));
+
+  Index index = load<Index>(&ind[0]);
+
+  Target loaded = load<Target>(&data[0],index,mask,old);
+
+  NT2_TEST_EQUAL(loaded,ref);
+}
+
+#ifdef BOOST_SIMD_HAS_AVX_SUPPORT
+
+//loading 64 bit avx types using 32bit sse indices
+NT2_TEST_CASE_TPL(test_gather32sse_64avx, (long long)(double))
+{
+  typedef boost::simd::native<T, boost::simd::tag::avx_> n_t;
+  typedef boost::simd::native<int32_t,boost::simd::tag::sse_> i_t;
+  typedef typename boost::simd::meta::as_logical<n_t>::type l_t;
+
+  gather_indices<T,n_t,i_t>();
+  gather_indices_mask<T,n_t,i_t,l_t>();
+}
+
+//loading 32 bit sse types using 64bit avx indices
+NT2_TEST_CASE_TPL(test_gather64avx_32sse, (int)(float))
+{
+  typedef boost::simd::native<T, boost::simd::tag::sse_> n_t;
+  typedef boost::simd::native<int64_t,boost::simd::tag::avx_> i_t;
+  typedef typename boost::simd::meta::as_logical<n_t>::type l_t;
+
+  gather_indices<T,n_t,i_t>();
+  gather_indices_mask<T,n_t,i_t,l_t>();
+}
+
+//loading 32 bit sse types using 32bit sse indices
+NT2_TEST_CASE_TPL(test_gather32sse_32sse, (int)(float))
+{
+  typedef boost::simd::native<T, boost::simd::tag::sse_> n_t;
+  typedef boost::simd::native<int32_t,boost::simd::tag::sse_> i_t;
+  typedef typename boost::simd::meta::as_logical<n_t>::type l_t;
+
+  gather_indices<T,n_t,i_t>();
+  gather_indices_mask<T,n_t,i_t,l_t>();
+}
+
+//loading 64 bit sse type using a 64 bit sse index
+NT2_TEST_CASE_TPL(test_gather64sse_64sse, (long long)(double))
+{
+  typedef boost::simd::native<T, boost::simd::tag::sse_> n_t;
+  typedef boost::simd::native<int64_t,boost::simd::tag::sse_> i_t;
+  typedef typename boost::simd::meta::as_logical<n_t>::type l_t;
+
+  gather_indices<T,n_t,i_t>();
+  gather_indices_mask<T,n_t,i_t,l_t>();
+}
+
+#endif
