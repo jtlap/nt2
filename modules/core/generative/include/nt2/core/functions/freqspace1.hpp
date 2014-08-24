8d16c3f9721844325bcea9e668cbe747878f68b6
jtlap <jtlapreste@gmail.com>
2014-08-24 16:45:45 +0200

    freqspace(1)

---
+++ modules/core/generative/include/nt2/core/functions/freqspace1.hpp
@@ -0,0 +1,46 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2011 - 2012   MetaScale SAS
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_FUNCTIONS_FREQSPACE1_HPP_INCLUDED
+#define NT2_CORE_FUNCTIONS_FREQSPACE1_HPP_INCLUDED
+
+#include <nt2/include/functor.hpp>
+#include <nt2/core/container/dsl/generative.hpp>
+#include <nt2/sdk/meta/generative_hierarchy.hpp>
+
+namespace nt2
+{
+  namespace tag
+  {
+    /*!
+      @brief Tag for the functor
+    **/
+    struct freqspace1_ : tag::formal_
+    {
+      typedef tag::formal_ parent;
+    };
+  }
+
+  /*!
+    f =  freqspace1(n, whole_, as<T>())
+    f =  freqspace1(n, as<T>())
+
+    f = freqspace1(n, as<T>()) returns the 1-d frequency vector f assuming n
+                               equally spaced points around the unit circle.
+                               f = _(0, 2/n, 1).
+    f = freqspace1(n, whole_, as<T>()) returns all n equally spaced points.
+                                       In this case, f = _(0, 2/n, 2*(n-1)/n).
+    T can be any floating type
+  **/
+  NT2_FUNCTION_IMPLEMENTATION(nt2::tag::freqspace1_, freqspace1, 3)
+  NT2_FUNCTION_IMPLEMENTATION(nt2::tag::freqspace1_, freqspace1, 2)
+  NT2_FUNCTION_IMPLEMENTATION(nt2::tag::freqspace1_, freqspace1, 1)
+}
+
+#endif
---
+++ modules/core/generative/include/nt2/core/functions/scalar/freqspace1.hpp
@@ -0,0 +1,78 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_CORE_FUNCTIONS_SCALAR_FREQSPACE1_HPP_INCLUDED
+#define NT2_CORE_FUNCTIONS_SCALAR_FREQSPACE1_HPP_INCLUDED
+
+#include <nt2/core/functions/freqspace1.hpp>
+#include <nt2/core/container/dsl.hpp>
+#include <nt2/core/functions/colon.hpp>
+
+namespace nt2 { namespace ext
+{
+  /// INTERNAL ONLY
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::freqspace1_, tag::cpu_
+                              , (A0)(T)
+                              , (scalar_< integer_<A0> >)
+                                ((target_<scalar_< floating_<T> > > ))
+                              )
+  {
+    typedef typename T::type target_t;
+
+    BOOST_DISPATCH_RETURNS_ARGS ( 2
+                                , (A0 const& n, T const& tgt)
+                                , (A0 const& n, T const&)
+                                , nt2::_(target_t(0),target_t(2)/n,target_t(1))
+                                );
+  };
+
+  /// INTERNAL ONLY
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::freqspace1_, tag::cpu_
+                            , (A0)
+                            , (scalar_< integer_<A0> >)
+                            )
+  {
+    BOOST_DISPATCH_RETURNS( 1, (A0 const& n), nt2::_(0., 2./n, 1.) );
+  };
+
+  /// INTERNAL ONLY
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::freqspace1_, tag::cpu_
+                              , (A0)(A1)(T)
+                              , (scalar_< integer_<A0> >)
+                                (target_<unspecified_<A1> >)
+                                (target_<scalar_< floating_<T> > >)
+                              )
+  {
+    typedef typename T::type target_t;
+
+    BOOST_DISPATCH_RETURNS_ARGS ( 3
+                                , (A0 const& n, A1 const& w, T const& tgt)
+                                , (A0 const& n, A1 const&  , T const&)
+                                , nt2::_( target_t(0)
+                                        , target_t(2)/n
+                                        , target_t(2)*(n-1)/n
+                                        )
+                                );
+  };
+
+  /// INTERNAL ONLY
+  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::freqspace1_, tag::cpu_
+                              , (A0)(A1)
+                              , (scalar_< integer_<A0> >)
+                                ((target_<unspecified_<A1> >))
+                              )
+  {
+    BOOST_DISPATCH_RETURNS_ARGS ( 2
+                                , (A0 const& n, A1 const& w)
+                                , (A0 const& n, A1 const&  )
+                                , nt2::_(0., 2./n, 2.*(n-1)/n)
+                                );
+  };
+} }
+
+#endif
