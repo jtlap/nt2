21ce783c963ba6ccd2efe93486513b8d61c0e734
Mathias Gaunard <mathias@gaunard.com>
2014-09-08 14:50:39 +0200

    wrong header guard in modf

--- modules/core/container/table/include/nt2/ieee/functions/container/modf.hpp
+++ modules/core/container/table/include/nt2/ieee/functions/container/modf.hpp
@@ -6,8 +6,8 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef NT2_IEEE_FUNCTIONS_CONTAINER_FREXP_HPP_INCLUDED
-#define NT2_IEEE_FUNCTIONS_CONTAINER_FREXP_HPP_INCLUDED
+#ifndef NT2_IEEE_FUNCTIONS_CONTAINER_MODF_HPP_INCLUDED
+#define NT2_IEEE_FUNCTIONS_CONTAINER_MODF_HPP_INCLUDED
 #include <nt2/ieee/functions/modf.hpp>
 #include <nt2/include/functions/tie.hpp>
 #include <nt2/core/container/dsl.hpp>
