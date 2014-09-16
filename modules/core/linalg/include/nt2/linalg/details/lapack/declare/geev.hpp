1a809052314fc104c20d76e786a99ab7420cba16
jtlap <jtlapreste@gmail.com>
2014-09-16 14:06:14 +0200

    missing headers in geev and getrf declare

--- modules/core/linalg/include/nt2/linalg/details/lapack/declare/geev.hpp
+++ modules/core/linalg/include/nt2/linalg/details/lapack/declare/geev.hpp
@@ -1,3 +1,12 @@
+//==============================================================================
+//         Copyright 2014 - Jean-Thierry Lapresté
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_LINALG_DETAILS_LAPACK_DECLARE_GEEV_HPP_INCLUDED
 #define NT2_LINALG_DETAILS_LAPACK_DECLARE_GEEV_HPP_INCLUDED
 #include <nt2/linalg/details/utility/f77_wrapper.hpp>
