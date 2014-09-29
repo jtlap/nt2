44e940bbaddc1e4b7b82894c7b84e6fc2b51e8eb
jtlap <jtlapreste@gmail.com>
2014-09-29 08:41:04 +0200

    more unused typedefs in iee operator and predicates

--- modules/boost/simd/base/unit/predicates/simd/is_finite.cpp
+++ modules/boost/simd/base/unit/predicates/simd/is_finite.cpp
@@ -37,7 +37,6 @@ NT2_TEST_CASE_TPL(is_finite_real, BOOST_SIMD_SIMD_REAL_TYPES)
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
   typedef native<T,ext_t>                  vT;
   typedef typename boost::dispatch::meta::call<is_finite_(vT)>::type r_t;
-  typedef boost::simd::logical<T> wished_r_t;
 
   // specific values tests
 #ifndef BOOST_SIMD_NO_INVALIDS
@@ -78,7 +77,6 @@ NT2_TEST_CASE_TPL(is_finite_si, BOOST_SIMD_SIMD_INTEGRAL_SIGNED_TYPES)
   typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
   typedef native<T,ext_t>                  vT;
   typedef typename boost::dispatch::meta::call<is_finite_(vT)>::type r_t;
-  typedef boost::simd::logical<vT> wished_r_t;
 
   // specific values tests
   NT2_TEST_EQUAL(is_finite(boost::simd::Mone<vT>()), boost::simd::True<r_t>());
