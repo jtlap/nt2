a8e0413c4047ad23fa44678a57a8298edcb27f02
Mathias Gaunard <mathias@gaunard.com>
2014-10-26 21:17:08 +0100

    be more restrictive for #835 bug workaround, AVX-only. Make handling of invalids separate.

--- modules/boost/simd/base/include/boost/simd/ieee/functions/simd/common/exponent.hpp
+++ modules/boost/simd/base/include/boost/simd/ieee/functions/simd/common/exponent.hpp
@@ -13,19 +13,24 @@
 #include <boost/simd/ieee/functions/exponent.hpp>
 #include <boost/simd/include/functions/simd/shr.hpp>
 #include <boost/simd/include/functions/simd/exponentbits.hpp>
-#include <boost/simd/include/functions/simd/is_invalid.hpp>
-#include <boost/simd/include/functions/simd/minus.hpp>
-#include <boost/simd/include/functions/simd/if_else_zero.hpp>
-#include <boost/simd/include/functions/simd/if_zero_else.hpp>
-#ifdef __INTEL_COMPILER
-#include <boost/simd/include/functions/simd/if_else.hpp>
-#endif
+#include <boost/simd/include/functions/simd/selsub.hpp>
+
 #include <boost/simd/include/constants/maxexponent.hpp>
 #include <boost/simd/include/constants/nbmantissabits.hpp>
 #include <boost/simd/include/constants/zero.hpp>
 #include <boost/dispatch/meta/as_integer.hpp>
 #include <boost/simd/sdk/meta/scalar_of.hpp>
 #include <boost/dispatch/attributes.hpp>
+
+#ifndef BOOST_SIMD_NO_INVALIDS
+#include <boost/simd/include/functions/simd/if_zero_else.hpp>
+#include <boost/simd/include/functions/simd/is_invalid.hpp>
+#endif
+
+#if defined(__INTEL_COMPILER) && defined(BOOST_SIMD_HAS_AVX_SUPPORT) && !defined(BOOST_SIMD_HAS_AVX2_SUPPORT)
+#include <boost/simd/include/functions/simd/if_else.hpp>
+#include <boost/simd/include/functions/simd/minus.hpp>
+#endif
 
 namespace boost { namespace simd { namespace ext
 {
@@ -52,11 +57,19 @@ namespace boost { namespace simd { namespace ext
     {
       typedef typename meta::scalar_of<A0>::type             s_type;
       const int nmb= int(Nbmantissabits<s_type>());
-      const result_type x = shri(exponentbits(a0), nmb);
-      #ifdef __INTEL_COMPILER
-      return if_zero_else( is_invalid(a0), x-if_else(a0, Maxexponent<A0>(), Zero<result_type>()));
+      result_type x = shri(exponentbits(a0), nmb);
+
+      // workaround for ICC 14.0.3, tested on AVX
+      #if defined(__INTEL_COMPILER) && defined(BOOST_SIMD_HAS_AVX_SUPPORT) && !defined(BOOST_SIMD_HAS_AVX2_SUPPORT)
+      x = x - if_else(a0, Maxexponent<A0>(), Zero<result_type>());
       #else
-      return if_zero_else( is_invalid(a0), x-if_else_zero(a0, Maxexponent<A0>());
+      x = selsub(a0, x, Maxexponent<A0>());
+      #endif
+
+      #ifndef BOOST_SIMD_NO_INVALIDS
+      return if_zero_else( is_invalid(a0), x );
+      #else
+      return x;
       #endif
     }
   };
