04315e9b1ce92b076b0b8616339edaad5a722203
Mathias Gaunard <mathias@gaunard.com>
2014-11-01 11:02:43 +0100

    wrong placement of simd/common/make.hpp

--- modules/boost/simd/sdk/include/boost/simd/memory/functions/simd/common/make.hpp
+++ modules/boost/simd/sdk/include/boost/simd/memory/functions/simd/common/make.hpp
@@ -7,8 +7,8 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_MEMORY_FUNCTIONS_COMMON_MAKE_HPP_INCLUDED
-#define BOOST_SIMD_MEMORY_FUNCTIONS_COMMON_MAKE_HPP_INCLUDED
+#ifndef BOOST_SIMD_MEMORY_FUNCTIONS_SIMD_COMMON_MAKE_HPP_INCLUDED
+#define BOOST_SIMD_MEMORY_FUNCTIONS_SIMD_COMMON_MAKE_HPP_INCLUDED
 
 #include <boost/simd/memory/functions/make.hpp>
 #include <boost/simd/include/functions/simd/aligned_load.hpp>
