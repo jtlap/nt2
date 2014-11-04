42fe9ab3629284b72f1a00f1a6d3400e171281f6
jtlap <jtlapreste@gmail.com>
2014-11-04 14:06:21 +0100

    suppressing overflowing tests for signed integer in dec and inc scalar tests

--- modules/boost/simd/base/unit/arithmetic/scalar/inc.cpp
+++ modules/boost/simd/base/unit/arithmetic/scalar/inc.cpp
@@ -36,7 +36,6 @@ NT2_TEST_CASE_TPL ( inc_signed_int,  BOOST_SIMD_INTEGRAL_SIGNED_TYPES)
   // specific values tests
   NT2_TEST_EQUAL(inc(boost::simd::Mone<T>()), boost::simd::Zero<T>());
   NT2_TEST_EQUAL(inc(boost::simd::One<T>()), boost::simd::Two<T>());
-  NT2_TEST_EQUAL(inc(boost::simd::Valmax<T>()), boost::simd::Valmin<T>());
   NT2_TEST_EQUAL(inc(boost::simd::Zero<T>()), boost::simd::One<T>());
 }
 
