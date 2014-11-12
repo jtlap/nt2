cedd9d53d7e420b7f9845ce7cffb409d714cf34c
Mathias Gaunard <mathias@gaunard.com>
2014-11-12 14:37:47 +0100

    remove unused typedefs in correct_fma tests

--- modules/core/base/unit/arithmetic/scalar/correct_fma.cpp
+++ modules/core/base/unit/arithmetic/scalar/correct_fma.cpp
@@ -38,10 +38,7 @@ NT2_TEST_CASE_TPL ( correct_fma_real__3_0,  NT2_REAL_TYPES)
 
   using nt2::correct_fma;
   using nt2::tag::correct_fma_;
-  typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<correct_fma_(T,T,T)>::type r_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  typedef typename nt2::meta::upgrade<T>::type u_t;
   typedef typename boost::common_type<T,T,T>::type wished_r_t;
 
 
@@ -66,10 +63,7 @@ NT2_TEST_CASE_TPL ( correct_fma_signed_int__3_0,  NT2_INTEGRAL_SIGNED_TYPES)
 
   using nt2::correct_fma;
   using nt2::tag::correct_fma_;
-  typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<correct_fma_(T,T,T)>::type r_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  typedef typename nt2::meta::upgrade<T>::type u_t;
   typedef typename boost::common_type<T,T,T>::type wished_r_t;
 
 
@@ -90,10 +84,7 @@ NT2_TEST_CASE_TPL ( correct_fma_unsigned_int__3_0,  NT2_UNSIGNED_TYPES)
 
   using nt2::correct_fma;
   using nt2::tag::correct_fma_;
-  typedef typename nt2::meta::as_integer<T>::type iT;
   typedef typename nt2::meta::call<correct_fma_(T,T,T)>::type r_t;
-  typedef typename nt2::meta::scalar_of<r_t>::type ssr_t;
-  typedef typename nt2::meta::upgrade<T>::type u_t;
   typedef typename boost::common_type<T,T,T>::type wished_r_t;
 
 
