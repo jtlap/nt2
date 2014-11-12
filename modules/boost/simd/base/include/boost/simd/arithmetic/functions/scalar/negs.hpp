1ca743f6f52672c6fc73e6da1e4dc5f435c47916
Mathias Gaunard <mathias@gaunard.com>
2014-11-12 16:00:08 +0100

    split sqrs/negs scalar and simd, add kludge for UBSan for sqrs

--- modules/boost/simd/base/include/boost/simd/arithmetic/functions/scalar/negs.hpp
+++ modules/boost/simd/base/include/boost/simd/arithmetic/functions/scalar/negs.hpp
@@ -7,13 +7,11 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_GENERIC_NEGS_HPP_INCLUDED
-#define BOOST_SIMD_ARITHMETIC_FUNCTIONS_GENERIC_NEGS_HPP_INCLUDED
+#ifndef BOOST_SIMD_ARITHMETIC_FUNCTIONS_SCALAR_NEGS_HPP_INCLUDED
+#define BOOST_SIMD_ARITHMETIC_FUNCTIONS_SCALAR_NEGS_HPP_INCLUDED
 
 #include <boost/simd/arithmetic/functions/negs.hpp>
-#include <boost/simd/include/functions/simd/if_else.hpp>
-#include <boost/simd/include/functions/simd/is_equal.hpp>
-#include <boost/simd/include/functions/simd/unary_minus.hpp>
+#include <boost/simd/include/functions/scalar/unary_minus.hpp>
 #include <boost/simd/include/constants/valmin.hpp>
 #include <boost/simd/include/constants/valmax.hpp>
 #include <boost/dispatch/attributes.hpp>
@@ -22,19 +20,19 @@ namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::negs_, tag::cpu_
                                     , (A0)
-                                    , (generic_<signed_<A0> >)
+                                    , (scalar_<signed_<A0> >)
                                     )
   {
     typedef A0 result_type;
     BOOST_FORCEINLINE BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      return if_else(eq(a0, Valmin<result_type>()),Valmax<result_type>(),unary_minus(a0));
+      return a0 == Valmin<result_type>() ? Valmax<result_type>() : unary_minus(a0);
     }
   };
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION ( boost::simd::tag::negs_, tag::cpu_
                                     , (A0)
-                                    , (generic_<floating_<A0> >)
+                                    , (scalar_<floating_<A0> >)
                                     )
   {
     typedef A0 result_type;
