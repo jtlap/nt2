13a9510ef29826f02f2ce1b09dfde7cf81e329f6
jtlap <jtlapreste@gmail.com>
2014-11-18 10:48:01 +0100

    unit in core have still some int overflow pb caused by undue tests

--- modules/core/base/unit/ieee/scalar/next.cpp
+++ modules/core/base/unit/ieee/scalar/next.cpp
@@ -98,6 +98,5 @@ NT2_TEST_CASE_TPL ( next_signed_int__1_0,  NT2_INTEGRAL_SIGNED_TYPES)
   // specific values tests
   NT2_TEST_EQUAL(next(nt2::Mone<T>()), nt2::Zero<r_t>());
   NT2_TEST_EQUAL(next(nt2::One<T>()), nt2::Two<r_t>());
-  NT2_TEST_EQUAL(next(nt2::Valmax<T>()), nt2::Valmin<r_t>());
   NT2_TEST_EQUAL(next(nt2::Zero<T>()), nt2::One<r_t>());
 } // end of test for signed_int_
