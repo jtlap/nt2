82fe090089613bce4bc6ee9281dd60393c54453a
Mathias Gaunard <mathias@gaunard.com>
2014-11-23 00:09:18 +0100

    missing logic for expcfd0

--- modules/core/statistics/include/nt2/statistics/functions/expcdf.hpp
+++ modules/core/statistics/include/nt2/statistics/functions/expcdf.hpp
@@ -41,6 +41,9 @@ namespace nt2 { namespace tag
     {
       /// @brief Parent hierarchy
       typedef ext::elementwise_<expcdf0_> parent;
+      template<class... Args>
+      static BOOST_FORCEINLINE BOOST_AUTO_DECLTYPE dispatch(Args&&... args)
+      BOOST_AUTO_DECLTYPE_BODY( dispatching_expcdf0_( ext::adl_helper(), static_cast<Args&&>(args)... ) )
     };
   }
   namespace ext
@@ -52,6 +55,13 @@ namespace nt2 { namespace tag
    }
    template<class... Args>
    struct impl_expcdf_;
+   template<class Site, class... H>
+   BOOST_FORCEINLINE generic_dispatcher<tag::expcdf0_, Site> dispatching_expcdf0_(adl_helper, boost::dispatch::meta::unknown_<Site>, boost::dispatch::meta::unknown_<H>...)
+   {
+     return generic_dispatcher<tag::expcdf0_, Site>();
+   }
+   template<class... Args>
+   struct impl_expcdf0_;
   }
   /*!
     exponential  cumulative distribution
