1286d905a697162b6d10bb1951f0186d448fd76d
Mathias Gaunard <mathias@gaunard.com>
2014-11-26 11:42:26 +0100

    sine didn't get modified by script correctly

--- modules/core/trigonometric/include/nt2/trigonometric/functions/sine.hpp
+++ modules/core/trigonometric/include/nt2/trigonometric/functions/sine.hpp
@@ -20,7 +20,17 @@ namespace nt2 { namespace tag
      @par Models:
         Hierarchy
    **/
-    template <class T> struct sine_ : ext::elementwise_< sine_<T> > { typedef ext::elementwise_< sine_<T> > parent;   template<class... Args>   static BOOST_FORCEINLINE BOOST_AUTO_DECLTYPE dispatch(Args&&... args)   BOOST_AUTO_DECLTYPE_BODY( dispatching( ext::adl_helper(), sine_(), static_cast<Args&&>(args)... ) ) };
+    template <class T>
+    struct sine_ : ext::elementwise_< sine_<T> >
+    {
+      typedef ext::elementwise_< sine_<T> > parent;
+      template<class... Args>
+      static BOOST_FORCEINLINE BOOST_AUTO_DECLTYPE dispatch(Args&&... args)
+      BOOST_AUTO_DECLTYPE_HEADER( dispatching( ext::adl_helper(), *(sine_*)0, static_cast<Args&&>(args)... ) )
+      {
+        return dispatching( ext::adl_helper(), sine_(), static_cast<Args&&>(args)... );
+      }
+    };
   }
   /*!
     templated sine.
