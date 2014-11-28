2818019c4cd7da1821e4d8f43de22d23a0fb6e94
jtlap <jtlapreste@gmail.com>
2014-11-28 13:36:05 +0100

    fixing logninv test

--- modules/core/statistics/unit/scalar/logninv.cpp
+++ modules/core/statistics/unit/scalar/logninv.cpp
@@ -29,42 +29,15 @@ NT2_TEST_CASE_TPL ( logninv_1,  NT2_REAL_TYPES)
   NT2_TEST_ULP_EQUAL(logninv(nt2::One<T>()), nt2::Inf<T>(), 0.5);
 } // end of test for floating_
 
-// NT2_TEST_CASE_TPL ( logninv_2,  NT2_REAL_TYPES)
-// {
-//   using nt2::logninv;
-//   using nt2::tag::logninv_;
-//   using nt2::_;
-//   // specific values tests
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::Nan<T>(), nt2::One<T>()), nt2::Nan<T>(), 0.5);
-//   NT2_TEST_ULP_EQUAL(logninv(T(0.841344746068542948585232545632), nt2::One<T>()), nt2::Two<T>(), 3);
-//   NT2_TEST_ULP_EQUAL(logninv(T(0.158655253931457051414767454368), nt2::One<T>()), nt2::Zero<T>(), 6.5);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::One<T>(), nt2::One<T>()), nt2::Inf<T>(), 0.5);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::Zero<T>(), nt2::One<T>()), nt2::Minf<T>(), 0.5);
-// } // end of test for floating_
-
-// NT2_TEST_CASE_TPL ( logninv_1v,  NT2_REAL_TYPES)
-// {
-//   using nt2::logninv;
-//   using nt2::tag::logninv_;
-//   using nt2::_;
-//   // specific values tests
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(nt2::Nan<T>(), 10, 2)), nt2::repnum(nt2::Nan<T>(), 10, 2), 0.5);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(T(0.841344746068542948585232545632), 10, 2)), nt2::repnum(nt2::One<T>(), 10, 2), 3);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(T(0.158655253931457051414767454368), 10, 2)), nt2::repnum(nt2::Mone<T>(), 10, 2), 6.5);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(nt2::Zero<T>(), 10, 2)), nt2::repnum(nt2::Minf<T>(), 10, 2), 0.5);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(nt2::One<T>(), 10, 2)), nt2::repnum(nt2::Inf<T>(), 10, 2), 0.5);
-// }
-
-// NT2_TEST_CASE_TPL ( logninv_2v,  NT2_REAL_TYPES)
-// {
-//   using nt2::logninv;
-//   using nt2::tag::logninv_;
-//   using nt2::_;
-//   // specific values tests
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(nt2::Nan<T>(), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Nan<T>(), 10, 2), 0.5);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(T(0.841344746068542948585232545632), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Two<T>(), 10, 2), 3);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(T(0.158655253931457051414767454368), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Zero<T>(), 10, 2), 6.5);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(nt2::One<T>(), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Inf<T>(), 10, 2), 0.5);
-//   NT2_TEST_ULP_EQUAL(logninv(nt2::repnum(nt2::Zero<T>(), 10, 2), nt2::One<T>()),  nt2::repnum(nt2::Minf<T>(), 10, 2), 0.5);
-// } // end of test for floating_
+NT2_TEST_CASE_TPL ( logninv_2,  NT2_REAL_TYPES)
+{
+  using nt2::logninv;
+  using nt2::tag::logninv_;
+  using nt2::_;
+  // specific values tests
+  NT2_TEST_ULP_EQUAL(logninv(nt2::Nan<T>(), nt2::One<T>()), nt2::Nan<T>(), 0.5);
+  NT2_TEST_ULP_EQUAL(logninv(nt2::Half<T>(), nt2::One<T>()), nt2::One<T>(), 3);
+  NT2_TEST_ULP_EQUAL(logninv(nt2::Zero<T>(), nt2::One<T>()), nt2::Zero<T>(), 0.5);
+  NT2_TEST_ULP_EQUAL(logninv(nt2::One<T>(), nt2::One<T>()), nt2::Inf<T>(), 0.5);
+} // end of test for floating_
 
