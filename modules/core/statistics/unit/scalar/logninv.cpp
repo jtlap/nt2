0776fa97d90697ba1b5c2ea9ce204e027d9e56d6
Mathias Gaunard <mathias@gaunard.com>
2014-12-01 10:52:16 +0100

    incorrect value in logninv test

--- modules/core/statistics/unit/scalar/logninv.cpp
+++ modules/core/statistics/unit/scalar/logninv.cpp
@@ -7,24 +7,18 @@
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
 #include <nt2/include/functions/logninv.hpp>
-#include <nt2/include/functions/eye.hpp>
-#include <nt2/include/functions/ones.hpp>
-#include <nt2/include/functions/abs.hpp>
-#include <nt2/include/functions/repnum.hpp>
-#include <nt2/sdk/unit/tests.hpp>
-#include <nt2/sdk/unit/module.hpp>
-
+#include <nt2/include/constants/exp_1.hpp>
 #include <nt2/constant/constant.hpp>
-#include <nt2/table.hpp>
+
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
 
 NT2_TEST_CASE_TPL ( logninv_1,  NT2_REAL_TYPES)
 {
   using nt2::logninv;
-  using nt2::tag::logninv_;
-  using nt2::_;
   // specific values tests
   NT2_TEST_ULP_EQUAL(logninv(nt2::Nan<T>()), nt2::Nan<T>(), 0.5);
-  NT2_TEST_ULP_EQUAL(logninv(nt2::Half<T>()), nt2::One<T>(), 3);
+  NT2_TEST_ULP_EQUAL(logninv(nt2::Half<T>()), nt2::One<T>(), 0.5);
   NT2_TEST_ULP_EQUAL(logninv(nt2::Zero<T>()), nt2::Zero<T>(), 0.5);
   NT2_TEST_ULP_EQUAL(logninv(nt2::One<T>()), nt2::Inf<T>(), 0.5);
 } // end of test for floating_
@@ -32,11 +26,9 @@ NT2_TEST_CASE_TPL ( logninv_1,  NT2_REAL_TYPES)
 NT2_TEST_CASE_TPL ( logninv_2,  NT2_REAL_TYPES)
 {
   using nt2::logninv;
-  using nt2::tag::logninv_;
-  using nt2::_;
   // specific values tests
   NT2_TEST_ULP_EQUAL(logninv(nt2::Nan<T>(), nt2::One<T>()), nt2::Nan<T>(), 0.5);
-  NT2_TEST_ULP_EQUAL(logninv(nt2::Half<T>(), nt2::One<T>()), nt2::One<T>(), 3);
+  NT2_TEST_ULP_EQUAL(logninv(nt2::Half<T>(), nt2::One<T>()), nt2::Exp_1<T>(), 0.5);
   NT2_TEST_ULP_EQUAL(logninv(nt2::Zero<T>(), nt2::One<T>()), nt2::Zero<T>(), 0.5);
   NT2_TEST_ULP_EQUAL(logninv(nt2::One<T>(), nt2::One<T>()), nt2::Inf<T>(), 0.5);
 } // end of test for floating_
