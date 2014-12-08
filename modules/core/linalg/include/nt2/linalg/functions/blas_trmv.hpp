7a5b34756902c5f491b58b671d8e79dd3d796a5d
jtlap <jtlapreste@gmail.com>
2014-12-08 15:59:01 +0100

    trmv

---
+++ modules/core/linalg/include/nt2/linalg/functions/blas_trmv.hpp
@@ -0,0 +1,60 @@
+//==============================================================================
+//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_LINALG_FUNCTIONS_BLAS_TRMV_HPP_INCLUDED
+#define NT2_LINALG_FUNCTIONS_BLAS_TRMV_HPP_INCLUDED
+
+/*!
+  @file
+  @brief Defines and implements blas_trmv function
+**/
+
+#include <nt2/include/functor.hpp>
+
+namespace nt2
+{
+  namespace tag
+  {
+    /// @brief Defines blas_trmv function tag
+    struct blas_trmv_ : ext::abstract_<blas_trmv_>
+    {
+      /// INTERNAL ONLY
+      typedef ext::abstract_<blas_trmv_> parent;
+      template<class... Args>
+      static BOOST_FORCEINLINE BOOST_AUTO_DECLTYPE dispatch(Args&&... args)
+      BOOST_AUTO_DECLTYPE_BODY( dispatching_blas_trmv_( ext::adl_helper(), static_cast<Args&&>(args)... ) )
+    };
+  }
+  namespace ext
+  {
+    template<class Site>
+    BOOST_FORCEINLINE generic_dispatcher<tag::blas_trmv_, Site> dispatching_blas_trmv_(adl_helper, boost::dispatch::meta::unknown_<Site>, ...)
+    {
+      return generic_dispatcher<tag::blas_trmv_, Site>();
+    }
+    template<class... Args>
+    struct impl_blas_trmv_;
+  }
+
+  /*!
+    @brief
+
+    @param
+    @param
+
+    @return
+  **/
+  NT2_FUNCTION_IMPLEMENTATION_TPL (tag::blas_trmv_, blas_trmv
+                                  , (const A0&)(const A1&)(const A2&)(const A3&)(A4&)(const A5&)
+                                  , 6
+                                  );
+
+}
+
+#endif
+
