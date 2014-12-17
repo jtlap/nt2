72bd9e3c53b53072e65f0741b87b054f9889c1ae
Mathias Gaunard <mathias@gaunard.com>
2014-12-17 11:18:26 +0100

    fix warning in shuffle2 test

--- modules/boost/simd/base/unit/swar/simd/shuffle2.cpp
+++ modules/boost/simd/base/unit/swar/simd/shuffle2.cpp
@@ -39,7 +39,6 @@ template<typename T, int IA, int IB> void binary_shuffle()
 {
   using boost::simd::shuffle;
   using boost::simd::meta::vector_of;
-  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
   typedef typename vector_of<T,2>::type    vT;
 
   vT ref, a, b;
