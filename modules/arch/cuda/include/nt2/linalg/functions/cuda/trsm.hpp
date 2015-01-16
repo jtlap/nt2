2ff043c7a8fa301a083c05bd6012c8078c802b14
masliah <ian.masliah@u-psud.fr>
2015-01-16 15:16:53 +0100

    corrected wrong version of trsm and commented test until buffers are ready

--- modules/arch/cuda/include/nt2/linalg/functions/cuda/trsm.hpp
+++ modules/arch/cuda/include/nt2/linalg/functions/cuda/trsm.hpp
@@ -13,6 +13,10 @@
 
 #include <nt2/linalg/functions/trsm.hpp>
 #include <nt2/sdk/cuda/cuda.hpp>
+
+#include <nt2/dsl/functions/terminal.hpp>
+#include <nt2/core/container/table/kind.hpp>
+
 #include <nt2/linalg/details/utility/f77_wrapper.hpp>
 #include <nt2/linalg/details/blas/blas3.hpp>
 #include <nt2/include/functions/of_size.hpp>
@@ -24,7 +28,7 @@
 
 namespace nt2 { namespace ext
 {
-  NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::trsm_, nt2::tag::cuda_<site>
+  BOOST_DISPATCH_IMPLEMENT ( trsm_, nt2::tag::cuda_<site>
                             , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
                             , (scalar_< ints8_<A0> >)
                               (scalar_< ints8_<A1> >)
@@ -39,205 +43,204 @@ namespace nt2 { namespace ext
      BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
                                              , A3 const& diag, A4 const& a, A5& b ) const
      {
-        nt2_la_int  m  = nt2::height(a);
-        nt2_la_int  n  = nt2::width(b);
-        nt2_la_int  lda = (side=='L'||'l')? m : n ;
+        int  m  = nt2::height(a);
+        int  n  = nt2::width(b);
+        int  lda = (side=='L'||'l') ? m : n ;
 
-        double  alpha = 1;
+        double alpha = 1.;
 
-        std::cout << "in cuda trsm" << std::endl;
-        cublasHandle_t handle;
-
-        // cublasStrsm( handle, side, uplo, transa, diag, m, n, &alpha
-        //                   , a.raw(),lda,b.raw(),m);
+        cublasDtrsm( side, uplo, transa, diag, m, n, alpha
+                          , a.data(),lda,b.data(),m);
 
      }
   };
 
-  // NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::trsm_,  nt2::tag::cuda_<site>
-  //                           , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
-  //                           , (scalar_< ints8_<A0> >)
-  //                             (scalar_< ints8_<A1> >)
-  //                             (scalar_< ints8_<A2> >)
-  //                             (scalar_< ints8_<A3> >)
-  //                             ((container_< nt2::tag::table_, single_<A4>, S4 >))
-  //                             ((container_< nt2::tag::table_, single_<A5>, S5 >))
-  //                           )
-  // {
-  //   typedef void  result_type;
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, single_<A4>, S4 >))
+                              ((container_< nt2::tag::table_, single_<A5>, S5 >))
+                            )
+  {
+    typedef void  result_type;
 
-  //    BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-  //                                            , A3 const& diag, A4 const& a, A5& b ) const
-  //    {
-  //       nt2_la_int  m  = nt2::height(a);
-  //       nt2_la_int  n  = nt2::width(b);
-  //       nt2_la_int  lda = (side=='L'||'l')? m : n ;
-  //       float  alpha = 1;
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b ) const
+     {
+        int  m  = nt2::height(a);
+        int  n  = nt2::width(b);
+        int  lda = (side=='L'||'l') ? m : n ;
 
-  //       NT2_F77NAME(strsm)( &side, &uplo, &transa, &diag, &m, &n, &alpha
-  //                         , a.raw(),&lda,b.raw(),&m);
+        double alpha = 1.;
 
-  //    }
-  // };
+        cublasStrsm( side, uplo, transa, diag, m, n, alpha
+                          , a.data(),lda,b.data(),m);
 
-  // NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::trsm_,  nt2::tag::cuda_<site>
-  //                           , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
-  //                           , (scalar_< ints8_<A0> >)
-  //                             (scalar_< ints8_<A1> >)
-  //                             (scalar_< ints8_<A2> >)
-  //                             (scalar_< ints8_<A3> >)
-  //                             ((container_< nt2::tag::table_, complex_<double_<A4> >, S4 >))
-  //                             ((container_< nt2::tag::table_, complex_<double_<A5> >, S5 >))
-  //                           )
-  // {
-  //   typedef void  result_type;
+     }
+  };
 
-  //    BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-  //                                            , A3 const& diag, A4 const& a, A5& b ) const
-  //    {
-  //       nt2_la_int  m  = nt2::height(a);
-  //       nt2_la_int  n  = nt2::width(b);
-  //       nt2_la_int  lda = (side=='L'||'l')? m : n ;
-  //       std::complex<double> alpha(1.0,0.);
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, complex_<double_<A4> >, S4 >))
+                              ((container_< nt2::tag::table_, complex_<double_<A5> >, S5 >))
+                            )
+  {
+    typedef void  result_type;
 
-  //       NT2_F77NAME(ztrsm)( &side, &uplo, &transa, &diag, &m, &n, &alpha
-  //                         , a.raw(),&lda,b.raw(),&m);
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b ) const
+     {
+        int  m  = nt2::height(a);
+        int  n  = nt2::width(b);
+        int  lda = (side=='L'||'l') ? m : n ;
 
-  //    }
-  // };
+        double alpha = 1.;
 
-  // NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::trsm_,  nt2::tag::cuda_<site>
-  //                           , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
-  //                           , (scalar_< ints8_<A0> >)
-  //                             (scalar_< ints8_<A1> >)
-  //                             (scalar_< ints8_<A2> >)
-  //                             (scalar_< ints8_<A3> >)
-  //                             ((container_< nt2::tag::table_, complex_<single_<A4> >, S4 >))
-  //                             ((container_< nt2::tag::table_, complex_<single_<A5> >, S5 >))
-  //                           )
-  // {
-  //   typedef void  result_type;
+        cublasZtrsm( side, uplo, transa, diag, m, n, alpha
+                          , a.data(),lda,b.data(),m);
 
-  //    BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-  //                                            , A3 const& diag, A4 const& a, A5& b ) const
-  //    {
-  //       nt2_la_int  m  = nt2::height(a);
-  //       nt2_la_int  n  = nt2::width(b);
-  //       nt2_la_int  lda = (side=='L'||'l')? m : n ;
-  //       std::complex<float> alpha(1.0,0.);
+     }
+  };
 
-  //       NT2_F77NAME(ctrsm)( &side, &uplo, &transa, &diag, &m, &n, &alpha
-  //                         , a.raw(),&lda,b.raw(),&m);
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, complex_<single_<A4> >, S4 >))
+                              ((container_< nt2::tag::table_, complex_<single_<A5> >, S5 >))
+                            )
+  {
+    typedef void  result_type;
 
-  //    }
-  // };
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b ) const
+     {
+        nt2_la_int  m  = nt2::height(a);
+        nt2_la_int  n  = nt2::width(b);
+        nt2_la_int  lda = (side=='L'||'l')? m : n ;
+        std::complex<float> alpha(1.0,0.);
 
-  // NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::trsm_,  nt2::tag::cuda_<site>
-  //                           , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(A6)(site)
-  //                           , (scalar_< ints8_<A0> >)
-  //                             (scalar_< ints8_<A1> >)
-  //                             (scalar_< ints8_<A2> >)
-  //                             (scalar_< ints8_<A3> >)
-  //                             ((container_< nt2::tag::table_, double_<A4>, S4 >))
-  //                             ((container_< nt2::tag::table_, double_<A5>, S5 >))
-  //                             (scalar_< floating_<A6> >)
-  //                           )
-  // {
-  //   typedef void  result_type;
+        NT2_F77NAME(ctrsm)( &side, &uplo, &transa, &diag, &m, &n, &alpha
+                          , a.raw(),&lda,b.raw(),&m);
 
-  //    BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-  //                                            , A3 const& diag, A4 const& a, A5& b, A6 const alpha
-  //                                             ) const
-  //    {
-  //       nt2_la_int  m  = nt2::height(a);
-  //       nt2_la_int  n  = nt2::width(b);
-  //       nt2_la_int  lda = (side=='L'||'l')? m : n ;
+     }
+  };
 
-  //       NT2_F77NAME(dtrsm)( &side, &uplo, &transa, &diag, &m, &n, &alpha
-  //                         , a.raw(),&lda,b.raw(),&m);
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(A6)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, double_<A4>, S4 >))
+                              ((container_< nt2::tag::table_, double_<A5>, S5 >))
+                              (scalar_< floating_<A6> >)
+                            )
+  {
+    typedef void  result_type;
 
-  //    }
-  // };
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b, A6 const alpha
+                                              ) const
+     {
+        int  m  = nt2::height(a);
+        int  n  = nt2::width(b);
+        int  lda = (side=='L'||'l') ? m : n ;
 
-  // NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::trsm_,  nt2::tag::cuda_<site>
-  //                           , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(A6)(site)
-  //                           , (scalar_< ints8_<A0> >)
-  //                             (scalar_< ints8_<A1> >)
-  //                             (scalar_< ints8_<A2> >)
-  //                             (scalar_< ints8_<A3> >)
-  //                             ((container_< nt2::tag::table_, single_<A4>, S4 >))
-  //                             ((container_< nt2::tag::table_, single_<A5>, S5 >))
-  //                             (scalar_< floating_<A6> >)
-  //                           )
-  // {
-  //   typedef void  result_type;
+        cublasDtrsm( side, uplo, transa, diag, m, n, alpha
+                          , a.data(),lda,b.data(),m);
 
-  //    BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-  //                                            , A3 const& diag, A4 const& a, A5& b, A6 const alpha ) const
-  //    {
-  //       nt2_la_int  m  = nt2::height(a);
-  //       nt2_la_int  n  = nt2::width(b);
-  //       nt2_la_int  lda = (side=='L'||'l')? m : n ;
+     }
+  };
 
-  //       NT2_F77NAME(strsm)( &side, &uplo, &transa, &diag, &m, &n, &alpha
-  //                         , a.raw(),&lda,b.raw(),&m);
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(A6)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, single_<A4>, S4 >))
+                              ((container_< nt2::tag::table_, single_<A5>, S5 >))
+                              (scalar_< floating_<A6> >)
+                            )
+  {
+    typedef void  result_type;
 
-  //    }
-  // };
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b, A6 const alpha ) const
+     {
+        int  m  = nt2::height(a);
+        int  n  = nt2::width(b);
+        int  lda = (side=='L'||'l') ? m : n ;
 
-  // NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::trsm_,  nt2::tag::cuda_<site>
-  //                           , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(A6)(site)
-  //                           , (scalar_< ints8_<A0> >)
-  //                             (scalar_< ints8_<A1> >)
-  //                             (scalar_< ints8_<A2> >)
-  //                             (scalar_< ints8_<A3> >)
-  //                             ((container_< nt2::tag::table_, complex_<double_<A4> >, S4 >))
-  //                             ((container_< nt2::tag::table_, complex_<double_<A5> >, S5 >))
-  //                             (scalar_< complex_<floating_<A6> > >)
-  //                           )
-  // {
-  //   typedef void  result_type;
+        cublasStrsm( side, uplo, transa, diag, m, n, alpha
+                          , a.data(),lda,b.data(),m);
 
-  //    BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-  //                                            , A3 const& diag, A4 const& a, A5& b, A6 const alpha
-  //                                             ) const
-  //    {
-  //       nt2_la_int  m  = nt2::height(a);
-  //       nt2_la_int  n  = nt2::width(b);
-  //       nt2_la_int  lda = (side=='L'||'l')? m : n ;
+     }
+  };
 
-  //       NT2_F77NAME(ztrsm)( &side, &uplo, &transa, &diag, &m, &n, &alpha
-  //                         , a.raw(),&lda,b.raw(),&m);
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(A6)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, complex_<double_<A4> >, S4 >))
+                              ((container_< nt2::tag::table_, complex_<double_<A5> >, S5 >))
+                              (scalar_< complex_<floating_<A6> > >)
+                            )
+  {
+    typedef void  result_type;
 
-  //    }
-  // };
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b, A6 const alpha
+                                              ) const
+     {
+        int  m  = nt2::height(a);
+        int  n  = nt2::width(b);
+        int  lda = (side=='L'||'l') ? m : n ;
 
-  // NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::trsm_,  nt2::tag::cuda_<site>
-  //                           , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(A6)(site)
-  //                           , (scalar_< ints8_<A0> >)
-  //                             (scalar_< ints8_<A1> >)
-  //                             (scalar_< ints8_<A2> >)
-  //                             (scalar_< ints8_<A3> >)
-  //                             ((container_< nt2::tag::table_, complex_<single_<A4> >, S4 >))
-  //                             ((container_< nt2::tag::table_, complex_<single_<A5> >, S5 >))
-  //                             (scalar_< complex_<floating_<A6> > >)
-  //                           )
-  // {
-  //   typedef void  result_type;
+        cublasZtrsm( side, uplo, transa, diag, m, n, alpha
+                          , a.data(),lda,b.data(),m);
 
-  //    BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-  //                                            , A3 const& diag, A4 const& a, A5& b ,A6 const alpha
-  //                                            ) const
-  //    {
-  //       nt2_la_int  m  = nt2::height(a);
-  //       nt2_la_int  n  = nt2::width(b);
-  //       nt2_la_int  lda = (side=='L'||'l')? m : n ;
 
-  //       NT2_F77NAME(ctrsm)( &side, &uplo, &transa, &diag, &m, &n, &alpha
-  //                         , a.raw(),&lda,b.raw(),&m);
+     }
+  };
 
-  //    }
-  // };
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(A6)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, complex_<single_<A4> >, S4 >))
+                              ((container_< nt2::tag::table_, complex_<single_<A5> >, S5 >))
+                              (scalar_< complex_<floating_<A6> > >)
+                            )
+  {
+    typedef void  result_type;
+
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b ,A6 const alpha
+                                             ) const
+     {
+        int  m  = nt2::height(a);
+        int  n  = nt2::width(b);
+        int  lda = (side=='L'||'l') ? m : n ;
+
+        cublasCtrsm( side, uplo, transa, diag, m, n, alpha
+                          , a.data(),lda,b.data(),m);
+     }
+  };
 
 } }
 
