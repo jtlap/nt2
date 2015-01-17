3f8675aa71700aeba0397b224354e1fb06005990
jtlap <jtlapreste@gmail.com>
2015-01-17 08:47:22 +0100

    suppressing comments in bitget.cpp

--- modules/boost/simd/base/unit/bitwise/simd/bitget.cpp
+++ modules/boost/simd/base/unit/bitwise/simd/bitget.cpp
@@ -22,43 +22,6 @@
 #include <boost/simd/include/constants/zero.hpp>
 
 
-
-// NT2_TEST_CASE_TPL ( bitget_ui_1,  BOOST_SIMD_SIMD_UNSIGNED_TYPES)
-// {
-//   using boost::simd::bitget;
-//   using boost::simd::tag::bitget_;
-//   using boost::simd::native;
-//   typedef BOOST_SIMD_DEFAULT_EXTENSION                          ext_t;
-//   typedef native<T,ext_t>                                          vT;
-//   typedef typename boost::dispatch::meta::as_integer<T>::type      iT;
-//   typedef typename boost::dispatch::meta::call<bitget_(vT,iT)>::type r_t;
-
-//   // specific values tests
-//   NT2_TEST_EQUAL(bitget(boost::simd::Two<vT>(),1), boost::simd::One<r_t>());
-//   NT2_TEST_EQUAL(bitget(boost::simd::Mone<vT>(),(sizeof(T)*8-1)), boost::simd::One<r_t>());
-//   NT2_TEST_EQUAL(bitget(boost::simd::Mone<vT>(),(sizeof(T)*8-2)), boost::simd::Three<r_t>());
-//   NT2_TEST_EQUAL(bitget(boost::simd::One<vT>(),1), boost::simd::Zero<r_t>());
-//   NT2_TEST_EQUAL(bitget(boost::simd::Zero<vT>(),1), boost::simd::Zero<r_t>());
-// } // end of test for unsigned_int_
-
-// NT2_TEST_CASE_TPL ( bitget_si_1,  BOOST_SIMD_SIMD_INTEGRAL_SIGNED_TYPES)
-// {
-//   using boost::simd::bitget;
-//   using boost::simd::tag::bitget_;
-//   using boost::simd::native;
-//   typedef BOOST_SIMD_DEFAULT_EXTENSION                          ext_t;
-//   typedef native<T,ext_t>                                          vT;
-//   typedef typename boost::dispatch::meta::as_integer<T>::type      iT;
-//   typedef typename boost::dispatch::meta::call<bitget_(vT,iT)>::type r_t;
-
-//   // specific values tests
-//   NT2_TEST_EQUAL(bitget(boost::simd::Two<vT>(),1), boost::simd::One<r_t>());
-//   NT2_TEST_EQUAL(bitget(boost::simd::Mone<vT>(),(sizeof(T)*8-1)), boost::simd::One<r_t>());
-//   NT2_TEST_EQUAL(bitget(boost::simd::Mone<vT>(),(sizeof(T)*8-2)), boost::simd::Three<r_t>());
-//   NT2_TEST_EQUAL(bitget(boost::simd::One<vT>(),1), boost::simd::Zero<r_t>());
-//   NT2_TEST_EQUAL(bitget(boost::simd::Zero<vT>(),1), boost::simd::Zero<r_t>());
-// } // end of test for unsigned_int_
-
 NT2_TEST_CASE_TPL ( bitget_ui_2,  BOOST_SIMD_SIMD_UNSIGNED_TYPES)
 {
   using boost::simd::bitget;
