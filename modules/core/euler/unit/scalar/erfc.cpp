ad2ddffceb98987bc4196de2c5847081d0b76e40
jtlap <jtlapreste@gmail.com>
2015-01-20 13:14:15 +0100

    adding erfcx and correcting erfc for large value in double

--- modules/core/euler/unit/scalar/erfc.cpp
+++ modules/core/euler/unit/scalar/erfc.cpp
@@ -12,10 +12,11 @@
 #include <nt2/sdk/unit/module.hpp>
 #include <nt2/sdk/unit/tests/relation.hpp>
 #include <nt2/sdk/unit/tests/type_expr.hpp>
+#include <nt2/sdk/unit/tests/basic.hpp>
 #include <nt2/sdk/unit/tests/ulp.hpp>
 #include <nt2/sdk/unit/module.hpp>
 #include <boost/simd/sdk/config.hpp>
-
+#include <nt2/include/functions/is_less.hpp>
 #include <nt2/include/constants/half.hpp>
 #include <nt2/include/constants/mzero.hpp>
 #include <nt2/include/constants/one.hpp>
@@ -23,6 +24,8 @@
 #include <nt2/include/constants/zero.hpp>
 #include <nt2/include/constants/inf.hpp>
 #include <nt2/include/constants/nan.hpp>
+#include <nt2/include/constants/ten.hpp>
+#include <nt2/include/constants/eps.hpp>
 
 NT2_TEST_CASE_TPL ( erfc_real,  NT2_REAL_TYPES)
 {
@@ -44,4 +47,8 @@ NT2_TEST_CASE_TPL ( erfc_real,  NT2_REAL_TYPES)
   NT2_TEST_ULP_EQUAL(erfc(nt2::One<T>()), T(0.157299207050285), 200);
   NT2_TEST_ULP_EQUAL(erfc(nt2::Two<T>()), T(0.004677734981047), 200);
   NT2_TEST_ULP_EQUAL(erfc(nt2::Zero<T>()), nt2::One<r_t>(), 200);
+  NT2_TEST_ULP_EQUAL(erfc(T(6)), T(2.151973671249892e-17), 200);
+  NT2_TEST(nt2::is_less(erfc((nt2::Ten<T>())- T(2.088487583762545e-45)), nt2::Eps<T>()));
+  NT2_TEST(nt2::is_less(erfc(T(15))- T(7.212994172451206e-100), nt2::Eps<T>()));
 }
+
