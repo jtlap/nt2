4e4155410731dd6d36d4dc2a3bbfa2a3fde22143
Joel Falcou <joel.falcou@lri.fr>
2015-01-23 13:35:30 +0100

    Missing resize include in along definition

--- modules/core/restructuring/include/nt2/core/functions/along.hpp
+++ modules/core/restructuring/include/nt2/core/functions/along.hpp
@@ -1,6 +1,6 @@
 //==============================================================================
-//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2009 -2015 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2012 -2015 NumScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -10,7 +10,7 @@
 #define NT2_CORE_FUNCTIONS_ALONG_HPP_INCLUDED
 
 #include <nt2/include/functor.hpp>
-
+#include <nt2/core/container/dsl/details/resize.hpp>
 
 namespace nt2
 {
