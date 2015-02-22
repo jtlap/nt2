4a733844c779ed8d1dac27be366016acf2a5a497
jtlap <jtlapreste@gmail.com>
2015-02-22 20:58:22 +0100

    qrupdate forgotten

--- modules/core/linalg/include/nt2/linalg/functions/qrupdate.hpp
+++ modules/core/linalg/include/nt2/linalg/functions/qrupdate.hpp
@@ -1,6 +1,5 @@
 //==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2015 J.T. Lapreste
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -47,7 +46,7 @@ namespace nt2
    * where u and v are column vectors of appropriate lengths and q and r are a previously
    * computed qr-factorization of a
    *
-   * @see @funcref{qr}
+   * @see @funcref{qr}, @funcref{qrdelete}, @funcref{qrshift}, @funcref{qrinsert}
    */
 }
 
