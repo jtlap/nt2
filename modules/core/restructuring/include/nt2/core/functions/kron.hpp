06f575b40ff11123c4f0d20da73b18e27e8ba04e
jtlap <jtlapreste@gmail.com>
2015-03-18 22:51:30 +0100

    changing comment in kron

--- modules/core/restructuring/include/nt2/core/functions/kron.hpp
+++ modules/core/restructuring/include/nt2/core/functions/kron.hpp
@@ -8,30 +8,6 @@
 #ifndef NT2_CORE_FUNCTIONS_KRON_HPP_INCLUDED
 #define NT2_CORE_FUNCTIONS_KRON_HPP_INCLUDED
 
-// Copyright (c) 2009, Bruno Luong
-// All rights reserved.
-
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in
-//       the documentation and/or other materials provided with the distribution
-
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
 
 #include <nt2/include/functor.hpp>
 #include <nt2/sdk/meta/value_as.hpp>
@@ -83,7 +59,7 @@ namespace nt2
     @endcode
 
     The result is a large matrix formed by taking all possible
-    products between the elements of X and those of Y.
+    products between the elements of a0 and those of a1.
 
 
     @param a0
