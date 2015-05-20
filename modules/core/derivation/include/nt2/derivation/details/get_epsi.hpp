c71f99b3e2ec082dda5d3b11da8118bed32578e3
jtlap <jtlapreste@gmail.com>
2015-05-20 13:22:56 +0200

    adding d2f computation of the second derivative

--- modules/core/derivation/include/nt2/derivation/details/get_epsi.hpp
+++ modules/core/derivation/include/nt2/derivation/details/get_epsi.hpp
@@ -8,6 +8,7 @@
 #ifndef NT2_MODULES_CORE_DERIVATION_UNIT_DETAILS_GET_EPSI_HPP_INCLUDED
 #define NT2_MODULES_CORE_DERIVATION_UNIT_DETAILS_GET_EPSI_HPP_INCLUDED
 #include <nt2/include/constants/derivinc.hpp>
+#include <nt2/include/constants/derivinc2.hpp>
 #include <boost/mpl/int.hpp>
 #include <nt2/sdk/meta/is_scalar.hpp>
 
@@ -35,6 +36,29 @@ namespace nt2 { namespace details
       return   boost::proto::value(boost::proto::child_c<2>(in));
     }
     static BOOST_FORCEINLINE R epsi(const A1& in, const boost::mpl::long_<4> & )
+    {
+      return  boost::proto::value(boost::proto::child_c<2>(in));
+    }
+
+    static BOOST_FORCEINLINE R epsi2(const A1&, const boost::mpl::long_<2> & )
+    {
+      return Derivinc2<R>();
+    }
+    static BOOST_FORCEINLINE R epsi2(const A1& in, const boost::mpl::long_<3> & )
+    {
+      typedef typename boost::proto::result_of::child_c<A1&, 2>::value_type child2_t;
+      typedef typename meta::is_scalar<child2_t>::type                      choose_t;
+      return epsi2(in, choose_t(), boost::mpl::long_<3>());
+    }
+    static BOOST_FORCEINLINE R epsi2(const A1&, boost::mpl::false_, const boost::mpl::long_<3> & )
+    {
+      return  Derivinc2<R>();
+    }
+    static BOOST_FORCEINLINE R epsi2(const A1& in, boost::mpl::true_, const boost::mpl::long_<3> & )
+    {
+      return   boost::proto::value(boost::proto::child_c<2>(in));
+    }
+    static BOOST_FORCEINLINE R epsi2(const A1& in, const boost::mpl::long_<4> & )
     {
       return  boost::proto::value(boost::proto::child_c<2>(in));
     }
