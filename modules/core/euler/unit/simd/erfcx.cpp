7842d07be147c47c7b0931eaeefadc5ff375575e
Joel Falcou <joel.falcou@lri.fr>
2015-05-21 14:12:46 +0200

    Fix types in erfcx simd types

--- modules/core/euler/unit/simd/erfcx.cpp
+++ modules/core/euler/unit/simd/erfcx.cpp
@@ -26,7 +26,7 @@
 #include <nt2/include/constants/inf.hpp>
 #include <nt2/include/constants/nan.hpp>
 
-NT2_TEST_CASE_TPL ( erfcx_real,  NT2_REAL_TYPES)
+NT2_TEST_CASE_TPL ( erfcx_real,  NT2_SIMD_REAL_TYPES)
 {
   using nt2::erfcx;
   using nt2::tag::erfcx_;
