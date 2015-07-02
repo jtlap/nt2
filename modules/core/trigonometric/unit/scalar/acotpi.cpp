b8a1f62de922e57f0083dc5ed2e30680e792ac5a
jtlap <jtlapreste@gmail.com>
2015-07-02 23:21:36 +0200

    adding acotpi with unit tests to trigonometric

---
+++ modules/core/trigonometric/unit/scalar/acotpi.cpp
@@ -0,0 +1,50 @@
+//==============================================================================
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <nt2/trigonometric/include/functions/acotpi.hpp>
+
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <nt2/sdk/unit/tests/relation.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
+#include <nt2/sdk/unit/tests/basic.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <boost/simd/sdk/config.hpp>
+
+#include <nt2/include/constants/mone.hpp>
+#include <nt2/include/constants/one.hpp>
+#include <nt2/include/constants/zero.hpp>
+#include <nt2/include/constants/inf.hpp>
+#include <nt2/include/constants/minf.hpp>
+#include <nt2/include/constants/nan.hpp>
+#include <nt2/include/constants/pio_2.hpp>
+#include <nt2/include/constants/pio_4.hpp>
+
+NT2_TEST_CASE_TPL ( acotpi_real,  NT2_REAL_TYPES)
+{
+  using nt2::acotpi;
+  using nt2::tag::acotpi_;
+
+  typedef typename nt2::meta::call<acotpi_(T)>::type r_t;
+  typedef T wished_r_t;
+
+  // return type conformity test
+  NT2_TEST_TYPE_IS(r_t, wished_r_t);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Inf<T>()), 0, 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Minf<T>()), 0, 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Nan<T>()), nt2::Nan<r_t>(), 0.5);
+#endif
+  NT2_TEST_ULP_EQUAL(acotpi(-nt2::Zero<T>()), -0.5, 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Mone<T>()), -0.25, 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::One<T>()), 0.25, 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Zero<T>()), 0.5, 0.5);
+}
---
+++ modules/core/trigonometric/unit/simd/acotpi.cpp
@@ -0,0 +1,55 @@
+//==============================================================================
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#include <nt2/trigonometric/include/functions/acotpi.hpp>
+
+#include <nt2/sdk/functor/meta/call.hpp>
+#include <boost/simd/sdk/simd/native.hpp>
+#include <nt2/sdk/unit/tests/type_expr.hpp>
+#include <nt2/sdk/unit/tests/ulp.hpp>
+#include <nt2/include/functions/splat.hpp>
+#include <nt2/include/functions/unary_minus.hpp>
+#include <nt2/sdk/unit/module.hpp>
+#include <boost/simd/sdk/config.hpp>
+#include <boost/simd/sdk/simd/io.hpp>
+
+#include <nt2/include/constants/mone.hpp>
+#include <nt2/include/constants/one.hpp>
+#include <nt2/include/constants/zero.hpp>
+#include <nt2/include/constants/inf.hpp>
+#include <nt2/include/constants/minf.hpp>
+#include <nt2/include/constants/nan.hpp>
+#include <nt2/include/constants/half.hpp>
+#include <nt2/include/constants/quarter.hpp>
+
+NT2_TEST_CASE_TPL ( acotpi_real_1,  NT2_SIMD_REAL_TYPES)
+{
+  using nt2::acotpi;
+  using nt2::tag::acotpi_;
+  using boost::simd::native;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef native<T,ext_t>                  vT;
+
+  typedef typename nt2::meta::call<acotpi_(vT)>::type r_t;
+  typedef vT wished_r_t;
+
+  // return type conformity test
+  NT2_TEST_TYPE_IS(r_t, wished_r_t);
+
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Inf<vT>()), nt2::Zero<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Minf<vT>()), nt2::Zero<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Nan<vT>()), nt2::Nan<r_t>(), 0.5);
+#endif
+  NT2_TEST_ULP_EQUAL(acotpi(-nt2::Zero<vT>()), -nt2::Half<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Mone<vT>()), -nt2::Quarter<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::One<vT>()), nt2::Quarter<r_t>(), 0.5);
+  NT2_TEST_ULP_EQUAL(acotpi(nt2::Zero<vT>()), nt2::Half<r_t>(), 0.5);
+}
